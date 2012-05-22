696185d592b1b81988a97eaa2d18f0f087e71582
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-05-22 10:55:05 -0400

    make sure exercise type is serializable

--- src/mitll/langtest/shared/Exercise.java
+++ src/mitll/langtest/shared/Exercise.java
@@ -15,7 +15,7 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 public class Exercise implements IsSerializable  {
-  public enum EXERCISE_TYPE { RECORD, TEXT_RESPONSE }
+  public enum EXERCISE_TYPE implements IsSerializable { RECORD, TEXT_RESPONSE }
 
   private String plan;
   private String content;
