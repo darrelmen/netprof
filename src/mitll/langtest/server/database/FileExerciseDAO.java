402f650331955189229e02e5277fa977688e9776
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-10-31 16:27:25 -0400

    does fast and slow 752 lesson
    Former-commit-id: fe074d776fca6bd8140a4d547dd440e4782a346c [formerly d87dc4b621dd3123fecdc2b877e44da43457b6ee]
    Former-commit-id: a8b268578fd22da6ecd3a07059aa23a7ea9d2b32

--- src/mitll/langtest/server/database/FileExerciseDAO.java
+++ src/mitll/langtest/server/database/FileExerciseDAO.java
@@ -1,16 +1,28 @@
 package mitll.langtest.server.database;
 
+import audio.imagewriter.AudioConverter;
+import audio.tools.FileCopier;
+import com.sun.media.sound.WaveFileWriter;
+import mitll.langtest.server.AudioCheck;
 import mitll.langtest.server.AudioConversion;
 import mitll.langtest.shared.Exercise;
 import org.apache.log4j.Logger;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +40,10 @@ public class FileExerciseDAO implements ExerciseDAO {
   private static Logger logger = Logger.getLogger(FileExerciseDAO.class);
   private static final String ENCODING = "UTF8";
   private static final String LESSON_FILE = "lesson-737.csv";
+  private static final String FAST_AND_SLOW_REF = "fastAndSlowRef";
+  private static final String LESSON_PLAN = "lesson.plan";
+  private static final String FAST = "fast";
+  private static final String SLOW = "slow";
 
   private List<Exercise> exercises;
 
@@ -39,23 +55,13 @@ public class FileExerciseDAO implements ExerciseDAO {
   /**
    * TODO : write to h2
    * @see mitll.langtest.server.database.DatabaseImpl#getExercises()
-   * @paramx installPath
+   * @param installPath
    */
   public void readExercises(String installPath) {
     if (exercises != null) return;
     String exerciseFile = LESSON_FILE;
-    InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(exerciseFile);
-    if (resourceAsStream == null) {
-      logger.error("can't find " + exerciseFile);
-      try {
-        resourceAsStream = new FileInputStream("C:\\Users\\go22670\\DLITest\\LangTest\\src\\" +LESSON_FILE);
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      }
-      if (resourceAsStream == null) {
-        return;
-      }
-    }
+    InputStream resourceAsStream = getExerciseListStream(exerciseFile);
+    if (resourceAsStream == null) return;
 
     try {
       AudioConversion audioConversion = new AudioConversion();
@@ -72,21 +78,7 @@ public class FileExerciseDAO implements ExerciseDAO {
         String translit = split[4];
         String english = split[5];
 
-        String content = "<div class=\"Instruction\">\n" +
-            "<span class=\"Instruction-title\">Say:</span>\n" +
-            "<span class=\"Instruction-data\"> " + arabic +
-            "</span>\n" +
-            "</div>\n" +
-            "<div class=\"Instruction\">\n" +
-            "<span class=\"Instruction-title\">Transliteration:</span>\n" +
-            "<span class=\"Instruction-data\"> " + translit+
-            "</span>\n" +
-            "</div>\n" +
-            "<div class=\"Instruction\">\n" +
-            "<span class=\"Instruction-title\">Translation:</span>\n" +
-            "<span class=\"Instruction-data\"> " + english+
-            "</span>\n" +
-            "</div>";
+        String content = getContent(arabic, translit, english);
         String audioRef =  "ref"+ File.separator+name+File.separator+"reference.wav";
        /* if (installPath.length() > 0) {
           if (!installPath.endsWith(File.separator)) installPath += File.separator;
@@ -116,6 +108,101 @@ public class FileExerciseDAO implements ExerciseDAO {
     }
   }
 
+  public void readFastAndSlowExercises(String installPath) {
+    if (exercises != null) return;
+    String exerciseFile = LESSON_PLAN;
+    InputStream resourceAsStream = getExerciseListStream(exerciseFile);
+    if (resourceAsStream == null) return;
+
+    try {
+      AudioConversion audioConversion = new AudioConversion();
+      exercises = new ArrayList<Exercise>();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream,ENCODING));
+      String line2;
+      int count = 0;
+      logger.debug("using install path " + installPath);
+      while ((line2 = reader.readLine()) != null) {
+      //  if (count++ > 10) break;
+        String[] split = line2.split("\\|");
+        String lastCol = split[6];
+        String[] split1 = lastCol.split("->");
+        String name = split1[1].trim();
+        String displayName = name;
+        String arabic = split1[2];
+        String translit = split1[3];
+        String english = split1[4];
+
+        String content = getContent(arabic, translit, english);
+        String fastAudioRef = FAST_AND_SLOW_REF + File.separator+name+File.separator+ FAST + ".wav";
+        String slowAudioRef = FAST_AND_SLOW_REF+ File.separator+name+File.separator+ SLOW + ".wav";
+
+        for (String audioRef : new String[]{fastAudioRef,slowAudioRef}) {
+          File file = new File(audioRef);
+          if (!file.exists()) {
+            file = new File(installPath,audioRef);
+          }
+          if (!file.exists()) {
+            if (count++ < 5) logger.debug("can't find audio file " + file.getAbsolutePath());
+          } else {
+            audioConversion.ensureWriteMP3(audioRef,installPath);
+          }
+        }
+
+          Exercise exercise = new Exercise("repeat", displayName, content,
+              ensureForwardSlashes(fastAudioRef), ensureForwardSlashes(slowAudioRef), arabic);
+          exercises.add(exercise);
+      }
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    if (exercises.isEmpty()) {
+      logger.error("no exercises found in " + exerciseFile +"?");
+    }
+    else {
+      logger.debug("found " + exercises.size() + " exercises, first is " + exercises.iterator().next());
+    }
+  }
+
+  private String getContent(String arabic, String translit, String english) {
+    return "<div class=\"Instruction\">\n" +
+        "<span class=\"Instruction-title\">Say:</span>\n" +
+        "<span class=\"Instruction-data\"> " + arabic +
+        "</span>\n" +
+        "</div>\n" +
+        "<div class=\"Instruction\">\n" +
+        "<span class=\"Instruction-title\">Transliteration:</span>\n" +
+        "<span class=\"Instruction-data\"> " + translit+
+        "</span>\n" +
+        "</div>\n" +
+        "<div class=\"Instruction\">\n" +
+        "<span class=\"Instruction-title\">Translation:</span>\n" +
+        "<span class=\"Instruction-data\"> " + english+
+        "</span>\n" +
+        "</div>";
+  }
+
+  private InputStream getExerciseListStream(String exerciseFile) {
+    InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(exerciseFile);
+    if (resourceAsStream == null) {
+      logger.error("can't find " + exerciseFile);
+      try {
+        String n2 = "C:\\Users\\go22670\\DLITest\\clean\\netPron2\\src\\";
+        String name = n2+ exerciseFile;
+        File file = new File(name);
+        boolean exists = file.exists();
+        if (!exists) System.err.println("can't find " + file);
+        resourceAsStream = new FileInputStream(name);
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+      if (resourceAsStream == null) {
+        return null;
+      }
+    }
+    return resourceAsStream;
+  }
+
   private String ensureForwardSlashes(String wavPath) {
     return wavPath.replaceAll("\\\\", "/");
   }
@@ -124,8 +211,60 @@ public class FileExerciseDAO implements ExerciseDAO {
     return exercises;
   }
 
-/*  public static void main(String [] arg) {
-    List<Exercise> rawExercises = new FileExerciseDAO(*//*"war"*//*).getRawExercises();
-    System.out.println("first is " + rawExercises.iterator().next());
-  }*/
+  public void convertPlan() {
+    InputStream resourceAsStream = getExerciseListStream("lesson.plan");
+
+    if (resourceAsStream == null) return;
+
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream,ENCODING));
+      String line2;
+      int count = 0;
+      while ((line2 = reader.readLine()) != null) {
+       // if (count++ > 3) break;
+        String[] split = line2.split("\\|");
+        String secondCol = split[1];
+        System.out.println("read " + line2 + " second " + secondCol);
+        String name = secondCol.split(",")[1].trim();
+        String longFile = "reference.wav";
+        String shortFile = "REF_MALE.wav";
+        File base = new File("C:\\Users\\go22670\\mt_repo\\github\\hs-levantine\\data752\\"+name);
+        File newRefDir = new File("C:\\Users\\go22670\\mt_repo\\github\\hs-levantine\\newRef\\"+name);
+        newRefDir.mkdirs();
+        if (!base.exists()) {
+          System.err.println("Can't find " + base.getAbsolutePath());
+          continue;
+        }
+        AudioInputStream shortAIS = AudioSystem.getAudioInputStream(new File(base,shortFile));
+        File longFileFile = new File(base, longFile);
+        AudioInputStream longAIS = AudioSystem.getAudioInputStream(longFileFile);
+
+        long shortFrames = shortAIS.getFrameLength();
+        long longFrames = longAIS.getFrameLength();
+        AudioFormat format = shortAIS.getFormat();
+        long shortBytes = shortFrames * format.getFrameSize();
+        long skip = longAIS.skip(shortBytes);
+        if (skip   != shortBytes) System.err.println("skipped " + skip + " but asked for " + shortBytes);
+
+        new AudioConverter().trim("C:\\Users\\go22670\\sox-14-3-2\\sox.exe",
+            longFileFile.getAbsolutePath(),
+            new File(newRefDir, SLOW + ".wav").getAbsolutePath(),
+            shortFrames,
+            longFrames-shortFrames);
+        shortAIS.close();
+        longAIS.close();
+
+        new FileCopier().copy(new File(base, shortFile).getAbsolutePath(), new File(newRefDir, FAST + ".wav").getAbsolutePath());
+      }
+      reader.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String [] arg) {
+    new FileExerciseDAO().convertPlan();
+    /*List<Exercise> rawExercises = new FileExerciseDAO(*//**//*"war"*//**//*).getRawExercises();
+    System.out.println("first is " + rawExercises.iterator().next());*/
+  }
 }
