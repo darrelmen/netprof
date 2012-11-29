bb0b332d16076462dae2f89bc4b9aa0459ebf2ae
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-11-29 18:20:47 -0500

    changes to support dari - don't transfer all of the Exercise data on start, added Export for mira3
    Former-commit-id: c3b1671897d5c740eac107df5e5088e1c041cd56
    Former-commit-id: 43116c8faf109e773e0e7ecce56b887c0761e1ea

---
+++ src/mitll/langtest/shared/ExerciseShell.java
@@ -0,0 +1,28 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 11/29/12
+ * Time: 3:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExerciseShell implements IsSerializable {
+  protected String tooltip;
+  protected String id;
+
+  public ExerciseShell() {}
+  public ExerciseShell(String id, String tooltip) {
+    this.id = id;
+    this.tooltip = tooltip;
+  }
+
+  public String getID() { return id; }
+  public String getTooltip() { return tooltip; }
+
+  public String toString() {
+      return "Exercise id =" +id;
+  }
+}
