0d6b09716a3bb4a1ee29a90d06bf166f565b8379
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-12-18 19:59:47 -0500

    partial steps towards waveform display on record
    Former-commit-id: 2d3eeb21045f39492fb786935a1f6bbb27fafd22 [formerly 262080cf70c94af14bf812a5eb2b2f6619ffe87f] [formerly 8202f9cf5cf439b99b8c820ea8129d48e1474026 [formerly 7f2b9626a58be509eadc71eb15cf474728554166]]
    Former-commit-id: aac05709179ce19df0c2bcbfa68f71d76ae82b46 [formerly 9e771f8671576c41d70544136fc72a9add7627d3]
    Former-commit-id: 95558787a4d9812f9e10cc9d04f257f7cf85fe74

---
+++ src/mitll/langtest/client/exercise/WaveformExercisePanel.java
@@ -0,0 +1,118 @@
+package mitll.langtest.client.exercise;
+
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.scoring.AudioPanel;
+import mitll.langtest.client.scoring.PostAudioRecordButton;
+import mitll.langtest.client.sound.PlayAudioPanel;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.shared.AudioAnswer;
+import mitll.langtest.shared.Exercise;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 12/18/12
+ * Time: 6:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WaveformExercisePanel extends ExercisePanel {
+  /**
+   * @see mitll.langtest.client.exercise.ExercisePanelFactory#getExercisePanel(mitll.langtest.shared.Exercise)
+   * @param e
+   * @param service
+   * @param userFeedback
+   * @param controller
+   */
+  public WaveformExercisePanel(final Exercise e, final LangTestDatabaseAsync service, final UserFeedback userFeedback,
+                                   final ExerciseController controller) {
+    super(e,service,userFeedback,controller);
+  }
+
+  /**
+   * Has a answerPanel mark to indicate when the saved audio has been successfully posted to the server.
+   *
+   *
+   * @see mitll.langtest.client.exercise.ExercisePanel#ExercisePanel(mitll.langtest.shared.Exercise, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.user.UserFeedback, mitll.langtest.client.exercise.ExerciseController)
+   * @param exercise
+   * @param service
+   * @param controller
+   * @param index
+   * @return
+   */
+  @Override
+  protected Widget getAnswerWidget(Exercise exercise, LangTestDatabaseAsync service, ExerciseController controller, final int index) {
+    return new RecordAudioPanel(service, index);
+  }
+
+  @Override
+  protected String getQuestionPrompt(boolean promptInEnglish) {
+    return getSpokenPrompt(promptInEnglish);
+  }
+
+  /**
+   * on the server, notice which audio posts have arrived, and take the latest ones...
+   * <br></br>
+   * Move on to next exercise...
+   *
+   * @param service
+   * @param userFeedback
+   * @param controller
+   * @param completedExercise
+   */
+  @Override
+  protected void postAnswers(LangTestDatabaseAsync service, UserFeedback userFeedback, ExerciseController controller, Exercise completedExercise) {
+    controller.loadNextExercise(completedExercise);
+  }
+
+
+  /**
+   * An ASR scoring panel with a record button.
+   */
+  private class RecordAudioPanel extends AudioPanel {
+    private final int index;
+
+    private PostAudioRecordButton postAudioRecordButton;
+
+    /**
+     * @param service
+     * @paramx controller
+     * @param index
+     */
+    public RecordAudioPanel(LangTestDatabaseAsync service, int index) {
+      super(null, service, controller.getSoundManager(), true);
+      this.index = index;
+    }
+
+    @Override
+    protected PlayAudioPanel makePlayAudioPanel(Widget toAdd) {
+      postAudioRecordButton = new PostAudioRecordButton(exercise, controller, service, index) {
+        @Override
+        public void useResult(AudioAnswer result) {
+          //setRefAudio(refAudio, exercise.getRefSentence());
+          getImagesForPath(wavToMP3(result.path));
+        }
+      };
+
+      return new PlayAudioPanel(soundManager) {
+        @Override
+        protected void addButtons() {
+          add(postAudioRecordButton.getRecord());
+          super.addButtons();
+        }
+      };
+    }
+
+    @Override
+    protected void onUnload() {
+      super.onUnload();
+      postAudioRecordButton.onUnload();
+    }
+  }
+  private static final String WAV = ".wav";
+  private static final String MP3 = ".mp3";
+  private String wavToMP3(String path) {
+    return (path.endsWith(WAV)) ? path.replace(WAV, MP3) : path;
+  }
+
+}
