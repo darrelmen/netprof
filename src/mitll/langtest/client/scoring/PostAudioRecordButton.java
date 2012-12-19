5b722e8d55fa6e4326d1bc4e0b00e403dd61b6a6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-12-18 19:59:47 -0500

    partial steps towards waveform display on record
    Former-commit-id: 7ea3f423c0ff9109678f011cb848f944e2359d5b
    Former-commit-id: 687670f0ac5554c4895a18ec3c7285b02b1f03a6 [formerly 9d3fb9aa306a303e37d57a9d2f211866e3f8690b]
    Former-commit-id: ca600d742260c0663166cc85e5d9199787f9d8bb

---
+++ src/mitll/langtest/client/scoring/PostAudioRecordButton.java
@@ -0,0 +1,108 @@
+package mitll.langtest.client.scoring;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.PopupPanel;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.recorder.RecordButton;
+import mitll.langtest.shared.AudioAnswer;
+import mitll.langtest.shared.Exercise;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 12/18/12
+ * Time: 6:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PostAudioRecordButton extends RecordButton {
+  private static final String RECORD = "record";
+  private static final String STOP = "stop";
+  private static final int AUTO_STOP_DELAY = 15000; // millis
+
+  private int index;
+  private int reqid = 0;
+  private final Exercise exercise;
+  private final ExerciseController controller;
+  private final LangTestDatabaseAsync service;
+
+  public PostAudioRecordButton(Exercise exercise, ExerciseController controller, LangTestDatabaseAsync service, int index) {
+    super(new Button(RECORD), AUTO_STOP_DELAY);
+
+    this.index = index;
+    this.exercise = exercise;
+    this.controller = controller;
+    this.service = service;
+  }
+
+  @Override
+  protected void stopRecording() {
+    controller.stopRecording();
+    reqid++;
+    service.writeAudioFile(controller.getBase64EncodedWavFile()
+        , exercise.getPlan(), exercise.getID(),
+        "" + index, "" + controller.getUser(),
+        false, reqid, new AsyncCallback<AudioAnswer>() {
+      public void onFailure(Throwable caught) {
+        showPopup(AudioAnswer.Validity.INVALID.getPrompt());
+      }
+
+      /**
+       * Feedback for when audio isn't valid for some reason.
+       * @param toShow
+       */
+      private void showPopup(String toShow) {
+        final PopupPanel popupImage = new PopupPanel(true);
+        popupImage.add(new HTML(toShow));
+        popupImage.showRelativeTo(getRecord());
+        Timer t = new Timer() {
+          @Override
+          public void run() {
+            popupImage.hide();
+          }
+        };
+        t.schedule(3000);
+      }
+
+      public void onSuccess(AudioAnswer result) {
+        System.out.println("PostAudioRecordButton : Got audio answer " + result);
+        if (result.reqid != reqid) {
+          System.out.println("ignoring old response " + result);
+          return;
+        }
+        if (result.validity == AudioAnswer.Validity.OK) {
+          //   audioPanel.setRefAudio(refAudio, exercise.getRefSentence());
+          useResult(result);
+          //audioPanel.getImagesForPath(wavToMP3(result.path));
+        } else {
+          showPopup(result.validity.getPrompt());
+        }
+      }
+    });
+  }
+
+  @Override
+  protected void startRecording() {
+    controller.startRecording();
+  }
+
+  /**
+   * So we don't want the button changing width when we change the text.
+   */
+  @Override
+  protected void showRecording() {
+    int w = getRecord().getOffsetWidth();
+    ((Button) getRecord()).setText(STOP);
+    if (getRecord().getOffsetWidth() < w) getRecord().setWidth(w + "px");
+  }
+
+  @Override
+  protected void showStopped() {
+    ((Button) getRecord()).setText(RECORD);
+  }
+
+  public void useResult(AudioAnswer result) {}
+}
