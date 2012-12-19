de8a42e28f51e668b59a9c042010d6831342180e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2012-12-19 17:52:37 -0500

    changes to support keyboard interaction with buttons and user registration and login
    Former-commit-id: a495945a6f7c52c07f41b664610c1ae9a581e6b5

--- src/mitll/langtest/server/database/UserDAO.java
+++ src/mitll/langtest/server/database/UserDAO.java
@@ -6,8 +6,6 @@ import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,13 +23,19 @@ public class UserDAO {
    * <p/>
    * Uses return generated keys to get the user id
    *
+   * @see DatabaseImpl#addUser
    * @param age
    * @param gender
    * @param experience
    * @param ipAddr
+   * @param firstName
+   * @param lastName
+   * @param nativeLang
+   * @param dialect
+   * @param userID
    * @return newly inserted user id, or 0 if something goes horribly wrong
    */
-  public synchronized long addUser(int age, String gender, int experience, String ipAddr) {
+  public synchronized long addUser(int age, String gender, int experience, String ipAddr, String firstName, String lastName, String nativeLang, String dialect, String userID) {
     try {
       // there are much better ways of doing this...
       long max = 0;
@@ -42,14 +46,19 @@ public class UserDAO {
 
       //System.out.println("adding " + age + " and " + gender + " and " + experience);
     //  statement = connection.prepareStatement("INSERT INTO users(id,age,gender,experience,ipaddr) VALUES(?,?,?,?,?);", Statement.RETURN_GENERATED_KEYS);
-      statement = connection.prepareStatement("INSERT INTO users(id,age,gender,experience,ipaddr) VALUES(?,?,?,?,?);");
+      statement = connection.prepareStatement("INSERT INTO users(id,age,gender,experience,ipaddr,firstName,lastName,nativeLang,dialect, userID) VALUES(?,?,?,?,?,?,?,?,?,?);");
       int i = 1;
       long newID = max + 1;
       statement.setLong(i++, newID);
       statement.setInt(i++, age);
       statement.setInt(i++, gender.equalsIgnoreCase("male") ? 0 : 1);
       statement.setInt(i++, experience);
-      statement.setString(i, ipAddr);
+      statement.setString(i++, ipAddr);
+      statement.setString(i++, firstName);
+      statement.setString(i++, lastName);
+      statement.setString(i++, nativeLang);
+      statement.setString(i++, dialect);
+      statement.setString(i++, userID);
       statement.executeUpdate();
 
       if (false) {
@@ -73,13 +82,45 @@ public class UserDAO {
     return 0;
   }
 
+  public synchronized int userExists(String id) {
+    int val = -1;
+    try {
+      Connection connection = database.getConnection();
+      PreparedStatement statement;
+      statement = connection.prepareStatement("SELECT id from users where userID='" +
+          id +
+          "'");
+      ResultSet rs = statement.executeQuery();
+      if (rs.next()) {
+        val = rs.getInt(1);
+      }
+      rs.close();
+      statement.close();
+      database.closeConnection(connection);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return val;
+  }
+
   void createUserTable(Database database) throws Exception {
     Connection connection = database.getConnection();
 
     PreparedStatement statement;
 
-    statement = connection.prepareStatement("CREATE TABLE if not exists users (id IDENTITY, " +
-      "age INT, gender INT, experience INT, ipaddr VARCHAR, password VARCHAR, timestamp TIMESTAMP AS CURRENT_TIMESTAMP, CONSTRAINT pkusers PRIMARY KEY (id))");
+    statement = connection.prepareStatement("CREATE TABLE if not exists users (" +
+        "id IDENTITY, " +
+        "age INT, " +
+        "gender INT, " +
+        "experience INT, " +
+        "ipaddr VARCHAR, " +
+        "password VARCHAR, " +
+        "firstName VARCHAR, " +
+        "lastName VARCHAR, " +
+        "nativeLang VARCHAR, " +
+        "dialect VARCHAR, " +
+        "userID VARCHAR, " +
+        "timestamp TIMESTAMP AS CURRENT_TIMESTAMP, CONSTRAINT pkusers PRIMARY KEY (id))");
     statement.execute();
     statement.close();
     database.closeConnection(connection);
@@ -113,20 +154,25 @@ public class UserDAO {
       List<User> users = new ArrayList<User>();
       while (rs.next()) {
     	  i = 1;
-        Timestamp timestamp;
+    /*    Timestamp timestamp;
         if (rs.getMetaData().getColumnCount() == 7) { // if we have a timestamp column --
           timestamp = rs.getTimestamp(i++);
         }
         else { // Wade's db schema doesn't have a timestamp column, currently
           timestamp = new Timestamp(System.currentTimeMillis());
-        }
+        }*/
         users.add(new User(rs.getLong(i++), //id
           rs.getInt(i++), // age
           rs.getInt(i++), //gender
           rs.getInt(i++), // exp
           rs.getString(i++), // ip
-          rs.getString(i), // password
-          timestamp.getTime()
+            rs.getString(i++), // password
+            rs.getString(i++), // first
+            rs.getString(i++), // last
+            rs.getString(i++), // native
+            rs.getString(i++), // dialect
+            rs.getString(i++), // dialect
+            rs.getTimestamp(i++).getTime()
         ));
       }
       rs.close();
