7ca318baed6e4b91c4c10d37a76aecffd932a3ac
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-01-11 19:45:33 -0500

    added session info to monitoring
    Former-commit-id: 08aff2203b731e61b4884a11dabb8ac66c432333

--- src/mitll/langtest/client/LangTestDatabaseAsync.java
+++ src/mitll/langtest/client/LangTestDatabaseAsync.java
@@ -72,4 +72,6 @@ public interface LangTestDatabaseAsync {
   void getResultByHourOfDay(AsyncCallback<Map<String, Integer>> async);
 
   void getResultPerExercise(boolean useFile, AsyncCallback<Map<String, List<Integer>>> async);
+
+  void getSessions(AsyncCallback<List<Session>> async);
 }
