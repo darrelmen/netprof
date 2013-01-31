16cd89bd8b6487342d1137ac9f64c193925e1ad0
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-01-30 19:20:49 -0500

    initial steps towards lesson plans

---
+++ src/mitll/langtest/shared/Lesson.java
@@ -0,0 +1,20 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 1/30/13
+ * Time: 7:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lesson implements IsSerializable {
+  private List<Exercise> exerciseList = new ArrayList<Exercise>();
+
+  public Lesson(){}
+  public void addExercise(Exercise e) { exerciseList.add(e); }
+}
