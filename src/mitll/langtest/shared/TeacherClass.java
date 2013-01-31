294d909d88a2ed4be3fd514f6af011fb5361fc54
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-01-30 19:20:49 -0500

    initial steps towards lesson plans
    Former-commit-id: d1c389c8af6650f76cd0e70ee399e6a907380a81
    Former-commit-id: 20335ede05bafe6123e1275ac8fc438d206a00d9 [formerly f346aca55eceb7da126283da0c15283aae7cb5dc]
    Former-commit-id: 12384157ad3beb099ae47a49972e154b1f8cc7b1

---
+++ src/mitll/langtest/shared/TeacherClass.java
@@ -0,0 +1,21 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 1/30/13
+ * Time: 7:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TeacherClass implements IsSerializable {
+  public List<Lesson> lessonList = new ArrayList<Lesson>();
+  public User userRef;
+
+  public TeacherClass() {}
+  public TeacherClass(User userRef) { this.userRef = userRef; }
+}
