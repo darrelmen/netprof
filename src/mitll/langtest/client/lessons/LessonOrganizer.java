46ed9ea155178abc4e2d05a26a4f123b92b5bbac
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-01-31 20:19:31 -0500

    updates to monitoring to merge in offline data, show gender breakdown better, other chart bug fixes, warn if don't fill in user name and password on registration
    Former-commit-id: f9687e2239a92a28151ad5e583ea8f8a0410ff43

---
+++ src/mitll/langtest/client/lessons/LessonOrganizer.java
@@ -0,0 +1,17 @@
+package mitll.langtest.client.lessons;
+
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.shared.User;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 1/31/13
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LessonOrganizer {
+  public LessonOrganizer(User user, LangTestDatabaseAsync service) {
+
+  }
+}
