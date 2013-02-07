3a4d5533fe8a4891359e02630fa06465c73bef66
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-02-07 13:02:28 -0500

    refactored auto crt and small vocab decoder code to make it easier to use for other tasks
    Former-commit-id: ba1446e68f465f54e88fdc8c2995db2438aaa21c [formerly d773dc1c378f5b24c5d092f27e0c57de86388ad1] [formerly d227bb6f8d0d0ce6fc2241dccbaa24172ee82098 [formerly 393de265cfb39024d00cc9adc9d1dae71748a7ed]]
    Former-commit-id: 1b36189ad19e33e0c44fb28bab8415c66db1b470 [formerly 15e63968c2bee3c1ed600f7b6c9b8798a715e1ef]
    Former-commit-id: fb8d58dc1908cbddd9a3ffed2345e11087872959

--- src/mitll/langtest/shared/TeacherClass.java
+++ src/mitll/langtest/shared/TeacherClass.java
@@ -16,6 +16,7 @@ public class TeacherClass implements IsSerializable {
   public List<Lesson> lessonList = new ArrayList<Lesson>();
   public User userRef;
   public String name;
+  public long timestamp;
 
   public TeacherClass() {}
   public TeacherClass(User userRef, String name) { this.userRef = userRef; this.name = name;}
