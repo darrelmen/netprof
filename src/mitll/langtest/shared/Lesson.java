580d806ef3c6140701a611d72ef91a9e3c84bef7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-02-07 18:33:09 -0500

    reads an excel spreadsheet and produces exercises and lessons
    Former-commit-id: d293cc46717b2ded11666a6bf509986c2f2b85fa [formerly bff2ab54b999c66e86f8cb2bc001b20303e2d038] [formerly c4f3433111951c61bc20a5389986a30cadd08129]
    Former-commit-id: aa48884d3d05d5bb9d169851e02eee1ef009ef56 [formerly 58b4e97c4e3da4199ac36f7b61ed489ba9f0cae5]
    Former-commit-id: 793427b9ee074f727c840e194c832f28e85f189e

--- src/mitll/langtest/shared/Lesson.java
+++ src/mitll/langtest/shared/Lesson.java
@@ -15,7 +15,17 @@ import java.util.List;
 public class Lesson implements IsSerializable {
   private List<Exercise> exerciseList = new ArrayList<Exercise>();
   public long timestamp;
+  public String unit;
+  public String chapter;
+  public String week;
 
   public Lesson(){}
+  public Lesson(String unit, String chapter, String week) { this.unit = unit; this.chapter = chapter; this.week = week; }
   public void addExercise(Exercise e) { exerciseList.add(e); }
+
+  public String toString() {
+    return "Lesson '" + unit + "/" + chapter + "/" + week + "' " + exerciseList.size() + " exercises" +
+        (exerciseList.isEmpty() ? "" :
+        ", first is " + exerciseList.iterator().next());
+  }
 }
