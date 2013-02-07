060ebf7cc90be6c8ebc46cbfc3ec7d3a6f2889d1
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-02-07 18:33:09 -0500

    reads an excel spreadsheet and produces exercises and lessons
    Former-commit-id: 7c3101d67aa6254cc72e47c28f8c8ca300c11921

--- src/mitll/langtest/shared/TeacherClass.java
+++ src/mitll/langtest/shared/TeacherClass.java
@@ -15,9 +15,28 @@ import java.util.List;
 public class TeacherClass implements IsSerializable {
   public List<Lesson> lessonList = new ArrayList<Lesson>();
   public User userRef;
+  public long userID;
   public String name;
   public long timestamp;
 
   public TeacherClass() {}
-  public TeacherClass(User userRef, String name) { this.userRef = userRef; this.name = name;}
+
+  public TeacherClass(User userRef, String name) {
+    this(userRef.id, name, System.currentTimeMillis());
+    this.userRef = userRef;
+  }
+
+  public TeacherClass(long userid, String name) {
+    this(userid, name, System.currentTimeMillis());
+  }
+
+  public TeacherClass(long userid, String name, long timestamp) {
+    this.userID = userid;
+    this.name = name;
+    this.timestamp = timestamp;
+  }
+
+  public TeacherClass(long timestamp) {
+    this.timestamp = timestamp;
+  }
 }
