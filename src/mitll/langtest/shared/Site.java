123dca76195599b5c2d68b73c328608559fe7241
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-02-20 17:33:57 -0500

    more improvements to data collect admin and parsing of excel files
    Former-commit-id: 88f37592f4780dcc5af0f962fad22c39900fb4b5 [formerly 8ff420faf9745db9c876ef972be4e9251c3162d9]
    Former-commit-id: ff4c06fa6705dcabfdb15611128adefc1afce9da

--- src/mitll/langtest/shared/Site.java
+++ src/mitll/langtest/shared/Site.java
@@ -2,6 +2,7 @@ package mitll.langtest.shared;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 
 /**
@@ -16,15 +17,18 @@ public class Site implements IsSerializable {
   public String name = null;
   public String language;
   public long creatorID;
+  private String creator;
   public String notes;
   public String exerciseFile;
   public String savedExerciseFile;
+  public String savedExerciseFileName;
 
   public transient Collection<Exercise> exercises; // i.e. don't serialize
   public Exercise example; //  don't write to db
   private String feedback;
   private boolean deployed;
-  private long  creationDate;
+  private long creationDate;
+  public String creationDateReadable;
 
   public Site() {}
 
@@ -38,11 +42,14 @@ public class Site implements IsSerializable {
    * @param notes
    * @param exerciseFile
    * @param filePath
+   * @param savedFileName
    * @param feedback
    * @param deployed
    * @param timestamp
    */
-  public Site(long id, long creatorID, String name, String language, String notes, String exerciseFile, String filePath, String feedback, boolean deployed, long timestamp) {
+  public Site(long id, long creatorID, String name, String language, String notes, String exerciseFile, String filePath, String savedFileName,
+              String feedback, boolean deployed, long timestamp,
+              String creationDateReadable) {
     this.id = id;
     this.creatorID = creatorID;
     this.name = name;
@@ -53,6 +60,8 @@ public class Site implements IsSerializable {
     this.feedback = feedback;
     this.deployed = deployed;
     this.creationDate = timestamp;
+    this.creationDateReadable = creationDateReadable;
+    this.savedExerciseFileName = savedFileName;
   }
 
   public Collection<Exercise> getExercises() { return exercises; }
@@ -66,12 +75,22 @@ public class Site implements IsSerializable {
     this.feedback = feedback;
   }
 
-  public String toString() { return "id " + id + " name " +name + " lang " +language + " creatorID " +creatorID +
-      " notes " +notes + " file " +exerciseFile + " path "+ savedExerciseFile;}
-
   public boolean isDeployed() {
     return deployed;
   }
+
+  public String getCreator() {
+    return creator;
+  }
+
+  public void setCreator(String creator) {
+    this.creator = creator;
+  }
+
+
+  public String toString() { return "id " + id + " name " +name + " lang " +language + " creatorID " +creatorID +
+      " notes " +notes + " file " +exerciseFile + " path "+ savedExerciseFile;}
+
 
 /*  public void setDeployed(boolean deployed) {
     this.deployed = deployed;
