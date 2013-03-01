942fd8414706d9787705ca38985b7845663b4399
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-01 13:10:57 -0500

    supports overlap menu choices
    Former-commit-id: 3c3346f76d9ea7718e727417104045878734eba8 [formerly 3c3346f76d9ea7718e727417104045878734eba8 [formerly 4e64e217d28561ad200c0bf880c57573c7c0023f]]
    Former-commit-id: fcc66667231b768161d6397de776c4f55ae3fca7 [formerly 9e7d7718b847874795a8d2cb3ca31704ee863099] [formerly 34d238d7337baaf7128973b4164cadce587f1689 [formerly ee3f1a82bd6be4dbc419fcc2ef8305e395334832]]
    Former-commit-id: 34d238d7337baaf7128973b4164cadce587f1689
    Former-commit-id: 9b45bafa7cfcc4287e4ad73c0b7162b1be31192f

--- src/mitll/langtest/server/mail/MailSupport.java
+++ src/mitll/langtest/server/mail/MailSupport.java
@@ -1,9 +1,10 @@
-package mitll.langtest.server;
+package mitll.langtest.server.mail;
 
 import org.apache.log4j.Logger;
 
 import javax.mail.Address;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -15,48 +16,20 @@ import java.util.Properties;
 public class MailSupport {
   private static final String SWADE = "swade@ll.mit.edu";
   private static final String RECIPIENT_NAME = "Gordon Vidaver";
+  private static final String DATA_COLLECT_WEBMASTER = "Data Collect Webmaster";
   private static Logger logger = Logger.getLogger(MailSupport.class);
   private static final String EMAIL = "gordon.vidaver@ll.mit.edu";
-  boolean debugEmail;
-  private Properties props;
-  //private final LangTestDatabaseImpl langTestDatabaseImpl;
-//  email = props.getProperty("email", EMAIL);
+  private boolean debugEmail;
 
   public MailSupport(Properties props) {
-    this.props = props;
     debugEmail = props.get("debugEmail") != null && !props.get("debugEmail").toString().equals("false");
   }
-
-/*  public void sendEmail(String recipientName, String recipientEmail, List<String> ccEmails, String subject, String message, boolean includeSiteURL) {
-    logger.debug("Began method: sendEmail(" + recipientName + ", " + recipientEmail + ", " + subject + ", " + message + ", " + includeSiteURL + ")");
-
-    if (includeSiteURL) {
-      message += "\n\nThe URL for the site is " + langTestDatabaseImpl.getServletContext().getInitParameter("siteurl");
-    }
-
-    try {
-      String email_transport = langTestDatabaseImpl.getServletContext().getInitParameter("email_transport");
-      String email_server    = langTestDatabaseImpl.getServletContext().getInitParameter("email_server");
-
-      boolean secureP = email_transport.equals("smtps");
-      if (secureP) {
-        String email_username  = langTestDatabaseImpl.getServletContext().getInitParameter("email_username");
-        String email_password  = langTestDatabaseImpl.getServletContext().getInitParameter("email_password");
-        secureEmail(recipientName, recipientEmail, ccEmails, subject, message, email_transport, email_server, email_username, email_password);
-      } else {
-        normalEmail(recipientName, recipientEmail, ccEmails, subject, message, email_server);
-      }
-    } catch (Exception ex) {
-      logger.error("Method: sendEmail(" + recipientName + ", " + recipientEmail + ", " + subject + ", " + message + ", " + includeSiteURL + ")", ex);
-    }
-  }*/
-
 
   public void email(String subject, String message) {
     normalEmail(RECIPIENT_NAME, EMAIL, new ArrayList<String>(),subject, message,"localhost");
   }
 
-  public void email(String recipientName, String recipientEmail, String subject, String message) {
+/*  public void email(String recipientName, String recipientEmail, String subject, String message) {
     normalEmail(recipientName, recipientEmail, new ArrayList<String>(),subject, message,"localhost");
   }
 
@@ -68,9 +41,10 @@ public class MailSupport {
 
   public void email(String recipientName, String recipientEmail, List<String> ccEmails, String subject, String message) {
     normalEmail(recipientName, recipientEmail, ccEmails, subject, message,"localhost");
-  }
+  }*/
 
-  private void normalEmail(String recipientName, String recipientEmail, List<String> ccEmails, String subject, String message, String email_server)/* throws Exception*/ {
+  private void normalEmail(String recipientName, String recipientEmail, List<String> ccEmails,
+                           String subject, String message, String email_server) {
     try {
       Properties props = new Properties();
       props.put("mail.smtp.host", email_server);
@@ -84,7 +58,7 @@ public class MailSupport {
   }
 
   /**
-   * @see LangTestDatabaseImpl#sendEmail
+   * @see mitll.langtest.server.LangTestDatabaseImpl#sendEmail
    * @param senderName
    * @param senderEmail
    * @param recipientEmails
@@ -131,7 +105,7 @@ public class MailSupport {
                               List<String> ccEmails,
                               String subject, String message) throws Exception {
     Message msg = new MimeMessage(session);
-    msg.setFrom(new InternetAddress(EMAIL, "Data Collect Webmaster"));
+    msg.setFrom(new InternetAddress(EMAIL, DATA_COLLECT_WEBMASTER));
     msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail, recipientName));
     for (String ccEmail : ccEmails) {
       msg.addRecipient(Message.RecipientType.CC, new InternetAddress(ccEmail));
@@ -153,18 +127,23 @@ public class MailSupport {
     logger.info("Sending from " + senderEmail + " " + senderName + " to " +recipientEmails + " sub " +subject + " " +message);
     msg.setFrom(new InternetAddress(senderEmail, senderName));
     for (String receiver : recipientEmails) {
-      logger.info("Sending  to " +receiver);
-
+      logger.info("\tSending  to " +receiver);
       msg.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
     }
     msg.setSubject(subject);
     msg.setText(message);
     msg.addHeader("Content-Type", "text/html");
-   // String replyToEmail = "gordon.vidaver@ll.mit.edu";
-    InternetAddress internetAddress = new InternetAddress(replyToEmail);
-    Address[] replies = new Address[1];
-    replies[0] = internetAddress;
-    msg.setReplyTo(replies);
+    logger.info("Session is " + session + " message " + msg);
+    addReplyTo(replyToEmail, msg);
     return msg;
+  }
+
+  private void addReplyTo(String replyToEmail, Message msg) throws MessagingException {
+    if (replyToEmail != null && replyToEmail.length() > 0) {
+      InternetAddress internetAddress = new InternetAddress(replyToEmail);
+      Address[] replies = new Address[1];
+      replies[0] = internetAddress;
+      msg.setReplyTo(replies);
+    }
   }
 }
