ef7e8d451f047888f79c778e277d08e89a03effb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-05 19:06:28 -0500

    initial steps towards flashcard
    Former-commit-id: f6944ced1b43ed5ffc5f4f505ed8b121889553ae [formerly a914e1439167f550d02e150a66d5f1b904a3d76d] [formerly 94a546b3e0b29a7c19d9a2101a800da4d9e5ca12 [formerly c0079fe3baea8684892102283350a49578395511]] [formerly d5d560afab15333b0d1b7f8b570b6149f7975fc6]
    Former-commit-id: 7224f915817f8a68cbd4de29e3764c181f8ac768 [formerly cf9e802efe1c7a7d4e7a9797965e4283997fd1b5] [formerly d688c38f5d1a62e2a4c62ee452dab7b600ff8a1c [formerly ed03529f807c81d73617c664731245eb0c1d0407]] [formerly b9d72479114291be004925fbca56d8f91918c680]
    Former-commit-id: d65a2490066674a3627eba9ed6cd0d9a4590674e
    Former-commit-id: b7b546ce17d4711fde82e54cc179a745f95ebda5 [formerly baa8d89d3ba4aa907baa550617cebe1926546af8]
    Former-commit-id: 8f72f26d69d39778ea6520b6537588ff0c565f6e

---
+++ src/mitll/langtest/client/flashcard/FlashcardExercisePanelFactory.java
@@ -0,0 +1,44 @@
+package mitll.langtest.client.flashcard;
+
+import com.google.gwt.user.client.ui.Panel;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.SimpleRecordExercisePanel;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.ExercisePanel;
+import mitll.langtest.client.exercise.ExercisePanelFactory;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.shared.Exercise;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 7/9/12
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FlashcardExercisePanelFactory extends ExercisePanelFactory {
+  /**
+   * @see mitll.langtest.client.LangTest#setFactory()
+   * @param service
+   * @param userFeedback
+   * @param controller
+   */
+  public FlashcardExercisePanelFactory(final LangTestDatabaseAsync service, final UserFeedback userFeedback,
+                                       final ExerciseController controller) {
+    super(service, userFeedback, controller);
+  }
+
+  public Panel getExercisePanel(Exercise e) {
+    if (e.getType() == Exercise.EXERCISE_TYPE.RECORD) {
+      return new SimpleRecordExercisePanel(e, service, userFeedback, controller) {
+        @Override
+        protected void clickNext(LangTestDatabaseAsync service, UserFeedback userFeedback, ExerciseController controller, Exercise e) {
+          System.out.println("loading next ");
+          controller.loadNextExercise(e);
+        }
+      };
+    } else {
+      return new ExercisePanel(e, service, userFeedback, controller);
+    }
+  }
+}
