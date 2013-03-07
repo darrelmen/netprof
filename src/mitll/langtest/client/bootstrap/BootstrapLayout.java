9b6e877ce55155e74c683224c1ea40111be47218
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-06 19:43:20 -0500

    another rev of flash card
    Former-commit-id: 4792c5d47565f0ff8bf4c3cd328e819cc5949eb4
    Former-commit-id: 11110bae7bf104a085bd0f49a7f31e67e5953118

---
+++ src/mitll/langtest/client/bootstrap/BootstrapLayout.java
@@ -0,0 +1,63 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.Heading;
+import com.github.gwtbootstrap.client.ui.Row;
+import com.github.gwtbootstrap.client.ui.base.IconAnchor;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import mitll.langtest.client.user.UserManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 3/6/13
+ * Time: 5:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BootstrapLayout {
+/*
+  private Panel doFlashcard(final UserManager userManager) {
+    FluidContainer container = new FluidContainer();
+    RootPanel.get().add(container);
+    //currentExerciseVPanel = container;
+    Row row = new FluidRow();
+    container.add(row);
+    Row row2 = new FluidRow();
+    container.add(row2);
+
+    IconAnchor logout = new IconAnchor();
+    logout.setText("Logout");
+    logout.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        userManager.clearUser();
+        lastUser = -1;
+        userManager.login();
+      }
+    });
+
+
+    row2.add(new Column(2));
+    row2.add(new Column(1, logout));
+    //userManager = new UserManager(this, service, isCollectAudio(), false);
+    this.exerciseList = new BootstrapFlashcardExerciseList(container, service, userManager);
+
+    makeFlashContainer();
+    Column col = new Column(1, flashRecordPanel);
+    row.add(new Column(1, new Heading(2, props.getAppTitle())));
+    //row.add(col);
+
+    Row row3 = new FluidRow();
+    container.add(row3);
+    row3.add(col);
+    return container;
+
+    setupSoundManager();
+
+    login();
+  }*/
+}
