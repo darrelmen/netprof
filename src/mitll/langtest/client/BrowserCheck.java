477eb0987862fe6e044f7cfbd2c211eccd94beb0
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-14 19:03:08 -0400

    added french english flashcards
    Former-commit-id: 464b940ad256d3fa5c1dbdba2b8d1e0b39dd82ec [formerly 0643cba311fcaaeb3c836eed10ade5fcf91a4bfd]
    Former-commit-id: 5f06acbd9a6c80324c90ba1bfbf9ef2cf08a229b

--- src/mitll/langtest/client/BrowserCheck.java
+++ src/mitll/langtest/client/BrowserCheck.java
@@ -6,6 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BrowserCheck {
+  private static final String FIREFOX = "firefox";
+  private static final String CHROME = "chrome";
+  private static final String MSIE = "msie";
+  private static final String SAFARI = "safari";
+  private static final String IE = "IE";
+
   public String browser = "Unknown";
   public int ver = 0;
   public String version = "";
@@ -13,13 +19,13 @@ public class BrowserCheck {
   private Map<String,Integer> browserToVersion = new HashMap<String,Integer>();
 
   public BrowserCheck() {
-    browserToVersion.put("firefox",14);
-    browserToVersion.put("chrome",21);
-    browserToVersion.put("IE",9);
-    browserToVersion.put("safari",5);
+    browserToVersion.put(FIREFOX,14);
+    browserToVersion.put(CHROME,21);
+    browserToVersion.put(IE,9);
+    browserToVersion.put(SAFARI,5);
   }
 
-  public void checkForCompatibleBrowser() {
+  public BrowserCheck checkForCompatibleBrowser() {
     if (browser.equals("Unknown")) getBrowserAndVersion();
     Integer min = browserToVersion.get(browser);
     if (browser.equals("IE")) {
@@ -28,13 +34,14 @@ public class BrowserCheck {
     if (min == null) {
       Window.alert("Your browser is " + browser + " version " + version + ". We strongly recommend any of " + browserToVersion.keySet());
     } else if (ver < min) {
-        if (!browser.equals("firefox") || !version.startsWith("10.0")) {
+        if (!browser.equals(FIREFOX) || !version.startsWith("10.0")) {
             Window.alert("Your browser is " + browser + " version " + version +
                     ". We strongly recommend upgrading to version " + min + " or later.");
         }
     } else {
       System.out.println("browser " +browser + " ver " + ver + " version " + version + " vs " + min);
     }
+    return this;
   }
 
   public boolean isIE7() { return getBrowserAndVersion().equals("IE 7"); }
@@ -46,21 +53,21 @@ public class BrowserCheck {
 
   public String getBrowser(String agent) {
     agent = agent.toLowerCase();
-    if (agent.contains("firefox")) {
-      version = agent.substring(agent.indexOf("firefox") + "firefox".length() + 1).split("\\s+")[0];
-      browser = "firefox";
-    } else if (agent.contains("chrome")) {
-      version = agent.substring(agent.indexOf("chrome") + "chrome".length() + 1).split("\\s+")[0];
-      browser = "chrome";
-    } else if (agent.contains("msie")) {
-      version = agent.substring(agent.indexOf("msie") + "msie".length() + 1).split(";")[0];
+    if (agent.contains(FIREFOX)) {
+      version = agent.substring(agent.indexOf(FIREFOX) + FIREFOX.length() + 1).split("\\s+")[0];
+      browser = FIREFOX;
+    } else if (agent.contains(CHROME)) {
+      version = agent.substring(agent.indexOf(CHROME) + CHROME.length() + 1).split("\\s+")[0];
+      browser = CHROME;
+    } else if (agent.contains(MSIE)) {
+      version = agent.substring(agent.indexOf(MSIE) + MSIE.length() + 1).split(";")[0];
       browser = "IE";
-    } else if (agent.contains("safari")) {
-      version = agent.substring(agent.indexOf("safari") + "safari".length() + 1).split("\\s+")[0];
+    } else if (agent.contains(SAFARI)) {
+      version = agent.substring(agent.indexOf(SAFARI) + SAFARI.length() + 1).split("\\s+")[0];
       if (version.length() > 1) {
         version = version.substring(0,1);
       }
-      browser = "safari";
+      browser = SAFARI;
     }
     String major = version.split("\\.")[0];
     try {
@@ -72,6 +79,8 @@ public class BrowserCheck {
 
     return browser + " " + ver;
   }
+
+  public boolean isFirefox() { return browser.equals(FIREFOX); }
 
   private static native String getUserAgent() /*-{
     return navigator.userAgent.toLowerCase();
