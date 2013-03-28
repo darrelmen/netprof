0ab25dc1e5a38e0c2f126d2b71e042a16ab6fd68
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-28 17:48:56 -0400

    basically got game mode working
    Former-commit-id: 76d4978e810dd250203edab9cd398e274c1732a3 [formerly c22c5b919e45ffe367097a5805922074baa49979] [formerly d429806498d8fbc5a71bf05aaa09cd2b4613e8e8]
    Former-commit-id: 36e5986960c0584bd0423c1d6d276b15d286dd4b [formerly 4f474e352e4df99399348abbd13caa4e4f877c33] [formerly 76b30abd281dcd105f98f814bbcb2474294469cb]
    Former-commit-id: 36b48be5ddb45ef4338cd5c1f682693db9ed3469
    Former-commit-id: e92d40f9e11627991e76e7988c1ea964a7211f72

--- src/mitll/langtest/shared/FlashcardResponse.java
+++ src/mitll/langtest/shared/FlashcardResponse.java
@@ -2,6 +2,8 @@ package mitll.langtest.shared;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: GO22670
@@ -13,6 +15,11 @@ public class FlashcardResponse implements IsSerializable {
   public Exercise e;
   public int correct, incorrect;
   public boolean finished = false;
+  private List<Integer> correctHistory;
+
+  public List<Integer> getCorrectHistory() { return correctHistory; }
+  public void setCorrectHistory(List<Integer> h) { correctHistory = h; }
+
   public FlashcardResponse() {}
   public FlashcardResponse(Exercise e, int correct, int incorrect) {this.e = e; this.correct =correct; this.incorrect = incorrect;}
   public FlashcardResponse(boolean finished, int correct, int incorrect) {this.finished = finished; this.correct =correct; this.incorrect = incorrect;}
