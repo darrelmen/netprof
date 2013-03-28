6a996af92360120f85dd69571da3204271427afa
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-28 17:48:56 -0400

    basically got game mode working
    Former-commit-id: a30298b4e682c24e25c7478d2ae7b3a55783d2c5 [formerly 77ef2cd331a741995cb21d2e7ff16438f2c15743] [formerly cdfb61e4deac1265e5ca5cf00658280870e5ee40 [formerly 09502f187abc327e1331994edf6c7e279f26f5a6]]
    Former-commit-id: dede357355c5e29adf2786b1d7d5f28d6f612a79 [formerly f18cdaf69ab2e13f8b4b4036f6be3f42eccab9fa] [formerly 7792fea476fdc084c0edbacd49e5a8231f08ad7b [formerly 9896ea013cf4909edcf907d9923be39271771298]]
    Former-commit-id: 74b68afef977333a8ff9acbabb457277c5f40ac6 [formerly b6cb16864fe7175ceb7b5404f3ab6d73dbdfe733]
    Former-commit-id: f5ea5c0686464d2d35de9de214dd853e56067e05

---
+++ src/mitll/langtest/client/bootstrap/AudioHelper.java
@@ -0,0 +1,56 @@
+package mitll.langtest.client.bootstrap;
+
+import com.google.gwt.media.client.Audio;
+import com.google.gwt.user.client.Timer;
+import mitll.langtest.client.BrowserCheck;
+
+public class AudioHelper {
+  private Audio mistakeAudio;
+  private BrowserCheck browserCheck = new BrowserCheck().checkForCompatibleBrowser();
+
+  public AudioHelper() {
+    mistakeAudio = Audio.createIfSupported();
+  }
+
+  public void playCorrect(final int n) {
+    playCorrect();
+
+    if (n > 0) {
+      new Timer() {
+        @Override
+        public void run() {
+          playCorrect(n - 1);
+        }
+      }.schedule(300);
+    }
+  }
+
+  public void playCorrect() {
+    play("langtest/sounds/correct4.wav", "langtest/sounds/correct4.mp3");
+  }
+
+  public void playIncorrect() {
+    play("langtest/sounds/incorrect1.wav", "langtest/sounds/incorrect1.mp3");
+  }
+
+  public void play(String mp3Audio) {
+    play(mp3Audio.replace(".mp3", ".wav"), mp3Audio.replace(".wav", ".mp3"));
+  }
+
+  public boolean hasEnded() {
+    return mistakeAudio != null && mistakeAudio.hasEnded();
+  }
+
+  private void play(String openAudio, String mp3Audio) {
+    if (mistakeAudio == null) {
+      // Window.alert("audio playback not supported.");
+    } else {
+      playAudio(openAudio, mp3Audio);
+      mistakeAudio.play();
+    }
+  }
+
+  private void playAudio(String openAudio, String mp3Audio) {
+    mistakeAudio.setSrc((browserCheck.isFirefox()) ? openAudio : mp3Audio);
+  }
+}
