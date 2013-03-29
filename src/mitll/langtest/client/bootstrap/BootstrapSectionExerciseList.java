81e4462a2f4d329e3d78db693efb7e67af379396
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-29 15:51:41 -0400

    partial steps towards stacked bar unit-chapter-week display
    Former-commit-id: 7073725e1944000d09a3c99a8d9f18e460bccd71 [formerly ebf3f6a9b8b01d94c190c25ae4c08ca4a4990b09] [formerly 3f352fd898ac7dbb25600b0a2e19343a00f4906c [formerly 5de0d40b7729e1f3d94ed1acb8b9231c512a02b7]] [formerly 4795e65d77139963213c051225396c59ab093d41 [formerly ebbd0d514db21d3a833d60294c132b870822f662] [formerly 6556a01f7ddc4819899e338d5ae0e6c13e046bca]]
    Former-commit-id: 5d20d34875a66ac041ad0325a324817ab9685c14 [formerly 14d9d5c5803e04eb1c21367db76b0c1e5ac1a2b2] [formerly d097c114004b9a49d96fa0307b3adad58a6c2374 [formerly 52670115e1c19d29f6a718f6731a7122a412eeec]] [formerly 727c3b80dbe92ce5045b5d57c01e6e6f68ba905c [formerly 20370f908103209db4f4fd6c8420799b4e1aee8b]]
    Former-commit-id: 5fe3bebd334e2338b48f733321cac2854b7c646a [formerly 8e4b18cc8d824209613bdabf25b29f1f4d586c89]
    Former-commit-id: a155818b02a4c3cb445276a03e21782787116373 [formerly dc64bcd11a306a3cc804bb69b602a1baf6d969a4]
    Former-commit-id: c787ead5d304236bed8223d4beaf645db8b023ca

---
+++ src/mitll/langtest/client/bootstrap/BootstrapSectionExerciseList.java
@@ -0,0 +1,563 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.Bar;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.Container;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.Heading;
+import com.github.gwtbootstrap.client.ui.Image;
+import com.github.gwtbootstrap.client.ui.ProgressBar;
+import com.github.gwtbootstrap.client.ui.StackProgressBar;
+import com.github.gwtbootstrap.client.ui.base.ProgressBarBase;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.DialogHelper;
+import mitll.langtest.client.LangTest;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.ExercisePanelFactory;
+import mitll.langtest.client.exercise.ListInterface;
+import mitll.langtest.client.exercise.PagingExerciseList;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.ExerciseShell;
+import mitll.langtest.shared.FlashcardResponse;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 3/5/13
+ * Time: 5:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BootstrapSectionExerciseList extends PagingExerciseList {
+  public static final String ANY = "Any";
+  private Container sectionPanel;
+  private long userID;
+  private Map<String,SelList> typeToBox = new HashMap<String, SelList>();
+
+  private static class SelList {
+    private int current = 0;
+    List<String> selections;
+    public SelList(List<String> selections) { this.selections = selections; }
+
+    public int getCurrent() {
+      return current;
+    }
+
+    public String getCurrentValue() { return selections.get(current); }
+
+    public void setCurrent(int current) {
+      this.current = current;
+    }
+  }
+  public BootstrapSectionExerciseList(Panel currentExerciseVPanel, LangTestDatabaseAsync service,
+                             UserFeedback feedback,
+                             boolean showTurkToken, boolean showInOrder) {
+    super(currentExerciseVPanel, service, feedback, showTurkToken, showInOrder);
+  }
+
+
+  @Override
+  protected void addWidgets(Panel currentExerciseVPanel) {
+    currentExerciseVPanel.add(sectionPanel = new FluidContainer());
+    super.addWidgets(currentExerciseVPanel);
+  }
+
+  @Override
+  protected void checkBeforeLoad(ExerciseShell e) {}
+
+/*  @Override
+  protected void addTableWithPager() {
+    add(sectionPanel = new VerticalPanel());
+    super.addTableWithPager();
+  }*/
+
+  @Override
+  public void getExercises(final long userID) {
+    System.out.println("------- Get exercises for " + userID);
+    this.userID = userID;
+    service.getTypeToSection(new AsyncCallback<Map<String, Collection<String>>>() {
+      @Override
+      public void onFailure(Throwable caught) {}
+
+      @Override
+      public void onSuccess(Map<String, Collection<String>> result) {
+        //typeToSections = result;
+        sectionPanel.clear();
+
+        //final FlexTable g = new FlexTable();
+        String first = null;
+        int row = 0;
+        Set<String> types = result.keySet();
+        System.out.println("got types " + types);
+        typeToBox.clear();
+
+        for (final String type : types) {
+          if (first == null) first = type;
+
+          //final ListBox listBox = new ListBox();
+          //typeToBox.put(type,listBox);
+          List<String> sections = getSections(result, type);
+          typeToBox.put(type,new SelList(sections));
+         // populateListBox(listBox, sections);
+      /*    listBox.addChangeHandler(new ChangeHandler() {
+            @Override
+            public void onChange(ChangeEvent event) {
+              getListBoxOnClick(listBox, type);
+            }
+          });
+          int col = 0;*/
+          StackProgressBar stackProgressBar = new StackProgressBar();
+          boolean oddEven = false;
+          for (String section : sections) {
+            Bar bar = new Bar();
+            int percent = (int) (100f * (1f / (float) sections.size())); // TODO : get per item quantity
+            bar.setPercent(percent);
+            bar.setColor(oddEven ? Bar.Color.INFO : Bar.Color.DEFAULT);
+            bar.setText(section);
+            stackProgressBar.add(bar);
+            oddEven = !oddEven;
+          }
+          //g.setWidget(row, col++, new HTML(type));
+          //g.setWidget(row++,col,listBox);
+          FluidRow typeRow = new FluidRow();
+          Heading heading = new Heading(4);
+          heading.setText(type);
+          typeRow.add(new Column(1, heading));
+          typeRow.add(new Column(11,stackProgressBar));
+          sectionPanel.add(typeRow);
+        }
+        // doNestedSections();
+      //  g.setWidget(row, 0, getEmailWidget());
+      //  g.getFlexCellFormatter().setColSpan(row, 0, 2);
+
+       // sectionPanel.add(g);
+
+        if (first != null) {
+          //typeToBox.get(first).setSelectedIndex(1); // not any
+          pushFirstSelection(first);
+        }
+        else {
+          noSectionsGetExercises(userID);
+        }
+      }
+    });
+  }
+
+  private void getListBoxOnClick(final ListBox listBox, final String type) {
+    final String itemText = listBox.getItemText(listBox.getSelectedIndex());
+    setListBox(type, itemText);
+  }
+
+  /**
+   * @see #getListBoxOnClick(com.google.gwt.user.client.ui.ListBox, String)
+   * @param type
+   * @param itemText
+   */
+  private void setListBox(final String type, final String itemText) {
+    System.out.println("setListBox given " + type + "=" + itemText);
+
+    if (itemText.equals(ANY)) {
+      service.getTypeToSection(new AsyncCallback<Map<String, Collection<String>>>() {
+        @Override
+        public void onFailure(Throwable caught) {}
+
+        @Override
+        public void onSuccess(Map<String, Collection<String>> result) {
+          Set<String> types = result.keySet();
+          System.out.println("setListBox onSuccess " + type + "=" + itemText + " got types = " + types);
+
+          for (final String type : types) {
+            List<String> sections = getSections(result, type);
+            populateListBox(type, sections);
+          }
+          pushNewSectionHistoryToken();
+
+          //noSectionsGetExercises(userID);
+        }
+      });
+    } else {
+      service.getTypeToSectionsForTypeAndSection(type, itemText, new AsyncCallback<Map<String, List<String>>>() {
+        @Override
+        public void onFailure(Throwable caught) {
+          Window.alert("Can't contact server.");
+        }
+
+        /**
+         * This is a map from type to sections
+         * @param result
+         */
+        @Override
+        public void onSuccess(Map<String, List<String>> result) {
+          System.out.println("setListBox onSuccess " + type + "=" + itemText + " yielded " +result);
+
+          for (Map.Entry<String, List<String>> pair : result.entrySet()) {
+            String type = pair.getKey();
+            List<String> sections = pair.getValue();
+            populateListBox(type, sections);
+          }
+          pushNewSectionHistoryToken();
+        }
+      });
+    }
+  }
+
+  /**
+   * @see #setListBox(String, String)
+   * @param type
+   * @param sections
+   */
+  private void populateListBox(String type, Collection<String> sections) {
+    //ListBox listBox = typeToBox.get(type);
+    System.out.println("populateListBox for " +type); // TODO : fill in
+  /*  populateListBox(listBox, sections);
+    if (listBox.getItemCount() > 0) {
+      listBox.setSelectedIndex(0);
+    }*/
+  }
+
+  /**
+   * @see #getExercises(long)
+   * @see #populateListBox(String, java.util.Collection)
+   * @param listBox
+   * @param sections
+   */
+  private void populateListBox(ListBox listBox, Collection<String> sections) {
+    listBox.clear();
+    listBox.addItem(ANY);
+    List<String> items = new ArrayList<String>(sections);
+    Collections.sort(items);
+    for (String section : items) {
+      listBox.addItem(section);
+    }
+  }
+
+  /**
+   * @see #getExercises(long)
+   * @return
+   */
+/*  private Widget getEmailWidget() {
+    FlexTable g = new FlexTable();
+    Anchor widget = new Anchor("E-MAIL");
+    int row = 0;
+    g.setWidget(row, 0, new HTML("Share via "));
+    g.setWidget(row,1, widget);
+    widget.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        String token = History.getToken();
+        if (token.trim().isEmpty()) token = getDefaultToken();
+        Triple triple = getTriple(token);
+        feedback.showEmail("Lesson " + triple, "", token);
+      }
+    });
+
+    FlowPanel widgets = new FlowPanel();
+    widgets.add(g);
+    return widgets;
+  }*/
+
+  /**
+   * @see #getExercises(long)
+   * @param first
+   */
+  private void pushFirstSelection(String first) {
+    String initToken = History.getToken();
+    if (initToken.length() == 0) {
+     // SelList listBox = typeToBox.get(first);
+     // String itemText = listBox.getItemText(listBox.getSelectedIndex());
+     // System.out.println("push first " + first + " select " + itemText);
+
+      pushNewSectionHistoryToken();
+    } else {
+      System.out.println("fire history for " +initToken);
+      History.fireCurrentHistoryState();
+    }
+  }
+
+  private List<String> getSections(Map<String, Collection<String>> result, String type) {
+    List<String> sections = new ArrayList<String>(result.get(type));
+    return getSections(sections);
+  }
+
+  private List<String> getSections(List<String> sections) {
+    boolean allInt = !sections.isEmpty();
+    for (String s : sections) {
+      try {
+        Integer.parseInt(s);
+      } catch (NumberFormatException e) {
+        allInt = false;
+        break;
+      }
+    }
+    if (allInt) {
+      Collections.sort(sections, new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+          int first = Integer.parseInt(o1);
+          int second = Integer.parseInt(o2);
+          return first < second ? -1 : first > second ? +1 : 0;
+        }
+      });
+    } else {
+      Collections.sort(sections);
+    }
+    return sections;
+  }
+
+  private void noSectionsGetExercises(long userID) {
+    super.getExercises(userID);
+  }
+
+  /**
+   * When we get a history token push, select the exercise type, section, and optionally item.
+   * @param type
+   * @param section
+   * @param item null is OK
+   * @see #onValueChange(com.google.gwt.event.logical.shared.ValueChangeEvent)
+   */
+  private void loadExercises(final String type, final String section, final String item) {
+    System.out.println("loadExercises " + type + " " + section + " item '" +item +"'");
+    service.getExercisesForSection(type, section, new SetExercisesCallback() {
+      @Override
+      public void onSuccess(List<ExerciseShell> result) {
+        if (!result.isEmpty()) {
+          if (item != null) {
+            rememberExercises(result);
+            if (!loadByID(item)) {
+              loadFirstExercise();
+            }
+          } else {
+            super.onSuccess(result);
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * @see #pushFirstSelection(String)
+   * @see #setListBox(String, String)
+   */
+  private void pushNewSectionHistoryToken() {
+    String historyToken = getHistoryToken(null);
+    System.out.println("------------ push history " + historyToken + " -------------- ");
+    History.newItem(historyToken);
+  }
+
+  @Override
+  protected void gotClickOnItem(ExerciseShell e) {
+  }
+
+  protected String getHistoryToken(String id) {
+    System.out.println("getHistoryToken for " + id + " examining " +typeToBox.size() + " boxes.");
+    StringBuilder builder = new StringBuilder();
+    for (String type : typeToBox.keySet()) {
+      SelList listBox = typeToBox.get(type);
+      String section = listBox.getCurrentValue();// listBox.getItemText(listBox.getSelectedIndex());
+      if (section.equals(ANY)) {
+        System.out.println("Skipping box " + type);
+      } else {
+        builder.append(type + "=" + section + ";");
+      }
+    }
+    if (id != null) {
+      builder.append(super.getHistoryToken(id));
+    }
+    return builder.toString();
+  }
+
+  @Override
+  public void onValueChange(ValueChangeEvent<String> event) {
+    String token = event.getValue();
+    System.out.println("onValueChange " + token);
+    if (token.length() == 0) {
+      setListBox("", ANY);
+      noSectionsGetExercises(userID);
+    } else {
+      token = unencodeToken(token);
+      if (!token.contains("=")) token = getDefaultToken();
+      System.out.println("onValueChange after " + token);
+      try {
+        Triple triple = getTriple(token);
+
+        restoreListBoxState(triple);
+        Map<String, String> typeToSection = triple.typeToSection;
+        String type = typeToSection.keySet().iterator().next();
+        String section = typeToSection.get(type);
+        setOtherListBoxes(type, section);
+
+        loadExercises(type, section, triple.item);
+      } catch (Exception e) {
+        System.out.println("onValueChange " + token + " badly formed. Got " + e);
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private void setOtherListBoxes(final String selectedType, String section) {
+    System.out.println("setOtherListBoxes type : " + selectedType + " section " + section);
+
+    service.getTypeToSectionsForTypeAndSection(selectedType, section, new AsyncCallback<Map<String, List<String>>>() {
+      @Override
+      public void onFailure(Throwable caught) {
+        Window.alert("Can't contact server.");
+      }
+
+      /**
+       * This is a map from type to sections
+       * @param result
+       */
+      @Override
+      public void onSuccess(Map<String, List<String>> result) {
+        for (Map.Entry<String, List<String>> pair : result.entrySet()) {
+          String type = pair.getKey();
+          if (type.equals(selectedType)) {
+            System.out.println("setOtherListBoxes skipping " + type);
+
+          } else {
+            List<String> sections = pair.getValue();
+            populateListBox(type, sections);
+          }
+        }
+        //pushNewSectionHistoryToken();
+      }
+    });
+  }
+
+  /**
+   * @see #getEmailWidget()
+   * @see #onValueChange(com.google.gwt.event.logical.shared.ValueChangeEvent)
+   * @return
+   */
+  private String getDefaultToken() {
+    StringBuilder builder = new StringBuilder();
+    for (String type : typeToBox.keySet()) {
+      SelList listBox = typeToBox.get(type);
+      String section = listBox.getCurrentValue();
+      if (section.equals(ANY)) {
+        // ?
+      }
+      else {
+        // System.out.println("------------ push history " + type +"/"+ section + "/" +id+ " -------------- ");
+
+        builder.append(type + "=" + section + ";");
+      }
+    }
+
+    return builder.toString();
+  }
+
+  private Triple getTriple(String token) {
+    Triple triple = new Triple();
+    String[] parts = token.split(";");
+
+    for (String part : parts) {
+      if (part.contains("=")) {
+        String[] segments = part.split("=");
+        String type = segments[0].trim();
+        String section = segments[1].trim();
+
+        triple.add(type, section);
+        System.out.println("part " + part + " : " + type + "->" +section + " : " + triple);
+      }
+      else {
+        System.err.println("getTriple skipping part '" + part+ "'");
+      }
+    }
+
+    if (token.contains("item")) {
+      int item1 = token.indexOf("item=");
+      String itemValue = token.substring(item1+"item=".length());
+      System.out.println("got " + itemValue);
+      triple.setItem(itemValue);
+    }
+
+    System.out.println("triple from token '" +token + "' = '" +triple + "'");
+
+    return triple;
+  }
+
+  private static class Triple {
+    private String item;
+    public Map<String, String> typeToSection = new HashMap<String, String>();
+
+    public void add(String type, String section) {
+      typeToSection.put(type, section);
+    }
+
+    public void setItem(String item) {
+      this.item = item;
+    }
+
+    public String toString() {
+      StringBuilder builder = new StringBuilder();
+      for (String section : typeToSection.values()) {
+        builder.append(section).append(", ");
+      }
+      String s = builder.toString();
+      return s.substring(0, s.length() - 2);
+    }
+  }
+
+  /**
+   * Given a triple state, make sure the list boxes are consistent with it.
+   * @see #onValueChange(com.google.gwt.event.logical.shared.ValueChangeEvent)
+   * @param triple
+   */
+  private void restoreListBoxState(Triple triple) {
+    for (Map.Entry<String, String> pair : triple.typeToSection.entrySet()) {
+      String type    = pair.getKey();
+      String section = pair.getValue();
+      SelList listBox = typeToBox.get(type);
+      if (listBox == null) {
+        if (!type.equals("item")) {
+          System.err.println("huh? bad type " + type);
+        }
+      } else {
+
+        // TODO : restore appearance...
+       /* for (int i = 0; i < listBox.getItemCount(); i++) {
+          String itemText = listBox.getItemText(i);
+          if (itemText.equals(section)) {
+            listBox.setSelectedIndex(i);
+            break;
+          }
+        }*/
+      }
+    }
+  }
+
+  private static final int HEIGHT_OF_CELL_TABLE_WITH_15_ROWS = 390 - 20-65;
+
+  protected int getTableHeaderHeight() {
+    return 625 - HEIGHT_OF_CELL_TABLE_WITH_15_ROWS;
+  }
+}
