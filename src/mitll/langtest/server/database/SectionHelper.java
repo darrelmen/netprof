428b1f5968dcd6cbea4b12f57cdbf7d32fc675ae
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-03-29 19:33:08 -0400

    does chapter unit week on excel spreadsheet data
    Former-commit-id: 8182258d5c44326890dab1a619f29ee1872200cb [formerly 3615cbd4bd3a32c5d6e9aaef50b3c38469727b86] [formerly 6bccd90981af9f68f0bee25f65eb82acaa49451a]
    Former-commit-id: a608631a40d6d41800041abb6d7c387b7ac2b51e [formerly 544dcd2c6543cf617ca27ecca7e4a7eacdbc137c] [formerly 0a225a3ba208d972281f793c8721896984a76154]
    Former-commit-id: 60dcba759f91532e64af2342b710586df8c8e413
    Former-commit-id: 82c9e579162dd1e5a8c8d16e0540d4e6a1441a59

---
+++ src/mitll/langtest/server/database/SectionHelper.java
@@ -0,0 +1,145 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.shared.Exercise;
+import mitll.langtest.shared.Lesson;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 3/29/13
+ * Time: 4:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SectionHelper {
+  private static Logger logger = Logger.getLogger(SectionHelper.class);
+  String unitType = "unit";
+  String chapterType = "chapter";
+  String weekType = "week";
+
+  private Map<String,Map<String,Lesson>> typeToUnitToLesson = new HashMap<String,Map<String,Lesson>>();
+  // e.g. "week"->"week 5"->[unit->["unit A","unit B"]],[chapter->["chapter 3","chapter 5"]]
+  private Map<String,Map<String,Map<String,Set<String>>>> typeToSectionToTypeToSections = new HashMap<String, Map<String,Map<String,Set<String>>>>();
+
+
+  public Map<String,List<String>> getTypeToSectionsForTypeAndSection(String type, String section) {
+    Map<String, Map<String, Set<String>>> sectionToSub = typeToSectionToTypeToSections.get(type);
+    if (sectionToSub == null) return Collections.emptyMap();
+    Map<String, Set<String>> typeToSections = sectionToSub.get(section);
+    if (typeToSections == null) return Collections.emptyMap();
+    Map<String,List<String>> retval = new HashMap<String, List<String>>();
+    for (Map.Entry<String,Set<String>> pair : typeToSections.entrySet()) {
+      retval.put(pair.getKey(),new ArrayList<String>(pair.getValue()));
+    }
+    return retval;
+  }
+
+  public Map<String, Collection<String>> getTypeToSections() {
+    Map<String,Collection<String>> typeToSection = new HashMap<String, Collection<String>>();
+    for (String key : typeToUnitToLesson.keySet()) {
+      Map<String, Lesson> stringLessonMap = typeToUnitToLesson.get(key);
+      typeToSection.put(key, new ArrayList<String>(stringLessonMap.keySet()));
+    }
+    return typeToSection;
+  }
+
+  public Collection<Exercise> getExercisesForSection(String type, String section) {
+    Map<String, Lesson> sectionToLesson = typeToUnitToLesson.get(type);
+    if (sectionToLesson == null) {
+      return Collections.emptyList();
+    }
+    else {
+      Lesson lesson = sectionToLesson.get(section);
+      if (lesson == null) {
+        logger.error("Couldn't find section " + section);
+        return Collections.emptyList();
+      } else {
+        return lesson.getExercises();
+      }
+    }
+  }
+
+  public Pair addUnitToLesson(Exercise exercise, String unitName) { return addExerciseToLesson(exercise, unitType, unitName);}
+  public Pair addChapterToLesson(Exercise exercise, String unitName) { return addExerciseToLesson(exercise, chapterType, unitName);}
+  public Pair addWeekToLesson(Exercise exercise, String unitName) { return addExerciseToLesson(exercise, weekType, unitName);}
+
+  public Pair addExerciseToLesson(Exercise exercise, String type, String unitName) {
+    Map<String, Lesson> unit = getSectionToLesson(type);
+
+    Lesson even = unit.get(unitName);
+    if (even == null) unit.put(unitName, even = new Lesson(unitName, "", ""));
+    even.addExercise(exercise);
+
+    return new Pair(type,unitName);
+  }
+
+  private Map<String, Lesson> getSectionToLesson( String section) {
+    Map<String, Lesson> unit = typeToUnitToLesson.get(section);
+    if (unit == null) typeToUnitToLesson.put(section, unit = new HashMap<String, Lesson>());
+    return unit;
+  }
+
+  public static class Pair {
+    private String type; private String section;
+
+    public Pair(String type, String section) {
+      this.type = type;
+      this.section = section;
+    }
+  }
+
+  public void addAssociations(List<Pair> pairs) {
+    for (Pair p : pairs) {
+      List<Pair> others = new ArrayList<Pair>(pairs);
+      others.remove(p);
+      for (Pair o : others) {
+        addAssociation(p, o);
+        // addAssociation(o, p);
+      }
+    }
+  }
+
+  private void addAssociation(Pair first, Pair second) {
+    addAssociation(first.type, first.section, second.type, second.section);
+  }
+
+  private void addAssociation(String type, String unitName, String otherType, String otherSection) {
+    Map<String, Map<String, Set<String>>> sectionToTypeToSections = typeToSectionToTypeToSections.get(type);
+    if (sectionToTypeToSections == null) {
+      typeToSectionToTypeToSections.put(type, sectionToTypeToSections = new HashMap<String, Map<String, Set<String>>>());
+    }
+    Map<String, Set<String>> subsections = sectionToTypeToSections.get(unitName);
+    if (subsections == null) {
+      sectionToTypeToSections.put(unitName, subsections = new HashMap<String, Set<String>>());
+    }
+    Set<String> sections = subsections.get(otherType);
+    if (sections == null) subsections.put(otherType, sections = new HashSet<String>());
+    sections.add(otherSection);
+  }
+
+
+  public Set<String> getSections() { return typeToUnitToLesson.keySet(); }
+  public Map<String, Lesson> getSection(String type) {
+    return typeToUnitToLesson.get(type);
+  }
+
+  public void report() {
+    for (String key : typeToUnitToLesson.keySet()) {
+      Map<String, Lesson> categoryToLesson = typeToUnitToLesson.get(key);
+      //lessons.addAll(categoryToLesson.values());
+      Set<String> sections = categoryToLesson.keySet();
+      if (!sections.isEmpty()) {
+        logger.debug("Section type : " + key + " : sections " + sections);
+      }
+    }
+  }
+}
