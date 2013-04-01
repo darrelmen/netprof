947a45683f545cf68806c3160ffaf63ed9fe2f2b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-01 13:17:05 -0400

    mostly working with old school unit and chapter interface
    Former-commit-id: b7deb499265df8e451e44619f820590860198c54 [formerly 84afeb2d5bfd57b97e8db2ead71d91621fb46960] [formerly 85bcb9dc69ab75b5cafc6fe70e93cc81304aeed1 [formerly c693308e99b7e3c0c9b07d8eb0366ae976aad3e9]] [formerly e4add52954f5f5cee85063813b5eee9c0017770c [formerly b3a0d26a2adebae1933a5cda57e5aa8c3fe4def7] [formerly 7076ff722c08ab6cd0dfdf9e062f5bff22cc81e9 [formerly e99c9a74ec2fccab1cc98e712e959de4b0e7550b]]]
    Former-commit-id: 539c489da392509079ca9c5682785f6122c4d8fc [formerly 13eca448e2b0e6492815bd94e99aa0f02cb9362e] [formerly a3afcd43e29f3392b557436f0667cd8d86385f2a [formerly cf87bc1246599d7514cfa1f8667fe6850e964cc8]] [formerly 1d8ce3b27059887a6c4b59b9a96fc35c3ab11527 [formerly dbae8e21722663e0c21410683ed4f10e3718197c] [formerly c1dcaa975e978829881ccfe0f2044d00b7c7d579 [formerly 0bb266ce62536cadb8a8b725bd012f2bad01efc6]]]
    Former-commit-id: dbcdbf6dadbd903a2a593017d59f8aaa77fb02cb [formerly e2a023b7f64c38a9d6c00b18e5f2f5ac7498a6f0] [formerly 6f6552e07fa2ac9ce32fe937f142b029dc12e28e [formerly deae0bee20fa45bc3a01a17084f0b14769612c23]]
    Former-commit-id: a68d8e11ee0ddf9884238b3f63824676e85a587a [formerly ad1152cd2e7cd0d0b9cdd915fdd02900ef028f58]
    Former-commit-id: 0bfbf08e8e7d2ca645d9d4951c7c7ea18354e3cb

--- src/mitll/langtest/server/LangTestDatabaseImpl.java
+++ src/mitll/langtest/server/LangTestDatabaseImpl.java
@@ -157,12 +157,21 @@ public class LangTestDatabaseImpl extends RemoteServiceServlet implements LangTe
   @Override
   public Map<String, Collection<String>> getTypeToSection() { return db.getTypeToSection(); }
   @Override
-  public List<ExerciseShell> getExercisesForSection(String type, String section) {
+  public List<ExerciseShell> getExercisesForSection(String type, String section, long userID) {
     Collection<Exercise> exercisesForSection = db.getExercisesForSection(type, section);
-    return getExerciseShells(exercisesForSection);
+    List<Exercise> exercisesBiasTowardsUnanswered = db.getExercisesBiasTowardsUnanswered(userID, exercisesForSection);
+    return getExerciseShells(exercisesBiasTowardsUnanswered);
   }
 
   @Override
+  public List<ExerciseShell> getExercisesForSelectionState(Map<String, String> typeToSection, long userID) {
+    Collection<Exercise> exercisesForSection = db.getExercisesForSelectionState(typeToSection);
+    List<Exercise> exercisesBiasTowardsUnanswered = db.getExercisesBiasTowardsUnanswered(userID, exercisesForSection);
+    return getExerciseShells(exercisesBiasTowardsUnanswered);
+
+  }
+
+    @Override
   public Map<String, List<String>> getTypeToSectionsForTypeAndSection(String type, String section) {
     return db.getTypeToSectionsForTypeAndSection(type, section);
   }
