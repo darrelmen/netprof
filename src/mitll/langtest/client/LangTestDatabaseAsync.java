3c88827a5e7835b22a0bc4357d9e03ef953e7db8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-02 14:16:03 -0400

    refactoring and cleanup
    Former-commit-id: d0a388ea0cc5a2b32a0574d0f6663633491665fb [formerly 728359dd99816291b47dee053365cddda573a9ce] [formerly d0a388ea0cc5a2b32a0574d0f6663633491665fb [formerly 728359dd99816291b47dee053365cddda573a9ce] [formerly d81e43c02f28e9cef27c78853121ef6d6b7cf6df]]
    Former-commit-id: 364ce2a86f237d62e0a713a74155f9f7be656bc2 [formerly 4bf5cc9751adca94bf6fdd251fafba1873d17e53] [formerly 06a7634a3edfc1d07ac62de3b41078880a936647] [formerly 08d9c7f18cc7a2dacffa1104cb7de9e98926e903 [formerly c2a62ed55b6225587241830aa19e162e0ae33472] [formerly 2dca7c991bd453b0b9b3ee84c5f89f1bc8bce686]]
    Former-commit-id: 08d9c7f18cc7a2dacffa1104cb7de9e98926e903 [formerly c2a62ed55b6225587241830aa19e162e0ae33472]
    Former-commit-id: ceac91f19af2bb007c3513420c1dc2455604a3cc
    Former-commit-id: e80c7d10a2b203ece1291085baab9c5116544869

--- src/mitll/langtest/client/LangTestDatabaseAsync.java
+++ src/mitll/langtest/client/LangTestDatabaseAsync.java
@@ -106,12 +106,12 @@ public interface LangTestDatabaseAsync {
   void logMessage(String message, AsyncCallback<Void> async);
 
   void getTypeToSection(AsyncCallback<Map<String, Collection<String>>> async);
+  void getTypeToSectionsForTypeAndSection(String type, String section, AsyncCallback<Map<String, Collection<String>>> async);
 
-  void getExercisesForSection(String type, String section, long userID, AsyncCallback<List<ExerciseShell>> async);
+//  void getExercisesForSection(String type, String section, long userID, AsyncCallback<List<ExerciseShell>> async);
 
   void sendEmail(int userID, String to, String replyTo, String subject, String message, String token, AsyncCallback<Void> async);
 
-  void getTypeToSectionsForTypeAndSection(String type, String section, AsyncCallback<Map<String, List<String>>> async);
 
   void getNextExercise(long userID, AsyncCallback<Exercise> async);
 
