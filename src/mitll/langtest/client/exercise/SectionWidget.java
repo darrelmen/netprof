16b646d72b7daf0b11ac2a74396fffc2756ca448
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-03 20:09:44 -0400

    partial steps towards stacked bar ui
    Former-commit-id: 2f93e135a1215dd6649c357fa220569c1034b1e2 [formerly 2313a7718fadcb0bdb54aca094a93420e336a65f] [formerly 2b2e64e12acf2aa70891be6e7df9473993c90218 [formerly 2ede78e60ca194950ed867563caf7228c3fbc140]]
    Former-commit-id: c61c0de6c7f6f049c012c000d0eb498df50b9138 [formerly 1d09520a1be7dd10d68bcfbbd18106f0bb2cbbcf]
    Former-commit-id: e893e99b7ec97f5f7032a469092580435673b998

---
+++ src/mitll/langtest/client/exercise/SectionWidget.java
@@ -0,0 +1,22 @@
+package mitll.langtest.client.exercise;
+
+import com.google.gwt.user.client.ui.Widget;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 4/3/13
+ * Time: 7:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SectionWidget {
+  String getCurrentSelection();
+  String getFirstItem();
+  void selectFirstAfterAny();
+  void selectItem(String section);
+  void retainCurrentSelectionState(String currentSelection);
+  void populateTypeWidget(Collection<String> items);
+  public Widget getWidget();
+}
