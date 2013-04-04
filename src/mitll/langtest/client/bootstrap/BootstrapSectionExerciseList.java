bbba06ad4736aa3f7a60a5eab03ef4820fd364fd
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-04 19:33:16 -0400

    valiant attempts at trying to get layout of button groups and stacked bar sections right
    Former-commit-id: ad42d4e59d6668d1f42255f5f7203121b9f5725c [formerly 89365d2b29f5aa3b2a0d90263b6425f5520b22e1] [formerly ad42d4e59d6668d1f42255f5f7203121b9f5725c [formerly 89365d2b29f5aa3b2a0d90263b6425f5520b22e1] [formerly 0165f6c51a42beb7a48d277c18243f3f7bd290a3 [formerly 2150b8172f3402af817c3bb6d0821123bd1b6df5]]]
    Former-commit-id: 8e71ba0a1ebe2d82043688feb689988a12dfadc2 [formerly d3f5e36718910f4c8fb7bb429c9a116c450687a3] [formerly b85ed32d19221bf8ce1e060d1a062028cb90ece5 [formerly d4107a9a96c9cd59ed9244f5ce81f201ad49096b]] [formerly d1f607f5bee706aaa16a2c6d36988de2bcbcc89f [formerly 45b74be864d10417a18e052197e0dfd4de33487f] [formerly 8247e17053e8fcb129aa16b54c9bfc249117ea96 [formerly 6b8b0857c631df4f7909b3d9a33c3a1e12982889]]]
    Former-commit-id: d1f607f5bee706aaa16a2c6d36988de2bcbcc89f [formerly 45b74be864d10417a18e052197e0dfd4de33487f]
    Former-commit-id: 0e15617802b5510049e7357ad0f3c33aa6bb19a6 [formerly c8aac4518b2dd2a0b334e534255ca9c5d6f752bc]
    Former-commit-id: b4a131c3f461742673df8fdcd35c6c511bdfeb08

--- src/mitll/langtest/client/bootstrap/BootstrapSectionExerciseList.java
+++ src/mitll/langtest/client/bootstrap/BootstrapSectionExerciseList.java
@@ -1,13 +1,25 @@
 package mitll.langtest.client.bootstrap;
 
+import com.github.gwtbootstrap.client.ui.Column;
 import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.SectionExerciseList;
 import mitll.langtest.client.exercise.SectionWidget;
 import mitll.langtest.client.user.UserFeedback;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,7 +29,7 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class BootstrapSectionExerciseList extends SectionExerciseList {
-  private static class SelList {
+/*  private static class SelList {
     private int current = 0;
     List<String> selections;
     public SelList(List<String> selections) { this.selections = selections; }
@@ -31,7 +43,7 @@ public class BootstrapSectionExerciseList extends SectionExerciseList {
     public void setCurrent(int current) {
       this.current = current;
     }
-  }
+  }*/
   public BootstrapSectionExerciseList(Panel currentExerciseVPanel, LangTestDatabaseAsync service,
                              UserFeedback feedback,
                              boolean showTurkToken, boolean showInOrder, boolean showListBox) {
@@ -49,9 +61,98 @@ public class BootstrapSectionExerciseList extends SectionExerciseList {
     addTableWithPager();
   }
 
+  protected Panel getWidgetsForTypes(Map<String, Map<String, Integer>> result, long userID) {
+    //final FlexTable flexTable = new FlexTable();
+    FluidContainer container = new FluidContainer();
+    //VerticalPanel container = new VerticalPanel();
+    //container.setWidth("100%");
+  //  int row = 0;
+    Set<String> types = result.keySet();
+    System.out.println("getExercises (success) for user = " + userID + " got types " + types);
+    typeToBox.clear();
+
+  //  String token = unencodeToken(History.getToken());
+ //   SelectionState selectionState = getSelectionState(token);
+
+    FluidRow outerRow = new FluidRow();
+    Column left = new Column(1);
+    outerRow.add(left);
+    Column right = new Column(11);
+    outerRow.add(right);
+    container.add(outerRow);
+
+    for (final String type : types) {
+      Map<String, Integer> sections = result.get(type);
+      System.out.println("\tgetExercises sections for " + type + " = " + sections);
+
+      final SectionWidget listBox = makeListBox(type);
+      typeToBox.put(type, listBox);
+      populateListBox(listBox, sections);
+     // int col = 0;
+      FluidRow leftRow = new FluidRow();
+      left.add(leftRow);
+      FluidRow rightRow = new FluidRow();
+      right.add(rightRow);
+
+      //HorizontalPanel fluidRow = new HorizontalPanel();
+      //FlowPanel fluidRow = new FlowPanel();
+     // fluidRow.setWidth("100%");
+
+      //container.add(fluidRow);
+
+      if (showListBoxes) {
+        HTML html = new HTML(type);
+        html.addStyleName("floatLeft");
+        leftRow.add(new Column(1,html));
+      //  fluidRow.add(html);
+        //fluidRow.add(new Column(1, html));
+        Widget widget = listBox.getWidget();
+        //Column w = new Column(11, widget);
+        //w.setWidth("100%");
+    //    widget.setWidth("100%");
+//        fluidRow.add(widget);
+        rightRow.add(new Column(11,widget));
+      //  rightRow.add(widget);
+        //right.add(widget);
+
+      } else {
+    /*    String typeValue = selectionState.typeToSection.get(type);
+        if (typeValue != null) {
+          fluidRow.add(new Column(1,new HTML(type)));
+          fluidRow.add(new Column(11,new HTML("<b>" + typeValue + "</b>")));
+        }*/
+      }
+    }
+
+    if (showListBoxes) {
+/*      flexTable.setWidget(row, 0, getEmailWidget());
+      flexTable.getFlexCellFormatter().setColSpan(row, 0, 2);*/
+
+      FluidRow fluidRow = new FluidRow();
+     // container.add(fluidRow);
+      Widget emailWidget = getEmailWidget();
+      fluidRow.add(new Column(12, emailWidget));
+
+      fluidRow = new FluidRow();
+    //  container.add(fluidRow);
+      Widget hideBoxesWidget = getHideBoxesWidget();
+      fluidRow.add(new Column(12, hideBoxesWidget));
+    }
+    else {
+/*      flexTable.setWidget(row, 0, new HTML("&nbsp;"));
+      flexTable.getFlexCellFormatter().setColSpan(row, 0, 2);*/
+    }
+    return container;
+  }
+
+  /**
+   * @see #getWidgetsForTypes(java.util.Map, long)
+   * @param type
+   * @return
+   */
   @Override
   protected SectionWidget makeListBox(String type) {
-    BarSectionWidget widgets = new BarSectionWidget(type, new ItemClickListener() {
+    SectionWidget widgets = new BarSectionWidget(type, new ItemClickListener() {
       @Override
       public void gotClick(String type, String item) {
         getListBoxOnClick(type);
@@ -59,5 +160,13 @@ public class BootstrapSectionExerciseList extends SectionExerciseList {
     });
 
     return widgets;
+  }
+
+  protected void setOtherListBoxes(final String selectedType, final String section) {
+    System.out.println("setOtherListBoxes skipping! type " + selectedType + "=" + section);
+  }
+
+  protected void populateListBox(Map<String, Collection<String>> result) {
+    System.out.println("populateListBox skipping! ");
   }
 }
