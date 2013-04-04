f51a8ae8d3696bc9e1eaed7ae8364124e303223a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-04 12:19:49 -0400

    reproduced list box behavior with button group
    Former-commit-id: 1b50872cd5ba569dc4dc8d7d13eda521cbc52e05 [formerly 4708d069c2a28e02086ecc5fd082a8186d5d134e] [formerly 5b3721141300b158c1762bba20815910fa6e2775 [formerly 7a39fa368b4ffeb281e9c89c2045b19d6b1925ce]] [formerly 1b50872cd5ba569dc4dc8d7d13eda521cbc52e05 [formerly 4708d069c2a28e02086ecc5fd082a8186d5d134e] [formerly 5b3721141300b158c1762bba20815910fa6e2775 [formerly 7a39fa368b4ffeb281e9c89c2045b19d6b1925ce]] [formerly 1b50872cd5ba569dc4dc8d7d13eda521cbc52e05 [formerly 4708d069c2a28e02086ecc5fd082a8186d5d134e] [formerly 5b3721141300b158c1762bba20815910fa6e2775 [formerly 7a39fa368b4ffeb281e9c89c2045b19d6b1925ce]] [formerly 35acb291f64ece031fd615ce8cc4de6aa6e474c8]]]
    Former-commit-id: 8cce6828a2772aafdc17ecd9861b11325fb75fd5 [formerly 2425dcae6560c17bb4ee7fb39707b47c8230453e] [formerly edde34cc9400751ab778ab403084c6043fdfdf53 [formerly 6342b5240d731dfb13b71fe7f9d84d2afd3ea80b]] [formerly 8cce6828a2772aafdc17ecd9861b11325fb75fd5 [formerly 2425dcae6560c17bb4ee7fb39707b47c8230453e] [formerly edde34cc9400751ab778ab403084c6043fdfdf53 [formerly 6342b5240d731dfb13b71fe7f9d84d2afd3ea80b]] [formerly e1a741f120e564cadd650ae9845faddd973e36ae]] [formerly a5b1a365f6a1f608b8769631fa696f46ffc16e40 [formerly 19a9ab822d23dc273723e94595ed477814118ee1] [formerly a5be510689153b48b91ae708714348c3a4b615ef [formerly d8032804e4ba2eab97a9de603ff378f3956f8b94]] [formerly a5b1a365f6a1f608b8769631fa696f46ffc16e40 [formerly 19a9ab822d23dc273723e94595ed477814118ee1] [formerly a5be510689153b48b91ae708714348c3a4b615ef [formerly d8032804e4ba2eab97a9de603ff378f3956f8b94]] [formerly 1f2c8b17757db0aeb4b3faa3f5bda3d80d7e4fa2]]]
    Former-commit-id: a5b1a365f6a1f608b8769631fa696f46ffc16e40 [formerly 19a9ab822d23dc273723e94595ed477814118ee1] [formerly a5be510689153b48b91ae708714348c3a4b615ef [formerly d8032804e4ba2eab97a9de603ff378f3956f8b94]]
    Former-commit-id: ea5f3ad18fcd1f76aa3a8ecbae31d5fa0725ed94 [formerly 6330281a10663072122bc56d4d287ddb1cf4137a] [formerly 9648c37a413fa0c469fb27e8258fabf9724ce95f [formerly 946a1bf222eb4c37b864c38df0a10e0f70668213]] [formerly cede643678ac50106c5ae8810333ae03937811af]
    Former-commit-id: 15e7eced2c1fc619e9075e80b7697fe36f0775f6
    Former-commit-id: bc40a39876ee1a2490aa6ee8114c0935c32f6f15 [formerly fdd1131dbefbcb531cdd7880c060172b2af2d85e]
    Former-commit-id: 6e67629a067502b14b4504973bfa4bc8b689090f

--- src/mitll/langtest/client/bootstrap/BootstrapSectionExerciseList.java
+++ src/mitll/langtest/client/bootstrap/BootstrapSectionExerciseList.java
@@ -17,8 +17,6 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class BootstrapSectionExerciseList extends SectionExerciseList {
-  //private Map<String,SelList> typeToBox = new HashMap<String, SelList>();
-
   private static class SelList {
     private int current = 0;
     List<String> selections;
@@ -53,80 +51,13 @@ public class BootstrapSectionExerciseList extends SectionExerciseList {
 
   @Override
   protected SectionWidget makeListBox(String type) {
-    BarSectionWidget widgets = new BarSectionWidget();
+    BarSectionWidget widgets = new BarSectionWidget(type, new ItemClickListener() {
+      @Override
+      public void gotClick(String type, String item) {
+        getListBoxOnClick(type);
+      }
+    });
 
     return widgets;
   }
-
-  /*  @Override
-  public void getExercises(final long userID) {
-    System.out.println("------- Get exercises for " + userID);
-    this.userID = userID;
-    service.getTypeToSection(new AsyncCallback<Map<String, Collection<String>>>() {
-      @Override
-      public void onFailure(Throwable caught) {}
-
-      @Override
-      public void onSuccess(Map<String, Collection<String>> result) {
-        //typeToSections = result;
-        sectionPanel.clear();
-
-        //final FlexTable g = new FlexTable();
-        String first = null;
-        int row = 0;
-        Set<String> types = result.keySet();
-        System.out.println("got types " + types);
-        typeToBox.clear();
-
-        for (final String type : types) {
-          if (first == null) first = type;
-
-          //final ListBox listBox = new ListBox();
-          //typeToBox.put(type,listBox);
-          List<String> sections = getSections(result, type);
-          typeToBox.put(type,new SelList(sections));
-         // populateListBox(listBox, sections);
-      *//*    listBox.addChangeHandler(new ChangeHandler() {
-            @Override
-            public void onChange(ChangeEvent event) {
-              getListBoxOnClick(listBox, type);
-            }
-          });
-          int col = 0;*//*
-          StackProgressBar stackProgressBar = new StackProgressBar();
-          boolean oddEven = false;
-          for (String section : sections) {
-            Bar bar = new Bar();
-            int percent = (int) (100f * (1f / (float) sections.size())); // TODO : get per item quantity
-            bar.setPercent(percent);
-            bar.setColor(oddEven ? Bar.Color.INFO : Bar.Color.DEFAULT);
-            bar.setText(section);
-            stackProgressBar.add(bar);
-            oddEven = !oddEven;
-          }
-          //g.setWidget(row, col++, new HTML(type));
-          //g.setWidget(row++,col,listBox);
-          FluidRow typeRow = new FluidRow();
-          Heading heading = new Heading(4);
-          heading.setText(type);
-          typeRow.add(new Column(1, heading));
-          typeRow.add(new Column(11,stackProgressBar));
-          sectionPanel.add(typeRow);
-        }
-        // doNestedSections();
-      //  g.setWidget(row, 0, getEmailWidget());
-      //  g.getFlexCellFormatter().setColSpan(row, 0, 2);
-
-       // sectionPanel.add(g);
-
-        if (first != null) {
-          //typeToBox.get(first).setSelectedIndex(1); // not any
-          pushFirstSelection(first);
-        }
-        else {
-          noSectionsGetExercises(userID);
-        }
-      }
-    });
-  }*/
 }
