749ead344e7abb7b42a14c9a9bd2e51079445de4
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-04 12:19:49 -0400

    reproduced list box behavior with button group
    Former-commit-id: 8b9ac4d26aa45f9b6550ccaa47cf5116a91e399a [formerly ed69386afd5c8797a43639c5ccb62dd15cc5af09]
    Former-commit-id: 03ab68f30e97d3f16d14a764de7e640ec9aec8f7

---
+++ src/mitll/langtest/client/bootstrap/ItemClickListener.java
@@ -0,0 +1,12 @@
+package mitll.langtest.client.bootstrap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 4/4/13
+ * Time: 12:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ItemClickListener {
+  void gotClick(String type, String item);
+}
