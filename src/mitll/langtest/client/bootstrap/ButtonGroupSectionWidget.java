dcd54ba6d2211ebe927062bad5228719e3ff7603
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-08 18:04:30 -0400

    made buttons toggle on and off, added text below buttons
    Former-commit-id: 92c5a0b7f8183734cd724d66729620229a1b7c60 [formerly 588da2ee98723a993d8247794284bd2a0c362af6] [formerly 839042334b8f7a7e5d58f69f59c1a76ac17bca05 [formerly cb95b3f88f7a77d8074a19a958e55ff7ceac933a]] [formerly 92c5a0b7f8183734cd724d66729620229a1b7c60 [formerly 588da2ee98723a993d8247794284bd2a0c362af6] [formerly 839042334b8f7a7e5d58f69f59c1a76ac17bca05 [formerly cb95b3f88f7a77d8074a19a958e55ff7ceac933a]] [formerly 92c5a0b7f8183734cd724d66729620229a1b7c60 [formerly 588da2ee98723a993d8247794284bd2a0c362af6] [formerly 839042334b8f7a7e5d58f69f59c1a76ac17bca05 [formerly cb95b3f88f7a77d8074a19a958e55ff7ceac933a]] [formerly 528c766f76585d4c85bd224aa98251139286b051 [formerly 8e5406d083c394d4de30c6d087d4a89aaee452be] [formerly adbe2e9555a4b595609dbd8e786d020c001e517d [formerly 5b50cf458637c2e1c7bfdb304a2a0e91ae41bdb7]]]]]
    Former-commit-id: 56d113840e72478a2fa4ab020c34c3ed3d133602 [formerly 1f986d293612bdba6fa70b1f9c5771da84f6448f] [formerly f0760e56522e81e363b00cec2d9d907ab9a7d91b [formerly 8c90376bbf876ed3ee5a43e6be257b46157921cf]] [formerly 56d113840e72478a2fa4ab020c34c3ed3d133602 [formerly 1f986d293612bdba6fa70b1f9c5771da84f6448f] [formerly f0760e56522e81e363b00cec2d9d907ab9a7d91b [formerly 8c90376bbf876ed3ee5a43e6be257b46157921cf]] [formerly b5448781c46b335ec547201a5da6073e7dd2fe43 [formerly e4d9934fe535a3b0a04eb7e175627a67a0ba257b] [formerly 89ec18cf3ad2940264256251c41d10fe1106a0b1 [formerly 59496d82bd12ff6774f53d37d67fbb4b8ab88f18]]]] [formerly 63b12706cee5feb0f616be8382ee5560896a8461 [formerly 8acf2190c07409d7b2bec1ce8ff77d3a5027fbde] [formerly afbcb7f268f593a5afd60d6a70f91ff4a6a213ff [formerly ef7f556d7be73d0ccdc146c3cb620679f02b9503]] [formerly 63b12706cee5feb0f616be8382ee5560896a8461 [formerly 8acf2190c07409d7b2bec1ce8ff77d3a5027fbde] [formerly afbcb7f268f593a5afd60d6a70f91ff4a6a213ff [formerly ef7f556d7be73d0ccdc146c3cb620679f02b9503]] [formerly 2c7534823dd9a1811e6da03d5ea48a3652904aec [formerly 38de00ba1c3047c32d59a50f5803814b543faf42] [formerly 363c13fe41936ca0755ddabdc3973ed7df81011a [formerly 129eeeb173e85899531756babacb43abeecfd722]]]]]
    Former-commit-id: 63b12706cee5feb0f616be8382ee5560896a8461 [formerly 8acf2190c07409d7b2bec1ce8ff77d3a5027fbde] [formerly afbcb7f268f593a5afd60d6a70f91ff4a6a213ff [formerly ef7f556d7be73d0ccdc146c3cb620679f02b9503]]
    Former-commit-id: 56d370475724b03eb513ce08629f6381b30d60c8 [formerly e1232d64f455d1a430c9d6ab6556423f5ef49e1b] [formerly 4d718f04b9775a346fa3c0487b62e0439002cf9a [formerly 1ef4c197f2557bde8016c1ebf97f3ebda7dbafc6]] [formerly ba4d36698bf8703f55ee5562e0750a7f52e48594 [formerly 72b5e94caf5bc71f7f1c4e51e6c69608f69dac4f] [formerly 230fa426cc10e5b58e5fb13e31bfb5c09a800988 [formerly a04ad5489ed2ad8c3de9068b55b6211db85c4d1e]]]
    Former-commit-id: e9234793a439763b500330225ab9c716d8645b58 [formerly 7b590bcae28c4bc435ffe612409eab9df0d29add] [formerly d108f0371a51c1d5787f6bf59f00a406de48a50e [formerly 05e33daa6e2bc6cd22984199b9aa0e178b638cd0]]
    Former-commit-id: f32671ebdadd83e5cf7f284ad3710f79f0f29a5f [formerly 657294da574b20883952a18b9db323b9bf82630a]
    Former-commit-id: 14b005f82130cab264d51944b14e3c5427d5130f

--- src/mitll/langtest/client/bootstrap/ButtonGroupSectionWidget.java
+++ src/mitll/langtest/client/bootstrap/ButtonGroupSectionWidget.java
@@ -3,9 +3,7 @@ package mitll.langtest.client.bootstrap;
 import com.github.gwtbootstrap.client.ui.Button;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
-import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.SectionExerciseList;
 import mitll.langtest.client.exercise.SectionWidget;
 
@@ -32,10 +30,11 @@ class ButtonGroupSectionWidget implements SectionWidget {
 
   public void addButton(Button b) {
     this.buttons.add(b);
+  //  b.setActive(true);
   }
   public void addClearButton(Button b) {
     clearButton = b;
-    clearButton.setEnabled(true);
+    clearButton.setEnabled(false);
     //clearButton.setVisible(false);
 
     clearButton.addClickHandler(new ClickHandler() {
@@ -44,7 +43,7 @@ class ButtonGroupSectionWidget implements SectionWidget {
         for (Button b : buttons) {
           if (b.isActive()) {
             b.setActive(false);
-            break;
+           // break;
           }
         }
         System.out.println("disable clear button " +clearButton);
@@ -56,13 +55,22 @@ class ButtonGroupSectionWidget implements SectionWidget {
 
   @Override
   public String getCurrentSelection() {
+    //System.out.println("getCurrentSelection for " +type + " checking " + buttons.size() + " buttons.");
+
     for (Button b : buttons) {
       if (b.isActive()) {
-        //System.out.println("getCurrentSelection for " +type + "=" + b.getText());
+        //System.out.println("\tgetCurrentSelection for " +type + "=" + b.getText());
         return b.getText().trim();
       }
     }
+    //System.out.println("\tgetCurrentSelection for " +type + " - none are selected");
+
     return SectionExerciseList.ANY;
+  }
+
+  private boolean isAnythingSelected() {
+    for (Button b : buttons) if (b.isActive()) return true;
+    return false;
   }
 
   /**
@@ -94,28 +102,48 @@ class ButtonGroupSectionWidget implements SectionWidget {
   @Override
   public void selectFirstAfterAny() {
     System.out.println("selectFirstAfterAny called?? --------------");
-    selectItem(getFirstItem());
+    selectItem(getFirstItem(), false);
   }
 
   @Override
-  public void selectItem(String section) {
+  public void selectItem(String section, boolean doToggle) {
     System.out.println("selectItem " + type + "="+section);
 
     for (Button b : buttons) {
-      if (b.isActive()) {
+      String trim = b.getText().trim();
+
+      if (b.isActive() && !trim.equals(section)) {
         b.setActive(false);
         break;
       }
     }
+
+    boolean didSelect = false;
+    // flip state
     for (Button b : buttons) {
-      if (b.getText().trim().equals(section)) {
-        b.setActive(true);
+      String trim = b.getText().trim();
+      if (trim.equals(section)) {
+        //System.out.println("\tselectItem found button is active = " +b.isActive());
+
+        b.setActive(!doToggle || !b.isActive());
+
+        //System.out.println("\tselectItem after, button is active = " +b.isActive());
+        didSelect = b.isActive();
         break;
       }
     }
+    boolean anythingSelected = isAnythingSelected();
+    if (didSelect && !anythingSelected) {
+      System.err.println("selectItem " + type + "="+section + " but nothing selected?");
+    }
+
+    setClearButtonState(section, anythingSelected);
+  }
+
+  private void setClearButtonState(String section, boolean anythingSelected) {
     if (clearButton != null) {
       System.out.println("\tselectItem for type " +type+"="+section + " set clear button.");
-      clearButton.setEnabled(true);
+      clearButton.setEnabled(anythingSelected);
     }
     else {
       System.err.println("clear button is not set? ");
