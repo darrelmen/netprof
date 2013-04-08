5d510be5b7e6ab56b334aa5917d4f389becbef25
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-08 13:27:43 -0400

    more or less got new scheme working
    Former-commit-id: e271c924709227de968fec09df30a28b0d9e1b6d [formerly c520439d0010d26af1ce8b6cf0b70c598a81c98e] [formerly fbbee9727be8764a8a79e3b74f345439a89b613d [formerly 2b51da93146a45b4e91331ed22ed868c48bcd4fe]] [formerly 31ff451b37c8e0d8bf301c235ce5949f6f2d9f12 [formerly f3b206979346455daceac94221019066c731a908] [formerly 1a260030463b2849746f82a8c2b32eed7e558994]]
    Former-commit-id: 91ecc6aca00e6054bb74b69a6f991693cd9238f8 [formerly 6ccd52a7dd7f13338f93840a555ba0d8225ed852] [formerly 6913c86b45fcdb5b3c6b4b26badee14d5dae891d [formerly 635b7dc46a4f5a8477fa024797b396ea60fa343a]] [formerly 4ea4d3863ceca03f8596a983cac7e05cc940803c [formerly f4b0716be345b7b35a0896d5bfdd7f681c32ee48]]
    Former-commit-id: 798d9b35c8ebe4204d0da2b98c7f03af9d5652f4 [formerly cace9577e7c67676a9deee433b323f7de40f3d48]
    Former-commit-id: 14c64b49c31341c0d0f3dde66ecd349ba77d9906 [formerly d5987993beec313ff0b05a385c5835d95520c30f]
    Former-commit-id: df58095858e5b320076bd8db8b085a313072f77e

---
+++ src/mitll/langtest/client/bootstrap/ButtonGroupSectionWidget.java
@@ -0,0 +1,182 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.SectionExerciseList;
+import mitll.langtest.client.exercise.SectionWidget;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+* Created with IntelliJ IDEA.
+* User: GO22670
+* Date: 4/8/13
+* Time: 11:19 AM
+* To change this template use File | Settings | File Templates.
+*/
+class ButtonGroupSectionWidget implements SectionWidget {
+  private List<Button> buttons = new ArrayList<Button>();
+  private Button clearButton;
+  private String type;
+  private FlexSectionExerciseList widgets;
+  LangTestDatabaseAsync service;
+
+  public ButtonGroupSectionWidget(FlexSectionExerciseList widgets, LangTestDatabaseAsync service, String type) {
+    this.widgets = widgets;
+    this.type = type;
+    this.service = service;
+  }
+
+  public void addButton(Button b) {
+    this.buttons.add(b);
+  }
+  public void addClearButton(Button b) {
+    clearButton = b;
+    clearButton.setEnabled(true);
+    //clearButton.setVisible(false);
+
+
+    clearButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        for (Button b : buttons) {
+          if (b.isActive()) {
+            b.setActive(false);
+            break;
+          }
+        }
+        System.out.println("disable clear button " +clearButton);
+
+        clearButton.setEnabled(false);
+      }
+    });
+  }
+
+  @Override
+  public String getCurrentSelection() {
+    for (Button b : buttons) {
+      if (b.isActive()) {
+        //System.out.println("getCurrentSelection for " +type + "=" + b.getText());
+        return b.getText().trim();
+      }
+    }
+    return SectionExerciseList.ANY;
+  }
+
+  /**
+   * @see FlexSectionExerciseList#populateListBoxAfterSelection(java.util.Map)
+   * @param inSet
+   */
+  @Override
+  public void enableInSet(Collection<String> inSet) {
+    System.out.println("enableInSet for " + type + " : " + inSet);
+
+    for (Button b : buttons) {
+      String trim = b.getText().trim();
+      b.setEnabled(inSet.contains(trim));
+    }
+  }
+
+  @Override
+  public void enableAll() {
+    for (Button b : buttons) {
+      b.setEnabled(true);
+    }
+  }
+
+  @Override
+  public String getFirstItem() {
+    return buttons.iterator().next().getText().trim();
+  }
+
+  @Override
+  public void selectFirstAfterAny() {
+    System.out.println("selectFirstAfterAny called?? --------------");
+    selectItem(getFirstItem());
+  }
+
+  @Override
+  public void selectItem(String section) {
+    System.out.println("selectItem " + type + "="+section);
+
+    for (Button b : buttons) {
+      if (b.isActive()) {
+        b.setActive(false);
+        break;
+      }
+    }
+    for (Button b : buttons) {
+      if (b.getText().trim().equals(section)) {
+        b.setActive(true);
+        break;
+      }
+    }
+    if (clearButton != null) {
+      System.out.println("\tselectItem for type " +type+"="+section + " set clear button.");
+      clearButton.setEnabled(true);
+    }
+    else {
+      System.err.println("clear button is not set? ");
+    }
+
+/*    final String section1 = section.equals(SectionExerciseList.ANY) ? "" : section;
+    System.out.println("\tselectItem 2 " + type + "="+section1);
+
+    service.getTypeToSectionsForTypeAndSection(type, section1, new AsyncCallback<Map<String, Collection<String>>>() {
+      @Override
+      public void onFailure(Throwable caught) {
+      }
+
+      @Override
+      public void onSuccess(Map<String, Collection<String>> result) {
+        System.out.println("\tselectItem onSuccess " + type + "=" + section1);
+
+        for (Map.Entry<String, Collection<String>> pair : result.entrySet()) {
+
+          String returnedType = pair.getKey();
+          if (!type.equals(returnedType)) {
+
+            SectionWidget sectionWidget = widgets.getWidgetForType(returnedType);
+            sectionWidget.enableInSet(pair.getValue());
+
+          }
+        }
+      }
+    });*/
+  }
+
+  @Override
+  public void retainCurrentSelectionState(String currentSelection) {
+    for (Button b : buttons) {
+      if (b.isActive()) {
+        b.setActive(false);
+        break;
+      }
+    }
+    boolean found = false;
+    for (Button b : buttons) {
+      if (b.getText().trim().equals(currentSelection)) {
+        b.setActive(true);
+        found = true;
+        break;
+      }
+    }
+    if (!found) buttons.iterator().next().setActive(true);
+  }
+
+  @Override
+  public void populateTypeWidget(Collection<String> items, Map<String, Integer> sectionToCount) {
+  }
+
+  @Override
+  public Widget getWidget() {
+    return null;
+  }
+}
