10002f28be69ada094290fc30e2e8df9b318b4a2
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-17 17:57:53 -0400

    better button group selection, better alignment of exercise panel
    Former-commit-id: 27d3e0273fb09c0f9896af516405c4e85a46b49e [formerly 3cd9e095c27bb618e89ed1b5798268b2d47f18e6] [formerly 9b390ac60895bd1103ec400bb0879a4f88cb4dc6 [formerly 5b5ed35da1cdb0b4b7a0faf4dfbb1a505c6bf124]] [formerly eae7197b7ed6f8a2d169e9bb4154eaaabdea1349 [formerly c5ff843b7f5a0f1f8dc23a3bdbd1a6740a3eb4b8] [formerly 53996ed5ec8d464272d1a001a7c432bd9a010425 [formerly 4102ebb7e92c17d546a9580274edd5334c7ab135]]]
    Former-commit-id: becc488bc39c1724edd1dadaf2b731e0f2bdf84f [formerly 08a22bdb2b9e94f5c64d98874b904fc22dae7cc9] [formerly 368c0cae78cc761fcb6399e076f8d3111ddc5321 [formerly 8adc3b18cad89e7a5cfe18b1b4f85756cc1aa9c4]] [formerly f645f4f64bb0a865ef637dce021ef94ca565e2e5 [formerly 6be1c89eba52ed638233a9a30dbe5c6a6fc7c2d0] [formerly c047ac4794ec6eacb48b78c2c552406cf74d88c2 [formerly 20098b848a8934eb5f9af571b6d3f4fb985ccf44]]]
    Former-commit-id: 52208ed51bd3e28de7643dc86d85613822fe76d6 [formerly ae7daab8c22362a4c845be6541947ba0835150a3] [formerly b4be2c6c0df6036b878400b57241cd5de90b31d2 [formerly 94f6ea77bafbd80448e7b54f0446a97b21949a44]]
    Former-commit-id: 51cb81c86dbb1c7b37182dd046a67c87e0f87f1b [formerly 09025231588443ee360c720b17005e4f6723c0fc]
    Former-commit-id: edf11e34dfa877561c3adff10a0e7d8d7a0a3f07

---
+++ src/mitll/langtest/client/TimedGame.java
@@ -0,0 +1,44 @@
+package mitll.langtest.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimedGame {
+  private final LangTest langTest;
+
+  public TimedGame(LangTest langTest) {
+    this.langTest = langTest;
+  }
+
+  void showTimedGameHelp(PropertyHandler props) {
+    List<String> msgs = new ArrayList<String>();
+    msgs.add("Practice your vocabulary by saying the matching " + props.getLanguage() + " phrase.");
+    String duration;// = "one " + "minute";
+    int secs = props.getGameTimeSeconds();
+    if (secs < 60) {
+      duration = secs + " seconds";
+    } else {
+      int min = secs / 60;
+      boolean even = secs % 60 == 0;
+      duration = min + " minute" + (min > 1 ? "s" : "") + (even ? "" : (secs - (min * 60)) + " secs");
+    }
+    msgs.add("See how many you can get right in " +
+      duration +
+      "!");
+    msgs.add("Press and hold the " + LangTest.RECORDING_KEY + " to record.");
+    msgs.add("Release to stop recording.");
+    msgs.add("Ready to start the clock?");
+    DialogHelper dialogHelper = new DialogHelper(false);
+    dialogHelper.showErrorMessage("Beat the clock!", msgs, "Yes!", new DialogHelper.CloseListener() {
+      @Override
+      public void gotYes() {
+        langTest.login();
+      }
+
+      @Override
+      public void gotNo() {
+
+      }
+    });
+  }
+}
