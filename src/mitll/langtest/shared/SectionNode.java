e0654ed74389780b64609d3d08e31b2cdc08646a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-18 12:15:47 -0400

    more or less working with tree fetch and recursive button creation
    Former-commit-id: 8e868fe1afd039ba7472fcb06a86706b64f7fc59 [formerly 576f4dd8efdd91492096f2d79ec5bd677db39eaa] [formerly 37f913157bf118114ef278ef55fa0ad5aea2eade [formerly 4f575ad45da07f8890a020733c41c776e2dbf120]]
    Former-commit-id: 1a8393a5cdde665940085f84d4ce9355e3b175aa [formerly be4c23afe5cf3ddc690c2c7f425eb27ce4456a61] [formerly 1cd90b7bae42608f2bd518033ddbc768edf06e7c [formerly a07e04fc41a7638e982539e37354ca670bdc2aa2]]
    Former-commit-id: 18e13407b9ee346699db9e4005ad57888dd4f44b [formerly 768fed15e7ce76cb85e8ef496cd72a1469d0b862]
    Former-commit-id: 040bbd46adf3e0f9a9e86465d7fdf94b5682e506

---
+++ src/mitll/langtest/shared/SectionNode.java
@@ -0,0 +1,32 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 3/29/13
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SectionNode implements IsSerializable {
+  public SectionNode() {}
+  private String type;
+  private String name;
+  private List<SectionNode> children = new ArrayList<SectionNode>();
+
+  public SectionNode(String type, String name) { this.type = type; this.name = name; }
+  public String getName() {
+    return name;
+  }
+
+  public void addChild(SectionNode node) { children.add(node);}
+  public List<SectionNode> getChildren() { return children; }
+
+  public String toString() {
+    return type+"="+name + (children.isEmpty() ? "" : (" : [(" + children.size() + ") " + children +"]"));
+  }
+}
