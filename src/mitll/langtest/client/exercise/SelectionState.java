e6dddffe6e5854ebfebf2337c7853d0849cac173
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-18 19:57:11 -0400

    support for jumping to flashcard mode from word list
    Former-commit-id: 0844f58e98b5c6e82bd6cbe487ed3525d7d1160e [formerly 527cce85687c1219c55eaa157a3c74f2500070a5] [formerly d0d898bba5641933dbca8758c4ac7fb50a109837 [formerly 28909161a870437ffcea90dd09ce3602bb8aaec0]]
    Former-commit-id: 12df0d9caea3ce653df60eae7d4fab9d605f49d6 [formerly 82a8a924f306cba5412abfe44d93f30f5b7acf52] [formerly c3ea0a2f702814016cf376271cca57dbfb86c510 [formerly 1f82b2159bd304cff935836e5a420e930ae0beee]]
    Former-commit-id: 8df376b08ab5ab82937e22934107f3335cfe76a9 [formerly 7ac6ad66e1f5f27281a41eb05f3e13a2090c10a1]
    Former-commit-id: cc68ba115be7d7c10254732de1b8012bf0f8a7f0

---
+++ src/mitll/langtest/client/exercise/SelectionState.java
@@ -0,0 +1,106 @@
+package mitll.langtest.client.exercise;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.user.client.History;
+import sun.nio.cs.HistoricallyNamedCharset;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+* Created with IntelliJ IDEA.
+* User: GO22670
+* Date: 4/18/13
+* Time: 6:31 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class SelectionState {
+  public String item;
+  public Map<String, Collection<String>> typeToSection = new HashMap<String, Collection<String>>();
+
+  public SelectionState(ValueChangeEvent<String> event) {
+    parseToken(getTokenFromEvent(event));
+  }
+
+  public SelectionState() {
+    this(History.getToken());
+  }
+
+  public SelectionState(String token) {
+    parseToken(unencodeToken(token));
+  }
+
+  public boolean isEmpty() { return typeToSection.isEmpty(); }
+
+  protected String getTokenFromEvent(ValueChangeEvent<String> event) {
+    String token = event.getValue();
+    token = unencodeToken(token);
+    return token;
+  }
+
+
+  protected String unencodeToken(String token) {
+    token = token.replaceAll("%3D", "=").replaceAll("%3B", ";").replaceAll("%2", " ").replaceAll("\\+", " ");
+    return token;
+  }
+
+  private void parseToken(String token) {
+    String[] parts = token.split(";");
+
+    for (String part : parts) {
+      //System.out.println("getSelectionState : part " + part + " : " + Arrays.asList(parts));
+
+      if (part.contains("=")) {
+        String[] segments = part.split("=");
+        //System.out.println("\tpart " + part + " : " + Arrays.asList(segments));
+
+        String type = segments[0].trim();
+        String section = segments[1].trim();
+        String[] split = section.split(",");
+        List<String> sections = Arrays.asList(split);
+
+        if (sections.isEmpty()) {
+          System.err.println("\tpart " + part + " is badly formed ");
+        }
+        else {
+          add(type, sections);
+        }
+        //System.out.println("getSelectionState : part " + part + " : " + type + "->" +section + " : " + selectionState);
+      }
+      else if (part.length() > 0) {
+        System.err.println("getSelectionState skipping part '" + part+ "'");
+      }
+    }
+
+    if (token.contains("item")) {
+      int item1 = token.indexOf("item=");
+      String itemValue = token.substring(item1+"item=".length());
+      //System.out.println("getSelectionState : got item = '" + itemValue +"'");
+      setItem(itemValue);
+    }
+
+    System.out.println("getSelectionState : got " +
+      this +
+      " from token '" +token + "'");
+  }
+
+  public void add(String type, Collection<String> section) {
+    typeToSection.put(type, section);
+  }
+
+  public void setItem(String item) {
+    this.item = item;
+  }
+
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    for (Collection<String> section : typeToSection.values()) {
+      builder.append(section).append(", ");
+    }
+    String s = builder.toString();
+    return s.substring(0, Math.max(0,s.length() - 2));
+  }
+}
