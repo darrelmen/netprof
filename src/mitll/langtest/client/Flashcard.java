5458e1fc43871bb3e20283b775446a3ddebad447
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-22 13:43:27 -0400

    more UI fixes
    Former-commit-id: cbc9595622aef972a23064ca44701cf1d08684b0 [formerly c7771a349f6d86430a5b75cd54cfdb2550783362] [formerly bbb52f31c31ec3a5ed0722647232a25a51efb776 [formerly 092961809ee6d6d2e667bbf3814498ad414baa2c]]
    Former-commit-id: a22fdf5121cf0fa65453b6ad8cb0343661259df3 [formerly d5c49aab55c8f110964357e9c8bd12228bd10126] [formerly beeddeb7e94ff725de2830fc8f9ba6067fc8ff33 [formerly 889d96bfbf07419daf2401cf15349bd24bacf4ca]]
    Former-commit-id: 706e5896dc6276870a5b8afa3694c895e2277ae5 [formerly 834a7c46d1e9ef128320b7f00cfb817fb5ea2e22]
    Former-commit-id: 5313fbf418c39a84cf947e8f3ac066c01246c27e

---
+++ src/mitll/langtest/client/Flashcard.java
@@ -0,0 +1,42 @@
+package mitll.langtest.client;
+
+import com.github.gwtbootstrap.client.ui.Image;
+import com.github.gwtbootstrap.client.ui.Paragraph;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
+
+public class Flashcard {
+  HorizontalPanel makeFlashcardHeaderRow(String splashText) {
+    HorizontalPanel headerRow = new HorizontalPanel();
+    headerRow.setWidth("100%");
+    headerRow.addStyleName("headerBackground");
+    headerRow.addStyleName("headerLowerBorder");
+
+    Image flashcardImage = new Image(LangTest.LANGTEST_IMAGES + "flashcardIcon2.png");
+    flashcardImage.addStyleName("floatLeft");
+
+    FlowPanel iconLeftHeader = new FlowPanel();
+    headerRow.add(iconLeftHeader);
+
+    Panel flashcard = new FlowPanel();
+    flashcard.addStyleName("inlineStyle");
+    flashcard.addStyleName("headerBackground");
+    flashcard.addStyleName("leftAlign");
+    Paragraph appName = new Paragraph("FLASHCARD");
+    appName.addStyleName("bigFont");
+    flashcard.add(appName);
+    Paragraph subtitle = new Paragraph(splashText);//langTest.getProps().getSplash());
+    subtitle.addStyleName("subtitleForeground");
+
+    flashcard.add(subtitle);
+    iconLeftHeader.add(flashcardImage);
+    iconLeftHeader.add(flashcard);
+    headerRow.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+
+    Image collab = new Image(LangTest.LANGTEST_IMAGES + "collabIcon3.png");
+    headerRow.add(collab);
+    return headerRow;
+  }
+}
