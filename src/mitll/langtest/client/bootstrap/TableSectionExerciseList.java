248119310d452bb92b57839fea4ae1b77bb1c8c4
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-23 18:29:22 -0400

    added table section exercise list
    Former-commit-id: 9e7379186b8ac6e95caf2fed9501ed581d9cfed1 [formerly f5be8eef3a284682aeb654f981b98f71c515fda7] [formerly 079b09d6563da3e60f8cc74f51c097a71ed107ed]
    Former-commit-id: 23b666e178aa653d2a31f8c5ac90c4e19605cfea
    Former-commit-id: 7cbaddb5430f5960967d6d54ce8b399a69f526fc

---
+++ src/mitll/langtest/client/bootstrap/TableSectionExerciseList.java
@@ -0,0 +1,198 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.CellTable;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
+import com.google.gwt.view.client.SingleSelectionModel;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.table.PagerTable;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.shared.Exercise;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 4/23/13
+ * Time: 4:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TableSectionExerciseList extends FlexSectionExerciseList {
+  public TableSectionExerciseList(FluidRow secondRow, Panel currentExerciseVPanel, LangTestDatabaseAsync service,
+                                  UserFeedback feedback, boolean showTurkToken, boolean showInOrder,
+                                  boolean showListBox, ExerciseController controller) {
+    super(secondRow, currentExerciseVPanel, service, feedback, showTurkToken, showInOrder, showListBox, controller);    //To change body of overridden methods use File | Settings | File Templates.
+    setWidth("100%");
+  }
+
+  public void doInitial(final Map<String, Collection<String>> typeToSection) {
+    service.getNumExercisesForSelectionState(typeToSection, new AsyncCallback<Integer>() {
+      @Override
+      public void onFailure(Throwable caught) {
+      }
+
+      @Override
+      public void onSuccess(Integer result) {
+        System.out.println("num exercises for " + typeToSection + " is " + result);
+        removeComponents();
+        FlowPanel p = new FlowPanel();
+        p.setWidth("100%");
+        Widget asyncTable = getAsyncTable(typeToSection, result);
+        p.add(asyncTable);
+        add(p);
+      }
+    });
+  }
+
+  @Override
+  public void clear() {
+    System.out.println("remove all components ");
+
+    removeComponents();
+  }
+
+  protected void noSectionsGetExercises(long userID) {
+    Map<String, Collection<String>> objectObjectMap = Collections.emptyMap();
+    doInitial(objectObjectMap);
+  }
+
+    @Override
+  protected void addTableWithPager() {  }
+
+
+  private Widget getAsyncTable(Map<String, Collection<String>> typeToSection,int numResults) {
+    CellTable<Exercise> table = makeCellTable();
+    table.setWidth("100%");
+   // TextColumn<Result> id = addColumnsToTable(gradingView, showQuestionColumn, grades, grader, numGrades, table);
+    table.setRowCount(numResults, true);
+    table.setVisibleRange(0,Math.min(numResults,15));
+    createProvider(typeToSection, numResults,table);
+
+    // Add a ColumnSortEvent.AsyncHandler to connect sorting to the
+    // AsyncDataPRrovider.
+/*    ColumnSortEvent.AsyncHandler columnSortHandler = new ColumnSortEvent.AsyncHandler(table);
+    table.addColumnSortHandler(columnSortHandler);*/
+
+    // We know that the data is sorted alphabetically by default.
+   // table.getColumnSortList().push(id);
+
+    Panel pagerAndTable = getPagerAndTable(table);
+    pagerAndTable.setWidth("100%");
+    return pagerAndTable;
+  }
+
+  private AsyncDataProvider<Exercise> createProvider(final Map<String, Collection<String>> typeToSection,
+                                                     final int numResults, CellTable<Exercise> table) {
+    AsyncDataProvider<Exercise> dataProvider = new AsyncDataProvider<Exercise>() {
+      @Override
+      protected void onRangeChanged(HasData<Exercise> display) {
+        final int start = display.getVisibleRange().getStart();
+        int end = start + display.getVisibleRange().getLength();
+        end = end >= numResults ? numResults : end;
+        //System.out.println("asking for " + start +"->" + end);
+        final int fend = end;
+        service.getFullExercisesForSelectionState(typeToSection, start, end, new AsyncCallback<List<Exercise>>() {
+          @Override
+          public void onFailure(Throwable caught) {
+            Window.alert("Can't contact server.");
+          }
+
+          @Override
+          public void onSuccess(List<Exercise> result) {
+            System.out.println("createProvider : onSuccess for " + start + "->" + fend + " got " + result.size());
+
+            updateRowData(start, result);
+          }
+        });
+      }
+    };
+
+    // Connect the table to the data provider.
+    dataProvider.addDataDisplay(table);
+    dataProvider.updateRowCount(numResults, true);
+
+    return dataProvider;
+  }
+
+  private Panel getPagerAndTable(CellTable<Exercise> table) {
+    return new PagerTable().getPagerAndTable(table, table, PAGE_SIZE, 1000);
+  }
+
+  @Override
+  protected CellTable<Exercise> makeCellTable() {
+ //   CellTable.Resources o = GWT.create(TableResources.class);
+    CellTable<Exercise> table = new CellTable<Exercise>(PAGE_SIZE);//, o);
+
+    table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.DISABLED);
+
+    // table.setWidth("100%", true);
+    table.setWidth("100%");
+    table.setHeight("auto");
+
+    // Add a selection model to handle user selection.
+    final SingleSelectionModel<Exercise> selectionModel = new SingleSelectionModel<Exercise>();
+    table.setSelectionModel(selectionModel);
+
+    addColumnsToTable(table);
+
+    return table;
+
+  }
+
+  protected void addColumnsToTable(CellTable<Exercise> table) {
+    TextColumn<Exercise> english = new TextColumn<Exercise>() {
+      @Override
+      public String getValue(Exercise exercise) {
+        return "" + exercise.getEnglishSentence();
+      }
+    };
+    english.setSortable(true);
+    table.addColumn(english, "Word/Expression");
+
+    TextColumn<Exercise> flword = new TextColumn<Exercise>() {
+      @Override
+      public String getValue(Exercise exercise) {
+        return "" + exercise.getRefSentence();
+      }
+    };
+    flword.setSortable(true);
+    table.addColumn(flword, controller.getLanguage());
+
+    TextColumn<Exercise> translit = new TextColumn<Exercise>() {
+      @Override
+      public String getValue(Exercise exercise) {
+        return "" + exercise.getTranslitSentence();
+      }
+    };
+    translit.setSortable(true);
+    table.addColumn(translit, "Transliteration");
+  }
+
+
+  /**
+   * When we get a history token push, select the exercise type, section, and optionally item.
+   * @param typeToSection
+   * @param item null is OK
+   * @see #onValueChange(com.google.gwt.event.logical.shared.ValueChangeEvent)
+   */
+  @Override
+
+  protected void loadExercises(Map<String, Collection<String>> typeToSection, final String item) {
+    System.out.println("loadExercises " + typeToSection + " and item '" + item + "'");
+    doInitial(typeToSection);
+  }
+}
