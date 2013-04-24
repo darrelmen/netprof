26fba24cdacf04fc8c95d0fda8100db1d846c22c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-24 19:08:35 -0400

    steps towards row highlight
    Former-commit-id: 6c57a2a99016cf226e30ccb1e875c14dd0649811 [formerly 416b5f3fbe88d361065399bae1e73c610cefcf28] [formerly 6c57a2a99016cf226e30ccb1e875c14dd0649811 [formerly 416b5f3fbe88d361065399bae1e73c610cefcf28] [formerly 4f5a67daf43345bf8b94514255fda47ed14a5510 [formerly 24e5e6772e920b050b80edbc61cbf7dfb4d10ad5]]]
    Former-commit-id: 16df6a7da0f31eddbfbff8dfe2976cdffa3cebb7 [formerly f750466ef411aa32b8bdac79bd2087e9bd3210a2] [formerly d322cb0498b05330e184908f84a415353fa4033d [formerly ab4f68690074e9d13f6f697092b5b7f9c4d86b84]] [formerly 10231b2306b7c04f644ebf14a53b481404c75aa7 [formerly 95fdaa7576566518e90b0bbe8e3bfd0d925226c5] [formerly a0ba44a0a93ce68942b62b791397bfce9e65e14f [formerly 9299913f39691ae46b8fd9bf5d023838f7f75165]]]
    Former-commit-id: 10231b2306b7c04f644ebf14a53b481404c75aa7 [formerly 95fdaa7576566518e90b0bbe8e3bfd0d925226c5]
    Former-commit-id: 6efbef9ac0883bcf33a1fbc710c5c6c2229cc18e [formerly 9f78ce902450e2d60033bbbadf4591b5829e65dc]
    Former-commit-id: d86d99ab904f2045e669a3ce8438f52b15ab6aa8

--- src/mitll/langtest/client/bootstrap/TableSectionExerciseList.java
+++ src/mitll/langtest/client/bootstrap/TableSectionExerciseList.java
@@ -1,22 +1,26 @@
 package mitll.langtest.client.bootstrap;
 
 import com.github.gwtbootstrap.client.ui.CellTable;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
 import com.github.gwtbootstrap.client.ui.FluidRow;
-import com.google.gwt.dom.client.Style;
+import com.github.gwtbootstrap.client.ui.SimplePager;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.AsyncDataProvider;
 import com.google.gwt.view.client.HasData;
+import com.google.gwt.view.client.HasRows;
 import com.google.gwt.view.client.SingleSelectionModel;
 import mitll.langtest.client.ExceptionHandlerDialog;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
-import mitll.langtest.client.table.PagerTable;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.Exercise;
 
@@ -40,7 +44,7 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
     setWidth("100%");
   }
 
-  public void doInitial(final Map<String, Collection<String>> typeToSection) {
+  public void addTableToLayout(final Map<String, Collection<String>> typeToSection) {
     service.getNumExercisesForSelectionState(typeToSection, new AsyncCallback<Integer>() {
       @Override
       public void onFailure(Throwable caught) {
@@ -53,9 +57,13 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
         removeComponents();
         FlowPanel p = new FlowPanel();
         p.setWidth("100%");
+        p.setHeight("100%");
         Widget asyncTable = getAsyncTable(typeToSection, result);
         p.add(asyncTable);
         add(p);
+        FluidContainer container = new FluidContainer();
+        addPreviewWidgets(container);
+        add(container);
       }
     });
   }
@@ -69,7 +77,7 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
 
   protected void noSectionsGetExercises(long userID) {
     Map<String, Collection<String>> objectObjectMap = Collections.emptyMap();
-    doInitial(objectObjectMap);
+    addTableToLayout(objectObjectMap);
   }
 
     @Override
@@ -81,7 +89,7 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
     table.setWidth("100%");
     table.setRowCount(numResults, true);
     int numRows = getNumTableRowsGivenScreenHeight();
-
+    numRows = Math.min(10,numRows);
     table.setVisibleRange(0,Math.min(numResults,numRows));
     createProvider(typeToSection, numResults,table);
 
@@ -129,7 +137,26 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
   }
 
   private Panel getPagerAndTable(CellTable<Exercise> table) {
-    return new PagerTable().getPagerAndTable(table, table, PAGE_SIZE, 1000);
+    return getPagerAndTable(table, table, PAGE_SIZE, 1000);
+  }
+
+  private Panel getPagerAndTable(HasRows table, Widget tableAsPanel, int pageSize, int fastForwardRows) {
+    SimplePager.Resources DEFAULT_RESOURCES = GWT.create(SimplePager.Resources.class);
+    SimplePager pager = new SimplePager(SimplePager.TextLocation.CENTER, DEFAULT_RESOURCES, true, fastForwardRows, true);
+    pager.addStyleName("alignCenter");
+    // Set the cellList as the display.
+    pager.setDisplay(table);
+    pager.setPageSize(pageSize);
+    // Add the pager and list to the page.
+    VerticalPanel vPanel = new VerticalPanel();
+    vPanel.setBorderWidth(1);
+    vPanel.add(tableAsPanel);
+
+    FluidRow row = new FluidRow();
+    row.add(new Column(4,5,pager));
+    vPanel.add(row);
+
+    return vPanel;
   }
 
   @Override
@@ -138,7 +165,7 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
 
     table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.DISABLED);
 
-     table.setWidth("100%", true);
+    table.setWidth("100%", true);
     table.setHeight("auto");
 
     // Add a selection model to handle user selection.
@@ -148,7 +175,6 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
     addColumnsToTable(table);
 
     return table;
-
   }
 
   protected void addColumnsToTable(CellTable<Exercise> table) {
@@ -194,7 +220,7 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
 
   protected void loadExercises(Map<String, Collection<String>> typeToSection, final String item) {
     System.out.println("loadExercises " + typeToSection + " and item '" + item + "'");
-    doInitial(typeToSection);
+    addTableToLayout(typeToSection);
   }
 
   @Override
@@ -202,6 +228,8 @@ public class TableSectionExerciseList extends FlexSectionExerciseList {
     setScrollPanelWidth();
 
     int numRows = getNumTableRowsGivenScreenHeight();
+    numRows = Math.min(10,numRows);
+
     if (table.getPageSize() != numRows) {
       System.out.println("onResize : num rows now " + numRows);
       table.setPageSize(numRows);
