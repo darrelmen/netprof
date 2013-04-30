f36f0b9e420c06f8a42fa3a5227cedd025ab21a8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-04-30 16:52:24 -0400

    latest ui fixes, including moving focus automatically to preview flashcard versions
    Former-commit-id: ccf7b7b4b6ef4a68fc3856dfcef27d4910fd8f82 [formerly ccf7b7b4b6ef4a68fc3856dfcef27d4910fd8f82 [formerly ccf7b7b4b6ef4a68fc3856dfcef27d4910fd8f82 [formerly 59deef61dacf117bc4c540bc49c77a37b91012d3]]]
    Former-commit-id: 833d37a48aea1c3058de3743cf807f06cbc63013 [formerly 833d37a48aea1c3058de3743cf807f06cbc63013 [formerly c27b97cae1d3b53d7ae8d0c9274df9b787ecd412]] [formerly afc9f9002b10aaadf9a9788bdc1bba0f35677365 [formerly afc9f9002b10aaadf9a9788bdc1bba0f35677365 [formerly 77d682da458e07e292c9ea969225e52f0e88630e]]]
    Former-commit-id: afc9f9002b10aaadf9a9788bdc1bba0f35677365
    Former-commit-id: 064bee0744a439b8640d8192936610b510055eab [formerly e1b9f4524ae881a8919d4384913412b267fd5990]
    Former-commit-id: f8f5863446a7ae9f9ceb1c847ddc3fbe755093de

--- src/mitll/langtest/client/Flashcard.java
+++ src/mitll/langtest/client/Flashcard.java
@@ -2,6 +2,7 @@ package mitll.langtest.client;
 
 import com.github.gwtbootstrap.client.ui.Image;
 import com.github.gwtbootstrap.client.ui.Paragraph;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -27,8 +28,9 @@ public class Flashcard {
     Paragraph appName = new Paragraph("FLASHCARD");
     appName.addStyleName("bigFont");
     flashcard.add(appName);
-    Paragraph subtitle = new Paragraph(splashText);//langTest.getProps().getSplash());
+    Paragraph subtitle = new Paragraph(splashText);
     subtitle.addStyleName("subtitleForeground");
+    DOM.setStyleAttribute(subtitle.getElement(), "marginBottom", "5px");
 
     flashcard.add(subtitle);
     iconLeftHeader.add(flashcardImage);
