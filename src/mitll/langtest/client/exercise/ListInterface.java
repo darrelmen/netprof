89e706e96563e32883656d835f42d696bab214f3
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-05-23 21:04:33 -0400

    changes for pashto -- font checker, progress bar, better keyboard focus, etc.
    Former-commit-id: f4d08f486bc6e193e3f8f22866bf12b4cc83f0a6 [formerly 4ab0ab3eae2fa8e8ba609a9a2095a5943a009966] [formerly 9cb5a09f42ec3cb57df33976a2b20adffa0f9c76 [formerly ff2d3fd7172c9b91643f34117a6578499dab4218]] [formerly c9162697f33ef6766dc1591350f7970f47b2d377 [formerly 3edfc43ff9292b9fbca8eae0414308d0ab8bea4d] [formerly e6de546412e4be70ad4973fd056353ff75fbd200]]
    Former-commit-id: 975ddf5ad3a355aacbf8d1749ba7e1c31cf0c5d9 [formerly 1d4db4272f6d1bfef3ed814b9844b2a7629e6bd0] [formerly 19503e1c4ff971b1928fd138f1a25125d2f5b651 [formerly 66d30680d9379765ec637092ddd0d48e6f9cc179]] [formerly e57e516974ffd81c82c443708e439eb5417a0193 [formerly 3ca59f57de0dfb710b48a937bb507b127223d3cc]]
    Former-commit-id: 4403148873c4a27530cde06a9256b6c5edae67f8 [formerly 2f64cf1c0b185f453b563e8b66a055204aed2366]
    Former-commit-id: 4bebf939d9169aea10d0debe594f6f9cdf522bb0 [formerly 3c468c88c53f50c0d279ab758ac34a2af88a6e72]
    Former-commit-id: 9ab772453ff1cf3b375c4796866ecf3965110c6c

--- src/mitll/langtest/client/exercise/ListInterface.java
+++ src/mitll/langtest/client/exercise/ListInterface.java
@@ -28,8 +28,6 @@ public interface ListInterface extends RequiresResize {
    */
   void getExercises(long userID);
 
-  //void getExercisesInOrder();
-
   /**
    * @see mitll.langtest.client.LangTest#makeExerciseList(com.google.gwt.user.client.ui.Panel, boolean)
    * @param exercise_title
@@ -63,4 +61,8 @@ public interface ListInterface extends RequiresResize {
    * @see mitll.langtest.client.LangTest#resetState()
    */
   void removeCurrentExercise();
+
+  int getPercentComplete();
+
+  int getComplete();
 }
