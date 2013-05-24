601e8bf0b2af3c81eb4c14db69a0ba385d682606
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-05-23 21:04:33 -0400

    changes for pashto -- font checker, progress bar, better keyboard focus, etc.
    Former-commit-id: 0dfb4120921f79eb81a9f06272249412f53571cd [formerly c8962b2d04e96d38e8cba6e89bb6095e094be03e]
    Former-commit-id: c008b3dfe2c678332bb1e0a55fc73c566d7aa06f [formerly ddad2932f3018d1a3250b6f3bd2383e25b820cbf]
    Former-commit-id: fb444ef7399d642588f0187791bb3fcf36129f14

--- src/mitll/langtest/client/exercise/ExerciseList.java
+++ src/mitll/langtest/client/exercise/ExerciseList.java
@@ -27,8 +27,10 @@ import mitll.langtest.shared.ExerciseShell;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Handles left side of NetPron2 -- which exercise is the current one, highlighting, etc.
@@ -418,6 +420,11 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
   }
 
   @Override
+  public int getPercentComplete() { return (int) (100f*((float)visited.size()/(float)currentExercises.size())); }
+  @Override
+  public int getComplete() { return visited.size(); }
+
+  @Override
   public void removeCurrentExercise() {
     Widget current = innerContainer.getWidget();
     if (current != null) {
@@ -453,8 +460,12 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
    */
   @Override
   public boolean loadNextExercise(ExerciseShell current) {
-    //System.out.println("loadNextExercise " +current);
+    System.out.println("---> loadNextExercise " +current + " visited : " + visited.size());
+
+
     int i = getIndex(current);
+
+    visited.add(i);
 
     boolean onLast = i == currentExercises.size() - 1;
     if (onLast) {
@@ -468,6 +479,8 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
     }
     return onLast;
   }
+
+  Set<Integer> visited = new HashSet<Integer>();
 
   /**
    * So a turker can get credit for their work.
