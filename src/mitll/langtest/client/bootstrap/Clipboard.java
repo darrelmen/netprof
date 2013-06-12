d8bad682c2523ff14586d6aba651a0b5661a2aa5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-06-12 17:04:30 -0400

    improvements to audio splitting -- throws out more that have bad phonemes, fix for race condition on getting back exercise lists, refactoring
    Former-commit-id: 1eefc36139fa7e3172c070c11ae205a4efeae198 [formerly aff2c7dfd4a9e29d7927fe12db08389056d3bb87] [formerly 9a6a2af2308b11327bc5bf7f61fdac6d32e479f3 [formerly abf2f20a4497ddbb1f1f1afcf091550dbdaa3ae2]] [formerly 9da53a79aeabe2ea74bcef16adfefb05cdde8c1a [formerly 1dd202330c2f91e320729ddb97dad9cbd326ea4d] [formerly ce320a95952b654523bcc065a09d2204e0b1f94c [formerly 2a05dddff5e006729fbb5c55963880ee9da45078]]]
    Former-commit-id: 03f2a94f8cead7c4a2080c2f1240791b2b893ba3 [formerly 0ff99dc31a6ce87d9d1d964ba6e3f8e2244b705b] [formerly 75d9253317a363919f43c4b7f0f8038f82163c82 [formerly ab8add12128a7c0f9d44500c257bb6ebfb6d6448]]
    Former-commit-id: 4ec3f600a343fd125717e59a4ae4e04e0b127c8f [formerly 35664e4d3a5610068475d82037415d81ee8ab879]
    Former-commit-id: f6d2e7c9dbb243dc501b82ed537db8837b6bb719

---
+++ src/mitll/langtest/client/bootstrap/Clipboard.java
@@ -0,0 +1,11 @@
+package mitll.langtest.client.bootstrap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 6/12/13
+ * Time: 3:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Clipboard {
+}
