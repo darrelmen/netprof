4ddd93edde1f159752201f7c6bac6dad4c717c80
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-06-17 17:38:25 -0400

    updates for crt1cPashto items
    Former-commit-id: 731707d54bc5ab9793e50703063e70f5404aa240 [formerly 1aa4c7bbdb1afe139b0b565e9d9aba4e79e94608] [formerly 731707d54bc5ab9793e50703063e70f5404aa240 [formerly 1aa4c7bbdb1afe139b0b565e9d9aba4e79e94608] [formerly 626f508e52981dc779ef6074aebb9dd62dea6664]]
    Former-commit-id: 3dda0108c8b03d5c603ce96b71a138b2007442ae [formerly 13d8bef2081764d7f78ecf45da6f85c06561e947] [formerly d3f20d5dfd12a113cbeec579594421f700b3566a] [formerly c7de790c929c83a434ca3b7999fc6a88ebf59b16 [formerly ef3bf7218ed03bf237f5fb1eec126e82540e61f9] [formerly d004d0ce65b07cd3191a3ecfdbe62993d9cc4b00]]
    Former-commit-id: 2d34aa1f0f0bf103daea0282ea689918562c56af [formerly c7de790c929c83a434ca3b7999fc6a88ebf59b16 [formerly ef3bf7218ed03bf237f5fb1eec126e82540e61f9]]
    Former-commit-id: 0649df516e9811d95beedc84fcf29a6896546797
    Former-commit-id: 473b610d608e55b97d3fd91c9065bf51aeba2aaa

--- src/mitll/langtest/client/recorder/RecordButton.java
+++ src/mitll/langtest/client/recorder/RecordButton.java
@@ -7,11 +7,11 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.FocusEvent;
 import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.Image;
 
 /**
  * Basically a click handler and a timer to click stop recording, if the user doesn't.
@@ -21,27 +21,41 @@ import com.google.gwt.user.client.ui.FocusWidget;
  * To change this template use File | Settings | File Templates.
  */
 public abstract class RecordButton {
+  private static final int SPACE_CHAR = 32;
+  private static final int PERIOD_MILLIS = 500;
+
   private boolean recording = false;
   private Timer recordTimer;
   private FocusWidget record = null;
   private int autoStopDelay;
   private HandlerRegistration keyHandler;
   private boolean hasFocus = false;
+  private Image recordImage1, recordImage2;
 
-  public RecordButton(int delay) {
+  /**
+   * @see mitll.langtest.client.bootstrap.BootstrapExercisePanel.MyRecordButtonPanel#makeRecordButton(mitll.langtest.client.exercise.ExerciseController, RecordButtonPanel)
+   * @param delay
+   * @param addKeyHandler
+   */
+  public RecordButton(int delay, boolean addKeyHandler) {
     this.autoStopDelay = delay;
-    keyHandler = addKeyHandler();
+    if (addKeyHandler) keyHandler = addKeyHandler();
   }
 
   /**
    * @see RecordButtonPanel#makeRecordButton(mitll.langtest.client.exercise.ExerciseController, RecordButtonPanel)
    * @param recordButton
    * @param delay
+   * @param recordImage1
+   * @param recordImage2
+   * @param addKeyHandler
    */
-  public RecordButton(FocusWidget recordButton, int delay) {
-    this(delay);
+  public RecordButton(FocusWidget recordButton, int delay, Image recordImage1, Image recordImage2, boolean addKeyHandler) {
+    this(delay, addKeyHandler);
     this.record = recordButton;
     setupRecordButton(recordButton);
+    this.recordImage1 = recordImage1;
+    this.recordImage2 = recordImage2;
   }
 
   protected void setupRecordButton(FocusWidget recordButton) {
@@ -53,7 +67,6 @@ public abstract class RecordButton {
     recordButton.addFocusHandler(new FocusHandler() {
       @Override
       public void onFocus(FocusEvent event) {
-        //System.out.println(new Date() + " : recordButton GOT   focus !----> ");
         hasFocus = true;
       }
     });
@@ -61,38 +74,36 @@ public abstract class RecordButton {
       @Override
       public void onBlur(BlurEvent event) {
         hasFocus = false;
-       // System.out.println(new Date() + " : recordButton LOST  focus !----> ");
       }
     });
-    recordButton.setTitle("Press Enter to record/stop recording");
+    recordButton.setTitle("Press the space bar to record/stop recording");
   }
 
   protected HandlerRegistration addKeyHandler() {
     return Event.addNativePreviewHandler(new
-                                                                              Event.NativePreviewHandler() {
+                                           Event.NativePreviewHandler() {
 
-                                                                                @Override
-                                                                                public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
-                                                                                  NativeEvent ne = event.getNativeEvent();
+                                             @Override
+                                             public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
+                                               NativeEvent ne = event.getNativeEvent();
 
-                                                                                  if (ne.getKeyCode() == KeyCodes.KEY_ENTER &&
-                                                                                    event.getTypeInt() == 512 &&
-                                                                                    "[object KeyboardEvent]".equals(ne.getString()) &&
-                                                                                    !hasFocus) {
-                                                                                    ne.preventDefault();
-
+                                               if (ne.getKeyCode() == SPACE_CHAR &&
+                                                 event.getTypeInt() == 512 &&
+                                                 "[object KeyboardEvent]".equals(ne.getString()) &&
+                                                 !hasFocus) {
+                                                 ne.preventDefault();
 /*                                                         System.out.println(new Date() + " : Click handler : Got " + event + " type int " +
                                                              event.getTypeInt() + " assoc " + event.getAssociatedType() +
                                                              " native " + event.getNativeEvent() + " source " + event.getSource());*/
-                                                                                    new Timer() {
-                                                                                      @Override
-                                                                                      public void run() {
-                                                                                        doClick();
-                                                                                      }
-                                                                                    }.schedule(10);
-                                                                                  }
-                                                                                }
-                                                                              });
+                                                 new Timer() {
+                                                   @Override
+                                                   public void run() {
+                                                     doClick();
+                                                   }
+                                                 }.schedule(10);
+                                               }
+                                             }
+                                           });
   }
 
   public void onUnload() {
@@ -136,8 +147,42 @@ public abstract class RecordButton {
   }
 
   protected abstract void startRecording();
-  protected abstract void showRecording();
-  protected abstract void showStopped();
+
+  protected void showRecording() {
+    if (recordImage1 != null) {
+      recordImage1.setVisible(true);
+      flipImage();
+    }
+  }
+
+  private boolean first = true;
+  private Timer t = null;
+
+  private void flipImage() {
+    t = new Timer() {
+      @Override
+      public void run() {
+        if (first) {
+          recordImage1.setVisible(false);
+          recordImage2.setVisible(true);
+        }
+        else {
+          recordImage1.setVisible(true);
+          recordImage2.setVisible(false);
+        }
+        first = !first;
+      }
+    };
+    t.scheduleRepeating(PERIOD_MILLIS);
+  }
+
+  protected void showStopped() {
+    if (recordImage1 != null) {
+      recordImage1.setVisible(false);
+      recordImage2.setVisible(false);
+      t.cancel();
+    }
+  }
 
   /**
    * Add a timer to automatically stop recording after 20 seconds.
@@ -148,8 +193,6 @@ public abstract class RecordButton {
       @Override
       public void run() {
         if (recording) {
-         // long now = System.currentTimeMillis();
-        //  System.out.println("stop schedule timer at " + now + " " + (now-then));
           stop();
         }
       }
@@ -159,7 +202,6 @@ public abstract class RecordButton {
     //System.out.println("start schedule timer at " + (then = System.currentTimeMillis()));
     recordTimer.schedule(autoStopDelay);
   }
-  //private long then = 0;
   private void cancelTimer() {
     if (recordTimer != null) {
       recordTimer.cancel();
