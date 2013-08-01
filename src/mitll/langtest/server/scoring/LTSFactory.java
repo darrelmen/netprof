d6924616352c2cb6ba4249dc66d95d96a703f7dc
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-01 17:07:38 -0400

    refactored ASRScoring to make it more readable
    Former-commit-id: 2c8ec065c03f9735ffe3485bc7ace97e6da7a69c [formerly 61ab46eacef0d93a64896ed2b68634c63595a8eb] [formerly 9328a5f4d306a6da9aafc6f2823a6a1fc7d5da5b]
    Former-commit-id: b492772ffa4078173a227cf9652d5f8a8b7be3e4 [formerly febd16af54a1329c142dbdd70850de6d3036b43d] [formerly 6c02d94eafd807cffc9a90a6d58ca7ef443c02c5]
    Former-commit-id: 2279d44b77f7f4bfbfd1f5db732fafc1c6cbcb8b
    Former-commit-id: d565bd15904d3a8f33a88e43aca7b19691ec1eac

---
+++ src/mitll/langtest/server/scoring/LTSFactory.java
@@ -0,0 +1,51 @@
+package mitll.langtest.server.scoring;
+
+import corpus.ArabicLTS;
+import corpus.DariLTS;
+import corpus.EnglishLTS;
+import corpus.FarsiLTS;
+import corpus.LTS;
+import corpus.LevantineLTS;
+import corpus.ModernStandardArabicLTS;
+import corpus.PashtoLTS;
+import org.apache.log4j.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 8/1/13
+ * Time: 4:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LTSFactory {
+  private static Logger logger = Logger.getLogger(LTSFactory.class);
+
+  public LTS getLTSClass(String language) {
+    LTS letterToSoundClass = language != null && language.equals("English") ? new EnglishLTS() : new ArabicLTS();
+
+    if (language == null) {
+      letterToSoundClass = new ArabicLTS();
+      logger.warn("using default arabic LTS -- this may not be what you want -- set the language property.");
+    } else if (language.equalsIgnoreCase("English")) {
+      letterToSoundClass = new EnglishLTS();
+    } else if (language.equalsIgnoreCase("Arabic")) {
+      letterToSoundClass = new ArabicLTS();
+    } else if (language.equalsIgnoreCase("Dari")) {
+      letterToSoundClass = new DariLTS();
+    } else if (language.equalsIgnoreCase("Pashto")) {
+      letterToSoundClass = new PashtoLTS();
+    } else if (language.equalsIgnoreCase("MSA")) {
+      letterToSoundClass = new ModernStandardArabicLTS();
+    } else if (language.equalsIgnoreCase("Levantine")) {
+      letterToSoundClass = new LevantineLTS();
+    } else if (language.equalsIgnoreCase("Farsi")) {
+      letterToSoundClass = new FarsiLTS();  // TODO is there a FarsiLTS class???
+    } else if (language.equalsIgnoreCase("Mandarin")) {
+      //ara logger.info("NOTE: there is no LTS for " + language + " : that's OK though.");
+    } else {
+      logger.warn("NOTE: we have no LTS for '" + language +"', using " + letterToSoundClass.getClass());
+    }
+    return letterToSoundClass;
+  }
+
+}
