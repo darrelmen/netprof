8dfda176876751a8d6f74956f9e7a726f360a632
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-05 22:23:16 -0400

    shows leaderboard chart after each timed avp session
    Former-commit-id: d0275d2d9bd53f0dda74dd677609dbf8442771db [formerly e296e98f684f5c69ca2524440926d4d12a0302da] [formerly 9581aba5e583a887f297a4343c600300143d8d6c]
    Former-commit-id: 10c571e002e1d1357388febc0dfa6db8376e4d9e [formerly 8a787b2b19e9a0a850ddff9d35144d975305b622] [formerly f74fad077d422812f19d0398e849b35a86a1562b]
    Former-commit-id: 5efa5783fa4b442973c1bea03adefa5e5eb9f76d
    Former-commit-id: 23a0d1275c32995481be9d91dc0a53311db595c3

--- src/mitll/langtest/client/bootstrap/BootstrapFlashcardExerciseList.java
+++ src/mitll/langtest/client/bootstrap/BootstrapFlashcardExerciseList.java
@@ -1,13 +1,21 @@
 package mitll.langtest.client.bootstrap;
 
+import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Column;
 import com.github.gwtbootstrap.client.ui.Container;
 import com.github.gwtbootstrap.client.ui.FluidRow;
 import com.github.gwtbootstrap.client.ui.Heading;
 import com.github.gwtbootstrap.client.ui.Image;
+import com.github.gwtbootstrap.client.ui.Modal;
+import com.github.gwtbootstrap.client.ui.ModalFooter;
 import com.github.gwtbootstrap.client.ui.ProgressBar;
 import com.github.gwtbootstrap.client.ui.base.ProgressBarBase;
+import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.github.gwtbootstrap.client.ui.event.HiddenEvent;
+import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
@@ -26,9 +34,18 @@ import mitll.langtest.client.exercise.SelectionState;
 import mitll.langtest.client.user.UserManager;
 import mitll.langtest.shared.ExerciseShell;
 import mitll.langtest.shared.FlashcardResponse;
+import mitll.langtest.shared.Leaderboard;
+import mitll.langtest.shared.ScoreInfo;
+import org.moxieapps.gwt.highcharts.client.Chart;
+import org.moxieapps.gwt.highcharts.client.PlotBand;
+import org.moxieapps.gwt.highcharts.client.Series;
+import org.moxieapps.gwt.highcharts.client.labels.PlotBandLabel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -39,6 +56,7 @@ import java.util.List;
  */
 public class BootstrapFlashcardExerciseList implements ListInterface {
   private static final int SIZE = 12;
+  public static final float HALF = (1f / 2f);
   private final Column exercisePanelColumn;
   private ExercisePanelFactory factory;
   private LangTestDatabaseAsync service;
@@ -56,6 +74,7 @@ public class BootstrapFlashcardExerciseList implements ListInterface {
   private final int gameTimeSeconds;
   private Panel bottomRow = new FlowPanel();
   private boolean isTimedGame = false;
+  private Map<String,Collection<String>> currentSelection;
 
   /**
    *
@@ -101,6 +120,8 @@ public class BootstrapFlashcardExerciseList implements ListInterface {
     pair.add(correct);
 
     this.user = user;
+
+    //showDialog();
   }
 
   @Override
@@ -129,7 +150,8 @@ public class BootstrapFlashcardExerciseList implements ListInterface {
       }
       else {
         System.out.println("Getting next for " +userID + " selection state : " +selectionState);
-        service.getNextExercise(userID, selectionState.typeToSection, new FlashcardResponseAsyncCallback());
+        this.currentSelection = selectionState.typeToSection;
+        service.getNextExercise(userID, currentSelection, new FlashcardResponseAsyncCallback());
       }
     }
   }
@@ -149,14 +171,127 @@ public class BootstrapFlashcardExerciseList implements ListInterface {
     timer.scheduleRepeating(intervalMillis);
   }
 
+  private void timesUp(final long userID) {
+     service.postTimesUp(userID, gameTimeSeconds*1000, currentSelection, new AsyncCallback<Leaderboard>() {
+       @Override
+       public void onFailure(Throwable caught) {
+         //To change body of implemented methods use File | Settings | File Templates.
+       }
+
+       @Override
+       public void onSuccess(Leaderboard result) {
+         List<ScoreInfo> scores = result.getScores(currentSelection);
+         showDialog(scores, userID);
+       }
+     });
+  }
+
+  private void showDialog(List<ScoreInfo> scores, final long userID) {
+    int pbCorrect = 0;
+    int top = 0;
+    float total = 0;
+    List<Float> yValuesForUser = new ArrayList<Float>();
+    for (ScoreInfo score : scores) {
+      if (score.userid == userID) {
+        if (score.correct > pbCorrect) pbCorrect = score.correct;
+        yValuesForUser.add((float)score.correct);
+        System.out.println("got " +score);
+      }
+      if (score.correct > top) top = score.correct;
+      total += score.correct;
+    }
+    float avg = total/(float)scores.size();
+
+    final Modal modal = new Modal();
+    modal.setAnimation(false);
+    modal.setCloseVisible(true);
+ //   modal.set(true);
+    modal.setWidth("720px");
+  //  modal.setTitle("Leaderboard");
+    Chart chart = new Chart()
+      .setType(Series.Type.SPLINE)
+      .setChartTitleText("Leaderboard")
+      .setChartSubtitleText(currentSelection.toString())
+      .setMarginRight(10);
+
+    // Number[] yValues = {163, 203, 276, 408, 547, 729, 628};
+    Float[] yValues = yValuesForUser.toArray(new Float[0]);
+
+    Series series = chart.createSeries()
+      .setName("Correct in " + gameTimeSeconds + " seconds")
+      .setPoints(yValues);
+    chart.addSeries(series);
+
+    float from = under(pbCorrect);
+    float to   = over(pbCorrect);
+    PlotBand correctBand = chart.getYAxis().createPlotBand()
+      .setColor("#f18d24")
+      .setFrom(from)
+      .setTo(to);
+
+    correctBand.setLabel(new PlotBandLabel().setAlign(PlotBandLabel.Align.LEFT).setText("Personal Best"));
+
+    PlotBand topScore = chart.getYAxis().createPlotBand()
+      .setColor("#46bf00")
+      .setFrom(under(top))
+      .setTo(over(top));
+
+    topScore.setLabel(new PlotBandLabel().setAlign(PlotBandLabel.Align.LEFT).setText("Top Score"));
+
+    PlotBand avgScore = chart.getYAxis().createPlotBand()
+      .setColor("#2031ff")
+      .setFrom(under(avg))
+      .setTo(over(avg));
+
+    avgScore.setLabel(new PlotBandLabel().setAlign(PlotBandLabel.Align.LEFT).setText("Course Average"));
+
+    chart.getYAxis().setPlotBands(
+      avgScore,
+      correctBand,
+      topScore
+      );
+
+    chart.getYAxis().setAxisTitleText("# Correct");
+    chart.getXAxis().setAllowDecimals(false);
+    chart.getYAxis().setAllowDecimals(true);
+
+    modal.setMaxHeigth("750px");
+    modal.add(chart);
+    Button widgets = new Button();
+    widgets.setType(ButtonType.PRIMARY);
+    widgets.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        modal.hide();
+      }
+    });
+    modal.add(widgets);//new ModalFooter(widgets));
+    modal.show();
+
+    modal.addHiddenHandler(new HiddenHandler() {
+      @Override
+      public void onHidden(HiddenEvent hiddenEvent) {
+        getOutOfTimeDialog(userID);
+      }
+    });
+  }
+
+  private float over(float pbCorrect) {
+    return pbCorrect + HALF;
+  }
+
+  private float under(float pbCorrect) {
+    return pbCorrect - HALF;
+  }
+
   private void getOutOfTimeDialog(final long userID) {
     List<String> msgs = new ArrayList<String>();
     msgs.add("You got "+lastCorrect +" correct!");
 
-    if (prevCorrect != -1 && lastCorrect > prevCorrect) {
+/*    if (prevCorrect != -1 && lastCorrect > prevCorrect) {
       msgs.add("Even better than last time!");
       msgs.add("Before you had " + prevCorrect + " correct.");
-    }
+    }*/
     msgs.add("Would you like to try again?");
 
     String title = lastCorrect == 0 ? "Try again?" : lastCorrect < 5 ? "Good job" : "Congratulations!";
@@ -325,8 +460,8 @@ public class BootstrapFlashcardExerciseList implements ListInterface {
           timerRunning = false;
           //audioHelper.playCorrect(3);
 
-          getOutOfTimeDialog(userID);
-
+          //getOutOfTimeDialog(userID);
+          timesUp(userID);
         } else {
           currentSteps--;
           boolean lastTenSeconds = currentSteps == 0 || numSteps / currentSteps > 5;
