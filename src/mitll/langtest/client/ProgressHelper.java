15255c7c70074cedd99a6337ed7669252999ed96
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-06 16:16:19 -0400

    refactoring to simplify LangTest
    Former-commit-id: 7d7a516f650d79ccb598efabb1ae27061ea0b442 [formerly 990bb25684d999782e187f43584e2fb37b57d4c7] [formerly de7a89b2255c0f49da8572030bcf889006ef39b9]
    Former-commit-id: 2a15d77916da9899a869a19e4ce03d7e50f3f7cb [formerly 5f39ba24b5ccc2974ff92fb2c4914a00038e5a4a] [formerly 81e4aa16edc28503614871bc71a8e3dfbc4efcd0]
    Former-commit-id: 84188b97fc0973d33835dd3eab241553fe0323e4
    Former-commit-id: cd6906b6a68c96e1f05f1fdde087f5391f367803

---
+++ src/mitll/langtest/client/ProgressHelper.java
@@ -0,0 +1,39 @@
+package mitll.langtest.client;
+
+import com.github.gwtbootstrap.client.ui.ProgressBar;
+import com.github.gwtbootstrap.client.ui.base.ProgressBarBase;
+import mitll.langtest.client.exercise.ListInterface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 8/6/13
+ * Time: 12:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProgressHelper {
+  private ProgressBar progressBar;
+  public ProgressHelper() {
+    progressBar = makeProgressBar();
+  }
+
+  private ProgressBar makeProgressBar() {
+    ProgressBar progressBar = new ProgressBar(ProgressBarBase.Style.DEFAULT);
+    progressBar.setWidth("70%");
+    progressBar.setPercent(100);
+    progressBar.setText("No items completed.");
+    progressBar.setColor(ProgressBarBase.Color.DEFAULT);
+    progressBar.addStyleName("leftFifteenPercentMargin");
+    progressBar.addStyleName("topMargin");
+    return progressBar;
+  }
+
+  public void showAdvance(ListInterface exerciseList) {
+    getProgressBar().setPercent(100 - exerciseList.getPercentComplete());
+    getProgressBar().setText((exerciseList.getComplete() + 1) + " complete.");
+  }
+
+  public ProgressBar getProgressBar() {
+    return progressBar;
+  }
+}
