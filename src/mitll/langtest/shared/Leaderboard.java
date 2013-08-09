870b00410e082392e8f4b8a14f2dcd2186fe86bf
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-09 18:35:11 -0400

    got handshake between giver and receiver mostly working
    Former-commit-id: 78f44c0d2f3a386ca6b889ff4e19da65c65e8f58 [formerly 40c08c071dd2ea6d64281420f4eb16c05cbb1991 [formerly a2e4b2cf56fffc472a391ba9fdee73cfb25f55ec]] [formerly 40c08c071dd2ea6d64281420f4eb16c05cbb1991 [formerly a2e4b2cf56fffc472a391ba9fdee73cfb25f55ec] [formerly 40c08c071dd2ea6d64281420f4eb16c05cbb1991 [formerly a2e4b2cf56fffc472a391ba9fdee73cfb25f55ec] [formerly bbd83012243e7e70f0317ba01d2157eae5db71f4]]]
    Former-commit-id: 724d938fe63727f57a29dc7527f4233f4193c0e7 [formerly c4abbae71f1939a9ca5301561d9729fc8998daa3] [formerly 724d938fe63727f57a29dc7527f4233f4193c0e7 [formerly c4abbae71f1939a9ca5301561d9729fc8998daa3] [formerly 6c34c875ff3b4180dc4db166cc15688848c8608d]] [formerly d635c105c0b92dad60ad0537d3f0f9d8dd3e0166 [formerly c6f846a33b239702eac459d7e69e8327522c34ef] [formerly d635c105c0b92dad60ad0537d3f0f9d8dd3e0166 [formerly c6f846a33b239702eac459d7e69e8327522c34ef] [formerly 5b09fb01b22085d5a35a2dc61a24852a4e4a381c]]]
    Former-commit-id: d635c105c0b92dad60ad0537d3f0f9d8dd3e0166 [formerly c6f846a33b239702eac459d7e69e8327522c34ef]
    Former-commit-id: bc1ccd6e4a04d1cad069dfba4b7c096b7e362a71 [formerly d101c79921ff0187c12689a54135c7c11da6052e] [formerly 0ed0736ba1a8df30e81a3586f7ed5c1086e284a6]
    Former-commit-id: b0026ae310ef42239e99e947f9662684798fc0f1
    Former-commit-id: 9b04d62cdb78ec68163968bec4ca4e9cb5981adc

--- src/mitll/langtest/shared/Leaderboard.java
+++ src/mitll/langtest/shared/Leaderboard.java
@@ -4,7 +4,6 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
