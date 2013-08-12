1fa985beeee9c353f6cf97cb7cd6db10084238e9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-12 19:14:31 -0400

    added example sentences, giver side (with choice of sentences)
    Former-commit-id: e878ba2d1bd7a9bbdf7553dc44b0440374d6cede [formerly a54750989eb2249e476326211e8ddbec50e036b8] [formerly 23b17c3dacd91544ab1ad646f03e622292047dc0 [formerly e2e952a53f96b1768b17c108fec913c2c43dd9a0]]
    Former-commit-id: 551e22ea005df4760a172892407b5b674175d844 [formerly e13677d0df218ad0cb22a76f00d1136d469f9208]
    Former-commit-id: a9ac073c4414c175f27854b5b2e169ffa0b8d653

--- src/mitll/langtest/client/taboo/Taboo.java
+++ src/mitll/langtest/client/taboo/Taboo.java
@@ -1,4 +1,4 @@
-package mitll.langtest.client;
+package mitll.langtest.client.taboo;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.ControlGroup;
@@ -12,6 +12,8 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import mitll.langtest.client.LangTest;
+import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.user.UserManager;
 import mitll.langtest.shared.TabooState;
 
@@ -25,7 +27,7 @@ import mitll.langtest.shared.TabooState;
 public class Taboo {
   private final UserManager userManager;
   private final LangTestDatabaseAsync service;
-  LangTest langTest;
+  private LangTest langTest;
 
   private static final int FIRST_POLL_PERIOD_MILLIS = 1000 * 3; // ten minutes
   private static final int INACTIVE_PERIOD_MILLIS = 1000 * 15; // ten minutes
@@ -56,22 +58,25 @@ public class Taboo {
     cancelTimer();
     service.anyUsersAvailable(fuserid, new AsyncCallback<TabooState>() {
       @Override
-      public void onFailure(Throwable caught) {
-        Window.alert("Couldn't contact server.");
-      }
+      public void onFailure(Throwable caught) { Window.alert("Couldn't contact server."); }
 
       @Override
       public void onSuccess(TabooState result) {
         System.out.println("me : " + fuserid + " checking, anyUsersAvailable : " + result);
-        if (result.joinedPair) {
-          Window.alert("Game on! You are the receiver or giver."); // TODO : check which you are
+        if (result.isJoinedPair()) {
+          if (result.isGiver()) {
+            doModal("You are the giver", "Now choose the next sentence your partner will see to guess the vocabulary word.", true);
+          }
+          else {
+            doModal("You are the receiver","Now choose the word that best fills in the blank in the sentence.",false);
+          }
 
           // TODO : somehow we begin receiving items to do and to guess at.
           // do the right factory, make a panel that can receive stimulus and show choices
           // panel posts results back, giver notices results
 
         }
-        else if (result.anyAvailable) {
+        else if (result.isAnyAvailable()) {
           doTabooModal(fuserid);
         }
         else {
@@ -81,6 +86,34 @@ public class Taboo {
         }
       }
     });
+  }
+
+  private void doModal(String title, String message, final boolean isGiver) {
+    final Modal modal = new Modal(true);
+  //  String title = "You are the receiver";
+    modal.setTitle(title);
+    Heading w = new Heading(4);
+    //String message = "Now choose the word that best fills in the blank in the sentence.";
+    w.setText(message);
+    modal.add(w);
+
+    final Button begin = new Button("Begin Game");
+    begin.setType(ButtonType.PRIMARY);
+    begin.setEnabled(true);
+
+    begin.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        modal.hide();
+        if (isGiver) {
+          Window.alert("Game on! You are the giver.");
+        } else {
+          Window.alert("You are the receiver.");
+        }
+        // TODO : set the factory for the receiver...
+      }
+    });
+    modal.show();
   }
 
 
@@ -111,7 +144,7 @@ public class Taboo {
     // recordingStyle.add(new ControlLabel("<b>Audio Recording Style</b>"));
     Controls controls = new Controls();
 
-    final RadioButton give = new RadioButton("Giver","Give");
+    final RadioButton give    = new RadioButton("Giver","Give");
     final RadioButton receive = new RadioButton("Giver","Receive");
     controls.add(give);
     controls.add(receive);
@@ -127,23 +160,20 @@ public class Taboo {
       public void onClick(ClickEvent event) {
         Boolean isGiver = give.getValue();
         if (!isGiver && !receive.getValue()) {
-          Window.alert("please choose to give or receive.");
-        }
-        else {
+          Window.alert("please choose to give or receive.");  // TODO better highlighting
+        } else {
           askGiverReceiver.hide();
 
-            // TODO : tell server we're playing... it will tell the partner to either be the giver or the receiver
-            service.registerPair(userID, isGiver, new AsyncCallback<Void>() {
-              @Override
-              public void onFailure(Throwable caught) {
-               Window.alert("Couldn't contact server.");
-              }
+          // TODO : tell server we're playing... it will tell the partner to either be the giver or the receiver
+          service.registerPair(userID, isGiver, new AsyncCallback<Void>() {
+            @Override
+            public void onFailure(Throwable caught) { Window.alert("Couldn't contact server."); }
 
-              @Override
-              public void onSuccess(Void result) {
-                langTest.setFactory(userID);
-              }
-            });
+            @Override
+            public void onSuccess(Void result) {  // TODO : make the right exercise panel factory for the role (giver chooses and gives...)
+              langTest.setFactory(userID);
+            }
+          });
 
           // if giver, then need to see wordlist, select next item to give to receiver
           // after giving, poll for answer submission by receiver - correct, move on to next item
