a3bfad8d41b8c61bd9539362fbdcdc62e0e683be
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-13 11:25:34 -0400

    steps towards collaborative taboo
    Former-commit-id: dd3d696e2c636e66f36ecc503678af33b56f64d2 [formerly 36f0f04e83c4a92946dc9a4418c7d4f5e9c0e8b4] [formerly 64e29b9d648958142bf7a723c928458b7bc43602 [formerly 13100b2fbe2509677379ab2a8ef56b13a91c4115]] [formerly 0c588996e46f5f1a3f06e5fd18a20ccbaa6ad71f [formerly 9c58ed6d6526beef500609076cdc47dc7595ff94] [formerly 7561989cc9af0b2ef39a3e6520d3283bf285d636 [formerly e013ecacb88ff75dc01edb1a7e07c01ecc25584e]]]
    Former-commit-id: 9adf16ebfd676eebe1a848393e13cc729fa96c38 [formerly 83923a32be15887d30c366e1c4656bb38e9468d5] [formerly b5f58492e50f76ffd301bd8256d6b41f54b2995c [formerly 26d3961775ad9e1b063cdcc4e95f555a4a0ba55d]] [formerly 120ac4c85572fac408c5cc25feea01e800b93840 [formerly fdab0745978bce44887a574bb3cd403a21c6b37f] [formerly ffd5fdb733545f20a5ad3ec7732f8afcd812502d [formerly 354b58c6708c4223eb7c7fe2f52532af155eee8f]]]
    Former-commit-id: b93300488fa8482f3508784e36aef1b1fd1e8d56 [formerly 549cedd8a6809fe49c6e483224171841223139c4] [formerly 02b0019b93f7909174dcf23d470e108c5594eb43 [formerly a050f8ea4c48e6244b4d380bed0d1174d4d7f45e]]
    Former-commit-id: ec3ab67d2d279e43675a9dd9b2a5dbdd54168c56 [formerly dc9da52696a13d0df7009b3328752d9ad8ca2509]
    Former-commit-id: ebec8f25f84368dd893fe9f475487fa9ae164a3d

---
+++ src/mitll/langtest/server/taboo/Tatoeba.java
@@ -0,0 +1,13 @@
+package mitll.langtest.server.taboo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 8/12/13
+ * Time: 5:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tatoeba {
+
+//  public static void main
+}
