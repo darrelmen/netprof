ea20f1e318a95c1941564a81c74f2edcbfe289e9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-13 19:01:59 -0400

    got rudimentary game play working
    Former-commit-id: d31e4770d76b4a66d512ae123e59f19b9824ada8 [formerly 75c7716e7ae9b7ab93850f9fa0c4bf18c9a57f31] [formerly 75c7716e7ae9b7ab93850f9fa0c4bf18c9a57f31 [formerly 75c7716e7ae9b7ab93850f9fa0c4bf18c9a57f31 [formerly 85c4644a55c40420db5b0d5f52e544a2278c0ae9]]]
    Former-commit-id: cd7ba0039fbcbe36f7ba0e6b92601f5d86d391eb [formerly cd7ba0039fbcbe36f7ba0e6b92601f5d86d391eb [formerly 9af5d24b7f44e616208ec430d81be73a0038a780]] [formerly eba1d4a37fb919a90f3b4421d15f395ba4512014 [formerly eba1d4a37fb919a90f3b4421d15f395ba4512014 [formerly 278e3795a75392826e03b28a2e91e3c34039ef4e]]]
    Former-commit-id: eba1d4a37fb919a90f3b4421d15f395ba4512014
    Former-commit-id: af1e89cf0354e23ffd7495c6c3adc0323874ba42 [formerly f34bbc4fb94a82ca14028427fc2f36780db0bd53]
    Former-commit-id: f386af051ebafe4fbbf067044fa20b557949971c

---
+++ src/mitll/langtest/shared/StimulusAnswerPair.java
@@ -0,0 +1,22 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+* Created with IntelliJ IDEA.
+* User: GO22670
+* Date: 8/13/13
+* Time: 2:37 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class StimulusAnswerPair implements IsSerializable {
+  public String stimulus;
+  public String answer;
+
+  public StimulusAnswerPair() {}
+
+  public StimulusAnswerPair(String stimulus, String answer) {
+    this.stimulus = stimulus;
+    this.answer = answer;
+  }
+}
