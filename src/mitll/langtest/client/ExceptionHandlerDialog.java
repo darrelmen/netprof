c97a4f30ef6115e9b8a8ed832234aae0036908c8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-13 13:55:42 -0400

    added better logging of browser exceptions
    Former-commit-id: f736470a0a926ef993ca89de924176758b19c90a

--- src/mitll/langtest/client/ExceptionHandlerDialog.java
+++ src/mitll/langtest/client/ExceptionHandlerDialog.java
@@ -11,11 +11,29 @@ import com.google.gwt.user.client.ui.DialogBox;
  * To change this template use File | Settings | File Templates.
  */
 public class ExceptionHandlerDialog {
-  public ExceptionHandlerDialog(Throwable throwable) {
-    this(null, throwable);
+  public ExceptionHandlerDialog(Throwable throwable) { showException(null, throwable);  }
+
+  public ExceptionHandlerDialog(){}
+
+  private void showException(BrowserCheck browserCheck, Throwable throwable) {
+    String text = getExceptionAsString(throwable);
+    showExceptionInDialog(browserCheck, text);
   }
 
-  public ExceptionHandlerDialog(BrowserCheck browserCheck, Throwable throwable) {
+  public void showExceptionInDialog(BrowserCheck browserCheck, String text) {
+    if (browserCheck != null && browserCheck.isIE7() && text.contains("Unknown runtime error")) { // hack for IE 7
+      return;
+    }
+    DialogBox dialogBox = new DialogBox(true, false);
+    dialogBox.setWidth("80%");
+    DOM.setStyleAttribute(dialogBox.getElement(), "backgroundColor", "#ABCDEF");
+    System.err.print(text);
+    text = text.replaceAll(" ", "&nbsp;");
+    dialogBox.setHTML("<pre>" + text + "</pre>");
+    dialogBox.center();
+  }
+
+  public String getExceptionAsString(Throwable throwable) {
     String text = "Uncaught exception: ";
     while (throwable != null) {
       StackTraceElement[] stackTraceElements = throwable.getStackTrace();
@@ -28,15 +46,6 @@ public class ExceptionHandlerDialog {
         text += "Caused by: ";
       }
     }
-    if (browserCheck != null && browserCheck.isIE7() && text.contains("Unknown runtime error")) { // hack for IE 7
-      return;
-    }
-    DialogBox dialogBox = new DialogBox(true, false);
-    dialogBox.setWidth("80%");
-    DOM.setStyleAttribute(dialogBox.getElement(), "backgroundColor", "#ABCDEF");
-    System.err.print(text);
-    text = text.replaceAll(" ", "&nbsp;");
-    dialogBox.setHTML("<pre>" + text + "</pre>");
-    dialogBox.center();
+    return text;
   }
 }
