649998d37c5e4eb591b940b1b8726813769a0361
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-14 17:38:10 -0400

    fixed issues with giver panel not appearing, added audio feedback
    Former-commit-id: 4706aed9e80a3041478888b1fb935986790d270e [formerly f1527ab1ef21a5f5cde16779ffcf2e25cf29d2cb] [formerly f5d1726c6389c904e63d006d6fe054c0a81a7873 [formerly 5ea071ebc8c3fab7546ecb8b40cd293a9b93fde0]] [formerly 225565b74ee11e8ce087070f594fd243875c3831 [formerly f5b02e8d9dc2e86b15bf40d36be9501559a553e9] [formerly 3a89595f64df9f6197321f2117b49c71a3ef18d1 [formerly 4ed71ff497835606854055c991c538aa16ac5e68]]]
    Former-commit-id: 80bc8011caac304f1d200c4fa2ed46f6597846da [formerly 713dcecb40f45bd25bd69bd39a2d50f6c845efb2] [formerly d508d0db717f0965e86f6a29d723f76000068f0c [formerly 3ace1a35a5dd561caf135ea4fe5c373d15cd3d2d]]
    Former-commit-id: b2cdb682685f01b311c65f92b297fd5c4f4891e3 [formerly 8c4673a622a7c5aa7a9ce50a40a767c4e9dd4e9e]
    Former-commit-id: 6d00444414a6884f15bbe21eabb924b6dc490376

---
+++ src/mitll/langtest/client/bootstrap/SoundFeedback.java
@@ -0,0 +1,93 @@
+package mitll.langtest.client.bootstrap;
+
+import com.google.gwt.user.client.ui.HTML;
+import mitll.langtest.client.sound.AudioControl;
+import mitll.langtest.client.sound.Sound;
+import mitll.langtest.client.sound.SoundManagerAPI;
+
+import java.util.Date;
+
+/**
+ * Does sound feedback - correct/incorrect to user.
+ * User: GO22670
+ * Date: 8/14/13
+ * Time: 3:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SoundFeedback {
+  private final HTML warnNoFlash;
+  private Sound currentSound = null;
+  private SoundManagerAPI soundManager;
+
+  public SoundFeedback(SoundManagerAPI soundManager,HTML warnNoFlash) {
+    this.soundManager = soundManager;
+    this.warnNoFlash = warnNoFlash;
+  }
+  public void playCorrect() {
+    startSong("langtest/sounds/correct4.mp3", new EndListener() {
+      @Override
+      public void songEnded() {}
+    });
+  }
+
+  public void playIncorrect() {
+    startSong("langtest/sounds/incorrect1.mp3", new EndListener() {
+      @Override
+      public void songEnded() {}
+    });
+  }
+
+
+  private void startSong(String path, EndListener endListener) {
+    // System.out.println("PlayAudioPanel : start song : " + path);
+    if (soundManager.isReady()) {
+      //System.out.println(new Date() + " Sound manager is ready.");
+      if (soundManager.isOK()) {
+        destroySound();
+        createSound(path, endListener);
+      } else {
+        System.out.println(new Date() + " Sound manager is not OK!.");
+        warnNoFlash.setVisible(true);
+      }
+    }
+  }
+
+  /**
+   * @param song
+   * @see #startSong
+   */
+  public void createSound(final String song, final EndListener endListener) {
+    currentSound = new Sound(new AudioControl() {
+      @Override
+      public void reinitialize() {
+        System.out.println("song " + song + " ended---");
+        destroySound();
+        endListener.songEnded();
+      }
+
+      @Override
+      public void songFirstLoaded(double durationEstimate) {
+      }
+
+      @Override
+      public void songLoaded(double duration) {
+        soundManager.play(currentSound);
+      }
+
+      @Override
+      public void update(double position) {
+      }
+    });
+    soundManager.createSound(currentSound, song, song);
+  }
+
+  private void destroySound() {
+    if (currentSound != null) {
+      this.soundManager.destroySound(currentSound);
+    }
+  }
+
+  public static interface EndListener {
+    void songEnded();
+  }
+}
