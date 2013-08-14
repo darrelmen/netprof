1b59a84c20d734e43964d2ea9f62ea093f7c24b8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-13 20:08:15 -0400

    better display of receiver state
    Former-commit-id: b87172efee32e9bf9ff14620c0dd7c3c34b197d0 [formerly 7f9212c6b7ec9041bf87a1084a84c57422bb154c]
    Former-commit-id: 6ce3647a83c7b13bf9e9414f94c4c5cf9a88bd2d [formerly de25702e3cb58697e37ce742b425981c1d0b5d84]
    Former-commit-id: 7623ca59b3c47f9250d1d93cb015d2d7abbf3375

---
+++ src/mitll/langtest/client/taboo/TabooExerciseList.java
@@ -0,0 +1,67 @@
+package mitll.langtest.client.taboo;
+
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.user.client.ui.Panel;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.bootstrap.FlexSectionExerciseList;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.ExercisePanelFactory;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.ExerciseShell;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 8/13/13
+ * Time: 7:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TabooExerciseList extends FlexSectionExerciseList {
+  private boolean isGiver = true;
+
+  public TabooExerciseList(FluidRow secondRow, Panel currentExerciseVPanel, LangTestDatabaseAsync service,
+                           UserFeedback feedback, boolean showTurkToken, boolean showInOrder, boolean showListBox,
+                           ExerciseController controller) {
+    super(secondRow, currentExerciseVPanel, service, feedback, showTurkToken, showInOrder, showListBox, controller);
+   // makeExercisePanel(null);
+  }
+
+  @Override
+  public void setFactory(ExercisePanelFactory factory, UserManager user, int expectedGrades) {
+    super.setFactory(factory, user, expectedGrades);
+    makeExercisePanel(null);
+  }
+
+  @Override
+  protected void getTypeOrder(long userID, FluidContainer container) {
+    if (isGiver) {
+      super.getTypeOrder(userID, container);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+    else {
+      addBottomText(container);
+    }
+  }
+
+  public void setGiver(boolean isGiver) { this.isGiver = isGiver; }
+
+  protected void rememberExercises(List<ExerciseShell> result) {
+    System.out.println("remembering " + result.size() + " exercises");
+    currentExercises = result; // remember current exercises
+    idToExercise = new HashMap<String, ExerciseShell>();
+    clear();
+    for (final ExerciseShell es : result) {
+      idToExercise.put(es.getID(),es);
+      if (isGiver) addExerciseToList(es);
+    }
+    flush();
+  }
+
+/*  protected void addExerciseToList(final ExerciseShell e) {
+   // final HTML w = makeAndAddExerciseEntry(e);
+  }*/
+}
