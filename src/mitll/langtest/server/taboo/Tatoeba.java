0949e8226dff58ed4db1e723071b492af9abc867
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-14 17:38:10 -0400

    fixed issues with giver panel not appearing, added audio feedback
    Former-commit-id: 69026b80ea38e8cffb2c2cf9c42d8a17b00c5a96 [formerly 4444bcc460f86813b485697a2fa704267ee60d84] [formerly 69026b80ea38e8cffb2c2cf9c42d8a17b00c5a96 [formerly 4444bcc460f86813b485697a2fa704267ee60d84] [formerly 69026b80ea38e8cffb2c2cf9c42d8a17b00c5a96 [formerly 4444bcc460f86813b485697a2fa704267ee60d84] [formerly e340d5d00aa7d47b29c33f97ef6cdf5ef68aa14d]]]
    Former-commit-id: 1ed929cc786d68e0216d0e44173d798e2e249276 [formerly ea22b31658fffeaf974bc7083480dc3d1117f53b] [formerly 1ed929cc786d68e0216d0e44173d798e2e249276 [formerly ea22b31658fffeaf974bc7083480dc3d1117f53b] [formerly 3ab2384171f48479f4bd26bd55586a4947d0917e]] [formerly b9d188efaac066db44f4e00ac5fb2d5de4296b00 [formerly 62a526aacc13bb98fa03e626cb47526b86241c45] [formerly b9d188efaac066db44f4e00ac5fb2d5de4296b00 [formerly 62a526aacc13bb98fa03e626cb47526b86241c45] [formerly 855820dd568527fbf1d7b528000a44120b4f8210]]]
    Former-commit-id: b9d188efaac066db44f4e00ac5fb2d5de4296b00 [formerly 62a526aacc13bb98fa03e626cb47526b86241c45]
    Former-commit-id: ce08a814b4bedf53f1ce7e4972c2b209e7032b55 [formerly ac309398fce303aac18b46f391a09c1d99f9007d] [formerly ffae72e734e4eba477247051115f9706d482b8d1]
    Former-commit-id: 6d933bceed89871c10a674c5f6612fd9ba720b6d
    Former-commit-id: bc14477971b785a3a9271cab4d34b42fe9b6f0e1

--- src/mitll/langtest/server/taboo/Tatoeba.java
+++ src/mitll/langtest/server/taboo/Tatoeba.java
@@ -1,5 +1,15 @@
 package mitll.langtest.server.taboo;
 
+import mitll.langtest.server.database.FileExerciseDAO;
+import mitll.langtest.shared.Exercise;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: GO22670
@@ -8,6 +18,31 @@ package mitll.langtest.server.taboo;
  * To change this template use File | Settings | File Templates.
  */
 public class Tatoeba {
+/*
+  public static void main(String [] arg) {
+    try {
+      String name = "C:\\Users\\go22670\\DLITest\\bootstrap\\netPron2\\spaFromTatoeba.txt";
+      String name2 = "C:\\Users\\go22670\\DLITest\\bootstrap\\netPron2\\war\\config\\taboo\\spa.txt";
+      File fname = new File(name);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fname), FileExerciseDAO.ENCODING));
+      String line2;
+      int c = 0;
+      while ((line2 = reader.readLine()) != null) {
+        c++;
+        String[] split = line2.split("\\t");
+        List<Exercise> exercises1 = refToEx.get(split[0].trim());
+        if (exercises1 != null) {
+          for (Exercise e : exercises1) {
+            if (e.getSynonymSentences().isEmpty()) logger.debug("adding to " +e);
+            e.getSynonymSentences().add(split[1]);
+          }
+        }
+      }
+      logger.debug("read " + c + " examples");
 
-//  public static void main
+      reader.close();
+    } catch (IOException e) {
+      logger.error("Got " + e, e);
+    }
+  }*/
 }
