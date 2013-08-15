11ebce3bb16c81b178025607ba9583cf574b5141
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-15 17:19:39 -0400

    initial single player robot added
    Former-commit-id: d3a2d86e14cb210925205e8f7d32e1bc798b3550 [formerly 0f0f6b3ae2bd90bc3974e2dfa967c8c1bd5e6a11] [formerly 522b44c1baef0f1d9fcf172ea1a42dcf4e6921a1 [formerly b2ce76c9485bc332d25c53212f55843a141fb5da]] [formerly 4d03d8f53272e512ad01e8db5f161dbd3f7fc8d8 [formerly a04a66dbcbe50d392cdcbac2e336ff79c26ba56a] [formerly 69e8e3800d6b6fee539b51f9c23bf7d7fa6605ea [formerly 4c3e6ec4c04422e0d72da9d4c5a3b27f2e2b1883]]]
    Former-commit-id: a2cca620c54bcaacaa3573c0c681ea7b8f33a3cc [formerly c9850eb631fd0e19d665f90945e2a4994a31054b] [formerly 557a43958d92dce2321259bb353bd4783529bffe [formerly eb9d41cb325e2e017b9d4cac959c12a288571081]] [formerly e7629a4ac67d044f8e4ad1e02d22a0c857c1a470 [formerly 1bd8ec5ce5a682c89dda24b13bca190838d077f6] [formerly 7aaf59cbba861d1c6b6bf4f099eb2fdbfc98088b [formerly 7e67c9916bea45c6625c86e35d892cfa2ca181d7]]]
    Former-commit-id: 1e8cc0a0cf7fbc72b733c7c43f404dca311e9e79 [formerly 266c9ef3eb21059d79dbddec56cc14f3c7e661e0] [formerly 3738a0000afaf4047ab12aab98c8d1cf186a49c2 [formerly a429f9dcbadc26b63fa21153bdd149e49c635d97]]
    Former-commit-id: 1a18f0881f44536c484a46e379f9e38491771437 [formerly 413c0f9596804075d1d5437816ff990b3ab0148f]
    Former-commit-id: 61cc4446e1a20aaa3cb2f8f6a8c31d5b5a746f83

--- src/mitll/langtest/client/taboo/ReceiverExerciseFactory.java
+++ src/mitll/langtest/client/taboo/ReceiverExerciseFactory.java
@@ -19,7 +19,6 @@ import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DecoratedPopupPanel;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.PopupPanel;
@@ -34,8 +33,6 @@ import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.Exercise;
 import mitll.langtest.shared.StimulusAnswerPair;
 
-import java.util.Date;
-
 /**
  * Created with IntelliJ IDEA.
  * <p/>
@@ -48,15 +45,23 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class ReceiverExerciseFactory extends ExercisePanelFactory {
+  SinglePlayerRobot singlePlayerRobot;
+
   /**
+   *
    * @param service
    * @param userFeedback
    * @param controller
+   * @param singlePlayerRobot
    * @see mitll.langtest.client.LangTest#setFactory
    */
   public ReceiverExerciseFactory(final LangTestDatabaseAsync service, final UserFeedback userFeedback,
-                                 final ExerciseController controller) {
+                                 final ExerciseController controller, SinglePlayerRobot singlePlayerRobot) {
     super(service, userFeedback, controller);
+    if (singlePlayerRobot != null) {
+      singlePlayerRobot.doSinglePlayer();
+    }
+    this.singlePlayerRobot = singlePlayerRobot;
   }
 
   /**
@@ -66,29 +71,25 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
    */
   public Panel getExercisePanel(Exercise e) {
     System.out.println("getExercisePanel getting receiver panel ...");
-    return new ReceiverPanel(service,userFeedback,controller);
+    return new ReceiverPanel(service,controller);
   }
 
-  private static class ReceiverPanel extends FluidContainer {
+  private class ReceiverPanel extends FluidContainer {
     private static final String PLEASE_WAIT = "Please wait for giver to send next sentence.";
-    Heading prompt = new Heading(3, PLEASE_WAIT);
+    Heading prompt = new Heading(3, "Checking for game partners...");
     TextBox guessBox = new NoPasteTextBox();
     Heading stimulus;
     String answer;
-    final Button begin = new Button("Send Answer");
-    //Image correct = BootstrapExercisePanel.incorrectImage;
-    Image correctImage = new Image(UriUtils.fromSafeConstant(LangTest.LANGTEST_IMAGES + "checkmark48.png"));
+    final Button send = new Button("Send Answer");
+    Image correctImage   = new Image(UriUtils.fromSafeConstant(LangTest.LANGTEST_IMAGES + "checkmark48.png"));
     Image incorrectImage = new Image(UriUtils.fromSafeConstant(LangTest.LANGTEST_IMAGES + "redx48.png"));
 
-    public ReceiverPanel(final LangTestDatabaseAsync service, final UserFeedback userFeedback,
-                         final ExerciseController controller) {
-      final ReceiverPanel outer = addWidgets(service,/*userFeedback,*/controller);
-      checkForStimlus(service, controller, outer);
-
+    public ReceiverPanel(final LangTestDatabaseAsync service, final ExerciseController controller) {
+      final ReceiverPanel outer = addWidgets(service,controller);
+      checkForStimulus(service, controller, outer);
     }
 
-    private ReceiverPanel addWidgets(final LangTestDatabaseAsync service, //final UserFeedback userFeedback,
-                                     final ExerciseController controller) {
+    private ReceiverPanel addWidgets(final LangTestDatabaseAsync service, final ExerciseController controller) {
       final ReceiverPanel outer = this;
       Row w4 = new Row();
       w4.add(prompt);
@@ -103,14 +104,13 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       w3.add(stimulus);
       add(w3);
 
-
       Row w = new Row();
       w.add(guessBox);
       add(w);
 
-      begin.setType(ButtonType.PRIMARY);
-      begin.setEnabled(true);
-
+      send.setType(ButtonType.PRIMARY);
+      send.setEnabled(true);
+      send.setTitle("Press the enter key.");
 
       HTML warnNoFlash = new HTML(BootstrapExercisePanel.WARN_NO_FLASH);
       warnNoFlash.setVisible(false);
@@ -118,7 +118,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
 
       waitForNext();
 
-      begin.addClickHandler(new ClickHandler() {
+      send.addClickHandler(new ClickHandler() {
         @Override
         public void onClick(ClickEvent event) {
           sendAnswer(service, controller, outer, soundFeedback);
@@ -127,7 +127,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       });
 
       FluidContainer container = new FluidContainer();
-      container.add(begin);
+      container.add(send);
  //     container.add(correctImage);
   //    correctImage.setVisible(false);
     //  container.add(incorrectImage);
@@ -142,17 +142,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
     private void sendAnswer(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer, SoundFeedback soundFeedback) {
       boolean isCorrect = guessBox.getText().equalsIgnoreCase(answer);
       System.out.println("sending answer '" + guessBox.getText() + "' vs '" + answer+ "' which is " + isCorrect);
-      service.registerAnswer(controller.getUser(),stimulus.getText(),answer,isCorrect,new AsyncCallback<Void>() {
-        @Override
-        public void onFailure(Throwable caught) {
-          Window.alert("couldn't contact server");
-        }
-
-        @Override
-        public void onSuccess(Void result) {
-          checkForStimlus(service,controller,outer);
-        }
-      });
+      registerAnswer(service, controller, outer, isCorrect);
 
       if (isCorrect) {
         showPopup("Correct! Please wait for the next item.");
@@ -168,6 +158,14 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       waitForNext();
     }
 
+    private void registerAnswer(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer, boolean correct) {
+      if (singlePlayerRobot != null) {
+        singlePlayerRobot.registerAnswer(correct);
+      }
+
+      service.registerAnswer(controller.getUser(), stimulus.getText(), answer, correct, new RegisterAnswerResponseCallback(service, controller, outer));
+    }
+
     private void waitForNext() {
       prompt.setText(PLEASE_WAIT);
       stimulus.setVisible(false);
@@ -177,7 +175,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       guessBox.setText("");
    //   correctImage.setVisible(false);
   //    incorrectImage.setVisible(false);
-      begin.setVisible(false);
+      send.setVisible(false);
     }
 
     private void showPopup(String html) {
@@ -195,82 +193,138 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       t.schedule(2000);
     }
 
-
-    private void checkForStimlus(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer) {
-      service.checkForStimulus(controller.getUser(), new AsyncCallback<StimulusAnswerPair>() {
-        @Override
-        public void onFailure(Throwable caught) {
-          Window.alert("couldn't contact server.");
-        }
-
-        @Override
-        public void onSuccess(StimulusAnswerPair result) {
-          if (result != null) {
-            System.out.println("checkForStimulus.onSuccess : got  "+ result);
-
-            prompt.setText("Please fill in the blank.");
-            guessBox.setVisible(true);
-            begin.setVisible(true);
-            stimulus.setVisible(true);
-            stimulus.setText(result.stimulus);
-            outer.answer = result.answer;
-          //  System.out.println("checkForStimulus : answer '" + answer + "'");
-          }
-          else {
-            Timer t = new Timer() {
-              @Override
-              public void run() {
-                checkForStimlus(service,controller,outer);
-              }
-            };
-
-            // Schedule the timer to run once in 1 seconds.
-            t.schedule(1000);
-          }
-        }
-      });
+    /**
+     * @see ReceiverPanel#ReceiverPanel(mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.exercise.ExerciseController)
+     * @param service
+     * @param controller
+     * @param outer
+     */
+    private void checkForStimulus(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer) {
+      if (singlePlayerRobot != null) {
+        System.out.println("we have a single player robot...");
+        singlePlayerRobot.checkForStimulus(controller.getUser(), new StimulusAnswerPairAsyncCallback(outer, service, controller));
+      } else {
+        service.checkForStimulus(controller.getUser(), new StimulusAnswerPairAsyncCallback(outer, service, controller));
+      }
     }
-/*
-    HandlerRegistration keyHandler;
+
+    private void gotStimlusResponse(StimulusAnswerPair result, final ReceiverPanel outer, final LangTestDatabaseAsync service, final ExerciseController controller) {
+      if (result != null) {
+        System.out.println("checkForStimulus.onSuccess : got  " + result);
+
+        showStimlus(result, outer);
+        //  System.out.println("checkForStimulus : answer '" + answer + "'");
+      } else {
+        Timer t = new Timer() {
+          @Override
+          public void run() {
+            checkForStimulus(service, controller, outer);
+          }
+        };
+
+        // Schedule the timer to run once in 1 seconds.
+        t.schedule(1000);
+      }
+    }
+
+    private void showStimlus(StimulusAnswerPair result, ReceiverPanel outer) {
+      prompt.setText("Please fill in the blank.");
+      guessBox.setVisible(true);
+      send.setVisible(true);
+      stimulus.setVisible(true);
+      stimulus.setText(result.stimulus);
+      outer.answer = result.answer;
+    }
+
+    private HandlerRegistration keyHandler;
 
     private void addKeyHandler() {
       keyHandler = Event.addNativePreviewHandler(new
-                                                                       Event.NativePreviewHandler() {
+                                                   Event.NativePreviewHandler() {
 
-                                                                         @Override
-                                                                         public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
-                                                                           NativeEvent ne = event.getNativeEvent();
-                                                                           int keyCode = ne.getKeyCode();
+                                                     @Override
+                                                     public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
+                                                       NativeEvent ne = event.getNativeEvent();
+                                                       int keyCode = ne.getKeyCode();
 
-                                                                           boolean isEnter = keyCode == KeyCodes.KEY_ENTER;
+                                                       boolean isEnter = keyCode == KeyCodes.KEY_ENTER;
 
-                                                                           //   System.out.println("key code is " +keyCode);
-                                                                           if (isEnter && event.getTypeInt() == 512 &&
-                                                                             "[object KeyboardEvent]".equals(ne.getString())) {
-                                                                             ne.preventDefault();
-                                                                             sendAnswer(service, controller, outer, soundFeedback);
-                                                                           }
-                                                                         }
-                                                                       });
+                                                       //   System.out.println("key code is " +keyCode);
+                                                       if (isEnter && event.getTypeInt() == 512 &&
+                                                         "[object KeyboardEvent]".equals(ne.getString())) {
+                                                         ne.preventDefault();
+                                                         send.fireEvent(new ButtonClickEvent ());
+                                                       }
+                                                     }
+                                                   });
       // System.out.println("addKeyHandler made click handler " + keyHandler);
-    }*/
+    }
+
+    private class ButtonClickEvent extends ClickEvent{
+        /*To call click() function for Programmatic equivalent of the user clicking the button.*/
+    }
 
     @Override
     protected void onLoad() {
       super.onLoad();
-    //  addKeyHandler();
+      addKeyHandler();
     }
 
     @Override
     protected void onUnload() {
       super.onUnload();
-     // removeKeyHandler();
+      removeKeyHandler();
     }
 
-   /* public void removeKeyHandler() {
+    public void removeKeyHandler() {
       System.out.println("removeKeyHandler : " + keyHandler);
 
       if (keyHandler != null) keyHandler.removeHandler();
-    }*/
+    }
+
+    private class StimulusAnswerPairAsyncCallback implements AsyncCallback<StimulusAnswerPair> {
+      private final ReceiverPanel outer;
+      private final LangTestDatabaseAsync service;
+      private final ExerciseController controller;
+
+      public StimulusAnswerPairAsyncCallback(ReceiverPanel outer, LangTestDatabaseAsync service, ExerciseController controller) {
+        this.outer = outer;
+        this.service = service;
+        this.controller = controller;
+      }
+
+      @Override
+      public void onFailure(Throwable caught) {
+        Window.alert("couldn't contact server.");
+      }
+
+      @Override
+      public void onSuccess(StimulusAnswerPair result) {
+        gotStimlusResponse(result, outer, service, controller);
+      }
+    }
+
+    private class RegisterAnswerResponseCallback implements AsyncCallback<Void> {
+      private final LangTestDatabaseAsync service;
+      private final ExerciseController controller;
+      private final ReceiverPanel outer;
+
+      public RegisterAnswerResponseCallback(LangTestDatabaseAsync service, ExerciseController controller, ReceiverPanel outer) {
+        this.service = service;
+        this.controller = controller;
+        this.outer = outer;
+      }
+
+      @Override
+      public void onFailure(Throwable caught) {
+        Window.alert("couldn't contact server");
+      }
+
+      @Override
+      public void onSuccess(Void result) {
+        checkForStimulus(service, controller, outer);
+      }
+    }
+
   }
 }
