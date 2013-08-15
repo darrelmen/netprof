7762d848b2785a8b1f2440a06304e5f7a13b02f6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-15 18:56:02 -0400

    Records answers in result table
    Former-commit-id: 628c0bfecf1c2b2ec1510c2ca00e8a0482e42497 [formerly 19a10b8f06a9df4df507b2081de3774fc611083b] [formerly 625382f46592cd24f36a42c37cc8d6d2d64be6f2 [formerly 0dd8db77e6ece2e9aeac89535fd3b0944de0e355]] [formerly f35bade7bd0adf3510edc0bf905006dbc2fa470a [formerly 197e0e56ecdbb2eaf7a3bfdcf97dce885acedef6] [formerly 5409b17bfab4e821efd817182864654aae76e35e]]
    Former-commit-id: e82da05812481e00f802982db4f872f05bbeb5b3 [formerly 67ff815ecd981ad59934278c2cc0ffa91c97c59c] [formerly ec607c1bbccb7221f53cc0feec44fa9cefa3d727 [formerly 226f6ef0b617b2f32a8a05e0eea0409fba73dc9c]] [formerly 03fd203ce918a12fce7169c2e1d304ad9c02425d [formerly f7fdaa84516e58cc27e4d6ec9bb53432fbb2488b]]
    Former-commit-id: 80457b4d8c5166726e1d72b3f05d7ff7b1d4779f [formerly 29da4ab8e498897463c8c680dc471dfb33ae9d73]
    Former-commit-id: c9b506615b7c8736d0000512d1084e3c52decd46 [formerly 02ebe87cc09ed02c95a2bbfe999b659fa91bd87a]
    Former-commit-id: 42d007b72cfa155114f8b7c1d8f3af654fc5dd6d

--- src/mitll/langtest/client/exercise/PagingExerciseList.java
+++ src/mitll/langtest/client/exercise/PagingExerciseList.java
@@ -136,12 +136,12 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
     table.setSelectionModel(selectionModel);
     // we don't want to listen for changes in the selection model, since that happens on load too -- we just want clicks
 
-    addColumnsToTable();
+    addColumnsToTable(true);
     return null;
   }
 
-  protected void addColumnsToTable() {
-    Column<ExerciseShell, SafeHtml> id2 = getExerciseIdColumn();
+  protected void addColumnsToTable(boolean consumeClicks) {
+    Column<ExerciseShell, SafeHtml> id2 = getExerciseIdColumn(consumeClicks);
 
     // this would be better, but want to consume clicks
   /*  TextColumn<ExerciseShell> id2 = new TextColumn<ExerciseShell>() {
@@ -158,12 +158,16 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
     table.addColumn(id2);
   }
 
-  private Column<ExerciseShell, SafeHtml> getExerciseIdColumn() {
+  /**
+   * @see #addColumnsToTable
+   * @return
+   */
+  private Column<ExerciseShell, SafeHtml> getExerciseIdColumn(final boolean consumeClicks) {
     return new Column<ExerciseShell, SafeHtml>(new SafeHtmlCell() {
         @Override
         public Set<String> getConsumedEvents() {
           Set<String> events = new HashSet<String>();
-          events.add(BrowserEvents.CLICK);
+          if (consumeClicks) events.add(BrowserEvents.CLICK);
           return events;
         }
       }) {
@@ -176,7 +180,7 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
         public void onBrowserEvent(Cell.Context context, Element elem, ExerciseShell object, NativeEvent event) {
           super.onBrowserEvent(context, elem, object, event);
           if (BrowserEvents.CLICK.equals(event.getType())) {
-            System.out.println("got click " + event);
+            System.out.println("getExerciseIdColumn.onBrowserEvent : got click " + event);
             final ExerciseShell e = object;
             if (isExercisePanelBusy()) {
               Window.alert("Please stop recording before changing items.");
