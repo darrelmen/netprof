5ef0d9d68f0bfea21c7e6db90981f6b5ec36b4a6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-20 20:01:35 -0400

    added correct/incorrect feedback, item header, refactored classes into different packages
    Former-commit-id: 7b5f76bb340653e916575e881025114f500d1768

--- src/mitll/langtest/server/taboo/Tatoeba.java
+++ src/mitll/langtest/server/taboo/Tatoeba.java
@@ -1,15 +1,5 @@
 package mitll.langtest.server.taboo;
 
-import mitll.langtest.server.database.FileExerciseDAO;
-import mitll.langtest.shared.Exercise;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-
 /**
  * Created with IntelliJ IDEA.
  * User: GO22670
