ecf8ccde40ebb37ddb8f2c6add68183dd0ecdfa6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-23 20:37:21 -0400

    almost there -- most states correct
    Former-commit-id: 6791a37806b9c82ceb6c0fea2a471806b9cd584f [formerly 1fadc24bef642cc43f96257d84bf8783e1a25588] [formerly 630279f537274c95811f693099abd27234d3d7ec [formerly 1a1f49a3b1c9b6f6d9f059f2a1deb6f4595eb5ee]] [formerly 6791a37806b9c82ceb6c0fea2a471806b9cd584f [formerly 1fadc24bef642cc43f96257d84bf8783e1a25588] [formerly 630279f537274c95811f693099abd27234d3d7ec [formerly 1a1f49a3b1c9b6f6d9f059f2a1deb6f4595eb5ee]] [formerly c13796e4a97e2dfaa844a4e596faae4689b10e8b [formerly 40f89be696c7dc3082d69e58a70d3653131f2d61] [formerly 0fd7202c5b299e2d4d766ccb200e601ad1f19fbe [formerly c201fc15c522875e418706e0e52c58fe79799bfa]]]]
    Former-commit-id: 5c8b4f7e7621dd0d7005d8f470198418dd10744e [formerly 9a9c5fb32934dcebadffb7a49dff1997198c5df5] [formerly 49233607e1b64454674389ceca38239e249ff57f [formerly a68ed2bec1e7666200f7f292eadbc75e68ff741c]] [formerly d363d17f9c190105c27350695cb4b5f6e54fb1f7 [formerly 03e3b1a9e3896afd8057a3931d69ad65958f2473] [formerly 3149991c0f9c07c4c33f0a2f0ccc5b51b7dea2cc [formerly ac024cbb3a92f9e8bd79bedbe752767f5b12751b]]] [formerly 6aabfcdb096b746e6ffebb7b43f721f302c4d9f2 [formerly 38700a62de4ff09dfba0fb55840992b3324fd562] [formerly 05eb1eff79eecb1ff0158746e29d9e725779e4be [formerly 5a24dcd8e44bd7b54cd4f8240ca9022291a0e316]] [formerly 5de3b673d4df59d50ebd348c17ad65912c7337fe [formerly 110052823006e37fdd19314718778a3b5d892a0f] [formerly 5bccf4ad71067275195f0e18d06804d00a5a87d7 [formerly e65d9619767da580fb4ce8e411db0b3d476393b9]]]]
    Former-commit-id: 6aabfcdb096b746e6ffebb7b43f721f302c4d9f2 [formerly 38700a62de4ff09dfba0fb55840992b3324fd562] [formerly 05eb1eff79eecb1ff0158746e29d9e725779e4be [formerly 5a24dcd8e44bd7b54cd4f8240ca9022291a0e316]]
    Former-commit-id: 1833494ec68287380d5ad2993149f7f1d55304c8 [formerly 340e7e6d028ca74ffb8d21b33c4ebf37562a7ce2] [formerly ca88c85caf1899a0d9e1874696931e312a0dd994 [formerly 4ab8dbfd94dd84a7d3a2152af7edef97ac2b3f29]]
    Former-commit-id: d21ffbd7fe03a362dedb31a8d26df2111c80efc8 [formerly 8f016cdc58be122dd7fc1e64afda3d8920744b0b]
    Former-commit-id: 634f3d5eaea31edf105da991cebc5661d1c19e06

--- src/mitll/langtest/client/taboo/ReceiverExerciseFactory.java
+++ src/mitll/langtest/client/taboo/ReceiverExerciseFactory.java
@@ -39,6 +39,7 @@ import mitll.langtest.shared.taboo.StimulusAnswerPair;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 
 /**
  * Created with IntelliJ IDEA.
@@ -53,7 +54,7 @@ import java.util.Date;
  */
 public class ReceiverExerciseFactory extends ExercisePanelFactory {
   public static final int POPUP_DURATION = 2000;
-  public static final int CHECK_FOR_STIMULUS_INTERVAL = 2000;
+  public static final int CHECK_FOR_STIMULUS_INTERVAL = 1000;
   private SinglePlayerRobot singlePlayerRobot;
 
   /**
@@ -78,13 +79,11 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
    * @return
    */
   public Panel getExercisePanel(Exercise totallyIgnored) {
-    System.out.println("\nReceiverExerciseFactory.getExercisePanel getting receiver panel ...");
+    System.out.println("ReceiverExerciseFactory.getExercisePanel getting receiver panel ...");
     controller.pingAliveUser();
 
     return new ReceiverPanel(service,controller);
   }
-
- // private Set<String> exerciseList = new HashSet<String>();
 
   /**
    * @see TabooExerciseList#rememberExercises(java.util.List)
@@ -94,13 +93,15 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
     if (singlePlayerRobot != null) {
       singlePlayerRobot.setExerciseShells(exerciseShells);
     }
-   // for (ExerciseShell shell : exerciseShells) exerciseList.add(shell.getID());
+    else {
+      System.err.println("\n\n\n--->setExerciseShells ignoring items...");
+    }
   }
 
   private class ReceiverPanel extends FluidContainer {
     private static final String PLEASE_WAIT = "Please wait for giver to send next sentence.";
     Heading exerciseDisplay = new Heading(3);
-    Heading prompt = new Heading(3/*, "Checking for game partners..."*/);
+    Heading prompt = new Heading(3);
     TextBox guessBox = new NoPasteTextBox();
     Heading stimulus;
     String answer;
@@ -112,7 +113,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
 
     public ReceiverPanel(final LangTestDatabaseAsync service, final ExerciseController controller) {
       final ReceiverPanel outer = addWidgets(service,controller);
-      System.out.println("-----> ReceiverPanel: making the panel...");
+      System.out.println("-----> ReceiverPanel: making the panel... check for stimulus");
       checkForStimulus(service, controller, outer);
     }
 
@@ -147,7 +148,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       warnNoFlash.setVisible(false);
       final SoundFeedback soundFeedback = new SoundFeedback(controller.getSoundManager(), warnNoFlash);
 
-      waitForNext(true);
+      waitForNext();
 
       send.addClickHandler(new ClickHandler() {
         @Override
@@ -159,13 +160,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
 
       FluidContainer container = new FluidContainer();
       container.add(send);
- //     container.add(correctImage);
-  //    correctImage.setVisible(false);
-    //  container.add(incorrectImage);
-   ///   incorrectImage.setVisible(false);
-
       add(container);
-
       add(warnNoFlash);
       add(addCorrectIncorrectFeedback());
       return outer;
@@ -190,28 +185,25 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       correct.addStyleName("correctRatio");
       pair.add(correct);
       setCorrect();
-      return correctAndImageRow;    //To change body of overridden methods use File | Settings | File Templates.
+      return correctAndImageRow;
     }
 
-    public void setCorrect() {
-      correct.setText(correctCount + "/" + (correctCount + incorrectCount));
-    }
+    public void setCorrect() {  correct.setText(correctCount + "/" + (correctCount + incorrectCount));   }
 
     private void sendAnswer(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer, SoundFeedback soundFeedback) {
-      boolean isCorrect = guessBox.getText().equalsIgnoreCase(answer);
+      boolean isCorrect = checkCorrect();
       System.out.println("sending answer '" + guessBox.getText() + "' vs '" + answer+ "' which is " + isCorrect);
-      registerAnswer(service, controller, outer, isCorrect);
+      registerAnswer(service, controller, outer, isCorrect, isCorrect || onLastStim);
 
       if (isCorrect) {
-        showPopup("Correct! Please wait for the next item.");
+        showPopup("Correct!" + (singlePlayerRobot == null ? " Please wait for the next item." : ""));
         correctImage.setVisible(true);
         controller.addAdHocExercise(answer);
         soundFeedback.playCorrect();
-       // System.out.println("increment correct");
 
         correctCount++;
         setCorrect();
-//        keepTrackOfRemainingExercises();
+        loadNext(controller);
       }
       else {
         incorrectImage.setVisible(true);
@@ -221,35 +213,49 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
           incorrectCount++;
           //System.out.println("---> incrementing incorrect");
           setCorrect();
-//          keepTrackOfRemainingExercises();
+          loadNext(controller);
         }
         else {
           showPopup("Try again...");
-         // System.out.println("\t not incrementing incorrect");
         }
       }
-      waitForNext(false);
+      waitForNext();
     }
 
-/*
-    private void keepTrackOfRemainingExercises() {
-      if (!exerciseList.contains(exerciseID)) System.err.println("huh? can't find " + exerciseID + " in list of current exercises...");
-      else exerciseList.remove(exerciseID);
+    private void loadNext(ExerciseController controller) {
+     // boolean found = false;
+      controller.loadNextExercise(exerciseID);
+/*      for (ExerciseShell es : exerciseList) {
+        if (es.getID().trim().equals(exerciseID.trim())) {
+          controller.loadNextExercise(es);
+          found = true;
+          break;
+        }
+      }*/
+    //  if (!found) System.err.println("->> couldn't find '" + exerciseID + "'");
     }
-*/
 
-    private void registerAnswer(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer, boolean correct) {
+    private boolean checkCorrect() {
+      boolean isCorrect = guessBox.getText().equalsIgnoreCase(answer);
+      if (answer.startsWith("to ")){ // verbs
+        isCorrect = guessBox.getText().equalsIgnoreCase(answer.substring(3));
+      }
+      return isCorrect;
+    }
+
+    private void registerAnswer(final LangTestDatabaseAsync service, final ExerciseController controller,
+                                final ReceiverPanel outer, boolean correct, boolean movingOnToNext) {
       if (singlePlayerRobot != null) {
         singlePlayerRobot.registerAnswer(correct);
       }
 
       service.registerAnswer(controller.getUser(), exerciseID, stimulus.getText(), guessBox.getText(), correct,
-        new RegisterAnswerResponseCallback(service, controller, outer));
+        new RegisterAnswerResponseCallback(service, controller, outer, movingOnToNext));
     }
 
-    private void waitForNext(boolean initialCall) {
+    private void waitForNext() {
       //if (!initialCall) {
-        prompt.setText(PLEASE_WAIT);
+        prompt.setText(singlePlayerRobot == null ? PLEASE_WAIT : "");
       //}
       exerciseDisplay.setText("");
       stimulus.setVisible(false);
@@ -257,18 +263,14 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
 
       guessBox.setVisible(false);
       guessBox.setText("");
-   //   correctImage.setVisible(false);
-  //    incorrectImage.setVisible(false);
       send.setVisible(false);
     }
-
 
     private void showPopup(String html) {
       showPopup(html, POPUP_DURATION);
     }
 
     private void showPopup(String html, int dur) {
-
       final PopupPanel pleaseWait = new DecoratedPopupPanel();
       pleaseWait.setAutoHideEnabled(true);
       pleaseWait.add(new HTML(html));
@@ -290,18 +292,32 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
      * @param outer
      */
     private void checkForStimulus(final LangTestDatabaseAsync service, final ExerciseController controller, final ReceiverPanel outer) {
-      //System.out.println(new Date() + " : checkForStimulus : user " + controller.getUser() + "----------------");
-      //cancelStimTimer();
-
+      System.out.println(new Date() + " : checkForStimulus : user " + controller.getUser() + " ----------------");
       if (singlePlayerRobot != null) {
-       // System.out.println("we have a single player robot...");
-        singlePlayerRobot.checkForStimulus(controller.getUser(), new StimulusAnswerPairAsyncCallback(outer, service, controller));
+        System.out.println("checkForStimulus : we have a single player robot...");
+        singlePlayerRobot.checkForStimulus(new AsyncCallback<StimulusAnswerPair>() {
+          @Override
+          public void onFailure(Throwable caught) {}
+
+          @Override
+          public void onSuccess(StimulusAnswerPair result) {
+            if (result != null) {
+              System.out.println(new Date() + " gotStimulusResponse : showStimlus  " + result);
+              showStimulus(result, outer);
+            } else { // no more stimulus left...
+              removeKeyHandler();
+              prompt.setText("Please choose another chapter(s).");
+              new ModalInfoDialog("Word list complete", "Please choose another chapter.");
+            }
+          }
+        });
       } else {
         service.checkForStimulus(controller.getUser(), new StimulusAnswerPairAsyncCallback(outer, service, controller));
       }
     }
 
     private Timer checkStimTimer = null;
+    private Timer verifyStimTimer = null;
 
     /**
      * See if giver has sent the next stimulus yet.
@@ -332,7 +348,7 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
           checkStimTimer = new Timer() {
             @Override
             public void run() {
-             // System.out.println("Fired!  Timer : " + checkStimTimer + " " + new Date() + " : checkForStimulus : " + controller.getUser());
+              //System.out.println("Fired!  Timer : " + checkStimTimer + " " + new Date() + " : checkForStimulus : " + controller.getUser());
               checkStimTimer = null;
               checkForStimulus(service, controller, outer);
             }
@@ -353,6 +369,40 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       }
     }
 
+    private void pollForStimChange(final ReceiverPanel outer) {
+      if (verifyStimTimer == null) {
+
+        verifyStimTimer = new Timer() {
+          @Override
+          public void run() {
+           // System.out.println("pollForStimChange : Fired!  Timer : " + checkStimTimer + " " + new Date() + " : checkForStimulus : " + controller.getUser());
+            verifyStimTimer = null;
+            service.checkForStimulus(controller.getUser(), new AsyncCallback<StimulusAnswerPair>() {
+              @Override
+              public void onFailure(Throwable caught) {
+              }
+
+              @Override
+              public void onSuccess(StimulusAnswerPair result) {
+                if (!result.equals(displayedStimulus)) {
+                  if (result.noStimYet) waitForNext();
+                  else showStimulus(result, outer);
+                }
+                else pollForStimChange(outer);
+              }
+            });
+          }
+        };
+        //System.out.println("pollForStimChange : Queued: Timer : " + checkStimTimer + " " + new Date() + " : checkForStimulus user #" + controller.getUser());
+
+        // Schedule the timer to run once in 1 seconds.
+        verifyStimTimer.schedule(CHECK_FOR_STIMULUS_INTERVAL);
+      }
+      else {
+        System.out.println("pollForStimChange : DID NOT Queue: Timer : " + checkStimTimer + " " + new Date() + " : checkForStimulus user #" + controller.getUser());
+      }
+    }
+
     private void cancelStimTimer() {
       if (checkStimTimer != null) {
         System.out.println("\tCancelling timer " + checkStimTimer);
@@ -362,7 +412,10 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
     }
 
     private boolean onLastStim = false;
+    private StimulusAnswerPair displayedStimulus;
+
     private void showStimulus(StimulusAnswerPair result, ReceiverPanel outer) {
+      displayedStimulus = result;
       onLastStim = result.isLastStimulus;
       exerciseDisplay.setText("Phrase # " + result.getExerciseID());
 
@@ -374,6 +427,9 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       outer.answer = result.getAnswer();
       exerciseID = result.getExerciseID();
       guessBox.setFocus(true);
+      if (singlePlayerRobot == null) {
+        pollForStimChange(outer);
+      }
     }
 
     private HandlerRegistration keyHandler;
@@ -453,11 +509,13 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
       private final LangTestDatabaseAsync service;
       private final ExerciseController controller;
       private final ReceiverPanel outer;
+      private final boolean movingOn;
 
-      public RegisterAnswerResponseCallback(LangTestDatabaseAsync service, ExerciseController controller, ReceiverPanel outer) {
+      public RegisterAnswerResponseCallback(LangTestDatabaseAsync service, ExerciseController controller, ReceiverPanel outer, boolean movingOn) {
         this.service = service;
         this.controller = controller;
         this.outer = outer;
+        this.movingOn = movingOn;
       }
 
       @Override
@@ -467,12 +525,12 @@ public class ReceiverExerciseFactory extends ExercisePanelFactory {
 
       @Override
       public void onSuccess(Void result) {
-        checkForStimulus(service, controller, outer);
+        if (!movingOn) {
+          System.out.println("check : RegisterAnswerResponseCallback");
+
+          checkForStimulus(service, controller, outer);
+        }
       }
     }
   }
-/*
-  private void showUserState(String title, String message) {
-    new ModalInfoDialog(title, message);
-  }*/
 }
