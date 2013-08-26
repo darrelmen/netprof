0680a49af57c885abea069148201ded5f2dc2d66
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-26 19:23:28 -0400

    some refactorings to make things simpler, documentation
    Former-commit-id: 0c28c1fa3766c2a6b53e124d547a06d808824151 [formerly 9c40dc7605bf497592352792ff46b8e0ed4727ae] [formerly ae7a5a159427bf92d9e68b472f2001f53494da31 [formerly 3c1f9cc1aa212863c2e762efa2b5b3717e35c5df]]
    Former-commit-id: 7af2babc4c1aadb56bb9a4ff3fd48c54dff4d5b1 [formerly 5abfde656defaace06f4cbc86428ef04f065d55f] [formerly b1d3f97fc5e43c28e1bfbb4c956373a20a68ddd6 [formerly 9c636f07659b77469f6992e6fc86e4f74aee27c1]]
    Former-commit-id: 84c743dbd7830767a64722ab4692ed2ccb8c5df4 [formerly c886e97584c2a2df6e9f72a169f582b38f098575]
    Former-commit-id: f35abaeec5a45bd58f05a3df8078e48662573bbc

--- src/mitll/langtest/client/taboo/ModalInfoDialog.java
+++ src/mitll/langtest/client/taboo/ModalInfoDialog.java
@@ -4,6 +4,9 @@ import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Heading;
 import com.github.gwtbootstrap.client.ui.Modal;
 import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
+import com.github.gwtbootstrap.client.ui.event.HideEvent;
+import com.github.gwtbootstrap.client.ui.event.HideHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 
@@ -12,10 +15,14 @@ import java.util.Collections;
 
 public class ModalInfoDialog {
   public ModalInfoDialog(String title, String message) {
-    this(title, Collections.singleton(message));
+    this(title, Collections.singleton(message), null);
   }
 
-  public ModalInfoDialog(String title, Collection<String> message) {
+  public ModalInfoDialog(String title, String message, HiddenHandler handler) {
+    this(title, Collections.singleton(message), handler);
+  }
+
+  public ModalInfoDialog(String title, Collection<String> message, HiddenHandler handler) {
     final Modal modal = new Modal(true);
     modal.setTitle(title);
     for (String m : message) {
@@ -37,6 +44,9 @@ public class ModalInfoDialog {
     });
     modal.add(begin);
 
+    if (handler != null) {
+      modal.addHiddenHandler(handler);
+    }
     modal.show();
   }
 }
