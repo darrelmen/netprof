4c122d2bd623bb6d24ca17f1b2a2e365c493359f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-29 19:44:49 -0400

    almost got game synchronization working
    Former-commit-id: b47c1a2535725e13f6ad9059f8847569ab7b2503 [formerly f099b6b66a64879f7af4b8836fd5de50f0a52baf] [formerly b47c1a2535725e13f6ad9059f8847569ab7b2503 [formerly f099b6b66a64879f7af4b8836fd5de50f0a52baf] [formerly b47c1a2535725e13f6ad9059f8847569ab7b2503 [formerly f099b6b66a64879f7af4b8836fd5de50f0a52baf] [formerly 0eef005fe3990e6f48e9adee299caba4dcb7107c]]]
    Former-commit-id: 165e54395990a224276f1cc83a7186d25be8911b [formerly fefbb999f32f25bb4c8f132b953cee4a3ca1e289] [formerly 165e54395990a224276f1cc83a7186d25be8911b [formerly fefbb999f32f25bb4c8f132b953cee4a3ca1e289] [formerly af109a78f698b454ddd3e35cead29f62350ef4ff]] [formerly b127a65f93b92a07ddfbc72b70efe2d35f67f825 [formerly 335731f8e3f119f9b9718c8af3f672bff79cfb66] [formerly b127a65f93b92a07ddfbc72b70efe2d35f67f825 [formerly 335731f8e3f119f9b9718c8af3f672bff79cfb66] [formerly 9d2f0dd31403b70d5e0e4253f4bacaab89245223]]]
    Former-commit-id: b127a65f93b92a07ddfbc72b70efe2d35f67f825 [formerly 335731f8e3f119f9b9718c8af3f672bff79cfb66]
    Former-commit-id: ce144193c0a489d6fb3f4a507cf1328ccd1054be [formerly f96874df597123ebbb9f600a74f3fadbcbffa14c] [formerly 7c9cceb0fa15d6fe582a3a2522abf4400cdf721e]
    Former-commit-id: 8cd4c6a3b449298fe04ce7db245d43e816df135d
    Former-commit-id: 7d6da911a652c68068b70466e6dd6079f8dd7b14

--- src/mitll/langtest/shared/taboo/PartnerState.java
+++ src/mitll/langtest/shared/taboo/PartnerState.java
@@ -15,9 +15,19 @@ import java.util.Map;
 public class PartnerState implements IsSerializable {
   private boolean online;
   private Map<String, Collection<String>> typeToSelection;
+  private GameInfo gameInfo;
 
   public PartnerState() {}
 
+  public PartnerState(boolean online, Map<String, Collection<String>> typeToSelection, GameInfo gameInfo) {
+    this(online, typeToSelection);
+    this.gameInfo = gameInfo;
+  }
+    /**
+     * @see mitll.langtest.server.database.taboo.OnlineUsers#isPartnerOnline(long, boolean)
+     * @param online
+     * @param typeToSelection
+     */
   public PartnerState(boolean online, Map<String, Collection<String>> typeToSelection) {
     this.online = online;
     this.typeToSelection = typeToSelection;
@@ -29,5 +39,14 @@ public class PartnerState implements IsSerializable {
 
   public Map<String, Collection<String>> getTypeToSelection() {
     return typeToSelection;
+  }
+
+
+  public GameInfo getGameInfo() {
+    return gameInfo;
+  }
+
+  public void setGameInfo(GameInfo gameInfo) {
+    this.gameInfo = gameInfo;
   }
 }
