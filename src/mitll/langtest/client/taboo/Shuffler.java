34ea81a6036275accfcb87dfc681de455b73e230
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-08-30 17:45:32 -0400

    added unrelated clues
    Former-commit-id: 9d339f4a8873c53707838dadb400d4cd14ffba5a
    Former-commit-id: 888903f8c0fa9186f0a703cc0d6bfea945f0fc71

--- src/mitll/langtest/client/taboo/Shuffler.java
+++ src/mitll/langtest/client/taboo/Shuffler.java
@@ -1,5 +1,6 @@
 package mitll.langtest.client.taboo;
 
+import mitll.langtest.shared.Exercise;
 import mitll.langtest.shared.ExerciseShell;
 
 import java.util.List;
@@ -33,6 +34,36 @@ public class Shuffler {
    */
   private void swap(ExerciseShell[] arr, int i, int j) {
     ExerciseShell tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  public void shuffle2(List<Exercise.QAPair> list, Random rnd) {
+    int size = list.size();
+    Exercise.QAPair arr[] = list.toArray(new Exercise.QAPair[size]);
+
+    if (size < 5) {
+      for (int i = size; i > 1; i--)
+        swap2(arr, i - 1, rnd.nextInt(i));
+    } else {
+      // Shuffle array
+      for (int i = size; i > 1; i--)
+        swap2(arr, i - 1, rnd.nextInt(i));
+
+      // Dump array back into list
+      ListIterator<Exercise.QAPair> it = list.listIterator();
+      for (Exercise.QAPair anArr : arr) {
+        it.next();
+        it.set(anArr);
+      }
+    }
+  }
+
+  /**
+   * Swaps the two specified elements in the specified array.
+   */
+  private void swap2(Exercise.QAPair[] arr, int i, int j) {
+    Exercise.QAPair tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
   }
