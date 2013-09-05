73d858c1ce1b72ae6614975403cec86414f34833
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-09-04 20:35:58 -0400

    mostly got it working with two players -- shows leaderboard, etc
    Former-commit-id: c7fb63c5b68234587cc584d2c018f9e5a7170122 [formerly 714f26afec50df15ea78e87b9e3c18f8c1b22076] [formerly c7fb63c5b68234587cc584d2c018f9e5a7170122 [formerly 714f26afec50df15ea78e87b9e3c18f8c1b22076] [formerly c7fb63c5b68234587cc584d2c018f9e5a7170122 [formerly 714f26afec50df15ea78e87b9e3c18f8c1b22076] [formerly 285041089d0d99873d1e2b8e9aecc8da9317836e]]]
    Former-commit-id: aeb57eb835e767aabfb009c4be7325c62d87a5cd [formerly ef65dd334d34b8df0ebab99199390805ff654ce0] [formerly aeb57eb835e767aabfb009c4be7325c62d87a5cd [formerly ef65dd334d34b8df0ebab99199390805ff654ce0] [formerly f4fcf0fd29f9d1339807c3a776cbc72e71ad208a]] [formerly fec4fc3d046e56208573e760baffffb3dfc794c4 [formerly 502016e662aeb0a9fdfbe8ff326b0f7a09cfdf64] [formerly fec4fc3d046e56208573e760baffffb3dfc794c4 [formerly 502016e662aeb0a9fdfbe8ff326b0f7a09cfdf64] [formerly 7db3134d7639cc9cd2a6cbf1ed30c61e673bbb59]]]
    Former-commit-id: fec4fc3d046e56208573e760baffffb3dfc794c4 [formerly 502016e662aeb0a9fdfbe8ff326b0f7a09cfdf64]
    Former-commit-id: 3a336c7fc0b924aa46a8d42abf19622498ca8257 [formerly 00013c07929b91bfdbe650fbe347968846758cd8] [formerly eb843c16c5cb27ec86588983e63ee5c13cf40645]
    Former-commit-id: f2ccc73dd7a793bfe5729d7bdbdbbb0b1ec27b34
    Former-commit-id: 7fd375505e3bb12da0702ca5650c2969a9a3f6bf

--- src/mitll/langtest/client/taboo/SinglePlayerRobot.java
+++ src/mitll/langtest/client/taboo/SinglePlayerRobot.java
@@ -10,8 +10,10 @@ import mitll.langtest.shared.taboo.Game;
 import mitll.langtest.shared.taboo.StimulusAnswerPair;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * TODO : add start/end game to protocol -- with starting a game, both sides get an item list.
@@ -25,19 +27,18 @@ import java.util.Random;
 public class SinglePlayerRobot {
   private final LangTestDatabaseAsync service;
   private List<ExerciseShell> exercisesRemaining = null;
+  private Set<String> currentIDs = new HashSet<String>();
   private Exercise currentExercise = null;
   private List<Exercise.QAPair> clueAnswerPairs = Collections.emptyList();
   private int numClues = 0;
-  //private PropertyHandler propertyHandler;
 
   /**
    * @see mitll.langtest.client.LangTest#setTabooFactory(long, boolean, boolean)
    * @param service
    * @param propertyHandler
    */
-  public SinglePlayerRobot(LangTestDatabaseAsync service, PropertyHandler propertyHandler) {
+  public SinglePlayerRobot(LangTestDatabaseAsync service) {
     this.service = service;
-    //this.propertyHandler = propertyHandler;
   }
 
   /**
@@ -53,7 +54,7 @@ public class SinglePlayerRobot {
     } else {
       if (clueAnswerPairs.isEmpty()) {
         System.out.println("SinglePlayerRobot.checkForStimulus " + exercisesRemaining.size());
-        getNextExercise(async);
+        getNextExercise(async,5);
       } else {
         replyWithNextClue(currentExercise.getID(), async);
       }
@@ -61,18 +62,17 @@ public class SinglePlayerRobot {
   }
 
   private boolean isGameOver() {
-    return exercisesRemaining.isEmpty();// && clueAnswerPairs.isEmpty();
+    return exercisesRemaining.isEmpty();
   }
 
   /**
    * @see #checkForStimulus(com.google.gwt.user.client.rpc.AsyncCallback
    * @param async
    */
-  private void getNextExercise(final AsyncCallback<StimulusAnswerPair> async) {
-    System.out.println("SinglePlayerRobot.getNextExercise exercisesRemaining = " + exercisesRemaining.size());
-
+  private void getNextExercise(final AsyncCallback<StimulusAnswerPair> async, final int n) {
     ExerciseShell nextExerciseShell = exercisesRemaining.get(0);
-    System.out.println("SinglePlayerRobot.getNextExercise for = " + nextExerciseShell.getID());
+    System.out.println("SinglePlayerRobot.getNextExercise : exercisesRemaining = " + exercisesRemaining.size() +
+      " ex id = " + nextExerciseShell.getID());
 
     service.getExercise(nextExerciseShell.getID(), new AsyncCallback<Exercise>() {
       @Override
@@ -84,17 +84,28 @@ public class SinglePlayerRobot {
       public void onSuccess(Exercise result) {
         System.out.println("SinglePlayerRobot.getNextExercise.onSuccess got " + result);
 
-        currentExercise = result;
-        exercisesRemaining.remove(0);
+        if (!currentIDs.contains(result.getID())) {
+          System.out.println("SinglePlayerRobot.getNextExercise.onSuccess exercise is stale : " + result);
 
-        clueAnswerPairs = Game.randomSample2(currentExercise.getQuestions(), ReceiverExerciseFactory.MAX_CLUES_TO_GIVE, rnd);
-        numClues = clueAnswerPairs.size();
+          if (n > 0) {
+            getNextExercise(async, n - 1);
+          } else { // should NEVER happen
+            System.err.println("SinglePlayerRobot.getNextExercise.onSuccess exercise is stale : " + result);
 
-        if (clueAnswerPairs.isEmpty()) {
-          System.err.println("huh? no stim sentences for " + currentExercise);
-          async.onSuccess(new StimulusAnswerPair(result.getID(), "Data error on server, please report.", "", false, false, numClues, true));
+          }
         } else {
-          replyWithNextClue(currentExercise.getID(),async);
+          currentExercise = result;
+          exercisesRemaining.remove(0);
+
+          clueAnswerPairs = Game.randomSample2(currentExercise.getQuestions(), ReceiverExerciseFactory.MAX_CLUES_TO_GIVE, rnd);
+          numClues = clueAnswerPairs.size();
+
+          if (clueAnswerPairs.isEmpty()) {
+            System.err.println("huh? no stim sentences for " + currentExercise);
+            async.onSuccess(new StimulusAnswerPair(result.getID(), "Data error on server, please report.", "", false, false, numClues, true));
+          } else {
+            replyWithNextClue(currentExercise.getID(), async);
+          }
         }
       }
     });
@@ -103,10 +114,8 @@ public class SinglePlayerRobot {
   private void replyWithNextClue(String exerciseID, AsyncCallback<StimulusAnswerPair> async) {
     Exercise.QAPair clueAnswerPair = clueAnswerPairs.remove(0);
     String rawStim = clueAnswerPair.getQuestion();
-    boolean empty = clueAnswerPairs.isEmpty();
-
     String answer = clueAnswerPair.getAnswer();
-    async.onSuccess(new StimulusAnswerPair(exerciseID, rawStim, answer, empty, false, numClues, isGameOver()));
+    async.onSuccess(new StimulusAnswerPair(exerciseID, rawStim, answer, clueAnswerPairs.isEmpty(), false, numClues, isGameOver()));
   }
 
   /**
@@ -116,9 +125,6 @@ public class SinglePlayerRobot {
   public void registerAnswer(boolean correct) {
     if (correct) clueAnswerPairs = Collections.emptyList();
   }
-
- // public GameInfo getGame() { return game; }
- // public boolean anyGamesRemaining() { return game.anyGamesRemaining(); }
 
   private Game game;
   private Random rnd = new Random();
@@ -131,9 +137,17 @@ public class SinglePlayerRobot {
     game = new Game(exerciseShells);
   }
 
+  /**
+   * @see ReceiverExerciseFactory#setExerciseShells(java.util.List, java.util.Map)
+   * @return
+   */
   public Game startGame() {
     exercisesRemaining = game.startGame();
-
+    System.out.println("startGame " + exercisesRemaining.size());
+    currentIDs.clear();
+    for (ExerciseShell es : exercisesRemaining) {
+      currentIDs.add(es.getID());
+    }
     clueAnswerPairs = Collections.emptyList();
     return game;
   }
