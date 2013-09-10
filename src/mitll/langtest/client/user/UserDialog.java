1c174208e3f86745bb6b45aa014a74c8fc38ba47
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-09-10 18:08:39 -0400

    refactoring of student and data collector dialog into separate classes
    Former-commit-id: 9874deef1dd798f10e272dc7373b00772276ee34 [formerly cafe61ff22c047cfc5d52c85c13ff6d7d4a2ffbb] [formerly 9874deef1dd798f10e272dc7373b00772276ee34 [formerly cafe61ff22c047cfc5d52c85c13ff6d7d4a2ffbb] [formerly 639c7e1237a2409cc836b938c7061c05f6549dd4]]
    Former-commit-id: 451c9cd5548f519fcf31618607dc9801df1fbc58 [formerly 71da1406799d57adf36fd023920a6a93909bdde9] [formerly e499ee2acceab63242c40012182958a44c5d3701] [formerly 4b8401f3f6f768a8ea35f9d811bb4a0815bb7fde [formerly 174d89a537cdd238a23cf0ed3c87bd7b06344a46] [formerly b0bb2cb591a62b6f87e2d8ce84b8a866c1324b08]]
    Former-commit-id: 4b8401f3f6f768a8ea35f9d811bb4a0815bb7fde [formerly 174d89a537cdd238a23cf0ed3c87bd7b06344a46]
    Former-commit-id: 432c1a65a198e3301309f6b3b32545e69a600790
    Former-commit-id: 6a430351f5f7a1291e52cb6c534d7e2354e94254

---
+++ src/mitll/langtest/client/user/UserDialog.java
@@ -0,0 +1,250 @@
+package mitll.langtest.client.user;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.ControlGroup;
+import com.github.gwtbootstrap.client.ui.ControlLabel;
+import com.github.gwtbootstrap.client.ui.ListBox;
+import com.github.gwtbootstrap.client.ui.Modal;
+import com.github.gwtbootstrap.client.ui.PasswordTextBox;
+import com.github.gwtbootstrap.client.ui.Popover;
+import com.github.gwtbootstrap.client.ui.TextBox;
+import com.github.gwtbootstrap.client.ui.constants.BackdropType;
+import com.github.gwtbootstrap.client.ui.constants.ControlGroupType;
+import com.github.gwtbootstrap.client.ui.constants.Placement;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.PropertyHandler;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 9/10/13
+ * Time: 4:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserDialog {
+  protected static final int USER_ID_MAX_LENGTH = 80;
+
+  private static final String GRADING = "grading";
+  private static final String TESTING = "testing";
+
+  protected static final int MIN_AGE = 12;
+  protected static final int MAX_AGE = 90;
+  protected static final int TEST_AGE = 100;
+  protected static final List<String> EXPERIENCE_CHOICES = Arrays.asList(
+    "0-3 months (Semester 1)",
+    "4-6 months (Semester 1)",
+    "7-9 months (Semester 2)",
+    "10-12 months (Semester 2)",
+    "13-16 months (Semester 3)",
+    "16+ months",
+    "Native speaker");
+  protected static final int NATIVE_MONTHS = 20 * 12;
+  protected final PropertyHandler props;
+
+  protected DisclosurePanel dp;
+  protected final LangTestDatabaseAsync service;
+
+  public UserDialog(LangTestDatabaseAsync service, PropertyHandler props) {
+    this.service = service;
+    this.props = props;
+  }
+
+  /**
+   * From Modal code.
+   * Centers fixed positioned element vertically.
+   * @param e Element to center vertically
+   */
+  protected native void centerVertically(Element e) /*-{
+      $wnd.jQuery(e).css("margin-top", (-1 * $wnd.jQuery(e).outerHeight() / 2) + "px");
+  }-*/;
+
+  protected int getAge(TextBox ageEntryBox) {
+    int i = 0;
+    try {
+      i = props.isDataCollectAdminView() ? 89 : Integer.parseInt(ageEntryBox.getText());
+    } catch (NumberFormatException e) {
+      System.out.println("couldn't parse " + ageEntryBox.getText());
+    }
+    return i;
+  }
+
+  protected Modal getDialog() {
+    final Modal dialogBox = new Modal();
+    dialogBox.setCloseVisible(false);
+    dialogBox.setKeyboard(false);
+    dialogBox.setBackdrop(BackdropType.STATIC);
+    return dialogBox;
+  }
+
+  protected static class FormField {
+    public final TextBox box;
+    public final ControlGroup group;
+
+    public FormField(final TextBox box, final ControlGroup group) {
+      this.box = box;
+
+      box.addKeyUpHandler(new KeyUpHandler() {
+        public void onKeyUp(KeyUpEvent event) {
+          if (box.getText().length() > 0) {
+            group.setType(ControlGroupType.NONE);
+          }
+        }
+      });
+
+      this.group = group;
+    }
+
+    public String getText() { return box.getText(); }
+  }
+
+  protected FormField addControlFormField(Panel dialogBox, String label) {
+    return addControlFormField(dialogBox, label, false);
+  }
+
+  protected FormField addControlFormField(Panel dialogBox, String label, boolean isPassword) {
+    final TextBox user = isPassword ? new PasswordTextBox() : new TextBox();
+
+    return getFormField(dialogBox, label, user);
+  }
+
+  private FormField getFormField(Panel dialogBox, String label, TextBox user) {
+    final ControlGroup userGroup = addControlGroupEntry(dialogBox, label, user);
+    return new FormField(user, userGroup);
+  }
+
+  protected ListBoxFormField getListBoxFormField(Panel dialogBox, String label, ListBox user) {
+    final ControlGroup userGroup = addControlGroupEntry(dialogBox, label, user);
+    return new ListBoxFormField(user, userGroup);
+  }
+
+  private ControlGroup addControlGroupEntry(Panel dialogBox, String label, Widget user) {
+    final ControlGroup userGroup = new ControlGroup();
+    userGroup.add(new ControlLabel(label));
+    dialogBox.add(userGroup);
+    userGroup.add(user);
+    userGroup.addStyleName("leftFiveMargin");
+    return userGroup;
+  }
+
+  protected static class ListBoxFormField {
+    public final ListBox box;
+    //public final ControlGroup group;
+
+    public ListBoxFormField(ListBox box, ControlGroup group) {
+      this.box = box;
+      //this.group = group;
+    }
+  }
+
+
+  protected void markError(FormField dialectGroup, String message) {
+    markError(dialectGroup.group, dialectGroup.box, "Try Again", message);
+  }
+
+  protected void markError(ControlGroup dialectGroup, FocusWidget dialect, String header, String message) {
+    dialectGroup.setType(ControlGroupType.ERROR);
+    dialect.setFocus(true);
+
+    setupPopover(dialect, header, message);
+  }
+
+  protected void setupPopover(final Widget w, String heading, final String message) {
+    // System.out.println("triggering popover on " + w + " with " + message);
+    final Popover popover = new Popover();
+    popover.setWidget(w);
+    popover.setText(message);
+    popover.setHeading(heading);
+    popover.setPlacement(Placement.RIGHT);
+    popover.reconfigure();
+    popover.show();
+
+    Timer t = new Timer() {
+      @Override
+      public void run() {
+        //System.out.println("hide popover on " + w + " with " + message);
+        popover.hide();
+      }
+    };
+    t.schedule(3000);
+  }
+
+  private HandlerRegistration keyHandler;
+
+  protected void addKeyHandler(final Button send) {
+    keyHandler = Event.addNativePreviewHandler(new
+                                                 Event.NativePreviewHandler() {
+
+                                                   @Override
+                                                   public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
+                                                     NativeEvent ne = event.getNativeEvent();
+                                                     int keyCode = ne.getKeyCode();
+
+                                                     boolean isEnter = keyCode == KeyCodes.KEY_ENTER;
+
+                                                     //   System.out.println("key code is " +keyCode);
+                                                     if (isEnter && event.getTypeInt() == 512 &&
+                                                       "[object KeyboardEvent]".equals(ne.getString())) {
+                                                       ne.preventDefault();
+                                                       send.fireEvent(new ButtonClickEvent());
+                                                     }
+                                                   }
+                                                 });
+    System.out.println("UserManager.addKeyHandler made click handler " + keyHandler);
+  }
+
+  private class ButtonClickEvent extends ClickEvent {
+        /*To call click() function for Programmatic equivalent of the user clicking the button.*/
+  }
+
+  protected void removeKeyHandler() {
+    System.out.println("UserManager.removeKeyHandler : " + keyHandler);
+
+    if (keyHandler != null) keyHandler.removeHandler();
+  }
+
+
+  protected ListBox getGenderBox() {
+    final ListBox genderBox = new ListBox(false);
+    for (String s : Arrays.asList("Male", "Female")) {
+      genderBox.addItem(s);
+    }
+    genderBox.ensureDebugId("cwListBox-dropBox");
+    return genderBox;
+  }
+
+
+  protected ListBox getExperienceBox() {
+    final ListBox experienceBox = new ListBox(false);
+    List<String> choices = EXPERIENCE_CHOICES;
+    for (String c : choices) {
+      experienceBox.addItem(c);
+    }
+    experienceBox.ensureDebugId("cwListBox-dropBox");
+    return experienceBox;
+  }
+
+  protected boolean checkPassword(FormField password) {
+    return checkPassword(password.box);
+  }
+
+  private boolean checkPassword(TextBox password) {
+    String trim = password.getText().trim();
+    return trim.equalsIgnoreCase(GRADING) || trim.equalsIgnoreCase(TESTING);
+  }
+}
