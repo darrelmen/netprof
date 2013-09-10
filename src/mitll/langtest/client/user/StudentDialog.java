1a959c9f10c6edd03e17addae478144986f313ee
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-09-10 19:33:43 -0400

    updated student login
    Former-commit-id: 822514ac9f52bbfa5be2a69be27d4e38ade7795a [formerly 7bd4b4719c53edf3bac75c27e1f2bdeec4412f9f] [formerly 2dd1412ddcff9b7fde057bfd151a06df43373ad3 [formerly 6fcf2b776a33c41359eb41d506b11b425cf72e5e]]
    Former-commit-id: 9b5bbf3372008f754075dadc8303540581e329b1 [formerly 1dc4401b2921bac3f830050ef4a487c0a0434ac4] [formerly 1b1d1d3b13f53357627d878ac2b14f9a8914976f [formerly 1b4db0105ca6006644dff5e7d99eaf9d8ce1ea13]]
    Former-commit-id: b4ff79a64f9cccab61499609168e8e27ef1ab367 [formerly bf50687dac8f838bce60b94012c513c032a29ef6]
    Former-commit-id: 6a96f93fd52153a94c80bf70ca34b98530ebed03

--- src/mitll/langtest/client/user/StudentDialog.java
+++ src/mitll/langtest/client/user/StudentDialog.java
@@ -1,6 +1,12 @@
 package mitll.langtest.client.user;
 
 import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.CheckBox;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.ControlGroup;
+import com.github.gwtbootstrap.client.ui.ControlLabel;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.Label;
 import com.github.gwtbootstrap.client.ui.ListBox;
 import com.github.gwtbootstrap.client.ui.Modal;
 import com.github.gwtbootstrap.client.ui.TextBox;
@@ -16,8 +22,12 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.PropertyHandler;
+import mitll.langtest.shared.DLIUser;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,10 +37,12 @@ import mitll.langtest.client.PropertyHandler;
  * To change this template use File | Settings | File Templates.
  */
 public class StudentDialog extends UserDialog {
+  private static final int MIN_WEEKS = 1;
+  private static final int MAX_WEEKS = 104;
   private final UserManager userManager;
 
-  public StudentDialog(LangTestDatabaseAsync service,PropertyHandler props, UserManager userManager) {
-    super(service,props);
+  public StudentDialog(LangTestDatabaseAsync service, PropertyHandler props, UserManager userManager) {
+    super(service, props);
     this.userManager = userManager;
   }
 
@@ -39,19 +51,55 @@ public class StudentDialog extends UserDialog {
    */
   public void displayLoginBox() {
     final Modal dialogBox = getDialog();
+    dialogBox.setMaxHeigth("650px");
+    dialogBox.setHeight("600px");
     dialogBox.setTitle("Login Questions");
-    final FormField dialectGroup, ageEntryGroup;
-    final ListBoxFormField genderGroup/*, experienceGroup*/;
-    ageEntryGroup = addControlFormField(dialogBox, "Your age");
-    genderGroup = getListBoxFormField(dialogBox, "Gender", getGenderBox());
 
-/*    ControlGroup expGroup = new ControlGroup();
-    expGroup.add(new ControlLabel("Select months of experience"));
-    final ListBox experienceBox = getExperienceBox();
-    expGroup.add(experienceBox);
-    dialogBox.add(expGroup);*/
+    final FormField ageEntryGroup = addControlFormField(dialogBox, "Your age");
+    final ListBoxFormField genderGroup = getListBoxFormField(dialogBox, "Gender", getGenderBox());
+    final FormField weeks = addControlFormField(dialogBox, "Weeks of Experience");
 
-    dialectGroup = addControlFormField(dialogBox, "Dialect");
+    final ControlGroup ilrLevel = new ControlGroup();
+    ilrLevel.addStyleName("leftFiveMargin");
+    ilrLevel.add(new ControlLabel("Select your ILR level (check boxes below if estimating)"));
+    dialogBox.add(ilrLevel);
+
+    FluidRow row = new FluidRow();
+    Column column = new Column(2,new HTML());
+    row.add(column);
+
+    List<String> levels = java.util.Arrays.asList("0+", "1", "1+", "2", "2+", "3", "3+", "4");
+    Column c1 = new Column(2);     row.add(c1);
+    Column c2 = new Column(2);     row.add(c2);
+    Column c3 = new Column(2);     row.add(c3);
+    Column c4 = new Column(2);     row.add(c4);
+
+    ListBox listBox = getListBox2(levels);
+    final ListBoxFormField reading = getListBoxFormField(c1, "Reading", listBox);
+    final ListBoxFormField listening = getListBoxFormField(c2, "Listening", getListBox2(levels));
+    final ListBoxFormField speaking = getListBoxFormField(c3, "Speaking", getListBox2(levels));
+    final ListBoxFormField writing = getListBoxFormField(c4, "Writing", getListBox2(levels));
+
+    dialogBox.add(row);
+
+    FluidRow row2 = new FluidRow();
+
+    Column cc0 = new Column(2,new HTML("Estimating:"));
+    row2.add(cc0);
+
+    final  CheckBox rilr = new CheckBox(); rilr.addStyleName("leftThirtyMargin");
+    final  CheckBox lilr = new CheckBox(); lilr.addStyleName("leftThirtyMargin");
+    final  CheckBox silr = new CheckBox(); silr.addStyleName("leftThirtyMargin");
+    final CheckBox wilr = new CheckBox();  wilr.addStyleName("leftThirtyMargin");
+    Column cc1 = new Column(2, rilr);     row2.add(cc1);
+    Column cc2 = new Column(2, lilr);     row2.add(cc2);
+    Column cc3 = new Column(2, silr);     row2.add(cc3);
+    Column cc4 = new Column(2, wilr);     row2.add(cc4);
+
+    dialogBox.add(row2);
+
+    final FormField dialectGroup = addControlFormField(dialogBox, "Dialect");
+    dialectGroup.box.addStyleName("topMargin");
 
     dialectGroup.box.addKeyUpHandler(new KeyUpHandler() {
       public void onKeyUp(KeyUpEvent event) {
@@ -72,16 +120,19 @@ public class StudentDialog extends UserDialog {
        * Fired when the user clicks on the sendButton.
        */
       public void onClick(ClickEvent event) {
-        if (highlightAgeBox(ageEntryGroup)) {
-          if (dialectGroup.getText().isEmpty()) {
-            markError(dialectGroup, "Please enter a language dialect.");
+        if (highlightIntegerBox(ageEntryGroup)) {
+          if (highlightIntegerBox(weeks, MIN_WEEKS, MAX_WEEKS)) {
+            if (dialectGroup.getText().isEmpty()) {
+              markError(dialectGroup, "Please enter a language dialect.");
+            } else {
+              dialogBox.hide();
+              sendNameToServer();
+            }
           } else {
-            dialogBox.hide();
-            sendNameToServer();
+            markError(weeks, "Please enter weeks between " +MIN_WEEKS + " and " + MAX_WEEKS + ".");
           }
-        }
-        else {
-          markError(ageEntryGroup,"Please enter age between " + MIN_AGE + " and " + MAX_AGE+".");
+        } else {
+          markError(ageEntryGroup, "Please enter age between " + MIN_AGE + " and " + MAX_AGE + ".");
         }
       }
 
@@ -89,18 +140,15 @@ public class StudentDialog extends UserDialog {
        * Send the name from the nameField to the server and wait for a response.
        */
       private void sendNameToServer() {
-      /*  int monthsOfExperience = experienceBox.getSelectedIndex() * 3;
-        if (experienceBox.getSelectedIndex() == EXPERIENCE_CHOICES.size() - 1) {
-          monthsOfExperience = NATIVE_MONTHS;
-        }*/
-        addUser(0, ageEntryGroup.box, genderGroup.box, dialectGroup.box);
+       int weeksValue = Integer.parseInt(weeks.getText());
+        addUser(0, ageEntryGroup.box, genderGroup.box, dialectGroup.box, weeksValue,
+          reading.getValue(), listening.getValue(), speaking.getValue(), writing.getValue(),
+          rilr.getValue(), lilr.getValue(), silr.getValue(), wilr.getValue());
       }
     }
 
     // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    closeButton.addClickHandler(handler);
-
+    closeButton.addClickHandler(new MyHandler());
 
     dialogBox.addHiddenHandler(new HiddenHandler() {
       @Override
@@ -119,44 +167,83 @@ public class StudentDialog extends UserDialog {
     dialogBox.show();
   }
 
+  protected ListBox getListBox2(List<String> values) {
+    final ListBox genderBox = new ListBox(false);
+    for (String s : values) {
+      genderBox.addItem(s);
+    }
+    genderBox.setWidth("60px");
+    // genderBox.ensureDebugId("cwListBox-dropBox");
+    return genderBox;
+  }
 
 
   /**
-   * @see #displayLoginBox()
    * @param monthsOfExperience
    * @param ageEntryBox
    * @param genderBox
    * @param dialectBox
+   * @see #displayLoginBox()
    */
-  private void addUser(int monthsOfExperience, TextBox ageEntryBox, ListBox genderBox, TextBox dialectBox) {
+  private void addUser(int monthsOfExperience, TextBox ageEntryBox, ListBox genderBox, TextBox dialectBox,
+                      final int weeksOfExperience,
+                      final  String reading, final String listening, final  String speaking,  final String writing,
+                      final  boolean rEst, final  boolean lEst, final  boolean sEst, final  boolean wEst) {
     int age = getAge(ageEntryBox);
     String gender = genderBox.getValue(genderBox.getSelectedIndex());
-    addUser(age, gender, monthsOfExperience, dialectBox.getText(), PropertyHandler.LOGIN_TYPE.STUDENT);
+
+    AsyncCallback<Long> async = new AsyncCallback<Long>() {
+      public void onFailure(Throwable caught) {
+        Window.alert("addUser : Couldn't contact server.");
+      }
+
+      public void onSuccess(Long result) {
+        System.out.println("addUser : server result is " + result);
+        userManager.storeUser(result, "", "" + result, PropertyHandler.LOGIN_TYPE.STUDENT);
+
+        DLIUser dliUser = new DLIUser(result, weeksOfExperience,
+          new DLIUser.ILRLevel(reading,rEst),
+          new DLIUser.ILRLevel(listening,lEst),
+          new DLIUser.ILRLevel(speaking,sEst),
+          new DLIUser.ILRLevel(writing,wEst)
+          );
+
+        service.addDLIUser(dliUser, new AsyncCallback<Void>() {
+          @Override
+          public void onFailure(Throwable caught) {
+            Window.alert("addDLIUser : Couldn't contact server.");
+          }
+
+          @Override
+          public void onSuccess(Void result) {
+          }
+        });
+      }
+    };
+    addUser(age, gender, monthsOfExperience, dialectBox.getText(), async);
   }
 
   /**
-   * @see UserManager#addAnonymousUser()
    * @param age
    * @param gender
    * @param monthsOfExperience
+   * @see UserManager#addAnonymousUser()
    */
   public void addUser(int age, String gender, int monthsOfExperience) {
     addUser(age, gender, monthsOfExperience, "", PropertyHandler.LOGIN_TYPE.ANONYMOUS);
   }
 
   /**
-   * @see #addUser(int, String, int)
    * @param age
    * @param gender
    * @param monthsOfExperience
    * @param dialect
    * @param loginType
+   * @see #addUser(int, String, int)
    */
   private void addUser(int age, String gender, int monthsOfExperience, String dialect,
                        final PropertyHandler.LOGIN_TYPE loginType) {
-    service.addUser(age,
-      gender,
-      monthsOfExperience, dialect, new AsyncCallback<Long>() {
+    AsyncCallback<Long> async = new AsyncCallback<Long>() {
       public void onFailure(Throwable caught) {
         Window.alert("addUser : Couldn't contact server.");
       }
@@ -165,7 +252,14 @@ public class StudentDialog extends UserDialog {
         System.out.println("addUser : server result is " + result);
         userManager.storeUser(result, "", "" + result, loginType);
       }
-    });
+    };
+    addUser(age, gender, monthsOfExperience, dialect, async);
+  }
+
+  private void addUser(int age, String gender, int monthsOfExperience, String dialect, AsyncCallback<Long> async) {
+    service.addUser(age,
+      gender,
+      monthsOfExperience, dialect, async);
   }
 
   private Button makeCloseButton() {
@@ -177,7 +271,15 @@ public class StudentDialog extends UserDialog {
     return closeButton;
   }
 
-  private boolean highlightAgeBox(FormField ageEntryGroup) {
+  private boolean highlightIntegerBox(FormField ageEntryGroup) {
+    return highlightIntegerBox(ageEntryGroup, MIN_AGE, MAX_AGE, TEST_AGE);
+  }
+
+  private boolean highlightIntegerBox(FormField ageEntryGroup, int min, int max) {
+    return highlightIntegerBox(ageEntryGroup, MIN_AGE, MAX_AGE, Integer.MAX_VALUE);
+  }
+
+  private boolean highlightIntegerBox(FormField ageEntryGroup, int min, int max, int exception) {
     String text = ageEntryGroup.box.getText();
     boolean validAge = false;
     if (text.length() == 0) {
@@ -185,7 +287,7 @@ public class StudentDialog extends UserDialog {
     } else {
       try {
         int age = Integer.parseInt(text);
-        validAge = (age > MIN_AGE && age < MAX_AGE) || age == TEST_AGE;
+        validAge = (age > min && age < max) || age == exception;
         ageEntryGroup.group.setType(validAge ? ControlGroupType.NONE : ControlGroupType.ERROR);
       } catch (NumberFormatException e) {
         ageEntryGroup.group.setType(ControlGroupType.ERROR);
