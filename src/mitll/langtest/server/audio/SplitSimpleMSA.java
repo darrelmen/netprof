085482cfda6a85fb9184041f677564e0efb8d61e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-09-28 00:40:26 -0400

    added soundmanager2.js, missingSlow & missingFast for pashto, steps toward user lists
    Former-commit-id: 5d6754eab7547ec7f87ef62d47f2d993370e7dac [formerly ab8d38f4186258089af7bbe5bf997eb61794edcd] [formerly aeef6ab192163d8b8aacc6a664b4e40e06faa3da [formerly d898abef363e6b41e63fa582527af22bf487cd0d]]
    Former-commit-id: 110f5c05350cb6043caa89c825e3f781d698d6d9 [formerly b873e6920f87da08cf8108b77ee916fd9c38d305]
    Former-commit-id: 0383cadda463223310423caf30a31c295282d42f

--- src/mitll/langtest/server/audio/SplitSimpleMSA.java
+++ src/mitll/langtest/server/audio/SplitSimpleMSA.java
@@ -30,6 +30,7 @@ import java.util.concurrent.Future;
  * To change this template use File | Settings | File Templates.
  */
 public class SplitSimpleMSA extends SplitAudio {
+  public static final float LOW_SCORE_THRESHOLD = 0.2f;
   private static Logger logger = Logger.getLogger(SplitSimpleMSA.class);
   protected static final float MSA_MIN_SCORE = 0.2f;
 
@@ -176,7 +177,7 @@ public class SplitSimpleMSA extends SplitAudio {
       }
       File refDirForExercise = new File(newRefDir, name);
       refDirForExercise.mkdir();
-      if (valid && hydecScore > LOW_SCORE_THRESHOLD) {
+      if (valid && hydecScore > Mandarin.LOW_SCORE_THRESHOLD) {
         FastAndSlow consistent = writeTheTrimmedFiles(refDirForExercise, parent, (float) durationInSeconds, testAudioFileNoSuffix,
           alignments, true);
 
