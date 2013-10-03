eeda8fd3b08170d6bc19c66f91daf0717d99b27e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-03 14:31:55 -0400

    added simple login
    Former-commit-id: a6e95c60389d30e869320cc97b52599798eb5f6f [formerly f3049f8ee257bc605216da21a9aef9d1c7cd648a] [formerly 243c1e5e2f8e15bfba0461849cec947cf377426c]
    Former-commit-id: 333b5b10a812155e9f8874770056de208056d8c2 [formerly d81132ba445f86c32aceb62d7c818917e3113f68]
    Former-commit-id: 7b79254914d8036bffbbc9d848c3cec71bd6f08b

--- src/mitll/langtest/client/user/BasicDialog.java
+++ src/mitll/langtest/client/user/BasicDialog.java
@@ -4,18 +4,18 @@ import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.ControlGroup;
 import com.github.gwtbootstrap.client.ui.ControlLabel;
 import com.github.gwtbootstrap.client.ui.ListBox;
+import com.github.gwtbootstrap.client.ui.Modal;
 import com.github.gwtbootstrap.client.ui.PasswordTextBox;
 import com.github.gwtbootstrap.client.ui.Popover;
 import com.github.gwtbootstrap.client.ui.TextBox;
+import com.github.gwtbootstrap.client.ui.constants.ControlGroupType;
 import com.github.gwtbootstrap.client.ui.constants.Placement;
-import com.google.gwt.dom.client.NativeEvent;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.dialog.EnterKeyButtonHelper;
 
 import java.util.List;
 
@@ -27,7 +27,7 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class BasicDialog {
-  private HandlerRegistration keyHandler;
+  private EnterKeyButtonHelper enterKeyButtonHelper = new EnterKeyButtonHelper();
 
   protected FormField addControlFormField(Panel dialogBox, String label) {
     return addControlFormField(dialogBox, label, false);
@@ -79,34 +79,14 @@ public class BasicDialog {
     t.schedule(3000);
   }
 
-  protected void addKeyHandler(final Button send) {
-    keyHandler = Event.addNativePreviewHandler(new
-                                                 Event.NativePreviewHandler() {
-
-                                                   @Override
-                                                   public void onPreviewNativeEvent(Event.NativePreviewEvent event) {
-                                                     NativeEvent ne = event.getNativeEvent();
-                                                     int keyCode = ne.getKeyCode();
-
-                                                     boolean isEnter = keyCode == KeyCodes.KEY_ENTER;
-
-                                                     //   System.out.println("key code is " +keyCode);
-                                                     if (isEnter && event.getTypeInt() == 512 &&
-                                                       "[object KeyboardEvent]".equals(ne.getString())) {
-                                                       ne.preventDefault();
-                                                       send.fireEvent(new ButtonClickEvent());
-                                                     }
-                                                   }
-                                                 });
-    System.out.println("UserManager.addKeyHandler made click handler " + keyHandler);
+  protected void addKeyHandler(final Button send, Modal dialog) {
+    enterKeyButtonHelper.addKeyHandler(send,dialog);
   }
 
-  protected void removeKeyHandler() {
-    System.out.println("UserManager.removeKeyHandler : " + keyHandler);
-
-    if (keyHandler != null) keyHandler.removeHandler();
+ /* protected void removeKeyHandler() {
+    enterKeyButtonHelper.removeKeyHandler();
   }
-
+*/
   protected ListBox getListBox(List<String> values) {
     final ListBox genderBox = new ListBox(false);
     for (String s : values) {
@@ -116,7 +96,36 @@ public class BasicDialog {
     return genderBox;
   }
 
-  private class ButtonClickEvent extends ClickEvent {
-        /*To call click() function for Programmatic equivalent of the user clicking the button.*/
+  protected void markError(FormField dialectGroup, String message) {
+    markError(dialectGroup.group, dialectGroup.box, "Try Again", message);
+  }
+
+  protected void markError(ControlGroup dialectGroup, FocusWidget dialect, String header, String message) {
+    dialectGroup.setType(ControlGroupType.ERROR);
+    dialect.setFocus(true);
+
+    setupPopover(dialect, header, message);
+  }
+
+  protected boolean highlightIntegerBox(FormField ageEntryGroup, int min, int max) {
+    return highlightIntegerBox(ageEntryGroup, min, max, Integer.MAX_VALUE);
+  }
+
+  protected boolean highlightIntegerBox(FormField ageEntryGroup, int min, int max, int exception) {
+    String text = ageEntryGroup.box.getText();
+    boolean validAge = false;
+    if (text.length() == 0) {
+      ageEntryGroup.group.setType(ControlGroupType.WARNING);
+    } else {
+      try {
+        int age = Integer.parseInt(text);
+        validAge = (age >= min && age < max) || age == exception;
+        ageEntryGroup.group.setType(validAge ? ControlGroupType.NONE : ControlGroupType.ERROR);
+      } catch (NumberFormatException e) {
+        ageEntryGroup.group.setType(ControlGroupType.ERROR);
+      }
+    }
+
+    return validAge;
   }
 }
