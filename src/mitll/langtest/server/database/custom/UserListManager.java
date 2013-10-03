133763e9c8d2e2770a6bf95835c2bbc7d1117dbf
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-03 17:50:00 -0400

    does basic viewing of list set, current list view
    Former-commit-id: 16acccdd71a4cff6d0d7dc54d8d8018d7f71d94e [formerly ecc6a2ebd4b2a5df9cab1ab4fd0d971f823db41f] [formerly e4009e479e75f1b801d1769df5f3ac65bb3bbdb7]
    Former-commit-id: 671ab535bc52d3f5ef55dd2f16c0b88981fe3f16 [formerly b018b81206d1a04480c04e8c9638d8a08b59200b]
    Former-commit-id: 8cde085f5534554dbdb3702031377e372d1238bf

--- src/mitll/langtest/server/database/custom/UserListManager.java
+++ src/mitll/langtest/server/database/custom/UserListManager.java
@@ -9,6 +9,8 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -27,21 +29,52 @@ public class UserListManager {
   List<UserList> userLists = new ArrayList<UserList>();
 
   public UserListManager(UserDAO userDAO) { this.userDAO = userDAO; }
+
   public int addUserList(long userid, String name, String description, String dliClass) {
+    UserList userList = getUserList(userid, name, description, dliClass);
+    if (userList == null) return -1;
+    else return userList.getUniqueID();
+  }
+
+  public UserList getUserList(long userid, String name, String description, String dliClass) {
     User userWhere = userDAO.getUserWhere(userid);
-    UserList e = new UserList(i++, userWhere, name, description, dliClass);
-    userLists.add(e);
-    logger.debug("now there are " + userLists.size() + " lists for " + userid);
-    return e.getUniqueID();
+    if (userWhere == null) {
+      logger.error("huh? no user with id " + userid);
+      return null;
+    } else {
+      UserList e = new UserList(i++, userWhere, name, description, dliClass);
+      userLists.add(e);
+      logger.debug("now there are " + userLists.size() + " lists for " + userid);
+      return e;
+    }
   }
 
   public Collection<UserList> getListsForUser(int userid) {
+    if (userid == -1) return Collections.emptyList();
+    logger.debug("getListsForUser for " + userid);
+
     List<UserList> listsForUser = new ArrayList<UserList>();
     for (UserList userList : userLists) {
       if (userList.getCreator().id == userid) {
         listsForUser.add(userList);
       }
     }
+
+    if (listsForUser.isEmpty()) {
+      UserList userList = getUserList(userid, "My List", "Default list", "Choose a class");
+      if (userList == null) return Collections.emptyList();
+      else return Collections.singletonList(userList);
+    }
+
+    Collections.sort(userLists,new Comparator<UserList>() {
+      @Override
+      public int compare(UserList o1, UserList o2) {
+        return o1.getModified() > o2.getModified() ? -1 : o1.getModified() < o2.getModified()? +1 : 0;  //To change body of implemented methods use File | Settings | File Templates.
+      }
+    });
+
+    logger.debug("getListsForUser " + listsForUser.size() + " for " + userid);
+
     return listsForUser;
   }
 
