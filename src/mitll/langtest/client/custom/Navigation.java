cf282280126ed81d6b2a81148edf4e9a60ad2826
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-03 17:50:00 -0400

    does basic viewing of list set, current list view
    Former-commit-id: 8953c91a778c912694112baa1090b9f817ae8305 [formerly 20e0540243bd4e7590b0b30e46a88a1a380daf46] [formerly 9ec91007ec8976c46e7307b4090d9551aae620fa]
    Former-commit-id: 2eee87fe00a2c66b95d0f38ddb758231452a8415 [formerly 6b991394d7ea5a1252ff7d3bf85471e93baac5eb] [formerly 76ce054404332673efc8477eb9c887e0484542b9]
    Former-commit-id: d0dbc9cf7accf5c24805e431221ce4916e2eb5f9
    Former-commit-id: d6cd4f110d6b4c6cc136b30428ecef5bd23562a0

--- src/mitll/langtest/client/custom/Navigation.java
+++ src/mitll/langtest/client/custom/Navigation.java
@@ -4,21 +4,35 @@ import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.FluidContainer;
 import com.github.gwtbootstrap.client.ui.FluidRow;
 import com.github.gwtbootstrap.client.ui.Heading;
+import com.github.gwtbootstrap.client.ui.Icon;
+import com.github.gwtbootstrap.client.ui.Image;
 import com.github.gwtbootstrap.client.ui.TextArea;
 import com.github.gwtbootstrap.client.ui.constants.ButtonType;
 import com.github.gwtbootstrap.client.ui.constants.IconType;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RequiresResize;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTest;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.dialog.EnterKeyButtonHelper;
 import mitll.langtest.client.exercise.ExerciseController;
-import mitll.langtest.client.exercise.PagingContainer;
 import mitll.langtest.client.user.BasicDialog;
 import mitll.langtest.client.user.FormField;
 import mitll.langtest.client.user.UserManager;
-import mitll.langtest.shared.ExerciseShell;
 import mitll.langtest.shared.custom.UserList;
 
 import java.util.Collection;
@@ -30,63 +44,120 @@ import java.util.Collection;
  * Time: 8:50 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Navigation extends BasicDialog {
-  private final ExerciseController controller;
+public class Navigation extends BasicDialog implements RequiresResize {
+  public static final ButtonType UNCLICKED = ButtonType.INFO;
+  //private final ExerciseController controller;
   LangTestDatabaseAsync service;
   UserManager userManager;
   int userListID;
+  Button yourItems;
+  Button lastClicked = null;
+  ScrollPanel listScrollPanel;
+
   public Navigation(LangTestDatabaseAsync service, UserManager userManager, ExerciseController controller) {
     this.service = service;
    this.userManager = userManager;
-    this.controller = controller;
+  //  this.controller = controller;
   }
 
+  /**
+   * @see mitll.langtest.client.LangTest#onModuleLoad2
+   * @param thirdRow
+   * @return
+   */
   public Widget getNav(final Panel thirdRow) {
     FluidContainer container = new FluidContainer();
-    Button yourItems = new Button("Your Lessons", IconType.FOLDER_CLOSE);
+     yourItems = new Button("Your Lessons", IconType.FOLDER_CLOSE);
     yourItems.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
         // ask the server for your lessons
+        System.out.println("clicked on your items.");
+        selectTab(yourItems);
         viewLessons(thirdRow);
       }
     });
+    yourItems.setType(UNCLICKED);
     container.add(yourItems);
-    Button create = new Button("Create", IconType.PLUS_SIGN);
+    final Button create = new Button("Create", IconType.PLUS_SIGN);
     create.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
+        selectTab(create);
+
         doCreate(thirdRow);
         // TODO : add box to permit adding new entries
         // record audio --- how much feedback?
       }
     });
+    create.setType(UNCLICKED);
+
     container.add(create);
-    Button browse = new Button("Browse", IconType.TH_LIST);
+    final Button browse = new Button("Browse", IconType.TH_LIST);
     browse.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
+        selectTab(browse);
+
         // ask the server for all the lists that are known.
         // maybe have a typeahead box to search over lists and items
       }
     });
+    browse.setType(UNCLICKED);
+
     container.add(browse);
-    Button chapters = new Button("Chapters", IconType.BOOK);
+    final Button chapters = new Button("Chapters", IconType.BOOK);
     chapters.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
+
+        selectTab(chapters);
+
         // show traditional chapter widget
       }
     });
+    chapters.setType(UNCLICKED);
+
     container.add(chapters);
+   /* Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand () {
+      public void execute () {
+
+
+        System.out.println("fireEvent for your items.");
+
+       // yourItems.fireEvent(new ButtonClickEvent());
+      }
+    });*/
+
     return container;
   }
 
-  private void viewLessons(Panel thirdRow) {
+  private void selectTab(Button clickedOn) {
+    clickedOn.setType(ButtonType.PRIMARY);
+    if (lastClicked != null) lastClicked.setType(UNCLICKED);
+    lastClicked = clickedOn;
+  }
+
+  public void showInitialState() {
+    yourItems.fireEvent(new ButtonClickEvent());
+  }
+
+  @Override
+  public void onResize() {
+    setScrollPanelWidth(listScrollPanel);
+  }
+
+  private class ButtonClickEvent extends ClickEvent {
+        /*To call click() function for Programmatic equivalent of the user clicking the button.*/
+  }
+
+  private void viewLessons(final Panel thirdRow) {
     thirdRow.clear();
+    thirdRow.getElement().setId("thirdRow");
+
     final FluidContainer child = new FluidContainer();
     thirdRow.add(child);
-
+    child.addStyleName("exerciseBackground");
     service.getListsForUser(userManager.getUser(),new AsyncCallback<Collection<UserList>>() {
       @Override
       public void onFailure(Throwable caught) {
@@ -95,22 +166,168 @@ public class Navigation extends BasicDialog {
 
       @Override
       public void onSuccess(Collection<UserList> result) {
-        PagingContainer<UserList> userListPagingContainer = new PagingContainer<UserList>(controller);
+       // PagingContainer<UserList> userListPagingContainer = new PagingContainer<UserList>(controller);
 
 
-        FluidRow row = new FluidRow();
+        listScrollPanel = new ScrollPanel();
+/*
+        listScrollPanel.addStyleName("fullWidth");
+*/
 
-        Panel container = userListPagingContainer.addTableWithPager();
-        for (ExerciseShell item : result) {
-          userListPagingContainer.addExerciseToList2(item);
+        setScrollPanelWidth(listScrollPanel);
+        final FluidContainer insideScroll = new FluidContainer();
+        insideScroll.addStyleName("userListContainer");
+        listScrollPanel.add(insideScroll);
+        for (final UserList ul : result) {
+          final FocusPanel widgets = new FocusPanel();
+          widgets.addStyleName("userListContent");
+          widgets.addStyleName("userListBackground");
 
+          widgets.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+            //  Window.alert("Do something on select.");
+              showList(ul,thirdRow);
+            }
+          });
+          widgets.addMouseOverHandler(new MouseOverHandler() {
+            @Override
+            public void onMouseOver(MouseOverEvent event) {
+              widgets.removeStyleName("userListBackground");
+              widgets.addStyleName("blueBackground");
+              widgets.addStyleName("handCursor");
+              widgets.getElement().getStyle().setCursor(Style.Cursor.POINTER);
+            }
+          });
+          widgets.addMouseOutHandler(new MouseOutHandler() {
+            @Override
+            public void onMouseOut(MouseOutEvent event) {
+              widgets.removeStyleName("blueBackground");
+              widgets.addStyleName("userListBackground");
+              widgets.removeStyleName("handCursor");
+            }
+          });
+          insideScroll.add(widgets);
+          FluidContainer w = new FluidContainer();
+          widgets.add(w);
+
+          FluidRow r1 = new FluidRow();
+          w.add(r1);
+          r1.add(new Heading(2,"Title : "+ul.getName()));
+
+          r1 = new FluidRow();
+          w.add(r1);
+          r1.add(new Heading(3,"Description : "+ul.getDescription()));
+
+          r1 = new FluidRow();
+          w.add(r1);
+          r1.add(new Heading(3,"Class : "+ul.getClassMarker()));
+
+          if (createdByYou(ul)) {
+            r1 = new FluidRow();
+            w.add(r1);
+            r1.add(new HTML("<b>Created by you.</b>"));
+          }
         }
-        userListPagingContainer.flush();
-
-        row.add(container);
-        child.add(row);
+        child.add(listScrollPanel);
       }
     });
+  }
+
+  private boolean createdByYou(UserList ul) {
+    return ul.getCreator().id == userManager.getUser();
+  }
+
+  private void showList(UserList ul, Panel thirdRow) {
+    FluidContainer container = new FluidContainer();
+    thirdRow.clear();
+    thirdRow.add(container);
+    thirdRow.addStyleName("fullWidth2");
+
+    container.getElement().setId("showListContainer");
+    container.addStyleName("userListDarkerBlueColor");
+    container.addStyleName("fullWidth2");
+
+    FluidRow child = new FluidRow();
+    container.add(child);
+    //child.addStyleName("fullWidth2");
+
+    child.add(new Heading(1,ul.getName()));
+    boolean created = createdByYou(ul);
+    if (created) {
+      child = new FluidRow();
+      container.add(child);
+      child.add(new Heading(3,"<b>Created by you.</b>"));
+    }
+    FluidContainer operations = new FluidContainer();
+    operations.addStyleName("userListOperations");
+    FluidRow opRow = new FluidRow();
+    operations.add(opRow);
+/*
+    FocusPanel pair = new FocusPanel();
+
+
+    FlowPanel rowPair = new FlowPanel();
+
+    rowPair.add(new Image(LangTest.LANGTEST_IMAGES + "NewProF1.png"));
+    rowPair.add(new Heading(3,"Learn"));
+
+    operations.add(rowPair);
+    pair.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        Window.alert("Do npf");
+      }
+    });
+    //opRow.add(new Image(LangTest.LANGTEST_IMAGES + "NewProF1.png"));
+    opRow.add(pair);
+    opRow.add(new Image(LangTest.LANGTEST_IMAGES + "NewProF2.png"));
+
+*/
+
+
+    Button w = new Button("Learn Pronunciation", IconType.LIGHTBULB);
+    opRow.add(w);
+    w.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        Window.alert("do npf!");
+      }
+    });
+    Button practice = new Button("Practice", IconType.CHECK);
+    opRow.add(practice);
+    practice.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        Window.alert("do avp!");
+      }
+    });
+    if (created) {
+      Button w1 = new Button("Add Item", IconType.PLUS_SIGN);
+      opRow.add(w1);
+      w1.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          Window.alert("add item to list");
+        }
+      });
+      Button edit = new Button("Edit", IconType.EDIT);
+      opRow.add(edit);
+      edit.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          Window.alert("edit items");
+        }
+      });
+    }
+    container.add(operations);
+  }
+
+  private void setScrollPanelWidth(ScrollPanel row) {
+    if (row != null) {
+      row.setWidth((Window.getClientWidth() * 0.9) + "px");
+      row.setHeight((Window.getClientHeight() * 0.7) + "px");
+    }
   }
 
   private void doCreate(Panel thirdRow) {
@@ -119,6 +336,7 @@ public class Navigation extends BasicDialog {
     thirdRow.clear();
     FluidContainer child = new FluidContainer();
     thirdRow.add(child);
+    child.addStyleName("userListContainer");
 
     FluidRow row = new FluidRow();
     child.add(row);
@@ -168,12 +386,13 @@ public class Navigation extends BasicDialog {
           public void onSuccess(Integer result) {
             userListID = result;
             System.out.println("userListID " + userListID);
-
+            showInitialState();
             // TODO : show enter item panel
           }
         });
       }
     });
+    new EnterKeyButtonHelper(true).addKeyHandler(submit);
     // group = addControlGroupEntry(row, "Class", dliClass);
     row.add(submit);
   }
