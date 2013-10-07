bd0aae6e752d6970baba875d3a1a995629a33a87
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-07 00:54:13 -0400

    now does recording of a reference audio segment on add item
    Former-commit-id: 971c21422fd7da628a85cf9a66b31e56d22153d2 [formerly 6b6e9ead51bedd9af44c8554e944594ef46a392b] [formerly ca2243d5e3c9d3a03e91c0c3ee806315ad4feb50 [formerly 81749beb36613c4689ea9d4511faa021be894a3d]] [formerly 971c21422fd7da628a85cf9a66b31e56d22153d2 [formerly 6b6e9ead51bedd9af44c8554e944594ef46a392b] [formerly ca2243d5e3c9d3a03e91c0c3ee806315ad4feb50 [formerly 81749beb36613c4689ea9d4511faa021be894a3d]] [formerly 3676a574a64a94e3c97a19ae84d99a5d6364c7b8 [formerly fb042417ed84d81bc33674d53f2d45cc7ffcf2bd] [formerly ebb19ce19e58233a0b4df3d91c7f2c3869315b9f [formerly c355fc0a9066adc7e71367d64c0ac735ac81a61d]]]]
    Former-commit-id: af5505bb30605e78a31ad536baa8844343e970a8 [formerly a796ae7babe7543cd0d18aa201b3bb406e33c290] [formerly deba3ef508d42fce98cb1f2639e202cbe03e8694 [formerly 0e694c0c250b7aca20822e28614285a3e7e4652a]] [formerly 123474c066a7cad813c8fd7d375c076bc1ab05e8 [formerly 699a67b9a82ea527e6c229bcd4bbba95393eac1c] [formerly 7a9412ed19b131754bec3255c7a85c9860e0b692 [formerly c414ac6e41e753bf966b9bb74ea36ab317fa4cdb]]] [formerly 0ca95f774054188dff99b5696c8cd6d49f82b799 [formerly 62d664b070e3dc914d16587925c1eb6e2f3984df] [formerly 283f2540a8096cae48cd42384849f7adfe63ae69 [formerly 00976d2edddfd73f0af4b1ff28d8e68f0ab473e1]] [formerly 88f96e8366d58d27160f9676f8d4bbf1775808b2 [formerly ea9eb465faa475105978719658f4a6b656ae8219] [formerly 194357cc9f28a4b27caa99cc10aa16f15a9c0cd0 [formerly 53ff1ba43d41f3e186ed1e33c123be22207ed228]]]]
    Former-commit-id: 0ca95f774054188dff99b5696c8cd6d49f82b799 [formerly 62d664b070e3dc914d16587925c1eb6e2f3984df] [formerly 283f2540a8096cae48cd42384849f7adfe63ae69 [formerly 00976d2edddfd73f0af4b1ff28d8e68f0ab473e1]]
    Former-commit-id: 4ac3aee4300d0cb01e78c964b0bb300f91eae99e [formerly e2db57543a7515268d12307f84d0d5ba5ff4a72a] [formerly 9993900522fc1f95b72e5953d3d143a6aaf9edeb [formerly d0dcfeb3a47d986b6e18cd97f976b0012fba0871]]
    Former-commit-id: 20576842292ecee82f1351a5ad14b0939d68bfcc [formerly b0ec4f995c8bc5a66e608712e4efd92c388c669a]
    Former-commit-id: d2a3f00df6a8d04aa2d079bca24bc913c3d864e0

--- src/mitll/langtest/client/exercise/PagingExerciseList.java
+++ src/mitll/langtest/client/exercise/PagingExerciseList.java
@@ -17,46 +17,8 @@ import mitll.langtest.shared.ExerciseShell;
  * To change this template use File | Settings | File Templates.
  */
 public class PagingExerciseList extends ExerciseList implements RequiresResize {
-/*  private static final int MAX_LENGTH_ID = 27;
-  protected static final int PAGE_SIZE = 15;   // TODO : make this sensitive to vertical real estate?
-  private ListDataProvider<ExerciseShell> dataProvider;
-  private static final boolean DEBUG = false;
-  private static final int ID_LINE_WRAP_LENGTH = 20;
-  private static final int HEIGHT_OF_CELL_TABLE_WITH_15_ROWS = 390;
-  private static final float MAX_PAGES = 2f;
-  private static final int MIN_PAGE_SIZE = 3;
-  private static final float DEFAULT_PAGE_SIZE = 15f;
-  private CellTable<ExerciseShell> table;*/
   protected ExerciseController controller;
   protected PagingContainer<? extends ExerciseShell> pagingContainer;
-
-/*
-  public interface TableResources extends CellTable.Resources {
-    */
-/**
-     * The styles applied to the table.
-     *//*
-
-    interface TableStyle extends CellTable.Style {}
-
-    @Override
-    @Source({CellTable.Style.DEFAULT_CSS, "ExerciseCellTableStyleSheet.css"})
-    TableStyle cellTableStyle();
-  }
-
-  public interface RTLTableResources extends CellTable.Resources {
-    */
-/**
-     * The styles applied to the table.
-     *//*
-
-    interface TableStyle extends CellTable.Style {}
-
-    @Override
-    @Source({CellTable.Style.DEFAULT_CSS, "RTLExerciseCellTableStyleSheet.css"})
-    TableStyle cellTableStyle();
-  }
-*/
 
   /**
    * @see mitll.langtest.client.LangTest#makeExerciseList
@@ -75,7 +37,6 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
 
   protected void addComponents() {
     PagingContainer<? extends ExerciseShell> exerciseShellPagingContainer = makePagingContainer();
-    System.out.println("made container " + exerciseShellPagingContainer);
     addTableWithPager(exerciseShellPagingContainer);
   }
 
@@ -99,114 +60,9 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
   }
 
   protected void addTableWithPager(PagingContainer<? extends ExerciseShell> pagingContainer) {
-    System.out.println("addTableWithPager container " + pagingContainer);
-
-    Panel container = pagingContainer.addTableWithPager();
+    Panel container = pagingContainer.getTableWithPager();
     add(container);
- /*   makeCellTable();
-
-    // Create a data provider.
-    this.dataProvider = new ListDataProvider<ExerciseShell>();
-
-    // Connect the table to the data provider.
-    dataProvider.addDataDisplay(table);
-
-    // Create a SimplePager.
-    SimplePager pager = new SimplePager();
-
-    // Set the cellList as the display.
-    pager.setDisplay(table);
-
-    FlowPanel column = new FlowPanel();
-    add(column);
-    column.add(pager);
-    column.add(table);*/
   }
-
-/*  protected CellTable<Exercise> makeCellTable() {
-    CellTable.Resources o;
-
-    if (controller.isRightAlignContent()) {   // so when we truncate long entries, the ... appears on the correct end
-      o = GWT.create(RTLTableResources.class);
-    }
-    else {
-      o = GWT.create(TableResources.class);
-    }
-
-    this.table = new CellTable<ExerciseShell>(PAGE_SIZE, o);
-
-    table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.DISABLED);
-
-    //  table.setWidth("100%", true);
-    table.setWidth("100%");
-    table.setHeight("auto");
-
-    // Add a selection model to handle user selection.
-    final SingleSelectionModel<ExerciseShell> selectionModel = new SingleSelectionModel<ExerciseShell>();
-    table.setSelectionModel(selectionModel);
-    // we don't want to listen for changes in the selection model, since that happens on load too -- we just want clicks
-
-    addColumnsToTable(true);
-    return null;
-  }*/
-
-/*  protected void addColumnsToTable(boolean consumeClicks) {
-    Column<ExerciseShell, SafeHtml> id2 = getExerciseIdColumn(consumeClicks);
-
-    // this would be better, but want to consume clicks
-  *//*  TextColumn<ExerciseShell> id2 = new TextColumn<ExerciseShell>() {
-      @Override
-      public String getValue(ExerciseShell exerciseShell) {
-        String columnText =  exerciseShell.getTooltip();
-        if (columnText.length() > MAX_LENGTH_ID) columnText = columnText.substring(0,MAX_LENGTH_ID-3)+"...";
-
-        return columnText;
-      }
-    };*//*
-
-    id2.setCellStyleNames("alignLeft");
-    table.addColumn(id2);
-  }*/
-
-  /**
-   * @seex #addColumnsToTable
-   * @return
-   */
-/*  private Column<ExerciseShell, SafeHtml> getExerciseIdColumn(final boolean consumeClicks) {
-    return new Column<ExerciseShell, SafeHtml>(new SafeHtmlCell() {
-        @Override
-        public Set<String> getConsumedEvents() {
-          Set<String> events = new HashSet<String>();
-          if (consumeClicks) events.add(BrowserEvents.CLICK);
-          return events;
-        }
-      }) {
-        @Override
-        public SafeHtml getValue(ExerciseShell object) {
-          return getColumnToolTip(object.getTooltip());
-        }
-
-        @Override
-        public void onBrowserEvent(Cell.Context context, Element elem, ExerciseShell object, NativeEvent event) {
-          super.onBrowserEvent(context, elem, object, event);
-          if (BrowserEvents.CLICK.equals(event.getType())) {
-            System.out.println("getExerciseIdColumn.onBrowserEvent : got click " + event);
-            final ExerciseShell e = object;
-            if (isExercisePanelBusy()) {
-              tellUserPanelIsBusy();
-              markCurrentExercise(currentExercise);
-            } else {
-              gotClickOnItem(e);
-            }
-          }
-        }
-
-        private SafeHtml getColumnToolTip(String columnText) {
-          if (columnText.length() > MAX_LENGTH_ID) columnText = columnText.substring(0,MAX_LENGTH_ID-3)+"...";
-          return new SafeHtmlBuilder().appendHtmlConstant(columnText).toSafeHtml();
-        }
-      };
-  }*/
 
   protected void tellUserPanelIsBusy() {
     Window.alert("Please stop recording before changing items.");
@@ -215,8 +71,6 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
   protected String getHistoryToken(String id) { return "item=" +id; }
 
   protected void gotClickOnItem(final ExerciseShell e) {
-   // pushNewItem(e.getID());
-
     if (isExercisePanelBusy()) {
       tellUserPanelIsBusy();
       markCurrentExercise(currentExercise);
@@ -225,42 +79,17 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
     }
   }
 
-  /**
-   * @see SectionExerciseList.MySetExercisesCallback#onSuccess
-   */
- /* @Override
-  protected void loadFirstExercise() {
-    super.loadFirstExercise();
-    selectFirst();
-  }*/
-/*
-  protected void selectFirst() {
-    table.getSelectionModel().setSelected(currentExercises.get(0), true);
-    table.redraw();
-    onResize();
-  }*/
-
   public void clear() {
-   /* List<ExerciseShell> list = dataProvider.getList();
-    List<ExerciseShell> copy = new ArrayList<ExerciseShell>();
-
-    for (ExerciseShell es : list) copy.add(es);
-    for (ExerciseShell es : copy) list.remove(es);
-    table.setRowCount(list.size());*/
     pagingContainer.clear();
   }
 
   @Override
   public void flush() {
-/*    dataProvider.flush();
-    table.setRowCount(dataProvider.getList().size());*/
     pagingContainer.flush();
   }
 
   @Override
   protected void addExerciseToList(ExerciseShell exercise) {
-/*    List<ExerciseShell> list = dataProvider.getList();
-    list.add(exercise);*/
     pagingContainer.addExerciseToList2(exercise);
   }
 
@@ -268,45 +97,7 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
   public void onResize() {
     super.onResize();
     pagingContainer.onResize(currentExercise);
-/*    System.out.println("Got on resize " + Window.getClientHeight() + " " +
-        getOffsetHeight() + " bodyheight = " + table.getBodyHeight() + " table offset height " + table.getOffsetHeight() + " parent height " + getParent().getOffsetHeight());*/
-/*    int numRows = getNumTableRowsGivenScreenHeight();
-    if (table.getPageSize() != numRows) {
-      //System.out.println("num rows now " + numRows);
-      table.setPageSize(numRows);
-      table.redraw();
-      markCurrentExercise(currentExercise);
-    }*/
   }
-
-/*  protected int getNumTableRowsGivenScreenHeight() {
-    int header = getTableHeaderHeight();
-    int leftOver = Window.getClientHeight() - header - 100;
-
-    //System.out.println("Got on resize " + Window.getClientHeight() + " " + header + " result = " + leftOver);
-
-    float rawRatio = ((float) leftOver) / (float) heightOfCellTableWith15Rows();
-    float tableRatio = Math.min(MAX_PAGES, rawRatio);
-    // System.out.println("left over " + leftOver + " raw " + rawRatio + " table ratio " + tableRatio);
-
-    float ratio = DEFAULT_PAGE_SIZE * tableRatio;
-    if (currentExercises != null) {
-      ExerciseShell toLoad = currentExercises.get(0);
-
-      if (toLoad.getID().length() > ID_LINE_WRAP_LENGTH) {
-        ratio /= 2; // hack for long ids
-      }
-    }
-    return Math.max(MIN_PAGE_SIZE, Math.round(ratio));
-  }*/
-
-/*  protected int heightOfCellTableWith15Rows() {
-    return HEIGHT_OF_CELL_TABLE_WITH_15_ROWS;
-  }*/
-/*
-  protected int getTableHeaderHeight() {
-    return controller.getHeightOfTopRows();
-  }*/
 
   /**
    * not sure how this happens, but need Math.max(0,...)
@@ -318,32 +109,4 @@ public class PagingExerciseList extends ExerciseList implements RequiresResize {
   protected void markCurrentExercise(int i) {
    pagingContainer.markCurrentExercise(i);
  }
-   /*
-    if (currentExercises == null) return;
-    ExerciseShell itemToSelect = currentExercises.get(i);
-    if (DEBUG)  System.out.println(new Date() + " markCurrentExercise : Comparing selected " + itemToSelect.getID());
-    table.getSelectionModel().setSelected(itemToSelect, true);
-    if (DEBUG) {
-      int pageEnd = table.getPageStart() + table.getPageSize();
-      System.out.println("marking " +i +" out of " +table.getRowCount() + " page start " +table.getPageStart() +
-        " end " + pageEnd);
-    }
-
-    int pageNum = i / table.getPageSize();
-    int newIndex = pageNum *table.getPageSize();
-    if (i < table.getPageStart()) {
-      int newStart = Math.max(0, newIndex);//table.getPageStart() - table.getPageSize());
-      if (DEBUG) System.out.println("new start of prev page " +newStart + " vs current " + table.getVisibleRange());
-      table.setVisibleRange(newStart, table.getPageSize());
-    } else {
-      int pageEnd = table.getPageStart() + table.getPageSize();
-      if (i >= pageEnd) {
-        int newStart = Math.max(0,Math.min(table.getRowCount() - table.getPageSize(), newIndex));   // not sure how this happens, but need Math.max(0,...)
-        if (DEBUG) System.out.println("new start of next newIndex " +newStart + "/" +newIndex +"/page = " + pageNum+
-          " vs current " + table.getVisibleRange());
-        table.setVisibleRange(newStart, table.getPageSize());
-      }
-    }
-    table.redraw();
-  }*/
 }
