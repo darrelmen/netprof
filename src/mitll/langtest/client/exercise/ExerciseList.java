b37133e5dd24280dc553d6f804dd7a653dfd01d5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-08 18:59:59 -0400

    basically got flashcard integrated
    Former-commit-id: 6dc23a1111fb1889668a6cbcaa75970c1f355b82
    Former-commit-id: 3f8197d0f6049e278a6455f332a5c4df491f912b

--- src/mitll/langtest/client/exercise/ExerciseList.java
+++ src/mitll/langtest/client/exercise/ExerciseList.java
@@ -249,7 +249,7 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
       idToExercise.put(es.getID(),es);
       addExerciseToList(es);
     }
-    System.out.println("ExerciseList : map now " + idToExercise);
+    System.out.println("ExerciseList : map keys now " + idToExercise.keySet());
 
     flush();
   }
@@ -668,11 +668,14 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
   @Override
   public boolean onFirst(ExerciseShell current) {
     System.out.println("on first " + current);
+    if (current == null) return true;
     return currentExercises.size() == 1 || getIndex(current) == 0;
   }
 
   @Override
   public boolean onLast(ExerciseShell current) {
+    if (current == null) return true;
+
     return currentExercises.size() == 1 || isOnLastItem(getIndex(current));
 //    System.out.println("on last " + current + " = " + onLastItem);
 
