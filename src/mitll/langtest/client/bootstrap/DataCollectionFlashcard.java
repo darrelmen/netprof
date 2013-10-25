fabb2eaa81d2ba99a50ccf52cd8e0f720cbe715b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-25 15:54:46 -0400

    almost done with adding next and prev button and enhancing visual layout for flashcards
    Former-commit-id: c273e2b074df65ad351371a0977efe7c78af00e6 [formerly 5c9f8c6f80b196df19cc3aef2aff6422286de85d] [formerly e94f5c13c0a3f82852a967b55ab719719c6252a4 [formerly 6bdae6d836d8b6073d6f3c6bf8b895b7c7a492e4]]
    Former-commit-id: 22b262383517231d17c3ef93e5955f76942bd947 [formerly bdfc24aaa69566d7491a8c2484d31eb7671d99e6]
    Former-commit-id: a7f4472778a73bf89af8979ff34e7a85c4931fde

---
+++ src/mitll/langtest/client/bootstrap/DataCollectionFlashcard.java
@@ -0,0 +1,68 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.NavigationHelper;
+import mitll.langtest.shared.Exercise;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 10/25/13
+ * Time: 12:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataCollectionFlashcard extends BootstrapExercisePanel {
+  public DataCollectionFlashcard(Exercise e, LangTestDatabaseAsync service, ExerciseController controller) {
+    super(e, service, controller);
+    System.out.println("making new flashcard");
+
+    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand () {
+      public void execute () {
+        int offsetHeight = cardPrompt.getOffsetHeight();
+        DOM.setStyleAttribute(leftButtonContainer.getElement(), "marginTop", (offsetHeight / 2) + "px");
+        DOM.setStyleAttribute(rightButtonContainer.getElement(), "marginTop", ((offsetHeight/2)-50) +"px");
+      }
+    });
+  }
+
+  Panel leftButtonContainer, rightButtonContainer;
+
+  protected Widget getCardPrompt(Exercise e, ExerciseController controller) {
+    FluidRow questionRow = new FluidRow();
+    Widget questionContent = getQuestionContent(e, controller);
+    Column contentContainer = new Column(8, questionContent);
+    contentContainer.addStyleName("blueBackground");
+    contentContainer.addStyleName("userNPFContent");
+    contentContainer.addStyleName("topMargin");
+    contentContainer.addStyleName("marginBottomTen");
+    NavigationHelper navigationHelper = new NavigationHelper(e,controller,false);
+
+    Widget prev = navigationHelper.getPrev();
+    HorizontalPanel hp = getCenteredContainer(prev);
+    leftButtonContainer = hp;
+    questionRow.add(new Column(2, hp));
+    questionRow.add(contentContainer);
+    questionRow.add(new Column(2,rightButtonContainer = getCenteredContainer(navigationHelper.getNext())));
+
+    return questionRow;
+  }
+
+  private HorizontalPanel getCenteredContainer(Widget prev) {
+    HorizontalPanel hp = new HorizontalPanel();
+    hp.setHeight("100%");
+    hp.setWidth("100%");
+    hp.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
+    hp.setVerticalAlignment(HorizontalPanel.ALIGN_MIDDLE);
+    //hp.addStyleName("topMarginFifty");
+    hp.add(prev);
+    return hp;
+  }
+}
