35740ce49890b5aeaa11dac145bbf47a9a5bae59
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-10-28 21:09:49 -0400

    completed bug fixes for issue #10 -- except leaves next button highlighted
    Former-commit-id: 2f208a4abddafc3ab6ec6927a15af9c7b5e078dd [formerly 46bf042793ad74364ebe305b34a5895401af0aa6 [formerly 16ac85d28ae6b6e0b2c47ffc673387cb26803c9c] [formerly ad90dc377ba7c216dd90d72131f72a87cf2f4e54 [formerly a6f9b3d856c2502ac4aff38c9919b0068ec587b3]]] [formerly 2f208a4abddafc3ab6ec6927a15af9c7b5e078dd [formerly 46bf042793ad74364ebe305b34a5895401af0aa6 [formerly 16ac85d28ae6b6e0b2c47ffc673387cb26803c9c] [formerly ad90dc377ba7c216dd90d72131f72a87cf2f4e54 [formerly a6f9b3d856c2502ac4aff38c9919b0068ec587b3]]] [formerly 46bf042793ad74364ebe305b34a5895401af0aa6 [formerly 16ac85d28ae6b6e0b2c47ffc673387cb26803c9c] [formerly ad90dc377ba7c216dd90d72131f72a87cf2f4e54 [formerly a6f9b3d856c2502ac4aff38c9919b0068ec587b3]] [formerly 6a92a31aa437e06041dbdda8b311a1740b1f790a [formerly 081f622ccd05465352ac3abe88c40c937e15d989] [formerly 669f4f94754e466df4199f1627a63d0b210abb72 [formerly 8daba0e0346749bffa5fd500cec611b75872a1b1]]]]]
    Former-commit-id: df7f8378c3801669b28c11df3a6ae4e776dcbdf8 [formerly 80c285ce35283579171913b03a8208968626df44 [formerly 639a31125c3f3015b46b4d43515d6f68867ba991] [formerly 6e5ee5a5e176887cbe6730a95b2ee877682f0462 [formerly 4f9f31ddf9efdc78eab4bb31b8e869b301e12574]]] [formerly 80c285ce35283579171913b03a8208968626df44 [formerly 639a31125c3f3015b46b4d43515d6f68867ba991] [formerly 6e5ee5a5e176887cbe6730a95b2ee877682f0462 [formerly 4f9f31ddf9efdc78eab4bb31b8e869b301e12574]] [formerly 075df5783b434ef79ef4ad8ed6de64932f3c5ac6 [formerly 5b14f481a0fca30bcf4357bd119abd184b1f3336] [formerly ea71b8def63c7d056adde02bad5fb97044291183 [formerly 334f9c5c956b75e7429dc2df6b7c4e7a501ee2ff]]]] [formerly 76ec25ba1ea4a2e85cebdec5a03f9dd75178d2bb [formerly c5ed9688930976cad974e262e84085d574dbe0d3] [formerly 72ad1085926edab894f47e0e1c2a35947ec5cf4b [formerly 8360b8888bce085eb9b8217ef827db455a01a23f]] [formerly 76ec25ba1ea4a2e85cebdec5a03f9dd75178d2bb [formerly c5ed9688930976cad974e262e84085d574dbe0d3] [formerly 72ad1085926edab894f47e0e1c2a35947ec5cf4b [formerly 8360b8888bce085eb9b8217ef827db455a01a23f]] [formerly 44633e324facbcba7da056ce6b8d99842b26c0ac [formerly 41eb957e20ec56820c3ff142fad9647b865d4fbf] [formerly c0c5417f6aea54e33079cadcdf15ac2a9e72ffb8 [formerly 332fc51a9e096054f35d9614f490af5b40808676]]]]]
    Former-commit-id: 76ec25ba1ea4a2e85cebdec5a03f9dd75178d2bb [formerly c5ed9688930976cad974e262e84085d574dbe0d3] [formerly 72ad1085926edab894f47e0e1c2a35947ec5cf4b [formerly 8360b8888bce085eb9b8217ef827db455a01a23f]]
    Former-commit-id: 6596a56f54e5e4d2c40d1fa4f46b5ddf5c4c7aa3 [formerly bda496d8449706672c0f3f526277651cccc62498] [formerly 277f949da175802d90025d68bf8f8600280837ea [formerly c8c12023b42c70b6eacf8060d8bfd02e1b2edb0d]] [formerly 1dd75e41fcdd289e89cda8e6519e056799f6c57d [formerly 223f30828f9169d6646f1cfa1c42d6c6939c6eef] [formerly 5e681eb6d83d7022f2dce545f04eda6d079e1b36 [formerly 29c788394b392906f601e894d3979c33d6cf48d6]]]
    Former-commit-id: 5b2fb0b075e33e874608c1fc50e1d2c63cc32ece [formerly a1b6cb6a210f82c74ab003cbb5f1ef4c48b11ce2] [formerly 110bc9c93f5335d0f91e9253e5ef2404cbd9349d [formerly a0ca0cf08478671784da4d6a4773425dc6bf159c]]
    Former-commit-id: c65e51c1ab23127dfb63288f875fb6bbeba005aa [formerly 507a396f90de3fb8c97ba97a939082ba63105a44]
    Former-commit-id: 56cfb30171cf2b4cc2739a8b0056c0ccc393ff5e

--- src/mitll/langtest/client/bootstrap/DataCollectionFlashcard.java
+++ src/mitll/langtest/client/bootstrap/DataCollectionFlashcard.java
@@ -2,10 +2,19 @@ package mitll.langtest.client.bootstrap;
 
 import com.github.gwtbootstrap.client.ui.Column;
 import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.Heading;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.i18n.client.HasDirection;
+import com.google.gwt.i18n.shared.WordCountDirectionEstimator;
+import com.google.gwt.media.client.Audio;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
@@ -20,9 +29,16 @@ import mitll.langtest.shared.Exercise;
  * To change this template use File | Settings | File Templates.
  */
 public class DataCollectionFlashcard extends BootstrapExercisePanel {
+  private static final String LISTEN_TO_THIS_AUDIO = "Listen to this audio";
   private Panel leftButtonContainer, rightButtonContainer;
   private NavigationHelper navigationHelper;
 
+  /**
+   * @see mitll.langtest.client.flashcard.DataCollectionFlashcardFactory#getExercisePanel(mitll.langtest.shared.Exercise)
+   * @param e
+   * @param service
+   * @param controller
+   */
   public DataCollectionFlashcard(Exercise e, LangTestDatabaseAsync service, ExerciseController controller) {
     super(e, service, controller);
     Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand () {
@@ -42,7 +58,7 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
 
   protected Widget getCardPrompt(Exercise e, ExerciseController controller) {
     FluidRow questionRow = new FluidRow();
-    Widget questionContent = getQuestionContent(e, controller);
+    Widget questionContent = getQuestionContent2(e, controller);
     Column contentContainer = new Column(8, questionContent);
     contentContainer.addStyleName("blueBackground");
     contentContainer.addStyleName("userNPFContent");
@@ -60,6 +76,16 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
     return questionRow;
   }
 
+  protected Widget getQuestionContent2(Exercise e, ExerciseController controller) {
+    String stimulus = e.getEnglishSentence();
+    String content = e.getContent();
+
+    if (content == null) {
+      content = stimulus;
+    }
+    return makeFlashcardForCRT(e, controller, content);
+  }
+
   private HorizontalPanel getCenteredContainer(Widget prev) {
     HorizontalPanel hp = new HorizontalPanel();
     hp.setHeight("100%");
@@ -74,5 +100,84 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
   protected void onUnload() {
     super.onUnload();
     navigationHelper.removeKeyHandler();
+  }
+
+  private Panel makeFlashcardForCRT(Exercise e, ExerciseController controller, String content) {
+    Exercise.QAPair qaPair = e.getForeignLanguageQuestions().get(0);
+   // content = content.replaceAll("<p> &nbsp; </p>", "");
+    String splitTerm = LISTEN_TO_THIS_AUDIO;
+    String[] split = content.split(splitTerm);
+    String prefix = split[0];
+    HTML contentPrefix = getHTML(prefix, true, controller);
+    contentPrefix.addStyleName("marginRight");
+
+    Panel container = new FlowPanel();
+    Heading child = new Heading(5, "Exercise " + e.getID());
+    child.addStyleName("leftTenMargin");
+    container.add(child);
+    container.add(contentPrefix);
+
+    // Todo : this is vulnerable to a variety of issues.
+    if (e.getRefAudio() != null && e.getRefAudio().length() > 0) {
+      Panel container2 = new FlowPanel();
+      container2.addStyleName("rightFiveMargin");
+      HTML prompt = getHTML("<h3 style='margin-right: 30px'>" + splitTerm + "</h3>", true, controller);
+      container2.add(prompt);
+      SimplePanel simplePanel = new SimplePanel();
+      simplePanel.add(getAudioWidget(e));
+      container2.add(simplePanel);
+
+      String suffix = split[1];
+      HTML contentSuffix = getHTML("<br/>" + // TODO: br is a hack
+        "<h3 style='margin-right: 30px'>" + suffix + "</h3>", true, controller);
+      contentSuffix.addStyleName("marginRight");
+      container2.add(contentSuffix);
+      container2.addStyleName("rightAlign");
+      container.add(container2);
+    }
+
+    container.add(getHTML("<h2 style='margin-right: 30px'>" + qaPair.getQuestion() + "</h2>", true, controller));
+    return container;
+  }
+
+  private Widget getAudioWidget(Exercise e) {
+    String refAudio = e.getRefAudio();
+    String type = refAudio.substring(refAudio.length() - 3);
+
+    final Audio audio = getAudioNoFocus(refAudio, type);
+    audio.addStyleName("floatRight");
+    audio.addStyleName("rightFiveMargin");
+
+    return audio;
+  }
+
+  private Audio getAudioNoFocus(String refAudio, String type) {
+    final Audio audio = Audio.createIfSupported();
+    audio.setControls(true);
+    audio.addSource(refAudio, "audio/" + type);
+    audio.addSource(refAudio.replace(".mp3", ".ogg"), "audio/ogg");
+    audio.addFocusHandler(new FocusHandler() {
+      @Override
+      public void onFocus(FocusEvent event) {
+        audio.setFocus(false);
+      }
+    });
+    return audio;
+  }
+
+  private HTML getHTML(String content, boolean requireAlignment, ExerciseController controller) {
+    boolean rightAlignContent = controller.isRightAlignContent();
+    HasDirection.Direction direction =
+      requireAlignment && rightAlignContent ? HasDirection.Direction.RTL : WordCountDirectionEstimator.get().estimateDirection(content);
+
+    HTML html = new HTML(content, direction);
+  //  html.setWidth("100%");
+    if (requireAlignment && rightAlignContent) {
+      html.addStyleName("rightAlign");
+    }
+    html.addStyleName("rightTenMargin");
+
+    html.addStyleName("wrapword");
+    return html;
   }
 }
