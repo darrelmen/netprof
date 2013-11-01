7913de702f1f3ea2a18e58273ba3dc6a132be2b9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 19:50:55 -0400

    added crt feedback to simple test taking interface
    Former-commit-id: 938160e38421f0c3bea642ca0380c2be8c6e5424 [formerly 5f986c7c9f79644671af1c380ab6cc8fc1363316] [formerly 07c006b0cbf57f7b51a30e786048c5a9e5f4f3cf [formerly cdbfd03e52c59320c11c6a0d03910e5ef48ffa00]]
    Former-commit-id: 214a524f2252d698fc29f3685a5cd8185f3cc76c [formerly 5b1eab8b0faa5ba75956ab3888ac511fd82a4d1f] [formerly 6ad7230491ecfed1f3c10a09861f54f2c7f4fa24 [formerly 42d9a092cec7e744428e0a894b72b43cf0974b4b]]
    Former-commit-id: a206f363eed362a50e62e809b0be4df7bd160e89 [formerly 6b9a5c1a037fe5b62b1f0e167ddd0cefdf8f96c9]
    Former-commit-id: d80cb0c396564f43bad7802d0755f7ec4ff840e2

--- src/mitll/langtest/client/exercise/ExercisePanelFactory.java
+++ src/mitll/langtest/client/exercise/ExercisePanelFactory.java
@@ -2,6 +2,7 @@ package mitll.langtest.client.exercise;
 
 import com.google.gwt.user.client.ui.Panel;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.recorder.FeedbackRecordPanel;
 import mitll.langtest.client.recorder.SimpleRecordExercisePanel;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.Exercise;
@@ -33,7 +34,11 @@ public class ExercisePanelFactory {
 
   public Panel getExercisePanel(Exercise e) {
     if (e.getType() == Exercise.EXERCISE_TYPE.RECORD) {
-      return new SimpleRecordExercisePanel(e, service, userFeedback, controller);
+      if (controller.isAutoCRTMode() && !e.promptInEnglish) {
+        return new FeedbackRecordPanel(e, service, userFeedback, controller);
+      } else {
+        return new SimpleRecordExercisePanel(e, service, userFeedback, controller);
+      }
     } else {
       return new ExercisePanel(e, service, userFeedback, controller);
     }
