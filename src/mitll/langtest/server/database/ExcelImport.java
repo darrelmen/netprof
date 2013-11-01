36bd430f73077023719d49473c60393c02623454
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 15:34:31 -0400

    added capability to train the svm model once for mira3, save the model to a file, then rehydrate from the saved model. This is helpful when it takes an hour to train on np
    Former-commit-id: 5d8dcca910eda5c16eebe9bb9a5fe8f2299a1281 [formerly c195919121510791fb1c5aff9326c591fb6e3b64]
    Former-commit-id: 2880d9ec6885904d4bd89c23dc9016a708e2d8c7

--- src/mitll/langtest/server/database/ExcelImport.java
+++ src/mitll/langtest/server/database/ExcelImport.java
@@ -986,7 +986,7 @@ public class ExcelImport implements ExerciseDAO {
       // "Spanish, language, game, clues, guess, español, lengua, juego, conjetura","$0.05","Fri Sep 06 20:54:21 GMT 2013","5","BatchId:1260169;","3600","172800","Fri Sep 13 20:54:21 GMT 2013","","","2H5ZKPFE4EGDYVOO2JZIGNZWY0D7HM","A3C2DVYUP88DE7","Approved","Sat Sep 07 10:42:30 GMT 2013","Sat Sep 07 10:43:07 GMT 2013","Mon Sep 09 03:43:07 PDT 2013","Sat Sep 07 17:50:38 PDT 2013","","","37","100% (641/641)","100% (641/641)","100% (488/488)","____ ____ ____ para limpiar el restaurante al final de la noche hay que ____ ____ ____","____ ____ Telas para cubrir las ventanas.","Tengo que hacer del numero uno, tengo que usar ____ ____","barrer el suelo","las cortinas","el primero","","","",""
 
 
-      Set<String> refs = new HashSet<String>();
+      //Set<String> refs = new HashSet<String>();
       line2 = reader.readLine();
       int c = 0;
       while ((line2 = reader.readLine()) != null) {
