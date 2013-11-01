e7d51f80533949549b31f54f49b3920e3e382807
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 15:34:31 -0400

    added capability to train the svm model once for mira3, save the model to a file, then rehydrate from the saved model. This is helpful when it takes an hour to train on np
    Former-commit-id: c77b526e4737a69d5cfc21510574eae5298519bf [formerly b08b99a0b35468658504f03a41bb9cc32c289e9e] [formerly 529894b3929a0d1af5c456a1b8a69140cf3bf302 [formerly 0ab968d480cbe50a37419d61ff61e789561c6e4e]]
    Former-commit-id: 15c7ea2b7e8d7cfd8383fe514a03df749453af9a [formerly 11fe4c3be1d5582e6ebc588d9dd694cc6e6b78ce] [formerly a1e70d36d05c4a04a216ab0242c48ec00915e8ba [formerly f371e9b6283ec0a26106da33d00c081395e25fc4]]
    Former-commit-id: 628e47ef00600c6470714f1ddea64cd94d59bd44 [formerly 9b95c61f884528f61cf468c17e8533cc2be97c5c]
    Former-commit-id: d3c9a485b7105d94de9241ab6647cb777e276d0a

--- src/mitll/langtest/client/flashcard/FlashcardExercisePanelFactory.java
+++ src/mitll/langtest/client/flashcard/FlashcardExercisePanelFactory.java
@@ -33,6 +33,6 @@ public class FlashcardExercisePanelFactory extends ExercisePanelFactory {
    * @return
    */
   public Panel getExercisePanel(Exercise e) {
-    return new BootstrapExercisePanel(e, service, controller, 30);
+    return new BootstrapExercisePanel(e, service, controller, 40);
   }
 }
