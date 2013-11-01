25346866fa1f2211d843b292298a629fa1826b61
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 19:50:55 -0400

    added crt feedback to simple test taking interface
    Former-commit-id: c91d81cf597de5d9d6ad8276b91e0d6ffd7355e5 [formerly 9c913e6c0c74fcfef461d371afa6bbd17b64c8e6] [formerly 0f31b024fbcbcf38da52a5debedc312d8dc9cd54 [formerly 04ca8335ba0b535314d7ae61642e4cca221e560d]] [formerly 3c0fb11f26e82dbae02c6a1f2105042fe228b849 [formerly 7a7253133b1f6fc3b7f1bbc8b71e113351cac1a5] [formerly 8140016c37e7d014b0f764f138198e94b6bbfdd8 [formerly 2b075398941d677f6e71e2f6e5aebba77a696887]]]
    Former-commit-id: 0fdfa07ff4243abce931dfa1e48f4879e227b389 [formerly 23eb2f28519cd58a3bc54f30b05c5cefddc98bd6] [formerly 5f3164ba2d91d36604a235c73c834f85cd392784 [formerly 98c741323219f8f6c20a787870baa560e439c35b]] [formerly 57e9c6c6ef5735e8d2677143af80bd63460e7fd2 [formerly ecd8c119623635149aa7a2f5b778ab81366c7a48] [formerly 765251d221622a9ee14b1eb224aa954f0b588ee0 [formerly 373d84d08b32207afd35b55cd1412a6bf9179d62]]]
    Former-commit-id: bf4869034cd5df0a3391a5c0bb2ce6406007ed37 [formerly a5f645c113f00c1f999b53d617a0193d4090418a] [formerly daaeb9c63338f3c90255e224eb761152f9289024 [formerly 6d6c0fe5ecdaadccb0d39e92a5ae2c06be413679]]
    Former-commit-id: f6cbcaf9be2a256be598e145685f0e81dcac91d9 [formerly b1870695cf2ec40fda0175e9f37f049c9a606858]
    Former-commit-id: 9f4d159165cb7db15c38a94161c7c8ffde4610ca

--- src/mitll/langtest/client/bootstrap/DataCollectionFlashcard.java
+++ src/mitll/langtest/client/bootstrap/DataCollectionFlashcard.java
@@ -2,19 +2,10 @@ package mitll.langtest.client.bootstrap;
 
 import com.github.gwtbootstrap.client.ui.Column;
 import com.github.gwtbootstrap.client.ui.FluidRow;
-import com.github.gwtbootstrap.client.ui.Heading;
 import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.i18n.client.HasDirection;
-import com.google.gwt.i18n.shared.WordCountDirectionEstimator;
-import com.google.gwt.media.client.Audio;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
@@ -30,7 +21,6 @@ import mitll.langtest.shared.Exercise;
  * To change this template use File | Settings | File Templates.
  */
 public class DataCollectionFlashcard extends BootstrapExercisePanel {
-  private static final String LISTEN_TO_THIS_AUDIO = "Listen to this audio";
   private static final int LOGIN_WIDGET_HEIGHT = 50; // TODO : do something better to figure out the height of the login widget
   private Panel leftButtonContainer, rightButtonContainer;
   protected NavigationHelper navigationHelper;
@@ -61,7 +51,7 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
 
   protected Widget getCardPrompt(Exercise e, ExerciseController controller) {
     FluidRow questionRow = new FluidRow();
-    Widget questionContent = getQuestionContent2(e, controller);
+    Widget questionContent = new AudioExerciseContent().getQuestionContent(e, controller, true, true);
     Column contentContainer = new Column(8, questionContent);
     contentContainer.addStyleName("blueBackground");
     contentContainer.addStyleName("userNPFContent");
@@ -83,62 +73,6 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
     navigationHelper = new NavigationHelper(e,controller,false, true);
   }
 
-  protected Widget getQuestionContent2(Exercise e, ExerciseController controller) {
-    String stimulus = e.getEnglishSentence();
-    String content = e.getContent();
-
-    if (content == null) {
-      content = stimulus;
-    }
-    return makeFlashcardForCRT(e, controller, content);
-  }
-
-  /**
-   * Do hacky stuff to take the content and splice in an html 5 audio widget to play back audio.
-   *
-   * @param e
-   * @param controller
-   * @param content
-   * @return
-   */
-  private Panel makeFlashcardForCRT(Exercise e, ExerciseController controller, String content) {
-    Exercise.QAPair qaPair = e.getForeignLanguageQuestions().get(0);
-    // content = content.replaceAll("<p> &nbsp; </p>", "");
-    String splitTerm = LISTEN_TO_THIS_AUDIO;
-    String[] split = content.split(splitTerm);
-    String prefix = split[0];
-    HTML contentPrefix = getHTML(prefix, true, controller);
-    contentPrefix.addStyleName("marginRight");
-
-    Panel container = new FlowPanel();
-    Heading child = new Heading(5, "Exercise " + e.getID());
-    child.addStyleName("leftTenMargin");
-    container.add(child);
-    container.add(contentPrefix);
-
-    // Todo : this is vulnerable to a variety of issues.
-    if (e.getRefAudio() != null && e.getRefAudio().length() > 0) {
-      Panel container2 = new FlowPanel();
-      container2.addStyleName("rightFiveMargin");
-      HTML prompt = getHTML("<h3 style='margin-right: 30px'>" + splitTerm + "</h3>", true, controller);
-      container2.add(prompt);
-      SimplePanel simplePanel = new SimplePanel();
-      simplePanel.add(getAudioWidget(e));
-      container2.add(simplePanel);
-
-      String suffix = split[1];
-      HTML contentSuffix = getHTML("<br/>" + // TODO: br is a hack
-        "<h3 style='margin-right: 30px'>" + suffix + "</h3>", true, controller);
-      contentSuffix.addStyleName("marginRight");
-      container2.add(contentSuffix);
-      container2.addStyleName("rightAlign");
-      container.add(container2);
-    }
-
-    container.add(getHTML("<h2 style='margin-right: 30px'>" + qaPair.getQuestion() + "</h2>", true, controller));
-    return container;
-  }
-
   private HorizontalPanel getCenteredContainer(Widget prev) {
     HorizontalPanel hp = new HorizontalPanel();
     hp.setHeight("100%");
@@ -155,69 +89,9 @@ public class DataCollectionFlashcard extends BootstrapExercisePanel {
     navigationHelper.removeKeyHandler();
   }
 
-  /**
-   * Make an html 5 audio widget using the exercise's ref audio field.
-   *
-   * @param e
-   * @return
-   */
-  private Widget getAudioWidget(Exercise e) {
-    String refAudio = e.getRefAudio();
-    String type = refAudio.substring(refAudio.length() - 3);
-
-    final Audio audio = getAudioNoFocus(refAudio, type);
-    audio.addStyleName("floatRight");
-    audio.addStyleName("rightFiveMargin");
-
-    return audio;
-  }
-
-  /**
-   * Make both mp3 and ogg types
-   * @param refAudio
-   * @param type
-   * @return
-   */
-  private Audio getAudioNoFocus(String refAudio, String type) {
-    final Audio audio = Audio.createIfSupported();
-    audio.setControls(true);
-    audio.addSource(refAudio, "audio/" + type);
-    audio.addSource(refAudio.replace(".mp3", ".ogg"), "audio/ogg");
-    audio.addFocusHandler(new FocusHandler() {
-      @Override
-      public void onFocus(FocusEvent event) {
-        audio.setFocus(false);
-      }
-    });
-    return audio;
-  }
-
-  /**
-   * @see #makeFlashcardForCRT(mitll.langtest.shared.Exercise, mitll.langtest.client.exercise.ExerciseController, String)
-   * @param content text we want to align
-   * @param requireAlignment so you can override it for certain widgets and not make it RTL
-   * @param controller to ask for the RTL property
-   * @return HTML that has it's text-align set consistent with the language (RTL for arabic, etc.)
-   */
-  private HTML getHTML(String content, boolean requireAlignment, ExerciseController controller) {
-    boolean rightAlignContent = controller.isRightAlignContent();
-    HasDirection.Direction direction =
-      requireAlignment && rightAlignContent ? HasDirection.Direction.RTL : WordCountDirectionEstimator.get().estimateDirection(content);
-
-    HTML html = new HTML(content, direction);
-  //  html.setWidth("100%");
-    if (requireAlignment && rightAlignContent) {
-      html.addStyleName("rightAlign");
-    }
-    html.addStyleName("rightTenMargin");
-
-    html.addStyleName("wrapword");
-    return html;
-  }
-
   @Override
-  public void showPronScoreFeedback(double score) {
-    super.showPronScoreFeedback(score);
+  public void showPronScoreFeedback(double score, String scorePrefix) {
+    super.showPronScoreFeedback(score, scorePrefix);
     navigationHelper.enableNextButton(true);
   }
 }
