b0c2437600fa47216a1517f8fb8b9f2ed8e4bb5c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 21:22:13 -0400

    support for showing crt feedback in simple test taking mode
    Former-commit-id: 46448b9e2ccbf9c6b7657ab053859589d52df18c

--- src/mitll/langtest/client/recorder/FeedbackRecordPanel.java
+++ src/mitll/langtest/client/recorder/FeedbackRecordPanel.java
@@ -1,8 +1,17 @@
 package mitll.langtest.client.recorder;
 
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.bootstrap.BootstrapExercisePanel;
+import mitll.langtest.client.bootstrap.ScoreFeedback;
 import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.sound.SoundFeedback;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.Exercise;
 
@@ -18,9 +27,41 @@ public class FeedbackRecordPanel extends SimpleRecordExercisePanel {
     super(e, service, userFeedback, controller);
   }
 
+  AutoCRTRecordPanel autoCRTRecordPanel;
+  ScoreFeedback scoreFeedback;
   @Override
   protected Widget getAnswerWidget(Exercise exercise, LangTestDatabaseAsync service, ExerciseController controller, final int index) {
-    return new  AutoCRTRecordPanel(service, controller,exercise, this, index).getPanel();
+    autoCRTRecordPanel = new AutoCRTRecordPanel(service, controller, exercise, this, index);
+    Panel panel = autoCRTRecordPanel.getPanel();
+   return getVerticalPanel(panel);
+  }
+
+  public Panel getVerticalPanel(Panel panel) {
+    FluidContainer container = new FluidContainer();
+    FluidRow row1 = new FluidRow();
+    container.add(row1);
+    row1.add(panel);
+
+    scoreFeedback = new ScoreFeedback(true);
+
+    SimplePanel simplePanel = new SimplePanel(scoreFeedback.getFeedbackImage());
+    simplePanel.addStyleName("floatLeft");
+
+    Panel scoreFeedbackRow = scoreFeedback.getSimpleRow(simplePanel,40);
+    scoreFeedback.getScoreFeedback().setWidth(Window.getClientWidth() * 0.5 + "px");
+    scoreFeedback.getScoreFeedback().addStyleName("topBarMargin");
+
+    container.add(scoreFeedbackRow);
+
+    HTML warnNoFlash = new HTML(BootstrapExercisePanel.WARN_NO_FLASH);
+    autoCRTRecordPanel.setSoundFeedback( new SoundFeedback(controller.getSoundManager(), warnNoFlash));
+    autoCRTRecordPanel.setScoreFeedback(scoreFeedback);
+    warnNoFlash.setVisible(false);
+    FluidRow row3 = new FluidRow();
+    container.add(row3);
+    row3.add(warnNoFlash);
+
+    return container;
   }
 
   @Override
