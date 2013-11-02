1e3816b0485f6899400d4dbd333c14ace84255c5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-01 21:22:13 -0400

    support for showing crt feedback in simple test taking mode
    Former-commit-id: b149d377715b34e4b26492ad2b8798ea4e55fc55 [formerly c412b3fca52a1fc69b7686456f75b974ce1b3828] [formerly 2936965776dd36745731f408272cea3d7f568575 [formerly 13eaea49ffcb91ee8d0b7b4560c46bf8cd1901b7]]
    Former-commit-id: 531ac17808bee529ffd6d65356abf4e8da808d15 [formerly 05d03a37444bad9412a259a2206660315a74769e] [formerly 88de821d060a4c0a37482464d33298d6628f2f8d [formerly f562903a176a91b7628a2997efba4d99bbb18c2a]]
    Former-commit-id: 12d1851eecc7b57146502eed14e85e0a7d511d05 [formerly c6207f2cd591b04f93e8d47f974eb4345cec78a2]
    Former-commit-id: 7c57e32f8221c80ecfcb2a41c9cec71235ea9fd5

--- src/mitll/langtest/client/recorder/AutoCRTRecordPanel.java
+++ src/mitll/langtest/client/recorder/AutoCRTRecordPanel.java
@@ -1,87 +1,69 @@
 /**
- * 
+ *
  */
 package mitll.langtest.client.recorder;
 
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.bootstrap.ScoreFeedback;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.exercise.ExerciseQuestionState;
+import mitll.langtest.client.sound.SoundFeedback;
 import mitll.langtest.shared.AudioAnswer;
 import mitll.langtest.shared.Exercise;
 
 /**
- *
  * Has three parts -- record/stop button, audio validity feedback, and audio html 5 control to playback audio just posted to the server. <br></br>
- *
+ * <p/>
  * On click on the stop button, posts audio to the server. <br></br>
- *
+ * <p/>
  * Automatically stops recording after 20 seconds.
  *
  * @author Gordon Vidaver
  */
 public class AutoCRTRecordPanel extends SimpleRecordPanel {
-  private final HTML resp = new HTML();
+  protected ScoreFeedback scoreFeedback;
+  private SoundFeedback soundFeedback;
 
   /**
    * Has three parts -- record/stop button, audio validity feedback icon, and the audio control widget that allows playback.
    *
-   * @see mitll.langtest.client.recorder.SimpleRecordExercisePanel#getAnswerWidget(mitll.langtest.shared.Exercise, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.exercise.ExerciseController, int)
+   * @see mitll.langtest.client.recorder.FeedbackRecordPanel
    */
-	public AutoCRTRecordPanel(final LangTestDatabaseAsync service, final ExerciseController controller,
-                            final Exercise exercise, final ExerciseQuestionState questionState, final int index){
+  public AutoCRTRecordPanel(final LangTestDatabaseAsync service, final ExerciseController controller,
+                            final Exercise exercise, final ExerciseQuestionState questionState, final int index) {
     super(service, controller, exercise, questionState, index);
-    getPanel().add(resp);
+  }
+
+  public void setSoundFeedback(SoundFeedback soundFeedback) {
+    this.soundFeedback = soundFeedback;
+  }
+
+  public void setScoreFeedback(ScoreFeedback scoreFeedback) {
+    this.scoreFeedback = scoreFeedback;
   }
 
   @Override
   public void startRecording() {
     super.startRecording();
-    resp.setText("");
+    scoreFeedback.clearFeedback();
   }
+
   /**
-   * @see RecordButtonPanel#stopRecording()
-   * @param result from server about the audio we just posted
+   * @param result        from server about the audio we just posted
    * @param questionState so we keep track of which questions have been answered
    * @param outer
+   * @see RecordButtonPanel#stopRecording()
    */
   @Override
   protected void receivedAudioAnswer(AudioAnswer result, final ExerciseQuestionState questionState, final Panel outer) {
-    super.receivedAudioAnswer(result,questionState,outer);
-
-    if (result.decodeOutput.length() > 0) {
-      // i.e. autocrt -- revisit?
-      showAutoCRTFeedback(result);
-    }
-    else if (result.getScore() != -1) {
-      resp.setHTML("I couldn't understand that, please record again.");
-    }
-  }
-
-  private void showAutoCRTFeedback(AudioAnswer result) {
-    double score = result.getScore();
-/*    score *= 2.5;
-    score -= 1.25;*/
-    score = Math.max(0,score);
-    score = Math.min(1.0,score);
-    String percent = ((int) (score * 100)) + "%";
-    if (result.isCorrect()) {//result.getScore() > 0.5) {
-      resp.setHTML("Correct! Score for <font size=+1>" + result.decodeOutput + "</font> was " + percent);
-      resp.setStyleName("correct");
-    } else {
-      resp.setHTML("Try again - score for <font size=+1>" + result.decodeOutput + "</font> was " + percent);
-      resp.setStyleName("incorrect");
-    }
+    super.receivedAudioAnswer(result, questionState, outer);
+    scoreFeedback.showCRTFeedback(result.getScore(), soundFeedback, "Score ");
   }
 
   @Override
   public void stopRecording() {
+    scoreFeedback.setWaiting();
     super.stopRecording();
-    if (controller.isAutoCRTMode())  {
-      resp.removeStyleName("incorrect");
-      resp.removeStyleName("correct");
-      resp.setText("Scoring... please wait.");
-    }
   }
 }
