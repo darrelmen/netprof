a98f7878b807c5863ecab57903a0a9dc55a36292
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-04 19:02:57 -0500

    support for entering both text and audio responses
    Former-commit-id: eee1562ef84f7cdf76eb0af996218742633959f7 [formerly 3610807a8c09925c0381a068bddde23a8776b762] [formerly 80d0f16de9d3dd5cb32266ab2a5611793a56e243 [formerly 8b65c44208a12e85a3d84e8fab86d2f1007ef0e4]] [formerly c9908f3f3e09549e7e6c58e56c9a30995782f85d [formerly f7142ed82b87a0cdb7c016262d69f028ee99251d] [formerly ce504cdde3b14e9be962e800d614afd024a97869 [formerly 89b9f7c95aa05e92fc9f5037c56e34015f938fb8]]]
    Former-commit-id: 14f8cd257df2d1528f41abaa19f63f0c0e2d08c7 [formerly de3d7cb600edc9c17663344fd93cb043ed0be4b4] [formerly 7439a38d5d30e86a446f7da4d83dc5c3a448e401 [formerly aa81d1719ba6fbc02f5966379f7dccd24cd3e1f8]]
    Former-commit-id: df0dd22f3eee121fa4461003e42c68127fe6907a [formerly cad42e5394e751c19f7a3084420a0bcee6b829ad]
    Former-commit-id: 8ef45060ef9226612974abe03490b271674d8a0d

---
+++ src/mitll/langtest/client/bootstrap/CombinedResponseFlashcard.java
@@ -0,0 +1,106 @@
+package mitll.langtest.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.ExerciseQuestionState;
+import mitll.langtest.client.recorder.AutoCRTRecordPanel;
+import mitll.langtest.client.sound.SoundFeedback;
+import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.AudioAnswer;
+import mitll.langtest.shared.Exercise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 11/4/13
+ * Time: 3:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CombinedResponseFlashcard extends TextCRTFlashcard implements ExerciseQuestionState {
+  private Set<Widget> completed = new HashSet<Widget>();
+
+  public CombinedResponseFlashcard(Exercise e, LangTestDatabaseAsync service, ExerciseController controller, UserManager userManager) {
+    super(e, service, controller, userManager);    //To change body of overridden methods use File | Settings | File Templates.
+  }
+
+  @Override
+  protected void gotScoreForGuess(Double result, Button check, ScoreFeedback scoreFeedback) {
+    super.gotScoreForGuess(result,check,scoreFeedback);
+    completed.add(check);
+    enableNext();
+  }
+
+  public void recordIncomplete(Widget answer) {
+    completed.remove(answer);
+    enableNext();
+  }
+
+  public void recordCompleted(Widget answer) {
+    completed.add(answer);
+    enableNext();
+  }
+
+  private void enableNext() {
+    System.out.println("enabled " + completed.size());
+    navigationHelper.enableNextButton((completed.size() == 2));
+  }
+
+  protected void addRecordingAndFeedbackWidgets(Exercise exercise, LangTestDatabaseAsync service, ExerciseController controller,
+                                                int feedbackHeight) {
+    super.addRecordingAndFeedbackWidgets(exercise,service,controller,feedbackHeight);
+    autoCRTRecordPanel = new AutoCRTRecordPanel(service, controller, exercise, this, 1) {
+      @Override
+      protected void receivedAudioAnswer(AudioAnswer result, final ExerciseQuestionState questionState, final Panel outer) {
+        super.receivedAudioAnswer(result, questionState, outer);
+        scoreFeedback.hideFeedback();
+      }
+    };
+    Panel panel = autoCRTRecordPanel.getPanel();
+    add(getRecordButtonRow(getVerticalPanel(panel, new ScoreFeedback(false), controller)));
+  }
+
+  private AutoCRTRecordPanel autoCRTRecordPanel;
+  public Panel getVerticalPanel(Panel panel, ScoreFeedback scoreFeedback, ExerciseController controller) {
+    FluidRow outer = new FluidRow();
+
+    FluidContainer container = new FluidContainer();
+    outer.add(new Column(5));
+    outer.add(container);
+
+    FluidRow row1 = new FluidRow();
+    container.add(row1);
+    row1.add(panel);
+
+    SimplePanel simplePanel = new SimplePanel(scoreFeedback.getFeedbackImage());
+    simplePanel.addStyleName("floatLeft");
+
+    Panel scoreFeedbackRow = scoreFeedback.getScoreFeedbackRow2(simplePanel, 48);
+
+    scoreFeedback.getScoreFeedback().setWidth(Window.getClientWidth() * 0.5 + "px");
+    scoreFeedback.getScoreFeedback().addStyleName("topBarMargin");
+
+    container.add(scoreFeedbackRow);
+
+    HTML warnNoFlash = new HTML(BootstrapExercisePanel.WARN_NO_FLASH);
+    autoCRTRecordPanel.setSoundFeedback(new SoundFeedback(controller.getSoundManager(), warnNoFlash));
+    autoCRTRecordPanel.setScoreFeedback(scoreFeedback);
+    warnNoFlash.setVisible(false);
+    FluidRow row3 = new FluidRow();
+    container.add(row3);
+    row3.add(warnNoFlash);
+
+    return outer;
+  }
+}
