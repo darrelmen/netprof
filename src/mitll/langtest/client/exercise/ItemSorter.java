7ede0d23c4aa99a6926ffd92e43717d8efb77da6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-06 17:45:59 -0500

    steps toward doing a crt MSA Audio site where you can select items based on ilr level
    Former-commit-id: e281bf67070a7810201b2899074d39f5db1fe4a0 [formerly bea8709aabfcd7e52a6c77fc58a73adc4c456a0f] [formerly e281bf67070a7810201b2899074d39f5db1fe4a0 [formerly bea8709aabfcd7e52a6c77fc58a73adc4c456a0f] [formerly e281bf67070a7810201b2899074d39f5db1fe4a0 [formerly bea8709aabfcd7e52a6c77fc58a73adc4c456a0f] [formerly 4c03b85332b1d6fd837d182c6836290d1606fb87]]]
    Former-commit-id: 33b42cec8ea492f5e03d9fcc48464b024e8f2571 [formerly 79d2a42aca502fc6313fce2830f67906b089ec66] [formerly 33b42cec8ea492f5e03d9fcc48464b024e8f2571 [formerly 79d2a42aca502fc6313fce2830f67906b089ec66] [formerly a11ade102a903415617c5bcca7d09f32aeb8e1d9]] [formerly 81e75ea63a9e3411519090f94bbee277d1f7d3e3 [formerly afd5ba5897677e1c8536649423b7bf5d43810e7c] [formerly 81e75ea63a9e3411519090f94bbee277d1f7d3e3 [formerly afd5ba5897677e1c8536649423b7bf5d43810e7c] [formerly d6edee971269d0b97ef552d21277106c296e0daf]]]
    Former-commit-id: 81e75ea63a9e3411519090f94bbee277d1f7d3e3 [formerly afd5ba5897677e1c8536649423b7bf5d43810e7c]
    Former-commit-id: 5793593ba9cbc718f24bd7f0651d16c32af53f62 [formerly 90269a1e7aa9bdbbd2071994a977598a9facf159] [formerly 6e5f1ec386ab9edc4bd77764de0cb69319d2081c]
    Former-commit-id: 8e55db22099a52a53a5413febedb1f25ea323300
    Former-commit-id: 541ca8c24ce83877d0f83998eacaa1b6a4ddc762

---
+++ src/mitll/langtest/client/exercise/ItemSorter.java
@@ -0,0 +1,113 @@
+package mitll.langtest.client.exercise;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 11/6/13
+ * Time: 3:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemSorter {
+  public List<String> getSortedItems(Collection<String> sections) {
+    List<String> items = new ArrayList<String>(sections);
+    boolean isInt = true;
+    for (String item : items) {
+      try {
+        Integer.parseInt(item);
+      } catch (NumberFormatException e) {
+        isInt = false;
+      }
+    }
+    if (isInt) {
+      Collections.sort(items, new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+          int first = Integer.parseInt(o1);
+          int second = Integer.parseInt(o2);
+          return first < second ? -1 : first > second ? +1 : 0;
+        }
+      });
+    }
+    else {
+      sortWithCompoundKeys(items);
+    }
+    return items;
+  }
+
+  /**
+   * @see #getSortedItems(java.util.Collection)
+   * @param items
+   */
+  private void sortWithCompoundKeys(List<String> items) {
+    Collections.sort(items, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        boolean firstHasSep = o1.contains("-");
+        boolean secondHasSep = o2.contains("-");
+        String left1 = o1;
+        String left2 = o2;
+        String right1 = "";
+        String right2 = "";
+
+        if (firstHasSep) {
+          String[] first = o1.split("-");
+          left1 = first[0];
+          if (first.length == 1) {
+            System.err.println("huh? couldn't split " + o1);
+            right1 = "";
+          }
+          else {
+            right1 = first[1];
+          }
+        } else if (o1.contains(" ")) {
+          firstHasSep = true;
+          String[] first = o1.split("\\s");
+          left1 = first[0];
+          right1 = first[1];
+        }
+
+        if (secondHasSep) {
+          String[] second = o2.split("-");
+          left2 = second[0];
+          right2 = second[1];
+        } else if (o2.contains(" ")) {
+          secondHasSep = true;
+          String[] second = o2.split("\\s");
+          left2 = second[0];
+          right2 = second[1];
+        }
+
+        if (firstHasSep || secondHasSep) {
+          int leftCompare = getIntCompare(left1, left2);
+          if (leftCompare != 0) {
+            return leftCompare;
+          } else {
+            return getIntCompare(right1, right2);
+          }
+        } else {
+          return getIntCompare(o1, o2);
+        }
+      }
+    });
+  }
+
+  private int getIntCompare(String first, String second) {
+    if (first.length() > 0 && !Character.isDigit(first.charAt(0))) {
+      return first.compareToIgnoreCase(second);
+    } else {
+      try {
+        int r1 = Integer.parseInt(first);
+        int r2 = Integer.parseInt(second);
+        return r1 < r2 ? -1 : r1 > r2 ? +1 : 0;
+      } catch (NumberFormatException e) {
+        return first.compareToIgnoreCase(second);
+      }
+    }
+  }
+}
