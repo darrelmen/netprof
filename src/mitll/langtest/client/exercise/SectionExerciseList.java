1e30305519419c9ff4c98ae7f82d82644bbdddbd
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-07 15:13:10 -0500

    shows completed items, jumps  to next incomplete item when we log back in
    Former-commit-id: ec9eb47ca72b50962459976c2ae8882c0020c147 [formerly f9436b97916de25aec5025cad71b678d6a446e4a] [formerly 5a1a19b00a41002e159e39f646a1dc317f2f6258 [formerly 80581cad3973d686bf37ac7a8952e3ad7aa64ad7]] [formerly 3637e49478f1bbf09acc6d90a0499fc31fee5aaf]
    Former-commit-id: 2473fd0481d59a8251ffadacdb0cec488a63b997 [formerly 64d305adf787fc1c57e8734767e1f11c4bc75c22] [formerly 93084ee1dc4254a565a623ce38b4d2a210788eb8 [formerly cb2004be9eb84b99453a08f402f7862534477e13]] [formerly f4456e20275567fe86e021d21f7d9241749922a4]
    Former-commit-id: 23cbfa711a9e2e15cf40246d71f1e17d20a75087
    Former-commit-id: 98bf34af2d82b90804aa9913926634d6dc824a83 [formerly 045942e81e795af00cf088e9d1b2e51414034818]
    Former-commit-id: 88679f4177dfe73c1c83b14f16405098b5f81d79

--- src/mitll/langtest/client/exercise/SectionExerciseList.java
+++ src/mitll/langtest/client/exercise/SectionExerciseList.java
@@ -33,7 +33,6 @@ import java.util.Set;
 public class SectionExerciseList extends HistoryExerciseList {
   protected Panel sectionPanel;
   protected final boolean showListBoxes;
-  private String lastSelectionState = "NO_SELECTION_STATE";
 
   /**
    * @see mitll.langtest.client.LangTest#makeExerciseList
@@ -91,8 +90,6 @@ public class SectionExerciseList extends HistoryExerciseList {
    */
   private void useInitialTypeToSectionMap(Map<String, Map<String, Integer>> result, long userID) {
     sectionPanel.clear();
-
-    //System.out.println("useInitialTypeToSectionMap for " + userID);
 
     Panel flexTable = getWidgetsForTypes(result, userID);
 
@@ -171,103 +168,6 @@ public class SectionExerciseList extends HistoryExerciseList {
     List<String> items = new ItemSorter().getSortedItems(sectionToCount.keySet());
     listBox.populateTypeWidget(items, sectionToCount);
   }
-
-/*  protected List<String> getSortedItems(Collection<String> sections) {
-    List<String> items = new ArrayList<String>(sections);
-    boolean isInt = true;
-    for (String item : items) {
-      try {
-        Integer.parseInt(item);
-      } catch (NumberFormatException e) {
-        isInt = false;
-      }
-    }
-    if (isInt) {
-      Collections.sort(items, new Comparator<String>() {
-        @Override
-        public int compare(String o1, String o2) {
-          int first = Integer.parseInt(o1);
-          int second = Integer.parseInt(o2);
-          return first < second ? -1 : first > second ? +1 : 0;
-        }
-      });
-    }
-    else {
-      sortWithCompoundKeys(items);
-    }
-    return items;
-  }
-
-  *//**
-   * @see #getSortedItems(java.util.Collection)
-   * @param items
-   *//*
-  private void sortWithCompoundKeys(List<String> items) {
-    Collections.sort(items, new Comparator<String>() {
-      @Override
-      public int compare(String o1, String o2) {
-        boolean firstHasSep = o1.contains("-");
-        boolean secondHasSep = o2.contains("-");
-        String left1 = o1;
-        String left2 = o2;
-        String right1 = "";
-        String right2 = "";
-
-        if (firstHasSep) {
-          String[] first = o1.split("-");
-          left1 = first[0];
-          if (first.length == 1) {
-        	  System.err.println("huh? couldn't split " + o1);
-        	  right1 = "";
-          }
-          else {
-        	  right1 = first[1];
-          }
-        } else if (o1.contains(" ")) {
-          firstHasSep = true;
-          String[] first = o1.split("\\s");
-          left1 = first[0];
-          right1 = first[1];
-        }
-
-        if (secondHasSep) {
-          String[] second = o2.split("-");
-          left2 = second[0];
-          right2 = second[1];
-        } else if (o2.contains(" ")) {
-          secondHasSep = true;
-          String[] second = o2.split("\\s");
-          left2 = second[0];
-          right2 = second[1];
-        }
-
-        if (firstHasSep || secondHasSep) {
-          int leftCompare = getIntCompare(left1, left2);
-          if (leftCompare != 0) {
-            return leftCompare;
-          } else {
-            return getIntCompare(right1, right2);
-          }
-        } else {
-          return getIntCompare(o1, o2);
-        }
-      }
-    });
-  }
-
-  private int getIntCompare(String first, String second) {
-    if (first.length() > 0 && !Character.isDigit(first.charAt(0))) {
-      return first.compareToIgnoreCase(second);
-    } else {
-      try {
-        int r1 = Integer.parseInt(first);
-        int r2 = Integer.parseInt(second);
-        return r1 < r2 ? -1 : r1 > r2 ? +1 : 0;
-      } catch (NumberFormatException e) {
-        return first.compareToIgnoreCase(second);
-      }
-    }
-  }*/
 
   /**
    * @see #getWidgetsForTypes(java.util.Map, long)
@@ -357,5 +257,4 @@ public class SectionExerciseList extends HistoryExerciseList {
 
     }
   }*/
-
 }
