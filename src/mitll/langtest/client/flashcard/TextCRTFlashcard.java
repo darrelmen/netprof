63537a3aa0232d7b2b613c8126bbd75db4e05ac0
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-07 19:13:06 -0500

    minor fix to progress tracker, moved classes around to different packages
    Former-commit-id: 3d73931ce4303305e4b811d2b63d142ad99a82d8 [formerly c18635064af59509a1fbcb2bdd59ab2a313f8a7e]
    Former-commit-id: 4956916652a09db8a8e52f3c48f3f8dc859e477c

--- src/mitll/langtest/client/flashcard/TextCRTFlashcard.java
+++ src/mitll/langtest/client/flashcard/TextCRTFlashcard.java
@@ -1,4 +1,4 @@
-package mitll.langtest.client.bootstrap;
+package mitll.langtest.client.flashcard;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.FluidRow;
