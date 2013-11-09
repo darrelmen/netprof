3298ca82e748d940fba3661b24d0f9b115e59e35
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-08 19:57:24 -0500

    support for text, audio, or both input in test crt mode
    Former-commit-id: 5c3c462f83892b7f7df21e76e5582ba1cb2eaefb [formerly 3a1be12b2ceb24f4dba901b80196d0b5644b5254] [formerly 4aad21857568a587955021a096db145f59057d4b [formerly 5b1e551c519719d551b62e75c9409ebc2b3b5a02]] [formerly 5c3c462f83892b7f7df21e76e5582ba1cb2eaefb [formerly 3a1be12b2ceb24f4dba901b80196d0b5644b5254] [formerly 4aad21857568a587955021a096db145f59057d4b [formerly 5b1e551c519719d551b62e75c9409ebc2b3b5a02]] [formerly c70d605bab204bfeb51a8df8963dcf08812ec4f6]]
    Former-commit-id: 0f16562663efbc8b74bc7f942f483b1c5da8799a [formerly 03005f6b9258f503858a4cd3d0bca2c9b617f5ed] [formerly 8937443e9973a0899ccb85e6e893c295e37f8e75 [formerly 89ba2abaab2f3d25ecbadf2089fb6fefd9ab57e9]] [formerly ce7fccc63a499a50b6be3991f9f51d263688d617] [formerly bbc0f97c1650f38977609e44252fa3527bdc1ca4 [formerly 09218bb1fc50db9221ba5c2290bb78773c8c7353] [formerly c9fc2001039b310e7c69c5b3a495936d7a315f60 [formerly f291c7bb9fd02950daa658a602b1b60ca54b80e8]] [formerly b3b1ab0cb112750b7d75ef64d03c88c05d7247c3]]
    Former-commit-id: bbc0f97c1650f38977609e44252fa3527bdc1ca4 [formerly 09218bb1fc50db9221ba5c2290bb78773c8c7353] [formerly c9fc2001039b310e7c69c5b3a495936d7a315f60 [formerly f291c7bb9fd02950daa658a602b1b60ca54b80e8]]
    Former-commit-id: 9c0968fa05fb2cb52b0fdf9ba8bd8f775f05fbf4
    Former-commit-id: 201a4598bce90376adbba655490936caad7a6e88 [formerly 0b054a7d1838781634c9cbfe2ecea4057f8ffdf5]
    Former-commit-id: abee46ab229d2b7bc750da39c4dea519ce60f7da

--- src/mitll/langtest/client/recorder/FeedbackRecordPanel.java
+++ src/mitll/langtest/client/recorder/FeedbackRecordPanel.java
@@ -8,12 +8,16 @@ import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.flashcard.BootstrapExercisePanel;
 import mitll.langtest.client.flashcard.ScoreFeedback;
-import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.flashcard.TextResponse;
 import mitll.langtest.client.sound.SoundFeedback;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.Exercise;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,34 +28,95 @@ import mitll.langtest.shared.Exercise;
  */
 public class FeedbackRecordPanel extends SimpleRecordExercisePanel {
   private AutoCRTRecordPanel autoCRTRecordPanel;
+  private Set<Widget> completed = new HashSet<Widget>();
 
   public FeedbackRecordPanel(Exercise e, LangTestDatabaseAsync service, UserFeedback userFeedback, ExerciseController controller) {
     super(e, service, userFeedback, controller);
   }
 
+  public void recordIncomplete(Widget answer) {
+    completed.remove(answer);
+    enableNext();
+  }
+
+  public void recordCompleted(Widget answer) {
+    completed.add(answer);
+    enableNext();
+  }
+
+  private void enableNext() {
+    navigationHelper.enableNextButton((completed.size() == (controller.getProps().getResponseType().equalsIgnoreCase("Both") ? 2 :1)));
+  }
+
+  @Override
+  protected String getQuestionPrompt(boolean promptInEnglish) {
+    String responseType = controller.getProps().getResponseType();
+    if (responseType.equalsIgnoreCase("Text")) {
+      return getWrittenPrompt(promptInEnglish);
+    } else if (responseType.equals("Audio")) {
+      return getSpokenPrompt(promptInEnglish);
+    } else return "";
+  }
+
   @Override
   protected Widget getAnswerWidget(Exercise exercise, LangTestDatabaseAsync service, ExerciseController controller, final int index) {
     autoCRTRecordPanel = new AutoCRTRecordPanel(service, controller, exercise, this, index);
-    return getVerticalPanel(autoCRTRecordPanel.getPanel(), new ScoreFeedback(true));
+    String responseType = controller.getProps().getResponseType();
+    final Widget outer = this;
+
+    if (responseType.equalsIgnoreCase("Audio")) {
+      return getVerticalPanel(autoCRTRecordPanel.getPanel(), new ScoreFeedback(true),true);
+    }
+    else if (responseType.equalsIgnoreCase("Text")){
+      Panel widget = doText(exercise, service, controller, outer);
+      return widget;
+    }
+    else {
+      Panel widget = doText(exercise, service, controller, outer);
+
+      widget.add(getVerticalPanel(autoCRTRecordPanel.getPanel(), new ScoreFeedback(true), true));
+      return widget;
+    }
   }
 
-  public Panel getVerticalPanel(Panel panel, ScoreFeedback scoreFeedback) {
+  private Panel doText(Exercise exercise, LangTestDatabaseAsync service, ExerciseController controller, final Widget outer) {
+    TextResponse textResponse = new TextResponse(controller.getUser(),soundFeedback,
+      new TextResponse.AnswerPosted() {
+      @Override
+      public void answerPosted() {
+        recordCompleted(outer);
+      }
+    });
+
+    FluidContainer container = new FluidContainer();
+    textResponse.addWidgets(container,exercise,service,controller,false, true);
+
+    Panel verticalPanel = getVerticalPanel(container, textResponse.getTextScoreFeedback(),false);
+    textResponse.setSoundFeedback(soundFeedback);
+    return verticalPanel;
+  }
+
+  SoundFeedback soundFeedback;
+  public Panel getVerticalPanel(Panel panel, ScoreFeedback scoreFeedback,boolean addToContainer) {
     FluidContainer container = new FluidContainer();
     FluidRow row1 = new FluidRow();
     container.add(row1);
     row1.add(panel);
 
-    SimplePanel simplePanel = new SimplePanel(scoreFeedback.getFeedbackImage());
-    simplePanel.addStyleName("floatLeft");
+    if (addToContainer) {
+      SimplePanel simplePanel = new SimplePanel(scoreFeedback.getFeedbackImage());
+      simplePanel.addStyleName("floatLeft");
 
-    Panel scoreFeedbackRow = scoreFeedback.getSimpleRow(simplePanel,40);
-    scoreFeedback.getScoreFeedback().setWidth(Window.getClientWidth() * 0.5 + "px");
-    scoreFeedback.getScoreFeedback().addStyleName("topBarMargin");
+      Panel scoreFeedbackRow = scoreFeedback.getSimpleRow(simplePanel, 40);
+      scoreFeedback.getScoreFeedback().setWidth(Window.getClientWidth() * 0.5 + "px");
+      scoreFeedback.getScoreFeedback().addStyleName("topBarMargin");
 
-    container.add(scoreFeedbackRow);
+      container.add(scoreFeedbackRow);
+    }
 
     HTML warnNoFlash = new HTML(BootstrapExercisePanel.WARN_NO_FLASH);
-    autoCRTRecordPanel.setSoundFeedback(new SoundFeedback(controller.getSoundManager(), warnNoFlash));
+    soundFeedback = new SoundFeedback(controller.getSoundManager(), warnNoFlash);
+    autoCRTRecordPanel.setSoundFeedback(soundFeedback);
     autoCRTRecordPanel.setScoreFeedback(scoreFeedback);
     warnNoFlash.setVisible(false);
     FluidRow row3 = new FluidRow();
