1dc16d30a912dd2f5c5415a5ce34707039314de7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-12 16:52:33 -0500

    fixed bugs with having the responseType be on the URL
    Former-commit-id: 73eaa07784aa1e170682320e7227035519d2f998 [formerly f69ac7522793316d02918a0eb46dbcbeb88abdf7] [formerly a82d6c624cad44007c75051bc91dc1f40411d5f0 [formerly bc0831fcf38daece72a0b0d2480d7fa413c5578a]] [formerly c6eaeb6ee4d6655f58891eb841536d8d0dff947c]
    Former-commit-id: 19255ebbbc38741cf31ed5407789acce8ce9e5af [formerly 9d81e923e62af29825221f1f3794b864c5bcebb7] [formerly 49163c5cdeb5273c27fb8e51bf8777fbfe6aab16 [formerly c908426dabdbcdb8a9095c889414f55a0673ef02]] [formerly 0aea04f243465c45ef63c4f732230909a536585b]
    Former-commit-id: 39741b618fc537c59df805bcc5071d7d78bcaa96
    Former-commit-id: 81118c3e1692463e2ac8660a774f788dab9abc86 [formerly 537483ba8ffa9b242daebc72d3398de0a039b649]
    Former-commit-id: 45faa4ea594d57993b97e458be011a3dc1d423b7

--- src/mitll/langtest/client/bootstrap/ResponseExerciseList.java
+++ src/mitll/langtest/client/bootstrap/ResponseExerciseList.java
@@ -8,6 +8,8 @@ import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.list.ResponseChoice;
+import mitll.langtest.client.places.ResponseTypeView;
+import mitll.langtest.client.places.WirePlaces;
 import mitll.langtest.client.user.UserFeedback;
 
 /**
@@ -17,14 +19,28 @@ import mitll.langtest.client.user.UserFeedback;
  * Time: 4:17 PM
  * To change this template use File | Settings | File Templates.
  */
-public class ResponseExerciseList extends FlexSectionExerciseList {
+public class ResponseExerciseList extends FlexSectionExerciseList implements ResponseTypeView {
   private ResponseChoice responseChoice;
+  //WirePlaces wirePlaces;
 
+  /**
+   * @see mitll.langtest.client.ExerciseListLayout#makeExerciseList(com.github.gwtbootstrap.client.ui.FluidRow, boolean, mitll.langtest.client.user.UserFeedback, com.google.gwt.user.client.ui.Panel, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.exercise.ExerciseController)
+   * @param secondRow
+   * @param currentExerciseVPanel
+   * @param service
+   * @param feedback
+   * @param showTurkToken
+   * @param showInOrder
+   * @param controller
+   * @param isCRTDataMode
+   */
   public ResponseExerciseList(FluidRow secondRow, Panel currentExerciseVPanel, LangTestDatabaseAsync service,
-                              UserFeedback feedback, boolean showTurkToken, boolean showInOrder, final ExerciseController controller, boolean isCRTDataMode) {
+                              UserFeedback feedback, boolean showTurkToken, boolean showInOrder,
+                              final ExerciseController controller, boolean isCRTDataMode) {
     super(secondRow, currentExerciseVPanel, service, feedback, showTurkToken, showInOrder, controller, isCRTDataMode);
     String responseType = controller.getProps().getResponseType();
-
+   // wirePlaces = new WirePlaces(responseType);
+   // wirePlaces.wire(this,responseType);
     responseChoice = new ResponseChoice(responseType, new ResponseChoice.ChoiceMade() {
       @Override
       public void choiceMade(String responseType) {
@@ -34,10 +50,17 @@ public class ResponseExerciseList extends FlexSectionExerciseList {
     });
   }
 
-
+  /**
+   * Adds the response type to the end of the history token
+   * @param historyToken
+   */
   protected void setHistoryItem(String historyToken) {
-    historyToken = historyToken.split("&")[0];
-    History.newItem(historyToken +"&responseType="+responseChoice.getResponseType() );
+  //  System.out.println("history token '" +historyToken+"'");
+    historyToken = historyToken.contains("###") ? historyToken.split("###")[0] : historyToken;
+   // System.out.println("history token '" +historyToken+ "'");
+    String historyToken1 =historyToken + "###" +  "responseType=" + responseChoice.getResponseType();
+    //System.out.println("history token1 '" +historyToken1+"'");
+    History.newItem(historyToken1);
   }
 
   @Override
@@ -45,5 +68,10 @@ public class ResponseExerciseList extends FlexSectionExerciseList {
     Widget widget = super.addBottomText(container);
     container.add(responseChoice.getResponseTypeWidget());
     return widget;
+  }
+
+  @Override
+  public void selectResponseType(String responseType) {
+    controller.getProps().setResponseType(responseType);
   }
 }
