91b623f874b02ace05fc817161fb573aa95d906b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-11-13 02:47:40 -0500

    added initial steps to supporting prefix exercise query
    Former-commit-id: 38da57999e54bfcf573fe364b83b931fde235253 [formerly edf081095b63a769a3d7c7efec79dc5f4cb94791] [formerly 078b0d11b1044fd2bc9863ad83b70cb30a263f84 [formerly 23c1595e85439408a31235c54da8db804cd3edc5]] [formerly 38da57999e54bfcf573fe364b83b931fde235253 [formerly edf081095b63a769a3d7c7efec79dc5f4cb94791] [formerly 078b0d11b1044fd2bc9863ad83b70cb30a263f84 [formerly 23c1595e85439408a31235c54da8db804cd3edc5]] [formerly e6d477f8c286f7f5c8941469bb72c0593667875b [formerly 2ca4803442a548e8a4b8f84beac64b7c2abb7320] [formerly e98ff69f3dc36d1726131eb5fddbc7cad1c00e16]]]
    Former-commit-id: 62385e7b6f85981d640d373021066b875e3562ce [formerly 87f1668af2ac4aa610ec3dd8ac7ff1e009449f93] [formerly b90b2d7f24b40820178e0a8231bc22812dd8263e [formerly 21b103abc73f7b25e591f3f05ee1b7dd42975f80]] [formerly 8508f7d32c6d782b60ffb5de3b7e3d33d84b05b8 [formerly 10bff76fd64042a22379c3f2cb33d6937b1b49b1]] [formerly c46ff48d45c47f8ddbca9f5fd753017b94afccb4 [formerly 8db6b6e6a79391eee4eb1d69b7b699e30f6466a7] [formerly c9a2b49d1eb45e25f2fdd586706aa166c49a67b7 [formerly 61952b90fa01d3bd7fe4de5d334567b0cf7270a4]] [formerly f13612048081b65ba790bfaffc6bff4b5668317c [formerly c1aa7c6db78c83b18754ba56ba408053372d8720]]]
    Former-commit-id: c46ff48d45c47f8ddbca9f5fd753017b94afccb4 [formerly 8db6b6e6a79391eee4eb1d69b7b699e30f6466a7] [formerly c9a2b49d1eb45e25f2fdd586706aa166c49a67b7 [formerly 61952b90fa01d3bd7fe4de5d334567b0cf7270a4]]
    Former-commit-id: f0e99e86cf96d7f790a11a0c39a4ce03a90dccdd [formerly 5ad5d4679c290e929ecbaf5e43732ec181b1a9f7]
    Former-commit-id: 77ea1b9fff34de1afe723bf17f60b18d2263469b [formerly eb666757503f8f929244c270e7fd3dee7c64617b]
    Former-commit-id: 84b1c4290c3d7857d87a62a2c8e0fba4cb1ad57e

---
+++ src/mitll/langtest/server/trie/TextEntityValue.java
@@ -0,0 +1,12 @@
+package mitll.langtest.server.trie;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: go22670
+ * Date: 11/13/13
+ * Time: 1:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TextEntityValue {
+  String getNormalizedValue();
+}
