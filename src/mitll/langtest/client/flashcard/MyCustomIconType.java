d6e8570d8b934f34662efce0fc5e4bb81957a82d
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-04 20:06:32 -0500

    fixed issues with avp advancing, added clicking on record button
    Former-commit-id: 3207be53b5798184d57ff8efbdbaf3dfadaa9cb8 [formerly 6671f7b5fe123a8a684b70e0857d99882c87f201]
    Former-commit-id: 528590518aa8e738099fc9b3c3ed4350bde6c613

---
+++ src/mitll/langtest/client/flashcard/MyCustomIconType.java
@@ -0,0 +1,35 @@
+package mitll.langtest.client.flashcard;
+
+import com.github.gwtbootstrap.client.ui.constants.BaseIconType;
+import com.google.gwt.core.client.GWT;
+
+/** My custom base icon collection */
+public enum MyCustomIconType implements BaseIconType {
+
+  enter,// Our runtime access
+  waiting, // Our runtime access
+  record1, // Our runtime access
+  record2, // Our runtime access
+  correct, // Our runtime access
+  record, // Our runtime access
+  stop, // Our runtime access
+  white, // Our runtime access
+  gray, // Our runtime access
+  incorrect; // Our runtime access
+
+  /** Inject the icon's css once at first usage */
+  static {
+    IconResources icons = GWT.create(IconResources.class);
+    icons.css().ensureInjected();
+  }
+
+  private static final String PREFIX = "myBaseIcon_";
+  private String className;
+
+  MyCustomIconType() {
+    this.className = this.name().toLowerCase();
+  }
+  @Override public String get() {
+    return PREFIX + className;
+  }
+}
