620975082bfaa0d6c338ed77044413e9e7446380
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-04 20:06:32 -0500

    fixed issues with avp advancing, added clicking on record button
    Former-commit-id: 5b9ac853b5afc0364f3a1a259246b59fedf1e9df [formerly a81576df45225bb812e5d9ae27424491288fe945]
    Former-commit-id: ff2691df19c89725fdff426f5cc14b6f15731515

---
+++ src/mitll/langtest/client/flashcard/IconResources.java
@@ -0,0 +1,52 @@
+package mitll.langtest.client.flashcard;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+/** The resources we use */
+public interface IconResources extends ClientBundle {
+
+  /** Get access to the css resource during gwt compilation */
+  @CssResource.NotStrict
+  @Source("resources/css/baseIcons.css")
+  CssResource css();
+
+  /** Our sample image icon. Makes the image resource for the gwt-compiler's css composer accessible */
+  @Source("resources/blueSpaceBar.png")
+  com.google.gwt.resources.client.ImageResource enter();
+
+  @Source("resources/animated_progress48.gif")
+  com.google.gwt.resources.client.ImageResource waiting();
+
+  @Source("resources/media-record-3.png")
+  com.google.gwt.resources.client.ImageResource record1();
+
+
+  @Source("resources/media-record-3_32x32.png")
+  com.google.gwt.resources.client.ImageResource record1Small();
+
+  @Source( "resources/media-record-4.png")
+  com.google.gwt.resources.client.ImageResource record2();
+
+  @Source( "resources/media-record-4_32x32.png")
+  com.google.gwt.resources.client.ImageResource record2Small();
+
+  @Source( "resources/checkmark48.png")
+  com.google.gwt.resources.client.ImageResource correct();
+
+
+  @Source( "resources/redx48.png")
+  com.google.gwt.resources.client.ImageResource incorrect();
+
+  @Source( "resources/record.png")
+  com.google.gwt.resources.client.ImageResource record();
+
+  @Source( "resources/stop.png")
+  com.google.gwt.resources.client.ImageResource stop();
+
+  @Source( "resources/white_48x48.png")
+  com.google.gwt.resources.client.ImageResource white();
+
+  @Source( "resources/gray_48x48.png")
+  com.google.gwt.resources.client.ImageResource gray();
+}
