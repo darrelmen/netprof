4043723ae014aaae67fa32f3287464635766d99e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-05 13:16:56 -0500

    fixes for issue where flashcard wouldn't work after a too short audio event, doesn't show pron feedback when wrong word, shows unit and chapter names from spreadsheet header row, warns if you press another key besides space, mouse button down works same way as key press
    Former-commit-id: 366949108a53a446eb409b658c41c554630a1417 [formerly b6cdfa33b97e2fe444988976de39bedf0d44127e] [formerly 6afd6c45059b2b51ddeefdefa2e96667a04d080a [formerly 418cd78cc2d41d5df0701d7aec7c08e90ea5e9c1]] [formerly 366949108a53a446eb409b658c41c554630a1417 [formerly b6cdfa33b97e2fe444988976de39bedf0d44127e] [formerly 6afd6c45059b2b51ddeefdefa2e96667a04d080a [formerly 418cd78cc2d41d5df0701d7aec7c08e90ea5e9c1]] [formerly 366949108a53a446eb409b658c41c554630a1417 [formerly b6cdfa33b97e2fe444988976de39bedf0d44127e] [formerly 6afd6c45059b2b51ddeefdefa2e96667a04d080a [formerly 418cd78cc2d41d5df0701d7aec7c08e90ea5e9c1]] [formerly 3a8f305f18607e531e5b76f58408bef67b20d6d7 [formerly 14feaad13de2f961bb0e3c31c939d79022c82b84] [formerly d4790e5b75ed025b26d60ad7605688a9638c81f4 [formerly 57ee1f038960327a7dfd8f537d9a270fef075bfb]]]]]
    Former-commit-id: 30f789cc60984ecc5c3600203bde8449bd1d3532 [formerly 5ae76bcd9e186bb24b10030deeed46cfab006549] [formerly 84216f2486ccdd5bb2a820d2d0be831c750d7ba0 [formerly f9adb9af9fc17eaa48c001b4ab4a9f6b394a130d]] [formerly 30f789cc60984ecc5c3600203bde8449bd1d3532 [formerly 5ae76bcd9e186bb24b10030deeed46cfab006549] [formerly 84216f2486ccdd5bb2a820d2d0be831c750d7ba0 [formerly f9adb9af9fc17eaa48c001b4ab4a9f6b394a130d]] [formerly 671701b77513f229ddfdd293491e1d417d623040 [formerly b22b6d70ce23509182eb789e51253f4bc5049946] [formerly 39317f6e63f58501dbc38ebb4114ce33a2a6562c [formerly ea85fe309b99d21f7628bb52218998ca0ade01e0]]]] [formerly 0ec796498b5906c296c7a20b5b52dd92a61257da [formerly abc26a8c69c1481991fdcbb6859c0bd3a6cb0424] [formerly 01092a6bdf88c7311d149c467e7ddcac9a70c4b3 [formerly 5dcc3e684b2bdb0f7dae085d22da1e2bac862b48]] [formerly 0ec796498b5906c296c7a20b5b52dd92a61257da [formerly abc26a8c69c1481991fdcbb6859c0bd3a6cb0424] [formerly 01092a6bdf88c7311d149c467e7ddcac9a70c4b3 [formerly 5dcc3e684b2bdb0f7dae085d22da1e2bac862b48]] [formerly 87ea133246f16894f7cb26d51ad2075f8af6fd1c [formerly 82795e72e806ff333f777da15399cc4b3652e0f2] [formerly 10d67058699bb6c66a6d9030548b1761f8df5204 [formerly 628cab3b494d180247293300cdd65fcd10164948]]]]]
    Former-commit-id: 0ec796498b5906c296c7a20b5b52dd92a61257da [formerly abc26a8c69c1481991fdcbb6859c0bd3a6cb0424] [formerly 01092a6bdf88c7311d149c467e7ddcac9a70c4b3 [formerly 5dcc3e684b2bdb0f7dae085d22da1e2bac862b48]]
    Former-commit-id: a2be6663c5924da1337331740e8ee81ca7161c71 [formerly ff1241ede901a693462f048924ecfd43f4685fbb] [formerly 98c13d7a8e1f7f46a9b3a21d85f4a9447f5bdb2e [formerly 3c43c35e0f7e744720a32d1950610074ab8ba453]] [formerly ae503531a516554a6a92a1bcdacd33f87d833e4a [formerly a554bd4044a4c5e1e6bb7427f056c96f380aa239] [formerly 89bbb539d176925ae964f2973d7a590e0b91a954 [formerly 1e0154624f68bf14673892f9c8b70b5f04e50c7c]]]
    Former-commit-id: 09d88e7f8357c39093fbf944477f677b14db5a92 [formerly 8f38e788fc268ec76844d4d5230d2c51bf7c4666] [formerly 43747d95b5933406c465d5241cc9cb8ae60f4484 [formerly 1c6f1bc11bc8ea45a8a31f00c3e0c04400e1c6b9]]
    Former-commit-id: ef621c99e24abcb3c2f0965936529ef719b7b86c [formerly 6ac99af3286406b7babc5c0e1c26b9971b264cc9]
    Former-commit-id: 964750778ff3cf5930ad31e9430ee2e834507a4b

--- src/mitll/langtest/server/scoring/SmallVocabDecoder.java
+++ src/mitll/langtest/server/scoring/SmallVocabDecoder.java
@@ -27,35 +27,14 @@ import java.util.Map;
 public class SmallVocabDecoder {
   private static final Logger logger = Logger.getLogger(ASRScoring.class);
 
-  public static final int VOCAB_SIZE_LIMIT = 200;
- // private static final String FINAL_BLEND_VOCAB = "finalBlend.vocab";
-/*  private static final String SMALL_LMOUT_SRILM = "smallLMOut.srilm";
-  private static final String LARGE_VOCAB_TXT = "largeVocab.txt";
-  private static final String BACKGROUND_LMOUT_SRILM = "backgroundLMOut.srilm";
-  private static final String COMBINED_SRILM = "combined.srilm";*/
-
-  /**
-   * How much weight to give the foreground vs the background lm.
-   */
- // private static final float BLEND_FOREGROUND_BACKGROUND = 0.8f;
-
   private static final String SMALL_LM_SLF = ASRScoring.SMALL_LM_SLF;
   /**
    * Limit on vocabulary size -- too big and dcodr will run out of memory and segfault
    */
-  //private static final int MAX_AUTO_CRT_VOCAB = 200;
-  // private static final String MAX_ORDER = "2";  // NOTE 3 does NOT work
   public static final String UNKNOWN_MODEL = "UNKNOWNMODEL";
   private static final String UNKNOWN_MODEL_BIAS = "-1.20";
-
   /**
-   * Platform -- windows, mac, linux, etc.
-   */
- // private final String platform = Utils.package$.MODULE$.platform();
- // private double foregroundBackgroundBlend;
-
-  /**
-   * @see mitll.langtest.server.LangTestDatabaseImpl#createSLFFile
+   * @see mitll.langtest.server.audio.AudioFileHelper#createSLFFile
    * @param lmSentences
    * @param tmpDir
    * @return
@@ -109,52 +88,7 @@ public class SmallVocabDecoder {
     return slfFile;
   }
 
-  /**
-   * Get the foreground and background sentences. <br></br>
-   * Create an srilm file using ngram-count <br></br>
-   * Create an slf file using HBuild <br></br>
-   * Do the octal conversion to utf-8 text on the result <br></br>
-   *
-   * This only works properly on the mac and linux, sorta emulated on win32
-   *
-   * @see ASRScoring#calcScoreForAudio
-   * @param lmSentences foreground sentences
-   * @param background background sentences
-   * @param tmpDir where everything is written
-   * @param modelsDir just for testing on windows
-   * @param scoringDir hydec location
-   * @return SLF file that is created, might not exist if any of the steps fail (e.g. if bin exes are not marked executable)
-   */
-/*  private String createSLFFile(List<String> lmSentences, List<String> background, List<String> vocab, String tmpDir, String modelsDir,
-                              String scoringDir) {
-    String convertedFile = tmpDir + File.separator + SMALL_LM_SLF;
-    if (platform.startsWith("win")) {
-      // hack -- get slf file from model dir
-      String slfDefaultFile = modelsDir + File.separator + SMALL_LM_SLF;
-      doOctalConversion(slfDefaultFile, convertedFile);
-    }
-    else {
-      String platform = Utils.package$.MODULE$.platform();
-      String pathToBinDir = scoringDir + File.separator + "bin." + platform;
-      //logger.info("platform  "+platform + " bins " + pathToBinDir);
-      File foregroundLMSentenceFile = writeLMToFile(lmSentences, tmpDir);
-      File foreGroundSRILMFile = runNgramCount(tmpDir, SMALL_LMOUT_SRILM, foregroundLMSentenceFile, null, pathToBinDir, true);
 
-      File backgroundLMSentenceFile = writeLMToFile(background, tmpDir);
-      String vocabFile = tmpDir + File.separator + LARGE_VOCAB_TXT;
-      writeVocab(vocabFile,vocab);
-      File backgroundSRILMFile = runNgramCount(tmpDir, BACKGROUND_LMOUT_SRILM, backgroundLMSentenceFile, vocabFile, pathToBinDir, false);
-
-      File combinedSRILM =runNgram(tmpDir, COMBINED_SRILM,foreGroundSRILMFile,backgroundSRILMFile,pathToBinDir);
-
-      //String slfFile = runHBuild(tmpDir,foreGroundSRILMFile,pathToBinDir); // only use foreground model
-      String slfFile = runHBuild(tmpDir,combinedSRILM,pathToBinDir);
-
-      doOctalConversion(slfFile, convertedFile);
-    }
-    if (!new File(convertedFile).exists()) logger.error("Couldn't create " +convertedFile);
-    return convertedFile;
-  }*/
 
   /**
    * Get the vocabulary to use when generating a language model. <br></br>
@@ -166,22 +100,7 @@ public class SmallVocabDecoder {
    * @return most frequent vocabulary words
    */
   public List<String> getVocab(List<String> background, int vocabSizeLimit) {
-    List<String> all = new ArrayList<String>();
-/*    all.add("-pau-");  // include?
-    all.add("<s>");
-    all.add("</s>");*/
-    /*, "<unk>"*/
-    List<String> simpleVocab = getSimpleVocab(background, vocabSizeLimit);
-    all.addAll(simpleVocab);
-
-/*    if (false) {
-      String sentence = background.iterator().next();
-      logger.debug("num sentences " + background.size() +
-        " first is " + sentence + " length " + sentence.length() + " got " + simpleVocab.size() + " tokens : '" + simpleVocab +
-        "'");
-    }*/
-
-    return all;
+    return getSimpleVocab(background, vocabSizeLimit);
   }
 
   /**
@@ -234,245 +153,4 @@ public class SmallVocabDecoder {
 
     return all;
   }
-
-  /**
-   * So for some reason the slf file generated from HBuild is in octal, so we have to convert it to UTF-8.
-   *
-   * @see #createSLFFile
-   * @param slfFile that is in octal
-   * @param convertedFile that will be in UTF-8
-   */
-/*  private void doOctalConversion(String slfFile, String convertedFile) {
-    try {
-      if (!new File(slfFile).exists()) {
-        if (platform.equals("win32")) {
-          logger.debug("slf file " + slfFile + " doesn't exist, skipping octal conversion.");
-        } else {
-          logger.error("slf file " + slfFile + " doesn't exist, skipping octal conversion.");
-        }
-      } else {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(slfFile), FileExerciseDAO.ENCODING));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(convertedFile), FileExerciseDAO.ENCODING));
-        String line2;
-        while ((line2 = reader.readLine()) != null) {
-          writer.write(package$.MODULE$.oct2string(line2).trim());
-          writer.write("\n");
-        }
-        reader.close();
-        writer.close();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-*/
-/*  private void writeVocab(String vocabFile, List<String> vocab) {
-    try {
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(vocabFile), FileExerciseDAO.ENCODING));
-
-      for (String v : vocab) {
-        writer.write(v +"\n");
-      }
-      writer.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }*/
-
-  /**
-   * Write LM sentences out, without punctuation.
-   *
-   * @see #createSLFFile
-   * @param lmSentences
-   * @param tmpDir
-   * @return smallLM file
-   */
-/*  private File writeLMToFile(List<String> lmSentences, String tmpDir) {
-    try {
-      File outFile = new File(tmpDir, "smallLM_" +lmSentences.size()+ ".txt");
-      logger.info("wrote lm with " + lmSentences.size() + " sentences to " +outFile.getAbsolutePath());
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), FileExerciseDAO.ENCODING));
-      for (String s : lmSentences) {
-        String punctRemoved = s.trim().replaceAll("\\p{P}", "");
-        writer.write(punctRemoved + "\n");
-      }
-      writer.close();
-      return outFile;
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return null;
-  }*/
-
-  /**
-   * ngram-count -text smallLM.txt -lm smallLmOut.srilm -write-vocab out.vocab -order 2 -cdiscount 0.0001 –unk
-   * ngram-count -text $1 -lm $2 -order 2 -kndiscount -interpolate -unk
-   * @param tmpDir
-   * @param lmFile
-   * @param pathToBinDir
-   * @return
-   */
-/*  private File runNgramCount(String tmpDir, String srllmOut, File lmFile, String vocabFile, String pathToBinDir, boolean isSmall) {
-    String srilm = tmpDir + File.separator + srllmOut;
-    ProcessBuilder soxFirst = isSmall ? new ProcessBuilder(pathToBinDir +File.separator+"ngram-count",
-        "-text",
-        lmFile.getAbsolutePath(),
-        "-lm",
-        srilm,
-        "-write-vocab",
-        tmpDir + File.separator + "small_out.vocab",
-        "-order",
-      MAX_ORDER,
-        "-cdiscount",
-        "0.0001"*//*,
-        "-unk"*//*
-    ) : new ProcessBuilder(pathToBinDir +File.separator+"ngram-count",
-        "-text",
-        lmFile.getAbsolutePath(),
-        "-lm",
-        srilm,
-        // "-gt1min", "3",
-        // "-gt2min", "3",
-        "-vocab",
-        vocabFile,
-        "-write-vocab",
-        tmpDir + File.separator + "large_out.vocab",
-        "-order",
-      MAX_ORDER,
-        "-kndiscount",
-        "-interpolate"*//*,
-        "-unk"*//*
-    );
-
-    logger.info("ran " +pathToBinDir +File.separator+"ngram-count"+" "+
-        "-text"+" "+
-        lmFile.getAbsolutePath()+" "+
-        "-lm"+" "+
-        srilm+" "+
-        //  "-gt1min"+" "+ "3"+" "+
-        //   "-gt2min"+" "+ "3"+" "+
-        "-write-vocab"+" "+
-        tmpDir + File.separator + "large_out.vocab"+" "+
-        "-order"+" "+
-      MAX_ORDER +" "+
-        "-kndiscount"+" "+
-        "-interpolate"*//*+" "+
-        "-unk"*//*);
-
-    try {
-      new ProcessRunner().runProcess(soxFirst);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    if (!new File(srilm).exists()) {
-      logger.info("didn't make " + srilm + " so trying again with cdiscount = 0.0001");
-      if (!isSmall) {
-        ProcessBuilder soxFirst2 = new ProcessBuilder(pathToBinDir +File.separator+"ngram-count",
-            "-text",
-            lmFile.getAbsolutePath(),
-            "-lm",
-            srilm,
-            //     "-gt1min", "3",
-            //    "-gt2min", "3",
-            "-write-vocab",
-            tmpDir + File.separator + "large_out.vocab",
-            "-order",
-          MAX_ORDER,
-            "-cdiscount",
-            "0.0001"*//*,
-            "-unk"*//*);
-
-        try {
-          new ProcessRunner().runProcess(soxFirst2);
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-
-
-    if (!new File(srilm).exists()) {
-      logger.error("didn't make " + srilm);
-    }
-    return new File(srilm);
-  }*/
-
-  /**
-   * By default the weight is 0.8 for the foreground model.
-   *
-   * @see #createSLFFile
-   * $BIN/ngram -lm $1 -lambda $3 -mix-lm $2 -order 2 -unk -write-lm $4.srilm -write-vocab $4.vocab
-   * @param tmpDir
-   * @param foregroundLM
-   * @param pathToBinDir
-   * @return
-   */
-/*  private File runNgram(String tmpDir, String srilmOut, File foregroundLM, File backgroundLM, String pathToBinDir) {
-    String srilm = tmpDir + File.separator + srilmOut;
-    ProcessBuilder ngram = new ProcessBuilder(pathToBinDir +File.separator+"ngram",
-        "-lm",
-        foregroundLM.getAbsolutePath(),
-        "-lambda",
-        ""+foregroundBackgroundBlend,
-        "-mix-lm",
-        backgroundLM.getAbsolutePath(),
-        "-order",
-      MAX_ORDER,
-       *//* "-unk",*//*
-        "-write-lm",
-        srilm,
-        "-write-vocab",
-        tmpDir + File.separator + FINAL_BLEND_VOCAB
-    );
-
-    try {
-      new ProcessRunner().runProcess(ngram);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    if (!new File(srilm).exists()) logger.error("didn't make " + srilm);
-    return new File(srilm);
-  }*/
-
-
-  /**
-   * HBuild -n smallLmOut.srilm -s '<s>' '</s>' out.vocab smallLM.slf
-   * @param tmpDir
-   * @param srilmFile                                                                       -
-   * @param pathToBinDir
-   */
-/*  private String runHBuild(String tmpDir, File srilmFile, String pathToBinDir) {
-    logger.info("running hbuild on " + srilmFile);
-    String slfOut = tmpDir + File.separator + "smallOctalLMFromHBuild.slf";
-    ProcessBuilder soxFirst = new ProcessBuilder(pathToBinDir +File.separator+"HBuild",
-        "-n",
-        srilmFile.getAbsolutePath(),
-        "-s",
-        "<s>",
-        "</s>",
-        tmpDir + File.separator + FINAL_BLEND_VOCAB,
-        slfOut
-    );
-
-    try {
-      new ProcessRunner().runProcess(soxFirst);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    if (!new File(slfOut).exists()) logger.error("runHBuild didn't make " + slfOut);
-    return slfOut;
-  }*/
-
-/*
-  public static void main(String []arg) {
-    String tmpDir = Files.createTempDir().getAbsolutePath();
-    String simpleSLFFile = new SmallVocabDecoder().createSimpleSLFFile(Arrays.asList("fred", "barney","fred barney","ace barry charley"), tmpDir);
-    //String simpleSLFFile = new SmallVocabDecoder().createSimpleSLFFile(Arrays.asList("fred"), tmpDir);
-   // String simpleSLFFile = new SmallVocabDecoder().createSimpleSLFFile(Arrays.asList("fred","barney"), tmpDir);
-    System.out.println("wrote to " +simpleSLFFile);
-  }
-*/
 }
