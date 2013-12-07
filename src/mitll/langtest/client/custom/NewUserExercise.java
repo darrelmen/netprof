4f6a3408a4b4442274ff33c79a0421935b11dc62
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-06 20:12:28 -0500

    allows recording fast and slow audio
    Former-commit-id: 3fe34b45cc9f9eb055d43a4559782dd759922d16 [formerly 7f6b05540dabd5569e4b9bb8bb7c504d68852b38] [formerly 5a20b6bed25a0757f5b46d15e3f6d76f237bc8cf [formerly 8e4a2e4e6e93b9d9be5db8bb3eedf0204dab46d8]] [formerly eb2dfef51d5c6b2371826f0c9a47303b68683750]
    Former-commit-id: af202888076ac450019703269f42defcf4e09edd
    Former-commit-id: 9ad4661ef12b4fea866549565593527c76838936 [formerly e13017683b0cffab74919688d8bee5a5aa75e698]
    Former-commit-id: 452d519fb69b0c48a6d5ef0a2d7df831b354c6d6

---
+++ src/mitll/langtest/client/custom/NewUserExercise.java
@@ -0,0 +1,273 @@
+package mitll.langtest.client.custom;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.ControlGroup;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.github.gwtbootstrap.client.ui.constants.ControlGroupType;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Panel;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.PagingContainer;
+import mitll.langtest.client.exercise.RecordAudioPanel;
+import mitll.langtest.client.exercise.WaveformPostAudioRecordButton;
+import mitll.langtest.client.scoring.PostAudioRecordButton;
+import mitll.langtest.client.user.BasicDialog;
+import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.AudioAnswer;
+import mitll.langtest.shared.custom.UserExercise;
+import mitll.langtest.shared.custom.UserList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 12/6/13
+ * Time: 5:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewUserExercise extends BasicDialog {
+ // private String slowPath, fastPath;
+  private UserExercise newUserExercise = null;
+  private final ExerciseController controller;
+  private LangTestDatabaseAsync service;
+  private UserManager userManager;
+  private HTML itemMarker;
+
+  public NewUserExercise(final LangTestDatabaseAsync service, final UserManager userManager, ExerciseController controller,HTML itemMarker) {
+    this.controller = controller;
+    this.service = service;
+    this.itemMarker = itemMarker;
+    this.userManager = userManager;
+  }
+
+  public Panel addNew(final UserList ul, final PagingContainer<?> pagingContainer, final Panel toAddTo) {
+    final FluidContainer container = new FluidContainer();
+    container.addStyleName("greenBackground");
+    FluidRow row;
+//    slowPath = null;
+ //   fastPath = null;
+/*    if (false) {
+      container.add(row);
+      final Heading header = new Heading(3, "Add a new item");
+      row.add(header);
+    }*/
+
+    row = new FluidRow();
+    container.add(row);
+    final BasicDialog.FormField english = addControlFormField(row, "English");
+
+    focusOnEnglish(english);
+
+    row = new FluidRow();
+    container.add(row);
+    final BasicDialog.FormField foreignLang = addControlFormField(row, "Foreign Language (" + controller.getLanguage() + ")");
+
+/*
+    row = new FluidRow();
+    container.add(row);
+    final Heading recordPrompt = new Heading(4, "Record normal speed reference recording");
+    row.add(recordPrompt);
+*/
+    row = new FluidRow();
+    container.add(row);
+
+    final CreateFirstRecordAudioPanel rap = makeRecordAudioPanel(row, english, foreignLang,true);
+    final ControlGroup normalSpeedRecording = addControlGroupEntry(row, "Record normal speed reference recording", rap);
+
+    final CreateFirstRecordAudioPanel rapSlow = makeRecordAudioPanel(row, english, foreignLang,false);
+    final ControlGroup slowSpeedRecording = addControlGroupEntry(row, "Record slow speed reference recording", rapSlow);
+    rap.setOtherRAP(rapSlow.getPostAudioButton());
+    rapSlow.setOtherRAP(rap.getPostAudioButton());
+
+    Button submit = makeCreateButton(ul, pagingContainer, toAddTo, /*row, */english, foreignLang, rap, normalSpeedRecording);
+ //   submit.addStyleName("marginBottomTen");
+    DOM.setStyleAttribute(submit.getElement(), "marginBottom", "5px");
+
+    Column column = new Column(2, 9, submit);
+    column.addStyleName("topMargin");
+    row.add(column);
+
+    return container;
+  }
+
+  private CreateFirstRecordAudioPanel makeRecordAudioPanel(final FluidRow row, final FormField english, final FormField foreignLang,boolean recordRegularSpeed) {
+    return new CreateFirstRecordAudioPanel(row, english, foreignLang,recordRegularSpeed);
+  }
+
+  private void focusOnEnglish(final FormField english) {
+    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+      public void execute() {
+        english.box.setFocus(true);
+      }
+    });
+  }
+
+  private Button makeCreateButton(final UserList ul, final PagingContainer<?> pagingContainer, final Panel toAddTo,
+                                //  FluidRow row,
+                                  final FormField english, final FormField foreignLang,
+                                  final RecordAudioPanel rap, final ControlGroup normalSpeedRecording) {
+    Button submit = new Button("Create");
+    submit.setType(ButtonType.SUCCESS);
+    submit.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        System.out.println("makeCreateButton : creating new item for " + english + " " + foreignLang);
+
+        if (validateForm(english, foreignLang, rap, normalSpeedRecording)) {
+//        /  System.out.println("really creating new item for " + english + " " + foreignLang + " created " + newUserExercise);
+
+    /*      newUserExercise.setRefAudio();
+          newUserExercise.setSlowRefAudio(slowPath);*/
+          newUserExercise.setEnglish(english.getText());
+          newUserExercise.setForeignLanguage(foreignLang.getText());
+          service.reallyCreateNewItem(ul, newUserExercise, new AsyncCallback<UserExercise>() {
+            @Override
+            public void onFailure(Throwable caught) {
+            }
+
+            @Override
+            public void onSuccess(UserExercise newExercise) {
+              ul.addExercise(newExercise);
+              itemMarker.setText(ul.getExercises().size() + " items");
+              pagingContainer.addAndFlush(newExercise);
+              toAddTo.clear();
+              toAddTo.add(addNew(ul, pagingContainer, toAddTo));
+            }
+          });
+        }
+      }
+    });
+    return submit;
+  }
+
+  private class CreateFirstRecordAudioPanel extends RecordAudioPanel {
+    private final FormField english;
+    private final FormField foreignLang;
+    boolean recordRegularSpeed = true;
+    private PostAudioRecordButton otherRAP;
+    private WaveformPostAudioRecordButton postAudioButton;
+
+    public CreateFirstRecordAudioPanel(FluidRow row, FormField english, FormField foreignLang,boolean recordRegularSpeed) {
+      super(null, NewUserExercise.this.controller, row, NewUserExercise.this.service, 0, false);
+      this.english = english;
+      this.foreignLang = foreignLang;
+      this.recordRegularSpeed = recordRegularSpeed;
+    }
+
+    @Override
+    protected void getEachImage(int width) {
+      float newWidth = Window.getClientWidth() * 0.65f;
+      super.getEachImage((int) newWidth);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    /**
+     * @see mitll.langtest.client.exercise.RecordAudioPanel#makePlayAudioPanel(com.google.gwt.user.client.ui.Widget)
+     * @return
+     */
+    @Override
+    protected WaveformPostAudioRecordButton makePostAudioRecordButton() {
+      postAudioButton =
+        new WaveformPostAudioRecordButton(exercise, controller, exercisePanel, this, service, 0) {
+          @Override
+          public void stopRecording() {
+            showStop();
+            System.out.println("WaveformPostAudioRecordButton.stopRecording with exercise " + exercise);
+            if (newUserExercise == null) {
+              // first we need to create an item to attach audio to it
+              NewUserExercise.this.service.createNewItem(userManager.getUser(), english.getText(), foreignLang.getText(),
+                new AsyncCallback<UserExercise>() {
+                @Override
+                public void onFailure(Throwable caught) {
+                  System.out.println("onFailure : stopRecording  " + caught);
+                }
+
+                @Override
+                public void onSuccess(UserExercise newExercise) {
+                  newUserExercise = newExercise;
+                  System.out.println("onSuccess : stopRecording with newUserExercise " + newUserExercise);
+
+                  exercise = newExercise.toExercise();
+                  otherRAP.setExercise(exercise);
+                  setExercise(exercise);
+                  stopRecording();
+                }
+              });
+            } else {
+              super.stopRecording();
+            }
+          }
+
+          @Override
+          public void startRecording() {
+            super.startRecording();
+            showStart();
+          }
+
+          @Override
+          public void flip(boolean first) {
+            super.flip(first);
+            flipRecordImages(first);
+          }
+
+          @Override
+          public void useResult(AudioAnswer result) {
+            super.useResult(result);
+            System.out.println("useResult : path to audio is " + result.path);
+            //  fastPath = result.path;
+            if (recordRegularSpeed) {
+              newUserExercise.setRefAudio(result.path);
+            } else {
+              newUserExercise.setSlowRefAudio(result.path);
+            }
+            System.out.println("newUserExercise " + newUserExercise);
+          }
+      };
+      postAudioButton.getElement().setId("NewUserExercise_WaveformPostAudioRecordButton");
+      return postAudioButton;
+    }
+
+    public void setOtherRAP(PostAudioRecordButton otherRAP) {
+      this.otherRAP = otherRAP;
+    }
+
+    public WaveformPostAudioRecordButton getPostAudioButton() {
+      return postAudioButton;
+    }
+  }
+
+  private boolean validateForm(final FormField english, final FormField foreignLang, final RecordAudioPanel rap,
+                               final ControlGroup normalSpeedRecording) {
+    if (english.getText().isEmpty()) {
+      markError(english, "Please enter an english word or phrase.");
+      return false;
+    } else if (foreignLang.getText().isEmpty()) {
+      markError(foreignLang, "Please enter the foreign language phrase.");
+      return false;
+    } else if (newUserExercise == null || newUserExercise.getRefAudio() == null) {
+
+      System.out.println("new user ex " + newUserExercise);
+
+      markError(normalSpeedRecording, rap.getButton(), rap.getButton(), "",
+        "Please record reference audio for the foreign language phrase.");
+      rap.getButton().addMouseOverHandler(new MouseOverHandler() {
+        @Override
+        public void onMouseOver(MouseOverEvent event) {
+          normalSpeedRecording.setType(ControlGroupType.NONE);
+        }
+      });
+      return false;
+    }
+    return true;
+  }
+}
