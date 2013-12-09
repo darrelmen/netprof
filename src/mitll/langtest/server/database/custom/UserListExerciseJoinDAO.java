12623b0dca69f9021be3e6cae5be7212c7701162
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-09 17:15:04 -0500

    database binding initially implemented for visitors and user exercises
    Former-commit-id: 35ff025e4900b56225e4167be4cc8a82e45d50c6
    Former-commit-id: f84a0e477053a60d9145157bd5752761453593b1

---
+++ src/mitll/langtest/server/database/custom/UserListExerciseJoinDAO.java
@@ -0,0 +1,125 @@
+package mitll.langtest.server.database.custom;
+
+import mitll.langtest.server.database.DAO;
+import mitll.langtest.server.database.Database;
+import mitll.langtest.server.database.UserDAO;
+import mitll.langtest.server.database.UserExerciseDAO;
+import mitll.langtest.shared.custom.UserExercise;
+import mitll.langtest.shared.custom.UserList;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 12/9/13
+ * Time: 2:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserListExerciseJoinDAO extends DAO {
+  private static Logger logger = Logger.getLogger(UserListExerciseJoinDAO.class);
+
+  public static final String USER_EXERCISE_LIST_EXERCISE = "userexerciselist_exercise";
+  //private UserDAO userDAO;
+
+  public UserListExerciseJoinDAO(Database database, UserDAO userDAO) {
+    super(database);
+    try {
+      createUserListTable(database);
+    } catch (SQLException e) {
+      logger.error("got " + e, e);
+    }
+   // this.userDAO = userDAO;
+  }
+
+  void createUserListTable(Database database) throws SQLException {
+    Connection connection = database.getConnection();
+    PreparedStatement statement;
+
+    statement = connection.prepareStatement("CREATE TABLE if not exists " +
+      USER_EXERCISE_LIST_EXERCISE +
+      " (" +
+      "uniqueid IDENTITY, " +
+      "userlistid LONG, " +
+      "exerciseid LONG, " +
+      "FOREIGN KEY(userlistid) REFERENCES " +
+      UserListDAO.USER_EXERCISE_LIST +
+      "(uniqueid)," +
+      "FOREIGN KEY(exerciseid) REFERENCES " +
+      UserExerciseDAO.USEREXERCISE +
+      "(uniqueid)" +
+      ")");
+    statement.execute();
+    statement.close();
+    database.closeConnection(connection);
+  }
+
+  /**
+   * Somehow on subsequent runs, the ids skip by 30 or so?
+   * <p/>
+   * Uses return generated keys to get the user id
+   *
+   * @see UserListManager#reallyCreateNewItem(mitll.langtest.shared.custom.UserList, mitll.langtest.shared.custom.UserExercise)
+   */
+  public void add(UserList userList, UserExercise exercise) {
+    long id = 0;
+
+    try {
+      // there are much better ways of doing this...
+      logger.info("add :userList " + userList);
+
+      Connection connection = database.getConnection();
+      PreparedStatement statement;
+
+      statement = connection.prepareStatement(
+        "INSERT INTO " + USER_EXERCISE_LIST_EXERCISE +
+          "(userlistid,exerciseid) " +
+          "VALUES(?,?);");
+      int i = 1;
+      //     statement.setLong(i++, userList.getUserID());
+      statement.setLong(i++, userList.getUniqueID());
+      statement.setLong(i++, exercise.getUniqueID());
+
+      int j = statement.executeUpdate();
+
+      if (j != 1)
+        logger.error("huh? didn't insert row for ");// + grade + " grade for " + resultID + " and " + grader + " and " + gradeID + " and " + gradeType);
+
+      ResultSet rs = statement.getGeneratedKeys(); // will return the ID in ID_COLUMN
+      if (rs.next()) {
+        id = rs.getLong(1);
+      } else {
+        logger.error("huh? no key was generated?");
+      }
+      logger.debug("unique id = " + id);
+
+      statement.close();
+      database.closeConnection(connection);
+
+      logger.debug("now " + getCount(USER_EXERCISE_LIST_EXERCISE) + " and user exercise is " + userList);
+    } catch (Exception ee) {
+      logger.error("got " + ee, ee);
+    }
+  }
+
+
+  /**
+   * Pulls the list of users out of the database.
+   *
+   * @return
+   */
+/*  public List<UserList> getAll() {
+    try {
+      String sql = "SELECT * from " + USER_EXERCISE_LIST + " order by modified";
+      return getUserLists(sql);
+    } catch (Exception ee) {
+      logger.error("got " + ee, ee);
+    }
+    return Collections.emptyList();
+  }*/
+
+}
