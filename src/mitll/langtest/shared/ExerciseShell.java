3823942c1f9e1008003dcac864619694d677b3f6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-10 15:28:29 -0500

    variety of bug fixes and almost got adding of predef items back again
    Former-commit-id: 5d4f3bf18d5e09371d1c00a5e6ad9825ed275387 [formerly 4052fd53920639fae9f68726db280d94dbc3d40a] [formerly 5d4f3bf18d5e09371d1c00a5e6ad9825ed275387 [formerly 4052fd53920639fae9f68726db280d94dbc3d40a] [formerly 079c499c3ff505cf3a181f51758b70c210e15a46 [formerly 35f376b78fd20dc4d6c0a496a6664416c70e0698]]]
    Former-commit-id: 325422500021614ce79d21fd0c926aac02b6660f [formerly 156d37d63c1bdb16ea11d4ce8031b2f2855f2d11] [formerly f594ecfdb7f223797a47fc83fd627a173f505182 [formerly fa21f5cd41bdc317e40a25ca268ac53c65fab01d]] [formerly 43af76b4edc6e5a3f053478df420cd39963ed529 [formerly 7c1566918f3ed6b316b4160f9e20e3bbdd61b0b6] [formerly 6a6b98988626d02dc3d609082f86ec5cbb5f0e6f [formerly adc225074229846c6c4ef466820847c53f348210]]]
    Former-commit-id: 43af76b4edc6e5a3f053478df420cd39963ed529 [formerly 7c1566918f3ed6b316b4160f9e20e3bbdd61b0b6]
    Former-commit-id: 00a6aa91d2249a22fae79490811334f7ec056a90 [formerly 1d3be2415b37e3e4bc17537965ab2ff63339fa8e]
    Former-commit-id: 4b3bba46ff4804d00f10d333e7cdec7ccb16af32

--- src/mitll/langtest/shared/ExerciseShell.java
+++ src/mitll/langtest/shared/ExerciseShell.java
@@ -20,6 +20,7 @@ public class ExerciseShell implements IsSerializable {
   }
 
   public String getID() { return id; }
+  protected void setID(String id) { this.id = id; }
   public String getTooltip() { return tooltip; }
   public void setTooltip(String tooltip) {
     this.tooltip = tooltip;
