68f4b62d274bc22f857aa66c954e65761c1e73e3
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-11 15:09:33 -0500

    more display fixes -- shows favorites first in list, fixed bug where radio buttons would interfere with each other
    Former-commit-id: 89eaeee2bb3696aa26e193eeca3c6afec63a7d6e [formerly f9e6abc28ffdf4b4b623a4dcc7a371271042545f] [formerly a8c861daee1cd6b54c12fd73039f040cafe1d433 [formerly 81d9df87d23c094150f1c8801d8dd1ad6587e83e]] [formerly 89eaeee2bb3696aa26e193eeca3c6afec63a7d6e [formerly f9e6abc28ffdf4b4b623a4dcc7a371271042545f] [formerly a8c861daee1cd6b54c12fd73039f040cafe1d433 [formerly 81d9df87d23c094150f1c8801d8dd1ad6587e83e]] [formerly 4144b97e278b18ba5a56225bf70465ae7532770f [formerly b423e8eb20a191bfc26615eb5b85834a85935046] [formerly 82cb8fe1f958e422527ae87e0c7b10906e1ceaa5 [formerly 179ca159cbf831549dd055e6fb591b39e91d58c8]]]]
    Former-commit-id: d8fbdd423c2c9d90296941149415673a1f4d0ba1 [formerly ad6dea48c2ab363494db5615cb644b42ca1ab22d] [formerly 77c72dae70717f6c1b1ae6bdc6f18fff48193fea [formerly eb569824df72f24261ebc4230cfe0e779b775aab]] [formerly f7b0ae55341e88d333075479c8596d57bf5bfcc5 [formerly 0059af934d9a50e596c4ee72586d868fb14551d1] [formerly 3a293d9a9fe84cb5a0edf18620e903840e0b8e8c [formerly 3494b1e9a6030762384b1e8c3c7187675c058d60]]] [formerly 229bf219bf9d43aafe9b49a4f3efc211fdcd034d [formerly eaa639548c9999fa85d0bd7a8b232fa0bb524e3a] [formerly 035919181da70c2c6d3dcfad2d5b73076cd399b8 [formerly 6964e630eb58be9a7078649c808a75c9dbe28a63]] [formerly 2b43e766f4027c218cc77da719b610f1c1cd5e72 [formerly 780b105cc6d3d341a679ec8ed3bc856a4c5b5efb] [formerly 2520a0f8829ee8933fdedfb222a17a059025a52e [formerly cd624fd789da96e222a1df5587cf67c3c7b16e3b]]]]
    Former-commit-id: 229bf219bf9d43aafe9b49a4f3efc211fdcd034d [formerly eaa639548c9999fa85d0bd7a8b232fa0bb524e3a] [formerly 035919181da70c2c6d3dcfad2d5b73076cd399b8 [formerly 6964e630eb58be9a7078649c808a75c9dbe28a63]]
    Former-commit-id: 4634db960a1825f3e4c5c0441e40a9a04e38971e [formerly f42650e426a42d4435738aec570f6bfa3a7b6c54] [formerly 7d10d8323f6f523196c2c9446bf841469b9450fe [formerly ec496faf52ce799edfb5763a5a9bdc0d9d5f5c05]]
    Former-commit-id: 1d57b4b5a5f1dd5f49483c69583b72bd906e9f8b [formerly dc86ccf9b8b24415062f855e29a9a4699a998b92]
    Former-commit-id: 0224624e3cbb1233d4bfb91c29d1b74641b394f2

--- src/mitll/langtest/client/scoring/AudioPanel.java
+++ src/mitll/langtest/client/scoring/AudioPanel.java
@@ -13,14 +13,11 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RequiresResize;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
-import mitll.langtest.client.sound.AudioControl;
 import mitll.langtest.client.sound.PlayAudioPanel;
 import mitll.langtest.client.sound.SoundManagerAPI;
 import mitll.langtest.shared.ImageResponse;
@@ -59,7 +56,6 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
   protected String audioPath;
   private final Map<String,Integer> reqs = new HashMap<String, Integer>();
   private int reqid;
- // private boolean useKeyboard = true;
 
   private ImageAndCheck waveform;
   private ImageAndCheck spectrogram;
@@ -87,6 +83,8 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
   public AudioPanel(String path, LangTestDatabaseAsync service,
                     boolean useKeyboard, ExerciseController controller, ScoreListener gaugePanel) {
     this(service, useKeyboard, controller, true, gaugePanel);
+    this.audioPath = path;
+
     addWidgets(path);
   }
 
@@ -115,8 +113,9 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
    * @return
    */
   public void addWidgets(String path) {
-    //System.out.println("addWidgets audio path = " + path);
+    System.out.println("AudioPanel.addWidgets audio path = " + path);
     imageContainer = new VerticalPanel();
+    imageContainer.getElement().setId("AudioPanel_imageContainer_"+path);
 
     HorizontalPanel hp = new HorizontalPanel();
     hp.setVerticalAlignment(ALIGN_MIDDLE);
@@ -155,8 +154,6 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
     add(new Heading(6));
 
     add(imageContainer);
-
-    this.audioPath = path;
   }
 
   public void doPause() { playAudio.doPause(); }
@@ -165,9 +162,7 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
    * This is sort of a hack -- so we can get left justify...
    * @return
    */
-  protected Widget getBeforePlayWidget() {
-    return null;
-  }
+  protected Widget getBeforePlayWidget() { return null; }
 
   @Override
   public void onLoad() {
@@ -262,7 +257,7 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
     */
   private PlayAudioPanel getPlayButtons(Widget toTheLeftWidget) {
     PlayAudioPanel playAudio = makePlayAudioPanel(toTheLeftWidget);
-    audioPositionPopup = new AudioPositionPopup();
+    audioPositionPopup = new AudioPositionPopup(imageContainer);
     playAudio.addListener(audioPositionPopup);
     return playAudio;
   }
@@ -276,13 +271,6 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
         }
         super.addButtons();
       }
-
-/*      @Override
-      protected void addKeyboardListener() {
-        if (useKeyboard) {
-          super.addKeyboardListener();
-        }
-      }*/
     };
   }
 
@@ -340,8 +328,6 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
    * @param imageAndCheck
    */
   private void getImageURLForAudio(final String path, final String type,int width, final ImageAndCheck imageAndCheck) {
-   // new Exception("get images").printStackTrace();
-
     int toUse = Math.max(MIN_WIDTH, width);
     float heightForType = type.equals(WAVEFORM) ? WAVEFORM_HEIGHT : SPECTROGRAM_HEIGHT;
     int height = Math.max(10,(int) (((float)Window.getClientHeight())/1200f * heightForType));
@@ -390,24 +376,17 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
         }
       });
     }
-    else {
-     // System.out.println("getImageURLForAudio : no audio path for " + type);
-    }
   }
 
   private void logMessage(ImageResponse result, long roundtrip) {
     String message = "getImageURLForAudio : (success) " + result + " took " + roundtrip + " millis, audio dur " +
       (result.durationInSeconds * 1000f) + " millis, " +
       " " + ((float) roundtrip / (float) (result.durationInSeconds * 1000f)) + " roundtrip/audio duration ratio.";
-   // System.out.println(message);
     service.logMessage(message, new AsyncCallback<Void>() {
       @Override
-      public void onFailure(Throwable caught) {
-      }
-
+      public void onFailure(Throwable caught) {}
       @Override
-      public void onSuccess(Void result) {
-      }
+      public void onSuccess(Void result) {}
     });
   }
 
@@ -415,7 +394,6 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
     synchronized (this) {
       int current = reqid++;
       reqs.put(type, current);
-    //  System.out.println("req map now " + reqs);
       return current;
     }
   }
@@ -450,108 +428,5 @@ public class AudioPanel extends VerticalPanel implements RequiresResize {
     p.setVisible(false);
     widget.check = p;
     return p;
-  }
-
-  private static int counter  = 0;
-
-  /**
-   * Makes a red line appear on top of the waveform/spectogram,etc. marking playback position.
-   */
-  private class AudioPositionPopup implements AudioControl {
-    private int id = 0;
-    private PopupPanel imageOverlay;
-    private float durationInMillis;
-
-    /**
-     * @see mitll.langtest.client.scoring.AudioPanel#getPlayButtons
-     */
-    public AudioPositionPopup() {
-      id = counter++;
-      imageOverlay = new PopupPanel(false);
-      imageOverlay.setStyleName("ImageOverlay");
-      SimplePanel w = new SimplePanel();
-      imageOverlay.add(w);
-      w.setStyleName("ImageOverlay");
-    }
-
-    /**
-     * @see AudioPanel#getImageURLForAudio(String, String, int, mitll.langtest.client.scoring.AudioPanel.ImageAndCheck)
-     */
-    public void reinitialize(double durationInSeconds) {
-      setWavDurationInSeconds(durationInSeconds);
-      reinitialize();
-    }
-
-    public void reinitialize() {
-      if (debug) System.out.println(this + "  : AudioPositionPopup.reinitialize ");
-
-      imageOverlay.hide();
-      int left = imageContainer.getAbsoluteLeft();
-      int top  = imageContainer.getAbsoluteTop();
-      imageOverlay.setPopupPosition(left, top);
-    }
-
-    private void setWavDurationInSeconds(double durationInSeconds) {
-      this.durationInMillis = (float)durationInSeconds*1000f;
-    }
-
-    public void songFirstLoaded(double durationEstimate) {}
-
-    /**
-     * @param durationInMillisIgnored
-     * @see PlayAudioPanel#songLoaded(double)
-     */
-    public void songLoaded(final double durationInMillisIgnored) {
-      int offsetHeight = imageContainer.getOffsetHeight();
-      int left = imageContainer.getAbsoluteLeft();
-      int top = imageContainer.getAbsoluteTop();
-      if (debug) System.out.println(this + " songLoaded " + durationInMillisIgnored + " height " + offsetHeight + " x " + left +
-          " y " + top);
-      imageOverlay.setSize("2px", offsetHeight + "px");
-      imageOverlay.setPopupPosition(left, top);
-      if (debug) System.out.println("songLoaded " + imageOverlay.isShowing() + " vis " + imageOverlay.isVisible() +
-          " x " + imageOverlay.getPopupLeft() + " y " + imageOverlay.getPopupTop() + " dim " +
-          imageOverlay.getOffsetWidth() + " x " + imageOverlay.getOffsetHeight());
-    }
-
-    /**
-     * @see PlayAudioPanel#update(double)
-     * @param position
-     */
-    public void update(double position) {
-      if (!imageOverlay.isShowing()) {
-        imageOverlay.show();
-      }
-      showAt(position);
-    }
-
-    /**
-     * So the story here is that the wav->mp3 conversion adds about 0.1 sec to the end of the mp3 file
-     * So to get the audio positionInMillis to translate to a positionInMillis on the image, we need to use the
-     * wav file duration from the server and not the mp3 duration {@link #setWavDurationInSeconds(double)}
-     *
-     * @param positionInMillis where soundmanager tells us the audio cursor is during playback
-     */
-    private void showAt(double positionInMillis) {
-      setHeightFromContainer();
-
-      float horizontalFraction = (float) positionInMillis / durationInMillis;
-      if (horizontalFraction > 1f) horizontalFraction = 1f;
-      int pixelProgress = (int) (((float) imageContainer.getOffsetWidth()) * horizontalFraction);
-      int left = imageContainer.getAbsoluteLeft() + pixelProgress;
-      int top = imageContainer.getAbsoluteTop();
-      imageOverlay.setPopupPosition(left, top);
-
-      if (debug) System.out.println(this + " showAt " + imageOverlay.isShowing() + " vis " + imageOverlay.isVisible() +
-          " x " + imageOverlay.getPopupLeft() + " y " + imageOverlay.getPopupTop() + " dim x " +
-          imageOverlay.getOffsetWidth() + " y " + imageOverlay.getOffsetHeight());
-    }
-
-    public void setHeightFromContainer() {
-      int offsetHeight = imageContainer.getOffsetHeight();
-      imageOverlay.setSize("2px", offsetHeight + "px");
-    }
-
-    public String toString() { return "popup #" +id; }
   }
 }
