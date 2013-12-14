a58d39bd8423edf69fc3794cb97411f8fa582c51
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-13 20:31:32 -0500

    Added review tab, which lists all the items marked incorrect
    Former-commit-id: d29a9c78109b6b3fdbfeb39b53214294439ce841
    Former-commit-id: 3dbcf362b104bcdfc0b2d1aaa5510c64ccfda280

--- src/mitll/langtest/client/LangTestDatabaseAsync.java
+++ src/mitll/langtest/client/LangTestDatabaseAsync.java
@@ -152,5 +152,7 @@ public interface LangTestDatabaseAsync {
 
   void addAnnotation(String exerciseID, String field, String status, String comment, AsyncCallback<Void> async);
 
-  void markReviewed(String id, AsyncCallback<Void> asyncCallback);
+  void markReviewed(String id, boolean isCorrect, AsyncCallback<Void> asyncCallback);
+
+  void getReviewList(AsyncCallback<UserList> async);
 }
