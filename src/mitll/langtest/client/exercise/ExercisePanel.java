83984ae081d32bbedee19bd55aeb8e40e4544d49
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-16 19:09:49 -0500

    turn off arrow key keyboard binding
    Former-commit-id: 66a35cdc7d9273d57f7f772be9b49311e8dc41f4 [formerly 1f04d9cfdb31c5dff2bc0549793b00d802edd166] [formerly a07fe6cab7091e4a431e5dd5d986c2d17e760412 [formerly e4d285a29394f6ba4672c1b212d8ad3e208787ae]] [formerly 9d42e090463acf662506e5d817e1be88f9ce3c13 [formerly 325d993bd433e2937034609bd2835cae787a1693] [formerly 784bb460903e3735b8919c2046e9bd3a3bb6a1a0 [formerly bc22d982a72ab360fccb9c6cd80bc9c464bb3f2a]]]
    Former-commit-id: 218a6587f9340f5e7ecc6c64503e3d3a6f378198 [formerly 36260833ff6e85e0e8687d14d30c195c1752533a] [formerly 03c1e8044433e920a9b2ce6a2d49761d80ad6d0e [formerly 0f2d94a8f85d36263d026eb90bfaf7eea4266c1d]]
    Former-commit-id: 431f0ffca3c5afededf18594bc17ae75eddbcdaa [formerly 751a163c2b4c6eb677df7401a94b1c491ad334c0]
    Former-commit-id: 2513c35c5d1b9a2c903bf66dbfb1917f5bcca1d1

--- src/mitll/langtest/client/exercise/ExercisePanel.java
+++ src/mitll/langtest/client/exercise/ExercisePanel.java
@@ -125,7 +125,7 @@ public class ExercisePanel extends VerticalPanel implements
   }
 
   protected NavigationHelper getNavigationHelper(ExerciseController controller) {
-    return new NavigationHelper(exercise,controller, this, exerciseList, true);
+    return new NavigationHelper(exercise,controller, this, exerciseList, false);
   }
 
   protected void addInstructions() {
