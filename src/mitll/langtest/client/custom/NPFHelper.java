ff553ddd92411e2508e772d9a7c9efb7b976f7b8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-18 19:46:09 -0500

    progress on intermittent play button issues
    Former-commit-id: c59e4a875b27b5480a72a9fe08ffe7b3cbc8b391 [formerly a5c863f7199025c94004ad0975456b2fee2e91f9] [formerly 870df04583d3d9bb22b68b6b464b82a93a9a1a23 [formerly 2eece972c7108318f77aa3ee398768d04e2df036]] [formerly c59e4a875b27b5480a72a9fe08ffe7b3cbc8b391 [formerly a5c863f7199025c94004ad0975456b2fee2e91f9] [formerly 870df04583d3d9bb22b68b6b464b82a93a9a1a23 [formerly 2eece972c7108318f77aa3ee398768d04e2df036]] [formerly 68f29bf70cd8a1ba7a1cb30fabda6e247bd1b6aa [formerly 656ee16c74090cda99a1367888467057d81ff531] [formerly f2089699f46ba56b057becc3ed2c35ee39ca2cf6 [formerly 3472ef96700d8f0e694f297d58661dcf5e1bc697]]]]
    Former-commit-id: 89f15e61c709bf9bf8ea3df42f13c677a826877d [formerly 524e26eded28046f7ba4149ba4cbc355be8e1a0c] [formerly 8c7c17d5b3ebc5ff55da484311bc266073ab2182 [formerly 8c70f02ad5e1e5690392a84d6eb694c2cf034282]] [formerly 7497e88b051bb551e69431dc81fabd4d406d8483 [formerly bb3b1be7bbee1cb679df079308ef36cf5d44622f] [formerly 51ed4bebf67c439c6cb54dcc0837332201d8bc05 [formerly 3d27fe27f5d8a1bf42aaa5577b4848324225def5]]] [formerly 7b6739e4fae4318e9737e123fe931c110ecf01b0 [formerly aca6a14268d858d7d83e16e560935cf84ca86e77] [formerly e6a9ada9b8807ca21fdff2cc608793577013aa9e [formerly 97cf11b8577e60a70ae06fd5c04f7ec5a3091555]] [formerly a85988f060512a69f76f8aebe10b108a8bfe7e16 [formerly 1f773c339c9bcf2edcaf0f5e4f9f69787b6a5a30] [formerly 889a21ed296c95b90c55cdfc71baea77dfd94429 [formerly 341a2b5e7d2a46067e5bf296c927c9a65ec9297b]]]]
    Former-commit-id: 7b6739e4fae4318e9737e123fe931c110ecf01b0 [formerly aca6a14268d858d7d83e16e560935cf84ca86e77] [formerly e6a9ada9b8807ca21fdff2cc608793577013aa9e [formerly 97cf11b8577e60a70ae06fd5c04f7ec5a3091555]]
    Former-commit-id: bbc9f7cbf5c1fca8e4f0200528ed5b3cfcd80af3 [formerly 9bd4ff3aab4fc4320845296976221b213e512e4d] [formerly e0745e7c43854fa309579b9e675f4e3941b5bb54 [formerly 8b525f045e82e8b37de8853aa87f0e5197f6e028]]
    Former-commit-id: 987a268713305f3cb169e9c49a0776f206cfb144 [formerly dff42118611dd22dbd308c034849051b30ff6471]
    Former-commit-id: 376023da75dbd87db9adcc182fcebb8643df788d

--- src/mitll/langtest/client/custom/NPFHelper.java
+++ src/mitll/langtest/client/custom/NPFHelper.java
@@ -136,15 +136,15 @@ public class NPFHelper implements RequiresResize {
   }
 
   protected void setFactory(final PagingExerciseList exerciseList, final String instanceName) {
-    exerciseList.setFactory(new GoodwaveExercisePanelFactory(service, feedback, controller, exerciseList, 0.7f) {
+    exerciseList.setFactory(new GoodwaveExercisePanelFactory(service, feedback, controller, exerciseList, 1.0f) {
       @Override
       public Panel getExercisePanel(Exercise e) {
         System.out.println("\nmaking new GoodwaveExercisePanel for " +e + " instance " + instanceName);
         if (controller.getAudioType().equalsIgnoreCase(Result.AUDIO_TYPE_REVIEW)) {
-          return new QCNPFExercise(e, controller, exerciseList, 0.65f, false, instanceName);
+          return new QCNPFExercise(e, controller, exerciseList, 1.0f, false, instanceName);
         }
         else {
-          return new NPFExercise(e, controller, exerciseList, 0.65f, false, instanceName);
+          return new NPFExercise(e, controller, exerciseList, 1.0f, false, instanceName);
         }
       }
     }, userManager, 1);
