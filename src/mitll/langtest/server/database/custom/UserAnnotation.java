19cdcd880d0dd4febd6d860256fde281be6fe5d5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-19 23:00:14 -0500

    steps toward annotation dao
    Former-commit-id: 1e4a207d2e88f232cfa384e7c620addd61443e63 [formerly a75d200e9d3001fcbd9722f3faf26795b39b880d] [formerly 3f07f5812598e9ce1c6218e42d5cc05369692554 [formerly 6bbd9d7ab4c07cc770466c8f67acba747cc9e717]] [formerly 2f100a5c25bb3f401ddcb3df91009fe932ff5d8c [formerly 15caebaef0a0ae9dc0d7d4563b9246c01f851560] [formerly 29bf52152e40388e804929ea267b4367433edda5 [formerly 3bd1fe08933ff033fd1d9b2b32144fb97315b047]]]
    Former-commit-id: 9f1e6772487d6f818359eeb86776b2cf4941e140 [formerly 6be1907ed2d5e4bbd83658a151b8f0b0ae0f8e3b] [formerly 7c000b69a8e61480489dda56cc901c0b57cb4e75 [formerly fc36aacbfc257b34984db39c0f944c94f76c31d5]] [formerly ead6c8e24b9b7fa991633906954bb3a883753e1a [formerly 8057182ad3932cb2924c8a242eb9cd1fc4dbc3b2] [formerly a27aa796081b8fafa9cb1fd3993d6e87e36edfab [formerly 72199e9229e4c14c571649fcf479a7a02f21effb]]]
    Former-commit-id: 3ba700851ba979c046088e84b6c8c0af02173341 [formerly 9043d0ae0fe3d161f4828efe6296074de26a9979] [formerly 79ce15f57799a20ddfe83daea5c7c2df2f443fbe [formerly 6592253adcd6d76d2c52c76b3fb01ecf396b207c]]
    Former-commit-id: 2d4119fcbf77040a751c57064656100ed3615ab2 [formerly 35d4b61513b40c802c55188ded095afc46687ab8]
    Former-commit-id: 24621f6c08fada7f655b2b42a61bd03daf07d87e

--- src/mitll/langtest/server/database/custom/UserAnnotation.java
+++ src/mitll/langtest/server/database/custom/UserAnnotation.java
@@ -8,12 +8,51 @@ package mitll.langtest.server.database.custom;
  * To change this template use File | Settings | File Templates.
  */
 public class UserAnnotation {
-  String exerciseID; String field; String status; String comment;
-  // long timestamp
-  public UserAnnotation(String exerciseID, String field, String status, String comment) {
+  private long uniqueID;
+  private String exerciseID; private String field; private String status; private String comment;
+  private long creatorID;
+
+   private long timestamp;
+  public UserAnnotation(String exerciseID, String field, String status, String comment, long userID) {
     this.exerciseID = exerciseID;
     this.field = field;
     this.status = status;
     this.comment =comment;
+    this.creatorID = userID;
   }
+
+
+  public String getExerciseID() {
+    return exerciseID;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public long getCreatorID() {
+    return creatorID;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public void setUniqueID(long uniqueID) {
+    this.uniqueID = uniqueID;
+  }
+
+  public String toString() { return "Annotation " + getExerciseID() + "/"+ getField() + " : " + getStatus() +"/" + getComment() + " by " + getCreatorID();}
 }
