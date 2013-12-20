911768f11f3224ef23bb235855c00af604e6f4a3
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-20 13:24:52 -0500

    fixed last set of bugs with assigning comments to fast or slow speed audio
    Former-commit-id: 5cb19073f7376e0c4edb2a3a24d350cf2e3ab319 [formerly 1841dcf977f54c88acdf10cedeaab2f37d27b16e] [formerly 5cb19073f7376e0c4edb2a3a24d350cf2e3ab319 [formerly 1841dcf977f54c88acdf10cedeaab2f37d27b16e] [formerly 5cb19073f7376e0c4edb2a3a24d350cf2e3ab319 [formerly 1841dcf977f54c88acdf10cedeaab2f37d27b16e] [formerly 71fc86ceba90368edf2d77a15b92e86cc2ccd67a]]]
    Former-commit-id: b8b3a4d601bf2bdbc4bb7096f9c142f457d37211 [formerly 1a9e07d29fabc2883e69e354e0af06848941a229] [formerly b8b3a4d601bf2bdbc4bb7096f9c142f457d37211 [formerly 1a9e07d29fabc2883e69e354e0af06848941a229] [formerly 6fe40fa000c7d2d6c6367cf9f9d573dba1701b86]] [formerly a8750d434974d3d79ad22d7efc578f8d6fa8f43b [formerly a057c5be69cf22b8f119890a3049f8af26bba420] [formerly a8750d434974d3d79ad22d7efc578f8d6fa8f43b [formerly a057c5be69cf22b8f119890a3049f8af26bba420] [formerly cabea04958a1c7821a89b8958042a616406ff1d8]]]
    Former-commit-id: a8750d434974d3d79ad22d7efc578f8d6fa8f43b [formerly a057c5be69cf22b8f119890a3049f8af26bba420]
    Former-commit-id: bd8bcebe8332feaa2332e17ab121b9cd2a7b5924 [formerly c30ab638cc71288f24352309af654689b5b4973f] [formerly abc066b14692e1522a280a8ec603593eefe655c3]
    Former-commit-id: 5e7ad871312e2e05d105ad94feadcae7fe4ffc27
    Former-commit-id: 452ad468e81a7f83ee530c4a7ea3f23eb4b99d0f

---
+++ src/mitll/langtest/server/database/custom/ReviewedDAO.java
@@ -0,0 +1,138 @@
+package mitll.langtest.server.database.custom;
+
+import mitll.langtest.server.database.DAO;
+import mitll.langtest.server.database.Database;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 12/9/13
+ * Time: 2:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReviewedDAO extends DAO {
+  private static Logger logger = Logger.getLogger(ReviewedDAO.class);
+
+  public static final String REVIEWED = "reviewed";
+
+  public ReviewedDAO(Database database) {
+    super(database);
+    try {
+      createTable(database);
+    } catch (SQLException e) {
+      logger.error("got " + e, e);
+    }
+  }
+
+  /**
+   *   String exerciseID; String field; String status; String comment;
+   String userID;
+   * @param database
+   * @throws java.sql.SQLException
+   */
+  void createTable(Database database) throws SQLException {
+    Connection connection = database.getConnection();
+    PreparedStatement statement;
+
+    statement = connection.prepareStatement("CREATE TABLE if not exists " +
+      REVIEWED +
+      " (" +
+      "uniqueid IDENTITY, " +
+      "creatorid LONG, " +
+      "exerciseid VARCHAR, " +
+      "modified TIMESTAMP,"+
+      "FOREIGN KEY(creatorid) REFERENCES " +
+      "USERS" +
+      "(ID)" +
+      ")");
+    statement.execute();
+    statement.close();
+    database.closeConnection(connection);
+  }
+
+
+  /**
+   * Somehow on subsequent runs, the ids skip by 30 or so?
+   * <p/>
+   * Uses return generated keys to get the user id
+   *
+   * @see UserListManager#reallyCreateNewItem(long, mitll.langtest.shared.custom.UserExercise)
+   */
+  public void add(String exerciseID, long creatorID) {
+    long id = 0;
+
+    try {
+      // there are much better ways of doing this...
+      logger.info("add :reviewed " + exerciseID + " by " + creatorID);
+
+      Connection connection = database.getConnection();
+      PreparedStatement statement;
+
+      statement = connection.prepareStatement(
+        "INSERT INTO " + REVIEWED +
+          "(creatorid,exerciseid,modified) " +
+          "VALUES(?,?,?);");
+      int i = 1;
+      //     statement.setLong(i++, annotation.getUserID());
+      statement.setLong(i++, creatorID);
+      statement.setString(i++, exerciseID);
+      statement.setTimestamp(i++, new Timestamp(System.currentTimeMillis()));
+
+      int j = statement.executeUpdate();
+
+      if (j != 1)
+        logger.error("huh? didn't insert row for ");// + grade + " grade for " + resultID + " and " + grader + " and " + gradeID + " and " + gradeType);
+/*
+      ResultSet rs = statement.getGeneratedKeys(); // will return the ID in ID_COLUMN
+      if (rs.next()) {
+        id = rs.getLong(1);
+      } else {
+        logger.error("huh? no key was generated?");
+      }
+      logger.debug("unique id = " + id);*/
+
+      statement.close();
+      database.closeConnection(connection);
+
+    logger.debug("now " + getCount() + " reviewed");
+    } catch (Exception ee) {
+      logger.error("got " + ee, ee);
+    }
+  }
+
+  public int getCount() { return getCount(REVIEWED); }
+
+  public Set<String> getReviewed()  {
+    Connection connection = database.getConnection();
+    String sql = "SELECT DISTINCT exerciseid from " + REVIEWED + " order by exerciseid";
+
+    Set<String> lists = Collections.emptySet();
+    try {
+      PreparedStatement statement = connection.prepareStatement(sql);
+      ResultSet rs = statement.executeQuery();
+      lists = new TreeSet<String>();
+
+      while (rs.next()) {
+        lists.add(rs.getString(1));
+      }
+
+      logger.debug("getUserAnnotations sql " + sql + " yielded " + lists);
+      rs.close();
+      statement.close();
+      database.closeConnection(connection);
+    } catch (SQLException e) {
+      logger.error("Got " +e + " doing " + sql,e);
+    }
+    return lists;
+  }
+}
