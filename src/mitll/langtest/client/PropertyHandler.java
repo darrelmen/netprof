3c6cd248465f2271cf4b4b656a0852e4e8d19a85
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2013-12-23 18:51:22 -0500

    changes to support data collection - better completion marking, records fast and slow separately
    Former-commit-id: 9e26ccd4f1698d5cc3094c21d4b14ed157993b50 [formerly dacf8ce87c476e3dceeca42fb10742c195b40e88] [formerly 9e26ccd4f1698d5cc3094c21d4b14ed157993b50 [formerly dacf8ce87c476e3dceeca42fb10742c195b40e88] [formerly 9e26ccd4f1698d5cc3094c21d4b14ed157993b50 [formerly dacf8ce87c476e3dceeca42fb10742c195b40e88] [formerly 68fac342a2b190bdfbe5255a6983a79fbc4536f4]]]
    Former-commit-id: 3c737ace98a3eb7daae469e979b0d44aff25d082 [formerly 67ad988a8f6e5bfcc0ed0cf1fdda80541621383a] [formerly 3c737ace98a3eb7daae469e979b0d44aff25d082 [formerly 67ad988a8f6e5bfcc0ed0cf1fdda80541621383a] [formerly 51d7f1990e272628b26c173d10c3ead3bb253176]] [formerly 6de8de1a7705292d1b7a3dabefdf903f5a1a01c9 [formerly 728454c8de455d006a73772b36b3c61548122b8f] [formerly 6de8de1a7705292d1b7a3dabefdf903f5a1a01c9 [formerly 728454c8de455d006a73772b36b3c61548122b8f] [formerly 603b6d52fea41892d4949439afc642afab90f4f5]]]
    Former-commit-id: 6de8de1a7705292d1b7a3dabefdf903f5a1a01c9 [formerly 728454c8de455d006a73772b36b3c61548122b8f]
    Former-commit-id: 5f52fa673020dfdbdb4e8da8f03764ec277e919d [formerly 579b33beb3881a5874986c35c6e2f6330b097f3b] [formerly a6a7e951eb6c81014650c2ad72c3909fd4e2ff27]
    Former-commit-id: dde7f6fb082ba10a99e3a006fb70d48836cbbb5b
    Former-commit-id: 3686267f54e5b6ed5581cbca8dff04b552190877

--- src/mitll/langtest/client/PropertyHandler.java
+++ src/mitll/langtest/client/PropertyHandler.java
@@ -1,6 +1,7 @@
 package mitll.langtest.client;
 
 import com.google.gwt.user.client.Window;
+import mitll.langtest.shared.Result;
 
 import java.util.Map;
 
@@ -85,6 +86,8 @@ public class PropertyHandler {
   private static final String ALLOW_PLUS_IN_URL = "allowPlusInURL";
   private static final String PURPOSE_DEFAULT = "purposeDefault";
   private static final String CLASSROOM_MODE = "classroomMode";
+  private static final String DEFAULT_AUDIO_TYPE = "audioType";
+  private String audioType = Result.AUDIO_TYPE_FAST_AND_SLOW;
 
   public enum LOGIN_TYPE { UNDEFINED, ANONYMOUS, STUDENT, DATA_COLLECTOR, SIMPLE }
 
@@ -203,6 +206,7 @@ public class PropertyHandler {
       else if (key.equals(BIND_NEXT_TO_ENTER)) bindNextToEnter = getBoolean(value);
       else if (key.equals(SCREEN_PORTION)) screenPortion = getFloat(value,1.0f,SCREEN_PORTION);
       else if (key.equals(CLASSROOM_MODE)) classroomMode = getBoolean(value);
+      else if (key.equals(DEFAULT_AUDIO_TYPE)) audioType = value;
       else if (key.equals(LOGIN_TYPE_PARAM)) {
         try {
           loginType = LOGIN_TYPE.valueOf(value.toUpperCase());
@@ -527,4 +531,5 @@ public class PropertyHandler {
   }
 
   public boolean isClassroomMode() { return classroomMode; }
+  public String getAudioType() { return audioType; }
 }
