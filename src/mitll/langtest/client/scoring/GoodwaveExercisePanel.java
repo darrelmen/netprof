82ee9de75b5d2096a59a733e300c090f6bdd06e0
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-06 16:40:31 -0500

    fixed bug where didn't record both fast and slow audio comments properly
    Former-commit-id: 9e1215f778eb1c8210c9c66bf25934962617f922 [formerly 9e1215f778eb1c8210c9c66bf25934962617f922 [formerly 13abbe72a561fc8c9a76525b3769e648ddb9a4a1]]
    Former-commit-id: c78a90e8e834b49deaf01eeb9d6a48dbac17ba55 [formerly 08bd542b655c35f53586d2feef8bc23e284e88fe] [formerly 9604218c2685fdbeaf025bfcf09eda683af2f877 [formerly bbc4a1aba2d1a914526485e3b6773f329669913a]]
    Former-commit-id: 9604218c2685fdbeaf025bfcf09eda683af2f877
    Former-commit-id: 201cbf4b63f2b7be94bb7eb0c934b9c190118e4a

--- src/mitll/langtest/client/scoring/GoodwaveExercisePanel.java
+++ src/mitll/langtest/client/scoring/GoodwaveExercisePanel.java
@@ -475,7 +475,7 @@ public class GoodwaveExercisePanel extends HorizontalPanel implements BusyPanel,
 
     private class MyPostAudioRecordButton extends PostAudioRecordButton {
       public MyPostAudioRecordButton(ExerciseController controller) {
-        super(exercise, controller, ASRRecordAudioPanel.this.service, ASRRecordAudioPanel.this.index, false, true);
+        super(exercise, controller, ASRRecordAudioPanel.this.service, ASRRecordAudioPanel.this.index, true);
       }
 
       @Override
