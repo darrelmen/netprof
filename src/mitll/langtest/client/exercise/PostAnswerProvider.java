06421b8f87cb888206ee73651e0e51829ae2e82b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-13 17:26:25 -0500

    added comment tab, fixed button to indicate when an item has been reviewed and is now fixed
    Former-commit-id: 458734f6df05366cbcbecb02e0cf498664fae2ee [formerly 2658c3b593a1f3a90181470a7cd776b59834d8d4] [formerly 2f4dcf4c5705d8885adb7c364322d5225244c879 [formerly 3d5d9d6d0da7d19bd73a118ebf274ba6ede3236c]]
    Former-commit-id: cd2c476f379093400437f6d8c2625a416366f944 [formerly 11e8eb9ae1cc2fff81dd31ccfe1c9f09cf1d30fa]
    Former-commit-id: a1ab618ce3660aa3c1e90582d89bb808ba89d49b

--- src/mitll/langtest/client/exercise/PostAnswerProvider.java
+++ src/mitll/langtest/client/exercise/PostAnswerProvider.java
@@ -1,6 +1,7 @@
 package mitll.langtest.client.exercise;
 
 import mitll.langtest.shared.Exercise;
+import mitll.langtest.shared.ExerciseShell;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,6 +10,6 @@ import mitll.langtest.shared.Exercise;
  * Time: 11:39 AM
  * To change this template use File | Settings | File Templates.
  */
-public interface PostAnswerProvider {
-  void postAnswers(ExerciseController controller, Exercise completedExercise);
+public interface PostAnswerProvider/*<T extends ExerciseShell>*/ {
+  /*<T extends ExerciseShell>*/ void postAnswers(ExerciseController controller, ExerciseShell completedExercise);
 }
