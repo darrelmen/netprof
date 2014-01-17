09568997d9b26e933faa01193b4956f7b3f9e03a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-17 12:06:26 -0500

    removed unused code and files, removed split audio stuff (presuming that's moot with new collection approach)
    Former-commit-id: 103342f602d21e8d36c71fb37f4078b5de84f24a [formerly 103342f602d21e8d36c71fb37f4078b5de84f24a [formerly 5f41563b2956beaba0ae55c17e4458028c7017c8]]
    Former-commit-id: 90308315522901a47f68effc8fe727c76e51f828 [formerly aa8505f79f67d2a86239d5639c8fc8da4d823a1c] [formerly 9042a4964ce8c075f60d922cf8b578090af2dea1 [formerly 16f8d16e8f67478914ceede0f2a75e67958d555f]]
    Former-commit-id: 9042a4964ce8c075f60d922cf8b578090af2dea1
    Former-commit-id: e0f2d4348627ae4c31688c5d2360df5dfcffaa10

--- src/mitll/langtest/server/scoring/SmallVocabDecoder.java
+++ src/mitll/langtest/server/scoring/SmallVocabDecoder.java
@@ -1,13 +1,7 @@
 package mitll.langtest.server.scoring;
 
-import mitll.langtest.server.database.FileExerciseDAO;
 import org.apache.log4j.Logger;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -26,69 +20,6 @@ import java.util.Map;
  */
 public class SmallVocabDecoder {
   private static final Logger logger = Logger.getLogger(ASRScoring.class);
-
-  private static final String SMALL_LM_SLF = ASRScoring.SMALL_LM_SLF;
-  /**
-   * Limit on vocabulary size -- too big and dcodr will run out of memory and segfault
-   */
-  public static final String UNKNOWN_MODEL = "UNKNOWNMODEL";
-  private static final String UNKNOWN_MODEL_BIAS = "-1.20";
-  /**
-   * @see mitll.langtest.server.audio.AudioFileHelper#createSLFFile
-   * @param lmSentences
-   * @param tmpDir
-   * @return
-   */
-  public String createSimpleSLFFile(Collection<String> lmSentences, String tmpDir) {
-    String slfFile = tmpDir + File.separator + SMALL_LM_SLF;
-
-    try {
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(slfFile), FileExerciseDAO.ENCODING));
-      writer.write("VERSION=1.0\n");
-
-      int linkCount = 0;
-      StringBuilder nodesBuf = new StringBuilder();
-      nodesBuf.append("I=0 W=<s>\n");
-      nodesBuf.append("I=1 W=</s>\n");
-      int newNodes = 2;
-      StringBuilder linksBuf = new StringBuilder();
-      Collection<String> sentencesToUse = new ArrayList<String>(lmSentences);
-      sentencesToUse.add(UNKNOWN_MODEL);
-      for (String sentence : sentencesToUse) {
-        Collection<String> tokens = getTokens(sentence);
-        //logger.debug("\tfor " + sentence + " tokens are " + tokens);
-        int start = 0;
-
-        for (String token : tokens) {
-          int next = newNodes++;
-          linksBuf.append("J=" + (linkCount++) + " S=" + start + " E=" + next +
-            " l=" +
-            (token.equals(UNKNOWN_MODEL) ? UNKNOWN_MODEL_BIAS : "-1.00") +
-            "\n");
-          nodesBuf.append("I=" +
-            next +
-            " W=" +
-            token +
-            "\n");
-
-          start = next;
-        }
-        linksBuf.append("J=" + (linkCount++) + " S=" + start + " E=1" + " l=-1.00\n");
-      }
-      writer.write("N=" + newNodes + " L=" + linkCount + "\n");
-
-      writer.write(nodesBuf.toString());
-      writer.write(linksBuf.toString());
-
-      writer.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    //logger.debug("wrote " + slfFile + " exists " + new File(slfFile).exists());
-    return slfFile;
-  }
-
-
 
   /**
    * Get the vocabulary to use when generating a language model. <br></br>
