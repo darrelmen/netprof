1ae452f1296616f58a900db01238035eb4d91d84
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-21 16:43:12 -0500

    combined add and edit into same tab
    Former-commit-id: fe4b27559f749c8b28bd5265bdb9f3989b409ebe [formerly d98a3e3e53988a5661f409d79d9a3ee7904ca734] [formerly fe4b27559f749c8b28bd5265bdb9f3989b409ebe [formerly d98a3e3e53988a5661f409d79d9a3ee7904ca734] [formerly 3a42fdb6701b62d0e317ad87fbef524309792ae4]]
    Former-commit-id: 90b31fd4fd0f0f7909f8f5fe4a1bbfb1cf8130fc [formerly d6144cf72ad3bd1c2b2f97e39754a37c61bc0864] [formerly df3f05c9fc7c9ac0b30f77bbba454b27b4439d3c] [formerly 48ea8c46b8ba2c36bffcd93afa41d01219fe3ca9 [formerly 3cfd6e04fad651a75600a9a7fb6cb9d78d903505] [formerly 44ebf724005222872e78d4c9f398ee99ed0e1fb7]]
    Former-commit-id: 48ea8c46b8ba2c36bffcd93afa41d01219fe3ca9 [formerly 3cfd6e04fad651a75600a9a7fb6cb9d78d903505]
    Former-commit-id: d0a954e8f011f741d75c0a7c98e5d21af636de4a
    Former-commit-id: 9882f376e3e0e9389541c06d661d39085f1f050a

--- src/mitll/langtest/client/AudioTag.java
+++ src/mitll/langtest/client/AudioTag.java
@@ -15,6 +15,7 @@ public class AudioTag {
   private static final boolean INCLUDE_ALTERNATE_COMPRESSED = LangTestDatabase.WRITE_ALTERNATE_COMPRESSED_AUDIO;
   private static final String ALTERNATE_TYPE = "webm";
   private static final boolean INCLUDE_ALTERNATE_AUDIO = true;
+  private static final String PRELOAD_HINT = "auto";
 
   /**
    * @see mitll.langtest.client.result.ResultManager#getTable
@@ -23,22 +24,20 @@ public class AudioTag {
    */
   public SafeHtml getAudioTag(String result) {
     result = ensureForwardSlashes(result);
-    SafeHtmlBuilder sb = new SafeHtmlBuilder();
     String firstSource = INCLUDE_ALTERNATE_AUDIO ?
         "<source type=\"audio/" + (INCLUDE_ALTERNATE_COMPRESSED ? ALTERNATE_TYPE : "wav") + "\" " +
            "src=\"" + (INCLUDE_ALTERNATE_COMPRESSED ? result.replace(".wav", "." +ALTERNATE_TYPE) : result) + "\">" +
         "</source>\n" : "";
     String secondSource = "<source type=\"audio/mp3\" src=\"" + result.replace(".wav", ".mp3") + "\"></source>\n";
-    sb.appendHtmlConstant("<audio preload=\"none\" controls=\"controls\" tabindex=\"0\">\n" +
+
+    SafeHtmlBuilder sb = new SafeHtmlBuilder();
+    sb.appendHtmlConstant("<audio preload=\"" + PRELOAD_HINT + "\" controls=\"controls\" tabindex=\"0\">\n" +
         firstSource +
         secondSource +
-        // "<source type=\"audio/ogg\" src=\"media/ac-LC1-009/ac-LC1-009-C.ogg\"></source>\n" +
         "Your browser does not support the audio tag.\n" +
         "</audio>");
     return sb.toSafeHtml();
   }
 
-  private String ensureForwardSlashes(String wavPath) {
-    return wavPath.replaceAll("\\\\", "/");
-  }
+  private String ensureForwardSlashes(String wavPath) {  return wavPath.replaceAll("\\\\", "/"); }
 }
