da9ae0677a4498d834636e3bd03b15dba4ac8d4e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-24 11:54:28 -0500

    got web audio recording working
    Former-commit-id: ef204557225a09e9adcb8b308ba733cd49860bcb [formerly 35572c55b908105b2f58c47cf959135a485c9a66] [formerly ef204557225a09e9adcb8b308ba733cd49860bcb [formerly 35572c55b908105b2f58c47cf959135a485c9a66] [formerly 10eda2ef00784d0a0e5fff1809cd9ff08e548a44 [formerly 52ff5a4e5cc2af8479e07b23618c7108a7b62eaa]]]
    Former-commit-id: ecf595ccf70b2a9f9e9352c5eea4d9eb572679bd [formerly d022145c2083a45ef9d5357409363e9b7f8834bd] [formerly 40486e6b5a7d827494329691f4bbdabbc6ac6f8f [formerly 0f856908f187a40c0da4d8784d1f154dfdec362d]] [formerly 81ac01b3e5037e38eac1a0d62d26556ceda891e6 [formerly 15ccc603949200add345beac6fb051e61ba2a87d] [formerly 5bcbdc2bf7ebb6b6638a61f519779e3160930af9 [formerly b40e76068b2b892f86a4b4d126ab029c8494e23a]]]
    Former-commit-id: 81ac01b3e5037e38eac1a0d62d26556ceda891e6 [formerly 15ccc603949200add345beac6fb051e61ba2a87d]
    Former-commit-id: a1b5d38558aba115f56ed33b84ccb90bf03e9f80 [formerly cc7c253ac718bd270211d12b0e16b94d5fd109e3]
    Former-commit-id: 0af0d7093082d10eaf30ef5cde75c23b9b95f3cf

--- src/mitll/langtest/public/js/webaudiorecorder.js
+++ src/mitll/langtest/public/js/webaudiorecorder.js
@@ -1,6 +1,6 @@
 function __log(e, data) {
     //log.innerHTML += "\n" + e + " " + (data || '');
-     $('#status').append("<p>"+e + "  at " + new Date().getTime());
+    $('#status').append("<p>"+e + "  at " + new Date().getTime());
 }
 
 var audio_context;
@@ -19,23 +19,130 @@ function startUserMedia(stream) {
     webAudioMicAvailable();
 }
 
-function startRecording(button) {
+function startRecording() {
     recorder && recorder.record();
-    button.disabled = true;
-    button.nextElementSibling.disabled = false;
     __log('Recording...');
 }
 
-function stopRecording(button) {
+function stopRecording() {
     recorder && recorder.stop();
-    button.disabled = true;
-    button.previousElementSibling.disabled = false;
     __log('Stopped recording.');
 
     // create WAV download link using audio data blob
-    createDownloadLink();
+    var grabWav2 = grabWav();
 
     recorder.clear();
+
+    return grabWav2;
+}
+
+function uint6ToB64 (nUint6) {
+
+    return nUint6 < 26 ?
+        nUint6 + 65
+        : nUint6 < 52 ?
+        nUint6 + 71
+        : nUint6 < 62 ?
+        nUint6 - 4
+        : nUint6 === 62 ?
+        43
+        : nUint6 === 63 ?
+        47
+        :
+        65;
+
+};
+
+function bytesToBase64(aBytes) {
+
+    var sB64Enc = "";
+
+    for (var nMod3, nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
+        nMod3 = nIdx % 3;
+        if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+        nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
+        if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+            sB64Enc += String.fromCharCode(uint6ToB64(nUint24 >>> 18 & 63), uint6ToB64(nUint24 >>> 12 & 63), uint6ToB64(nUint24 >>> 6 & 63), uint6ToB64(nUint24 & 63));
+            nUint24 = 0;
+        }
+    }
+
+    return sB64Enc.replace(/A(?=A$|$)/g, "=");
+
+};
+
+function grabWav() {
+    recorder && recorder.exportWAV(function (blob) {
+        __log('Got ... wav data!');
+        try {
+            __log('2 Got ... wav data!' + blob);
+            __log('2 Got ... wav data!' + blob.byteLength);
+            __log('3 Got ... wav data!' + blob.buffer);
+            //__log('3 Got ... wav data!' + dataview.buffer.byteLength);
+
+            var reader = new FileReader();
+
+            var arrayBuffer;
+            reader.onloadend = function () {
+                arrayBuffer = reader.result;
+
+                var myArray = new Uint8Array(arrayBuffer);
+
+                __log('5 Got ... array ' + myArray);
+                __log('6 Got ... array len!' + myArray.length);
+
+                var bytes = bytesToBase64(myArray);
+                //__log('7 Got ... wav data!' + bytes);
+
+                var length = bytes.length;
+
+                __log('8 Got ... wav data!' + length);
+
+                getBase64(bytes);
+            }
+
+            reader.readAsArrayBuffer(blob);
+
+            /*     var number = dataview.byteLength / 2;
+             __log('4 Got ... wav data!' + number);
+             */
+            /*      startBuffer(number);
+
+             __log('5 Got ... wav data!' + number);
+
+             for (i = 0; i < blob.byteLength/2; i++) {
+             var x = blob.getInt16(i);
+             setBuf(i,x);
+             }
+             endBuffer();
+             */
+
+            /*
+             var myArray = new Uint8Array(dataview.buffer);
+
+             __log('5 Got ... wav data!' + myArray.length);
+
+             var bytesToBase64 = bytesToBase64(myArray);
+             __log('6 Got ... wav data!' + bytesToBase64);
+
+             var length = bytesToBase64.length;
+
+             __log('7 Got ... wav data!' + length);*/
+
+            return "";
+        } catch (e) {
+            __log('Bad call to blob');
+
+            var vDebug = "";
+            for (var prop in e)
+            {
+                vDebug += "property: "+ prop+ " value: ["+ e[prop]+ "]\n";
+            }
+            vDebug += "toString(): " + " value: [" + e.toString() + "]";
+            __log(vDebug);
+            throw e;
+        }
+    });
 }
 
 function initWebAudio() {
