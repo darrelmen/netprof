9e7dcf15d3728d0f7ad2ba8f66b440cda6868488
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-01-30 16:28:02 -0500

    Added tests for user list manager
    Former-commit-id: f6108d018481606f11c6408585fc9bc9828d8290 [formerly 1997da8b3b0f05f6f782e5dc7a66019328985817] [formerly f6108d018481606f11c6408585fc9bc9828d8290 [formerly 1997da8b3b0f05f6f782e5dc7a66019328985817] [formerly f6108d018481606f11c6408585fc9bc9828d8290 [formerly 1997da8b3b0f05f6f782e5dc7a66019328985817] [formerly adafd321b95f584269501ce5e740085446f39bf1]]]
    Former-commit-id: efecb44203e772cbbadb6547b4c0a014e276a808 [formerly 7a2ac10d7313fcd72ae2626d1690cb7974d94597] [formerly efecb44203e772cbbadb6547b4c0a014e276a808 [formerly 7a2ac10d7313fcd72ae2626d1690cb7974d94597] [formerly 1d75af60e2a9a702bba344c5fab77f1d569962cb]] [formerly 84634cd564d118ccd296a34d1e3f011576a7f895 [formerly 3260001375976724859557596434864a919932a6] [formerly 84634cd564d118ccd296a34d1e3f011576a7f895 [formerly 3260001375976724859557596434864a919932a6] [formerly 8f93844df6d47d8bb012df04090ec4f6b2090f80]]]
    Former-commit-id: 84634cd564d118ccd296a34d1e3f011576a7f895 [formerly 3260001375976724859557596434864a919932a6]
    Former-commit-id: 3890ef78c57e0a720b9efeeafee3a67343d40afc [formerly 6011670656191805515e6ed430f764256a38e8d8] [formerly c274c88e35a7e7c2d3045a3516f66ea336993719]
    Former-commit-id: abc3dd7215d82045b1e81d75ccfc91ed1e4f615e
    Former-commit-id: ceb26f0bf710d38fc72ee6a5adfea8c34c60fa61

--- test/mitll/langtest/server/database/custom/UserListManagerTest.java
+++ test/mitll/langtest/server/database/custom/UserListManagerTest.java
@@ -3,6 +3,7 @@ package mitll.langtest.server.database.custom;
 import mitll.langtest.server.database.Database;
 import mitll.langtest.server.database.DatabaseImpl;
 import mitll.langtest.shared.User;
+import mitll.langtest.shared.custom.UserList;
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -10,14 +11,16 @@ import org.junit.Test;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by GO22670 on 1/30/14.
  */
-public class UserListManagerTest  {
+public class UserListManagerTest {
   private static Logger logger = Logger.getLogger(UserListManagerTest.class);
   private static DatabaseImpl database;
   private static String test;
@@ -35,14 +38,13 @@ public class UserListManagerTest  {
   public static void tearDown() {
     try {
       database.closeConnection();
-      File db = new File(getConfigDir(),test + ".h2.db");
+      File db = new File(getConfigDir(), test + ".h2.db");
       if (db.exists()) {
         if (!db.delete()) {
           logger.error("huh? couldn't delete " + db.getAbsolutePath());
 
         }
-      }
-      else {
+      } else {
         logger.error("huh? no " + db.getAbsolutePath());
       }
     } catch (SQLException e) {
@@ -57,16 +59,177 @@ public class UserListManagerTest  {
   @Test
   public void testAdd() {
     List<User> users = database.getUsers();
-    assertTrue(users.isEmpty());
-    long user = database.addUser(89, "male", 1, "", "english", "boston", "test");
-    assertTrue(database.getUsers().size() == 1);
-
+    long user;
+    if (users.isEmpty()) {
+      users = addAndGetUsers("test1");
+    }
+    user = users.iterator().next().id;
     UserListManager userListManager = database.getUserListManager();
 
-    assertTrue(userListManager.getListsForUser(user,false).isEmpty());
+    Collection<UserList> listsForUser = userListManager.getListsForUser(user, false);
+    int size = listsForUser.size();
+    assertTrue("size is " + size, size == 1);
+    assertTrue("first is favorite", listsForUser.iterator().next().isFavorite());
 
-    long listid = userListManager.addUserList(user, "test", "", "");
-    assertTrue(userListManager.getListsForUser(user,false).size()==1);
+    // add
+    long listid = addListCheck(user, userListManager, "test");
+    // what happens if add list a second time?
+    long listid2 = addListCheck(user, userListManager, "test", false);
 
+    // remove
+    removeList(user, userListManager, listid, true);
+    assertFalse(userListManager.listExists(listid));
+
+    // remove again
+    removeList(user, userListManager, listid, false);
+
+    // make sure we can remove lists that have been visited
+    // testAddVisitor(user, userListManager);
+  }
+
+  @Test
+  public void testAddVisitor() {
+    List<User> users = addAndGetUsers("test2");
+    logger.debug("1 users size " + users.size());
+
+    logger.debug("2 users size " + users.size());
+
+    User user = users.iterator().next();
+    UserListManager userListManager = database.getUserListManager();
+
+    long listid = addListCheck(user.id, userListManager, "test");
+    assertTrue(userListManager.getUserListsForText("").contains(userListManager.getUserListByID(listid)));
+
+    Iterator<UserList> iterator = userListManager.getListsForUser(user.id, false).iterator();
+    UserList favorite = iterator.next();
+    assertTrue(favorite.isFavorite());
+    UserList test = iterator.next();
+    assertFalse(test.isFavorite());
+    assertTrue(test.getName().equals("test"));
+
+    long visitor = getUser("visitor");
+    int size = database.getUsers().size();
+    assertTrue("size is " +size,size >= 2);
+
+    Collection<UserList> listsForUser = userListManager.getListsForUser(visitor, false);
+    assertTrue(!listsForUser.contains(test));  // haven't visited yet, shouldn't see it
+
+    userListManager.addVisitor(test, visitor);
+    listsForUser = userListManager.getListsForUser(visitor, false);
+    assertTrue(listsForUser.contains(test));   // now that I visited, I should see it on my list
+    Collection<UserList> listsForUser2 = userListManager.getListsForUser(visitor, true);
+    assertTrue(!listsForUser2.contains(test)); // list isn't mine, I'm just a visitor
+
+    removeList(user.id, userListManager, listid);
+
+    // after removing, I shouldn't see it
+    Collection<UserList> listsForUser3 = userListManager.getListsForUser(visitor, false);
+    assertTrue(!listsForUser3.contains(test));
+  }
+
+  @Test
+  public void testAddVisitor2() {
+    List<User> users = database.getUsers();
+    logger.debug("1 users size " + users.size());
+    if (users.isEmpty()) {
+      users = addAndGetUsers("test2");
+    }
+
+    logger.debug("2 users size " + users.size());
+
+    User owner = users.iterator().next();
+    UserListManager userListManager = database.getUserListManager();
+
+    long listid = addListCheck(owner.id, userListManager, "test");
+    UserList testList = userListManager.getUserListByID(listid);
+    assertTrue(userListManager.getUserListsForText("").contains(testList));
+
+    long visitor = getUser("visitor");
+    int size = database.getUsers().size();
+    assertTrue("size was " +size,size == 2);
+
+    userListManager.addVisitor(testList, visitor);
+
+    // what happens if the owner adds himself as a visitor
+    userListManager.addVisitor(testList, owner.id);
+
+    Collection<UserList> listsForUser = userListManager.getListsForUser(owner.id, false);
+    assertTrue(listsForUser.contains(testList));   // should be able to see it, it's mine
+
+    listsForUser = userListManager.getListsForUser(owner.id, true);
+    assertTrue(listsForUser.contains(testList));  // should be able to see it, it's mine
+
+    listsForUser = userListManager.getListsForUser(visitor, false);
+    assertTrue(listsForUser.contains(testList));   // should be able to see it, it's mine
+
+    listsForUser = userListManager.getListsForUser(visitor, true);
+    assertTrue(!listsForUser.contains(testList));   // should be able to see it, it's mine
+
+    removeList(owner.id, userListManager, listid);
+
+    // after removing, I shouldn't see it
+    listsForUser = userListManager.getListsForUser(visitor, false);
+    assertTrue(!listsForUser.contains(testList));
+
+    listsForUser = userListManager.getListsForUser(owner.id, false);
+    assertTrue(!listsForUser.contains(testList));
+  }
+
+  private List<User> addAndGetUsers(String test2) {
+    List<User> users;
+    long l = getUser(test2);
+    users = database.getUsers();
+//    assertTrue(users.size() == 1);
+    assertTrue(database.userExists(test2) == l);
+    return users;
+  }
+
+  private long getUser(String test2) {
+    long l = database.userExists(test2);
+    if (l == -1) {
+      l = database.addUser(89, "male", 1, "", "english", "boston", test2);
+    }
+    return l;
+  }
+
+  private long addListCheck(long user, UserListManager userListManager, String name) {
+    return addListCheck(user, userListManager, name, true);
+  }
+
+  private long addListCheck(long user, UserListManager userListManager, String name, boolean expectSuccess) {
+    long listid = addList(user, userListManager, name);
+    if (expectSuccess) {
+      assertTrue(listid != -1);
+      assertTrue("got list id " + listid, userListManager.listExists(listid));
+      assertTrue(userListManager.hasByName(user, name));
+      UserList userListByID = userListManager.getUserListByID(listid);
+      assertNotNull(userListByID);
+    } else {
+      assertTrue(listid == -1);
+    }
+    return listid;
+  }
+
+  private void removeList(long user, UserListManager userListManager, long listid) {
+    removeList(user, userListManager, listid, true);
+  }
+
+  private void removeList(long user, UserListManager userListManager, long listid, boolean expectSuccess) {
+    boolean b = userListManager.deleteList(listid);
+    if (expectSuccess) {
+      assertTrue(b);
+    } else {
+      assertFalse(b);
+    }
+    Collection<UserList> listsForUser2 = userListManager.getListsForUser(user, false);
+    assertTrue(listsForUser2.size() == 1);
+  }
+
+  private long addList(long user, UserListManager userListManager, String name) {
+    long listid = userListManager.addUserList(user, name, "", "");
+    logger.debug("adding list " + name + " got " + listid);
+    Collection<UserList> listsForUser1 = userListManager.getListsForUser(user, false);
+    assertTrue(" size is " + listsForUser1.size(), listsForUser1.size() == 2);
+    return listid;
   }
 }
