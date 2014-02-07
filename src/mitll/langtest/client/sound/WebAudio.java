283e27a148f78f40727f0dcaaec4a75b0f92f613
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-06 19:48:24 -0500

    partial steps towards web audio play audio
    Former-commit-id: 81adad4fdf038732f3c172bad54a6114ca8ac3c9 [formerly e1a7763c7fe99c39674d7ec77f17fd21f5e63091] [formerly 81adad4fdf038732f3c172bad54a6114ca8ac3c9 [formerly e1a7763c7fe99c39674d7ec77f17fd21f5e63091] [formerly 81adad4fdf038732f3c172bad54a6114ca8ac3c9 [formerly e1a7763c7fe99c39674d7ec77f17fd21f5e63091] [formerly 5bfb2ca1d26b8bc628d49060c8b89dde2a159ffe]]]
    Former-commit-id: de822ffdf8f4c0c080e8b8d2d9773a1391ad95bc [formerly 20637510c788bdd1970bd91719104b6e8774ed3b] [formerly de822ffdf8f4c0c080e8b8d2d9773a1391ad95bc [formerly 20637510c788bdd1970bd91719104b6e8774ed3b] [formerly 25e6e1f201a659a0916c1e6f41fa0784000dc1f0]] [formerly 9b77f3f375fbe0cb42806b621d7fd952d5f48cc8 [formerly a4cd6b48acc92085e61e32ba1618cfd12aefa48e] [formerly 9b77f3f375fbe0cb42806b621d7fd952d5f48cc8 [formerly a4cd6b48acc92085e61e32ba1618cfd12aefa48e] [formerly 5e6a489d1b944fbc406c845840dce5d6d4c46fe1]]]
    Former-commit-id: 47544d6e7217e3a3fcdc36b986b686c3c15a8b70 [formerly 9b77f3f375fbe0cb42806b621d7fd952d5f48cc8 [formerly a4cd6b48acc92085e61e32ba1618cfd12aefa48e]]
    Former-commit-id: d177a4677d275fd20e7ea100d38035e5c1845021 [formerly 71ce0b34adbda8831d97c23174a389fe9511a544] [formerly 041755ca2ee3e626f53518134d6f4b878a6b1738]
    Former-commit-id: f1dc27dee5d222ea3c85a29027c17ede5a854717
    Former-commit-id: 4abd68bd87ebd850ebc017b0fc99d82308a3c459

--- src/mitll/langtest/client/sound/WebAudio.java
+++ src/mitll/langtest/client/sound/WebAudio.java
@@ -6,9 +6,9 @@ import com.google.gwt.core.client.JavaScriptObject;
  * Created by go22670 on 2/6/14.
  */
 public class WebAudio {
-  public JavaScriptObject context;
+  public static JavaScriptObject context;
 
-  public native boolean checkIfWebAudioInstalled() /*-{
+  public static native boolean checkIfWebAudioInstalled() /*-{
       try {
           // webkit shim
           window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -19,51 +19,75 @@ public class WebAudio {
          // window.URL = window.URL || window.webkitURL;
 
           //audio_context = new AudioContext;
-          __log('Audio context set up.');
-          __log('navigator.getUserMedia ' + (navigator.getMedia ? 'available.' : 'not present!'));
+         console.log('Audio context set up.');
+         // console.log('navigator.getUserMedia ' + (navigator.getMedia ? 'available.' : 'not present!'));
           return true;
       } catch (e) {
-          __log('No web audio support in this browser!');
+         // __log('No web audio support in this browser!');
           console.error(e);
           //webAudioMicNotAvailable();
           return false;
       }
   }-*/;
 
-  public static native void loadSound(WebAudio webAudio, String file) /*-{
+  public static native void loadSound(String file) /*-{
       var bufferLoader;
 
-      function init() {
           // Fix up prefixing
-          window.AudioContext = window.AudioContext || window.webkitAudioContext;
-          context = new AudioContext();
-          webAudio.@mitll.langtest.client.sound.WebAudio::context = javascriptSound;
+          //window.AudioContext = window.AudioContext || window.webkitAudioContext;
+          var context = new AudioContext();
+          @mitll.langtest.client.sound.WebAudio::context = context;
 
-          bufferLoader = new BufferLoader(
-              context,
-              [
-                  '../sounds/hyper-reality/br-jam-loop.wav',
-                  '../sounds/hyper-reality/laughter.wav',
-              ],
-              finishedLoading
-          );
 
-          bufferLoader.load();
-      }
 
-      function finishedLoading(bufferList) {
-          // Create two sources and play them both together.
-          var source1 = context.createBufferSource();
-          var source2 = context.createBufferSource();
-          source1.buffer = bufferList[0];
-          source2.buffer = bufferList[1];
 
-          source1.connect(context.destination);
-          source2.connect(context.destination);
-          source1.start(0);
-          source2.start(0);
-      }
+
   }-*/;
 
+  /**
+   *   bufferLoader = new BufferLoader(
+   context,
+   [
+   file
+   ],
+   loaded
+   );
+
+   bufferLoader.load();
+   */
+
+  /**
+   *  function finishedLoading(bufferList) {
+   // Create two sources and play them both together.
+   var source1 = context.createBufferSource();
+   var source2 = context.createBufferSource();
+   source1.buffer = bufferList[0];
+   source2.buffer = bufferList[1];
+
+   source1.connect(context.destination);
+   source2.connect(context.destination);
+   source1.start(0);
+   source2.start(0);
+   }
+   */
+  public static void setLoadedCallback(Loaded callback) {
+              WebAudio.callback = callback;
+  }
+
+  static Loaded callback = null;
+  public static interface Loaded {
+    void audioLoaded();
+  }
+
+  public static void loaded() {
+    if (callback != null) callback.audioLoaded();
+  }
+
+  /**
+   * @see mitll.langtest.client.sound.SoundManagerStatic#exportStaticMethods()
+   */
+  public static native void exportStaticMethods() /*-{
+      $wnd.loaded = $entry(@mitll.langtest.client.sound.WebAudio::loaded());
+  }-*/;
 
 }
