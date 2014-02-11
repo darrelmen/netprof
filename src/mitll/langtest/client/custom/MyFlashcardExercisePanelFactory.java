ea155a745d85a4799cdf753b9a77a1aa8b8caaa8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-11 18:43:31 -0500

    does audio feedback and english/mandarin/both
    Former-commit-id: 412926d2fc0bf6239855370ad968fd67b21ca60e [formerly 508a9cda281a2c423aa7897bc5f84dec9d318439]
    Former-commit-id: 95c1849ff7095cfb4dba48d9c28c22b25e31cdee [formerly 698e655b9880dfaa8e09be127e880e378f7b7f4a]
    Former-commit-id: 77934e2f44189d250b5f686a7ae5891282023900

--- src/mitll/langtest/client/custom/MyFlashcardExercisePanelFactory.java
+++ src/mitll/langtest/client/custom/MyFlashcardExercisePanelFactory.java
@@ -1,6 +1,5 @@
 package mitll.langtest.client.custom;
 
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
@@ -8,84 +7,54 @@ import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.exercise.NavigationHelper;
 import mitll.langtest.client.exercise.PostAnswerProvider;
 import mitll.langtest.client.flashcard.BootstrapExercisePanel;
+import mitll.langtest.client.flashcard.ControlState;
 import mitll.langtest.client.flashcard.FlashcardExercisePanelFactory;
-import mitll.langtest.client.flashcard.FlashcardRecordButtonPanel;
 import mitll.langtest.client.list.ListInterface;
-import mitll.langtest.client.list.PagingExerciseList;
-import mitll.langtest.client.recorder.FlashcardRecordButton;
-import mitll.langtest.client.recorder.RecordButton;
-import mitll.langtest.client.recorder.RecordButtonPanel;
 import mitll.langtest.client.user.UserFeedback;
-import mitll.langtest.client.user.UserManager;
 import mitll.langtest.shared.Exercise;
 import mitll.langtest.shared.ExerciseShell;
-import mitll.langtest.shared.custom.UserExercise;
 
 /**
 * Created by go22670 on 2/10/14.
 */
 class MyFlashcardExercisePanelFactory extends FlashcardExercisePanelFactory {
-  //private AVPHelper avpHelper;
-
- // private final LangTestDatabaseAsync service;
- // private final UserManager userManager;
- // private final ExerciseController controller;
- // private ListInterface<UserExercise> outerExerciseList;
   private Exercise currentExercise;
- // private UserFeedback feedback;
- // private BootstrapExercisePanel bootstrapPanel;
+  ControlState controlState;
 
   public MyFlashcardExercisePanelFactory(LangTestDatabaseAsync service, UserFeedback feedback, ExerciseController controller,
                                          ListInterface<? extends ExerciseShell> exerciseList) {
     super(service, feedback, controller, exerciseList);
-
+    controlState = new ControlState();
+     System.out.println("made " + controlState);
   }
 
   @Override
   public Panel getExercisePanel(Exercise e) {
     currentExercise = e;
 
-    BootstrapExercisePanel bootstrapPanel = new BootstrapExercisePanel(e, service, controller, 40, false) {
-      NavigationHelper<UserExercise> navigationHelper;
-
-      protected RecordButtonPanel getAnswerWidget(final Exercise exercise, LangTestDatabaseAsync service,
-                                                  ExerciseController controller, final int index, boolean addKeyBinding) {
-
-          return new FlashcardRecordButtonPanel(this, service, controller, exercise, index) {
-
-            @Override
-            protected RecordButton makeRecordButton(ExerciseController controller) {
-              return new FlashcardRecordButton(controller.getRecordTimeout(), this, true, false);
-            }
-
-            @Override
-            protected void loadNext() {
-              exerciseList.loadNext();
-            }
-          };
-      }
-
+    return new BootstrapExercisePanel(e, service, controller, 40, false, controlState) {
       @Override
-      protected FlowPanel getCardPrompt(Exercise e, ExerciseController controller) {
-        FlowPanel cardPrompt = super.getCardPrompt(e, controller);
-        UserExercise currentExercise1 = new UserExercise(currentExercise);
-        navigationHelper = new NavigationHelper<UserExercise>(currentExercise1, controller, new PostAnswerProvider() {
+      protected void addRecordingAndFeedbackWidgets(Exercise e, LangTestDatabaseAsync service,
+                                                    ExerciseController controller, int feedbackHeight, Panel toAddTo) {
+        super.addRecordingAndFeedbackWidgets(e, service, controller, feedbackHeight, toAddTo);
+        NavigationHelper<Exercise> child = new NavigationHelper<Exercise>(currentExercise, controller, new PostAnswerProvider() {
           @Override
           public void postAnswers(ExerciseController controller, ExerciseShell completedExercise) {
             exerciseList.loadNextExercise(completedExercise.getID());
           }
-        }, exerciseList, false, false);
+        }, exerciseList, true, false);
+        child.setWidth("100%");
+        toAddTo.add(child);
+      }
 
-        cardPrompt.insert(navigationHelper.getPrev(), 0);
-        cardPrompt.add(navigationHelper.getNext());
-        return cardPrompt;
+      @Override
+      protected void loadNext() {
+        exerciseList.loadNextExercise(currentExercise.getID());
       }
 
       @Override
       protected Widget getHelpRow(ExerciseController controller) { return null; }
     };
-
-    return bootstrapPanel;
   }
 
   // TODO : pass these in
@@ -94,11 +63,4 @@ class MyFlashcardExercisePanelFactory extends FlashcardExercisePanelFactory {
   // TODO : display score when complete
   // TODO : when set done, do we do it again or go on to next set?
 
-  public static class ControlState {
-    boolean audioOn;
-    boolean audioFeedbackOn;
-    boolean visualFeedbackOn;
-    String showState; // english/foreign/both
-    boolean playStateOn;
-  }
 }
