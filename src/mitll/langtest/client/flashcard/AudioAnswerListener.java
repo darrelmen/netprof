9d87d2c0a072f2109d5132efdacead5bf235bffb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-11 18:43:31 -0500

    does audio feedback and english/mandarin/both
    Former-commit-id: 742446163aae315c5df1395ce1ab11b772065882 [formerly 3ce8904235172bf5cd0f8f20348338f2c3554d05] [formerly c9e3bae682d5d13705e3583553dcb6f2f7f6d689]
    Former-commit-id: 6538440c5316ebc430a7f2e209c34a1447d5713b [formerly 44c38702aa7b206604594d3264f06bc1997bbbd2] [formerly c3d0b8715593d020c7a93dba5ba3561c7451be97]
    Former-commit-id: 7537514c5ba20b17ae04b4b72543f741d9ce6d4a
    Former-commit-id: 706b8aabb763d3413c7f6522b79cb410e4f40b00

---
+++ src/mitll/langtest/client/flashcard/AudioAnswerListener.java
@@ -0,0 +1,10 @@
+package mitll.langtest.client.flashcard;
+
+import mitll.langtest.shared.AudioAnswer;
+
+/**
+ * Created by go22670 on 2/11/14.
+ */
+public interface AudioAnswerListener {
+  void receivedAudioAnswer(AudioAnswer result);
+}
