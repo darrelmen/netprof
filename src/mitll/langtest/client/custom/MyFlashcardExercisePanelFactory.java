902ebc53aa39def0271b170d22232f98b2bb7088
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-11 21:52:19 -0500

    does one set scoring
    Former-commit-id: c774a9c6d5903a7d0ea7d030940e60339ca449d2 [formerly 9f18a97d18bf42b22aed48edf16f8f4417057f00 [formerly 8a3f742bcb6d13fcbbbbbd3643d38a7a960fc58f] [formerly 0df96a497e90fe9ccb3f46169f797be568bd2e0d [formerly 1dec94afc8fbcfa3fb474b926d330719d91f6c72]]] [formerly 9f18a97d18bf42b22aed48edf16f8f4417057f00 [formerly 8a3f742bcb6d13fcbbbbbd3643d38a7a960fc58f] [formerly 0df96a497e90fe9ccb3f46169f797be568bd2e0d [formerly 1dec94afc8fbcfa3fb474b926d330719d91f6c72]] [formerly 9f18a97d18bf42b22aed48edf16f8f4417057f00 [formerly 8a3f742bcb6d13fcbbbbbd3643d38a7a960fc58f] [formerly 0df96a497e90fe9ccb3f46169f797be568bd2e0d [formerly 1dec94afc8fbcfa3fb474b926d330719d91f6c72]] [formerly 75f661d88af932ade78dac352a374719f3e63bac]]]
    Former-commit-id: b4693c424eb113264400b9ae3cf831d111b37194 [formerly c9267ea4d19cdefd253eedc53544bbe89d2b9b2a] [formerly 3f93004b890be15a1bb8859cebee8c993abc2d79 [formerly 17825c3d8b28f60832359ec94ceabf3411db0eff]] [formerly b4693c424eb113264400b9ae3cf831d111b37194 [formerly c9267ea4d19cdefd253eedc53544bbe89d2b9b2a] [formerly 3f93004b890be15a1bb8859cebee8c993abc2d79 [formerly 17825c3d8b28f60832359ec94ceabf3411db0eff]] [formerly 8968d94983a00fb25987f1062bfcf4dd9f9c792e]] [formerly be9473e6895a38c1f611b6b101c34a6a080948f9 [formerly 96a878638eb71bb0d71228b5c45bb163267c53f6] [formerly 52a8942585d0f169238b1c954426f1eef2a00190 [formerly de0b578841f6ff9dd5ceec75fe8ec2d259a96d0b]] [formerly be9473e6895a38c1f611b6b101c34a6a080948f9 [formerly 96a878638eb71bb0d71228b5c45bb163267c53f6] [formerly 52a8942585d0f169238b1c954426f1eef2a00190 [formerly de0b578841f6ff9dd5ceec75fe8ec2d259a96d0b]] [formerly 4f3156f9010a9569555aa7690241d98f5e589113]]]
    Former-commit-id: be9473e6895a38c1f611b6b101c34a6a080948f9 [formerly 96a878638eb71bb0d71228b5c45bb163267c53f6] [formerly 52a8942585d0f169238b1c954426f1eef2a00190 [formerly de0b578841f6ff9dd5ceec75fe8ec2d259a96d0b]]
    Former-commit-id: 579a057768bf9107ab51cdc87c6356615e381811 [formerly aae673eed9b3cdcc457f6b44295d81011aaa9473] [formerly 39d3509157daf823ada6ce544eeed69dedd8e35e [formerly cc753a1f4befd4636928eb5ed91d63a61154e3ce]] [formerly 8df1f6ce2868799dc4080dae6467b7d4ed79abf1]
    Former-commit-id: a4f9c48534d25d1d16639f5b00b78927eafbec54
    Former-commit-id: ea08398a84327243e968357595bb75bdf9ad45f1 [formerly 97f5d89fd666f433f7c1359faf08373bcd301147]
    Former-commit-id: ae4de52f568eb2f1b47f3bbffa058e8775892780

--- src/mitll/langtest/client/custom/MyFlashcardExercisePanelFactory.java
+++ src/mitll/langtest/client/custom/MyFlashcardExercisePanelFactory.java
@@ -1,7 +1,24 @@
 package mitll.langtest.client.custom;
 
+import com.github.gwtbootstrap.client.ui.Heading;
+import com.github.gwtbootstrap.client.ui.NumberedPager;
+import com.github.gwtbootstrap.client.ui.Pager;
+import com.github.gwtbootstrap.client.ui.SimplePager;
+import com.google.common.collect.Lists;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.cellview.client.AbstractPager;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.HasRows;
+import com.google.gwt.view.client.Range;
+import com.google.gwt.view.client.RangeChangeEvent;
+import com.google.gwt.view.client.RowCountChangeEvent;
+import java_cup.sym;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.exercise.NavigationHelper;
@@ -9,53 +26,155 @@ import mitll.langtest.client.exercise.PostAnswerProvider;
 import mitll.langtest.client.flashcard.BootstrapExercisePanel;
 import mitll.langtest.client.flashcard.ControlState;
 import mitll.langtest.client.flashcard.FlashcardExercisePanelFactory;
+import mitll.langtest.client.list.ListChangeListener;
 import mitll.langtest.client.list.ListInterface;
 import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.shared.AudioAnswer;
 import mitll.langtest.shared.Exercise;
 import mitll.langtest.shared.ExerciseShell;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
 * Created by go22670 on 2/10/14.
 */
-class MyFlashcardExercisePanelFactory extends FlashcardExercisePanelFactory {
+class MyFlashcardExercisePanelFactory<T extends ExerciseShell> extends FlashcardExercisePanelFactory {
   private Exercise currentExercise;
   ControlState controlState;
+  List<List<T>> sets = new ArrayList<List<T>>();
+  int setIndex = 0;
+  Pager pager = new Pager("<",">");
+  float totalScore;
+  int totalCorrect;
+  float totalDone;
+  Set<T> currentSet = new HashSet<T>();
+  int size = 10;
 
   public MyFlashcardExercisePanelFactory(LangTestDatabaseAsync service, UserFeedback feedback, ExerciseController controller,
-                                         ListInterface<? extends ExerciseShell> exerciseList) {
+                                         ListInterface<T> exerciseList) {
     super(service, feedback, controller, exerciseList);
     controlState = new ControlState();
-     System.out.println("made " + controlState);
+
+    exerciseList.addListChangedListener(new ListChangeListener<T>() {
+      @Override
+      public void listChanged(List<T> items) {
+        sets = Lists.partition(items, size);
+        currentSet = new HashSet<T>(sets.get(0));
+        System.out.println("====> new set : currentSet " + currentSet);
+      }
+    });
   }
 
   @Override
   public Panel getExercisePanel(Exercise e) {
     currentExercise = e;
+    T current = null;
+    for (T t : currentSet) {
+      if (t.getID().equals(e.getID())) {
+        current = t; break;
+      }
+    }
+
+    if (current != null) {
+      currentSet.remove(current);
+      System.out.println("========> after removing " + current.getID()+" currentSet " + currentSet.size()+ " : " + currentSet);
+
+    }
+    else {
+      System.err.println("========> couldn't find " + e.getID()+" in currentSet " + currentSet.size()+ " : " + currentSet);
+
+    }
 
     return new BootstrapExercisePanel(e, service, controller, 40, false, controlState) {
-      @Override
-      protected void addRecordingAndFeedbackWidgets(Exercise e, LangTestDatabaseAsync service,
-                                                    ExerciseController controller, int feedbackHeight, Panel toAddTo) {
-        super.addRecordingAndFeedbackWidgets(e, service, controller, feedbackHeight, toAddTo);
-        NavigationHelper<Exercise> child = new NavigationHelper<Exercise>(currentExercise, controller, new PostAnswerProvider() {
-          @Override
-          public void postAnswers(ExerciseController controller, ExerciseShell completedExercise) {
-            exerciseList.loadNextExercise(completedExercise.getID());
-          }
-        }, exerciseList, true, false);
-        child.setWidth("100%");
-        toAddTo.add(child);
-      }
-
       @Override
       protected void loadNext() {
         exerciseList.loadNextExercise(currentExercise.getID());
       }
 
+      public void receivedAudioAnswer(final AudioAnswer result) {
+        if (result.validity == AudioAnswer.Validity.OK) {
+          totalDone++;
+          totalScore += result.getScore();
+        }
+        if (result.isCorrect()) {
+          totalCorrect++;
+        }
+
+        System.out.println("receivedAudioAnswer : currentSet " + currentSet.size() + " : " + currentSet);
+
+        if (currentSet.isEmpty()) {
+         // String suffix = totalDone != 10 ? " over " + totalDone + " answered." : "";
+          add(new Heading(2, totalCorrect + " Correct - Average Score " + ((int) ((totalScore*100f) / totalDone)) + "%"));
+          // TODO : add do this again or do next set?
+          // TODO : maybe add table showing results per word
+          // TODO : do we do aggregate scores
+
+        } else {
+          super.receivedAudioAnswer(result);
+        }
+      }
+
+      @Override
+      protected void addWidgetsBelow() {
+        pager = new Pager("<", ">");
+
+        Scheduler.get().scheduleDeferred(new Command() {
+          public void execute() {
+
+//            if (exerciseList.onFirst()) {
+              pager.getLeft().setDisabled(exerciseList.onFirst());
+  //          }
+
+            if (exerciseList.onLast()) {
+              // TODO : show score for ones we've done!       ?
+            }
+            pager.getRight().setDisabled(exerciseList.onLast());
+          }
+        });
+
+     /*   if (exerciseList.onFirst()) {
+          pager.getLeft().setActive(false);
+        }
+
+        if (exerciseList.onLast()) {
+          pager.getRight().setActive(false);
+        }
+*/
+        pager.getLeft().addClickHandler(new ClickHandler() {
+          @Override
+          public void onClick(ClickEvent event) {
+            boolean b = exerciseList.loadPrev();
+            /*if (exerciseList.onFirst()) {
+              System.out.println("onClick onfirst!");
+
+              pager.getLeft().setActive(false);
+            }*/
+          }
+        });
+        pager.getRight().addClickHandler(new ClickHandler() {
+          @Override
+          public void onClick(ClickEvent event) {
+            boolean b = exerciseList.loadNext();
+           /* if (exerciseList.onLast()) {
+              System.out.println("===> onClick on last!");
+
+              pager.getRight().setActive(false);
+            }*/
+          }
+        });
+
+        add(pager);
+      }
+
+
       @Override
       protected Widget getHelpRow(ExerciseController controller) { return null; }
     };
   }
+
 
   // TODO : pass these in
   // TODO : how do we do a sublist of the larger list
