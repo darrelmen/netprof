9e2705726dac01a48bcad36575876e7537ebe78a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-12 19:24:45 -0500

    added ability to choose set size
    Former-commit-id: 7f17d332268f1149537c4ed8a375df7becf35779 [formerly 2d14412857fffa9dfc7dcfffa35fab6975f4589f] [formerly 57532cf2fe1f923cdd02b2a0128f3c2d81ec8656 [formerly 598c96b58463efc75bf6254b5d5bc5858dd301ff]] [formerly 0b865482eeb2501761d53cdabf52cb90619132c3]
    Former-commit-id: 31e862f1a20613df8dd53bc55a4f2c407be11648
    Former-commit-id: b5317ef43bd4e9f11a36262371b738b4975f05f3 [formerly 3d257ca29dd1bdaad16f78f61bbad9ae461d1245]
    Former-commit-id: 4228865664543d1b8402a105c240e1b646f575f5

--- src/mitll/langtest/client/flashcard/ControlState.java
+++ src/mitll/langtest/client/flashcard/ControlState.java
@@ -12,12 +12,21 @@ public class ControlState {
   public static final String FOREIGN = "foreign";
   public static final String BOTH = "both";
   String showState = ENGLISH; // english/foreign/both
+  private int setSize = 10;
   boolean playStateOn = false;
                       int id;
   public ControlState() { id = count++;}
   public boolean showEnglish() { return showState.equals(ENGLISH) || showState.equals(BOTH);}
   public boolean showForeign() { return showState.equals(FOREIGN) || showState.equals(BOTH);}
   public boolean showBoth() { return  showState.equals(BOTH);}
+
+  public int getSetSize() {
+    return setSize;
+  }
+
+  public void setSetSize(int setSize) {
+    this.setSize = setSize;
+  }
 
   public String toString() {
     return "ControlState : id " + id + " audio " + audioOn + " show " + showState;
