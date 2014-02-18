8620955cebfd8886b5d4c5ef2e9a2a44d6f6eae2
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-18 17:21:54 -0500

    steps toward support of items that have missing audio
    Former-commit-id: eb93ddb328b5ddb2c899715c607eae848f03ba71 [formerly cc1d8c03b36d34e5d4c0776651e3ed7b80c7124a] [formerly f1c7fac08a845c98559199eee5abe537d483cbd7 [formerly dca6286e399e9a0076020cdd8e6b51989b3942ad]] [formerly eb93ddb328b5ddb2c899715c607eae848f03ba71 [formerly cc1d8c03b36d34e5d4c0776651e3ed7b80c7124a] [formerly f1c7fac08a845c98559199eee5abe537d483cbd7 [formerly dca6286e399e9a0076020cdd8e6b51989b3942ad]] [formerly eb93ddb328b5ddb2c899715c607eae848f03ba71 [formerly cc1d8c03b36d34e5d4c0776651e3ed7b80c7124a] [formerly f1c7fac08a845c98559199eee5abe537d483cbd7 [formerly dca6286e399e9a0076020cdd8e6b51989b3942ad]] [formerly 1739091b05ced29a78a7077a354576e04c6d5042 [formerly 519cec38e988fe312b00d34843511b65595a4aa2] [formerly 52f03a713ae70a8531860fe4cb86ad9f9e7aa188 [formerly c61a8735dbe7d0e134fa200aa7c51cea3a63865e]]]]]
    Former-commit-id: d80f2e96fca7f90291769100a34d51cc7c6c0105 [formerly f0e52c1c43875b7c17588c4a039711c6215f9622] [formerly c6c3d98306e5a1268b116feb685c748c6c341efa [formerly fdad64fa88a05a578de55f8a7d056936f23b542d]] [formerly d80f2e96fca7f90291769100a34d51cc7c6c0105 [formerly f0e52c1c43875b7c17588c4a039711c6215f9622] [formerly c6c3d98306e5a1268b116feb685c748c6c341efa [formerly fdad64fa88a05a578de55f8a7d056936f23b542d]] [formerly e29502ede280b369ef720710a29681af7e2d8395 [formerly 75f2a9457ce95e02c224b2149e1eb54f1f33213f] [formerly 47020b1b77a9baab2bcb4be75595902c39f405eb [formerly 7409ccc56a550fa65d5aa773b8385d12d931ba5d]]]] [formerly fc6b2b3d8977cdde61fcab849a7d2f7c12916819 [formerly 5532e5a4484f264d8eba80b2bb77039aa20a939c] [formerly fa71a839061b678b917f8698a86dfa36dc67e2f7 [formerly 3dc80d83916746f609d6730b73e445fca63e9ed9]] [formerly fc6b2b3d8977cdde61fcab849a7d2f7c12916819 [formerly 5532e5a4484f264d8eba80b2bb77039aa20a939c] [formerly fa71a839061b678b917f8698a86dfa36dc67e2f7 [formerly 3dc80d83916746f609d6730b73e445fca63e9ed9]] [formerly 728b25a44f0ddfe9b75c5c78986ede204c83777d [formerly 5d9c4999f332880280b8a1b682ae2cbf2fbcf910] [formerly a69bf3adb788c6d4a91027a0b8ecf30af50ebbf0 [formerly 03bdcab5f056feba586ec5efebd83ffddef4276e]]]]]
    Former-commit-id: fc6b2b3d8977cdde61fcab849a7d2f7c12916819 [formerly 5532e5a4484f264d8eba80b2bb77039aa20a939c] [formerly fa71a839061b678b917f8698a86dfa36dc67e2f7 [formerly 3dc80d83916746f609d6730b73e445fca63e9ed9]]
    Former-commit-id: bc4751f57710e937d4618964340711ab0469608e [formerly f021f9df70423bc7540ccfd39331a077a4748373] [formerly d8efa35f180f68ca91a8c271285427b9a9b7f52c [formerly bc6106aa5654adc65733a40c3b0282c69ac0caa5]] [formerly a25f9f572325ef9547a9f9eb53aea7ad6adb5ff2 [formerly 2583d4d9ccfa8a24dbb2039a8ce55869aee5ea88] [formerly 0def6196b2831e7a3b5edc33b834423d2cfa9a40 [formerly 8da50cfbc11ee736a2dc2fb4410578a211f49875]]]
    Former-commit-id: d90a6fdb5608de7d40324593aec3f588544d9a79 [formerly c6d7d0e0dcbd2e19189f03ec0d774ad89d826bc2] [formerly 51cd882e0c1a02c24af04dcc582a6d160277bbfe [formerly d90d8225e5161576dea811876eff47926371fe56]]
    Former-commit-id: 17f33b50e1e40bb8e8ce0a2474c2c9e71fdb5063 [formerly b22a7e6eab8a21f58c9e38f86981a56b9b3ac58e]
    Former-commit-id: 3eae05f7ab34b65a24d852982bf1ee3214afe92d

---
+++ src/mitll/langtest/server/filter/ForceNocacheFilter.java
@@ -0,0 +1,50 @@
+package mitll.langtest.server.filter;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * ForceNocacheFilter: Force the GWT nocache files to not cache.
+ *
+ * @author Raymond Budd <a href=mailto:raymond.budd@ll.mit.edu>raymond.budd@ll.mit.edu</a>
+ * @since Feb 6, 2014 8:44:28 AM
+ */
+public class ForceNocacheFilter implements Filter {
+  @Override
+  public void doFilter(final ServletRequest request,
+                       final ServletResponse response, final FilterChain chain)
+    throws IOException, ServletException {
+
+    final HttpServletRequest httpRequest = (HttpServletRequest) request;
+    final String requestUri = httpRequest.getRequestURI();
+
+    if (requestUri.contains(".nocache.")) {
+      Date now = new Date();
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.setDateHeader("Date", now.getTime());
+// one day old
+      httpResponse.setDateHeader("Expires", now.getTime() - 86400000L);
+      httpResponse.setHeader("Pragma", "no-cache");
+      httpResponse.setHeader("Cache-control", "no-cache, no-store, must-revalidate");
+    }
+
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {}
+
+  @Override
+  public void init(FilterConfig arg0) throws ServletException {}
+}
