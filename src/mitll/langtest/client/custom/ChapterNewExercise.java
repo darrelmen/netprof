ee66f936ec36841dbb633e59638866c26d7c95d9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-24 15:37:49 -0500

    steps towards adding items
    Former-commit-id: 5107bfa6485a4b00fd2f417d2bee4ee6740038be [formerly 7f479d75b4896bc142f8fb1dbd7f3ce335bcb8b7] [formerly be3399323dcd5db95bde3279c6d96c7b0811ae0d [formerly b70f7c6b999413ac1f9de1d52c9d66a253d8c7d4]] [formerly 6963ee4a0f9c7e618435d4f5abe990138665a0aa [formerly 15d1481cb683acae3a2d3158a89424c4277418e5] [formerly 191e0283d1fefebd3ee4bb7f0089c2afb5bf53f4]]
    Former-commit-id: 2e243080296d600a75fe3302709e41e0d2e0ebb4 [formerly 4a6753732c8c3489c0b00e2cc76e925c08b04887] [formerly 0f41285d626489dd6e04bca7d44f9a22b6f37451 [formerly 10ca6570dda5754aec491de1e2a25b1470778c0c]] [formerly f95616c01fa9a77fe8edcc9af34854f5f1702b8e [formerly a670b81021c9b838f7d99175aba23e75776c6bd1]]
    Former-commit-id: 5d631c601d00faaf1bfc91b76d09623180128e7f [formerly 4a45c3608f82283cc96da9d43ed4c1059e8a052c]
    Former-commit-id: c1ac380112ee0f0524b31f0afc47e0c29755df24 [formerly ebd19fd11126f7aa2e593def44ad34261aa63ca4]
    Former-commit-id: 31c4a836144bffa07bae20e7dbdb33c20a3deafa

--- src/mitll/langtest/client/custom/ChapterNewExercise.java
+++ src/mitll/langtest/client/custom/ChapterNewExercise.java
@@ -3,10 +3,19 @@ package mitll.langtest.client.custom;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Panel;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.bootstrap.ItemSorter;
 import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.user.BasicDialog;
 import mitll.langtest.shared.ExerciseShell;
+import mitll.langtest.shared.SectionNode;
 import mitll.langtest.shared.StartupInfo;
 import mitll.langtest.shared.custom.UserExercise;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by GO22670 on 2/20/14.
@@ -16,6 +25,8 @@ public class ChapterNewExercise<T extends ExerciseShell> extends NewUserExercise
                             ExerciseController controller, HasText itemMarker, EditItem editItem, UserExercise newExercise) {
     super(service, controller, itemMarker, editItem, newExercise);
   }
+
+  Map<String,ListBoxFormField> nameToWidget = new HashMap<String, ListBoxFormField>();
 
   /**
    * TODO add drop down widget to allow selection of unit/lesson
@@ -33,6 +44,57 @@ public class ChapterNewExercise<T extends ExerciseShell> extends NewUserExercise
   protected void addItemsAtTop(Panel container) {
     //super.addItemsAtTop(container);
     StartupInfo startupInfo = controller.getStartupInfo();
-    System.out.println("startup info " + startupInfo);
+    Collection<String> typeOrder = controller.getStartupInfo().getTypeOrder();
+//  System.out.println("startup info " + startupInfo);
+    if (startupInfo.getTypeOrder().isEmpty()) return;
+
+    BasicDialog basicDialog = new BasicDialog();
+
+    List<SectionNode> sectionNodes = controller.getStartupInfo().getSectionNodes();
+   // List<String> values = new ArrayList<String>();
+    Map<String,List<String>> typeToValues = new HashMap<String, List<String>>();
+    String first = typeOrder.iterator().next();
+
+    for (SectionNode sectionNode : sectionNodes) {
+      if (sectionNode.getType().equals(first)) {
+        populate(typeToValues, first, sectionNode);
+      }
+    }
+
+    ItemSorter sorter = new ItemSorter();
+  //  Collections.sort(values);
+
+    for (String type : typeOrder) {
+//        flow.getElement().setId("unitLesson");
+      //      flow.addStyleName("leftFiveMargin");
+
+      //String value = newUserExercise.getUnitToValue().get(type);
+      List<String> sortedItems = sorter.getSortedItems(typeToValues.get(type));
+      ListBoxFormField listBoxFormField = basicDialog.getListBoxFormField(container, type, sortedItems);
+      nameToWidget.put(type, listBoxFormField);
+      //Heading child = new Heading(4, type, value);
+      // child.addStyleName("rightFiveMargin");
+      // flow.add(child);
+    }
+  }
+
+  private void populate(Map<String, List<String>> typeToValues, String first, SectionNode sectionNode) {
+    List<String> valuesForType = typeToValues.get(first);
+    if (valuesForType == null) typeToValues.put(first, valuesForType = new ArrayList<String>());
+
+    valuesForType.add(sectionNode.getName());
+
+    for (SectionNode child : sectionNode.getChildren()) {
+      populate(typeToValues, child.getType(), child);
+    }
+  }
+
+  @Override
+  protected void setFields() {
+    super.setFields();
+    for (Map.Entry<String, ListBoxFormField> pair : nameToWidget.entrySet()) {
+      newUserExercise.addUnitToValue(pair.getKey(), pair.getValue().getValue());
+
+    }
   }
 }
