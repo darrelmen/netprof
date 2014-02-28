2ffb1ec3f5650071c96f40521d71b94d49d457a1
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-02-28 14:57:53 -0500

    better grading tabs -- is complete flag in users table
    Former-commit-id: 14881b83bbea5140bd5725f3ae61465e64e0e6ae
    Former-commit-id: 1038525fa8fc5b1e589bdeb325b2daf90ab9e451

--- src/mitll/langtest/server/database/ResultDAO.java
+++ src/mitll/langtest/server/database/ResultDAO.java
@@ -11,9 +11,7 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
 import java.io.OutputStream;
