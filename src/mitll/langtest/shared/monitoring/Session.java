280f197cd8a374cf28ec8efa3a6011780fb5c4ae
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-05 15:32:04 -0500

    shows pronunciation score graph too
    Former-commit-id: 7885adab896aa9079789c28be61048850b63b787 [formerly 9fedf122bea885dc6313558d6c6cbbe7bfa8b16c] [formerly 3d5e4d30ffce90bb3725558063fcab48cc402379 [formerly 8ae984fb9eca8bbf920289e8d41df31f220cc007]]
    Former-commit-id: 49f0a2ff75802790aec386c1c59131714092cbbf [formerly a466dd66010e49700e5abf2714c8195d6189c6c1]
    Former-commit-id: 2029bc47aa2fb44de023936915585e73fdd38eaa

--- src/mitll/langtest/shared/monitoring/Session.java
+++ src/mitll/langtest/shared/monitoring/Session.java
@@ -3,6 +3,7 @@ package mitll.langtest.shared.monitoring;
 import com.google.gwt.user.client.rpc.IsSerializable;
 import mitll.langtest.shared.flashcard.SetScore;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -18,16 +19,18 @@ import java.util.Set;
 public class Session implements IsSerializable, SetScore {
   private int numAnswers;
   public long duration;
-  //private int correct;
   private long userid;
-  Map<String,Boolean> exidToCorrect = new HashMap<String,Boolean>();
+  private int correct;
+  private float avgScore;
+  private Map<String,Boolean> exidToCorrect = new HashMap<String,Boolean>();
+  private Map<String,Float> exidToScore = new HashMap<String,Float>();
 
   private Set<String> exids = new HashSet<String>();
 
   public Session(){} // required
   public Session(long userid) { this.userid = userid;  }
 
-  private long getAverage() { return duration/ getNumAnswers(); }
+  public long getAverageDurMillis() { return duration/ getNumAnswers(); }
   public long getSecAverage() { return (duration/ getNumAnswers())/1000; }
 
   public void addExerciseID(String id) { exids.add(id);  }
@@ -38,11 +41,14 @@ public class Session implements IsSerializable, SetScore {
 
   public void setNumAnswers() {
     this.numAnswers = exids.size();
+    this.avgScore = calcAvgScore();
+    this.correct = calcCorrect();
     exids = null;
+    exidToCorrect = null;
+    exidToScore = null;
   }
 
-  @Override
-  public int getCorrect() {
+  private int calcCorrect() {
     int count = 0;
     for (Boolean correct : exidToCorrect.values()) {
       if (correct) count++;
@@ -50,12 +56,26 @@ public class Session implements IsSerializable, SetScore {
     return count;
   }
 
-/*  public void setCorrect(int correct) {
-    this.correct = correct;
-  }*/
+  private float calcAvgScore() {
+    float total = 0f;
+    Collection<Float> values = exidToScore.values();
+    System.out.println("scores "+ values);
+    for (Float score : values) {
+      total += Math.max(0f,score);
+    }
+    return total/(float)values.size();
+  }
+
+  @Override
+  public int getCorrect() {
+    return correct;
+  }
+
+  @Override
+  public float getAvgScore() { return avgScore; }
 
   public void incrementCorrect(String id,boolean correct) {
-    exidToCorrect.put(id,correct);
+    exidToCorrect.put(id, correct);
   }
 
   @Override
@@ -67,8 +87,13 @@ public class Session implements IsSerializable, SetScore {
     this.userid = userid;
   }
 
+  public void setScore(String id, float pronScore) {
+    exidToScore.put(id, pronScore);
+  }
+
   public String toString() {
     return "user " + userid+
-      " num " + getNumAnswers() + " dur " + duration/(60*1000) + " minutes, avg " + getAverage()/1000 +
-      " secs " + getCorrect() + " correct : " + exidToCorrect; }
+      " num " + getNumAnswers() + " dur " + duration/(60*1000) + " minutes, avg " + getAverageDurMillis()/1000 +
+      " secs " + correct + " avg score : " + avgScore;
+  }
 }
