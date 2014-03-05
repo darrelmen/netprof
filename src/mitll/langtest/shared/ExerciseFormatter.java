8216efc61720c251a2f57b93db0d54cb71bd9f60
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-05 18:50:33 -0500

    variety of changes -- cleaned up the display, record button is now press and hold everywhere
    Former-commit-id: 32557f1a8142edef3d32532eb19619576ebfb047 [formerly 69261734c0455ef3952b3aa06f56a38b8c7afdd9] [formerly 32557f1a8142edef3d32532eb19619576ebfb047 [formerly 69261734c0455ef3952b3aa06f56a38b8c7afdd9] [formerly 7d0fcdf27fbc78897ba7f2330921fdbb0640e183]]
    Former-commit-id: f4386615d84583447ba3bf04953b80b9dee7ce03 [formerly 5af590a9443afa0e021bab7a3ecb16d15b4bfa1f] [formerly c3cfb20b0427fc264b32c18fa57865d9a452e9ec] [formerly f7a4d5724d795638105fd7f2228cdd546385c089 [formerly 58a3a5d9f7e2b8046a1e59ba03ab06e084a0894e] [formerly 2cc652f82742fc5ddf0407be16cc062f20ad9e47]]
    Former-commit-id: f7a4d5724d795638105fd7f2228cdd546385c089 [formerly 58a3a5d9f7e2b8046a1e59ba03ab06e084a0894e]
    Former-commit-id: bd48d69ca03f360709e54d458a076c1a0aa5c8ce
    Former-commit-id: af32adfbb9383ded2c478188c3be00aa8b0858c4

--- src/mitll/langtest/shared/ExerciseFormatter.java
+++ src/mitll/langtest/shared/ExerciseFormatter.java
@@ -11,6 +11,8 @@ public class ExerciseFormatter {
   public static final String FOREIGN_LANGUAGE_PROMPT = "Say:";
   public static final String ENGLISH_PROMPT = "Meaning:";
   public static final String TRANSLITERATION = "Transliteration:";
+  public static final String TRANSLATION = "Translation:";
+  public static final String CONTEXT = "Context:";
 
   /**
    * @see mitll.langtest.server.database.ExcelImport#getExercise(String, String, String, String, String, String, boolean, String)
@@ -36,17 +38,17 @@ public class ExerciseFormatter {
 
   private static String getContent(String arabic, String translit, String english, String meaning, String context,
                                   boolean isEnglish, boolean isUrdu, boolean isPashto) {
-    String arabicHTML = getArabic(arabic, isUrdu, isPashto);
+    String arabicHTML = getArabic(arabic, isUrdu, isPashto, false);
     String translitHTML = translit.length() > 0 ?
-      getSpanWrapper(TRANSLITERATION, translit)
+      getSpanWrapper(TRANSLITERATION, translit, false)
       : "";
     String translationHTML = english != null && english.length() > 0 ?
-      getSpanWrapper("Translation:", english) : "";
+      getSpanWrapper(TRANSLATION, english, false) : "";
     String meaningHTML = (isEnglish && meaning.length() > 0) ?
-      getSpanWrapper(ENGLISH_PROMPT, meaning) : "";
+      getSpanWrapper(ENGLISH_PROMPT, meaning, false) : "";
 
     String contextHTML = (context.length() > 0) ?
-      getSpanWrapper("Context:", context) : "";
+      getSpanWrapper(CONTEXT, context, false) : "";
     return arabicHTML +
       translitHTML +
       translationHTML +
@@ -54,21 +56,23 @@ public class ExerciseFormatter {
       contextHTML;
   }
 
-  private static String getSpanWrapper(String rowTitle, String english) {
-    return "<div class=\"Instruction\">\n" +
-      "<span class=\"Instruction-title\">" +
+  private static String getSpanWrapper(String rowTitle, String english, boolean includePrompt) {
+    String prompt = includePrompt ? "<span class=\"Instruction-title\">" +
       rowTitle +
-      "</span>\n" +
+      "</span>\n" : "";
+    return "<div class=\"Instruction\">\n" +
+      prompt +
       "<span class=\"Instruction-data\"> " + english +
       "</span>\n" +
       "</div>";
   }
 
-  public static String getArabic(String arabic, boolean isUrdu, boolean isPashto) {
-    return "<div class=\"Instruction\">\n" +
-      "<span class=\"Instruction-title\">" +
+  public static String getArabic(String arabic, boolean isUrdu, boolean isPashto, boolean includePrompt) {
+    String prompt = includePrompt ? "<span class=\"Instruction-title\">" +
       FOREIGN_LANGUAGE_PROMPT +
-      "</span>\n" +
+      "</span>\n" : "";
+    return "<div class=\"Instruction\">\n" +
+      prompt +
       "<span class=\"" +
       (isUrdu ? "urdufont" : isPashto ? "pashtofont" : "Instruction-data") +
       "\"> " + arabic +
