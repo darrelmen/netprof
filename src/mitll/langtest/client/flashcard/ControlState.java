95907d573883730b1c220a26d82544d89c3f80a7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-05 13:41:10 -0500

    fixed bug where going back in the list would mess up the scores, remembers control state in local storage
    Former-commit-id: 69863587e2565257c2088efbab2cae4b60c95837 [formerly 217c7f5aa700129c5bd6d5b100a3194d5657db3d]
    Former-commit-id: a4e0e5ac90bf1d331a63c4bf6261bfccfabf69fe

--- src/mitll/langtest/client/flashcard/ControlState.java
+++ src/mitll/langtest/client/flashcard/ControlState.java
@@ -1,34 +1,71 @@
 package mitll.langtest.client.flashcard;
 
+import com.github.gwtbootstrap.client.Bootstrap;
+import mitll.langtest.client.custom.KeyStorage;
+import org.apache.xpath.operations.Bool;
+
 /**
 * Created by go22670 on 2/11/14.
 */
 public class ControlState {
   static int count = 0;
-  boolean audioOn = true;
-  boolean audioFeedbackOn = true;
-  boolean visualFeedbackOn;
+  private boolean audioOn = true;
+  private boolean audioFeedbackOn = true;
   public static final String ENGLISH = "english";
   public static final String FOREIGN = "foreign";
   public static final String BOTH = "both";
-  String showState = ENGLISH; // english/foreign/both
-  private int setSize = 10;
-  boolean playStateOn = false;
-                      int id;
+  private String showState = ENGLISH; // english/foreign/both
+ // boolean playStateOn = false;
+  private KeyStorage storage = null;
+  private int id;
+
   public ControlState() { id = count++;}
   public boolean showEnglish() { return showState.equals(ENGLISH) || showState.equals(BOTH);}
   public boolean showForeign() { return showState.equals(FOREIGN) || showState.equals(BOTH);}
   public boolean showBoth() { return  showState.equals(BOTH);}
 
-  public int getSetSize() {
-    return setSize;
+  public void setStorage(KeyStorage storage) {
+    this.storage = storage;
+
+    String showState1 = storage.getValue("showState");
+    if (showState1.equals(FOREIGN)) showState = FOREIGN;
+    else if (showState1.equals(BOTH)) showState = BOTH;
+
+    String audioOnKey = storage.getValue("audioOn");
+    String anObject = Boolean.TRUE.toString();
+    //System.out.println("audioOn " + audioOnKey + " vs " + anObject);
+    audioOn = audioOnKey.equalsIgnoreCase(anObject);
+
+    String audioFeedbackOnKey = storage.getValue("audioFeedbackOn");
+    audioFeedbackOn = audioFeedbackOnKey.equalsIgnoreCase(anObject);
   }
 
-  public void setSetSize(int setSize) {
-    this.setSize = setSize;
+  public void setAudioOn(boolean audioOn) {
+    this.audioOn = audioOn;
+    if (storage != null) storage.storeValue("audioOn", Boolean.toString(audioOn));
   }
 
-  public String toString() {
-    return "ControlState : id " + id + " audio " + audioOn + " show " + showState;
+  public void setAudioFeedbackOn(boolean audioFeedbackOn) {
+    this.audioFeedbackOn = audioFeedbackOn;
+    if (storage != null) storage.storeValue("audioFeedbackOn", Boolean.toString(audioFeedbackOn));
   }
+
+  public void setShowState(String showState) {
+    this.showState = showState;
+    if (storage != null) storage.storeValue("showState", showState);
+  }
+
+  public String getShowState() {
+    return showState;
+  }
+
+  public boolean isAudioOn() {
+    return audioOn;
+  }
+
+  public boolean isAudioFeedbackOn() {
+    return audioFeedbackOn;
+  }
+
+  public String toString() {  return "ControlState : id " + id + " audio " + audioOn + " show " + showState;  }
 }
