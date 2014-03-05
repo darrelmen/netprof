8cc0ea68e10fa4a64c1d1dcf1817f05226fc81f4
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-04 19:38:10 -0500

    now shows leaderboard after doing a list
    Former-commit-id: ed532eb0b27d6147a4cf017d1dfbecfae5e01934 [formerly 95dbafe8848c08700d94f4308f7967c8cc9e27a9] [formerly ed532eb0b27d6147a4cf017d1dfbecfae5e01934 [formerly 95dbafe8848c08700d94f4308f7967c8cc9e27a9] [formerly dc3d75db2d463a6372f070f0a0a2d2f30a42a83a]]
    Former-commit-id: 5e4ab3f5916238696d155a49cd5b235ce5425884 [formerly bcc50c17d692d5434b682deb88f3a0414b69edae] [formerly 17b8d83e345344fdb43848c6448afbf4bb434dcb] [formerly 55158ac2c0d17723ce195270d67488307588512e [formerly 96db24de0c2e9b8237c0be577868038ed729da7e] [formerly 9200c082542b746e8352dcf1aebb124076cd55c7]]
    Former-commit-id: 55158ac2c0d17723ce195270d67488307588512e [formerly 96db24de0c2e9b8237c0be577868038ed729da7e]
    Former-commit-id: 79c76ecb4b12cc19767a72a312003edce31ba702
    Former-commit-id: a0c843142b5ab97b5cbcad9c1fcb79c79afae6a2

--- src/mitll/langtest/shared/monitoring/Session.java
+++ src/mitll/langtest/shared/monitoring/Session.java
@@ -1,8 +1,11 @@
 package mitll.langtest.shared.monitoring;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
+import mitll.langtest.shared.flashcard.SetScore;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -12,17 +15,22 @@ import java.util.Set;
 * Time: 7:07 PM
 * To change this template use File | Settings | File Templates.
 */
-public class Session implements IsSerializable {
+public class Session implements IsSerializable, SetScore {
   private int numAnswers;
   public long duration;
+  private int correct;
+  private long userid;
+  Map<String,Boolean> exidToCorrect = new HashMap<String,Boolean>();
+
   private Set<String> exids = new HashSet<String>();
+
+  public Session(){} // required
+  public Session(long userid) { this.userid = userid;  }
 
   private long getAverage() { return duration/ getNumAnswers(); }
   public long getSecAverage() { return (duration/ getNumAnswers())/1000; }
 
-  public void addExerciseID(String id) {
-    exids.add(id);
-  }
+  public void addExerciseID(String id) { exids.add(id);  }
 
   public int getNumAnswers() {
     return numAnswers;
@@ -31,6 +39,32 @@ public class Session implements IsSerializable {
   public void setNumAnswers() {
     this.numAnswers = exids.size();
     exids = null;
+  }
+
+  @Override
+  public int getCorrect() {
+    int count = 0;
+    for (Boolean correct : exidToCorrect.values()) {
+      if (correct) count++;
+    }
+    return count;
+  }
+
+  public void setCorrect(int correct) {
+    this.correct = correct;
+  }
+
+  public void incrementCorrect(String id,boolean correct) {
+    exidToCorrect.put(id,correct);
+  }
+
+  @Override
+  public long getUserid() {
+    return userid;
+  }
+
+  public void setUserid(long userid) {
+    this.userid = userid;
   }
 
   public String toString() { return "num " + getNumAnswers() + " dur " + duration/(60*1000) + " minutes, avg " + getAverage()/1000 + " secs"; }
