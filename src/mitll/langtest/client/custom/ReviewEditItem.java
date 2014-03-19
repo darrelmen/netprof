78ac718b0d4e338e94eb9d1d300163e5cec27e18
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-19 18:28:37 -0400

    moved add list button next to prev/next, made sure user chooses a gender if a reviewer, limited user id length
    Former-commit-id: b9b5e1914e4c1d961552754ce119f297d88561a8 [formerly bf95c0bdfab70b86627dbde6c4b5e68a243c552b]
    Former-commit-id: b8c55b5666efbff151104f0a5e4c4047ab7de275

--- src/mitll/langtest/client/custom/ReviewEditItem.java
+++ src/mitll/langtest/client/custom/ReviewEditItem.java
@@ -126,35 +126,6 @@ public class ReviewEditItem<T extends ExerciseShell> extends EditItem<T> {
       return row;
     }
 
-    Tooltip addTooltip(Widget w, String tip) {
-      return createAddTooltip(w, tip, Placement.RIGHT);
-    }
-
-    /**
-     * @see mitll.langtest.client.custom.NPFExercise#makeAddToList(mitll.langtest.shared.Exercise, mitll.langtest.client.exercise.ExerciseController)
-     * @param widget
-     * @param tip
-     * @param placement
-     * @return
-     */
-    private Tooltip createAddTooltip(Widget widget, String tip, Placement placement) {
-      Tooltip tooltip = new Tooltip();
-      tooltip.setWidget(widget);
-      tooltip.setText(tip);
-      tooltip.setAnimation(true);
-// As of 4/22 - bootstrap 2.2.1.0 -
-// Tooltips have an bug which causes the cursor to
-// toggle between finger and normal when show delay
-// is configured.
-
-      tooltip.setShowDelay(500);
-      tooltip.setHideDelay(500);
-
-      tooltip.setPlacement(placement);
-      tooltip.reconfigure();
-      return tooltip;
-    }
-
     private Button getRemove() {
       Button remove = new Button(DELETE);
       remove.setType(ButtonType.WARNING);
