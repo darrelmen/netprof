7c830bf101308626618b71460c64b49a0c1127ea
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-21 19:18:52 -0400

    lots of changes - added interface classes for exercises -- CommonExercise, CommonUserExercise, etc
    Former-commit-id: 011d76aad702984bdefadbd602d77fd473d970d8 [formerly ca63caf9f929dcc505cfc916bc55861d9f4c0d7e]
    Former-commit-id: 74c7eea206608a166c88bb74d7cfaac47a436104 [formerly 5f161317e914abc3a69f7eb9bcd9af64c7fbe917]
    Former-commit-id: c9e5d276c64f262dfeeb7fa776857cb282b59a67

---
+++ src/mitll/langtest/shared/CommonExercise.java
@@ -0,0 +1,49 @@
+package mitll.langtest.shared;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by GO22670 on 3/20/2014.
+ */
+public interface CommonExercise extends CommonShell {
+  String getPlan();
+
+  String getEnglish();
+
+  String getMeaning();
+
+  String getForeignLanguage();
+  String getRefSentence();
+
+  String getTransliteration();
+
+  String getContent();
+
+  boolean hasRefAudio();
+
+  String getRefAudio();
+
+  String getSlowAudioRef();
+
+  Collection<AudioAttribute> getAudioAttributes();
+
+  Map<String, String> getUnitToValue();
+
+
+  CommonShell getShell();
+
+  CommonShell getShellCombinedTooltip();
+
+  Map<String, ExerciseAnnotation> getFieldToAnnotation();
+
+  ExerciseAnnotation getAnnotation(String field);
+
+  Exercise toExercise();
+  CommonUserExercise toCommonUserExercise();
+
+  // super nice to remove these... and make read only
+  void addUnitToValue(String unit, String value);
+
+  void addAnnotation(String field, String status, String comment);
+}
