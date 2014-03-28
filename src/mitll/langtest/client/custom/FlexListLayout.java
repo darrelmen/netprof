77da305c2b90af6ea661c7028f9e00ebc2a817eb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-28 19:27:17 -0400

    basically got hierarchical fix defects tab working
    Former-commit-id: 4701775a5717b572806138652ce64935285079c6 [formerly e0e610b25d93f36d1bca74083215b456f1b421c0] [formerly 4701775a5717b572806138652ce64935285079c6 [formerly e0e610b25d93f36d1bca74083215b456f1b421c0] [formerly 1de4c115f33e9b577eee871e4edb3189d76ff853]]
    Former-commit-id: 4aac346a84f535145232d98c1b293992aaf257f5 [formerly a86304db3c4b4ee603f923529ec603bb9eb4fc43] [formerly d1e75da83467c026c979a3d669caf24284e73cbe] [formerly 30e094787f67a728ac5090fe1aa4fe382da9f25c [formerly e1763a085ee32f670bbaffa00f74aec1bf415863] [formerly 0ec1ca603b27a5344c38d12693bf660f352b0d36]]
    Former-commit-id: 30e094787f67a728ac5090fe1aa4fe382da9f25c [formerly e1763a085ee32f670bbaffa00f74aec1bf415863]
    Former-commit-id: 808d8ccc11c081f998d962dd0ec3657b344b14a7
    Former-commit-id: ea3ee34a640402d37fd3630660b09cddd0d1310d

---
+++ src/mitll/langtest/client/custom/FlexListLayout.java
@@ -0,0 +1,130 @@
+package mitll.langtest.client.custom;
+
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.event.HiddenEvent;
+import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.bootstrap.FlexSectionExerciseList;
+import mitll.langtest.client.dialog.ModalInfoDialog;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.client.exercise.ExercisePanelFactory;
+import mitll.langtest.client.list.PagingExerciseList;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.custom.UserList;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by GO22670 on 3/28/2014.
+ */
+public abstract class FlexListLayout {
+  protected PagingExerciseList npfExerciseList;
+  private final ExerciseController controller;
+  private final LangTestDatabaseAsync service;
+  private final UserFeedback feedback;
+  private final UserManager userManager;
+
+  public FlexListLayout(LangTestDatabaseAsync service, UserFeedback feedback,
+                        UserManager userManager, ExerciseController controller) {
+    //super(service, feedback, userManager, controller);
+    this.controller = controller;
+    this.service = service;
+    this.feedback = feedback;
+    this.userManager = userManager;
+    System.out.println(getClass() + " : ChapterNPFHelper ");
+  }
+
+  protected Panel doInternalLayout(UserList ul, String instanceName) {
+    //System.out.println(getClass() + " : doInternalLayout instanceName = " + instanceName + " for list " + ul);
+
+    Panel twoRows = new FlowPanel();
+    twoRows.getElement().setId("twoRows");
+
+    Panel exerciseListContainer = new SimplePanel();
+    exerciseListContainer.addStyleName("floatLeft");
+    exerciseListContainer.getElement().setId("exerciseListContainer");
+
+    // second row ---------------
+    FluidRow topRow = new FluidRow();
+    topRow.getElement().setId("topRow");
+
+    twoRows.add(topRow);
+
+    Panel bottomRow = new HorizontalPanel();
+    bottomRow.add(exerciseListContainer);
+    bottomRow.getElement().setId("bottomRow");
+    bottomRow.setWidth("100%");
+    bottomRow.addStyleName("trueInlineStyle");
+
+    twoRows.add(bottomRow);
+
+    FlowPanel currentExerciseVPanel = new FlowPanel();
+    currentExerciseVPanel.getElement().setId("defect_currentExercisePanel");
+
+    currentExerciseVPanel.addStyleName("floatLeftList");
+    bottomRow.add(currentExerciseVPanel);
+
+    FlexSectionExerciseList widgets = makeNPFExerciseList(topRow, currentExerciseVPanel, instanceName, ul.getUniqueID());
+    npfExerciseList = widgets;
+
+    Widget exerciseListOnLeftSide = npfExerciseList.getExerciseListOnLeftSide(controller.getProps());
+    exerciseListContainer.add(exerciseListOnLeftSide);
+
+    widgets.addWidgets();
+    return twoRows;
+  }
+
+  private FlexSectionExerciseList makeNPFExerciseList(final Panel topRow, Panel currentExercisePanel, String instanceName, long userListID) {
+    final FlexSectionExerciseList exerciseList = makeExerciseList(topRow, currentExercisePanel, instanceName);
+    exerciseList.setUserListID(userListID);
+
+    setFactory(exerciseList, instanceName, userListID);
+    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+      @Override
+      public void execute() {
+        exerciseList.onResize();
+      }
+    });
+    return exerciseList;
+  }
+
+  void setFactory(final PagingExerciseList exerciseList, final String instanceName, long userListID) {
+    exerciseList.setFactory(getFactory(exerciseList, instanceName), userManager, 1);
+  }
+
+  protected abstract ExercisePanelFactory getFactory(final PagingExerciseList exerciseList, final String instanceName);
+
+  private FlexSectionExerciseList makeExerciseList(final Panel topRow, Panel currentExercisePanel, final String instanceName) {
+    return new FlexSectionExerciseList(topRow, currentExercisePanel, service, feedback, false, false, controller, true, instanceName) {
+      @Override
+      protected void onLastItem() {
+        new ModalInfoDialog("Complete", "List complete!", new HiddenHandler() {
+          @Override
+          public void onHidden(HiddenEvent hiddenEvent) {
+            reloadExercises();
+          }
+        });
+      }
+
+      @Override
+      protected void noSectionsGetExercises(long userID) {
+        System.out.println(getClass() + " : noSectionsGetExercises instanceName = " + instanceName + " for list " + userListID);
+        loadExercises(getHistoryToken(""), getPrefix());
+      }
+
+      @Override
+      protected void loadExercises(final Map<String, Collection<String>> typeToSection, final String item) {
+        System.out.println(getClass() + ".loadExercises : instance " + instance + " " + typeToSection + " and item '" + item + "'" + " for list " + userListID);
+        loadExercisesUsingPrefix(typeToSection, getPrefix());
+      }
+    };
+  }
+}
