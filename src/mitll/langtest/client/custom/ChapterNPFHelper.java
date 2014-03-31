a25ff914e88a45b19a140b2cf81df729efdbd5a9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-03-31 14:28:07 -0400

    got tab resize working properly for hierarchy review/comment tabs
    Former-commit-id: b55157048bdd0ca69e666ac9ef93bd8365c0d513 [formerly 31fa4a40472fd80463b7623aef4d90e9c9d762c6] [formerly b55157048bdd0ca69e666ac9ef93bd8365c0d513 [formerly 31fa4a40472fd80463b7623aef4d90e9c9d762c6] [formerly 1e73c8ecb633ddc501f93b63087220f20f9db026]]
    Former-commit-id: d5c6696a5784eaef52c5e12710b1773ce780ce35 [formerly ec4849f023201a01967205c0058ccafde6a5fe59] [formerly e899727ba68f068b404d750cada17fe7fc2a7e4a] [formerly 7f12b33fd88f11652b7672bce99dc800ca07c9eb [formerly 8fae90b5129899f80dce0e55ab2e369a099c0fe9] [formerly fa713adc7248a83417304fcdbb1364f9f286b331]]
    Former-commit-id: 7f12b33fd88f11652b7672bce99dc800ca07c9eb [formerly 8fae90b5129899f80dce0e55ab2e369a099c0fe9]
    Former-commit-id: 2f76610beebbd8813efe657fd7710830a63df38f
    Former-commit-id: be53f209598a398ff77cb2fa403410e739d84a9f

--- src/mitll/langtest/client/custom/ChapterNPFHelper.java
+++ src/mitll/langtest/client/custom/ChapterNPFHelper.java
@@ -1,35 +1,19 @@
 package mitll.langtest.client.custom;
 
-import com.github.gwtbootstrap.client.ui.FluidRow;
-import com.github.gwtbootstrap.client.ui.event.HiddenEvent;
-import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.LangTestDatabaseAsync;
-import mitll.langtest.client.bootstrap.FlexSectionExerciseList;
-import mitll.langtest.client.dialog.ModalInfoDialog;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.exercise.ExercisePanelFactory;
 import mitll.langtest.client.list.PagingExerciseList;
-import mitll.langtest.client.scoring.GoodwaveExercisePanelFactory;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.client.user.UserManager;
 import mitll.langtest.shared.custom.UserList;
-
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by GO22670 on 3/26/2014.
  */
 public class ChapterNPFHelper extends NPFHelper {
-  FlexListLayout flexListLayout;
+  private FlexListLayout flexListLayout;
   public ChapterNPFHelper(final LangTestDatabaseAsync service, final UserFeedback feedback,
                           final UserManager userManager, final ExerciseController controller) {
     super(service, feedback, userManager, controller);
@@ -41,7 +25,7 @@ public class ChapterNPFHelper extends NPFHelper {
       }
     };
 
-    System.out.println(getClass() + " : ChapterNPFHelper " );
+    System.out.println(getClass() + " : ChapterNPFHelper flexListLayout " + flexListLayout );
   }
 
   /**
@@ -51,48 +35,20 @@ public class ChapterNPFHelper extends NPFHelper {
    * @return
    */
   protected Panel doInternalLayout(UserList ul, String instanceName) {
+    System.out.println(getClass() + " : doInternalLayout instanceName = " + instanceName + " for list " + ul);
     Panel widgets = flexListLayout.doInternalLayout(ul, instanceName);
     npfExerciseList = flexListLayout.npfExerciseList;
     return widgets;
   }
-/*
-  private FlexSectionExerciseList makeNPFExerciseList(final Panel topRow,Panel currentExercisePanel, String instanceName, long userListID) {
-    final FlexSectionExerciseList exerciseList = makeExerciseList(topRow,currentExercisePanel, instanceName);
-    exerciseList.setUserListID(userListID);
 
-    setFactory(exerciseList, instanceName, userListID);
-    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-      @Override
-      public void execute() {
-        exerciseList.onResize();
-      }
-    });
-    return exerciseList;
-  }*/
-
-/*  private FlexSectionExerciseList makeExerciseList(final Panel topRow,Panel currentExercisePanel, final String instanceName) {
-    return new FlexSectionExerciseList(topRow, currentExercisePanel, service, feedback, false, false, controller, true, instanceName) {
-      @Override
-      protected void onLastItem() {
-        new ModalInfoDialog("Complete", "List complete!", new HiddenHandler() {
-          @Override
-          public void onHidden(HiddenEvent hiddenEvent) {
-            reloadExercises();
-          }
-        });
-      }
-
-      @Override
-      protected void noSectionsGetExercises(long userID) {
-        System.out.println(getClass() + " : noSectionsGetExercises instanceName = " + instanceName + " for list " + userListID);
-        loadExercises(getHistoryToken(""), getPrefix());
-      }
-
-      @Override
-      protected void loadExercises(final Map<String, Collection<String>> typeToSection, final String item) {
-        System.out.println(getClass() +".loadExercises : instance " + instance + " " + typeToSection + " and item '" + item + "'"+ " for list " + userListID);
-        loadExercisesUsingPrefix(typeToSection, getPrefix());
-      }
-    };
-  }*/
+  @Override
+  public void onResize() {
+    if (flexListLayout != null) {
+      flexListLayout.onResize();
+    } else if (npfExerciseList != null) {
+      npfExerciseList.onResize();
+    } else {
+      System.err.println("not sending resize event - flexListLayout is null?");
+    }
+  }
 }
--- src/mitll/langtest/client/custom/ReviewItemHelper.java
+++ src/mitll/langtest/client/custom/ReviewItemHelper.java
@@ -18,10 +18,10 @@ import mitll.langtest.shared.custom.UserList;
  * Created by GO22670 on 3/26/2014.
  */
 public class ReviewItemHelper extends NPFHelper {
-  FlexListLayout flexListLayout;
-  HasText itemMarker;
-  ListInterface predefinedContent;
-  NPFHelper npfHelper;
+  private FlexListLayout flexListLayout;
+  private HasText itemMarker;
+  private ListInterface predefinedContent;
+  private NPFHelper npfHelper;
 
   /**
    * @see mitll.langtest.client.custom.Navigation#Navigation(mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.user.UserManager, mitll.langtest.client.exercise.ExerciseController, mitll.langtest.client.list.ListInterface, mitll.langtest.client.user.UserFeedback)
@@ -35,15 +35,13 @@ public class ReviewItemHelper extends NPFHelper {
   public ReviewItemHelper(final LangTestDatabaseAsync service, final UserFeedback feedback,
                           final UserManager userManager, final ExerciseController controller,
                           final HasText itemMarker,
-                          // final UserList originalList,
                           final ListInterface predefinedContent,
                           NPFHelper npfHelper) {
     super(service, feedback, userManager, controller);
-    // final NPFHelper outer = this;
     this.itemMarker = itemMarker;
     this.predefinedContent = predefinedContent;
     this.npfHelper = npfHelper;
-    System.out.println(getClass() + " : ReviewItemHelper "+ npfHelper);
+    System.out.println(getClass() + " : ReviewItemHelper ");
   }
 
   /**
@@ -56,53 +54,49 @@ public class ReviewItemHelper extends NPFHelper {
    * @return
    */
   protected Panel doInternalLayout(final UserList ul, String instanceName) {
+    System.out.println(getClass() + " : doInternalLayout instanceName = " + instanceName + " for list " + ul);
+
     this.flexListLayout = new FlexListLayout(service,feedback,userManager,controller) {
       @Override
       protected ExercisePanelFactory getFactory(final PagingExerciseList pagingExerciseList, String instanceName) {
         return new ExercisePanelFactory(service,feedback,controller,predefinedContent) {
           @Override
           public Panel getExercisePanel(CommonExercise exercise) {
-
-         //   makeListOfOnlyYourItems
-
-            System.out.println("list " + pagingExerciseList);
-            System.out.println("npfExerciseList " + npfExerciseList);
+            System.out.println(getClass() + " : list ");
 
             ReviewEditableExercise reviewEditableExercise =
               new ReviewEditableExercise(service, controller, itemMarker, exercise.toCommonUserExercise(), ul,
                 pagingExerciseList, predefinedContent, npfHelper);
-            SimplePanel toAddTo = new SimplePanel();
+            SimplePanel ignoredContainer = new SimplePanel();
 
             Panel widgets = reviewEditableExercise.addNew(ul, ul,
-              //exerciseList,
               npfExerciseList,
-              toAddTo);
+              ignoredContainer);
             reviewEditableExercise.setFields(exercise);
 
             return widgets;
-
-
-
- /*           Panel panel = new SimplePanel();
-            panel.getElement().setId("EditItemPanel");
-            // TODO : do something better here than toCommonUserExercise
-            reviewEditableExercise.populatePanel(e.toCommonUserExercise(), panel, ul, originalList, itemMarker, outer);
-            return panel;*/
           }
-
-    /*      private UserList makeListOfOnlyYourItems(UserList toCopy) {
-            UserList copy2 = new UserList(toCopy);
-            for (CommonUserExercise ue : toCopy.getExercises()) {
-              copy2.addExercise(ue);
-            }
-            return copy2;
-          }*/
         };
       }
     };
 
     Panel widgets = flexListLayout.doInternalLayout(ul, instanceName);
     npfExerciseList = flexListLayout.npfExerciseList;
+
+  //  System.out.println(getClass() + " : doInternalLayout flexListLayout = " + flexListLayout);
+   // System.out.println(getClass() + " : doInternalLayout npfExerciseList = " + npfExerciseList);
+
     return widgets;
+  }
+
+  @Override
+  public void onResize() {
+    if (flexListLayout != null) {
+      flexListLayout.onResize();
+    } else if (npfExerciseList != null) {
+      npfExerciseList.onResize();
+    } else {
+      System.err.println("not sending resize event - flexListLayout is null?");
+    }
   }
 }
