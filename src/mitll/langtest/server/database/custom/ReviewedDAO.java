32eacb1d5a4661f44aebb326e37a185b64326f6d
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-01 19:21:44 -0400

    mostly there toward recording exercise states
    Former-commit-id: 00b166d7fe20a7aec7cfd0204e9abb1c28b612bd [formerly f1ae3de059635c524e2de5d78c915b51be1244d7] [formerly 6dd1bc3a9bf18688794d8c3c7fb3492165f86cac [formerly 0bad4b8be3ecfa8dcf342dcf489a2d9ead964b3b]] [formerly 00b166d7fe20a7aec7cfd0204e9abb1c28b612bd [formerly f1ae3de059635c524e2de5d78c915b51be1244d7] [formerly 6dd1bc3a9bf18688794d8c3c7fb3492165f86cac [formerly 0bad4b8be3ecfa8dcf342dcf489a2d9ead964b3b]] [formerly 00b166d7fe20a7aec7cfd0204e9abb1c28b612bd [formerly f1ae3de059635c524e2de5d78c915b51be1244d7] [formerly 6dd1bc3a9bf18688794d8c3c7fb3492165f86cac [formerly 0bad4b8be3ecfa8dcf342dcf489a2d9ead964b3b]] [formerly 4f27b42fedac96559344955c0878596a2e9401fb]]]
    Former-commit-id: 93fb57197b5f7040a9842d042a8f3d465ffd297f [formerly 117cac563b2f122a2db54c2467038fabed10c4d0] [formerly ffbdcb3dca0e568de8ca05587912291f2eec51b9 [formerly 1d2860c870244c07e428ed5411788ca04a5b0987]] [formerly 93fb57197b5f7040a9842d042a8f3d465ffd297f [formerly 117cac563b2f122a2db54c2467038fabed10c4d0] [formerly ffbdcb3dca0e568de8ca05587912291f2eec51b9 [formerly 1d2860c870244c07e428ed5411788ca04a5b0987]] [formerly eef372ef39bdbd4497a7f6926d6c1bea860a6c23]] [formerly 6255b770d7af564ca56ad7c6f23d9f308e6f3c3f [formerly bb27f6329f791bf60d9a860a2d0f52d993ac3f77] [formerly 16e4ba3580a61c00126c448780f829e8090b3bfa [formerly 6a0f74669469158dc4fdf2de13215c33bfe5814c]] [formerly 6255b770d7af564ca56ad7c6f23d9f308e6f3c3f [formerly bb27f6329f791bf60d9a860a2d0f52d993ac3f77] [formerly 16e4ba3580a61c00126c448780f829e8090b3bfa [formerly 6a0f74669469158dc4fdf2de13215c33bfe5814c]] [formerly 029f3de1f59b1e12309992d15d08c22f1b5bc65b]]]
    Former-commit-id: 6255b770d7af564ca56ad7c6f23d9f308e6f3c3f [formerly bb27f6329f791bf60d9a860a2d0f52d993ac3f77] [formerly 16e4ba3580a61c00126c448780f829e8090b3bfa [formerly 6a0f74669469158dc4fdf2de13215c33bfe5814c]]
    Former-commit-id: 0d91caac5a99b8189c021041e1cdf04f5458f9b5 [formerly 27a81610385c689757928703b332ad15a113716f] [formerly 3366004645d92196e37504cb9e4846504ca442c4 [formerly a8156113079e95ae3be65d704a6375fe8c237837]] [formerly b08a5fb72b6cf18fbb50f91f7ca8c669f7908a3d]
    Former-commit-id: cd6c15f529bc0cfac2c0d4d37bc5707ee55ddf4d
    Former-commit-id: 85038925aaa9febdbdff1090b3541c405e8f0163 [formerly 6290da5dbc7b2e9c08548a6887ac2a11c20055eb]
    Former-commit-id: 9e5f19aa79374407d9ffccb367e2a3cbcd69aae8

--- src/mitll/langtest/server/database/custom/ReviewedDAO.java
+++ src/mitll/langtest/server/database/custom/ReviewedDAO.java
@@ -9,12 +9,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Created with IntelliJ IDEA.
+ * Let's us keep track of state of read-only exercises too.
+ *
+ *
  * User: GO22670
  * Date: 12/9/13
  * Time: 2:23 PM
@@ -24,11 +29,17 @@ public class ReviewedDAO extends DAO {
   private static final Logger logger = Logger.getLogger(ReviewedDAO.class);
 
   private static final String REVIEWED = "reviewed";
+  private static final String STATE = "state";
 
   public ReviewedDAO(Database database) {
     super(database);
     try {
       createTable(database);
+      Collection<String> columns = getColumns(REVIEWED);
+      Connection connection = database.getConnection();
+      if (!columns.contains(STATE)) {
+        addVarchar(connection, REVIEWED, STATE);
+      }
     } catch (SQLException e) {
       logger.error("got " + e, e);
     }
@@ -51,6 +62,7 @@ public class ReviewedDAO extends DAO {
       "uniqueid IDENTITY, " +
       "creatorid LONG, " +
       "exerciseid VARCHAR, " +
+      "state VARCHAR, " +
       "modified TIMESTAMP," +
       "FOREIGN KEY(creatorid) REFERENCES " +
       "USERS" +
@@ -67,9 +79,9 @@ public class ReviewedDAO extends DAO {
    * <p/>
    * Uses return generated keys to get the user id
    *
-   * @see UserListManager#markReviewed(String, long)
+   * @see UserListManager#markApproved(String, long)
    */
-  public void add(String exerciseID, long creatorID) {
+  public void add(String exerciseID, long creatorID, String state) {
     try {
       // there are much better ways of doing this...
       logger.info("add :reviewed " + exerciseID + " by " + creatorID);
@@ -77,12 +89,14 @@ public class ReviewedDAO extends DAO {
       Connection connection = database.getConnection();
       PreparedStatement statement = connection.prepareStatement(
         "INSERT INTO " + REVIEWED +
-          "(creatorid,exerciseid,modified) " +
-          "VALUES(?,?,?);");
+          "(creatorid,exerciseid,modified," +STATE+
+          ") " +
+          "VALUES(?,?,?,?);");
       int i = 1;
       statement.setLong(i++, creatorID);
       statement.setString(i++, exerciseID);
       statement.setTimestamp(i++, new Timestamp(System.currentTimeMillis()));
+      statement.setString(i++, state);
 
       int j = statement.executeUpdate();
 
@@ -125,9 +139,42 @@ public class ReviewedDAO extends DAO {
       statement.close();
       database.closeConnection(connection);
 
-   //   int count = getCount();
-  //    logger.debug("now " + count + " reviewed");
-   //   if (before-count != 1) logger.error("ReviewedDAO : huh? there were " +before +" before");
+      //   int count = getCount();
+      //    logger.debug("now " + count + " reviewed");
+      //   if (before-count != 1) logger.error("ReviewedDAO : huh? there were " +before +" before");
+    } catch (Exception ee) {
+      logger.error("got " + ee, ee);
+    }
+  }
+
+  public void setState(String exerciseID, String state) {
+    try {
+    //  int before = getCount();
+      Connection connection = database.getConnection();
+      PreparedStatement statement = connection.prepareStatement(
+        "UPDATE " + REVIEWED +
+          " SET STATE='" +state+
+          "' " +
+          " WHERE " +
+          "exerciseid" +
+          "='" + exerciseID +
+          "'");
+
+      int j = statement.executeUpdate();
+
+      if (j != 1) {
+        logger.error("remove : huh? didn't change row for " + exerciseID);
+        //int count = getCount();
+      //  logger.debug("now " + count + " reviewed");
+        //if (before-count != 1) logger.error("ReviewedDAO : huh? there were " +before +" before");
+      }
+
+      statement.close();
+      database.closeConnection(connection);
+
+      //   int count = getCount();
+      //    logger.debug("now " + count + " reviewed");
+      //   if (before-count != 1) logger.error("ReviewedDAO : huh? there were " +before +" before");
     } catch (Exception ee) {
       logger.error("got " + ee, ee);
     }
@@ -139,7 +186,11 @@ public class ReviewedDAO extends DAO {
    */
   public Set<String> getReviewed() {
     Connection connection = database.getConnection();
-    String sql = "SELECT DISTINCT exerciseid from " + REVIEWED + " order by exerciseid";
+    String state = "approved";
+    String sql = "SELECT DISTINCT exerciseid from " + REVIEWED + " where " +STATE+"='" +
+      state +
+      "'"+
+      " order by exerciseid";
 
     Set<String> lists = Collections.emptySet();
     try {
@@ -160,6 +211,35 @@ public class ReviewedDAO extends DAO {
       logger.error("Got " + e + " doing " + sql, e);
     }
     return lists;
+  }
+
+  public Map<String,String> getStateMap() {
+    Connection connection = database.getConnection();
+    String sql = "SELECT exerciseid," +
+      STATE+
+      " from " + REVIEWED;
+
+    try {
+      PreparedStatement statement = connection.prepareStatement(sql);
+      ResultSet rs = statement.executeQuery();
+      Map<String,String> exidToState = new HashMap<String, String>();
+      while (rs.next()) {
+        String string = rs.getString(1);
+        String state = rs.getString(2);
+        if (state == null) state = "approved";
+        exidToState.put(string, state);
+      }
+
+      // logger.debug("getReviewed sql " + sql + " yielded " + lists.size());
+      logger.debug("getStateMap yielded " + exidToState.size());
+      rs.close();
+      statement.close();
+      database.closeConnection(connection);
+      return exidToState;
+    } catch (SQLException e) {
+      logger.error("Got " + e + " doing " + sql, e);
+    }
+    return Collections.emptyMap();
   }
 
   public int getCount() { return getCount(REVIEWED);  }
