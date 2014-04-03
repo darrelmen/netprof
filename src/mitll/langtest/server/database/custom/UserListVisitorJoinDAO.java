9f5af490cb78c476c79c599c16be7ce02d1bfa23
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-03 19:59:53 -0400

    fixed bug where wouldn't advance in avp if did start over after first item
    Former-commit-id: 4c28fd72681a7caaf323b284790a0c4625c83ada [formerly e5d8dc8d7ca694b173c1598932d4a13f2e21c4a6] [formerly 494801492b3663b9887e774aa3b7179654d247ad [formerly 6fb2f523decd8f04728d458f91452fd7612ec50f]] [formerly 4c28fd72681a7caaf323b284790a0c4625c83ada [formerly e5d8dc8d7ca694b173c1598932d4a13f2e21c4a6] [formerly 494801492b3663b9887e774aa3b7179654d247ad [formerly 6fb2f523decd8f04728d458f91452fd7612ec50f]] [formerly 66f75b6482ec0c12996c19bc76e46f2b9d03f32b]]
    Former-commit-id: 4039b0dc169fc39e936de09346f5ce15ae4d4811 [formerly 07ad8484e24dfac318f8e747dce8530058ba9be7] [formerly 41a35f3540a0a5aa402e71903bb997c3e5ec222a [formerly ef15f798866cfc75ff1be6a8ecb646b560f87716]] [formerly 46101ec9236643facc5fbdc85c6c56a3e8d2a072] [formerly 410160d2a8bac1ff0df06ed3e04fb9183ca3d186 [formerly 317ffe16be23dd675027c68d14f3acf93ff23eea] [formerly 42d26f94b5a2c5383c2eb112ab400c4f1f9eb201 [formerly 3f8b253e025c6eec9c7a20dac333b74e41253eaf]] [formerly 2e1a5646695169a9dc15e71eb13f8726b59bc405]]
    Former-commit-id: 410160d2a8bac1ff0df06ed3e04fb9183ca3d186 [formerly 317ffe16be23dd675027c68d14f3acf93ff23eea] [formerly 42d26f94b5a2c5383c2eb112ab400c4f1f9eb201 [formerly 3f8b253e025c6eec9c7a20dac333b74e41253eaf]]
    Former-commit-id: 2c317063c2ceeabcb37e2a90254bde0242a522ac
    Former-commit-id: eb88a9dc65e9620b8482804b42efb7fba7fafaa4 [formerly 1383fa9d7b5b8cacfd2fe6230163611b6ba76cf9]
    Former-commit-id: 75d9ba94628c54abcef361cbd2103733e7445b60

--- src/mitll/langtest/server/database/custom/UserListVisitorJoinDAO.java
+++ src/mitll/langtest/server/database/custom/UserListVisitorJoinDAO.java
@@ -11,9 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
