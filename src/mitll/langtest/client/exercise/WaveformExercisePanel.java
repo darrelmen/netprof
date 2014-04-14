7cee2a0834e392a71a36eeff5a1d329b2c40d21a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-14 18:17:55 -0400

    adds in semi colon entries at the bottom of the list, steps towards allowing course example style recording
    Former-commit-id: 40807070e109804714e6967dfe5ddd3e40ab75a3 [formerly a9296737ead63bb77cef7ee2b88ba3fda1fb3257]
    Former-commit-id: 8cf6acbcafc73ec00b12597cfbd4ecdf1b724c40

---
+++ src/mitll/langtest/client/exercise/WaveformExercisePanel.java
@@ -0,0 +1,143 @@
+package mitll.langtest.client.exercise;
+
+import com.google.gwt.user.client.ui.CaptionPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ProvidesResize;
+import com.google.gwt.user.client.ui.RequiresResize;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.list.ListInterface;
+import mitll.langtest.client.user.UserFeedback;
+import mitll.langtest.shared.CommonExercise;
+import mitll.langtest.shared.Result;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Does fancy flashing record bulb while recording.
+ * User: GO22670
+ * Date: 12/18/12
+ * Time: 6:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WaveformExercisePanel extends ExercisePanel {
+  public static final String REPEAT_TWICE = "<i>Record the word or phrase twice, first at normal speed, then again at slow speed.</i>";
+  private boolean isBusy = false;
+  private Collection<RecordAudioPanel> audioPanels;
+
+  /**
+   * @param e
+   * @param service
+   * @param userFeedback
+   * @param controller
+   * @seex mitll.langtest.client.exercise.ExercisePanelFactory#getExercisePanel(mitll.langtest.shared.Exercise)
+   */
+  public WaveformExercisePanel(final CommonExercise e, final LangTestDatabaseAsync service, final UserFeedback userFeedback,
+                               final ExerciseController controller, ListInterface exerciseList) {
+    super(e, service, userFeedback, controller, exerciseList);
+    getElement().setId("WaveformExercisePanel");
+  }
+
+  public void setBusy(boolean v) {
+    this.isBusy = v;
+    setButtonsEnabled(!isBusy);
+  }
+
+  public boolean isBusy() {
+    return isBusy;
+  }
+
+  /**
+   * Has a answerPanel mark to indicate when the saved audio has been successfully posted to the server.
+   *
+   * @param exercise
+   * @param service
+   * @param controller
+   * @param index
+   * @return
+   * @seex ExercisePanel#ExercisePanel(mitll.langtest.shared.Exercise, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.user.UserFeedback, ExerciseController, ListInterface)
+   */
+  protected Widget getAnswerWidget(CommonExercise exercise, LangTestDatabaseAsync service, ExerciseController controller, final int index) {
+    audioPanels = new ArrayList<RecordAudioPanel>();
+    VerticalPanel vp = new VerticalPanel();
+
+    RecordAudioPanel fast = new RecordAudioPanel(exercise, controller, this, service, index, true, Result.AUDIO_TYPE_REGULAR);
+    ResizableCaptionPanel cp = new ResizableCaptionPanel("Regular speed recording");
+    cp.setContentWidget(fast);
+    vp.add(cp);
+
+    audioPanels.add(fast);
+    addAnswerWidget(index, fast);
+
+    RecordAudioPanel slow = new RecordAudioPanel(exercise, controller, this, service, index + 1, true, Result.AUDIO_TYPE_SLOW);
+
+    cp = new ResizableCaptionPanel("Slow speed recording");
+    cp.setContentWidget(slow);
+    audioPanels.add(slow);
+    addAnswerWidget(index + 1, slow);
+
+    vp.add(cp);
+    return vp;
+  }
+
+  protected static class ResizableCaptionPanel extends CaptionPanel implements ProvidesResize, RequiresResize {
+    public ResizableCaptionPanel(String name) {
+      super(name);
+    }
+
+    public void onResize() {
+      Widget contentWidget = getContentWidget();
+      if (contentWidget instanceof RequiresResize) {
+        ((RequiresResize) contentWidget).onResize();
+      }
+    }
+  }
+
+
+  protected Widget getContentScroller(HTML maybeRTLContent) {
+    return maybeRTLContent;
+  }
+
+  @Override
+  protected String getInstructions() {
+    String prefix = "<br/>" + "";//THREE_SPACES;
+    String prompt = REPEAT_TWICE;
+    if (controller.getAudioType().equals(Result.AUDIO_TYPE_REGULAR)) {
+      prompt = REPEAT_ONCE;
+    }
+    return prefix + prompt;
+  }
+
+  @Override
+  public void onResize() {
+    for (RecordAudioPanel ap : audioPanels) {
+      ap.onResize();
+    }
+  }
+
+  /**
+   * @param promptInEnglish
+   * @return
+   * @seex ExercisePanel#addQuestionPrompt(com.google.gwt.user.client.ui.Panel, mitll.langtest.shared.Exercise)
+   */
+  @Override
+  protected String getQuestionPrompt(boolean promptInEnglish) {
+    return getSpokenPrompt(promptInEnglish);
+  }
+
+  /**
+   * on the server, notice which audio posts have arrived, and take the latest ones...
+   * <br></br>
+   * Move on to next exercise...
+   *
+   * @param controller
+   * @param completedExercise
+   */
+  @Override
+  public void postAnswers(ExerciseController controller, CommonExercise completedExercise) {
+    // exerciseList.addCompleted(completedExercise.getID());
+    exerciseList.loadNextExercise(completedExercise);
+  }
+}
