1677a0f6179d8952bf9cee6aa991dc1a7ae1f41a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-17 20:30:33 -0400

    got fixing items working for duplicated items
    Former-commit-id: daf282cf25abd3900cc1aa74dce5ef35c0480488 [formerly b73592ba038875c6707bad3afab393d98595e7f4] [formerly daf282cf25abd3900cc1aa74dce5ef35c0480488 [formerly b73592ba038875c6707bad3afab393d98595e7f4] [formerly 02fcab21a7efca1bc695330e08f7d4689f946554 [formerly 76c1600d5c4d007fe2e88f9b6a3b441001d0acc4]]]
    Former-commit-id: ed5533253c1058be05b88002bddfba194c8d3adb [formerly 441fa44fa80f030ab6b9cee35cef4fd58d7c4ff5] [formerly 2db2b21da5ab02255b840030a256044123cf58ef [formerly 61d5d1759aa0c979b9b3436059306632f2938ae7]] [formerly 6706b2f516c8aaad35bb8572dc88182fa4a0fc3b [formerly f1dc3047ae2aaf2665529b4cba23459bec21321d] [formerly d01d05c193e5963a636f8ed6f7cb73e856bfc620 [formerly 0fb438ff9208c40e28c9f431ea8366a1d4901115]]]
    Former-commit-id: 6706b2f516c8aaad35bb8572dc88182fa4a0fc3b [formerly f1dc3047ae2aaf2665529b4cba23459bec21321d]
    Former-commit-id: 0f466d096673b9132c5d958dac06e860de087b78 [formerly 16fdb7e19cd195fb55775cbe55abbf3a2cc8b53a]
    Former-commit-id: 7cfb0de55be452c7a738f980eb1dc82bcb680d94

---
+++ src/mitll/langtest/client/custom/MyDropdown.java
@@ -0,0 +1,136 @@
+package mitll.langtest.client.custom;
+
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.ButtonToolbar;
+import com.github.gwtbootstrap.client.ui.NavList;
+import com.github.gwtbootstrap.client.ui.base.IconAnchor;
+import com.github.gwtbootstrap.client.ui.constants.AlertType;
+import com.github.gwtbootstrap.client.ui.constants.IconType;
+import com.github.gwtbootstrap.client.ui.constants.Placement;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.ui.PopupPanel;
+
+import java.util.Set;
+
+
+/**
+ * Created by GO22670 on 4/17/2014.
+ */
+public class MyDropdown {
+  private Button addBtn;
+  // avoid issues in bootstrap dropdown button sizing by creating our own
+  // small popup.
+  // TODO Refactor. Duplicates Domino Toolbar.
+  private PopupPanel addPopup;
+  private Button removeBtn;
+  // private OrganizerMainPanel mainPanel;
+  private UIHandler uiHandler;
+
+  public MyDropdown(/*OrganizerMainPanel parent*/) {
+    //   super(parent);
+    //  this.mainPanel = parent;
+    init();
+  }
+/*
+    private ProjectDocListPanel getDocList() {
+      return mainPanel.getAssembleView().getProjectDocListPanel();
+    }*/
+
+  private void init() {
+    uiHandler = new UIHandler();
+    ButtonToolbar toolbar = new ButtonToolbar();
+    //   boolean toggleBtn = mainPanel.getAssembleView().getExam().getContent().getFormIds().size() > 1;
+    //  addBtn = createBSButton(IconType.ARROW_DOWN, null, toggleBtn, uiHandler);
+    addBtn.setCaret(true);
+    // createAddTooltip(addBtn, "Add from Project to Assembly", Placement.RIGHT);
+    //  removeBtn = createBSButton(IconType.ARROW_UP, null, false, uiHandler);
+    //  createAddTooltip(removeBtn, "Remove from Assembly", Placement.RIGHT);
+    toolbar.add(addBtn);
+    toolbar.add(removeBtn);
+    //  initWidget(toolbar);
+  }
+
+/*    private void doHandleRemove() {
+      if (mainPanel.getOrganizerList().getSelected().size() <= 0) {
+        getMsgHelper().makeInlineMessage("Please select an item from the Exam Included Items table to remove.",
+          true, AlertType.INFO);
+      } else {
+        mainPanel.removeDocumentsFromAssembly(mainPanel.getOrganizerList().getSelected());
+      }
+    }*/
+
+/*    private void doHandleAdd() {
+      Set<ValidationFormID> fids = mainPanel.getAssembleView().getExam().getContent().getFormIds();
+      if (fids.size() == 1) {
+        addToAssembly(fids.iterator().next());
+      } else {
+        updateDropdownMenu(fids);
+      }
+    }*/
+
+/*    private void addToAssembly(ValidationFormID fid) {
+      if (getDocList().getSelected().size() == 0) {
+        getMsgHelper().makeInlineMessage("Please select an item from the Exam Excluded Items table to add.",
+          true, AlertType.INFO);
+      } else {
+        mainPanel.addDocumentsToAssembly(fid, getDocList().getSelected());
+      }
+    }*/
+
+  private void updateDropdownMenu(Set<String> fids) {
+    if (!addBtn.isActive()) {
+      if (addPopup == null) {
+        addPopup = new PopupPanel(true);
+        addPopup.addStyleName("dropdown-popup");
+        addPopup.addAutoHidePartner(addBtn.getElement());
+        addPopup.addCloseHandler(new CloseHandler<PopupPanel>() {
+          @Override
+          public void onClose(CloseEvent<PopupPanel> event) {
+            Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+              @Override
+              public void execute() {
+                addBtn.setActive(false);
+              }
+            });
+          }
+        });
+        NavList theList = new NavList();
+
+
+     /*     for (ValidationFormID fid : fids) {
+            theList.add(createNavLink(fid.getDisplayName(), null, null, uiHandler));
+          }*/
+
+        addPopup.add(theList);
+      }
+      addPopup.showRelativeTo(addBtn);
+    } else if (addPopup != null) {
+      addPopup.hide();
+    }
+  }
+
+  private class UIHandler implements ClickHandler {
+
+    @Override
+    public void onClick(ClickEvent event) {
+      Object src = event.getSource();
+      if (src == removeBtn) {
+        //doHandleRemove();
+      } else if (src == addBtn) {
+        //doHandleAdd();
+      } else if (true) {//hasAncestor(addPopup, src)) {
+        addPopup.hide();
+        // handle split button and item selection
+        if (src instanceof IconAnchor) {
+          String formIdNm = ((IconAnchor) src).getText().trim();
+          //addToAssembly(ValidationFormID.valueOfDN(formIdNm));
+        }
+      }
+    }
+  }
+}
+
