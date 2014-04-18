15a4f252f6eb146f7c2ba773a63229e259040365
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-18 17:06:22 -0400

    added a new list button next to add to list button
    Former-commit-id: ecdbfbbb986b571e53199f76b0034a0520389a41 [formerly 44e5e731977d13c0c112d568d86dc3522106e6af] [formerly d593202b076990eba899ac23f3ec87345e0d250d [formerly b62a4e043518eba9ea5f955c7fa696c565ecff92]] [formerly ecdbfbbb986b571e53199f76b0034a0520389a41 [formerly 44e5e731977d13c0c112d568d86dc3522106e6af] [formerly d593202b076990eba899ac23f3ec87345e0d250d [formerly b62a4e043518eba9ea5f955c7fa696c565ecff92]] [formerly 5a0a5d921f08998b2a26f750586868bc8771980e [formerly f0d8b2d870e03894a4d21c03616c4dd1f1780844] [formerly 630e204455a69e0e49b6d7b631d71e5ffe29e031 [formerly 6af378f727a195121581515aac882ce6c4e33855]]]]
    Former-commit-id: 2726d4defc1ee50cf704588bcfc4671e38429ff5 [formerly 8a9c8d4e12b172810ac27b4447f8e7cae9cbb31a] [formerly 917dc861de67999f1b846cc486720b63626df931 [formerly 740baacea2a881674d93ec750782194607448693]] [formerly 1ad7d61ba9b227842971e402d93124126f8f8121 [formerly 07b9100e430275b2bd278a3b303c2c905fde41c6] [formerly 6ebdb80b757292c4d177226782f920d95066bdf1 [formerly a9140b9ee02f4ee8a3253443fab9047ed12c8365]]] [formerly e67c5e31e9694e6fb14f937c9d4e9f9d110ebcd4 [formerly b3f2fd9b98cafc5ae566e7c3ac5de99fbeccf7f1] [formerly cc3e01a309ec075986f8c613e698e9e4ff37ac01 [formerly 32245c8623f58dffd2628f216c88d505f561e4a4]] [formerly 79e1a7b4a2891f0af4062090c60be8ea50c37078 [formerly 8aff9e77d8c5726c4305c1f4845a279dfd7c7af9] [formerly afb0a12aa07b0893187f4b2e9c490d484043ddda [formerly 66e53d6ad6f5ecea7b4aecf13b8296a7fe172108]]]]
    Former-commit-id: e67c5e31e9694e6fb14f937c9d4e9f9d110ebcd4 [formerly b3f2fd9b98cafc5ae566e7c3ac5de99fbeccf7f1] [formerly cc3e01a309ec075986f8c613e698e9e4ff37ac01 [formerly 32245c8623f58dffd2628f216c88d505f561e4a4]]
    Former-commit-id: 56b85ca46d80cfc25fa8f73914021bba271b1bb4 [formerly 2c0817432fa06908429d3a118e63d107956d4472] [formerly 1eaa1db74d0d30bb5011aa6cf53cd224be347032 [formerly e6970e1d7a41fd8b3d37eddb651a79710ba42927]]
    Former-commit-id: a165c752abac13ba7c336385a21e941aa6f027e1 [formerly af200340a18adecd77095f4b1b803c45ce809705]
    Former-commit-id: b1af528c6a0650c0e4ef8ae81c4f7dd641cc020e

--- src/mitll/langtest/client/custom/NPFExercise.java
+++ src/mitll/langtest/client/custom/NPFExercise.java
@@ -1,16 +1,24 @@
 package mitll.langtest.client.custom;
 
+import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.DropdownButton;
 import com.github.gwtbootstrap.client.ui.NavLink;
+import com.github.gwtbootstrap.client.ui.TextBox;
+import com.github.gwtbootstrap.client.ui.Tooltip;
 import com.github.gwtbootstrap.client.ui.base.DropdownBase;
 import com.github.gwtbootstrap.client.ui.constants.ButtonType;
 import com.github.gwtbootstrap.client.ui.constants.IconType;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DecoratedPopupPanel;
+import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -33,6 +41,7 @@ import java.util.Collection;
  */
 public class NPFExercise extends GoodwaveExercisePanel {
   private static final String ADD_ITEM = "Add Item to List";
+  public static final String ITEM_ALREADY_ADDED = "Item already added to your list(s)";
 
   private DropdownButton addToList;
   private int activeCount = 0;
@@ -56,17 +65,18 @@ public class NPFExercise extends GoodwaveExercisePanel {
                                                            ListInterface listContainer, boolean addKeyHandler) {
     NavigationHelper navigationHelper = super.getNavigationHelper(controller, listContainer, addKeyHandler);
     navigationHelper.add(makeAddToList(exercise, controller));
+    navigationHelper.add(getNextListButton());
     return navigationHelper;
   }
 
   /**
-   * @see #addQuestionContentRow
+   * @see #getNavigationHelper(mitll.langtest.client.exercise.ExerciseController, mitll.langtest.client.list.ListInterface, boolean)
    * @param e
    * @param controller
    * @return
    */
   private Panel makeAddToList(CommonExercise e, ExerciseController controller) {
-    addToList = new DropdownButton("");
+    addToList = new DropdownButton("Add to List");
     addToList.getElement().setId("NPFExercise_AddToList");
     addToList.setDropup(true);
     addToList.setIcon(IconType.PLUS_SIGN);
@@ -77,12 +87,150 @@ public class NPFExercise extends GoodwaveExercisePanel {
     return addToList;
   }
 
+  private Widget getNextListButton() {
+    final HidePopupTextBox textBox = new HidePopupTextBox();
+    textBox.getElement().setId("NewList");
+    textBox.setVisibleLength(60);
+
+    final DecoratedPopupPanel commentPopup = makePopupAndButton(textBox);
+
+    final Button newListButton = new Button("New List");
+    commentPopup.addAutoHidePartner(newListButton.getElement()); // fix for bug Wade found where click didn't toggle comment
+    configureTextBox("", textBox, commentPopup);
+
+    configureNewListButton(newListButton,
+      commentPopup,
+      textBox);
+    return newListButton;
+  }
+
   /**
+   * @seex #getEntry(String, com.google.gwt.user.client.ui.Widget, mitll.langtest.shared.ExerciseAnnotation)
+   * @param popupButton
+   * @paramx alreadyMarkedCorrect
+   * @param popup
+   * @paramx comment
+   * @param textEntry
+   * @return
+   */
+  private void configureNewListButton(final Button popupButton,
+                                      final PopupPanel popup,
+                                      final TextBox textEntry) {
+    popupButton.setIcon(IconType.LIST_UL);
+    popupButton.setType(ButtonType.PRIMARY);
+    popupButton.addStyleName("leftFiveMargin");
+    popupButton.getElement().setId("NPFExercise_popup");
+    controller.register(popupButton, exercise.getID(), "show new list");
+
+    configurePopupButton(popupButton, popup, textEntry, addTooltip(popupButton, "Make a new list"));
+  }
+
+  protected void configurePopupButton(final Button popupButton, final PopupPanel popup, final TextBox textEntry, final Tooltip tooltip) {
+    popupButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        boolean visible = popup.isShowing();
+
+        if (visible) {// fix for bug that Wade found -- if we click off of popup, it dismisses it,
+          // but if that click is on the button, it would immediately shows it again
+          //System.out.println("popup visible " + visible);
+          popup.hide();
+        } else {
+          popup.showRelativeTo(popupButton);
+          textEntry.setFocus(true);
+          tooltip.hide();
+        }
+
+        makeANewList(textEntry);
+      }
+    });
+  }
+
+  protected void makeANewList(TextBox textEntry) {
+    String newListName = textEntry.getValue();
+    controller.logEvent(textEntry, "NewList_TextBox", exercise.getID(), "make new list called '" + newListName +
+      "'");
+    boolean addIt = !newListName.isEmpty();
+    for (UserList ul : listsForUser) {
+      if (ul.getName().equals(newListName)) {
+        addIt = false; break;
+      }
+    }
+    if (addIt) {
+      addUserList(controller.getUser(), newListName);
+    }
+  }
+
+  /**
+   * @seex #getEntry(String, com.google.gwt.user.client.ui.Widget, mitll.langtest.shared.ExerciseAnnotation)
+   * @paramx xfield
+   * @paramx commentButton
+   * @param commentEntryText
+   * @paramx clearButton
+   * @return
+   */
+  private DecoratedPopupPanel makePopupAndButton(/*Button commentButton,*/ TextBox commentEntryText) {
+    final DecoratedPopupPanel commentPopup = new DecoratedPopupPanel();
+    commentPopup.setAutoHideEnabled(true);
+    //commentPopup.configure(commentEntryText, commentButton, clearButton);
+    // commentPopup.setField(field);
+
+    Panel hp = new HorizontalPanel();
+    hp.add(commentEntryText);
+    hp.add(getOKButton(commentPopup));
+
+    commentPopup.add(hp);
+    return commentPopup;
+  }
+
+  /**
+   * Clicking OK just dismisses the popup.
+   * @param commentPopup
+   * @return
+   */
+  protected Button getOKButton(final PopupPanel commentPopup) {
+    Button ok = new Button("OK");
+    ok.setType(ButtonType.PRIMARY);
+    ok.addStyleName("leftTenMargin");
+    ok.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        commentPopup.hide();
+      }
+    });
+    return ok;
+  }
+
+  private void addUserList(long userID, String title) {
+    service.addUserList(userID,
+      title,
+      "",
+      "", new AsyncCallback<Long>() {
+        @Override
+        public void onFailure(Throwable caught) {
+        }
+
+        @Override
+        public void onSuccess(Long result) {
+          if (result == -1) {
+            System.err.println("should never happen!");
+          } else {
+            wasRevealed();
+          }
+        }
+      }
+    );
+  }
+
+  /**
+   * Every time this panel becomes visible again, we need to check the lists for this user.
+   *
    * @see Navigation#getTabPanel(com.google.gwt.user.client.ui.Panel)
    */
   @Override
   public void wasRevealed() { populateListChoices(exercise, controller, addToList);  }
 
+  private Collection<UserList> listsForUser;
   /**
    * Ask server for the set of current lists for this user.
    *
@@ -95,13 +243,13 @@ public class NPFExercise extends GoodwaveExercisePanel {
    * @see #wasRevealed()
    */
   private void populateListChoices(final CommonExercise e, final ExerciseController controller, final DropdownBase w1) {
-    //System.out.println("populateListChoices : populate list choices for " + controller.getUser());
     service.getListsForUser(controller.getUser(), true, false, new AsyncCallback<Collection<UserList>>() {
       @Override
       public void onFailure(Throwable caught) {}
 
       @Override
       public void onSuccess(Collection<UserList> result) {
+        listsForUser = result;
         w1.clear();
         activeCount = 0;
         boolean anyAdded = false;
@@ -127,7 +275,7 @@ public class NPFExercise extends GoodwaveExercisePanel {
                     widget.setVisible(false);
                     activeCount--;
                     if (activeCount == 0) {
-                      NavLink widget = new NavLink("Item already added to your list(s)");
+                      NavLink widget = new NavLink(ITEM_ALREADY_ADDED);
                       w1.add(widget);
                     }
                   }
@@ -137,7 +285,7 @@ public class NPFExercise extends GoodwaveExercisePanel {
           }
         }
         if (!anyAdded) {
-          NavLink widget = new NavLink("CommonExercise already added to your list(s)");
+          NavLink widget = new NavLink(ITEM_ALREADY_ADDED);
           w1.add(widget);
         }
       }
@@ -157,5 +305,42 @@ public class NPFExercise extends GoodwaveExercisePanel {
       }
     };
     t.schedule(2000);
+  }
+
+  /**
+   * For this field configure the textBox box to post annotation on blur and enter
+   *
+   * @param currentComment fill in with existing annotation, if there is one
+   * @param textBox comment box to configure
+   * @return
+   */
+  protected FocusWidget configureTextBox(String currentComment,
+                                         final HidePopupTextBox textBox,
+                                         final PopupPanel popup) {
+    if (currentComment != null) {
+      textBox.setText(currentComment);
+      if (textBox.getVisibleLength() < currentComment.length()) {
+        textBox.setVisibleLength(70);
+      }
+    }
+
+    textBox.addStyleName("leftFiveMargin");
+    textBox.configure(popup);
+
+    return textBox;
+  }
+
+  protected static class HidePopupTextBox extends TextBox {
+    public void configure( final PopupPanel popup) {
+      addKeyPressHandler(new KeyPressHandler() {
+        @Override
+        public void onKeyPress(KeyPressEvent event) {
+          int keyCode = event.getNativeEvent().getKeyCode();
+          if (keyCode == KeyCodes.KEY_ENTER) {
+            popup.hide();
+          }
+        }
+      });
+    }
   }
 }
