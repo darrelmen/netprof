f38386351a89a9aaa1d6fe5cc35a4f9071d423f7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-18 21:22:37 -0400

    almost there with getting all the tabs to be in right states with the right names and exercise panels
    Former-commit-id: 8ccf4859e1e35bf415654377f8d56d5c66114803

--- src/mitll/langtest/client/exercise/ExerciseController.java
+++ src/mitll/langtest/client/exercise/ExerciseController.java
@@ -77,7 +77,6 @@ public interface ExerciseController {
   void logMessageOnServer(String message, String prefix);
 
   StartupInfo getStartupInfo();
-  //boolean gotMicPermission();
   EventLogger getButtonFactory();
   void register(Button button, String exid);
 
