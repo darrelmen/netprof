39020391f6cc1a3369d132a82828fa7660ab6c58
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-04-29 15:36:51 -0400

    Fixed time marker and bugs with new list button
    Former-commit-id: 0921f29ff78f3b31f4ee043e49f9f24cb938cf65

--- src/mitll/langtest/client/scoring/AudioPositionPopup.java
+++ src/mitll/langtest/client/scoring/AudioPositionPopup.java
@@ -1,24 +1,21 @@
 package mitll.langtest.client.scoring;
 
-import com.github.gwtbootstrap.client.ui.base.DivWidget;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.sound.AudioControl;
 
 /**
  * Makes a red line appear on top of the waveform/spectogram,etc. marking playback position.
  */
-class AudioPositionPopup extends DivWidget implements AudioControl  {
+class AudioPositionPopup extends SimplePanel implements AudioControl  {
   private static int counter  = 0;
   private int id = 0;
 
-  //private final PopupPanel imageOverlay;
- // private final Panel imageOverlay;
   private float durationInMillis;
-  private static final boolean debugPartial = true;
-  private static final boolean debug = true;
+  private static final boolean debugPartial = false;
+  private static final boolean debug = false;
   private final Panel imageContainer;
 
   /**
@@ -27,23 +24,14 @@ class AudioPositionPopup extends DivWidget implements AudioControl  {
   public AudioPositionPopup(Panel imageContainer) {
     id = counter++;
     this.imageContainer = imageContainer;
-   // imageOverlay = new PopupPanel(false);
-    //imageOverlay = new DivWidget();
-    // imageOverlay.setStyleName("ImageOverlay");
     setStyleName("ImageOverlay");
     getElement().setId("AudioPositionPopup");
-   SimplePanel w = new SimplePanel();
-    //imageOverlay.add(w);
-    /*imageOverlay.*/
-     add(w);
-   // w.setStyleName("ImageOverlay");
   }
 
   /**
    * @see mitll.langtest.client.scoring.AudioPanel#getImageURLForAudio(String, String, int, mitll.langtest.client.scoring.AudioPanel.ImageAndCheck)
    */
   public void reinitialize(double durationInSeconds) {
-
     setWavDurationInSeconds(durationInSeconds);
     if (debugPartial) System.out.println(this + "  : AudioPositionPopup.reinitialize dur = " + durationInSeconds + "/" + durationInMillis);
     reinitialize();
@@ -65,16 +53,9 @@ class AudioPositionPopup extends DivWidget implements AudioControl  {
    * @see mitll.langtest.client.sound.PlayAudioPanel#songLoaded(double)
    */
   public void songLoaded(final double durationInMillisIgnored) {
-    int offsetHeight = imageContainer.getOffsetHeight();
-    int left = imageContainer.getAbsoluteLeft();
-    int top = imageContainer.getAbsoluteTop();
-    if (debugPartial) {
-      System.out.println(this + " songLoaded " + durationInMillisIgnored + " height " + offsetHeight + " x " + left +
-        " y " + top);
-    }
-    setSize("2px", offsetHeight + "px");
- //   imageOverlay.setPopupPosition(left, top);
-    getElement().getStyle().setPosition(Style.Position.ABSOLUTE);
+    Widget parent = imageContainer;// getParent();
+    setSize("2px", parent.getOffsetHeight() + "px");
+    getElement().getStyle().setPosition(Style.Position.FIXED);
     getElement().getStyle().setLeft(0, Style.Unit.PX);
     getElement().getStyle().setTop(0, Style.Unit.PX);
 /*    if (debug) System.out.println("songLoaded " + imageOverlay.isShowing() + " vis " + imageOverlay.isVisible() +
@@ -86,11 +67,7 @@ class AudioPositionPopup extends DivWidget implements AudioControl  {
   public void songFinished() {
     if (debugPartial) System.out.println(this + "  : AudioPositionPopup.songFinished ");
 
-    //imageOverlay.hide();
     setVisible(false);
-    int left = imageContainer.getAbsoluteLeft();
-    int top  = imageContainer.getAbsoluteTop();
-    //imageOverlay.setPopupPosition(left, top);
 
     getElement().getStyle().setLeft(0, Style.Unit.PX);
     getElement().getStyle().setTop(0, Style.Unit.PX);
@@ -101,9 +78,7 @@ class AudioPositionPopup extends DivWidget implements AudioControl  {
    * @param position
    */
   public void update(double position) {
-/*    if (!imageOverlay.isShowing()) {
-      imageOverlay.show();
-    }*/
+    if (!isVisible()) setVisible(true);
     showAt(position);
   }
 
@@ -115,39 +90,43 @@ class AudioPositionPopup extends DivWidget implements AudioControl  {
    * @param positionInMillis where soundmanager tells us the audio cursor is during playback
    */
   private void showAt(double positionInMillis) {
-   // setHeightFromContainer();
+    Widget parent = imageContainer;//getParent();
+    int offsetHeight = parent.getOffsetHeight();
+    int width = parent.getOffsetWidth();
 
-    int offsetHeight = getParent().getOffsetHeight();
-
+    if (!parent.getElement().getId().equals(imageContainer.getElement().getId())) {
+      System.err.println("parent " + parent.getElement().getId() + " but container " + imageContainer.getElement().getId());
+    }
     //positionInMillis -= (double)ScoringAudioPanel.MP3_HEADER_OFFSET;
     float positionInMillisF = (float)positionInMillis - ScoringAudioPanel.MP3_HEADER_OFFSET*1000;
     float horizontalFraction = positionInMillisF / durationInMillis;
     if (horizontalFraction > 1f) horizontalFraction = 1f;
-    int pixelProgress = (int) (((float) imageContainer.getOffsetWidth()) * horizontalFraction);
-    //int left = imageContainer.getAbsoluteLeft() + pixelProgress;
-    //int top = imageContainer.getAbsoluteTop();
-    //imageOverlay.setPopupPosition(left, top);
+    int pixelProgress = (int) (((float) width) * horizontalFraction);
 
-    getElement().getStyle().setLeft(pixelProgress, Style.Unit.PX);
-   // getElement().getStyle().setTop(0, Style.Unit.PX);
+    getElement().getStyle().setLeft(pixelProgress + parent.getAbsoluteLeft(), Style.Unit.PX);
+    getElement().getStyle().setTop(parent.getAbsoluteTop() + 1, Style.Unit.PX);
     setSize("2px", offsetHeight + "px");
 
-    if (debug) System.out.println(this + " showAt " + positionInMillis + "/" + positionInMillisF + " parent " +getParent().getElement().getId()+
-      " millis " +//imageOverlay.isShowing() +
-      " vis " + isVisible() +
-      // " x " + imageOverlay.getPopupLeft() +
-      // " y " + imageOverlay.getPopupTop() +
-      " dim x " +
-      getOffsetWidth() +
-      " y " + offsetHeight);
+    if (debug) {
+      System.out.println(this + " showAt " + positionInMillis + " millis " + "/" +pixelProgress
+        +" parent " + parent.getElement().getId()+ " width " + width+
+        " millis " +
+        " dim x " +
+        getOffsetWidth() +
+        " y " + offsetHeight);
+    }
   }
 
   public void setHeightFromContainer() {
-    int offsetHeight = imageContainer.getOffsetHeight();
+    Widget parent = imageContainer;//getParent();
+    int offsetHeight = parent.getOffsetHeight();
     System.out.println("Set height to " + offsetHeight);
-    if (offsetHeight > 0)
+    if (offsetHeight > 0) {
       setSize("2px", offsetHeight + "px");
+    }
   }
 
-  public String toString() { return "popup #" +id + " inside " +imageContainer.getElement().getId(); }
+  public String toString() {
+    Widget parent = imageContainer;//getParent();
+    return "popup #" +id + " inside " + parent.getElement().getId(); }
 }
