b4586aab4670cc5713ab518b5c25c475951210f6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-05-06 20:43:01 -0400

    added download link below unit and chapter
    Former-commit-id: 2c2f0ae35630d37279abf1262ce191c5a8f649b2 [formerly 93c5823c133e6c006bca2914db0731feb5a10921] [formerly d614b76832cd4ce22b67edea8d4f87ace51b4171 [formerly 52e18824b0bc91cc576a1103794414cb878c8124]] [formerly 4004a8b4ec7fd6b9bb6e9549d553d30ec7982160 [formerly 2192f16d3781d655e8946cf32da7cf75b6ca5933] [formerly 1aff83a0ce68ae99afec454b6e5b517ecc4381f9 [formerly 721c971ae2f2e51ccb144ea4791edeb4c0426e83]]]
    Former-commit-id: 88b16da3804939a174be25ed41bdca6d4f1d2843 [formerly b9ae054298a3daf9c0ac11b2dc3d0a73718f9f54] [formerly 0459d36d4626b31a29ac25197745181013c8939e [formerly 9eaa220d141846805f9c14838f4de5e291bd2ad0]] [formerly 06266cc554805026f45cef50aa82d6ca5c377e10 [formerly 73d2c0bf192b6cb04d8c35a96365833060b5c4ff] [formerly 49c4fca1fb445e47ecc4a8b571311d4e847160b8 [formerly e6fba82dd4a1deb4643ed3db6fab3530958d1c6f]]]
    Former-commit-id: 6085361665fcaa6a1cc342462f6cbd72eb178511 [formerly 14694aa9d008b80ca69c4a73b700c3c2edd24497] [formerly feff7e94b5e080637ffc96e458cf7c31e2ad29c7 [formerly 04ae83f28d159dadd5fc8961b26291d052413217]]
    Former-commit-id: 8f67fcb722f9230d153871ebfbc03613af9ab91e [formerly 9e5937a3a62b91da035b617b356eac33770ae613]
    Former-commit-id: a9d2e223ce22624709e96686c86570370c1e4974

---
+++ src/mitll/langtest/server/database/ImportCourseExamples.java
@@ -0,0 +1,107 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.shared.Result;
+import mitll.langtest.shared.User;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by GO22670 on 5/6/2014.
+ */
+public class ImportCourseExamples {
+  private static final Logger logger = Logger.getLogger(ImportCourseExamples.class);
+
+  private static DatabaseImpl makeDatabaseImpl(String h2DatabaseFile, String configDir) {
+    ServerProperties serverProps = new ServerProperties(configDir,"quizlet.properties");
+    return new DatabaseImpl(configDir, configDir, h2DatabaseFile, serverProps, null, true);
+  }
+
+  protected static void importCourseExamples() {
+    DatabaseImpl russianCourseExamples = makeDatabaseImpl("russianCourseExamples_04_16", "war/config/russian");
+    ResultDAO resultDAO1 = russianCourseExamples.getResultDAO();
+    System.out.println("got " + resultDAO1.getNumResults());
+    Map<Long, Map<String, Result>> userToResultsRegular = resultDAO1.getUserToResults(true, russianCourseExamples.getUserDAO());
+    System.out.println("got " + userToResultsRegular.size() + " keys " + userToResultsRegular.keySet());
+
+    //  System.out.println("got " + result);
+
+    Map<Long, Map<String, Result>> userToResultsSlow = resultDAO1.getUserToResults(false, russianCourseExamples.getUserDAO());
+    System.out.println("got " + userToResultsSlow.size() + " keys " + userToResultsSlow.keySet());
+
+    // so now we have the latest audio
+    // write to id/regular_or_slow/user_id
+
+    // copy users to real database
+
+    DatabaseImpl npfRussian = makeDatabaseImpl("npfRussian", "war/config/russian");
+    Map<Long, User> userMap = russianCourseExamples.getUserDAO().getUserMap();
+    Map<Long, Long> oldToNew = new HashMap<Long, Long>();
+
+    for (long userid : userToResultsRegular.keySet()) {
+      copyUser(npfRussian, userMap, oldToNew, userid);
+    }
+
+    for (long userid : userToResultsSlow.keySet()) {
+      copyUser(npfRussian, userMap, oldToNew, userid);
+    }
+
+    // so now we have the users in the database
+
+    // add a audio reference to the audio ref table for each recording
+    AudioDAO audioDAO = npfRussian.getAudioDAO();
+    // audioDAO.drop();
+    copyAudio(userToResultsRegular, oldToNew, audioDAO);
+    copyAudio(userToResultsSlow, oldToNew, audioDAO);
+  }
+
+  private static void copyUser(DatabaseImpl npfRussian, Map<Long, User> userMap, Map<Long, Long> oldToNew, long userid) {
+    User user = userMap.get(userid);
+    int i = npfRussian.userExists(user.getUserID());
+    if (i > 0) logger.debug("found duplicate " + user);
+    long l = i != -1 ? i : npfRussian.addUser(user);
+    oldToNew.put(user.getId(), l);
+  }
+
+  /**
+   * TODO : deal with the user ids being the same after toLowerCase
+   * @param userToResultsRegular
+   * @param oldToNew
+   * @param audioDAO
+   */
+  protected static void copyAudio(Map<Long, Map<String, Result>> userToResultsRegular, Map<Long, Long> oldToNew, AudioDAO audioDAO) {
+    int count = 0;
+    for (Map.Entry<Long, Map<String, Result>> userToExIdToResult : userToResultsRegular.entrySet()) {
+      //for (Long userid : userToExIdToResult.getKey())
+      logger.debug("User " +userToExIdToResult.getKey());
+      Map<String, Result> exIdToResult = userToExIdToResult.getValue();
+      logger.debug("num = " + exIdToResult.size() + " exercises->results ");
+      for (Result r : exIdToResult.values()) {
+        if (count %  100 == 0) {
+          logger.debug("\tcount " + count +
+            " result = " + r.uniqueID + " for " + r.getID() + " type " + r.getAudioType() + " path " + r.answer);
+        }
+
+        audioDAO.add(r, oldToNew.get(r.userid).intValue(), "bestAudio/" + r.answer);
+
+        try {
+          File destFile = new File("war/config/russian/bestAudio", r.answer);
+          destFile.getParentFile().mkdirs();
+          FileUtils.copyFile(new File("war/config/russian/candidateAudio", r.answer), destFile);
+          count++;
+          if (count % 100 == 0)  {
+            logger.debug("\tcount " + count + " copied to " + destFile.getAbsolutePath());
+          }
+        } catch (IOException e) {
+          logger.error("got " + e, e);
+        }
+      }
+    }
+    logger.debug("copied " + count + " files.");
+  }
+}
