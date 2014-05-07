bf990d408c0fa9a418b493ee33997234b50de96a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-05-06 20:43:01 -0400

    added download link below unit and chapter
    Former-commit-id: f5e49fe1612a71a861ebb3fb1d86478c2f0d9613 [formerly 0f19f28daf02023d4582df4c74a3c76879010fab] [formerly d44805c7205534de3e8ac96f1a4a976487162a2a [formerly b138f0f60c9a0be2b0d32dd45ce528cde226b3c5]]
    Former-commit-id: 1d213a4deae12d958dba3f03c4e0a41613f83a07 [formerly e2fdedb4cc10165dfbafbe9401350e7232421427]
    Former-commit-id: e75fe2fb941fe72488696e0b5a59621e32858d79

--- src/mitll/langtest/server/DownloadServlet.java
+++ src/mitll/langtest/server/DownloadServlet.java
@@ -4,10 +4,18 @@ import mitll.langtest.server.database.DLIUserDAO;
 import mitll.langtest.server.database.DatabaseImpl;
 import org.apache.log4j.Logger;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,6 +28,17 @@ import java.io.IOException;
 public class DownloadServlet extends DatabaseServlet {
   private static final Logger logger = Logger.getLogger(DownloadServlet.class);
 
+/*  @Override
+  public void init() {
+    //this.pathHelper = new PathHelper(getServletContext());
+
+    String config = servletContext.getInitParameter("config");
+    this.relativeConfigDir = "config" + File.separator + config;
+    this.configDir = pathHelper.getInstallPath() + File.separator + relativeConfigDir;
+
+    readProperties(servletContext);
+    setInstallPath(serverProps.getUseFile(), db);
+  }*/
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String encodedFileName = request.getRequestURI();
@@ -31,7 +50,38 @@ public class DownloadServlet extends DatabaseServlet {
     response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     DatabaseImpl db = readProperties();
 
-    if (encodedFileName.toLowerCase().contains("users")) {
+    setInstallPath(db);
+
+    if (encodedFileName.toLowerCase().contains("audio")) {
+      String queryString = request.getQueryString();
+      if (queryString.length() > 2) {
+        queryString = queryString.substring(1, queryString.length() - 1);
+      }
+      String[] split = queryString.split(",");
+      Map<String, Collection<String>> typeToSection = new HashMap<String, Collection<String>>();
+      for (String section : split) {
+        String[] split1 = section.split("=");
+        String s = split1[1];
+        if (!s.isEmpty()) {
+          s = s.substring(1, s.length() - 1);
+        }
+        String key = split1[0];
+        List<String> value = Arrays.asList(s.split(","));
+       // logger.debug("\tkey " + key + "=" + value);
+        typeToSection.put(key, value);
+      }
+
+      logger.debug("Selection " + typeToSection);
+      String name = typeToSection.isEmpty() ? "audio" : db.getPrefix(typeToSection);
+      response.setHeader("Content-Disposition", "attachment; filename=" + name);
+      response.setContentType("application/zip");
+      try {
+        db.writeZip(response.getOutputStream(), typeToSection);
+      } catch (Exception e) {
+        logger.error("couldn't write zip?",e);
+      }
+    }
+    else if (encodedFileName.toLowerCase().contains("users")) {
       response.setHeader("Content-Disposition", "attachment; filename=users");
       db.getUserDAO().toXLSX(response.getOutputStream(), new DLIUserDAO(db));
     }
@@ -44,5 +94,30 @@ public class DownloadServlet extends DatabaseServlet {
       db.getEventDAO().toXLSX(response.getOutputStream());
     }
     response.getOutputStream().close();
+  }
+
+  private void setInstallPath(DatabaseImpl db) {
+    ServletContext servletContext = getServletContext();
+    String config = servletContext.getInitParameter("config");
+    String relativeConfigDir = "config" + File.separator + config;
+    String installPath = new PathHelper(servletContext).getInstallPath();
+    String configDir = installPath + File.separator + relativeConfigDir;
+
+    setInstallPath(serverProps.getUseFile(), db, installPath, relativeConfigDir,configDir);
+  }
+
+  private String setInstallPath(boolean useFile, DatabaseImpl db,String installPath,String relativeConfigDir,String configDir) {
+    String lessonPlanFile = getLessonPlan(configDir);
+    if (useFile && !new File(lessonPlanFile).exists()) logger.error("couldn't find lesson plan file " + lessonPlanFile);
+
+    //String installPath = pathHelper.getInstallPath();
+    db.setInstallPath(installPath, lessonPlanFile, serverProps.getLanguage(), useFile,
+      relativeConfigDir+File.separator+serverProps.getMediaDir());
+
+    return lessonPlanFile;
+  }
+
+  private String getLessonPlan(String configDir) {
+    return configDir + File.separator + serverProps.getLessonPlan();
   }
 }
