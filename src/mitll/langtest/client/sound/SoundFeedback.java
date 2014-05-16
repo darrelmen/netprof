267ae8e0c61a44f96a1d13e3f9d7d87c52155d5f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-05-15 20:49:27 -0400

    deals with issue where we play f.l. audio and switch chapters quickly, also fixed bug in reco test for correctness of response
    Former-commit-id: 3732fa4430143338ead4bf2e7f27beea7d8c5010 [formerly 6a5cae4c6da5c927c803864745e52b8a8f56cd43]
    Former-commit-id: 2eeafc7693e1adc3b1afb1734da5e2457317ad78

--- src/mitll/langtest/client/sound/SoundFeedback.java
+++ src/mitll/langtest/client/sound/SoundFeedback.java
@@ -12,54 +12,60 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class SoundFeedback {
-  private static final String CORRECT   = "langtest/sounds/correct4.mp3";
-  private static final String INCORRECT = "langtest/sounds/incorrect1.mp3";
+  public static final String CORRECT   = "langtest/sounds/correct4.mp3";
+  public static final String INCORRECT = "langtest/sounds/incorrect1.mp3";
   private static final int SOFT_VOL = 50;
 
-  private final HTML warnNoFlash;
+ // private final HTML warnNoFlash;
   private Sound currentSound = null;
   private final SoundManagerAPI soundManager;
 
-  /**
-   * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#BootstrapExercisePanel(mitll.langtest.shared.CommonExercise, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.exercise.ExerciseController, boolean, mitll.langtest.client.flashcard.ControlState)
-   * @param soundManager
-   * @param warnNoFlash
-   */
-  public SoundFeedback(SoundManagerAPI soundManager,HTML warnNoFlash) {
-    this.soundManager = soundManager;
-    this.warnNoFlash = warnNoFlash;
+  public SoundFeedback(SoundManagerAPI soundManager) {
+    this(soundManager, null);
   }
 
-  public void playCorrect() {
-    startSong(CORRECT, new EndListener() {
-      @Override
-      public void songEnded() {}
-    }, false);
+  /**
+   * @param soundManager
+   * @param warnNoFlash
+   * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#BootstrapExercisePanel(mitll.langtest.shared.CommonExercise, mitll.langtest.client.LangTestDatabaseAsync, mitll.langtest.client.exercise.ExerciseController, boolean, mitll.langtest.client.flashcard.ControlState, SoundFeedback, mitll.langtest.client.sound.SoundFeedback.EndListener)
+   */
+  private SoundFeedback(SoundManagerAPI soundManager, HTML warnNoFlash) {
+    this.soundManager = soundManager;
+ //   this.warnNoFlash = warnNoFlash;
   }
+
+/*
+  public boolean playCorrect(EndListener endListener) {
+    return startSong(CORRECT, endListener, false);
+  }
+*/
 
   /**
    * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#showIncorrectFeedback(mitll.langtest.shared.AudioAnswer, double, boolean)
+   * @param endListener
    */
-  public void playIncorrect() {
-    startSong(INCORRECT, new EndListener() {
-      @Override
-      public void songEnded() {}
-    }, true);
-  }
+/*  public boolean playIncorrect(EndListener endListener) {
+    return startSong(INCORRECT, endListener, true);
+  }*/
 
-
-  private void startSong(String path, EndListener endListener, boolean soft) {
-    // System.out.println("PlayAudioPanel : start song : " + path);
+/*  private boolean startSong(String path, EndListener endListener, boolean soft) {
+   //  System.out.println("SoundFeedback : start song : " + path);
     if (soundManager.isReady()) {
       if (soundManager.isOK()) {
         destroySound();
         createSound(path, endListener, soft);
+        return true;
       } else {
         System.out.println(new Date() + " Sound manager is not OK!.");
-        warnNoFlash.setVisible(true);
+        if (warnNoFlash != null) {  // TODO : remove this...
+          warnNoFlash.setVisible(true);
+        }
+        return false;
       }
+    } else {
+      return false;
     }
-  }
+  }*/
 
   /**
    * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#playRef(String)
@@ -72,29 +78,40 @@ public class SoundFeedback {
      *
      * @param song
      * @param soft
-     * @see #startSong
-     * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#playAllAudio
+     * @seex #startSong
+     * @seex mitll.langtest.client.flashcard.BootstrapExercisePanel#playAllAudio
      * @see mitll.langtest.client.flashcard.BootstrapExercisePanel#playRefAndGoToNext
      */
-  public void createSound(final String song, final EndListener endListener, final boolean soft) {
-    //System.out.println("playing " +song);
+  private void createSound(final String song, final EndListener endListener, final boolean soft) {
+    System.out.println("playing " +song);
     currentSound = new Sound(new AudioControl() {
       @Override
       public void reinitialize() {}
 
       @Override
-      public void songFirstLoaded(double durationEstimate) {}
+      public void songFirstLoaded(double durationEstimate) {
+       // System.out.println("songFirstLoaded " +song);
+
+      }
 
       @Override
       public void songLoaded(double duration) {
+      //  System.out.println("songLoaded " +song);
+
         if (soft) {
           soundManager.setVolume(song, SOFT_VOL);
         }
+        if (endListener != null) {
+          endListener.songStarted();
+        }
+
         soundManager.play(currentSound);
       }
 
       @Override
       public void songFinished() {
+      //  System.out.println("songFinished " +song);
+
         destroySound();
         if (endListener != null) endListener.songEnded();
       }
@@ -113,6 +130,7 @@ public class SoundFeedback {
   }
 
   public static interface EndListener {
+    void songStarted();
     void songEnded();
   }
 }
