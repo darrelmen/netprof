eae5378619dbb0f6d8b0721ac4a8e74837223740
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-05-28 18:32:21 -0400

    partially working with trying flash first then falling back to new AudioContext/getUserMedia calls - doesn't show waveform or do mp3 encoding for some reason
    Former-commit-id: 9084f7dff082064247b2be2614973d5cc968a691

---
+++ src/mitll/langtest/client/recorder/WebAudioRecorder.java
@@ -0,0 +1,96 @@
+package mitll.langtest.client.recorder;
+
+import mitll.langtest.client.WavCallback;
+
+/**
+ * Created by GO22670 on 5/27/2014.
+ */
+public class WebAudioRecorder {
+  private static boolean webAudioMicAvailable;
+  private static boolean micConnected = true;  // TODO how to determine if mic not connected in web audio world?
+  private static boolean tried = false;
+
+  public void tryWebAudio() {
+    if (!tried) {
+      tried = true;
+      System.out.println("webAudioMicAvailable -- tryWebAudio!");
+
+      initWebaudio();
+    }
+  }
+
+  private native void initWebaudio() /*-{
+      $wnd.initWebAudio();
+  }-*/;
+
+  public native void advertise() /*-{
+      $wnd.webAudioMicAvailable = $entry(@mitll.langtest.client.recorder.WebAudioRecorder::webAudioMicAvailable());
+      $wnd.webAudioMicNotAvailable = $entry(@mitll.langtest.client.recorder.WebAudioRecorder::webAudioMicNotAvailable());
+      $wnd.getBase64 = $entry(@mitll.langtest.client.recorder.WebAudioRecorder::getBase64(Ljava/lang/String;));
+  }-*/;
+
+  public void recordOnClick() {
+    System.out.println("webAudioMicAvailable -- recording!");
+
+    if (webAudioMicAvailable) {
+      startRecording();
+    }
+    else {
+      System.err.println("web audio not available for recording...");
+    }
+  }
+
+  public native void startRecording() /*-{
+      $wnd.startRecording();
+  }-*/;
+
+  public native void stopRecording() /*-{
+      return $wnd.stopRecording();
+  }-*/;
+
+  native static void consoleLog( String message) /*-{
+      console.log( "me:" + message );
+  }-*/;
+
+  public boolean isMicConnected() { return micConnected; }
+  public boolean isWebAudioMicAvailable() { return webAudioMicAvailable; }
+
+  public static void webAudioMicAvailable() {
+    webAudioMicAvailable = true;
+
+    System.out.println("webAudioMicAvailable -- connected!");
+    // permissionReceived = true;
+    consoleLog("webAudioMicAvailable -- connected!");
+    FlashRecordPanelHeadless.micPermission.gotPermission();
+  }
+
+  public static void webAudioMicNotAvailable() {
+    System.out.println("webAudioMicNotAvailable!");
+    webAudioMicAvailable = false;
+    FlashRecordPanelHeadless.micPermission.noRecordingMethodAvailable();
+    //   selfRef.rememberInstallFlash();
+  }
+
+  /**
+   * @see #advertise()
+   * @param encoded
+   */
+  public static void getBase64(String encoded) {
+    System.out.println("getBase64 " + encoded.length());
+    if (WebAudioRecorder.wavCallback == null) {
+      System.err.println("getBase64 no callback?");
+    } else {
+      WebAudioRecorder.wavCallback.getBase64EncodedWavFile(encoded);
+      WebAudioRecorder.wavCallback = null;
+    }
+  }
+
+  /**
+   * @see mitll.langtest.client.LangTest#stopRecording(mitll.langtest.client.WavCallback)
+   */
+  static WavCallback wavCallback = null;
+  public void stopRecording(WavCallback wavCallback) {
+    WebAudioRecorder.wavCallback = wavCallback;
+    stopRecording();
+  }
+}
