25f1d8ef8e08474253ddb2d9ab7ae719abbef379
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-05-30 18:26:31 -0400

    made work with IE 9 and 10
    Former-commit-id: efc6eeebd2090957ada4fc51cb236312a9e7ce76 [formerly caba214ab8f3ff753fe16248cb7d38e8642389d0] [formerly e458043d98811ee67e08b92602b483da5e413209 [formerly f683e921ab8daf3d15ec678aefcf341879ef6c79]]
    Former-commit-id: 24f2a5b1d3d1bc3aaa98ee25302efde722acee68 [formerly fa216233a0981d7d81851fce2f2a50779eefcb9c] [formerly a704c46d2d69ab0163211533a35bf78c81144eb2 [formerly f39c8e3da9f0d83fd1e28d0c5bcd3de38ecb2743]]
    Former-commit-id: 1ab4bd315d216d55b37cbc873c445bc3afde13bd [formerly 97004e2e3df7e140be6dfdef79773082f516cd45]
    Former-commit-id: 0cef6565e7dacf1d450f85493c2e42b385e129aa

--- src/mitll/langtest/client/BrowserCheck.java
+++ src/mitll/langtest/client/BrowserCheck.java
@@ -96,6 +96,26 @@ public class BrowserCheck {
       return navigator.userAgent.toLowerCase();
   }-*/;
 
+  public static native int getIEVersion() /*-{
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    if (trident > 0) {
+        // IE 11 (or newer) => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // other browser
+    return -1;
+  }-*/;
+
 /*  private static native String getAppName() *//*-{
       return navigator.appName;
   }-*//*;
