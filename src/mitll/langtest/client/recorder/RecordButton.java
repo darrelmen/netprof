4e1998d1a4a3a76d60f6d35408d7dc41f5a8e507
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-05 15:34:46 -0400

    partial steps toward public/private lists
    Former-commit-id: b36bc3869985659e08658915f52471d13a643aad [formerly d8cdbbc9109b71019e4302f8334ae97901f1683b] [formerly b36bc3869985659e08658915f52471d13a643aad [formerly d8cdbbc9109b71019e4302f8334ae97901f1683b] [formerly c65ec436539a4087e25599e24b2c544475053057 [formerly 5d8c9ebd9910ff663bdb779206c3d5770828590d]]]
    Former-commit-id: ff5e51b2306bb85f6f6910ecc140abb2e233f73f [formerly f642fc64b8186fad0710e5b6be484f84ccf20831] [formerly 41582fd5cb38ebcd0b5bcac96822b06d01335178 [formerly 54fe5c23c636632bcacbe365365e255796ef0954]] [formerly abdb29a4076772dca8d6f97385c4bc6c33556712 [formerly a2e49a4eab5f3d4ab6624f7b74f103b05354b66c] [formerly 999badb10c10afce972bd074e9c29510247282cc [formerly ebdf7e75c9813c1d953e8b1aa9c230c247571eed]]]
    Former-commit-id: abdb29a4076772dca8d6f97385c4bc6c33556712 [formerly a2e49a4eab5f3d4ab6624f7b74f103b05354b66c]
    Former-commit-id: be6e855efadec7a0bef483b99fea96be9aca98bc [formerly 08041bb49999b3d3b946f590660b798346d7610c]
    Former-commit-id: 307796169c3c007933b881125bc14f37439544ef

--- src/mitll/langtest/client/recorder/RecordButton.java
+++ src/mitll/langtest/client/recorder/RecordButton.java
@@ -8,8 +8,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.user.client.Timer;
