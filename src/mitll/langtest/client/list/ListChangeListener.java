5c680c7cc16efdf4e9a3455328d68903c427d673
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-10 19:16:07 -0400

    finished getting shuffle working properly
    Former-commit-id: c7a92424cf6da66725db5059b844f8c9fe62467f [formerly cc582d2f52250c9112da6cae19a39ca1b0cebb25] [formerly c7a92424cf6da66725db5059b844f8c9fe62467f [formerly cc582d2f52250c9112da6cae19a39ca1b0cebb25] [formerly ab270c394da11fb8c742db9184f37f3d90c4a9cf]]
    Former-commit-id: 2e2e015177f4f2b3e7519bfef39afc724f5e62cf [formerly 421df21d7044ab907ee4681f4d8f56b934e94560] [formerly 1340ba677197484b90db61fceb65ba2ad26fd982] [formerly b616b56b23dcb96c6262527a2ff8089c923fd758 [formerly 5ff0a1bdf1d020a2388fba7f84c8e2705a5b153b] [formerly cf5dc67fb2091841c0ac01811872cd6f5350b291]]
    Former-commit-id: b616b56b23dcb96c6262527a2ff8089c923fd758 [formerly 5ff0a1bdf1d020a2388fba7f84c8e2705a5b153b]
    Former-commit-id: 31ead72464e30ebfee150d30bd6cc03f10b6c0cf
    Former-commit-id: 6a00c2a59a7ef759d4d679399bd1facf4ff622e3

--- src/mitll/langtest/client/list/ListChangeListener.java
+++ src/mitll/langtest/client/list/ListChangeListener.java
@@ -6,5 +6,10 @@ import java.util.List;
  * Created by go22670 on 2/11/14.
  */
 public interface ListChangeListener<T> {
+  /**
+   * @see mitll.langtest.client.list.ExerciseList#rememberAndLoadFirst(java.util.List, mitll.langtest.shared.CommonExercise, String)
+   * @param items
+   * @param selectionID
+   */
   public void listChanged(List<T> items, String selectionID);
 }
