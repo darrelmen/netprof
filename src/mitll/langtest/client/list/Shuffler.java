b90c56a7c5747fc635dcb8886ee1b3d354e50a74
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-10 19:16:07 -0400

    finished getting shuffle working properly
    Former-commit-id: c503de49668c52ed61034fa1eef24d1f95fa2789 [formerly 7dd08d6adecc70dc9d5f145e175d4c0bcf643358]
    Former-commit-id: db8485b394db31b9a25888173cc66e499ed62fe2 [formerly ed052f331fce4bfb4396e4d59bacd0695e1f62f0]
    Former-commit-id: 00b00fff46986cf9413120051262604eb8abdbaa

---
+++ src/mitll/langtest/client/list/Shuffler.java
@@ -0,0 +1,74 @@
+package mitll.langtest.client.list;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.RandomAccess;
+
+/**
+ * Created by GO22670 on 6/10/2014.
+ */
+public class Shuffler {
+  private static final int SHUFFLE_THRESHOLD = 5;
+
+  public static void shuffle(List<?> list) {
+    Random rnd = r;
+    if (rnd == null)
+      r = rnd = new Random(); // harmless race.
+    shuffle(list, rnd);
+  }
+
+  private static Random r;
+
+  private static void shuffle(List<?> list, Random rnd) {
+    int size = list.size();
+    if (size < SHUFFLE_THRESHOLD || list instanceof RandomAccess) {
+      for (int i = size; i > 1; i--)
+        swap(list, i - 1, rnd.nextInt(i));
+    } else {
+      Object arr[] = list.toArray();
+
+      // Shuffle array
+      for (int i = size; i > 1; i--)
+        swap(arr, i - 1, rnd.nextInt(i));
+
+      // Dump array back into list
+      ListIterator it = list.listIterator();
+      for (int i = 0; i < arr.length; i++) {
+        it.next();
+        it.set(arr[i]);
+      }
+    }
+  }
+
+  /**
+   * Swaps the two specified elements in the specified array.
+   */
+  private static void swap(Object[] arr, int i, int j) {
+    Object tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  /**
+   * Swaps the elements at the specified positions in the specified list.
+   * (If the specified positions are equal, invoking this method leaves
+   * the list unchanged.)
+   *
+   * @param list The list in which to swap elements.
+   * @param i    the index of one element to be swapped.
+   * @param j    the index of the other element to be swapped.
+   * @throws IndexOutOfBoundsException if either <tt>i</tt> or <tt>j</tt>
+   *                                   is out of range (i &lt; 0 || i &gt;= list.size()
+   *                                   || j &lt; 0 || j &gt;= list.size()).
+   * @since 1.4
+   */
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static void swap(List<?> list, int i, int j) {
+    // instead of using a raw type here, it's possible to capture
+    // the wildcard but it will require a call to a supplementary
+    // private method
+    final List l = list;
+    l.set(i, l.set(j, l.get(i)));
+  }
+}
