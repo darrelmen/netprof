d80e28f822cc3aef7948886901c01339856da573
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-10 19:16:07 -0400

    finished getting shuffle working properly
    Former-commit-id: ab494c3c92c1a24ceefa297cdf34abcf5b3e06d0 [formerly 532aa97340748e5b6a4c2b8699348ce5a96aef3e] [formerly 0563cf36285eb7aaa6c070b47a8b7a02bc895209 [formerly 24cd1e5f88419147e0871a131b48bd298b4ed8bd]] [formerly ab494c3c92c1a24ceefa297cdf34abcf5b3e06d0 [formerly 532aa97340748e5b6a4c2b8699348ce5a96aef3e] [formerly 0563cf36285eb7aaa6c070b47a8b7a02bc895209 [formerly 24cd1e5f88419147e0871a131b48bd298b4ed8bd]] [formerly ab494c3c92c1a24ceefa297cdf34abcf5b3e06d0 [formerly 532aa97340748e5b6a4c2b8699348ce5a96aef3e] [formerly 0563cf36285eb7aaa6c070b47a8b7a02bc895209 [formerly 24cd1e5f88419147e0871a131b48bd298b4ed8bd]] [formerly b4b35076db82d8f5f2e39fbd71cf4f7bd819f71c]]]
    Former-commit-id: 5b5628858ed341abf3c5b7c4586725aac1f8ddd5 [formerly 413c22357659f60ebd1ade1584d9c0343d530695] [formerly f21c7e512ee03d39f26a4c23014780c444f47f3a [formerly 2683c39f79dc1f0918970e52ce47e8da5f32af00]] [formerly 5b5628858ed341abf3c5b7c4586725aac1f8ddd5 [formerly 413c22357659f60ebd1ade1584d9c0343d530695] [formerly f21c7e512ee03d39f26a4c23014780c444f47f3a [formerly 2683c39f79dc1f0918970e52ce47e8da5f32af00]] [formerly 00e93d9dcb0db17735e27b5d117911825471f50e]] [formerly f18cdfab566cb7626f55ad0f82d853c5d5e5ba9e [formerly 1368363c6d38f056bb73c9487c4745f0734c1baf] [formerly d294539aafff9946715ab8822c050ec93fad9ac0 [formerly 72577e992f177f0f176c75aa42aaef5bc077149f]] [formerly f18cdfab566cb7626f55ad0f82d853c5d5e5ba9e [formerly 1368363c6d38f056bb73c9487c4745f0734c1baf] [formerly d294539aafff9946715ab8822c050ec93fad9ac0 [formerly 72577e992f177f0f176c75aa42aaef5bc077149f]] [formerly 8c8d75dbda8738924b525940ee717bc97f0807e2]]]
    Former-commit-id: f18cdfab566cb7626f55ad0f82d853c5d5e5ba9e [formerly 1368363c6d38f056bb73c9487c4745f0734c1baf] [formerly d294539aafff9946715ab8822c050ec93fad9ac0 [formerly 72577e992f177f0f176c75aa42aaef5bc077149f]]
    Former-commit-id: e2adc395504d5ea2dedefccbacba7a0a1d32385f [formerly f2ea70dc35d0f7a60372914a73cdccde30fb7532] [formerly 5f69cd32546a8b27d2db5f1fbb26a3337ee64ce3 [formerly 266e77a16549791ac2b4b087a627b371ed7c856b]] [formerly 4bf738b8daba1797d5d7f99fdf2a29db6a57d3ee]
    Former-commit-id: a9f16d40c86720077d52b6fd74d524eef1dba31f
    Former-commit-id: a9ce9bd408e5b7a697167471b9623a19b9b3058b [formerly 4a43ec754542bc9d22b8a1855f6b266c418cc165]
    Former-commit-id: 4820779d45ffad6d7da72edccf8059fd3b788412

--- src/mitll/langtest/client/list/ExerciseList.java
+++ src/mitll/langtest/client/list/ExerciseList.java
@@ -878,8 +878,7 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
     loadFirstExercise();
   }
 
-  public void redraw() {
-  }
+  public void redraw() {}
 
   /**
    * @param listener
@@ -895,76 +894,15 @@ public abstract class ExerciseList extends VerticalPanel implements ListInterfac
 
   @Override
   public void setShuffle(boolean doShuffle) {
-    if (doShuffle != this.doShuffle) {
-      rememberAndLoadFirst(getInOrder());
-    }
     simpleSetShuffle(doShuffle);
+    rememberAndLoadFirst(getInOrder());
   }
 
+  /**
+   * @see mitll.langtest.client.custom.MyFlashcardExercisePanelFactory.StatsPracticePanel#StatsPracticePanel(mitll.langtest.shared.CommonExercise)
+   * @param doShuffle
+   */
   public void simpleSetShuffle(boolean doShuffle) {
     this.doShuffle = doShuffle;
-  }
-
-  private static final int SHUFFLE_THRESHOLD = 5;
-
-  public static void shuffle(List<?> list) {
-    Random rnd = r;
-    if (rnd == null)
-      r = rnd = new Random(); // harmless race.
-    shuffle(list, rnd);
-  }
-
-  private static Random r;
-
-  private static void shuffle(List<?> list, Random rnd) {
-    int size = list.size();
-    if (size < SHUFFLE_THRESHOLD || list instanceof RandomAccess) {
-      for (int i = size; i > 1; i--)
-        swap(list, i - 1, rnd.nextInt(i));
-    } else {
-      Object arr[] = list.toArray();
-
-      // Shuffle array
-      for (int i = size; i > 1; i--)
-        swap(arr, i - 1, rnd.nextInt(i));
-
-      // Dump array back into list
-      ListIterator it = list.listIterator();
-      for (int i = 0; i < arr.length; i++) {
-        it.next();
-        it.set(arr[i]);
-      }
-    }
-  }
-
-  /**
-   * Swaps the two specified elements in the specified array.
-   */
-  private static void swap(Object[] arr, int i, int j) {
-    Object tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-  }
-
-  /**
-   * Swaps the elements at the specified positions in the specified list.
-   * (If the specified positions are equal, invoking this method leaves
-   * the list unchanged.)
-   *
-   * @param list The list in which to swap elements.
-   * @param i    the index of one element to be swapped.
-   * @param j    the index of the other element to be swapped.
-   * @throws IndexOutOfBoundsException if either <tt>i</tt> or <tt>j</tt>
-   *                                   is out of range (i &lt; 0 || i &gt;= list.size()
-   *                                   || j &lt; 0 || j &gt;= list.size()).
-   * @since 1.4
-   */
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  private static void swap(List<?> list, int i, int j) {
-    // instead of using a raw type here, it's possible to capture
-    // the wildcard but it will require a call to a supplementary
-    // private method
-    final List l = list;
-    l.set(i, l.set(j, l.get(i)));
   }
 }
