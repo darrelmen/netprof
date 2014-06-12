d0f139b11db389059ca638fe94d94d1ee75cf7bb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-12 15:39:04 -0400

    event logging on history audio click, changed list font to be just sans-serif

--- src/mitll/langtest/client/instrumentation/EventLogger.java
+++ src/mitll/langtest/client/instrumentation/EventLogger.java
@@ -2,6 +2,7 @@ package mitll.langtest.client.instrumentation;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Tab;
+import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.ui.UIObject;
 import mitll.langtest.client.exercise.ExerciseController;
 
@@ -12,6 +13,8 @@ public interface EventLogger {
   void register(ExerciseController controller, Button button, String exid);
 
   void registerButton(Button button, String exid, String context, long userid);
+
+  void registerWidget(HasClickHandlers clickable, UIObject uiObject, String exid, String context, long userid);
 
   void logEvent(UIObject button, String widgetType, String exid, String context, long userid);
 
