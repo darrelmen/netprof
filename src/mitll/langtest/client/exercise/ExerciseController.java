d3331f1fcbbcadbb908fdcc71dd16a9651270f4e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-12 15:39:04 -0400

    event logging on history audio click, changed list font to be just sans-serif
    Former-commit-id: 74605f786fabb6ba6a0f1b0185042f8b32844310 [formerly 271c85bd7a4c0197758839b1ce0b97f5e0a77f66] [formerly 74605f786fabb6ba6a0f1b0185042f8b32844310 [formerly 271c85bd7a4c0197758839b1ce0b97f5e0a77f66] [formerly 74605f786fabb6ba6a0f1b0185042f8b32844310 [formerly 271c85bd7a4c0197758839b1ce0b97f5e0a77f66] [formerly ffb2b87c2be52ae36e81435a5eb0a737b520cd0a]]]
    Former-commit-id: defdb733edb2e0c2b6366391efea9a07cbd56702 [formerly ffa65fff47910a25301c94f1bbd80520035bf5a3] [formerly defdb733edb2e0c2b6366391efea9a07cbd56702 [formerly ffa65fff47910a25301c94f1bbd80520035bf5a3] [formerly 85e33b3ee386eb12d8336b61b8d64dc7f81667d3]] [formerly ec5b538eee8ed6ebb1be019582b708ac6a8b70b1 [formerly 6a60b8c198b6e72677bb279f26d49ab9a795428c] [formerly ec5b538eee8ed6ebb1be019582b708ac6a8b70b1 [formerly 6a60b8c198b6e72677bb279f26d49ab9a795428c] [formerly ebf225cc1ea3d70cf91537ac8a9a579dc7bc19b6]]]
    Former-commit-id: 4220fe0658b5db05e6de54e7a7bef95061b60633 [formerly ec5b538eee8ed6ebb1be019582b708ac6a8b70b1 [formerly 6a60b8c198b6e72677bb279f26d49ab9a795428c]]
    Former-commit-id: 342946ebc1cf85a1a46b1e65ca2cac6568144bcd [formerly dab3c97fcdd2bcdd384ef3d9d6616222d49fcd29] [formerly a0b8b1197f2123287428438741ce227c17a678ff]
    Former-commit-id: e3f82378524b332606855f92321fdbbbc8bc5201
    Former-commit-id: c1d85393ba54e0a52c5ae1cac83964376a9aa044

--- src/mitll/langtest/client/exercise/ExerciseController.java
+++ src/mitll/langtest/client/exercise/ExerciseController.java
@@ -2,6 +2,7 @@ package mitll.langtest.client.exercise;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Tab;
+import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.UIObject;
 import mitll.langtest.client.LangTestDatabaseAsync;
@@ -80,6 +81,9 @@ public interface ExerciseController {
   void register(Button button, String exid, String context);
 
   void logEvent(Tab button, String widgetType, String exid, String context);
+
+  void registerWidget(HasClickHandlers clickable, UIObject uiObject, String exid, String context);
+
   void logEvent(UIObject button, String widgetType, String exid, String context);
 
   void addKeyListener(KeyPressHelper.KeyListener listener);
