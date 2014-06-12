32ede798b3da97d574ca0b7061b827e763a702ee
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-06-12 15:39:04 -0400

    event logging on history audio click, changed list font to be just sans-serif
    Former-commit-id: 54ab9007bb3735ae0d0b615007c33e7ca89715d7 [formerly abb7dc1d7f98055171d71761735a3a5870a3468c] [formerly 20f095e2f60ce0eb4e28a47a564f01810a0496e3 [formerly 0807886e778b4213658dd0a8ff85c42cff2bb175]]
    Former-commit-id: b34b442cf53334b373576381fe61645744dfca0b [formerly 76654fa69b130c6ac324a02bb3f7bae3bffd777e]
    Former-commit-id: 0e30b9fbee3b48944d4c25add80cad680d8de653

--- src/mitll/langtest/client/instrumentation/EventMock.java
+++ src/mitll/langtest/client/instrumentation/EventMock.java
@@ -2,6 +2,7 @@ package mitll.langtest.client.instrumentation;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.Tab;
+import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.ui.UIObject;
 import mitll.langtest.client.exercise.ExerciseController;
 
@@ -13,11 +14,11 @@ public class EventMock implements EventLogger {
   public void register(ExerciseController controller, Button button, String exid) {
 
   }
-/*
-  @Override
-  public void registerButton(Button button, String exid, long userid) {
 
-  }*/
+  @Override
+  public void registerWidget(HasClickHandlers clickable, UIObject uiObject, String exid, String context, long userid) {
+
+  }
 
   @Override
   public void registerButton(Button button, String exid, String context, long userid) {
