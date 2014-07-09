87bba4dcee3dc6fdcbfcc7cb24b16bd3c678a49c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2014-07-08 20:09:23 -0400

    got logic working where it comes to end of list and then goes back to the start
    Former-commit-id: 00f244e4d3cbfcf2326e21139239a9a7f13f6c15 [formerly 00ac274d91c2f70aef177384bdc73c41976e6628] [formerly 00f244e4d3cbfcf2326e21139239a9a7f13f6c15 [formerly 00ac274d91c2f70aef177384bdc73c41976e6628] [formerly c4b0bdaece769d04e0cf6f25d841f7cc6482b486 [formerly c3e3bf54400598fedee4959de5925a05c737c866]]]
    Former-commit-id: 64ee436bd0bfc5c11acf35463d7949be828ab81f [formerly 06b49ce3ebf3c020cc7553e9ebe47b5d38135c10] [formerly 30f655102507c16e1e462ca045955189ada8edd1 [formerly 854b4d8edcabd82b7aac04709cd0cdf0becd3a81]] [formerly 36973f13c05227d4d76f5ad640adb547a8b7d098 [formerly 7fcc115991b35dbb231557e44ed08d171b0fc9c8] [formerly b1bf0ef090c3c5b8f7952db907e68a953ff07efb [formerly 962130445ad6b26d537aed1ca83aa619c15acda0]]]
    Former-commit-id: 36973f13c05227d4d76f5ad640adb547a8b7d098 [formerly 7fcc115991b35dbb231557e44ed08d171b0fc9c8]
    Former-commit-id: 993a1498ba3f2fe3947a637d134c1653c09dc6a7 [formerly 8a375110d565aa38ce4873b467a5277a1c474a2d]
    Former-commit-id: 408dd8b903e7882881b632e44469add740caac70

--- src/mitll/langtest/client/custom/AVPHelper.java
+++ src/mitll/langtest/client/custom/AVPHelper.java
@@ -4,6 +4,7 @@ import com.github.gwtbootstrap.client.ui.event.HiddenEvent;
 import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.dialog.ModalInfoDialog;
 import mitll.langtest.client.exercise.ExerciseController;
@@ -12,6 +13,7 @@ import mitll.langtest.client.exercise.PagingContainer;
 import mitll.langtest.client.list.PagingExerciseList;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.client.user.UserManager;
+import mitll.langtest.shared.custom.UserList;
 
 /**
 * Created with IntelliJ IDEA.
@@ -21,9 +23,6 @@ import mitll.langtest.client.user.UserManager;
 * To change this template use File | Settings | File Templates.
 */
 class AVPHelper extends NPFHelper {
-  private static final String COMPLETE = "Complete";
-  private static final String LIST_COMPLETE = "List complete!";
-
   /**
    * @see Navigation#Navigation
    * @param service
@@ -35,6 +34,16 @@ class AVPHelper extends NPFHelper {
     super(service, feedback, userManager, controller, false);
   }
 
+  @Override
+  protected Panel getRightSideContent(UserList ul, String instanceName) {
+    Panel npfContentPanel = new SimplePanel();
+    npfContentPanel.getElement().setId("AVPHelper_internalLayout_RightSideContent");
+
+    npfExerciseList = makeNPFExerciseList(npfContentPanel, instanceName + "_"+ul.getUniqueID());
+    return npfContentPanel;
+  }
+
+
   /**
    * @see mitll.langtest.client.custom.NPFHelper#makeNPFExerciseList(com.google.gwt.user.client.ui.Panel, String)
    * @param right
@@ -43,14 +52,16 @@ class AVPHelper extends NPFHelper {
    */
   @Override
   protected PagingExerciseList makeExerciseList(final Panel right, final String instanceName) {
-    ExercisePanelFactory factory = getFactory(null, instanceName, true);
+    final ExercisePanelFactory factory = getFactory(null, instanceName, true);
     PagingExerciseList complete = new PagingExerciseList(right, service, feedback, factory, controller,
       true, instanceName) {
       @Override
       protected void onLastItem() {
+        ((MyFlashcardExercisePanelFactory)factory).resetStorage();
         new ModalInfoDialog(COMPLETE, LIST_COMPLETE, new HiddenHandler() {
           @Override
           public void onHidden(HiddenEvent hiddenEvent) {
+            ((MyFlashcardExercisePanelFactory)factory).resetStorage();
             reloadExercises();
           }
         });
@@ -62,8 +73,7 @@ class AVPHelper extends NPFHelper {
       }
 
       @Override
-      protected void addMinWidthStyle(Panel leftColumn) {
-      }
+      protected void addMinWidthStyle(Panel leftColumn) {}
     };
     return complete;
   }
@@ -72,19 +82,15 @@ class AVPHelper extends NPFHelper {
   protected ExercisePanelFactory getFactory(PagingExerciseList exerciseList, final String instanceName, boolean showQC) {
     return new MyFlashcardExercisePanelFactory(service, feedback, controller, exerciseList, "AVPHelper");
   }
-
+/*
   @Override
   protected Panel setupContent(Panel hp) {
     Panel widgets = super.setupContent(hp);
     float v = Window.getClientWidth() * 0.5f;
     widgets.setWidth(v + "px");
     return widgets;
-  }
+  }*/
 
   @Override
-  public void onResize() {
-    if (getNpfContentPanel() != null) {
-      getNpfContentPanel().setWidth(((Window.getClientWidth() * 0.6f) - 100) + "px");
-    }
-  }
+  public void onResize() {}
 }
