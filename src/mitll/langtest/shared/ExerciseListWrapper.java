f9861d00706eedacee4befd04337a60eae95c9b6
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-08-11 11:47:30 -0400

    fixed bug where wouldn't let you choose an exercise with exercise_title argument
    Former-commit-id: 6cb036dde38a31aa808021e30df384c5884f5b41 [formerly 44ef28f147344986a1878703d41b8c8c7bd2c00f] [formerly 459c9b0f5bd034c7dd6ab6e1af6f4c3cfdf78255]
    Former-commit-id: 809b394c1e7ec31c28362e1d8f207efb2a5aa046 [formerly f6092d99a0fff4c3353af3baca51db175e9bbd30] [formerly a7f5b50b896b117458967d6886fbbe3cd6ed3942]
    Former-commit-id: e0458930d7fdb47631f3cef7239b8b2e98b3a0fc
    Former-commit-id: 4c7abb2edcbe8d6920d2b098d7b43dbe7a087385

--- src/mitll/langtest/shared/ExerciseListWrapper.java
+++ src/mitll/langtest/shared/ExerciseListWrapper.java
@@ -5,21 +5,25 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
+ * Includes a reqID so if the client gets responses out of order it can ignore responses to old requests.
+ * Also includes the first exercise.
+ * The list of exercises here is just a list of {@link mitll.langtest.shared.CommonShell} objects to
+ * reduce the bytes sent to get the exercise list. Perhaps in the future we could move to an async list.
+ * <p/>
  * User: GO22670
  * Date: 6/12/13
  * Time: 4:19 PM
  * To change this template use File | Settings | File Templates.
  */
 public class ExerciseListWrapper implements IsSerializable {
-  public ExerciseListWrapper(){}
+  public ExerciseListWrapper() {} // req for serialization
 
   private int reqID;
   private List<CommonShell> exercises;
   private CommonExercise firstExercise;
 
   /**
-   * @see mitll.langtest.server.LangTestDatabaseImpl#makeExerciseListWrapper(int, java.util.Collection)
+   * @see mitll.langtest.server.LangTestDatabaseImpl#makeExerciseListWrapper
    * @param reqID
    * @param ids
    * @param firstExercise
@@ -33,12 +37,10 @@ public class ExerciseListWrapper implements IsSerializable {
   public int getReqID() {
     return reqID;
   }
-
   public List<CommonShell> getExercises() {
     return exercises;
   }
+  public CommonExercise getFirstExercise() { return firstExercise;  }
 
-  public CommonExercise getFirstExercise() {
-    return firstExercise;
-  }
+  public String toString() { return "req " + reqID + " has " + exercises.size() + " exercises, first is " + firstExercise.getID(); }
 }
