61b6cbbc0ef00621a21bff9dcf0202458cdc1e80
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-08-12 20:34:07 -0400

    got to the point of adding a new user and logging in as that user
    Former-commit-id: 911786e942f3b1d7cf298fb4f8bb44f39d9e3e7a
    Former-commit-id: efa6a935de380973da27a62ed5bca804fa8eff34 [formerly 11bef333a5a1c12106c9ac721cca59437f8edc6b]
    Former-commit-id: d64d2d0b940330af9aebafd336b9c0be12c036ad

--- src/mitll/langtest/client/user/UserPassLogin.java
+++ src/mitll/langtest/client/user/UserPassLogin.java
@@ -1,11 +1,9 @@
 package mitll.langtest.client.user;
 
-import com.gargoylesoftware.htmlunit.javascript.host.Element;
 import com.github.gwtbootstrap.client.ui.*;
-import com.github.gwtbootstrap.client.ui.Button;
-import com.github.gwtbootstrap.client.ui.base.AbstractTypography;
 import com.github.gwtbootstrap.client.ui.base.DivWidget;
 import com.github.gwtbootstrap.client.ui.constants.ButtonType;
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Node;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -13,145 +11,117 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.util.tools.shared.Md5Utils;
-import com.google.gwt.util.tools.shared.StringUtils;
-import com.ibm.icu.impl.CalendarAstronomer;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.PropertyHandler;
 import mitll.langtest.client.dialog.KeyPressHelper;
 import mitll.langtest.shared.Result;
 import mitll.langtest.shared.User;
 
-import java.awt.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Created by go22670 on 8/11/14.
  */
 public class UserPassLogin extends UserDialog {
-  public static final int MIN_LENGTH_USER_ID = 8;
+  public static final int MIN_LENGTH_USER_ID = 6;
   private static final String STUDENT = "Student";
   private static final String TEACHER = "Teacher";
 
   public static final int MIN_PASSWORD = 6;
   private final UserManager userManager;
-  final KeyPressHelper enterKeyButtonHelper = new KeyPressHelper(true);
+  private final KeyPressHelper enterKeyButtonHelper;
+  private FormField user;
   private FormField password;
 
   public UserPassLogin(LangTestDatabaseAsync service, PropertyHandler props,UserManager userManager) {
     super(service,props);
     this.userManager = userManager;
 
-    //getContent();
-
+     enterKeyButtonHelper = new KeyPressHelper(true) {
+      @Override
+      public void userHitEnterKey(Button button) {
+        if (user.box.getValue().isEmpty() || password.box.getValue().isEmpty()) {
+          signUp.fireEvent(new KeyPressHelper.ButtonClickEvent());
+        }
+        else {
+          button.fireEvent(new ButtonClickEvent());
+        }
+      }
+    };
   }
 
   public Panel getContent() {
-   // Panel leftAndRight = new HorizontalPanel();
-    //Panel container = new FluidContainer();
+
     Panel container = new DivWidget();
     DivWidget child = new DivWidget();
     container.add(child);
     child.addStyleName("loginPageBack");
-   // container.setWidth("838px");
-   // container.addStyleName("loginPage");
-    //container.getElement().getStyle().setPaddingLeft(0, Style.Unit.PX);
-   // container.getElement().getStyle().setPaddingRight(0, Style.Unit.PX);
-  //  Panel leftAndRight = new FluidRow();
+
     Panel leftAndRight = new DivWidget();
 
     leftAndRight.addStyleName("loginPage");
 
-    // leftAndRight.getElement().getStyle().setPaddingLeft(5, Style.Unit.PX);
-   // leftAndRight.getElement().getStyle().setPaddingRight(5, Style.Unit.PX);
     container.add(leftAndRight);
 
-   // leftAndRight.getElement().getStyle().setBackgroundColor("lightBlue");
-     DivWidget left = new DivWidget();
-    left.addStyleName("floatLeft");
-    left.setWidth(494 + "px");
-    //Column left = new Column(6);
-    leftAndRight.add(left);
-   // left.addStyleName("floatLeft");
-    int size = 2;
-    int subSize = size+1;
-    left.add(new Heading(size,"Learn how to pronounce words and practice vocabulary."));
-    Heading w1 = new Heading(subSize, "Do flashcards to learn or review vocabulary", "Speak your answers. Compete with your friends.");
-    left.add(w1);
+    getLeftIntro(leftAndRight);
 
-    Node child2 = w1.getElement().getChild(1);
+    getRightLogin(leftAndRight);
+    return container;
+  }
 
-    com.google.gwt.dom.client.Element as = com.google.gwt.dom.client.Element.as(child2);
-    //as.getStyle().setPaddingLeft(2, Style.Unit.PX);
-    as.getStyle().setFontSize(16, Style.Unit.PX);
-    Heading w = new Heading(subSize, "Get feedback on your pronunciation", "Compare yourself to a native speaker.");
-    left.add(w);
-    child2 = w.getElement().getChild(1);
-    as = com.google.gwt.dom.client.Element.as(child2);
-    as.getStyle().setPaddingLeft(2, Style.Unit.PX);
-    as.getStyle().setFontSize(16, Style.Unit.PX);
-  //  AbstractTypography.asWidgetOrNull(child2);
-   // Heading.Small.asWidgetOrNull(child2);
-    left.add(new Heading(subSize, "Make your own lists of words to study later or to share."));
-
-
+  public void getRightLogin(Panel leftAndRight) {
     DivWidget right = new DivWidget();
 
     leftAndRight.add(right);
     right.addStyleName("floatRight");
-    //FormPanel w = new FormPanel();
     DivWidget rightDiv = new DivWidget();
 
     Form form = new Form();
     rightDiv.add(form);
+
+    rightDiv.add(getSignUpForm());
+
     form.addStyleName("topMargin");
     form.addStyleName("formRounded");
     form.getElement().getStyle().setBackgroundColor("white");
 
-    //form.addStyleName("form-horizontal");
-
     Fieldset fieldset = new Fieldset();
     form.add(fieldset);
-    //right.add(new Heading(size,));
     right.add(rightDiv);
 
-   // final ListBoxFormField purpose = getListBoxFormField(fieldset, ARE_YOU_A, getListBox2(ROLES));
-  //  purpose.box.setWidth("150px");
-   // purpose.box.addStyleName("floatRight");
+    // final ListBoxFormField purpose = getListBoxFormField(fieldset, ARE_YOU_A, getListBox2(ROLES));
+    //  purpose.box.setWidth("150px");
+    // purpose.box.addStyleName("floatRight");
 
-    final FormField user = addControlFormField(fieldset, false, MIN_LENGTH_USER_ID,USER_ID_MAX_LENGTH,"Username or Email");
+    user = addControlFormField(fieldset, false, MIN_LENGTH_USER_ID,USER_ID_MAX_LENGTH,"Username or Email");
     user.box.addStyleName("topMargin");
     user.box.addStyleName("rightFiveMargin");
-  //  user.box.addStyleName("floatRight");
 
-    //user.setVisible(isDataCollection(purpose) || isPractice(purpose));
-    //addTooltip(user.box);
     user.box.setFocus(true);
     user.box.setWidth("266px");
 
-     password = addControlFormField(fieldset, true, MIN_PASSWORD, 15, "Password");
+    password = addControlFormField(fieldset, true, MIN_PASSWORD, 15, "Password");
     Panel hp = new HorizontalPanel();
     hp.add(password.box);
     hp.addStyleName("leftFiveMargin");
-    //password.box.setWidth("");
-
 
     Button signIn = new Button("Sign In");
     hp.add(signIn);
     signIn.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
-        if (user.box.getValue().length() < MIN_LENGTH_USER_ID) {
-          markError(user, "Please enter either a user id or email.");
-        } else if (password.box.getValue().length() < MIN_PASSWORD) {
-          markError(user, "Please enter user password.");
-        } else {
-          gotLogin(user.box.getValue(), password.box.getValue());
+        if (user.box.getValue().isEmpty() || password.box.getValue().isEmpty()) {
+          signUp.fireEvent(new KeyPressHelper.ButtonClickEvent());
+        }
+        else {
+          if (user.box.getValue().length() < MIN_LENGTH_USER_ID) {
+            markError(user, "Please enter either a user id or email.");
+          } else if (password.box.getValue().length() < MIN_PASSWORD) {
+            markError(user, "Please enter user password.");
+          } else {
+            gotLogin(user.box.getValue(), password.box.getValue());
+          }
         }
       }
     });
@@ -162,7 +132,113 @@ public class UserPassLogin extends UserDialog {
 
     signIn.setType(ButtonType.PRIMARY);
     fieldset.add(hp);
-    return container;
+
+    getFocusOnUserID(user);
+  }
+
+  protected void getFocusOnUserID(final FormField user) {
+    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+      public void execute() {
+        user.box.setFocus(true);
+      }
+    });
+  }
+
+  Button signUp;
+  public Form getSignUpForm() {
+    Form form = new Form();
+    form.addStyleName("topMargin");
+    form.addStyleName("formRounded");
+    form.getElement().getStyle().setBackgroundColor("white");
+
+    Fieldset fieldset = new Fieldset();
+    Heading w = new Heading(3, "Sign Up", "New to Classroom?");
+    w.addStyleName("signUp");
+    form.add(w);
+    form.add(fieldset);
+
+    // final ListBoxFormField purpose = getListBoxFormField(fieldset, ARE_YOU_A, getListBox2(ROLES));
+    //  purpose.box.setWidth("150px");
+    // purpose.box.addStyleName("floatRight");
+
+    final FormField user = addControlFormField(fieldset, false, MIN_LENGTH_USER_ID,20,"Username");
+    user.box.addStyleName("topMargin");
+    user.box.addStyleName("rightFiveMargin");
+    user.box.setWidth("266px");
+    user.box.getElement().getStyle().setMarginBottom(0, Style.Unit.PX);
+
+    final FormField email = addControlFormField(fieldset, false, MIN_LENGTH_USER_ID,USER_ID_MAX_LENGTH,"Email");
+    email.box.addStyleName("topMargin");
+    email.box.addStyleName("rightFiveMargin");
+    email.box.setWidth("266px");
+
+    final FormField password = addControlFormField(fieldset, true, MIN_PASSWORD, 15, "Password");
+    //Panel hp = new HorizontalPanel();
+   // hp.add(password.box);
+   // hp.addStyleName("leftFiveMargin");
+
+    signUp = new Button("Sign Up");
+    fieldset.add(signUp);
+
+    signUp.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        if (user.box.getValue().length() < MIN_LENGTH_USER_ID) {
+          markError(user, "Please enter either a user id or email.");
+        } else if (password.box.getValue().length() < MIN_PASSWORD) {
+          markError(user, "Please enter user password.");
+        } else {
+          gotSignUp(user.box.getValue(), password.box.getValue(), email.box.getValue());
+        }
+      }
+    });
+    signUp.addStyleName("floatRight");
+    signUp.addStyleName("rightFiveMargin");
+    signUp.addStyleName("leftFiveMargin");
+
+    signUp.setType(ButtonType.SUCCESS);
+    return form;
+  }
+
+  private void gotSignUp(String user, String password, String email) {
+    String passH = toHexString(getMd5Digest(password.getBytes()));
+    String emailH = toHexString(getMd5Digest(email.getBytes()));
+
+    service.addUser(user,passH,emailH,User.Kind.STUDENT,new AsyncCallback<User>() {
+      @Override
+      public void onFailure(Throwable caught) {
+
+      }
+
+      @Override
+      public void onSuccess(User result) {
+        storeUser(result);
+      }
+    });
+  }
+
+  public void getLeftIntro(Panel leftAndRight) {
+    DivWidget left = new DivWidget();
+    left.addStyleName("floatLeft");
+    left.setWidth(494 + "px");
+    leftAndRight.add(left);
+    int size = 2;
+    int subSize = size+1;
+    left.add(new Heading(size,"Learn how to pronounce words and practice vocabulary."));
+    Heading w1 = new Heading(subSize, "Do flashcards to learn or review vocabulary", "Speak your answers. Compete with your friends.");
+    left.add(w1);
+
+    Node child2 = w1.getElement().getChild(1);
+
+    com.google.gwt.dom.client.Element as = com.google.gwt.dom.client.Element.as(child2);
+    as.getStyle().setFontSize(16, Style.Unit.PX);
+    Heading w = new Heading(subSize, "Get feedback on your pronunciation", "Compare yourself to a native speaker.");
+    left.add(w);
+    child2 = w.getElement().getChild(1);
+    as = com.google.gwt.dom.client.Element.as(child2);
+    as.getStyle().setPaddingLeft(2, Style.Unit.PX);
+    as.getStyle().setFontSize(16, Style.Unit.PX);
+    left.add(new Heading(subSize, "Make your own lists of words to study later or to share."));
   }
 
 
@@ -201,7 +277,7 @@ public class UserPassLogin extends UserDialog {
 
     String hashed = toHexString(getMd5Digest(pass.getBytes()));
 
-    System.out.println("hash is " +hashed);
+   // System.out.println("hash is " +hashed);
 
     service.userExists(user, hashed, new AsyncCallback<User>() {
       @Override
@@ -213,14 +289,18 @@ public class UserPassLogin extends UserDialog {
       public void onSuccess(User result) {
         if (result == null) {
           System.out.println("No user with that name?");
-          markError(password,"User-password combination not found.");
+          markError(password, "User-password combination not found.");
         }
         else {
-          enterKeyButtonHelper.removeKeyHandler();
-          userManager.storeUser(result, getAudioTypeFromPurpose(result.getUserKind()));
+          storeUser(result);
         }
       }
     });
+  }
+
+  public void storeUser(User result) {
+    enterKeyButtonHelper.removeKeyHandler();
+    userManager.storeUser(result, getAudioTypeFromPurpose(result.getUserKind()));
   }
 
   private String getAudioTypeFromPurpose(User.Kind kind) {
