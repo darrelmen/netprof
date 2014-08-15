b8f5004bad8fb6928913d1feda78245d0a864ada
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-08-15 17:25:54 -0400

    added options to support selenium testing -- clickAndHold property that determines record button behavior, and quietAudioOK which lets you record an audio file no matter what, also other bug fixes, logging and reporting changes to report when slowness occurs, and increased h2 cache settings
    Former-commit-id: 2a12601e6ca14284a607618915706c22d3ea0839 [formerly af3c5f925641b2bb2fef0c2c427a3717929b7bf5] [formerly 81bab578d69cd687f3611422cdb4738622ea15b9 [formerly edead3d09806406b520c6cafda400f26469fc449]]
    Former-commit-id: 1a637913a62b4a5cd964d36c7b4f905ee3e335cf [formerly 814c736645503831acb365d5a9ed461196aac2a5]
    Former-commit-id: 96b706cae414a25bf4efd9c65caa70353eb6a461

--- src/mitll/langtest/client/PropertyHandler.java
+++ src/mitll/langtest/client/PropertyHandler.java
@@ -15,23 +15,15 @@ import java.util.Map;
 public class PropertyHandler {
   // property file property names
   private static final String GRADING_PROP = "grading";
-  private static final String ENGLISH_ONLY_MODE = "englishOnlyMode";
-  private static final String GOODWAVE_MODE = "goodwaveMode";
-  private static final String ARABIC_TEXT_DATA_COLLECT = "arabicTextDataCollect";
-  private static final String SHOW_TURK_TOKEN = "showTurkToken";
   private static final String APP_TITLE = "appTitle";
   private static final String SPLASH_TITLE = "splashTitle";
-  private static final String SEGMENT_REPEATS = "segmentRepeats";
   private static final String RELEASE_DATE = "releaseDate";
   private static final String BKG_COLOR_FOR_REF1 = "bkgColorForRef";
-  private static final String AUTO_CRT = "autocrt";
   private static final String DEMO_MODE = "demo";
-  private static final String DATA_COLLECT_MODE = "dataCollect";
-  private static final String CRT_DATA_COLLECT_MODE = "crtDataCollect";
   private static final String COLLECT_AUDIO= "collectAudio";
   private static final String RECORD_TIMEOUT = "recordTimeout";
  // private static final String SHORT_RECORD_TIMEOUT = "shortRecordTimeout";
-  private static final String TEACHER_VIEW = "teacherView";
+
   private static final String ADMIN_VIEW = "adminView";
   private static final String MINIMAL_UI = "minimalUI";
   private static final String NAME_FOR_ITEM = "nameForItem";
@@ -39,12 +31,7 @@ public class PropertyHandler {
   private static final String NAME_FOR_RECORDER = "nameForRecorder";
   private static final String NUM_GRADES_TO_COLLECT = "numGradesToCollect";
   private static final String LOG_CLIENT_MESSAGES = "logClient";
-  private static final String SHOW_SECTIONS = "showSections";
-  private static final String FLASHCARD = "flashcard";
-  private static final String FLASHCARD_TEACHER_VIEW = "flashcardTeacherView";
 
-  private static final String TIMED_GAME = "timedGame";
-  private static final String GAME_TIME = "gameTimeSeconds";
   private static final String LANGUAGE = "language";
   private static final String CONTINUE_PROMPT = "promptBeforeNextItem";
   private static final String RIGHT_ALIGN_CONTENT = "rightAlignContent";
@@ -53,11 +40,7 @@ public class PropertyHandler {
 
   // URL parameters that can override above parameters
   private static final String GRADING = GRADING_PROP;
-  private static final String ENGLISH = "english";
   private static final String GOODWAVE = "goodwave";
-  private static final String ARABIC_COLLECT = "arabicCollect";
-  private static final String TURK = "turk";
-  private static final String REPEATS = "repeats";
   private static final String BKG_COLOR_FOR_REF = "bkgColorForRef";
   private static final String EXERCISE_TITLE = "exercise_title";
   private static final String ADMIN_PARAM = "admin";
@@ -66,31 +49,33 @@ public class PropertyHandler {
 
   private static final String DLI_LANGUAGE_TESTING = "NetProF";
   private static final boolean DEFAULT_GOODWAVE_MODE = false;
-  private static final boolean DEFAULT_ARABIC_TEXT_COLLECT = false;
-  private static final boolean DEFAULT_SHOW_TURK_TOKEN = false;
-  private static final int DEFAULT_SEGMENT_REPEATS = 0;
   private static final int DEFAULT_TIMEOUT = 45000;
-  //private static final int DEFAULT_SHORT_TIMEOUT = 15000;
-  private static final int DEFAULT_GAME_TIME_SECONDS = 60;
   private static final String DEFAULT_EXERCISE = null;
   private static final int NUM_GRADES_TO_COLLECT_DEFAULT = 1;
- // private static final String ADD_RECORD_KEY_BINDING = "addRecordKeyBinding";
   private static final String LOGIN_TYPE_PARAM = "loginType";
-  //private static final String FLASHCARD_PREVIEW_HEIGHT = "flashcardPreviewHeight";
   private static final String SHOW_FLASHCARD_ANSWER = "showFlashcardAnswer";
   private static final String FLASHCARD_TEXT_RESPONSE = "flashcardTextResponse";
-  private static final String EXERCISES_IN_ORDER = "exercisesInOrder";
   private static final String ALLOW_PLUS_IN_URL = "allowPlusInURL";
   private static final String CLASSROOM_MODE = "classroomMode";
   private static final String SHOW_SPECTROGRAM = "spectrogram";
+  private static final String CLICK_AND_HOLD = "clickAndHold";
   private boolean spectrogram = false;
-  //private boolean combinedMode = false;
   private static final String NO_MODEL = "noModel";
-  private static final String INSTRUMENT = "instrument";
-  private boolean instrument = true; // by default we instrument for now 4/3/14
+ // private static final String INSTRUMENT = "instrument";
+  private boolean clickAndHold = true;
+  private boolean quietAudioOK;
 
-  public boolean doInstrumentation() {
-    return instrument;
+  public boolean doClickAndHold() {
+    System.out.println("click and hold " + clickAndHold);
+    return clickAndHold;
+  }
+
+  public boolean isQuietAudioOK() {
+    return quietAudioOK;
+  }
+
+  public void setQuietAudioOK(boolean quietAudioOK) {
+    this.quietAudioOK = quietAudioOK;
   }
 
   public enum LOGIN_TYPE { UNDEFINED, ANONYMOUS, STUDENT }
@@ -98,19 +83,14 @@ public class PropertyHandler {
   private final Map<String, String> props;
 
   private boolean grading = false;
-  private boolean englishOnlyMode = false;
   private boolean goodwaveMode = DEFAULT_GOODWAVE_MODE;
-  private boolean arabicTextDataCollect = DEFAULT_ARABIC_TEXT_COLLECT;
-  private boolean showTurkToken = DEFAULT_SHOW_TURK_TOKEN;
-  private int segmentRepeats = DEFAULT_SEGMENT_REPEATS;
   private boolean bkgColorForRef = false;
   private String exercise_title;
   private String appTitle = DLI_LANGUAGE_TESTING;
   private boolean autocrt;
   private boolean demoMode;
-  private boolean dataCollectMode;
+  //private boolean dataCollectMode;
   private boolean collectAudio = true;
-  private boolean teacherView = false;
   private boolean adminView = false;
   private boolean logClientMessages = false;
   private boolean minimalUI = false;
@@ -119,19 +99,13 @@ public class PropertyHandler {
   private String nameForAnswer = "Recording";
   private String nameForRecorder = "Speaker";
   private String language = "";
-  private boolean showSections = false;
-  private boolean flashcardTeacherView = false;
-  private boolean flashCard = false;
-  //private boolean timedGame = false;
+
   private String releaseDate;
   private String turkID = "";
 
   private int recordTimeout = DEFAULT_TIMEOUT;
-  //private int shortRecordTimeout = DEFAULT_SHORT_TIMEOUT;
 
-  private int gameTimeSeconds = DEFAULT_GAME_TIME_SECONDS;
   private float screenPortion = 1.0f;
-  private boolean CRTDataCollectMode;
   private String splashTitle;
   private boolean promptBeforeNextItem = false;
   private boolean rightAlignContent;
@@ -142,7 +116,6 @@ public class PropertyHandler {
 
   private boolean flashcardTextResponse = false;
   private boolean showFlashcardAnswer = true;
-  private boolean showExercisesInOrder = false;
   private boolean allowPlusInURL;
   private boolean bindNextToEnter;
   private boolean classroomMode = false;
@@ -163,18 +136,17 @@ public class PropertyHandler {
       String key = kv.getKey();
       String value = kv.getValue();
       if (key.equals(GRADING_PROP)) grading = getBoolean(value);
-      else if (key.equals(ENGLISH_ONLY_MODE)) englishOnlyMode = getBoolean(value);
-      else if (key.equals(GOODWAVE_MODE)) goodwaveMode = getBoolean(value);
-      else if (key.equals(ARABIC_TEXT_DATA_COLLECT)) arabicTextDataCollect = getBoolean(value);
-      else if (key.equals(SHOW_TURK_TOKEN)) showTurkToken = getBoolean(value);
+   //   else if (key.equals(ENGLISH_ONLY_MODE)) englishOnlyMode = getBoolean(value);
+   //   else if (key.equals(GOODWAVE_MODE)) goodwaveMode = getBoolean(value);
+   //   else if (key.equals(ARABIC_TEXT_DATA_COLLECT)) arabicTextDataCollect = getBoolean(value);
+   //   else if (key.equals(SHOW_TURK_TOKEN)) showTurkToken = getBoolean(value);
       else if (key.equals(APP_TITLE)) appTitle = value;
-      else if (key.equals(SEGMENT_REPEATS)) segmentRepeats = getInt(value,DEFAULT_SEGMENT_REPEATS,SEGMENT_REPEATS)-1;
+     // else if (key.equals(SEGMENT_REPEATS)) segmentRepeats = getInt(value,DEFAULT_SEGMENT_REPEATS,SEGMENT_REPEATS)-1;
       else if (key.equals(RELEASE_DATE)) releaseDate = value;
       else if (key.equals(BKG_COLOR_FOR_REF1)) bkgColorForRef = getBoolean(value);
-      else if (key.equals(AUTO_CRT)) autocrt = getBoolean(value);
       else if (key.equals(DEMO_MODE)) demoMode = getBoolean(value);
-      else if (key.equals(DATA_COLLECT_MODE)) dataCollectMode = getBoolean(value);
-      else if (key.equals(CRT_DATA_COLLECT_MODE)) CRTDataCollectMode = getBoolean(value);
+   //   else if (key.equals(DATA_COLLECT_MODE)) dataCollectMode = getBoolean(value);
+ //     else if (key.equals(CRT_DATA_COLLECT_MODE)) CRTDataCollectMode = getBoolean(value);
       else if (key.equals(RECORD_TIMEOUT)) recordTimeout = getInt(value, DEFAULT_TIMEOUT, RECORD_TIMEOUT);
      // else if (key.equals(SHORT_RECORD_TIMEOUT)) shortRecordTimeout = getInt(value, DEFAULT_SHORT_TIMEOUT, SHORT_RECORD_TIMEOUT);
       else if (key.equals(COLLECT_AUDIO)) collectAudio = getBoolean(value);
@@ -183,27 +155,25 @@ public class PropertyHandler {
       else if (key.equals(NAME_FOR_ITEM)) nameForItem = value;
       else if (key.equals(NAME_FOR_ANSWER)) nameForAnswer = value;
       else if (key.equals(NAME_FOR_RECORDER)) nameForRecorder = value;
-      else if (key.equals(TEACHER_VIEW)) teacherView = getBoolean(value);
+   //   else if (key.equals(TEACHER_VIEW)) teacherView = getBoolean(value);
       else if (key.equals(NUM_GRADES_TO_COLLECT)) numGradesToCollect = getInt(value, NUM_GRADES_TO_COLLECT_DEFAULT, NUM_GRADES_TO_COLLECT);
       else if (key.equals(LOG_CLIENT_MESSAGES)) logClientMessages = getBoolean(value);
-      else if (key.equals(SHOW_SECTIONS)) showSections = getBoolean(value);
-      else if (key.equals(FLASHCARD_TEACHER_VIEW)) flashcardTeacherView = getBoolean(value);
-      else if (key.equals(FLASHCARD)) flashCard = getBoolean(value);
       else if (key.equals(LANGUAGE)) language = value;
       else if (key.equals(SPLASH_TITLE)) splashTitle = value;
-      else if (key.equals(GAME_TIME)) gameTimeSeconds = getInt(value, DEFAULT_GAME_TIME_SECONDS, GAME_TIME);
+      //else if (key.equals(GAME_TIME)) gameTimeSeconds = getInt(value, DEFAULT_GAME_TIME_SECONDS, GAME_TIME);
       else if (key.equals(CONTINUE_PROMPT)) promptBeforeNextItem = getBoolean(value);
       else if (key.equals(RIGHT_ALIGN_CONTENT)) rightAlignContent = getBoolean(value);
       else if (key.equals(FLASHCARD_TEXT_RESPONSE)) flashcardTextResponse = getBoolean(value);
       else if (key.equals(SHOW_FLASHCARD_ANSWER)) showFlashcardAnswer = getBoolean(value);
-      else if (key.equals(EXERCISES_IN_ORDER)) showExercisesInOrder = getBoolean(value);
+    //  else if (key.equals(EXERCISES_IN_ORDER)) showExercisesInOrder = getBoolean(value);
       else if (key.equals(ALLOW_PLUS_IN_URL)) allowPlusInURL = getBoolean(value);
       else if (key.equals(BIND_NEXT_TO_ENTER)) bindNextToEnter = getBoolean(value);
       else if (key.equals(SCREEN_PORTION)) screenPortion = getFloat(value, 1.0f, SCREEN_PORTION);
       else if (key.equals(CLASSROOM_MODE)) classroomMode = getBoolean(value);
       else if (key.equals(SHOW_SPECTROGRAM)) spectrogram = getBoolean(value);
-      else if (key.equals(INSTRUMENT)) instrument = getBoolean(value);
+   //   else if (key.equals(INSTRUMENT)) instrument = getBoolean(value);
       else if (key.equals(NO_MODEL)) noModel = getBoolean(value);
+      else if (key.equals("quietAudioOK")) quietAudioOK = getBoolean(value);
       else if (key.equals(LOGIN_TYPE_PARAM)) {
         try {
           loginType = LOGIN_TYPE.valueOf(value.toUpperCase());
@@ -254,15 +224,12 @@ public class PropertyHandler {
    */
   private boolean checkParams() {
     String isGrading = Window.Location.getParameter(GRADING);
-    String isEnglish = Window.Location.getParameter(ENGLISH);
     String numGrades = Window.Location.getParameter(NUM_GRADES_TO_COLLECT_PARAM);
     String goodwave = Window.Location.getParameter(GOODWAVE);
-    String repeats = Window.Location.getParameter(REPEATS);
-    String arabicCollect = Window.Location.getParameter(ARABIC_COLLECT);
-    String turk = Window.Location.getParameter(TURK);
     String bkgColorForRefParam = Window.Location.getParameter(BKG_COLOR_FOR_REF);
     String demoParam = Window.Location.getParameter(DEMO_MODE);
 
+    // supports headstart mode
     String exercise_title = Window.Location.getParameter(EXERCISE_TITLE);
     if (exercise_title != null) {
       this.exercise_title = exercise_title;
@@ -279,31 +246,23 @@ public class PropertyHandler {
         e.printStackTrace();
       }
     }
-    // System.out.println("param grading " + isGrading);
-    englishOnlyMode = isEnglishOnlyMode() || (isEnglish != null && !isEnglish.equals("false"));
     goodwaveMode    = isGoodwaveMode() || (goodwave != null && !goodwave.equals("false"));
     if (goodwave != null && goodwave.equals("false")) goodwaveMode = false;
-    boolean grading = this.isGrading() || (isGrading != null && !isGrading.equals("false")) || isEnglishOnlyMode();
+    boolean grading = this.isGrading() || (isGrading != null && !isGrading.equals("false"));
     setGrading(grading);
     // get audio repeats
-    segmentRepeats = getInt(repeats, DEFAULT_SEGMENT_REPEATS,REPEATS) -1;
     numGradesToCollect = getInt(numGrades, numGradesToCollect, NUM_GRADES_TO_COLLECT);
-    if (numGrades != null)
-    if (arabicCollect != null) {
-      arabicTextDataCollect = !arabicCollect.equals("false");
-    }
-    if (turk != null) {
-      showTurkToken = !turk.equals("false");
-    }
+//    if (turk != null) {
+ //     showTurkToken = !turk.equals("false");
+  //  }
     if (bkgColorForRefParam != null) {
       bkgColorForRef = !bkgColorForRefParam.equals("false");
     }
     if (demoParam != null) {
       demoMode = !demoParam.equals("false");
     }
-    if (Window.Location.getParameter(DATA_COLLECT_MODE) != null) {
-      dataCollectMode = !Window.Location.getParameter(DATA_COLLECT_MODE).equals("false");
-    }
+
+
     String adminParam = Window.Location.getParameter(ADMIN_PARAM);
     if (adminParam != null) {
       adminView = !adminParam.equals("false");
@@ -313,22 +272,9 @@ public class PropertyHandler {
     if (turkParam != null) {
       turkID = turkParam;
     }
-    gameTimeSeconds = getInt(Window.Location.getParameter(GAME_TIME), gameTimeSeconds, REPEATS);
 
-    String flashcardParam = Window.Location.getParameter(FLASHCARD);
-    if (flashcardParam != null) {
-      flashCard = !flashcardParam.equals("false");
-    }
-
-    String timedgameParam = Window.Location.getParameter(TIMED_GAME);
-    if (timedgameParam != null) {
-     // timedGame = !timedgameParam.equals("false");
-    }
-    if (Window.Location.getParameter(SHOW_SECTIONS) != null) {
-      showSections = !Window.Location.getParameter(SHOW_SECTIONS).equals("false");
-    }
-    if (Window.Location.getParameter(EXERCISES_IN_ORDER) != null) {
-      showExercisesInOrder = !Window.Location.getParameter(EXERCISES_IN_ORDER).equals("false");
+    if (Window.Location.getParameter(CLICK_AND_HOLD) != null) {
+      clickAndHold = !Window.Location.getParameter(CLICK_AND_HOLD).equals("false");
     }
 
     if (Window.Location.getParameter(SHOW_SPECTROGRAM) != null) {
@@ -368,9 +314,6 @@ public class PropertyHandler {
     }
   }
 
-  public boolean isArabicTextDataCollect() {
-    return arabicTextDataCollect;
-  }
 
   /**
    * @see mitll.langtest.client.LangTest#modeSelect()
@@ -385,21 +328,21 @@ public class PropertyHandler {
 
   private void setGrading(boolean v) { this.grading = v; }
 
-  public boolean isEnglishOnlyMode() {
+  /*boolean isEnglishOnlyMode() {
     return englishOnlyMode;
   }
-
+*/
   public boolean isGoodwaveMode() {
     return goodwaveMode;
   }
 
-  public boolean isShowTurkToken() {
+/*  public boolean isShowTurkToken() {
     return showTurkToken;
   }
 
   public int getSegmentRepeats() {
     return segmentRepeats;
-  }
+  }*/
 
   public boolean isBkgColorForRef() {
     return bkgColorForRef;
@@ -421,16 +364,12 @@ public class PropertyHandler {
     return demoMode;
   }
 
-  public boolean isDataCollectMode() {
-    return dataCollectMode;
-  }
+ // public boolean isDataCollectMode() {
+ //   return dataCollectMode;
+ // }
 
   public boolean isCollectAudio() {
     return collectAudio;
-  }
-
-  public boolean isTeacherView() {
-    return teacherView;
   }
 
   public boolean isAdminView() {
@@ -459,34 +398,28 @@ public class PropertyHandler {
   public boolean isLogClientMessages() {
     return logClientMessages;
   }
-
+/*
   public boolean isShowSections() {
     return showSections;
   }
 
-  /**
+  *//**
    * @see LangTest#makeExerciseList
    * @see mitll.langtest.client.LangTest#onModuleLoad2()
    * @seex mitll.langtest.client.LangTest#setFactory
    * @return
-   */
+   *//*
   public boolean isFlashCard() {  return flashCard; }
 
   public boolean isFlashcardTeacherView() {
     return flashcardTeacherView;
-  }
+  }*/
 
   public String getLanguage() {
     return language;
   }
 
-  public String getSplash() {
-    return splashTitle;
-  }
-
-  public boolean isCRTDataCollectMode() {
-    return CRTDataCollectMode;
-  }
+  public String getSplash() { return splashTitle; }
 
   public boolean isPromptBeforeNextItem() {
     return promptBeforeNextItem;
@@ -506,6 +439,7 @@ public class PropertyHandler {
   }
 
   public boolean showExercisesInOrder() {
+    boolean showExercisesInOrder = false;
     return showExercisesInOrder;
   }
 
