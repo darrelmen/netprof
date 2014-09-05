82e939c0e536cb7ca76f5ae339583729a6dd6a54
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-09-05 18:18:57 -0400

    fixed bug with download audio for egyptian, sends email when download servlet has an exception
    Former-commit-id: 3b3eda30286b6cb3913276d8c73035a9d643347b [formerly 5d108e27d24207ee8cb861ebe80c7909830e5451] [formerly d5a4ae68d7e91af906aeeb8a4da2ce07adbea7ba [formerly 187f19d66438181d37fe771b8b50fc74f61c535f]]
    Former-commit-id: a301903636f07ca0dad9b26d66952fc04bf49c53 [formerly 6cc66252494c035e2ee6e834cb96cd8ae8ecb145]
    Former-commit-id: 4861c8fa7707948bcf04b45f6297886e14e398c0

--- src/mitll/langtest/shared/AudioExercise.java
+++ src/mitll/langtest/shared/AudioExercise.java
@@ -1,6 +1,5 @@
 package mitll.langtest.shared;
 
-import javax.validation.constraints.Min;
 import java.util.*;
 
 /**
@@ -12,13 +11,13 @@ import java.util.*;
  */
 public class AudioExercise extends ExerciseShell {
   private static final String SPEED = "speed";
-  public static final String REGULAR = "regular";
-  public static final String SLOW = "slow";
+  private static final String REGULAR = "regular";
+  private static final String SLOW = "slow";
   private static final String WAV = ".wav";
   private static final String MP3 = ".mp3";
 
-  protected Map<String,AudioAttribute> audioAttributes = new HashMap<String, AudioAttribute>();
-  protected Map<String,String> unitToValue = new HashMap<String, String>();
+  private Map<String,AudioAttribute> audioAttributes = new HashMap<String, AudioAttribute>();
+  private Map<String,String> unitToValue = new HashMap<String, String>();
   private Map<String,ExerciseAnnotation> fieldToAnnotation = new HashMap<String, ExerciseAnnotation>();
 
   public AudioExercise() {}
@@ -84,7 +83,7 @@ public class AudioExercise extends ExerciseShell {
     if (audio != null) audioAttributes.remove(audio.getKey());
   }
 
-  public AudioAttribute getAudio(String name, String value) {
+  AudioAttribute getAudio(String name, String value) {
     for (AudioAttribute audio : getAudioAttributes()) {
       if (audio.matches(name,value)) return audio;
     }
@@ -186,11 +185,17 @@ public class AudioExercise extends ExerciseShell {
     return null;
   }
 
+  /**
+   * @see mitll.langtest.server.database.AudioExport#getAudioAttribute(MiniUser, CommonExercise, boolean, String)
+   * @param userID
+   * @param speed
+   * @return
+   */
   public AudioAttribute getRecordingsBy(long userID, String speed) {
     List<AudioAttribute> recordingsBy = getRecordingsBy(userID);
 
     for (AudioAttribute attr : recordingsBy) {
-      if (attr.getSpeed().equalsIgnoreCase(speed)) {
+      if (attr.getSpeed() != null && attr.getSpeed().equalsIgnoreCase(speed)) {
         return attr;
       }
     }
@@ -198,7 +203,7 @@ public class AudioExercise extends ExerciseShell {
     return null;
   }
 
-  public List<AudioAttribute> getRecordingsBy(long userID) {
+  List<AudioAttribute> getRecordingsBy(long userID) {
     List<AudioAttribute> mine = new ArrayList<AudioAttribute>();
     for (AudioAttribute attr : getAudioAttributes()) {
       if (attr.getUser() != null) {
