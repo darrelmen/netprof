ca6a0f607321e19f763b29330255e512e14c08d4
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-09-16 19:50:28 -0400

    shows summary of scores at the end of doing flashcards
    Former-commit-id: cda16667b46acc5afbe596bb6c0ebbee77e0fb68

--- src/mitll/langtest/shared/flashcard/CorrectAndScore.java
+++ src/mitll/langtest/shared/flashcard/CorrectAndScore.java
@@ -7,27 +7,37 @@ import mitll.langtest.shared.Result;
  * Created by go22670 on 9/13/14.
  */
 public class CorrectAndScore implements IsSerializable, Comparable<CorrectAndScore> {
-  boolean wasCorrect;
-  int score;
+  private boolean correct;
+  private int score;
   long timestamp;
 
   public CorrectAndScore() {
   }
 
-  public CorrectAndScore(boolean wasCorrect, int score, long timestamp) {
-    this.wasCorrect = wasCorrect;
+  public CorrectAndScore(boolean correct, int score, long timestamp) {
+    this.correct = correct;
     this.score = score;
     this.timestamp = timestamp;
   }
 
   public CorrectAndScore(Result r) {
-    this.wasCorrect =r.isCorrect();
-    this.score = (int)r.getPronScore()*100;
+    this.correct =r.isCorrect();
+    this.score = (int)(r.getPronScore()*100f);
     this.timestamp = r.timestamp;
   }
 
   @Override
   public int compareTo(CorrectAndScore o) {
     return timestamp < o.timestamp ? -1:timestamp>o.timestamp ? +1 :0;
+  }
+
+  public String toString() { return isCorrect() ? "C":"I"+ " score " + getScore(); }
+
+  public boolean isCorrect() {
+    return correct;
+  }
+
+  public int getScore() {
+    return score;
   }
 }
