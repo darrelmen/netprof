8c40c70af1f4ce2f7d6e8d9171cff57cae3032e1
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-09-19 23:29:14 -0400

    keeps track of incorrect responses for flashcards and displays in that order
    Former-commit-id: ce8d512b84a933d96eea0ef8bac492207625813d [formerly 75c6e0b3d2c20fafe7893a3bd36f567cb6de0b50] [formerly 9e468ce10396dfec6b1d64902858485e5e4b95e1 [formerly d039be91024f4547d05de1144e2f9ef7c37cf836]] [formerly 709df91511625dbda53f60257e777cb4a101e3e4]
    Former-commit-id: 28c0c81a54ec031532d76622f91556be30b6781f [formerly 5c4cfb5179158a75182e6b10c00d4098a5ecdfcf] [formerly 494606d65f7c646b4e948f6fe216e0b0c0d01329 [formerly 5092893027fc0f0c01e9ee04ffe0dc9f07d2d932]] [formerly 1ac5ef9d233f01a104516a7313613124bf2bdf8b]
    Former-commit-id: 958d3f20b14270932d6826aa44421b7015961614
    Former-commit-id: 17a88d2a1c54dc174712b43308dffc69f3008e21 [formerly cce460683ddd4a3bcaa5691a9e62868e25e2c1b6]
    Former-commit-id: 97fc788d3b93d76a94f4029d4a170cae02a7ac28

--- src/mitll/langtest/shared/flashcard/CorrectAndScore.java
+++ src/mitll/langtest/shared/flashcard/CorrectAndScore.java
@@ -7,6 +7,7 @@ import mitll.langtest.shared.Result;
  * Created by go22670 on 9/13/14.
  */
 public class CorrectAndScore implements IsSerializable, Comparable<CorrectAndScore> {
+  private String id;
   private boolean correct;
   private int score;
   private long timestamp;
@@ -14,7 +15,8 @@ public class CorrectAndScore implements IsSerializable, Comparable<CorrectAndSco
   public CorrectAndScore() {
   }
 
-  public CorrectAndScore(boolean correct, int score, long timestamp) {
+  public CorrectAndScore(String id, boolean correct, int score, long timestamp) {
+    this.id = id;
     this.correct = correct;
     this.score = score;
     this.timestamp = timestamp;
@@ -28,16 +30,28 @@ public class CorrectAndScore implements IsSerializable, Comparable<CorrectAndSco
 
   @Override
   public int compareTo(CorrectAndScore o) {
-    return timestamp < o.timestamp ? -1:timestamp>o.timestamp ? +1 :0;
+    return getTimestamp() < o.getTimestamp() ? -1: getTimestamp() > o.getTimestamp() ? +1 :0;
   }
-
-  public String toString() { return isCorrect() ? "C":"I"+ " score " + getScore(); }
 
   public boolean isCorrect() {
     return correct;
   }
 
+  /**
+   *
+   * @return 0-100
+   */
   public int getScore() {
     return score;
   }
+
+  public String getId() {
+    return id;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public String toString() { return "id " + getId() + " " +(isCorrect() ? "C":"I")+ " score " + getScore(); }
 }
