918ae8027efa3dbb5c38a32ea5de5c72e7390ec7
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-09-23 15:29:13 -0400

    changed sort order for flashcards, record button gets the focus so arrow keys don't go to chapter selection widget
    Former-commit-id: 0e16bc8cfc538eb6a4df86aec16cb952650f9cc6 [formerly 26908acc62a3f26612f661b0ff6f7a880b2530bd] [formerly 220f947c461c6ea8337a7c2cf648e430417498bd [formerly 162fe89c68795a9010a18a0de77dc294e53b1696]] [formerly 0e16bc8cfc538eb6a4df86aec16cb952650f9cc6 [formerly 26908acc62a3f26612f661b0ff6f7a880b2530bd] [formerly 220f947c461c6ea8337a7c2cf648e430417498bd [formerly 162fe89c68795a9010a18a0de77dc294e53b1696]] [formerly 0e16bc8cfc538eb6a4df86aec16cb952650f9cc6 [formerly 26908acc62a3f26612f661b0ff6f7a880b2530bd] [formerly 220f947c461c6ea8337a7c2cf648e430417498bd [formerly 162fe89c68795a9010a18a0de77dc294e53b1696]] [formerly 4384d43a6486709022d2811d90aa0670cdaedf86 [formerly 437c8394544a90bf19b418f1ae18c2352784a0e4] [formerly 320824dab3e91bfcdff2039405a496327f0b4c30 [formerly 46f42f822d391ea0304c07036187c3be24356707]]]]]
    Former-commit-id: 3417a6e3bc28ba59eaa3e792241da99b4933811a [formerly 24528ba2280a88e9c2fa1bacac18b2ee3c257ea1] [formerly fedcec8500e706bb9d4ff1b6c48d6ee88c22f959 [formerly baa35c6876b2b76faa8559a953739bf679e9f38e]] [formerly 3417a6e3bc28ba59eaa3e792241da99b4933811a [formerly 24528ba2280a88e9c2fa1bacac18b2ee3c257ea1] [formerly fedcec8500e706bb9d4ff1b6c48d6ee88c22f959 [formerly baa35c6876b2b76faa8559a953739bf679e9f38e]] [formerly ac18feb27a7ec0d2b9d117f618c4b236b7c25338 [formerly 1c7518727d8edefdd5e8905ad81fa0bc9f1d1f10] [formerly 7a8d6bf3c0e52646c0e96175cf684a2052b3df27 [formerly 0019eb4fca88f1a08cfa7062465948714e80b015]]]] [formerly f19c6a2fa0203b14079fb3a2752e78a9efea480d [formerly 6fd92e8663ca60c52f64cdafb4068478a0c0def7] [formerly 81e155029f153156cc963f439e9f15555f592123 [formerly 98acaad134451660e50a041088e05a3eb473b0d9]] [formerly f19c6a2fa0203b14079fb3a2752e78a9efea480d [formerly 6fd92e8663ca60c52f64cdafb4068478a0c0def7] [formerly 81e155029f153156cc963f439e9f15555f592123 [formerly 98acaad134451660e50a041088e05a3eb473b0d9]] [formerly c75584feac4087dbbb41185ecfb463c7edb590b0 [formerly de8adc5dddaf3e8ef7944c499d8a979644fdc9ee] [formerly 88c5ce33a7d1b080cfa12f4dff37ffb91fec0bdf [formerly afc0f5e98f158eadb0d56269871d7ec6d83be6c6]]]]]
    Former-commit-id: f19c6a2fa0203b14079fb3a2752e78a9efea480d [formerly 6fd92e8663ca60c52f64cdafb4068478a0c0def7] [formerly 81e155029f153156cc963f439e9f15555f592123 [formerly 98acaad134451660e50a041088e05a3eb473b0d9]]
    Former-commit-id: 436d1f1495c7db1ea0906b5ccad6cde7773303f9 [formerly f38d318f7ce0977d269651bf0f107a641798a87e] [formerly 6ed0c4d1da39969ba3dfd78871f4264be4d67314 [formerly 83daebe590f70d32b114d9956b6b7b0d4bc7e9a3]] [formerly 7a10998d5c2986c67756078d4ab4b46ad6521125 [formerly a1d5f1c2703abcb46e0897e2877470af5f0ac49c] [formerly ce98bc4a13c6e67c4f681db2ac612614e89cf42e [formerly d3bb2aed45f584a00056df301f05858f88064202]]]
    Former-commit-id: 5a339bc214e0d292b262afae9a8af3fb96de91e1 [formerly 901ed6b556c04eed2aabd1da7b77b0414fccd2c4] [formerly b6871313b160fefc6dc91bd5b265105501f1249a [formerly b9eff1afd8294ccd399826964e948b2239e8b301]]
    Former-commit-id: 3913759d9a0f5b9acd9c39330d36b34495284d20 [formerly fd6b4f2a82efe9913cca84abf6e5106ce7493683]
    Former-commit-id: 3e035cdee8c1a280200a0a25c111158d0446382a

--- src/mitll/langtest/shared/flashcard/ExerciseCorrectAndScore.java
+++ src/mitll/langtest/shared/flashcard/ExerciseCorrectAndScore.java
@@ -14,13 +14,7 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
   private List<CorrectAndScore> correctAndScores = new ArrayList<CorrectAndScore>();
   private String id;
 
-  public ExerciseCorrectAndScore() {
-  }
-
-/*  public ExerciseCorrectAndScore(String id, List<CorrectAndScore> correctAndScores) {
-    this.id = id;
-    this.correctAndScores = correctAndScores;
-  }*/
+  public ExerciseCorrectAndScore() {}
 
   public ExerciseCorrectAndScore(String id) {
     this.id = id;
@@ -29,40 +23,25 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
   @Override
   public int compareTo(ExerciseCorrectAndScore o) {
     if (isEmpty() && o.isEmpty()) {
-      return getId().compareTo(o.getId());
-    }
-    else if (isEmpty()) return -1;
+      return getId().compareTo(o.getId()); // TODO : consider compare on tooltip
+    } else if (isEmpty()) return -1;
     else if (o.isEmpty()) return +1;
     else { // neither is empty
-      int myI = getNumIncorrect();
-      int oI = o.getNumIncorrect();
-      int i = myI > oI ? -1 : myI < oI ? +1 : 0;
+      int myI = getDiff();
+      int oI = o.getDiff();
+      int i = myI < oI ? -1 : myI > oI ? +1 : 0;
       if (i == 0) {
-        int n = getNumCorrect();
-        int on = o.getNumCorrect();
+        float myScore = getAvgScore();
+        float otherScore = o.getAvgScore();
 
-        i = n < on ? -1 : n > on ? +1 : 0;
-
-        if (i == 0) {
-          float myScore = getAvgScore();
-          float otherScore = o.getAvgScore();
-
-          return myScore < otherScore ? -1 : myScore > otherScore ? +1 : getId().compareTo(o.getId());
-        } else {
-          return i;
-        }
+        return myScore < otherScore ? -1 : myScore > otherScore ? +1 : getId().compareTo(o.getId());
       } else {
         return i;
       }
     }
   }
 
-/*  @Override
-  public boolean equals(Object obj) {
-    return super.equals(obj);
-  }*/
-
-  private boolean isEmpty() {  return correctAndScores.isEmpty();  }
+  private boolean isEmpty() { return correctAndScores.isEmpty();  }
 
   private int getNumCorrect() {
     int c = 0;
@@ -84,6 +63,18 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
     return c;
   }
 
+
+  private int getDiff() {
+    int c = 0;
+    List<CorrectAndScore> toUse = getCorrectAndScores();
+    if (toUse.size() > MAX_TO_USE) toUse = toUse.subList(toUse.size() - MAX_TO_USE, toUse.size());
+    for (CorrectAndScore correctAndScore : toUse) {
+      if (correctAndScore.isCorrect()) c++;
+      else c--;
+    }
+    return c;
+  }
+
 /*  private float getAvgCorrect() {
     if (isEmpty()) return 0f;
     float c = 0;
@@ -96,12 +87,12 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
   }*/
 
   private float getAvgScore() {
-    if  (isEmpty()) return 0f;
+    if (isEmpty()) return 0f;
 
     float c = 0f;
     float n = 0f;
-    List<CorrectAndScore> toUse= getCorrectAndScores();
-    if (toUse.size() > MAX_TO_USE) toUse=toUse.subList(toUse.size()- MAX_TO_USE,toUse.size());
+    List<CorrectAndScore> toUse = getCorrectAndScores();
+    if (toUse.size() > MAX_TO_USE) toUse = toUse.subList(toUse.size() - MAX_TO_USE, toUse.size());
     for (CorrectAndScore correctAndScore : toUse) {
       if (correctAndScore.getScore() > 0f) {
         c += correctAndScore.getScore();
@@ -113,7 +104,9 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
     return v;
   }
 
-  public int getAvgScorePercent() { return Math.round(getAvgScore()*100f);}
+  public int getAvgScorePercent() {
+    return Math.round(getAvgScore() * 100f);
+  }
 
   public void add(CorrectAndScore correctAndScore) {
     getCorrectAndScores().add(correctAndScore);
@@ -126,17 +119,19 @@ public class ExerciseCorrectAndScore implements IsSerializable, Comparable<Exerc
   public String getId() {
     return id;
   }
+
   public List<CorrectAndScore> getCorrectAndScores() {
     return correctAndScores;
   }
 
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    for (CorrectAndScore correctAndScore: getCorrectAndScores()) {
-      if (correctAndScore.isCorrect()) builder.append("+"); else builder.append("-");
+    for (CorrectAndScore correctAndScore : getCorrectAndScores()) {
+      if (correctAndScore.isCorrect()) builder.append("+");
+      else builder.append("-");
     }
 
-    return "id " + id + " " +builder +" correct " +getNumCorrect() + "/" + getNumIncorrect() +
-        " score " + (int)getAvgScore();
+    return "id " + id + " " + builder + " correct " + getNumCorrect() + "/" + getNumIncorrect() +
+        " score " + (int) getAvgScore();
   }
 }
