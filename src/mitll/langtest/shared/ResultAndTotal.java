4ab1d6fe2b538582f34930b1dd646a6dab98f900
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-09-25 17:29:42 -0400

    mostly does the filtering functionality in result table
    Former-commit-id: 3fe6cddc780e001d8dbad59b6facbd2a1af339b1 [formerly 631df6bc4b693d3ccccc2cb7fbc220c2f1993bcc]
    Former-commit-id: f8c207f282ded7ede28ea0f98d3e71f74b502107

---
+++ src/mitll/langtest/shared/ResultAndTotal.java
@@ -0,0 +1,21 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by go22670 on 9/25/14.
+ */
+public class ResultAndTotal implements IsSerializable {
+  public List<MonitorResult> results;
+  public int numTotal;
+  public ResultAndTotal() {}
+
+  public ResultAndTotal(ArrayList<MonitorResult> monitorResults, int n) {
+    this.results = monitorResults;
+    this.numTotal = n;
+  }
+
+}
