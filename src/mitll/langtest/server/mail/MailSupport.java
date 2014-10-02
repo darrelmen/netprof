12e6480f16c7eab8e02821b0b8ddf68a371866fa
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-10-02 19:07:35 -0400

    steps towards better feedback for login form
    Former-commit-id: 14d58814f230669d4733eb0f1da0e8e012bd521b

--- src/mitll/langtest/server/mail/MailSupport.java
+++ src/mitll/langtest/server/mail/MailSupport.java
@@ -24,7 +24,10 @@ public class MailSupport {
    * @see mitll.langtest.server.LangTestDatabaseImpl#getMailSupport()
    */
   public MailSupport(boolean debugEmail) {
-    this.debugEmail = debugEmail;
+
+    this.debugEmail = true;
+
+
   }
 
   public void sendEmail(String serverName, String to, String replyTo, String subject, String message) {
