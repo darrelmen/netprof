082046b1fb53b04a4e0732ad2a236029c33e4773
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-10-06 16:29:10 -0400

    moved classes to different packages that were under server.database
    Former-commit-id: a8ef98752a9ebe342bf34e8f95be172bd594f049 [formerly bc86b66f80f97de31a569f81892cfa0aba7427e4] [formerly de6ea59bdea2d2f9b3c03624a0059e494c11f557 [formerly daf4b69ac9bfd213bdab5e9ef57bcaf72370ec66]] [formerly e94dbc84e93ba92204a4bf4015bc50e6dbafb21d [formerly e17ea04c6ca310cdeaa082f11d7180ded7302e1b] [formerly 1e93e7b8f8a6b99fce0badea4f5b7fb3a1e32b93 [formerly 780ac11759dc8168229a8b92f3bb2ea576055c18]]]
    Former-commit-id: c14e12fe8d73dada14d332e88076d8f5c60ecd2a [formerly 787a55e8dc44c45a6a1559578909461a1ac51aea] [formerly 3976d0dd1009bebd01df1b8aaa515fb64dbe87fb [formerly 11775bd0a49bc6608e4e0a0459f9620c0b9b2d3c]] [formerly e8e44b294246c2f07208e04aa56f56c8fc98f29d [formerly d2d21bc49ef4e821b1da8e13370583864f33aa52] [formerly 9db87992075fcb0b9c1f9c79da1cd0932a9e0415 [formerly ad0cbcc1d0e2b84b4873e77bf8f1e999ddae6616]]]
    Former-commit-id: d78dc655c08f218b47bc532e5dbc6b119f9ab634 [formerly 2576d73bab553bea67cc4f23ba40fc668da46fa0] [formerly aef7b803870767b5adf62a9376dd9fe026ec88ae [formerly 378b9f5d48d392f01200259af6e8b580ba6f107c]]
    Former-commit-id: fab8c171ba8ffe098c30dcc5467561db59454153 [formerly 8941cedcd66f21bf468ad859b5e6d2f1b18b3c62]
    Former-commit-id: 36e5ab25923c57988d3edc1bd8021f6dcdea724c

--- src/mitll/langtest/client/user/UserPassLogin.java
+++ src/mitll/langtest/client/user/UserPassLogin.java
@@ -143,9 +143,7 @@ public class UserPassLogin extends UserDialog {
     if (!hasShownWelcome()) {
       keyStorage.storeValue(SHOWN_HELLO, "yes");
       showWelcome();
-    } ///else {
-      //logger.info("---- we've shown the welcome screen\n\n");
-    //}
+    }
   }
 
   public boolean hasShownWelcome() {
@@ -199,114 +197,6 @@ public class UserPassLogin extends UserDialog {
     getRightLogin(leftAndRight);
     return container;
   }
-/*
-  public Panel getResetPassword(final String token) {
-    Panel container = new DivWidget();
-    DivWidget child = new DivWidget();
-    container.add(child);
-    child.addStyleName("loginPageBack");
-
-    Panel leftAndRight = new DivWidget();
-    leftAndRight.addStyleName("resetPage");
-    container.add(leftAndRight);
-
-    DivWidget right = new DivWidget();
-
-    leftAndRight.add(right);
-    right.addStyleName("floatRight");
-
-    DivWidget rightDiv = new DivWidget();
-
-    Form form = new Form();
-    form.getElement().setId("resetForm");
-    rightDiv.add(form);
-
-    form.addStyleName("topMargin");
-    form.addStyleName("formRounded");
-    form.getElement().getStyle().setBackgroundColor("white");
-
-    final Fieldset fieldset = new Fieldset();
-    form.add(fieldset);
-
-    Heading w = new Heading(3, CHOOSE_A_NEW_PASSWORD);
-    fieldset.add(w);
-    w.addStyleName("leftFiveMargin");
-    final FormField firstPassword  = addControlFormFieldWithPlaceholder(fieldset, true, MIN_PASSWORD, 15, PASSWORD);
-    final FormField secondPassword = addControlFormFieldWithPlaceholder(fieldset, true, MIN_PASSWORD, 15, "Confirm " + PASSWORD);
-
-    getChangePasswordButton(token, fieldset, firstPassword, secondPassword);
-    right.add(rightDiv);
-    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-      public void execute() {
-        firstPassword.getWidget().setFocus(true);
-      }
-    });
-    return container;
-  }
-
-  private void getChangePasswordButton(final String token, Fieldset fieldset, final FormField firstPassword, final FormField secondPassword) {
-    final Button changePassword = new Button(CHANGE_PASSWORD);
-    changePassword.getElement().setId("changePassword");
-    eventRegistration.register(changePassword);
-    changePassword.addStyleName("floatRight");
-    fieldset.add(changePassword);
-    changePassword.addClickHandler(new ClickHandler() {
-      @Override
-      public void onClick(ClickEvent event) {
-        String first = firstPassword.box.getText();
-        String second = secondPassword.box.getText();
-        if (first.isEmpty()) {
-          markErrorBlur(firstPassword, PLEASE_ENTER_A_PASSWORD);
-        } else if (first.length() < MIN_PASSWORD) {
-          markErrorBlur(firstPassword, PLEASE_ENTER_A_LONGER_PASSWORD);
-        } else if (second.isEmpty()) {
-          markErrorBlur(secondPassword, PLEASE_ENTER_A_PASSWORD);
-        } else if (second.length() < MIN_PASSWORD) {
-          markErrorBlur(secondPassword, PLEASE_ENTER_A_LONGER_PASSWORD);
-        } else if (!second.equals(first)) {
-          markErrorBlur(secondPassword, PLEASE_ENTER_THE_SAME_PASSWORD);
-
-        } else {
-          changePassword.setEnabled(false);
-          enterKeyButtonHelper.removeKeyHandler();
-          service.changePFor(token, Md5Hash.getHash(first), new AsyncCallback<Boolean>() {
-            @Override
-            public void onFailure(Throwable caught) {
-              changePassword.setEnabled(true);
-              markErrorBlur(changePassword, "Can't communicate with server - check network connection.");
-            }
-
-            @Override
-            public void onSuccess(Boolean result) {
-              if (!result) {
-                markErrorBlur(changePassword, "Password has already been changed?");
-              } else {
-                markErrorBlur(changePassword, SUCCESS, PASSWORD_HAS_BEEN_CHANGED, Placement.LEFT);
-                Timer t = new Timer() {
-                  @Override
-                  public void run() {
-
-                    String newURL = trimURL(Window.Location.getHref());
-                    //  System.out.println("url now " +newURL);
-                    Window.Location.replace(newURL);
-                    Window.Location.reload();
-
-                  }
-                };
-                t.schedule(3000);
-              }
-            }
-          });
-        }
-      }
-    });
-    enterKeyButtonHelper.addKeyHandler(changePassword);
-
-    changePassword.addStyleName("rightFiveMargin");
-    changePassword.addStyleName("leftFiveMargin");
-
-    changePassword.setType(ButtonType.PRIMARY);
-  }*/
 
   private void getRightLogin(Panel leftAndRight) {
     DivWidget right = new DivWidget();
@@ -785,22 +675,6 @@ public class UserPassLogin extends UserDialog {
     // TODO : this competes with the warning for existing users.
    // new TooltipHelper().addTooltip(emailBox,"Your email is only used to reset your password or recover your username.");
 
-/*    emailBox.addBlurHandler(new BlurHandler() {
-      @Override
-      public void onBlur(BlurEvent event) {
-        String text = emailBox.getText();
-        logger.info("blur : gosh email '" + text + "' checking...");
-
-        if (!text.isEmpty() && !isValidEmail(text)) {
-          logger.info("blur : email '" + text + "' INVALID.");
-
-          markErrorBlur(signUpEmail, VALID_EMAIL);
-        }
-        else {
-          logger.info("blur : gosh email '" + text + "'  seems valid.");
-        }
-      }
-    });*/
     return emailBox;
   }
 
@@ -1125,6 +999,8 @@ public class UserPassLogin extends UserDialog {
   }
 
   /**
+   * Don't enable the teacher choice for legacy users, b/c it lets them skip over the
+   * recorder/not a recorder choice.
    *
    * @param result
    * @see #foundExistingUser(mitll.langtest.shared.User, boolean, String)
@@ -1144,14 +1020,6 @@ public class UserPassLogin extends UserDialog {
         signUpPassword.getGroup().setType(ControlGroupType.NONE);
       }
     });
-
-    // if they had permissions before, they were a teacher
-/*
-    if (!result.getPermissions().isEmpty()) {
-      teacherChoice.setValue(true);
-      teacherChoice.fireEvent(new KeyPressHelper.ButtonClickEvent());
-    }
-*/
   }
 
   /**
