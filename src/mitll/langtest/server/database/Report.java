7c5e6f198b0f5ef72dde5e4cc67be1861cecc287
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-10-16 15:11:33 -0400

    removes ref audio from recorded audio count
    Former-commit-id: 2fb42446540e83d45085bf2fc1a642534fdf44b8 [formerly 0a60516fdc1d1faf241dcfa2a8125c7c67da654d] [formerly 2fb42446540e83d45085bf2fc1a642534fdf44b8 [formerly 0a60516fdc1d1faf241dcfa2a8125c7c67da654d] [formerly c4e362c6ef9d7908effac3d7388c5594d6361efb [formerly 2f5637be534e1fdd7a7122326122f0e6d6b7cdb5]]]
    Former-commit-id: ce795f3218ff539a2771d26a81013c92cc5b9606 [formerly 82433ced0c421dd1c69e9dad3e9f461c38776fbb] [formerly a707ed4f8b8df67939d2f8b29ea823cd4db973a5 [formerly bb83fda430e9ee81dc8949f7329b2a7d1b0b3fea]] [formerly adc1aa39d3e8e900e98d4752568df6e7fd137b69 [formerly 6cebf49bbe78e39f1d13b4b31711afbda3c54857] [formerly 4d8f6ead9110c7a144e21f47d6eb4e1a05ccd3da [formerly fd445db1bceec6865400950e6dc31b0febf3bf30]]]
    Former-commit-id: adc1aa39d3e8e900e98d4752568df6e7fd137b69 [formerly 6cebf49bbe78e39f1d13b4b31711afbda3c54857]
    Former-commit-id: 77c128eb1e51688bd0b1c0390e30b22f14620b92 [formerly e0d4df3fdcf240946f066c180fef351f826b2a7a]
    Former-commit-id: 47d74645af5be41efb941cefeff03c1c6090531c

---
+++ src/mitll/langtest/server/database/Report.java
@@ -0,0 +1,458 @@
+package mitll.langtest.server.database;
+
+import com.google.gwt.media.client.Audio;
+import mitll.langtest.server.database.instrumentation.EventDAO;
+import mitll.langtest.shared.AudioAttribute;
+import mitll.langtest.shared.Result;
+import mitll.langtest.shared.User;
+import mitll.langtest.shared.instrumentation.Event;
+import org.apache.log4j.Logger;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by go22670 on 10/16/14.
+ */
+public class Report {
+  private static final Logger logger = Logger.getLogger(Report.class);
+  public static final int MIN_MILLIS = (1000 * 60);
+  public static final int SAMPLE = 50;
+  public static final String BEST_AUDIO = "bestAudio";
+  public static final int LENGTH = BEST_AUDIO.length();
+  private final UserDAO userDAO;
+  private final ResultDAO resultDAO;
+  private final EventDAO eventDAO;
+  private final AudioDAO audioDAO;
+
+  public Report(UserDAO userDAO, ResultDAO resultDAO, EventDAO eventDAO, AudioDAO audioDAO) {
+    this.userDAO = userDAO;
+    this.resultDAO = resultDAO;
+    this.eventDAO = eventDAO;
+    this.audioDAO = audioDAO;
+  }
+
+  public String doReport() {
+    List<User> users = userDAO.getUsers();
+
+    Calendar calendar = new GregorianCalendar();
+    Date january1st = getJanuaryFirst(calendar);
+
+    int ytd = 0;
+
+    SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("MM/dd/yy h:mm aaa");
+    Map<Integer, Integer> monthToCount = new TreeMap<Integer, Integer>();
+    Map<Integer, Integer> weekToCount = new TreeMap<Integer, Integer>();
+    for (User user : users) {
+      try {
+        if (user.getTimestamp().isEmpty()) continue;
+        Date parse = simpleDateFormat2.parse(user.getTimestamp());
+        if (parse.getTime() > january1st.getTime()) {
+          ytd++;
+
+          calendar.setTime(parse);
+          int month = calendar.get(Calendar.MONTH);
+          Integer integer = monthToCount.get(month);
+          monthToCount.put(month, (integer == null) ? 1 : integer + 1);
+
+          int w = calendar.get(Calendar.WEEK_OF_YEAR);
+          Integer integer2 = weekToCount.get(w);
+
+          weekToCount.put(w, (integer2 == null) ? 1 : integer2 + 1);
+        } else {
+          //   logger.debug("NO time " +user.getTimestamp() + " " + parse);
+        }
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
+    }
+//    logger.debug("ytd " +ytd);
+//    logger.debug("month " +monthToCount);
+//    logger.debug("week " +weekToCount);
+
+    StringBuilder builder = new StringBuilder();
+    String users1 = "New Users";
+    builder.append("<html><head><body>" +
+            //"<div style=\"width:100%\">" +"<div style=\"position: relative; width:25%\">"+
+            //"\n<table>" +  "<tr><td>"+
+            getYTD(ytd, users1) +
+            //"</td>" +  "<td>\n"+
+            // "</div>"+"<div style=\"position: relative; width:25%\">"+
+            getMC(monthToCount, "month", "New Users") +
+            //"</td><td>"+
+            //"</div>"+"<div style=\"position: relative; width:25%\">"+
+            getWC(weekToCount, "week", "New Users")
+        // +        "</td></tr></table>"
+
+        //    +"</div>"+"</div>"
+    );
+    getResults(builder);
+    getEvents(builder);
+
+    builder.append("</body></head></html>");
+    return builder.toString();
+  }
+
+  private String getYTD(int ytd, String users1) {
+    return "<table>" +
+        "<tr>" +
+        "<th>" +
+        users1 +
+        " YTD</th>" + "</tr>" +
+        "<tr>" +
+        "<td>" + ytd +
+        "</td>" + "</tr>" +
+        "</table><br/>\n";
+  }
+
+/*  private String getMC(Map<Integer, ?> monthToCount, String unit) {
+    String count = "Count";
+    return getMC(monthToCount, unit, count);
+  }*/
+
+  private String getMC(Map<Integer, ?> monthToCount, String unit, String count) {
+    String s = "";
+    for (Map.Entry<Integer, ?> pair : monthToCount.entrySet()) {
+      Object value = pair.getValue();
+      if (value instanceof Collection<?>) {
+        value = ((Collection<?>) value).size();
+      }
+      Integer key = pair.getKey();
+      String month = getMonth(key);
+      s += "<tr><td>" + month + "</td><td>" + value + "</td></tr>";
+    }
+    return "<table>" +
+        "<tr>" +
+        "<th>" +
+        unit +
+        "</th>" +
+        "<th>" + count + "</th>" +
+        "</tr>" +
+        s +
+        "</table><br/>\n";
+  }
+
+/*
+  private String getWC(Map<?, ?> monthToCount, String unit) {
+    String count = "Count";
+    return getWC(monthToCount, unit, count);
+  }
+*/
+
+  private String getWC(Map<?, ?> monthToCount, String unit, String count) {
+    String s = "";
+    for (Map.Entry<?, ?> pair : monthToCount.entrySet()) {
+      Object value = pair.getValue();
+      if (value instanceof Collection<?>) {
+        value = ((Collection<?>) value).size();
+      }
+      s += "<tr><td>" + pair.getKey() + "</td><td>" + value + "</td></tr>";
+    }
+    return "<table>" +
+        "<tr>" +
+        "<th>" +
+        unit +
+        "</th>" +
+        "<th>" + count + "</th>" +
+        "</tr>" +
+        s +
+        "</table><br/>\n";
+  }
+
+  private void getResults(StringBuilder builder) {
+    Calendar calendar = new GregorianCalendar();
+    Date january1st = getJanuaryFirst(calendar);
+
+    int ytd = 0;
+
+    List<Result> results = resultDAO.getResults();
+    Map<Integer, Integer> monthToCount = new TreeMap<Integer, Integer>();
+    Map<Integer, Integer> weekToCount = new TreeMap<Integer, Integer>();
+
+//    Set<String> audioMap = getAudioMap();
+
+    Map<String, List<AudioAttribute>> exToAudio = audioDAO.getExToAudio();
+    logger.debug("found " + exToAudio.size() + " ref audio entries.");
+    if (!exToAudio.isEmpty()) {
+      Iterator<AudioAttribute> iterator = exToAudio.values().iterator().next().iterator();
+      try {
+        for (int i = 0; i < SAMPLE; i++) {
+          logger.debug("e.g. " + iterator.next());
+        }
+      } catch (Exception e) {
+
+      }
+    }
+
+    int refSkip = 0;
+    int c = 0;
+    for (Result result : results) {
+      if (result.getTimestamp() > january1st.getTime()) {
+//        String normalize = normalize2(result.getAnswer()) + "_by_" +result.getUserid();
+  //      if (c++ < SAMPLE) {
+    //      logger.debug("result path "+normalize);
+      //  }
+        boolean skip = isRefAudioResult(exToAudio, result);
+
+        if (skip) {
+          refSkip++;
+        } else {
+          ytd++;
+          calendar.setTimeInMillis(result.getTimestamp());
+          int month = calendar.get(Calendar.MONTH);
+          Integer integer = monthToCount.get(month);
+          monthToCount.put(month, (integer == null) ? 1 : integer + 1);
+
+          int w = calendar.get(Calendar.WEEK_OF_YEAR);
+          Integer integer2 = weekToCount.get(w);
+
+          weekToCount.put(w, (integer2 == null) ? 1 : integer2 + 1);
+        }
+      }
+    }
+    //  logger.debug("ytd " + ytd);
+    //  logger.debug("month " + monthToCount);
+    //  logger.debug("week " + weekToCount);
+    logger.debug("ref " + refSkip);
+    builder.append(getYTD(ytd, "Recordings") +
+        getMC(monthToCount, "month", "Recordings") +
+        getWC(weekToCount, "week", "Recordings"));
+  }
+
+  private boolean isRefAudioResult(Map<String, List<AudioAttribute>> exToAudio, Result result) {
+    boolean skip = false;
+    List<AudioAttribute> audioAttributes = exToAudio.get(result.getExerciseID());
+    for (AudioAttribute audioAttribute : audioAttributes) {
+      if (audioAttribute.getDuration() == result.getDurationInMillis()) {
+        long userid = result.getUserid();
+        if (audioAttribute.getUser().getId() == userid) {
+          skip = true;
+          break;
+        }
+      }
+    }
+    return skip;
+  }
+
+/*
+   private String normalize(String audioRef) {
+    int answer = audioRef.indexOf("regular_");
+    if (answer != -1)
+      audioRef = audioRef.substring(answer + "regular_".length());
+
+    answer = audioRef.indexOf("slow_");
+    if (answer != -1)
+      audioRef = audioRef.substring(answer + "slow_".length());
+    if (audioRef.endsWith(".wav")) {
+      audioRef = audioRef.substring(0, audioRef.length() - 4);
+    }
+
+    return audioRef;
+  }
+
+  private String normalize2(String audioRef) {
+    int answer = audioRef.indexOf("answer_");
+    if (answer != -1) {
+      audioRef = audioRef.substring(answer + "answer_".length());
+    }
+    if (audioRef.endsWith(".wav")) {
+      audioRef = audioRef.substring(0, audioRef.length() - 4);
+    }
+    return audioRef;
+  }
+*/
+
+  private Date getJanuaryFirst(Calendar calendar) {
+    int year = calendar.get(Calendar.YEAR);
+    // logger.debug("year " + year);
+    calendar.set(Calendar.YEAR, year);
+    calendar.set(Calendar.DAY_OF_YEAR, 1);
+    Date january1st = calendar.getTime();
+    //   logger.debug("jan first " + january1st);
+    return january1st;
+  }
+
+
+  private void getEvents(StringBuilder builder) {
+    Calendar calendar = new GregorianCalendar();
+    Date january1st = getJanuaryFirst(calendar);
+
+    int ytd = 0;
+
+    List<Event> all = eventDAO.getAll();
+    Map<Integer, Set<Long>> monthToCount = new TreeMap<Integer, Set<Long>>();
+
+    Map<Integer, Map<Long, Set<Event>>> monthToCount2 = new TreeMap<Integer, Map<Long, Set<Event>>>();
+    Map<Integer, Map<Long, Set<Event>>> weekToCount2 = new TreeMap<Integer, Map<Long, Set<Event>>>();
+
+    Map<Integer, Set<Long>> weekToCount = new TreeMap<Integer, Set<Long>>();
+
+    for (Event event : all) {
+      if (event.getTimestamp() > january1st.getTime()) {
+        ytd++;
+        calendar.setTimeInMillis(event.getTimestamp());
+
+        // months
+        int month = calendar.get(Calendar.MONTH);
+        // String month1 = getMonth(i);
+        long creatorID = event.getCreatorID();
+        Set<Long> users = monthToCount.get(month);
+        if (users == null) {
+          monthToCount.put(month, users = new HashSet<Long>());
+        }
+        users.add(creatorID);
+
+
+        Map<Long, Set<Event>> userToEvents = monthToCount2.get(month);
+        if (userToEvents == null) {
+          monthToCount2.put(month, userToEvents = new HashMap<Long, Set<Event>>());
+        }
+        Set<Event> events = userToEvents.get(creatorID);
+        if (events == null) userToEvents.put(creatorID, events = new TreeSet<Event>());
+        events.add(event);
+
+        // weeks
+
+        int w = calendar.get(Calendar.WEEK_OF_YEAR);
+        Set<Long> users2 = weekToCount.get(w);
+        if (users2 == null) {
+          weekToCount.put(w, users2 = new HashSet<Long>());
+        }
+        users2.add(creatorID);
+
+        userToEvents = weekToCount2.get(w);
+        if (userToEvents == null) {
+          weekToCount2.put(w, userToEvents = new HashMap<Long, Set<Event>>());
+        }
+        events = userToEvents.get(creatorID);
+
+        if (events == null) userToEvents.put(creatorID, events = new TreeSet<Event>());
+        events.add(event);
+      }
+    }
+    //logger.debug("ytd " + ytd);
+
+    // logger.debug("month " + monthToCount);
+    // logger.debug("week " + weekToCount);
+
+    builder.append(//getYTD(ytd, "Active Users") +
+        getMC(monthToCount, "month", "# Active Users") +
+            getWC(weekToCount, "week", "# Active Users"));
+
+    Map<Integer, Long> monthToDur = getMonthToDur(monthToCount2);
+    //  logger.debug("month to dur " + monthToDur);
+    long total = 0;
+    for (Long v : monthToDur.values()) total += v;
+
+    Map<Integer, Long> weekToDur = getWeekToDur(weekToCount2);
+    //logger.debug("week to dur " + weekToDur);
+
+    builder.append(getYTD(Math.round(total / (60)), "Total time on task (hours)") +
+        getMC(monthToDur, "month", "Time on Task Minutes") +
+        getWC(weekToDur, "week", "Time on Task Minutes"));
+  }
+
+  /**
+   * @param monthToCount2
+   * @return in minutes
+   */
+  private Map<Integer, Long> getMonthToDur(Map<Integer, Map<Long, Set<Event>>> monthToCount2) {
+    Map<Integer, Long> monthToDur = new TreeMap<Integer, Long>();
+    for (Map.Entry<Integer, Map<Long, Set<Event>>> monthToUserToEvents : monthToCount2.entrySet()) {
+      Integer month = monthToUserToEvents.getKey();
+      //logger.debug("month " + month);
+
+      Map<Long, Set<Event>> userToEvents = monthToUserToEvents.getValue();
+
+      for (Map.Entry<Long, Set<Event>> eventsForUser : userToEvents.entrySet()) {
+        Long user = eventsForUser.getKey();
+        //logger.debug("\tuser " + user);
+        long start = 0;
+        long dur = 0;
+        // long begin = 0;
+        long last = 0;
+        for (Event event : eventsForUser.getValue()) {
+          long now = event.getTimestamp();
+          //     if (user == INTTEST) {
+          //     logger.debug("Event " + event);
+          //  }
+          if (start == 0) {
+            start = now;
+          } else if (now - last > 1000 * 300) {
+            dur += (last - start) / MIN_MILLIS;
+            start = now;
+          }
+
+          last = now;
+        }
+        dur += (last - start) / MIN_MILLIS;
+//        if (user == INTTEST) {
+        //        logger.debug("dur " + dur);
+        //     }
+        Long aLong = monthToDur.get(month);
+        monthToDur.put(month, aLong == null ? dur : aLong + dur);
+      }
+    }
+    return monthToDur;
+  }
+
+  private Map<Integer, Long> getWeekToDur(Map<Integer, Map<Long, Set<Event>>> weekToCount) {
+    Map<Integer, Long> weekToDur = new TreeMap<Integer, Long>();
+    for (Map.Entry<Integer, Map<Long, Set<Event>>> weekToUserToEvents : weekToCount.entrySet()) {
+      Integer week = weekToUserToEvents.getKey();
+      //logger.debug("week " + week);
+
+      Map<Long, Set<Event>> userToEvents = weekToUserToEvents.getValue();
+
+      for (Map.Entry<Long, Set<Event>> eventsForUser : userToEvents.entrySet()) {
+        Long user = eventsForUser.getKey();
+        //logger.debug("\tuser " + user);
+        long start = 0;
+        long dur = 0;
+        // long begin = 0;
+        long last = 0;
+        for (Event event : eventsForUser.getValue()) {
+          long now = event.getTimestamp();
+   /*       if (user == INTTEST) {
+            logger.debug("Event " + event);
+          }
+      */
+          if (start == 0) {
+            start = now;
+          } else if (now - last > 1000 * 300) {
+            dur += (last - start) / MIN_MILLIS;
+            start = now;
+          }
+
+          last = now;
+        }
+        dur += (last - start) / MIN_MILLIS;
+//        if (user == INTTEST) {
+        //        logger.debug("dur " + dur);
+        //     }
+        Long aLong = weekToDur.get(week);
+        weekToDur.put(week, aLong == null ? dur : aLong + dur);
+      }
+    }
+    return weekToDur;
+  }
+
+
+  private String getMonth(int i) {
+    return Arrays.asList("JANUARY",
+        "FEBRUARY",
+        "MARCH",
+        "APRIL",
+        "MAY",
+        "JUNE",
+        "JULY",
+        "AUGUST",
+        "SEPTEMBER",
+        "OCTOBER",
+        "NOVEMBER",
+        "DECEMBER",
+        "UNDECEMBER").get(i);
+  }
+
+}
