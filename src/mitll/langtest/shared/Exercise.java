8b54be90afd923f2c96ffcc0ea1446b71f064d2a
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2014-10-28 16:29:37 -0400

    Fixed bug where wouldn't show all attached audio in score servlet nested chapters call
    Former-commit-id: 18b25574e7f52fac5aa3522d91656c8e31e2968b [formerly 019e458d46ee62cbefe9daa62105bf64697ed8a5]
    Former-commit-id: 2910601ce4e4dd487c2af00b9d2a95d135fb226d [formerly bb982b034875233c42d090a9efd5339a111bf648]
    Former-commit-id: b9fd11826250cdc263d65cd02d7a60190f360ffb

--- src/mitll/langtest/shared/Exercise.java
+++ src/mitll/langtest/shared/Exercise.java
@@ -20,7 +20,6 @@ public class Exercise extends AudioExercise implements CommonExercise {
   public static final String EN = "en";
   public static final String FL = "fl";
   private static final int MAX_TOOLTIP_LENGTH = 15;
-  //private static final boolean DEBUG = false;
   private String plan;
   private String content;
   private boolean promptInEnglish = true;
