ea270d5c4fa20bf0550932a3c42f40688e89609a
Jennifer Melot <jennifer.melot@ll.mit.edu>
2014-11-12 09:36:46 -0500

    whoops, realized I forgot to turn off hydec directory saving
    Former-commit-id: 0fe1c5de98121b7dfb3c14d4f6f94788fbd282da

--- src/mitll/langtest/server/audio/AudioFileHelper.java
+++ src/mitll/langtest/server/audio/AudioFileHelper.java
@@ -211,8 +211,6 @@ public class AudioFileHelper {
       long answerID = db.addAudioAnswer(user, exerciseID, questionID, file.getPath(),
           isValid, audioType, validity.durationInMillis, answer.isCorrect(), (float) answer.getScore(), recordedWithFlash, deviceType, device, json.toString());
       answer.setResultID(answerID);
-      System.out.println(answerID);
-      System.out.println("THAT WAS THE ID FOLKS");
 
       recordWordAndPhoneInfo(answer, answerID);
     }
