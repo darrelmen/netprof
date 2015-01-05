a5ffb5d3f74e55d342bd42bc6ac2f1e99ec013d7
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-01-05 17:55:39 -0500

    fixed regression where we couldn't delete audio in QA mode, added support for finding phones for an item and the first possible pronunciation and sorting the items by phone
    Former-commit-id: bb55c47410919b899abd9069e2663544668ee269 [formerly 38b02e345420b791d10cbe69ff731bd7edb8dd91] [formerly 008f7c51d6b80ac0034acc216494f19a21778db9 [formerly d54ccbaaa14fc3ad27ddff7a0dca4a16754399d8]] [formerly bb55c47410919b899abd9069e2663544668ee269 [formerly 38b02e345420b791d10cbe69ff731bd7edb8dd91] [formerly 008f7c51d6b80ac0034acc216494f19a21778db9 [formerly d54ccbaaa14fc3ad27ddff7a0dca4a16754399d8]] [formerly e8affaa80c936d2326ff2c7254a647ae9ea00213 [formerly 49635630d4823add9ae139a2f6b2192daba3f4e7] [formerly 738aa233fecba01a4e2b05e9f72a27a3302a0331 [formerly 080fb33423450f78eff3352e27fce358b018490a]]]]
    Former-commit-id: e81375bfe6a50fff437c8098541119087b307fcd [formerly 058cb3a49ecc7a5a0033cf883dddd03567068f93] [formerly da13f81a172a5f26bbb889a78c345599509c65b3 [formerly a932ba3613f15acd563f99f1c0d624e0dd4241ca]] [formerly 94a98c6fc5342814bca62632e3faaa225a08a3b1 [formerly fac1c5b33512bf1c8447e58988d8caf80a6e7f37] [formerly ac6deb1d30bc38ac8e65b5c54dd69d3aefb5ee04 [formerly 07c42669718f71cd0e495e28b34deb9a3fb13dc0]]] [formerly ded252289f3638b219b81d24ec6d473d97d7f3c3 [formerly 1c2e81492afa02ff899cee1a11d9d05d8de03037] [formerly ce93aedfbfc4373b7813ddd28140213829614a2a [formerly e82db1298482e03cd0a495ef6acf4429c0e5c925]] [formerly 1a95d59d5acfcd9649b9e639476fc4a06f663430 [formerly abb480ca2e3d0f25db6313e2d55edfc89411cee5] [formerly 6855bf96cd51b444f64d8903932c7095a5484a00 [formerly 2ea3d39308b132357620096e5eda240d75fec029]]]]
    Former-commit-id: ded252289f3638b219b81d24ec6d473d97d7f3c3 [formerly 1c2e81492afa02ff899cee1a11d9d05d8de03037] [formerly ce93aedfbfc4373b7813ddd28140213829614a2a [formerly e82db1298482e03cd0a495ef6acf4429c0e5c925]]
    Former-commit-id: 7c9c69618b47af8c3c335bc1273a345a91e84985 [formerly aadc7213565bf24351f0239c300ab83f55c62b79] [formerly ca495f747042223de9f884b7aca11142e4cfda7b [formerly 58ae24b81c3c7ee105933b7047950ddbf39c20e3]]
    Former-commit-id: 8193164ffe127cc2c5877499f95157e8dafbb1df [formerly 61e397bad783b33b085a730c011b2dfd098fbb4b]
    Former-commit-id: 691d4920195afd441cc9e14c61def8b3b230a4f9

--- src/mitll/langtest/server/ExerciseSorter.java
+++ src/mitll/langtest/server/ExerciseSorter.java
@@ -5,19 +5,22 @@ import mitll.langtest.shared.CommonShell;
 import mitll.langtest.shared.STATE;
 import mitll.langtest.shared.custom.UserExercise;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by GO22670 on 4/30/2014.
  */
 public class ExerciseSorter {
   private Collection<String> typeOrder;
+  private Map<String, Integer> phoneToCount;
 
   public ExerciseSorter(Collection<String> typeOrder) {
     this.typeOrder = typeOrder;
+  }
+
+  public ExerciseSorter(Collection<String> typeOrder, Map<String, Integer> phoneToCount) {
+    this(typeOrder);
+    this.phoneToCount = phoneToCount;
   }
 
   /**
@@ -54,23 +57,7 @@ public class ExerciseSorter {
             }
 
             // compare first by hierarchical order - unit, then chapter, etc.
-            for (String type : typeOrder) {
-              String type1 = o1.getUnitToValue().get(type);
-              String type2 = o2.getUnitToValue().get(type);
-              boolean t1Null = type1 == null;
-              boolean t2Null = type2 == null;
-              if (t1Null) {
-                i = t2Null ? 0 : +1;
-              } else if (t2Null) {
-                i = -1;
-              } else {
-                i = type1.compareTo(type2);
-              }
-
-              if (i != 0) {
-                break;
-              }
-            }
+            i = getTypeOrder(o1, o2, i);
 
             // items in same chapter alphabetical by tooltip
             return (i == 0) ? tooltipComp(o1, o2) : i;
@@ -82,11 +69,156 @@ public class ExerciseSorter {
     }
   }
 
+  public void getSortedByUnitThenPhone(List<? extends CommonExercise> toSort, final boolean recordedLast,
+                                       final Map<String, Integer> phoneToCount, final boolean checkUnitAndChapter) {
+    if (typeOrder.isEmpty()) {
+      sortByTooltip(toSort);
+    } else {
+      boolean allPredef = true;
+      // first check if we have a homogenous predefined exercise list
+      for (CommonExercise commonExercise : toSort) {
+        if (!new UserExercise(commonExercise).isPredefined()) {
+          allPredef = false;
+          break;
+        }
+      }
+      if (!allPredef) System.out.println("not all predef\n\n");
+      else {
+        //System.out.println("phoneToCount " + phoneToCount);
+      }
+      if (allPredef) {
+        Collections.sort(toSort, new Comparator<CommonExercise>() {
+          @Override
+          public int compare(CommonExercise o1, CommonExercise o2) {
+            int i = 0;
+            if (recordedLast) {
+              if (o1.getState() != STATE.RECORDED && o2.getState() == STATE.RECORDED) {
+                return +1;
+              } else if (o1.getState() == STATE.RECORDED && o2.getState() != STATE.RECORDED) {
+                return -1;
+              }
+            }
+
+            // compare first by hierarchical order - unit, then chapter, etc.
+            if (checkUnitAndChapter) {
+              i = getTypeOrder(o1, o2, i);
+            }
+
+            // items in same chapter alphabetical by tooltip
+            return (i == 0) ? phoneCompFirst(o1, o2, phoneToCount) : i;
+          }
+        });
+      } else {
+        sortByTooltip(toSort);
+      }
+    }
+  }
+
+  public int phoneCompByFirst(CommonExercise o1, CommonExercise o2) {
+    return phoneCompFirst(o1,o2,phoneToCount);
+  }
+  private int phoneCompFirst(CommonExercise o1, CommonExercise o2, final Map<String, Integer> phoneToCount) {
+    List<String> bagOfPhones1 = o1.getFirstPron();
+    List<String> bagOfPhones2 = o2.getFirstPron();
+    if (bagOfPhones1 == null && bagOfPhones2 != null) return +1;
+    if (bagOfPhones1 != null && bagOfPhones2 == null) return -1;
+    if (bagOfPhones1 == null && bagOfPhones2 == null) {
+      return o1.getForeignLanguage().toLowerCase().compareTo(o2.getForeignLanguage().toLowerCase());
+    }
+//    if (bagOfPhones2 == null) return +1;
+
+    int o1Num = bagOfPhones1.size();
+    int o2Num = bagOfPhones2.size();
+
+    //  if (o1Num < o2Num) return -1;
+    //  else if (o1Num > o2Num) return +1;
+    //  else {
+
+    int n = Math.min(o1Num, o2Num);
+    for (int i = 0; i < n; i++) {
+      String a = bagOfPhones1.get(i);
+      String b = bagOfPhones2.get(i);
+      if (!a.equals(b)) {
+        Integer a1 = phoneToCount.get(a);
+        Integer b1 = phoneToCount.get(b);
+        if (a1 > b1) return -1;
+        else if (a1 < b1) return +1;
+        else return a.compareTo(b);
+      }
+    }
+
+    if (o1Num < o2Num) return -1;
+    else if (o1Num > o2Num) return +1;
+    else {
+      return o1.getForeignLanguage().toLowerCase().compareTo(o2.getForeignLanguage().toLowerCase());
+    }
+  }
+
+  private int phoneComp(CommonExercise o1, CommonExercise o2, final Map<String, Integer> phoneToCount) {
+    Set<String> bagOfPhones1 = o1.getBagOfPhones();
+    int o1Num = bagOfPhones1.size();
+    Set<String> bagOfPhones2 = o2.getBagOfPhones();
+    int o2Num = bagOfPhones2.size();
+    if (o1Num < o2Num) return -1;
+    else if (o1Num > o2Num) return +1;
+    else {
+//        // e.g. a b c vs a b d or x y z
+      List<String> seq1 = new ArrayList<String>(bagOfPhones1);
+      Comparator<String> c = new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+          return phoneToCount.get(o2).compareTo(phoneToCount.get(o1));
+        }
+      };
+      Collections.sort(seq1, c);
+
+      List<String> seq2 = new ArrayList<String>(bagOfPhones2);
+      Collections.sort(seq2, c);
+
+      for (int i = 0; i < seq1.size(); i++) {
+        String a = seq1.get(i);
+        String b = seq2.get(i);
+        if (a.equals(b)) {
+          // next
+        } else {
+          Integer a1 = phoneToCount.get(a);
+          Integer b1 = phoneToCount.get(b);
+          if (a1 > b1) return -1;
+          else if (a1 < b1) return +1;
+        }
+      }
+
+      return o1.getForeignLanguage().toLowerCase().compareTo(o2.getForeignLanguage().toLowerCase());
+    }
+  }
+
+  private int getTypeOrder(CommonExercise o1, CommonExercise o2, int i) {
+    for (String type : typeOrder) {
+      String type1 = o1.getUnitToValue().get(type);
+      String type2 = o2.getUnitToValue().get(type);
+      boolean t1Null = type1 == null;
+      boolean t2Null = type2 == null;
+      if (t1Null) {
+        i = t2Null ? 0 : +1;
+      } else if (t2Null) {
+        i = -1;
+      } else {
+        i = type1.compareTo(type2);
+      }
+
+      if (i != 0) {
+        break;
+      }
+    }
+    return i;
+  }
+
   /**
    * I.e. by the lexicographic order of the displayed words in the word list
+   *
    * @param exerciseShells
    */
-  public  <T extends CommonShell> void sortByTooltip(List<T> exerciseShells) {
+  public <T extends CommonShell> void sortByTooltip(List<T> exerciseShells) {
     Collections.sort(exerciseShells, new Comparator<T>() {
       @Override
       public int compare(T o1, T o2) {
