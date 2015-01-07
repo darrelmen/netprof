dbf1c88424fdc82cda3a793806451e92fb9e96b4
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-01-06 19:48:45 -0500

    explicitly uses collators tied to languages and dialects to sort strings instead of String.compare
    Former-commit-id: 0a64d299ef1a20318202fd25951a98a67ed3bb2a
    Former-commit-id: 805d2a50085c052466210fbb8c3ea2e3a4557ff2 [formerly 98d158ed3a80e74429e9ecc73f5b03705cbdee7f]
    Former-commit-id: 60c6f06e089837c8bbe430ed6b5efbc558d436a0

---
+++ src/mitll/langtest/server/scoring/CollationSort.java
@@ -0,0 +1,15 @@
+package mitll.langtest.server.scoring;
+
+import mitll.langtest.shared.CommonExercise;
+
+import java.text.Collator;
+import java.util.List;
+
+/**
+ * Created by go22670 on 1/6/15.
+ */
+public interface CollationSort {
+  Collator getCollator();
+
+  <T extends CommonExercise> void sort(List<T> toSort);
+}
