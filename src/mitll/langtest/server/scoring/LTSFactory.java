00a34a7fe001a34d35730f0cae5b7016a34a8c2b
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-01-06 19:48:45 -0500

    explicitly uses collators tied to languages and dialects to sort strings instead of String.compare
    Former-commit-id: d59af402502300adb834e07a090f7703af5ef205 [formerly f0f14fd34b0cb09287932f49596053b76b1e6792]
    Former-commit-id: e6de5054d38c411b2c6de487eb7b02902f068741

--- src/mitll/langtest/server/scoring/LTSFactory.java
+++ src/mitll/langtest/server/scoring/LTSFactory.java
@@ -1,20 +1,12 @@
 package mitll.langtest.server.scoring;
 
-import corpus.DariLTS;
-import corpus.EnglishLTS;
-import corpus.FarsiLTS;
-import corpus.EgyptianLTS;
-import corpus.LTS;
-import corpus.LevantineLTS;
-import corpus.ModernStandardArabicLTS;
-import corpus.PashtoLTS;
-import corpus.UrduLTS;
-import corpus.SudaneseLTS;
-import corpus.EmptyLTS;
+import corpus.*;
+import mitll.langtest.shared.CommonExercise;
 import org.apache.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.CollationKey;
+import java.text.Collator;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,44 +15,128 @@ import java.util.Map;
  * Time: 4:54 PM
  * To change this template use File | Settings | File Templates.
  */
-public class LTSFactory {
+public class LTSFactory implements CollationSort {
   private static final Logger logger = Logger.getLogger(LTSFactory.class);
 
+  private Language thisLanguage;
   // known languages
-  private static final String ARABIC = "Arabic";
-  private static final String DARI = "Dari";
-  private static final String EGYPTIAN = "Egyptian";
-  private static final String ENGLISH = "English";
-  private static final String FARSI = "Farsi";
-  private static final String LEVANTINE = "Levantine";
-  private static final String MANDARIN = "Mandarin";
-  private static final String MSA = "MSA";
-  private static final String PASHTO = "Pashto";
-  private static final String URDU = "Urdu";
-  private static final String SUDANESE = "Sudanese";
+
+  public enum Language {
+    ARABIC, DARI, EGYPTIAN, ENGLISH, FARSI, JAPANESE, LEVANTINE, KOREAN, MANDARIN, MSA, PASHTO, RUSSIAN, SPANISH, SUDANESE, URDU
+  }
   // TODO : what about Japanese, Korean, ...?
 
-  private static final Map<String,LTS> languageToLTS = new HashMap<String,LTS>();
-  private static final LTS unknown = new EmptyLTS();
+  private final Map<String, LTS> languageToLTS = new HashMap<String, LTS>();
+  private final LTS unknown = new EmptyLTS();
 
-  public LTSFactory() {
-    languageToLTS.put(ARABIC.toLowerCase(), new ModernStandardArabicLTS());
-    languageToLTS.put(DARI.toLowerCase(), new DariLTS());
-    languageToLTS.put(EGYPTIAN.toLowerCase(), new EgyptianLTS());
-    languageToLTS.put(ENGLISH.toLowerCase(),new EnglishLTS());
-    languageToLTS.put(FARSI.toLowerCase(), new FarsiLTS());
-    languageToLTS.put(LEVANTINE.toLowerCase(), new LevantineLTS());
-    languageToLTS.put(MANDARIN.toLowerCase(), unknown);
-    languageToLTS.put(MSA.toLowerCase(), new ModernStandardArabicLTS());
-    languageToLTS.put(PASHTO.toLowerCase(), new PashtoLTS());
-    languageToLTS.put(URDU.toLowerCase(), new UrduLTS());
-    languageToLTS.put(SUDANESE.toLowerCase(), new SudaneseLTS());
+  /**
+   * @param thisLanguage
+   * @see ASRScoring#ASRScoring
+   */
+  public LTSFactory(Language thisLanguage) {
+    this.thisLanguage = thisLanguage;
+    languageToLTS.put(Language.ARABIC.name().toLowerCase(), new ModernStandardArabicLTS());
+    languageToLTS.put(Language.DARI.name().toLowerCase(), new DariLTS());
+    languageToLTS.put(Language.EGYPTIAN.name().toLowerCase(), new EgyptianLTS());
+    languageToLTS.put(Language.ENGLISH.name().toLowerCase(), new EnglishLTS());
+    languageToLTS.put(Language.FARSI.name().toLowerCase(), new FarsiLTS());
+    // languageToLTS.put(Language.JAPANESE.name().toLowerCase(), new LevantineLTS());
+    languageToLTS.put(Language.LEVANTINE.name().toLowerCase(), new LevantineLTS());
+    languageToLTS.put(Language.MANDARIN.name().toLowerCase(), unknown);
+    languageToLTS.put(Language.MSA.name().toLowerCase(), new ModernStandardArabicLTS());
+    languageToLTS.put(Language.PASHTO.name().toLowerCase(), new PashtoLTS());
+    languageToLTS.put(Language.URDU.name().toLowerCase(), new UrduLTS());
+    languageToLTS.put(Language.SUDANESE.name().toLowerCase(), new SudaneseLTS());
+    for (Language lang : Language.values()) {
+      getLocale(lang);
+    }
+  }
+
+  public LTSFactory(String thisLanguage) {
+    this(Language.valueOf(thisLanguage.toUpperCase()));
+  }
+
+  @Override
+  public Collator getCollator() {
+    return Collator.getInstance(getLocale(thisLanguage));
+  }
+
+  public <T extends CommonExercise> void sort(List<T> toSort) {
+    Collator collator = getCollator();
+    final Map<T, CollationKey> exToKey = new HashMap<T, CollationKey>();
+    for (T t : toSort) {
+      CollationKey collationKey = collator.getCollationKey(t.getForeignLanguage());
+      exToKey.put(t, collationKey);
+    }
+
+    Collections.sort(toSort, new Comparator<T>() {
+      @Override
+      public int compare(T o1, T o2) {
+        CollationKey collationKey1 = exToKey.get(o1);
+        CollationKey collationKey2 = exToKey.get(o2);
+        return collationKey1.compareTo(collationKey2);
+      }
+    });
+  }
+
+  public Locale getLocale(Language lang) {
+    Locale locale = Locale.ENGLISH;
+    switch (lang) {
+      case ARABIC:
+        locale = new Locale.Builder().setLanguage("ar").build();
+        break;
+      case DARI:
+        locale = new Locale.Builder().setLanguage("fa").setRegion("pk").build();
+        break;
+      case EGYPTIAN:
+        locale = new Locale.Builder().setLanguage("ar").setRegion("eg").build();
+        break;
+      case FARSI:
+        locale = new Locale.Builder().setLanguage("fa").setRegion("ir").build();
+        break;
+      case KOREAN:
+        locale = Locale.KOREAN;
+        break;
+      case JAPANESE:
+        locale = Locale.JAPANESE;
+        break;
+      case LEVANTINE:
+        locale = new Locale.Builder().setLanguage("ar").setRegion("sy").build();
+        break;
+      case MANDARIN:
+        locale = Locale.CHINESE;
+        break;
+      case MSA:
+        locale = new Locale.Builder().setLanguage("ar").build();
+        break;
+      case PASHTO:
+        locale = new Locale.Builder().setLanguage("ps").build();
+        break;
+      case RUSSIAN:
+        locale = new Locale.Builder().setLanguage("ru").build();
+        break;
+      case SPANISH:
+        locale = new Locale.Builder().setLanguage("es").build();
+        break;
+      case SUDANESE:
+        locale = new Locale.Builder().setLanguage("ar").setRegion("sd").build();
+        break;
+      case URDU:
+        locale = new Locale.Builder().setLanguage("ur").build();
+        break;
+    }
+//
+//    logger.debug("Name of Locale: " + locale.getDisplayName());
+//    logger.debug("Language Code: " + locale.getLanguage() + ", Language Display Name: " + locale.getDisplayLanguage());
+//    logger.debug("Country Code: " + locale.getCountry() + ", Country Display Name: " + locale.getDisplayCountry());
+
+    return locale;
   }
 
   /**
-   * @see mitll.langtest.server.scoring.ASRScoring#ASRScoring
    * @param language
    * @return
+   * @see mitll.langtest.server.scoring.ASRScoring#ASRScoring
    */
   public LTS getLTSClass(String language) {
     LTS letterToSoundClass = languageToLTS.get(language.toLowerCase());
