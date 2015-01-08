30fc4f9aeb7e691c42aadd5fe979375d7201138e
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-01-08 11:41:27 -0500

    fixed bug where main panel wouldn't resize
    Former-commit-id: 38c0fa486e5691c63764aacdb1c4ceb1cec3df82 [formerly 38c0fa486e5691c63764aacdb1c4ceb1cec3df82 [formerly 70be779d32a8c581de4a4128da7e63c59f748582]]
    Former-commit-id: f78c14e2bdb751b1b790b3bbebf53440f9b47cf3 [formerly a8d0d275e21cda9b20fbb5acaeb9e088c1cee513] [formerly 16729108a4965ad2ccb35c23f06d796f551098fa [formerly ea110bb289e45cc35bfa7ea6b96956281343bdf2]]
    Former-commit-id: 16729108a4965ad2ccb35c23f06d796f551098fa
    Former-commit-id: 58892be9bd2f803877e821ae9fb11f614fe6e57e

--- src/mitll/langtest/client/custom/content/ReviewItemHelper.java
+++ src/mitll/langtest/client/custom/content/ReviewItemHelper.java
@@ -1,7 +1,5 @@
 package mitll.langtest.client.custom.content;
 
-import com.github.gwtbootstrap.client.ui.event.HiddenEvent;
-import com.github.gwtbootstrap.client.ui.event.HiddenHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -11,7 +9,6 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.bootstrap.FlexSectionExerciseList;
 import mitll.langtest.client.custom.dialog.ReviewEditableExercise;
-import mitll.langtest.client.dialog.ModalInfoDialog;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.client.exercise.ExercisePanelFactory;
 import mitll.langtest.client.exercise.PagingContainer;
@@ -20,21 +17,14 @@ import mitll.langtest.client.list.PagingExerciseList;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.client.user.UserManager;
 import mitll.langtest.shared.CommonExercise;
-import mitll.langtest.shared.CommonShell;
-import mitll.langtest.shared.ExerciseAnnotation;
 import mitll.langtest.shared.custom.UserExercise;
 import mitll.langtest.shared.custom.UserList;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
 /**
  * Created by GO22670 on 3/26/2014.
  */
 public class ReviewItemHelper extends NPFHelper {
-  private Logger logger = Logger.getLogger("ReviewItemHelper");
+  //private Logger logger = Logger.getLogger("ReviewItemHelper");
 
   private FlexListLayout flexListLayout;
   private final HasText itemMarker;
@@ -57,55 +47,7 @@ public class ReviewItemHelper extends NPFHelper {
     this.itemMarker = null;
     this.predefinedContent = predefinedContent;
     this.npfHelper = npfHelper;
-    logger.info("making ReviewItemHelper --->");
   }
-
-/*  @Override
-  PagingExerciseList makeExerciseList(Panel right, String instanceName) {
-    // return super.makeExerciseList(right, instanceName);
-    logger.info("making ReviewItemHelper.makeExerciseList --->");
-
-    return new PagingExerciseList(right, service, feedback, null, controller,
-        true, instanceName, false) {
-      @Override
-      protected void onLastItem() {
-        new ModalInfoDialog(COMPLETE, LIST_COMPLETE, new HiddenHandler() {
-          @Override
-          public void onHidden(HiddenEvent hiddenEvent) {
-            reloadExercises();
-          }
-        });
-      }
-
-      @Override
-      protected void addTableWithPager(PagingContainer pagingContainer) {
-
-        logger.info("doing new makeExerciseList.addTableWithPager --->");
-        // row 1
-        Panel column = new FlowPanel();
-        add(column);
-        addTypeAhead(column);
-
-        // row 2
-        final com.github.gwtbootstrap.client.ui.CheckBox w = new com.github.gwtbootstrap.client.ui.CheckBox("Only with audio defects");
-        w.addClickHandler(new ClickHandler() {
-          @Override
-          public void onClick(ClickEvent event) {
-            logger.info("got click " + w.getValue());
-            //  setUnrecorded(w.getValue());
-            //  scheduleWaitTimer();
-            //  loadExercises(getHistoryToken(""), getTypeAheadText());
-          }
-        });
-        w.addStyleName("leftFiveMargin");
-        add(w);
-
-        // row 3
-        add(pagingContainer.getTableWithPager());
-        //setOnlyExamples(!doNormalRecording);
-      }
-    };
-  }*/
 
   /**
    * Left and right components
@@ -117,8 +59,7 @@ public class ReviewItemHelper extends NPFHelper {
    * @see #doNPF(mitll.langtest.shared.custom.UserList, String, boolean)
    */
   protected Panel doInternalLayout(final UserList ul, String instanceName) {
-
-    logger.info(getClass() + " : doInternalLayout instanceName = " + instanceName + " for list " + ul);
+//    logger.info(getClass() + " : doInternalLayout instanceName = " + instanceName + " for list " + ul);
 
     this.flexListLayout = new FlexListLayout(service,feedback,userManager,controller) {
       @Override
@@ -143,15 +84,10 @@ public class ReviewItemHelper extends NPFHelper {
 
       @Override
       protected FlexSectionExerciseList makeExerciseList(Panel topRow, Panel currentExercisePanel, String instanceName, boolean incorrectFirst) {
-        //return super.makeExerciseList(topRow, currentExercisePanel, instanceName, incorrectFirst);
-
         return new MyFlexSectionExerciseList(topRow, currentExercisePanel, instanceName, incorrectFirst) {
           com.github.gwtbootstrap.client.ui.CheckBox onlyAudio;
           @Override
           protected void addTableWithPager(PagingContainer pagingContainer) {
-            //super.addTableWithPager(pagingContainer);
-
-            logger.info("doing new makeExerciseList.addTableWithPager --->");
             // row 1
             Panel column = new FlowPanel();
             add(column);
@@ -163,11 +99,7 @@ public class ReviewItemHelper extends NPFHelper {
             w.addClickHandler(new ClickHandler() {
               @Override
               public void onClick(ClickEvent event) {
-                logger.info("got click " + w.getValue());
-                //  setUnrecorded(w.getValue());
-                //  scheduleWaitTimer();
                   loadExercises(getHistoryToken(""), getTypeAheadText(),w.getValue());
-                //reload();
               }
             });
             w.addStyleName("leftFiveMargin");
