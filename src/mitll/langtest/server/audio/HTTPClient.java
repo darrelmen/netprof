749d7f653c94812c73af4fb77681d5c5e0db829e
Jessica Ray <jessica.ray@ll.mit.edu>
2015-02-12 11:43:01 -0500

    got webservice things working with the big kinks worked out
    Former-commit-id: f7a038c356d945703b850f9fd58444245c79beff [formerly b2b8b24c3849ad5481d07354bc05512455a4c6db] [formerly f7a038c356d945703b850f9fd58444245c79beff [formerly b2b8b24c3849ad5481d07354bc05512455a4c6db] [formerly f7a038c356d945703b850f9fd58444245c79beff [formerly b2b8b24c3849ad5481d07354bc05512455a4c6db] [formerly 2841d531807b67d6bcc0a36919a54f8b82dbb6ee]]]
    Former-commit-id: 7c8e9c5f673c2548d004cd25293aa2bb162364ce [formerly 6ab1a0c33e2d76d230efa4e58d265f66b78e5425] [formerly 7c8e9c5f673c2548d004cd25293aa2bb162364ce [formerly 6ab1a0c33e2d76d230efa4e58d265f66b78e5425] [formerly 67323d86e8e7949e5dfd3ed8aadf3c6fa342d214]] [formerly f2ea17d7244e1645ca166ddee8be4a7100e122b9 [formerly 70e441b22d485916900fe577ca87f9ed678068d8] [formerly f2ea17d7244e1645ca166ddee8be4a7100e122b9 [formerly 70e441b22d485916900fe577ca87f9ed678068d8] [formerly 94d84d27953c976bdbe80896ae2808ef561f51d5]]]
    Former-commit-id: f2ea17d7244e1645ca166ddee8be4a7100e122b9 [formerly 70e441b22d485916900fe577ca87f9ed678068d8]
    Former-commit-id: dd110025d6a2fc43a58a431a55b56e5f5737ace1 [formerly a3ef29c18c6be90a50c1b97c4042b8dd60fb2e5d] [formerly d2bfc9fb987daa9193fc088577c2fcc1261a2658]
    Former-commit-id: f0ffc9349baa7a46ec7e525fa81299794adf8c64
    Former-commit-id: 9bee4f6fbf429de797896ad22b5c7dc58b04d520

--- src/mitll/langtest/server/audio/HTTPClient.java
+++ src/mitll/langtest/server/audio/HTTPClient.java
@@ -3,94 +3,99 @@ package mitll.langtest.server.audio;
 import java.net.*;
 import java.io.*;
 
+import mitll.langtest.server.scoring.ASRWebserviceScoring;
+
+import org.apache.log4j.Logger;
+
 // closes and reopens the connection after every call to sendAndReceive()
 public class HTTPClient {
 
-  private HttpURLConnection httpConn;
-  private OutputStreamWriter sender;
-  private BufferedReader receiver;
-  private String webserviceIP;
-  private int webservicePort;
-  private String url;
+	private HttpURLConnection httpConn;
+	private OutputStreamWriter sender;
+	private BufferedReader receiver;
+	private String webserviceIP;
+	private int webservicePort;
+	private String url;
+	private static final Logger logger = Logger.getLogger(HTTPClient.class);
 
-  /* Constructor */
+	/* Constructor */
 
-  public HTTPClient(String webserviceIP, int webservicePort) {
-    this.webserviceIP = webserviceIP;
-    this.webservicePort = webservicePort;
-    this.url = "http://" + webserviceIP + ":" + webservicePort + "/jcodr";
-    try {
-      httpConn = setupHttpConn(url);
-    }
-    catch(IOException e) {
-      logger.error("Error constructing HTTPClient:\n" + e.getStackTrace());
-    }
-  }
+	public HTTPClient(String webserviceIP, int webservicePort) {
+		this.webserviceIP = webserviceIP;
+		this.webservicePort = webservicePort;
+		this.url = "http://" + webserviceIP + ":" + webservicePort + "/jcodr";
+		try {
+			httpConn = setupHttpConn(url);
+		}
+		catch(IOException e) {
+			logger.error("Error constructing HTTPClient:\n" + e.getStackTrace());
+		}
+	}
 
-  /* private methods */
+	/* private methods */
 
-  private HttpURLConnection setupHttpConn(String url) throws IOException {
-    HttpURLConnection httpConn = (HttpURLConnection)(new URL(url)).openConnection();
-    httpConn.setRequestMethod("POST");
-    httpConn.setDoOutput(true);
-    httpConn.setConnectTimeout(20000);
-    httpConn.setReadTimeout(20000);
-    httpConn.setRequestProperty("Content-Type", "application/json");    
-    httpConn.setRequestProperty("Accept-Charset", "UTF-8");
-    httpConn.setRequestProperty("charset", "UTF-8");
-    return httpConn;
-  }
+	private HttpURLConnection setupHttpConn(String url) throws IOException {
+		HttpURLConnection httpConn = (HttpURLConnection)(new URL(url)).openConnection();
+		httpConn.setRequestMethod("POST");
+		httpConn.setDoOutput(true);
+		httpConn.setConnectTimeout(5000);
+		httpConn.setReadTimeout(20000);
+		httpConn.setRequestProperty("Content-Type", "application/json");    
+		httpConn.setRequestProperty("Accept-Charset", "UTF-8");
+		httpConn.setRequestProperty("charset", "UTF-8");
+		return httpConn;
+	}
 
-  private void resetConn() {
-    setupHttpConn(url);
-  }
+	//private void resetConn() throws IOException {
+	//	setupHttpConn(url);
+	//}
 
-  private void closeConn() throws IOException {
-    httpConn.disconnect();
-    httpConn = null;
-  }
+	public void closeConn() throws IOException {
+		httpConn.disconnect();
+		httpConn = null;
+	}
 
-  private void send(String input) throws IOException {
-    sender = new OutputStreamWriter(httpConn.getOutputStream(), "UTF-8");
-    sender.write(input);
-    sender.flush();    
-    sender.close();
-    sender = null;
-  }
+	private void send(String input) throws IOException {
+		sender = new OutputStreamWriter(httpConn.getOutputStream(), "UTF-8");
+		sender.write(input);
+		sender.flush();    
+		sender.close();
+		sender = null;
+	}
 
-  private String receive() throws IPException {
-    receiver = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
-    int code = httpConn.getResponseCode();
-    if(code == 200) {
-      String output = "";
-      String current = "";
-      while((current = receiver.readLine()) != null) {
-        output += output + "\n";
-      }
-      receiver.close();
-      receiver = null;
-      return output;
-    }
-    else {
-      logger.error("Received HTTP Code of: " + code + ". Expecting code 200.");
-      return "";
-    }
-  }
+	private String receive() throws IOException {
+		receiver = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
+		int code = httpConn.getResponseCode();
+		if(code == 200) {
+			String output = "";
+			String current = "";
+			while((current = receiver.readLine()) != null) {
+				output += current + "\n";
+			}
+			receiver.close();
+			receiver = null;
+			return output;
+		}
+		else {
+			logger.error("Received HTTP Code of: " + code + ". Expecting code 200.");
+			return "";
+		}
+	}
 
-  /* public methods */
+	/* public methods */
 
-  public String sendAndReceive(String input) {
-    try {
-      send(input);
-      String response = receive();
-      httpConn.closeConn();
-      resetConn();
-      return response;
-    }
-    catch(IOException e) {
-      logger.error("Error interacting with HTTPClient:\n" + e.getStackTrace());
-      return "";
-    }
-  }
+	public String sendAndReceive(String input) {
+		try {
+			send(input);
+			String response = receive();
+			//closeConn();
+			//resetConn();
+			return response;
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
 
 }
