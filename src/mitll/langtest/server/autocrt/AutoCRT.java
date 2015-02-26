6e018dfed31b9cee0dc70edbd96b7c8a41f19504
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-02-25 19:00:42 -0500

    code cleanup
    Former-commit-id: cc8ba33f29e11c699e6513af3dfb14b711425950 [formerly 000395b09ece5a48c37e89b27a61cad58c0d748e] [formerly 72259554f66c31da55c1bcf9c9068121fc2f122d]
    Former-commit-id: f753fb0f8d1958256abbd5c14865f2ac9bd12b92 [formerly 35bd83b950aa436eed6a85502f677bc7b3824186] [formerly 1e11a6b0ae172006992c60112636bf2793aba642]
    Former-commit-id: 23430a17684faf87ca4751ec81fc61b5b8a2580f
    Former-commit-id: fd79597da5c734b6ed2a9be99223eafe24b2b98b

--- src/mitll/langtest/server/autocrt/AutoCRT.java
+++ src/mitll/langtest/server/autocrt/AutoCRT.java
@@ -1,9 +1,6 @@
 package mitll.langtest.server.autocrt;
 
-//import ag.experiment.AutoGradeExperiment;
-//import mira.classifier.Classifier;
 import mitll.langtest.server.audio.SLFFile;
-import mitll.langtest.server.database.Export;
 import mitll.langtest.server.scoring.AutoCRTScoring;
 import mitll.langtest.server.scoring.SmallVocabDecoder;
 import mitll.langtest.shared.AudioAnswer;
@@ -13,7 +10,6 @@ import mitll.langtest.server.scoring.ASRScoring;
 import org.apache.log4j.Logger;
 
 import java.io.File;
-//import java.text.DecimalFormat;
 import java.util.*;
 
 /**
@@ -27,115 +23,19 @@ import java.util.*;
  */
 public class AutoCRT {
   private static final Logger logger = Logger.getLogger(AutoCRT.class);
-//  private static final String SERIALIZED_CLASSIFIER = "serializedClassifier.ser";
-
- // private static final double CORRECT_THRESHOLD = 0.499;
- // private static final boolean GET_MSA = false;
- // private static final boolean TESTING = false; // this doesn't really work
-//  private Classifier<AutoGradeExperiment.Event> classifier = null;
-//  private Map<String, Export.ExerciseExport> exerciseIDToExport;
- // private final String installPath;
- // private final String mediaDir;
-//  private final Export exporter;
   private final AutoCRTScoring autoCRTScoring;
   private final double minPronScore;
+  private final SmallVocabDecoder svd = new SmallVocabDecoder();
 
   /**
    * @see mitll.langtest.server.audio.AudioFileHelper#makeAutoCRT
-   * @param exporter
    * @param db
-   * @param installPath
-   * @param relativeConfigDir
    * @param minPronScore
    */
-  public AutoCRT(Export exporter, AutoCRTScoring db, String installPath, String relativeConfigDir, double minPronScore) {
-    //this.installPath = installPath;
-    //this.mediaDir = relativeConfigDir;
- //   this.exporter = exporter;
+  public AutoCRT(AutoCRTScoring db, double minPronScore) {
     this.autoCRTScoring = db;
     this.minPronScore = minPronScore;
   }
-
-  /**
-   * Get an auto crt reco output and score given an audio answer.
-   *
-   * On this branch this isn't used - see newAutoCRT
-   *
-   * @seex mitll.langtest.server.LangTestDatabaseImpl#writeAudioFile(String, String, String, int, int, int, boolean, String, boolean)
-   * @seex mitll.langtest.server.audio.AudioFileHelper#getAudioAnswer(String, int, int, int, java.io.File, mitll.langtest.server.audio.AudioCheck.ValidityAndDur, String, boolean, mitll.langtest.client.LangTestDatabase)
-   * @param exerciseID for this exercise
-   * @param questionID and this question
-   * @paramx audioFile score this file (
-   * @param answer mark decode output, correctness, and whether they said something in the set of expected sentences
-   */
-/*  public void getAutoCRTDecodeOutput(String exerciseID, int questionID, File audioFile, AudioAnswer answer) {
-    PretestScore asrScoreForAudio = getScoreForAudio(exerciseID, questionID, audioFile);
-    markCorrectnessOnAnswer(exerciseID, questionID, asrScoreForAudio, answer);
-  }*/
-
-/*  private void markCorrectnessOnAnswer(String exerciseID, int questionID, PretestScore asrScoreForAudio, AudioAnswer answer) {
-    String recoSentence = asrScoreForAudio.getRecoSentence();
-    boolean lowPronScore = asrScoreForAudio.getHydecScore() < minPronScore;
-    boolean matchedUnknown = recoSentence.equals(SLFFile.UNKNOWN_MODEL);
-
-    logger.info("markCorrectnessOnAnswer: took " + //(now - then) + " millis to get score "+
-      " for " + exerciseID + "/" +questionID+
-      //" given " +exportedAnswers.size() + " possible matches," +
-      " reco sentence was '" + recoSentence + "', score " + asrScoreForAudio.getHydecScore() +
-      (lowPronScore ? " score too low " : "") +
-      (matchedUnknown ? " matched unknown model " : ""));
-
-    if (matchedUnknown || lowPronScore) {
-      //if (lowPronScore) logger.info("\t-----------> rejecting result since score too low");
-      answer.setDecodeOutput("Unexpected word.");
-      answer.setScore(asrScoreForAudio.getHydecScore());
-      answer.setCorrect(false);
-      answer.setSaidAnswer(false);
-    } else {
-      String annotatedResponse = getAnnotatedResponse(exerciseID, questionID, recoSentence);
-
-      double scoreForAnswer = (recoSentence.length() > 0) ? getScoreForExercise(exerciseID, questionID, recoSentence) : 0.0d;
-
-      answer.setDecodeOutput(annotatedResponse);
-      answer.setScore(scoreForAnswer);
-      boolean correct = scoreForAnswer > CORRECT_THRESHOLD;
-
-      logger.info("markCorrectnessOnAnswer: for " + exerciseID +
-        " reco sentence was '" + recoSentence + "' classifier score "+scoreForAnswer +
-        " correct " + correct + " matched answer is "+ !matchedUnknown);
-      answer.setCorrect(correct);
-      answer.setSaidAnswer(!matchedUnknown);
-    }
-  }*/
-
-  /**
-   * Do decoding on an audio file over a set of possible reco sentences and the UNKNOWN model.
-   * <p></p>
-   * The possible sentences are student responses collected previously (exported answers from the student h2 autoCRTScoring).
-   * Filter out student answers that we can't decode given our dictionary (e.g. numbers "222").
-   *
-   * @see #getAutoCRTDecodeOutput
-   * @see #getExportedAnswers(String, int)
-   * @param exerciseID for this exercise
-   * @param questionID for this question
-   * @param audioFile decode this file
-   * @return the reco sentence and the score for this sentence
-   */
-/*  private PretestScore getScoreForAudio(String exerciseID, int questionID, File audioFile) {
-    Collection<String> exportedAnswers = getExportedAnswers(exerciseID, questionID);
-    exportedAnswers = autoCRTScoring.getValidPhrases(exportedAnswers);   // remove phrases that break hydec
-    //logger.info("getAutoCRTDecodeOutput : got answers, num = " + exportedAnswers.size());
-    long then = System.currentTimeMillis();
-
-    PretestScore asrScoreForAudio = autoCRTScoring.getASRScoreForAudio(audioFile, exportedAnswers);
-    long now = System.currentTimeMillis();
-    if (now-then > 100) {
-      logger.info("getScoreForAudio : took " + (now - then) + " millis to get score " + asrScoreForAudio +
-        " given" + exportedAnswers.size() +
-        " answers for exercise " + exerciseID + "/" + questionID);
-    }
-    return asrScoreForAudio;
-  }*/
 
   /**
    * Decode the phrase from the exercise in {@link mitll.langtest.shared.CommonExercise#getForeignLanguage}
@@ -170,7 +70,6 @@ public class AutoCRT {
 
   /**
    * @see mitll.langtest.server.audio.AudioFileHelper#getFlashcardAnswer(java.io.File, String)
-   * @see mitll.langtest.server.ScoreServlet#getFlashcardScore
    *
    * @param audioFile
    * @param foregroundSentence
@@ -226,15 +125,6 @@ public class AutoCRT {
     return asrScoreForAudio;
   }
 
-  private List<String> removePunct(Collection<String> possibleSentences) {
-    List<String> foreground = new ArrayList<String>();
-    for (String ref : possibleSentences) {
-      foreground.add(removePunct(ref));
-    }
-    return foreground;
-  }
-
-  private final SmallVocabDecoder svd = new SmallVocabDecoder();
   /**
    * Convert dashes into spaces and remove periods, and other punct
    * @param answerSentences
@@ -288,80 +178,13 @@ public class AutoCRT {
         rawRefSentence.trim().toUpperCase();
   }
 
-  /**
-   * Mark words in the response as right or wrong depending on their overlap with answers marked
-   * good or bad.
-   * Mainly for demo purposes.
-   * @param exercise
-   * @param questionID
-   * @param recoSentence
-   * @return to show to user
-   */
-/*  private String getAnnotatedResponse(String exercise, int questionID, String recoSentence) {
-    List<String> good = new ArrayList<String>();
-    List<String> bad = new ArrayList<String>();
-    for (Export.ResponseAndGrade resp : getExportForExercise(exercise, questionID).rgs) {
-      if (resp.grade >= 0.6) good.add(resp.response);
-      else bad.add(resp.response);
+  private List<String> removePunct(Collection<String> possibleSentences) {
+    List<String> foreground = new ArrayList<String>();
+    for (String ref : possibleSentences) {
+      foreground.add(removePunct(ref));
     }
-
-    Set<String> goodTokens = getTokenSet(good);
-    Set<String> badTokens = getTokenSet(bad);
-
-    StringBuilder sb = new StringBuilder();
-    for (String recoToken : recoSentence.split("\\s")) {
-      if (goodTokens.contains(recoToken)) {
-        sb.append("<u>" + recoToken + "</u> ");
-      } else if (badTokens.contains(recoToken)) {
-        sb.append("<s>"+recoToken +"</s> ");
-      } else sb.append(recoToken + " ");
-    }
-    return sb.toString().trim();
-  }*/
-
-  /**
-   * For this exercise and question, score the answer.<br></br>
-   * Do this by getting all other answers to this question and the answer key and given this information
-   * and the answer, ask the classifier to score the answer.
-   * @seex mitll.langtest.server.audio.AudioFileHelper#getScoreForAnswer(mitll.langtest.shared.Exercise, int, String)
-   * @seex mitll.langtest.server.LangTestDatabaseImpl#getScoreForAnswer
-   * @paramx e for this exercise
-   * @paramx questionID for this question (when multiple questions in an exercise)
-   * @paramx answer to score (correct->incorrect)
-   * @return 0-1
-   */
-/*  private double getScoreForExercise(String id, int questionID, String answer) {
-    if (TESTING) return 0.1;
-    getClassifier();
-    String key = id + "_" + questionID;
-    Export.ExerciseExport exerciseExport = getExportForExercise(key);
-    if (exerciseExport == null) {
-      logger.error("couldn't find exercise id " + key + " in " + exerciseIDToExport.keySet());
-      return 0d;
-    }
-    else {
-      long then = System.currentTimeMillis();
-
-      double score = AutoGradeExperiment.getScore(getClassifier(), answer, exerciseExport);
-      long now = System.currentTimeMillis();
-
-      DecimalFormat format = new DecimalFormat("#.###");
-      logger.info("AutoGradeExperiment : score was " + format.format(score) + " for answer '" +  answer+
-        "' in context of " + exerciseExport +" and took " +(now-then) + " millis");
-      return score;
-    }
-  }*/
-
-/*  private Set<String> getTokenSet(List<String> exportedAnswers) {
-    Set<String> tokens = new HashSet<String>();
-    for (String l : exportedAnswers) {
-      for (String t : l.split("\\s")) {
-        String tt = removePunct(t);
-        if (tt.trim().length() > 0) {
-          tokens.add(tt.trim());
-        }}}
-    return tokens;
-  }*/
+    return foreground;
+  }
 
   /**
    * Replace elipsis with space. Then remove all punct.
@@ -371,151 +194,4 @@ public class AutoCRT {
   private String removePunct(String t) {
     return t.replaceAll("\\.\\.\\."," ").replaceAll("\\p{P}","");
   }
-
-  /**
-   * @seex #getAutoCRTDecodeOutput
-   * @param id
-   * @param questionID
-   * @return
-   */
-//  private Collection<String> getExportedAnswers(String id, int questionID) {
-//    getClassifier();
-//    long then = System.currentTimeMillis();
-//
-//    Set<String> answers = new TreeSet<String>();
-//    Export.ExerciseExport exportForExercise = getExportForExercise(id, questionID);
-//    if (exportForExercise != null) {
-//      for (Export.ResponseAndGrade resp : exportForExercise.rgs) {
-//        answers.add(resp.response);
-//      }
-//    }
-//
-//    long now = System.currentTimeMillis();
-//    if (now-then > 100) {
-//      logger.info("getExportedAnswers : took " + (now - then) + " millis to get " + answers.size() +
-//        " possible answers for exercise " + id);
-//    }
-//    return answers;
-//  }
-
-/*
-  private Export.ExerciseExport getExportForExercise(String id, int questionID) {
-    return getExportForExercise(id + "_" + questionID);
-  }
-  private Export.ExerciseExport getExportForExercise(String key) {
-    return exerciseIDToExport.get(key);
-  }
-*/
-
- // public void makeClassifier() { getClassifier(); }
-
-  /**
-   * Make a classifier given the export date, which has answers and their grades.<br></br>
-   * The export data also includes the answer key for each question.<br></br>
-   * This uses Jacob's classifier.
-   * @seex #getExportedAnswers(String, int)
-   * @see #getScoreForExercise(String, int, String)
-   *
-   * @see Export.ExerciseExport
-   * @see AutoGradeExperiment
-   * @return a mira classifier
-   */
-/*  private Classifier<AutoGradeExperiment.Event> getClassifier() {
-    if (classifier != null) return classifier;
-
-    String configDir = (installPath != null ? installPath + File.separator : "") + mediaDir + File.separator;
-    File serializedClassifier = new File(configDir, SERIALIZED_CLASSIFIER);
-    if (serializedClassifier.exists()) {
-      classifier = rehydrateClassifier(configDir, serializedClassifier);
-      return classifier;
-    } else {
-      if (TESTING) {
-        exerciseIDToExport = new HashMap<String, Export.ExerciseExport>();
-        return null;
-      } else if (GET_MSA) {
-        List<Export.ExerciseExport> export = getExportedGradedItems();
-        AutoGradeExperiment.runOverallModelOnExport(export);
-        return null;
-      } else {
-        List<Export.ExerciseExport> export = getExportedGradedItems();
-        readConfigFile(configDir);
-
-        long then = System.currentTimeMillis();
-        AutoGradeExperiment.saveClassifierAfterExport(export, serializedClassifier.getAbsolutePath());
-        long now = System.currentTimeMillis();
-
-        logger.info("took " + ((now - then) / 1000) + " seconds to saved classifier to " + serializedClassifier.getAbsolutePath());
-        classifier = AutoGradeExperiment.getClassifierFromSavedModel(serializedClassifier.getAbsolutePath(), export);
-        return classifier;
-      }
-    }
-  }*/
-
-  /**
-   * TODO : this is overkill - we should serialize/rehydrate the model without calling the svm load/save code directly
-   * @param configDir
-   * @paramx serializedClassifier
-   * @return
-   */
-/*  private Classifier<AutoGradeExperiment.Event> rehydrateClassifier(String configDir, File serializedClassifier) {
-    logger.info("rehydrateClassifier : using previously calculated classifier.");
-    long then = System.currentTimeMillis();
-
-    List<Export.ExerciseExport> export = getExportedGradedItems();
-    long now = System.currentTimeMillis();
-    if (now-then > 100) {
-      logger.info("rehydrateClassifier : took " + (now - then) + " millis to export " + export.size() + " items");
-    }
-    readConfigFile(configDir);
-    then = now;
-    Classifier<AutoGradeExperiment.Event> classifier =
-      AutoGradeExperiment.getClassifierFromSavedModel(serializedClassifier.getAbsolutePath(), export);
-     now = System.currentTimeMillis();
-    long l = (now - then) / 1000;
-    if (l > 60) {
-      long min = l/60;
-      l -= min*60;
-      logger.warn("rehydrateClassifier : took " + min + " minutes, " + l + " seconds to rehydrate classifier");
-    }
-    else if (l > 1) {
-      logger.debug("rehydrateClassifier : took " + l + " seconds to rehydrate classifier");
-    }
-    return classifier;
-  }*/
-
-/*  private void readConfigFile(String configDir) {
-    String[] args = new String[6];
-
-    String config = configDir + "runAutoGradeWinNoBad.cfg";     // TODO use template for deploy/platform specific config
-    if (!new File(config).exists()) logger.error("readConfigFile : couldn't find " + config);
-    args[0] = "-C";
-    args[1] = config;
-    args[2] = "-log";
-    args[3] = configDir + "out.log";
-    args[4] = "-blacklist-file";
-    args[5] = configDir + "blacklist.txt";
-
-    AutoGradeExperiment.main(args);
-  }
-
-  private List<Export.ExerciseExport> getExportedGradedItems() {
-    long then = System.currentTimeMillis();
-
-    List<Export.ExerciseExport> export = exporter.getExport(true, false);
-    populateExportIdToExport(export);
-    long now = System.currentTimeMillis();
-
-    if (now - then > 100) {
-      logger.debug("getExportedGradedItems took " + (now - then) + " millis to get " + export.size() +
-        " exported GradedItems items.");
-    }
-    return export;
-  }
-
-  private void populateExportIdToExport(List<Export.ExerciseExport> export) {
-    exerciseIDToExport = new HashMap<String, Export.ExerciseExport>();
-    for (Export.ExerciseExport exp : export) {
-      exerciseIDToExport.put(exp.id, exp);
-    }
-  }*/
 }
