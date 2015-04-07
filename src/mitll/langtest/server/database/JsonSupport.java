a354c125a4415fa936c85f6ef8452ba9aae572e4
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-04-07 19:45:01 -0400

    returns score history in chapter history so we can show it on the iPad, attempts to fix issue where we can't find entries with an apostrophe on search from the website, refactored json delivery into separate class
    Former-commit-id: fb3b94485222ba7d6f35d5904798705809001b62 [formerly 5f0361cf463d4318195b47b5cca7a6f014f9b3d7] [formerly fb3b94485222ba7d6f35d5904798705809001b62 [formerly 5f0361cf463d4318195b47b5cca7a6f014f9b3d7] [formerly 907964d1f12ba5a2c9cb27f7ac765a6d9c30f0ef]]
    Former-commit-id: 88bb125ea37ea97edde3f3606ce061a21b5305dd [formerly e77dabb76c2dfeeb0736f7f3c908fb932d109460] [formerly e647be6ee35fa610cba341dacf38855b4f766164] [formerly 9eb5872501ec4f2fd0c9f99ef33e9d3bd65f0683 [formerly 99740921cb9cc4af844d29398cee823d87e75f20] [formerly e2985ecf4a5f58b46e683991a21992f4aa465ae8]]
    Former-commit-id: 9eb5872501ec4f2fd0c9f99ef33e9d3bd65f0683 [formerly 99740921cb9cc4af844d29398cee823d87e75f20]
    Former-commit-id: 326c00a818aa1a5c366e0f6b5dcdc10109dcc2a6
    Former-commit-id: a286789d9f2fda5ed25ccd30c1fe98f5be31e1e6

---
+++ src/mitll/langtest/server/database/JsonSupport.java
@@ -0,0 +1,262 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.ExerciseSorter;
+import mitll.langtest.server.database.exercise.SectionHelper;
+import mitll.langtest.shared.AudioAttribute;
+import mitll.langtest.shared.CommonExercise;
+import mitll.langtest.shared.flashcard.CorrectAndScore;
+import mitll.langtest.shared.flashcard.ExerciseCorrectAndScore;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
+
+import java.math.BigDecimal;
+import java.text.CollationKey;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by go22670 on 4/7/15.
+ */
+public class JsonSupport {
+  private static final Logger logger = Logger.getLogger(JsonSupport.class);
+
+  private final SectionHelper sectionHelper;
+  private final ResultDAO resultDAO;
+  private final AudioDAO audioDAO;
+  private final PhoneDAO phoneDAO;
+  private final String configDir;
+  private final String installPath;
+
+  public JsonSupport(SectionHelper sectionHelper, ResultDAO resultDAO, AudioDAO audioDAO,
+                     PhoneDAO phoneDAO,String configDir, String installPath) {
+    this.sectionHelper = sectionHelper;
+    this.resultDAO = resultDAO;
+    this.audioDAO = audioDAO;
+    this.phoneDAO = phoneDAO;
+    this.configDir = configDir;
+    this.installPath = installPath;
+  }
+  /**
+   * @see mitll.langtest.server.ScoreServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+   * @param userid
+   * @param typeToSection
+   * @paramx collator
+   * @return
+   */
+  public JSONObject getJsonScoreHistory(long userid,
+                                        Map<String, Collection<String>> typeToSection,
+                                        ExerciseSorter sorter//,
+                                        //  Collator collator
+  ) {
+    Collection<CommonExercise> exercisesForState = sectionHelper.getExercisesForSelectionState(typeToSection);
+    List<String> allIDs = new ArrayList<String>();
+
+/*    Map<String, CollationKey> idToKey = new HashMap<String, CollationKey>();
+    for (CommonExercise exercise : exercisesForState) {
+      String id = exercise.getID();
+      allIDs.add(id);
+      CollationKey collationKey = collator.getCollationKey(exercise.getForeignLanguage());
+      idToKey.put(id, collationKey);
+    }*/
+
+    Map<String, CommonExercise> idToEx = new HashMap<String, CommonExercise>();
+    for (CommonExercise exercise : exercisesForState) {
+      String id = exercise.getID();
+      allIDs.add(id);
+      idToEx.put(id, exercise);
+    }
+
+    //List<ExerciseCorrectAndScore> exerciseCorrectAndScores = resultDAO.getExerciseCorrectAndScores(userid, allIDs, idToKey);
+    List<ExerciseCorrectAndScore> exerciseCorrectAndScores =
+        resultDAO.getExerciseCorrectAndScoresByPhones(userid, allIDs, idToEx, sorter);
+
+    return addJsonHistory(exerciseCorrectAndScores);
+  }
+
+
+  /**
+   * A special, slimmed down history just for the Appen recording app.
+   * @see mitll.langtest.server.ScoreServlet#getRecordHistory
+   * @param userid
+   * @param typeToSection
+   * @param collator
+   * @return
+   */
+  public JSONObject getJsonScoreHistoryRecorded(long userid,
+                                                Map<String, Collection<String>> typeToSection,
+                                                Collator collator
+  ) {
+    Collection<CommonExercise> exercisesForState = sectionHelper.getExercisesForSelectionState(typeToSection);
+
+    final Map<String, CollationKey> idToKey = new HashMap<String, CollationKey>();
+    for (CommonExercise exercise : exercisesForState) {
+      String id = exercise.getID();
+      idToKey.put(id, collator.getCollationKey(exercise.getForeignLanguage()));
+    }
+
+    List<CommonExercise> copy = new ArrayList<CommonExercise>(exercisesForState);
+    final Set<String> recordedForUser = audioDAO.getRecordedRegularForUser(userid);
+
+    Collections.sort(copy, new Comparator<CommonExercise>() {
+      @Override
+      public int compare(CommonExercise o1, CommonExercise o2) {
+        String id = o1.getID();
+        String id1 = o2.getID();
+
+        boolean firstRecorded = recordedForUser.contains(id);
+        boolean secondRecorded = recordedForUser.contains(id1);
+        if (!firstRecorded && secondRecorded) {
+          return -1;
+        } else if (firstRecorded && !secondRecorded) {
+          return +1;
+        } else {
+          CollationKey fl = idToKey.get(id);
+          CollationKey otherFL = idToKey.get(id1);
+          return fl.compareTo(otherFL);
+        }
+      }
+    });
+
+
+    JSONArray scores = new JSONArray();
+    for (CommonExercise ex : copy) {
+      //logger.debug("for " + ex);
+      JSONObject exAndScores = new JSONObject();
+      exAndScores.put("ex", ex.getID());
+      boolean wasRecorded = recordedForUser.contains(ex.getID());
+      exAndScores.put("s", wasRecorded ? "100" : "0");
+
+      JSONArray history = new JSONArray();
+      if (wasRecorded) {
+        history.add("Y");
+      }
+      exAndScores.put("h", history);
+
+      scores.add(exAndScores);
+    }
+
+    JSONObject container = new JSONObject();
+    container.put("scores", scores);
+    container.put("lastCorrect", ""+recordedForUser.size());
+    container.put("lastIncorrect", Integer.toString(0));
+    return container;
+  }
+
+  /**
+   * scoreJson has the complete scoring json for the last item only.
+   *
+   * @see #getJsonScoreHistory
+   * @param exerciseCorrectAndScores
+   * @return
+   */
+  private JSONObject addJsonHistory(Collection<ExerciseCorrectAndScore> exerciseCorrectAndScores) {
+    JSONArray scores = new JSONArray();
+    int correct = 0, incorrect = 0;
+    for (ExerciseCorrectAndScore ex : exerciseCorrectAndScores) {
+      //logger.debug("for " + ex);
+      List<CorrectAndScore> correctAndScoresLimited = ex.getCorrectAndScoresLimited();
+
+      JSONArray history = new JSONArray();
+      boolean lastCorrect = getHistoryAsJson(history, correctAndScoresLimited);
+      boolean empty = correctAndScoresLimited.isEmpty();
+      if (!empty) {
+        if (lastCorrect) correct++; else incorrect++;
+      }
+
+      JSONObject exAndScores = new JSONObject();
+      exAndScores.put("ex", ex.getId());
+      exAndScores.put("s", Integer.toString(ex.getAvgScorePercent()));
+      exAndScores.put("h", history);
+      exAndScores.put("scores", getScoresAsJson(correctAndScoresLimited));
+      exAndScores.put("scoreJson",empty?"":correctAndScoresLimited.get(correctAndScoresLimited.size()-1).getScoreJson());
+      scores.add(exAndScores);
+    }
+
+    JSONObject container = new JSONObject();
+    container.put("scores", scores);
+    container.put("lastCorrect",   Integer.toString(correct));
+    container.put("lastIncorrect", Integer.toString(incorrect));
+    return container;
+  }
+
+  /**
+   *
+   * @param history
+   * @param correctAndScoresLimited
+   * @return array of Y's and N's
+   * @see #addJsonHistory
+   */
+  private boolean getHistoryAsJson(JSONArray history, Collection<CorrectAndScore> correctAndScoresLimited) {
+    boolean lastCorrect = false;
+    for (CorrectAndScore cs : correctAndScoresLimited) {
+      history.add(cs.isCorrect() ? "Y" : "N");
+      lastCorrect = cs.isCorrect();
+    }
+    return lastCorrect;
+  }
+
+
+  private JSONArray getScoresAsJson( Collection<CorrectAndScore> correctAndScoresLimited) {
+    JSONArray history = new JSONArray();
+
+    for (CorrectAndScore cs : correctAndScoresLimited) {
+      history.add(round(cs.getScore()));
+    }
+    return history;
+  }
+
+  private static float round(float d) { return round(d, 3);	}
+  private static float round(float d, int decimalPlace) {
+    BigDecimal bd = new BigDecimal(Float.toString(d));
+    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+    return bd.floatValue();
+  }
+  /**
+   * For all the exercises in a chapter
+
+   Get latest results
+   Get phones for latest
+
+   //Score phones
+   Sort phone scores – asc
+
+   Map phone->example
+
+   Join phone->word
+
+   Sort word by score asc
+   * @return
+   * @see mitll.langtest.server.ScoreServlet#getPhoneReport
+   */
+  public JSONObject getJsonPhoneReport(long userid, Map<String, Collection<String>> typeToValues) {
+    Collection<CommonExercise> exercisesForState = sectionHelper.getExercisesForSelectionState(typeToValues);
+
+    long then = System.currentTimeMillis();
+    Map<String, List<AudioAttribute>> exToAudio = audioDAO.getExToAudio();
+    long now = System.currentTimeMillis();
+
+    if (now-then > 500) logger.warn("took " + (now-then) + " millis to get ex->audio map");
+
+    List<String> ids = new ArrayList<String>();
+    Map<String, String> idToRef = new HashMap<String, String>();
+    for (CommonExercise exercise : exercisesForState) {
+      List<AudioAttribute> audioAttributes = exToAudio.get(exercise.getID());
+      if (audioAttributes != null) {
+        audioDAO.attachAudio(exercise, installPath, configDir, audioAttributes);
+      }
+      String id = exercise.getID();
+      ids.add(id);
+      idToRef.put(id, exercise.getRefAudio());
+    }
+
+    return phoneDAO.getWorstPhonesJson(userid, ids, idToRef);
+  }
+}
