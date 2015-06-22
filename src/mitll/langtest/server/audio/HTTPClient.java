005c8c9c3b008b963239c817ce712f47521f5968
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-06-22 18:57:35 -0400

    Added support for universal login - looks for another account on another site and adds it locally if it doesn't exist
    Former-commit-id: 91f3239639ff7bd5d6440371d01840c287507257
    Former-commit-id: b766672863c6e77c0809d79a9d22c2965ca72711

--- src/mitll/langtest/server/audio/HTTPClient.java
+++ src/mitll/langtest/server/audio/HTTPClient.java
@@ -54,6 +54,31 @@ public class HTTPClient {
 		return httpConn;
 	}
 
+	public String readFromGET(String url) {
+		try {
+			return receive(setupGetHttpConn(url));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	private HttpURLConnection setupGetHttpConn(String url) throws IOException {
+		HttpURLConnection httpConn = (HttpURLConnection)(new URL(url)).openConnection();
+		httpConn.setRequestMethod("GET");
+		httpConn.setConnectTimeout(1000);
+		//httpConn.setReadTimeout(20000);
+		//httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+		httpConn.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
+		httpConn.setRequestProperty("Accept-Charset", "UTF8");
+		httpConn.setRequestProperty("charset", "UTF8");
+		return httpConn;
+	}
+
+	public String read(HttpURLConnection conn) {
+		return receive(conn);
+	}
+
 	//private void resetConn() throws IOException {
 	//	setupHttpConn(url);
 	//}
@@ -76,22 +101,39 @@ public class HTTPClient {
 	}
 
 	private String receive() throws IOException {
-		receiver = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF8"));
+		HttpURLConnection httpConn = this.httpConn;
+		receiver = getReader(httpConn);
+		return receive(httpConn, receiver);
+	}
+
+	private String receive(HttpURLConnection httpConn) {
+		try {
+			return receive(httpConn, getReader(httpConn));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	private String receive(HttpURLConnection httpConn, BufferedReader reader) throws IOException {
 		int code = httpConn.getResponseCode();
-		if(code == 200) {
+		if (code == 200) {
 			String output = "";
 			String current = "";
-			while((current = receiver.readLine()) != null) {
+			while ((current = reader.readLine()) != null) {
 				output += current + "\n";
 			}
-			receiver.close();
+			reader.close();
 			receiver = null;
 			return output;
-		}
-		else {
+		} else {
 			logger.error("Received HTTP Code of: " + code + ". Expecting code 200.");
 			return "";
 		}
+	}
+
+	private BufferedReader getReader(HttpURLConnection httpConn) throws IOException {
+		return new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF8"));
 	}
 
 	public String sendAndReceive(String input) {
@@ -105,5 +147,4 @@ public class HTTPClient {
 			return "";
 		}
 	}
-
 }
