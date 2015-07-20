0480e1adc9400116bfdec28ca66690b9db218bd5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-07-20 18:12:09 -0400

    shows phone and word scores to the right of the wave form
    Former-commit-id: e57ded10badb9c20256e5cd51f7706699dea2a42 [formerly e178a3c7ddaec35c9ca44b95825e15f64fbdaa44]
    Former-commit-id: 1d88be25684c1699ab2a8359438ff7d5d70a5f54

--- src/mitll/langtest/client/scoring/ReviewScoringPanel.java
+++ src/mitll/langtest/client/scoring/ReviewScoringPanel.java
@@ -1,30 +1,111 @@
 package mitll.langtest.client.scoring;
 
+import com.github.gwtbootstrap.client.ui.incubator.Table;
+import com.github.gwtbootstrap.client.ui.incubator.TableHeader;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
 import mitll.langtest.client.LangTest;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.shared.flashcard.AVPHistoryForList;
 import mitll.langtest.shared.scoring.PretestScore;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
  * Created by go22670 on 7/17/15.
  */
 public class ReviewScoringPanel extends ScoringAudioPanel {
-
     private Logger logger = Logger.getLogger("ReviewScoringPanel");
+    private HTML scoreInfo;
+    private Panel tablesContainer;
 
+    /**
+     *
+     * @param refSentence
+     * @param service
+     * @param controller
+     * @param gaugePanel
+     * @param playButtonSuffix
+     * @param exerciseID
+     */
     public ReviewScoringPanel(String refSentence, LangTestDatabaseAsync service, ExerciseController controller, ScoreListener gaugePanel,
                               String playButtonSuffix, String exerciseID) {
         super(refSentence, service, controller, gaugePanel, playButtonSuffix, exerciseID);
-
+        tablesContainer = new HorizontalPanel();
+        tablesContainer.getElement().setId("TablesContainer");
+        addStyleName("topFiveMargin");
+        addStyleName("leftFiveMargin");
+        addStyleName("rightFiveMargin");
     }
 
     @Override
+    protected Widget getAfterPlayWidget() {
+        scoreInfo = new HTML();
+        scoreInfo.addStyleName("leftFiveMargin");
+        scoreInfo.getElement().setId("scoreInfo");
+        return scoreInfo;
+    }
+
+    private Table makeTable(String label, String scoreColHeader, Map<String, Float> scores) {
+        Table table = new Table();
+        table.getElement().setId("LeaderboardTable_" + label + "_" + scoreColHeader.substring(0, 3));
+        table.add(new TableHeader(label));
+        table.add(new TableHeader(scoreColHeader));
+
+        if (scores == null) {
+            logger.warning("scores is null?");
+        }
+        else {
+            List<String> keys = new ArrayList<String>(scores.keySet());
+            Collections.sort(keys);
+            for (String key : keys) {
+
+                HTMLPanel row = new HTMLPanel("tr", "");
+
+                // add index col
+                HTMLPanel col = new HTMLPanel("td", "");
+                col.add(new HTML(key));
+                row.add(col);
+
+                // add score
+                col = new HTMLPanel("td", "");
+                String html = "" + Math.round(scores.get(key)*100);
+                col.add(new HTML(html));
+                row.add(col);
+
+                table.add(row);
+            }
+        }
+        return table;
+    }
+
+    @Override
+    protected int getWidthForWaveform(int leftColumnWidth1, int leftColumnWidth, int rightSide) {
+        return Window.getClientWidth() - 380;
+    }
+
+        /**
+         * @see ScoringAudioPanel#getTranscriptImageURLForAudio(String, String, int, ImageAndCheck, ImageAndCheck)
+         * @param path
+         * @param resultID
+         * @param refSentence
+         * @param wordTranscript
+         * @param phoneTranscript
+         * @param width
+         * @param height
+         * @param reqid
+         */
+    @Override
     protected void scoreAudio(String path, long resultID, String refSentence, final ImageAndCheck wordTranscript,
                               final ImageAndCheck phoneTranscript, int width, int height, int reqid) {
+
         boolean wasVisible = wordTranscript.image.isVisible();
 
         // only show the spinning icon if it's going to take awhile
@@ -54,8 +135,43 @@ public class ReviewScoringPanel extends ScoringAudioPanel {
                 t.cancel();
                 if (result != null) {
                     useResult(result, wordTranscript, phoneTranscript, false, "");
+
+                    float hydecScore = result.getHydecScore();
+                    float zeroToHundred = hydecScore * 100f;
+                    String html = "Score : <b>" + Math.round(Math.min(100.0f, zeroToHundred)) +
+                            "%</b>";
+                    scoreInfo.setHTML(html);
+
+                    // logger.info("Setting " + scoreInfo.getElement().getId() + " to " + html);
+                    tablesContainer.clear();
+
+                    if (result.getWordScores() != null) {
+                        if (!result.getWordScores().isEmpty()) {
+                            Table wordTable = makeTable("Word", "Score", result.getWordScores());
+
+                            ScrollPanel child = new ScrollPanel(wordTable);
+                            child.getElement().setId("TableScroller_Word");
+                            child.setWidth("150px");
+                            child.setHeight("200px");
+                            tablesContainer.add(child);
+                        }
+
+                        if (!result.getPhoneScores().isEmpty()) {
+                            Table phoneTable = makeTable("Phone", "Score", result.getPhoneScores());
+
+                            ScrollPanel child = new ScrollPanel(phoneTable);
+                            child.getElement().setId("TableScroller_Phone");
+                            child.setWidth("120px");
+                            child.setHeight("200px");
+                            tablesContainer.add(child);
+                        }
+                    }
                 }
             }
         });
+    }
+
+    public Widget getTables() {
+        return tablesContainer;
     }
 }
