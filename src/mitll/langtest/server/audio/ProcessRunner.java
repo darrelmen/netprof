ec09927ce38bbb98f2c0b4df98043cf4e29dd576
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-07-22 16:49:57 -0400

    flash samples at 16K by default, removing need to run sox to resample
    Former-commit-id: ba8b5d1d1d9e66e598d02017c7bfbc4945b9d141 [formerly f60fc36e6ab12b693e8751e7eac67ea03202bb52] [formerly ae66e153d2c2939294464d26a563f69971e4f1e1 [formerly a0c94710270890e5b7e78be4e45852477aecb8e3]] [formerly ba8b5d1d1d9e66e598d02017c7bfbc4945b9d141 [formerly f60fc36e6ab12b693e8751e7eac67ea03202bb52] [formerly ae66e153d2c2939294464d26a563f69971e4f1e1 [formerly a0c94710270890e5b7e78be4e45852477aecb8e3]] [formerly ba8b5d1d1d9e66e598d02017c7bfbc4945b9d141 [formerly f60fc36e6ab12b693e8751e7eac67ea03202bb52] [formerly ae66e153d2c2939294464d26a563f69971e4f1e1 [formerly a0c94710270890e5b7e78be4e45852477aecb8e3]] [formerly 6895520a9cd7cfb3aab48c867864296347e81f97]]]
    Former-commit-id: e59918294c290663c50cedef7329754f438cb50f [formerly 4c23a36f61022c9791084eb92ec1704f0704021a [formerly 3cd62cbfc7e5c7f526fe7bff68077894bbd74cd5] [formerly c7727efc30ac95fcd5291352124b649c41a8888d [formerly c85d31c1c0906e9b553f4f5eb6d22195d97e0b99]]] [formerly 4c23a36f61022c9791084eb92ec1704f0704021a [formerly 3cd62cbfc7e5c7f526fe7bff68077894bbd74cd5] [formerly c7727efc30ac95fcd5291352124b649c41a8888d [formerly c85d31c1c0906e9b553f4f5eb6d22195d97e0b99]] [formerly e8fb06ee427af38cd4e84f0fd8f5f8cfb92fc73e]] [formerly 8dd15ac482b292d8b72ae2de7b892c554f4264c4 [formerly 08a1bc9115d7f252c0a1fd87a5e0efe146c4e4a1] [formerly ed06c606d3949c2ba476935c31ec0df41e44d68e [formerly 061b1e5f5ba9dfadaba8eaa14aa8f183087cd746]] [formerly 8dd15ac482b292d8b72ae2de7b892c554f4264c4 [formerly 08a1bc9115d7f252c0a1fd87a5e0efe146c4e4a1] [formerly ed06c606d3949c2ba476935c31ec0df41e44d68e [formerly 061b1e5f5ba9dfadaba8eaa14aa8f183087cd746]] [formerly 1bdacf952acfa83f8832a0ea7ae24f7ebbbc9ff7]]]
    Former-commit-id: 8dd15ac482b292d8b72ae2de7b892c554f4264c4 [formerly 08a1bc9115d7f252c0a1fd87a5e0efe146c4e4a1] [formerly ed06c606d3949c2ba476935c31ec0df41e44d68e [formerly 061b1e5f5ba9dfadaba8eaa14aa8f183087cd746]]
    Former-commit-id: a4f0dcd0863099f459cf2119e89d09fd3bc607f1 [formerly 9e36a6e1dc54248a5f8992a0890763c459ce42ca] [formerly 6e79c06975d155c823456e95b8ca41246d511f5a [formerly f423de99d0c2722142659d0a9e8aeee117435473]] [formerly 0f1265926273dd0de99abcfe5945835f58afacda]
    Former-commit-id: ddecfa2ff7f789e1cd4c52878a0e4d06ca228e8a
    Former-commit-id: b697c13fd51e611430fc6abde6958f794f586b4c [formerly 22c30c928009836caa8c1520ef591bdedb0bbcba]
    Former-commit-id: 7d4557a50fb5dd1ce61c748ae30f8a06898675da

--- src/mitll/langtest/server/audio/ProcessRunner.java
+++ src/mitll/langtest/server/audio/ProcessRunner.java
@@ -1,12 +1,9 @@
 package mitll.langtest.server.audio;
 
-import org.apache.log4j.Logger;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,28 +14,40 @@ import java.util.Date;
  */
 public class ProcessRunner {
  // private static final Logger logger = Logger.getLogger(ProcessRunner.class);
-
   //private static final boolean SHOW_OUTPUT = true;
 
-  public void runProcess(ProcessBuilder shellProc) throws IOException {
-    runProcess(shellProc,false);
+  public boolean runProcess(ProcessBuilder shellProc) throws IOException {
+    return runProcess(shellProc,false);
   }
 
-  public void runProcess(ProcessBuilder shellProc, boolean showOutput) throws IOException {
-    //logger.debug(new Date() + " : proc " + shellProc.command() + " started...");
+    public boolean runProcess(ProcessBuilder shellProc, boolean showOutput) throws IOException {
+        //logger.debug(new Date() + " : proc " + shellProc.command() + " started...");
 
-    shellProc.redirectErrorStream(true);
-    Process process2 = shellProc.start();
+        shellProc.redirectErrorStream(true);
+        Process process2 = shellProc.start();
 
-    // read the output
-    InputStream stdout = process2.getInputStream();
-    readFromStream(stdout, showOutput);
-    InputStream errorStream = process2.getErrorStream();
-    readFromStream(errorStream, true);
+        // read the output
+        InputStream stdout = process2.getInputStream();
+        readFromStream(stdout, showOutput);
+        InputStream errorStream = process2.getErrorStream();
+        readFromStream(errorStream, true);
 
-    process2.destroy();
-  //  System.out.println(new Date() + " : proc " + shellProc.command() + " finished");
-  }
+        try {
+            int i = process2.waitFor();
+            if (i != 0) {
+                //  log.warning("got exit status " + i + " from " + shellProc.command());
+                return false;
+            }
+            //System.out.println("got " + i + " for " +shellProc);
+        } catch (InterruptedException e) {
+            // log.warning("got " +e);
+
+        }
+
+        process2.destroy();
+        return true;
+        //  System.out.println(new Date() + " : proc " + shellProc.command() + " finished");
+    }
 
   private void readFromStream(InputStream is2, boolean showOutput) throws IOException {
     InputStreamReader isr2 = new InputStreamReader(is2);
