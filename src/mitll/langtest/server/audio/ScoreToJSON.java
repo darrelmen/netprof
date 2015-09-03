9c3cfea3b37296fc69910a83859c59dc8d5ea9da
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-03 14:10:29 -0400

    Fixed bug where wouldn't always write out slf file for hydec backup mode, refactored AudioFileHelper to make it more tractable
    Former-commit-id: ff4a0ad829b00090f82aa1f0edbc3a3211851f1e [formerly 38934f6123f72e46a3d4e23265e7259bcafa846a] [formerly 24fbe08e8ae8cf2bd076d1bd6b7275e182ab0a66 [formerly 495261c347dd32c2d71a9288adc912731d24fce0]]
    Former-commit-id: 96aceaa3f4cb8bb401cc2af2766e28216993afd8 [formerly ef2c57dfe9dcac645465548c523da06d78421fb7]
    Former-commit-id: f1570a700843c38744b61df3bba62af2f9a31846

---
+++ src/mitll/langtest/server/audio/ScoreToJSON.java
@@ -0,0 +1,101 @@
+package mitll.langtest.server.audio;
+
+import mitll.langtest.shared.AudioAnswer;
+import mitll.langtest.shared.instrumentation.TranscriptSegment;
+import mitll.langtest.shared.scoring.NetPronImageType;
+import mitll.langtest.shared.scoring.PretestScore;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by go22670 on 9/3/15.
+ */
+public class ScoreToJSON {
+
+  /**
+   * We skip sils, since we wouldn't want to show them to the user.
+   *
+   * @param answer
+   * @return
+   * @see #getAudioAnswerDecoding
+   */
+  public JSONObject getJsonFromAnswer(AudioAnswer answer) {
+    PretestScore pretestScore = answer.getPretestScore();
+    return getJsonObject(pretestScore);
+  }
+
+  public JSONObject getJsonObject(PretestScore pretestScore) {
+    JSONObject jsonObject = new JSONObject();
+    if (pretestScore != null) {
+      Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeListMap = pretestScore.getsTypeToEndTimes();
+      List<TranscriptSegment> words = netPronImageTypeListMap.get(NetPronImageType.WORD_TRANSCRIPT);
+      List<TranscriptSegment> phones = netPronImageTypeListMap.get(NetPronImageType.PHONE_TRANSCRIPT);
+      if (words != null) {
+        int windex = 0;
+        int pindex = 0;
+        JSONArray jsonWords = new JSONArray();
+
+        for (TranscriptSegment segment : words) {
+          String event = segment.getEvent();
+          if (!event.equals(SLFFile.UNKNOWN_MODEL) && !event.equals("sil")) {
+            JSONObject wordJson = new JSONObject();
+            String wid = Integer.toString(windex++);
+            wordJson.put("id", wid);
+            wordJson.put("w", event);
+            wordJson.put("s", getScore(segment));
+            wordJson.put("str", floatToString(segment.getStart()));
+            wordJson.put("end", floatToString(segment.getEnd()));
+
+            JSONArray jsonPhones = new JSONArray();
+
+            for (TranscriptSegment pseg : phones) {
+              if (pseg.getStart() >= segment.getStart() && pseg.getEnd() <= segment.getEnd()) {
+                String pevent = pseg.getEvent();
+                if (!pevent.equals(SLFFile.UNKNOWN_MODEL) && !pevent.equals("sil")) {
+                  JSONObject phoneJson = new JSONObject();
+                  phoneJson.put("id", Integer.toString(pindex++));
+                  phoneJson.put("p", pevent);
+                  phoneJson.put("s", getScore(pseg));
+                  phoneJson.put("str", floatToString(pseg.getStart()));
+                  phoneJson.put("end", floatToString(pseg.getEnd()));
+                  jsonPhones.add(phoneJson);
+                }
+              }
+            }
+            wordJson.put("phones", jsonPhones);
+            jsonWords.add(wordJson);
+          }
+        }
+
+        jsonObject.put("words", jsonWords);
+      }
+    }
+    return jsonObject;
+  }
+
+  private String getScore(TranscriptSegment segment) {
+    float score = segment.getScore();
+    return floatToString(score);
+  }
+
+  private String floatToString(float round) {
+    return Float.toString(round(round));
+  }
+
+
+  private static float round(float d) {
+    return round(d, 3);
+  }
+
+  private static float round(float d, int decimalPlace) {
+    BigDecimal bd = new BigDecimal(Float.toString(d));
+    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+    return bd.floatValue();
+  }
+}
+
+
