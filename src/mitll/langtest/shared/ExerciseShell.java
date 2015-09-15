18a61f74694fb46903aa3a69c102c2d17292c335
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-15 18:10:20 -0400

    does two column exercise list
    Former-commit-id: 62d4311d6489dea140207383062d3e924da411ed [formerly ccd9c84cb712586e08cb7eb0f83e42ef30ba3110] [formerly 43fa6a2ad156b996a6c3e03a44996d05b96d3355 [formerly f158055a0e1c9b189964c34d803a2e9e72b69637]]
    Former-commit-id: 06b158bd6e6ea94f3910b62d282d23400b990b07 [formerly 2ff87b3bc68c24e597f9025746bab669a1094b54] [formerly 92777fbd9bb5a6e178915d6edea18cf3b0547ea3 [formerly 1bd8b0445dbd80954d99011201381eac1c3b183f]]
    Former-commit-id: ddde7c14c8fb7b0ddee81757ac54e11ec28341bc [formerly fb409a585896c7346e847072b9c458de579a4749]
    Former-commit-id: 347a9222daf65d3257c68e81fddf8d871d49e95f

--- src/mitll/langtest/shared/ExerciseShell.java
+++ src/mitll/langtest/shared/ExerciseShell.java
@@ -12,33 +12,84 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public class ExerciseShell implements IsSerializable, CommonShell {
   private String tooltip;
   protected String id;
+
   protected STATE state = STATE.UNSET;
   protected STATE secondState = STATE.UNSET;
 
-  public ExerciseShell() {}
+  protected String englishSentence;
+  protected String meaning;
+  protected String foreignLanguage;
 
+  public ExerciseShell() {
+  }
+
+  /**
+   * @see AudioExercise#AudioExercise(String)
+   * @param id
+   */
   public ExerciseShell(String id) {
     this.id = id;
+    this.englishSentence = "";
+    this.meaning = "";
+    this.foreignLanguage = "";
   }
 
   public ExerciseShell(String id, String tooltip) {
     this.id = id;
     setTooltip(tooltip);
+    this.englishSentence = "";
+    this.meaning = "";
+    this.foreignLanguage = "";
   }
 
-  public String getID() { return id; }
-  public void setID(String id) { this.id = id; }
-  public String getTooltip() { return tooltip; }
+  public ExerciseShell(String id, String tooltip, String englishSentence, String meaning, String foreignLanguage) {
+    this.id = id;
+    setTooltip(tooltip);
+    this.englishSentence = englishSentence;
+    this.meaning = meaning;
+    this.foreignLanguage = foreignLanguage;
+  }
+
+  public String getID() {
+    return id;
+  }
+
+  public void setID(String id) {
+    this.id = id;
+  }
+
+  public String getTooltip() {
+    return tooltip;
+  }
+
   public void setTooltip(String tooltip) {
     this.tooltip = tooltip;
     //if (tooltip.isEmpty() && !id.equals("-1")) throw new IllegalArgumentException("tooltip is empty for " + id);
   }
 
-  @Override
-  public STATE getState() { return state;  }
+  public String getEnglish() {
+    return englishSentence;
+  }
 
   @Override
-  public void setState(STATE state) { this.state = state;  }
+  public String getMeaning() {
+    return meaning;
+  }
+
+  @Override
+  public String getForeignLanguage() {
+    return foreignLanguage;
+  }
+
+  @Override
+  public STATE getState() {
+    return state;
+  }
+
+  @Override
+  public void setState(STATE state) {
+    this.state = state;
+  }
 
   @Override
   public STATE getSecondState() {
@@ -47,18 +98,20 @@ public class ExerciseShell implements IsSerializable, CommonShell {
 
   @Override
   public void setSecondState(STATE state) {
-     this.secondState = state;
+    this.secondState = state;
   }
 
   /**
-   * @see mitll.langtest.server.LangTestDatabaseImpl#getExerciseShellsShort(java.util.Collection)
    * @return
+   * @see mitll.langtest.server.LangTestDatabaseImpl#getExerciseShellsShort(java.util.Collection)
    */
-  public CommonShell getShell() { return new ExerciseShell(getID(), getTooltip()); }
+  public CommonShell getShell() {
+    return new ExerciseShell(getID(), getTooltip(), englishSentence,meaning,foreignLanguage);
+  }
 
   @Override
   public boolean equals(Object other) {
-    return other instanceof ExerciseShell && getID().equals(((ExerciseShell)other).getID());
+    return other instanceof ExerciseShell && getID().equals(((ExerciseShell) other).getID());
   }
 
   public String toString() {
