b889989695e9db57682e4909287c891944872a7d
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-15 18:10:20 -0400

    does two column exercise list
    Former-commit-id: 4aa6cf6b767f59f41e54b54ceebf06227717338b [formerly a2fe770c23f88d0cb86c4a9c17d2cc0cec14827d] [formerly b7c0739eaef2540b84ca95337ac5d1265a25c561 [formerly 4e319f04e8196bd3ff5a9c823ca49e8bb97f2bb5]] [formerly 4aa6cf6b767f59f41e54b54ceebf06227717338b [formerly a2fe770c23f88d0cb86c4a9c17d2cc0cec14827d] [formerly b7c0739eaef2540b84ca95337ac5d1265a25c561 [formerly 4e319f04e8196bd3ff5a9c823ca49e8bb97f2bb5]] [formerly ca7ec5e52053e1a9a5f4a9114cc87ce9d370168a]]
    Former-commit-id: 517dbc1908bd16ea4bab2cdd9a9a318b9488dab3 [formerly fb5b50a37f806e0fe938b2f1de78db0ee582b568] [formerly e1646e12899cebe87da48920a10d53393f943a07 [formerly 42282acae78d27634ca5257f531965654395f87a]] [formerly 11a19e089e0716c6a1d1df072adf24ad37b5abeb] [formerly d344362bdb977ad2426a97bbdeb0ecc9cfc00b14 [formerly 869dba38e05742e59930e3fd1462670d3cee24ed] [formerly 8c5fbd47a25e3eb5a7d8f4eb4834179f5c543b37 [formerly f16102082de2fbd8abdcbedda239e37cc92a28e7]] [formerly 07b6ac0f436af60392abcc633f855a6a20ae105b]]
    Former-commit-id: d344362bdb977ad2426a97bbdeb0ecc9cfc00b14 [formerly 869dba38e05742e59930e3fd1462670d3cee24ed] [formerly 8c5fbd47a25e3eb5a7d8f4eb4834179f5c543b37 [formerly f16102082de2fbd8abdcbedda239e37cc92a28e7]]
    Former-commit-id: 67ed0c16bc8657041cb20b0700cbccf49265059c
    Former-commit-id: 930ee9ecb7324fa60d182289cdc2a88af6467e63 [formerly 6b0dad66c5754029b92c7375dc32d20ab6e4dec1]
    Former-commit-id: a4dfea122254ef3bbd00273cb01568a4e06b4646

--- src/mitll/langtest/shared/CommonShell.java
+++ src/mitll/langtest/shared/CommonShell.java
@@ -6,12 +6,16 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  * Created by GO22670 on 3/21/2014.
  */
 public interface CommonShell extends IsSerializable {
-  public String getID();
-  public String getTooltip();
+  String getID();
+  String getTooltip();
   void setTooltip(String tooltip);
   STATE getState();
   void setState(STATE state);
 
   STATE getSecondState();
   void setSecondState(STATE state);
+
+  String getEnglish();
+  String getMeaning();
+  String getForeignLanguage();
 }
