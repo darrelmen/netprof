0a69abc71aed7742a478e4b090cad6239ceca467
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-15 18:10:20 -0400

    does two column exercise list
    Former-commit-id: b8fe2418bb94d7259d05ffd5d3fa464b0841523f [formerly 88495ddca974fd5e23d66a2066bbb76de50a5830]
    Former-commit-id: 03dab274819c4b6c0e17991831226d947d2bb56c [formerly f691a3aa52d8925ed62b64b64da773f0451f9cbf]
    Former-commit-id: 25bbcab839dd20b5fcf60655d0763d83a690e527

--- src/mitll/langtest/shared/Exercise.java
+++ src/mitll/langtest/shared/Exercise.java
@@ -11,9 +11,9 @@ import java.util.Set;
 /**
  * Representation of a individual item of work the user sees.  Could be a pronunciation exercise or a question(s)
  * based on a prompt.
- *
+ * <p>
  * TODO : consider subclass for pronunciation exercises?
- *
+ * <p>
  * User: GO22670
  * Date: 5/8/12
  * Time: 1:03 PM
@@ -23,26 +23,26 @@ public class Exercise extends AudioExercise implements CommonExercise {
   private static final int MAX_TOOLTIP_LENGTH = 15;
   private String content;
 
-  private String englishSentence;
-  private String meaning, context, contextTranslation;
+  protected String context;
+  protected String contextTranslation;
   private transient Collection<String> refSentences = new ArrayList<String>();
   private List<String> translitSentences = new ArrayList<String>();
-  private String foreignLanguage;
   private STATE state;
   private List<CorrectAndScore> scores;
   private float avgScore;
 
   private transient List<String> firstPron = new ArrayList<String>();
 
-  public Exercise() {}     // required for serialization
+  public Exercise() {
+  }     // required for serialization
 
   /**
-   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise
-   *
    * @param id
    * @param content
    * @param tooltip
-   * @param context   */
+   * @param context
+   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise
+   */
   public Exercise(String id, String content, String tooltip, String context, String contextTranslation) {
     super(id, tooltip);
     this.setContent(content);
@@ -51,11 +51,11 @@ public class Exercise extends AudioExercise implements CommonExercise {
   }
 
   /**
-   * @see UserExercise#toExercise()
    * @param id
    * @param content
    * @param sentenceRef
    * @param tooltip
+   * @see UserExercise#toExercise()
    */
   public Exercise(String id, String content, String sentenceRef, String tooltip) {
     super(id, tooltip);
@@ -70,11 +70,12 @@ public class Exercise extends AudioExercise implements CommonExercise {
 
   public CommonShell getShellCombinedTooltip() {
     String combined = getCombinedTooltip();
-    return new ExerciseShell(getID(), combined);
+    return new ExerciseShell(getID(), combined, englishSentence, meaning, foreignLanguage);
   }
 
   /**
    * Hack - if we have "N/A" for english, don't show it in the list.
+   *
    * @return
    */
   public String getCombinedTooltip() {
@@ -83,50 +84,56 @@ public class Exercise extends AudioExercise implements CommonExercise {
       refSentence = refSentence.substring(0, MAX_TOOLTIP_LENGTH);
     }
     boolean refSentenceEqualsTooltip = getTooltip().trim().equals(getRefSentence().trim());
-    String combined = refSentenceEqualsTooltip ? getTooltip() : getTooltip() + (refSentence.isEmpty() ? "": " / " + refSentence);
+    String combined = refSentenceEqualsTooltip ? getTooltip() : getTooltip() + (refSentence.isEmpty() ? "" : " / " + refSentence);
     if (getTooltip().isEmpty() || getTooltip().equals("N/A")) combined = refSentence;
     return combined;
   }
 
-  public String getContent() { return content; }
+  public String getContent() {
+    return content;
+  }
 
-  public String getRefSentence() {  return getForeignLanguage();  }
+  public String getRefSentence() {
+    return getForeignLanguage();
+  }
 
   @Override
-  public Collection<String> getRefSentences() { return refSentences; }
+  public Collection<String> getRefSentences() {
+    return refSentences;
+  }
 
   /**
-   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise(String, int, org.apache.poi.ss.usermodel.Row, String, String, String, String, String, boolean, String, boolean)
    * @param sentenceRefs
+   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise(String, int, org.apache.poi.ss.usermodel.Row, String, String, String, String, String, boolean, String, boolean)
    */
   @Override
   public void setRefSentences(Collection<String> sentenceRefs) {
     this.refSentences = sentenceRefs;
   }
 
-  public void setForeignLanguage(String foreignLanguage) { this.foreignLanguage = foreignLanguage; }
+  public void setForeignLanguage(String foreignLanguage) {
+    this.foreignLanguage = foreignLanguage;
+  }
 
-  public String getTransliteration() { return translitSentences.isEmpty() ? "" : translitSentences.get(0); }
+  public String getTransliteration() {
+    return translitSentences.isEmpty() ? "" : translitSentences.get(0);
+  }
 
   public void setTranslitSentence(String translitSentence) {
     translitSentences.clear();
     translitSentences.add(translitSentence);
   }
 
-  public String getEnglish() {  return englishSentence;  }
+  private void setContent(String content) {
+    this.content = content;
+  }
 
-  private void setContent(String content) { this.content = content;  }
   /**
-   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise
    * @param englishSentence
+   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise
    */
   public void setEnglishSentence(String englishSentence) {
     this.englishSentence = englishSentence;
-  }
-
-  @Override
-  public String getMeaning() {
-    return meaning;
   }
 
   public void setMeaning(String meaning) {
@@ -134,32 +141,29 @@ public class Exercise extends AudioExercise implements CommonExercise {
   }
 
   @Override
-  public String getForeignLanguage() {  return foreignLanguage;  }
-
-  @Override
   public String getContext() {
     return context;
   }
-  
+
   @Override
   public String getContextTranslation() {
     return contextTranslation;
   }
 
   /**
-   * @see mitll.langtest.shared.custom.UserExercise#copyFields(Exercise)  - only
    * @param context
+   * @see mitll.langtest.shared.custom.UserExercise#copyFields(Exercise)  - only
    */
   public void setContext(String context) {
     this.context = context;
   }
 
   /**
-   * @see mitll.langtest.shared.custom.UserExercise#copyFields
    * @param contextTranslation
+   * @see mitll.langtest.shared.custom.UserExercise#copyFields
    */
-  public void setContextTranslation(String contextTranslation){
-	  this.contextTranslation = contextTranslation;
+  public void setContextTranslation(String contextTranslation) {
+    this.contextTranslation = contextTranslation;
   }
 
   public Exercise toExercise() {
@@ -186,21 +190,25 @@ public class Exercise extends AudioExercise implements CommonExercise {
   }
 
   @Override
-  public void setScores(List<CorrectAndScore> scores) { this.scores = scores; }
+  public void setScores(List<CorrectAndScore> scores) {
+    this.scores = scores;
+  }
 
   public float getAvgScore() {
     return avgScore;
   }
+
   public void setAvgScore(float avgScore) {
     this.avgScore = avgScore;
   }
 
   /**
-   * @see mitll.langtest.server.audio.AudioFileHelper#countPhones
    * @param bagOfPhones
+   * @see mitll.langtest.server.audio.AudioFileHelper#countPhones
    */
   @Override
-  public void setBagOfPhones(Set<String> bagOfPhones) {}
+  public void setBagOfPhones(Set<String> bagOfPhones) {
+  }
 
   @Override
   public List<String> getFirstPron() {
@@ -219,16 +227,16 @@ public class Exercise extends AudioExercise implements CommonExercise {
 
     // warn about attr that have no user
     StringBuilder builder = new StringBuilder();
-    for (AudioAttribute attr:audioAttributes1) {
+    for (AudioAttribute attr : audioAttributes1) {
       if (attr.getUser() == null) {
         builder.append("\t").append(attr.toString()).append("\n");
       }
     }
 
-    return "Exercise " + id +  " content bytes = " + content.length() + " english '" + getEnglish() +
-        "'/'" + getRefSentence() +"' context " + getContext() + "/"+getContextTranslation()+
-        " audio count = " + audioAttributes1.size()+
-        (builder.toString().isEmpty() ? "":" \n\tmissing user audio " + builder.toString()) +
+    return "Exercise " + id + " content bytes = " + content.length() + " english '" + getEnglish() +
+        "'/'" + getRefSentence() + "' context " + getContext() + "/" + getContextTranslation() +
+        " audio count = " + audioAttributes1.size() +
+        (builder.toString().isEmpty() ? "" : " \n\tmissing user audio " + builder.toString()) +
         //    " : " + questionInfo +
         " unit->lesson " + getUnitToValue();
   }
