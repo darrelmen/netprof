13f65abe59b496aea9b023392456a49e28f95b5b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-17 18:27:54 -0400

    fixed bug where didn't report ref recordings in report email
    Former-commit-id: bd5e8a43bdaaa0c8c920a751d710a5d90856141d [formerly 9a12478fae178ed696c3a0fd7b3e120d3540ae6f] [formerly 3d55961b830f3251187880b131ee73b46c657383]
    Former-commit-id: bfe7662dfe3998177a08fe45f130afeea443b1a5 [formerly 2f2d2e4cc62e911b4e34dcbb88f2c8eb5b1f4a71] [formerly 427dd8ef4896f70895a6ddd9c0c4338f22676002]
    Former-commit-id: 557fb8427e2803c1e3a9acc7ba588c39d1593dbc
    Former-commit-id: 5b346c3c50f44595b6bd95f530ec157d1332a737

--- src/mitll/langtest/shared/Result.java
+++ src/mitll/langtest/shared/Result.java
@@ -19,7 +19,7 @@ import java.util.Date;
  * Time: 5:45 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Result implements IsSerializable {
+public class Result implements IsSerializable, UserAndTime {
   private int uniqueID;
   private long userid;
   public String plan;
