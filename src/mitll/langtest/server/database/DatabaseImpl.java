572be97b7a41a4411f16848fdf016a87e3ba6d06
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-09-18 14:13:20 -0400

    refactored UserManagement out of DatabaseImpl
    Former-commit-id: bf4f1a218efd73b3ab98cdb8ce20fd79d14e6c46 [formerly fd206f9a41e31cbac9bcb7ea4ee81e4d834e9a4a] [formerly 1479d672d1e488345fa043f8215a850409e1fb81]
    Former-commit-id: 612c1df07a4ef98a99a0f6a51be46a443b97e032 [formerly 0aec31afe3bb9e97da08bbf72cc053638aeab5ce] [formerly b383fbb12f437e0e58b62c1916b5ade002dc2576]
    Former-commit-id: b064d9ec748a3c549305307aa3ff406ffd0ffbd3
    Former-commit-id: 77d7e166a3972619bfdcc77065f984f9c62786bf

--- src/mitll/langtest/server/database/DatabaseImpl.java
+++ src/mitll/langtest/server/database/DatabaseImpl.java
@@ -65,9 +65,6 @@ public class DatabaseImpl implements Database {
   private static final Logger logger = Logger.getLogger(DatabaseImpl.class);
   private static final int LOG_THRESHOLD = 10;
   private static final String UNKNOWN = "unknown";
-  private final List<String> sites = Arrays.asList("Dari", "Egyptian", "English", "Farsi", "Korean", "Levantine",
-      "Mandarin",
-      "MSA", "Pashto1", "Pashto2", "Pashto3", "Spanish", "Sudanese", "Urdu");
 
   private String installPath;
   private ExerciseDAO exerciseDAO = null;
@@ -97,6 +94,7 @@ public class DatabaseImpl implements Database {
   private JsonSupport jsonSupport;
 
   private final boolean addDefects = true;
+  private UserManagement userManagement;
 
   /**
    * @param configDir
@@ -244,7 +242,7 @@ public class DatabaseImpl implements Database {
     }
   }
 
-  MonitoringSupport getMonitoringSupport() { return new MonitoringSupport(userDAO, resultDAO);  }
+  private MonitoringSupport getMonitoringSupport() { return new MonitoringSupport(userDAO, resultDAO);  }
 
   /**
    * @param installPath
@@ -325,6 +323,8 @@ public class DatabaseImpl implements Database {
 
       userDAO.checkForFavorites(userListManager);
       userExerciseDAO.setAudioDAO(audioDAO);
+
+      userManagement = new UserManagement(userDAO,exerciseDAO,resultDAO,userListManager);
     }
   }
 
@@ -454,7 +454,7 @@ public class DatabaseImpl implements Database {
   public JSONObject getJsonScoreHistoryRecorded(long userid,
                                                 Map<String, Collection<String>> typeToSection,
                                                 Collator collator) {
-    return jsonSupport.getJsonScoreHistoryRecorded(userid,typeToSection,collator);
+    return jsonSupport.getJsonScoreHistoryRecorded(userid, typeToSection, collator);
   }
 
   /**
@@ -595,47 +595,8 @@ public class DatabaseImpl implements Database {
    * @see mitll.langtest.client.user.UserPassLogin#makeSignInUserName(com.github.gwtbootstrap.client.ui.Fieldset)
    * @see mitll.langtest.server.LangTestDatabaseImpl#userExists(String, String)
    */
-  public User userExists(HttpServletRequest request,String login, String passwordH) {
-    UserDAO userDAO = getUserDAO();
-    User user = userDAO.getUser(login, passwordH);
-
-    if (user == null && !passwordH.isEmpty()) {
-      logger.debug("userExists : checking '" + login + "'");
-
-      for (String site : sites) {
-        String url = "https://np.ll.mit.edu/npfClassroom" + site.replaceAll("Mandarin", "CM") + "/scoreServlet";
-        String json = new HTTPClient("", 0).readFromGET(url + "?hasUser=" + login + "&passwordH=" + passwordH);
-
-//        logger.info("asking " + site + " got '" + json + "'");
-        if (!json.isEmpty()) {
-          try {
-            JSONObject jsonObject = JSONObject.fromObject(json);
-            Object o = jsonObject.get(ScoreServlet.USERID);
-            Object pc = jsonObject.get(ScoreServlet.PASSWORD_CORRECT);
-
-            if (!o.toString().equals("-1")) {
-              logger.info(site + " : found user " + o);
-
-              if (pc.toString().equals("true")) {
-                logger.info("\tmatching password for " + site);
-
-                String ip = getIPInfo(request);
-                Object emailH = jsonObject.get(ScoreServlet.EMAIL_H);
-                Object kind   = jsonObject.get(ScoreServlet.KIND);
-                User.Kind realKind = kind == null ? User.Kind.STUDENT : User.Kind.valueOf(kind.toString());
-
-                user = addUser(login, passwordH, emailH.toString(), "browser", ip, realKind, true);
-                break;
-              }
-            }
-          } catch (Exception e) {
-            logger.error("Got " + e, e);
-          }
-        }
-      }
-    }
-
-    return user;
+  public User userExists(HttpServletRequest request, String login, String passwordH) {
+    return userManagement.userExists(request, login, passwordH);
   }
 
   /**
@@ -649,19 +610,12 @@ public class DatabaseImpl implements Database {
    * @see mitll.langtest.server.ScoreServlet#doPost
    */
   public User addUser(String userID, String passwordH, String emailH, String deviceType, String device) {
-    boolean isMale = true;
-    User.Kind kind = User.Kind.STUDENT;
-    return addUser(userID, passwordH, emailH, deviceType, device, kind, isMale);
+    return userManagement.addUser(userID, passwordH, emailH, deviceType, device);
   }
 
-  public User addUser(String userID, String passwordH, String emailH, String deviceType, String device, User.Kind kind, boolean isMale) {
-    int age = 89;
-    String dialect = "unk";
-    return addUser(userID, passwordH, emailH, deviceType, device, kind, isMale, age, dialect);
-  }
-
-  public User addUser(String userID, String passwordH, String emailH, String deviceType, String device, User.Kind kind, boolean isMale, int age, String dialect) {
-    return addAndGetUser(userID, passwordH, emailH, kind, isMale, age, dialect, deviceType, device);
+  public User addUser(String userID, String passwordH, String emailH, String deviceType, String device, User.Kind kind,
+                      boolean isMale, int age, String dialect) {
+    return userManagement.addUser(userID, passwordH, emailH, deviceType, device, kind, isMale, age, dialect);
   }
 
   /**
@@ -679,16 +633,7 @@ public class DatabaseImpl implements Database {
    */
   public User addUser(HttpServletRequest request, String userID, String passwordH, String emailH, User.Kind kind,
                       boolean isMale, int age, String dialect, String device) {
-    String ip = getIPInfo(request);
-    return addUser(userID, passwordH, emailH, device, ip, kind, isMale, age, dialect);
-  }
-
-  private User addAndGetUser(String userID, String passwordH, String emailH, User.Kind kind, boolean isMale, int age, String dialect, String device, String ip) {
-    User user = userDAO.addUser(userID, passwordH, emailH, kind, ip, isMale, age, dialect, device);
-    if (user != null) {
-      userListManager.createFavorites(user.getId());
-    }
-    return user;
+    return userManagement.addUser(request, userID, passwordH, emailH,  kind, isMale, age, dialect,device);
   }
 
   /**
@@ -697,14 +642,7 @@ public class DatabaseImpl implements Database {
    * @seex mitll.langtest.server.database.ImportCourseExamples#copyUser
    */
   public long addUser(User user) {
-    long l;
-    if ((l = userDAO.userExists(user.getUserID())) == -1) {
-      logger.debug("addUser " + user);
-      l = userDAO.addUser(user.getAge(), user.getGender() == 0 ? UserDAO.MALE : UserDAO.FEMALE,
-          user.getExperience(), user.getIpaddr(), user.getNativeLang(), user.getDialect(), user.getUserID(), false,
-          user.getPermissions(), User.Kind.STUDENT, "", "", "");
-    }
-    return l;
+    return userManagement.addUser(user);
   }
 
   /**
@@ -725,24 +663,14 @@ public class DatabaseImpl implements Database {
    */
   public long addUser(int age, String gender, int experience, String ipAddr,
                       String nativeLang, String dialect, String userID, Collection<User.Permission> permissions, String device) {
-    logger.debug("addUser " + userID);
-    long l = userDAO.addUser(age, gender, experience, ipAddr, nativeLang, dialect, userID, false, permissions, User.Kind.STUDENT, "", "", device);
-    userListManager.createFavorites(l);
-    return l;
-  }
-
-  private String getIPInfo(HttpServletRequest request) {
-    String header = request.getHeader("User-Agent");
-    SimpleDateFormat sdf = new SimpleDateFormat();
-    String format = sdf.format(new Date());
-    return request.getRemoteHost() +/*"/"+ request.getRemoteAddr()+*/(header != null ? "/" + header : "") + " at " + format;
+    return userManagement.addUser(age, gender, experience, ipAddr, nativeLang, dialect, userID,permissions,device);
   }
 
   /**
    * @see mitll.langtest.server.DownloadServlet
    * @param out
    */
-  public void usersToXLSX(OutputStream out) {  userDAO.toXLSX(out, getUsers());  }
+  public void usersToXLSX(OutputStream out) {  userManagement.usersToXLSX(out);  }
 
   /**
    * Adds some sugar -- sets the answers and rate per user, and joins with dli experience data
@@ -751,57 +679,12 @@ public class DatabaseImpl implements Database {
    * @see mitll.langtest.server.LangTestDatabaseImpl#getUsers()
    */
   public List<User> getUsers() {
-    Map<Long, Float> userToRate = resultDAO.getSessions().userToRate;
-    List<User> users = null;
-    try {
-      Pair idToCount = populateUserToNumAnswers();
-      users = userDAO.getUsers();
-      int total = exerciseDAO.getRawExercises().size();
-      for (User u : users) {
-        Integer numResults = idToCount.idToCount.get(u.getId());
-        if (numResults != null) {
-          u.setNumResults(numResults);
-
-          if (userToRate.containsKey(u.getId())) {
-            u.setRate(userToRate.get(u.getId()));
-          }
-          int size = idToCount.idToUniqueCount.get(u.getId()).size();
-          boolean complete = size >= total;
-          u.setComplete(complete);
-          u.setCompletePercent(Math.min(1.0f, (float) size / (float) total));
-/*          logger.debug("user " +u + " : results "+numResults + " unique " + size +
-            " vs total exercises " + total + " complete " + complete);*/
-        }
-      }
-    } catch (Exception e) {
-      logger.error("Got " + e, e);
-    }
-
-    return users;
-  }
-
-  private Pair populateUserToNumAnswers() {
-    Map<Long, Integer> idToCount = new HashMap<Long, Integer>();
-    Map<Long, Set<String>> idToUniqueCount = new HashMap<Long, Set<String>>();
-    for (Result r : resultDAO.getResults()) {
-      Integer count = idToCount.get(r.getUserid());
-      if (count == null) idToCount.put(r.getUserid(), 1);
-      else idToCount.put(r.getUserid(), count + 1);
-
-      Set<String> uniqueForUser = idToUniqueCount.get(r.getUserid());
-      if (uniqueForUser == null) idToUniqueCount.put(r.getUserid(), uniqueForUser = new HashSet<String>());
-      uniqueForUser.add(r.getExerciseID());
-    }
-    return new Pair(idToCount, idToUniqueCount);
+    return userManagement.getUsers();
   }
 
   public void logEvent(String exid, String context, long userid, String device) {
     if (context.length() > 100) context = context.substring(0, 100).replace("\n", " ");
-    try {
-      logEvent(UNKNOWN, "server", exid, context, userid, UNKNOWN, device);
-    } catch (SQLException e) {
-      logAndNotify(e);
-    }
+    logEvent(UNKNOWN, "server", exid, context, userid, UNKNOWN, device);
   }
 
   /**
@@ -815,15 +698,11 @@ public class DatabaseImpl implements Database {
    * @return
    */
   public boolean logEvent(String id, String widgetType, String exid, String context, long userid, String device) {
-    try {
-      return logEvent(id, widgetType, exid, context, userid, UNKNOWN, device);
-    } catch (SQLException e) {
-      logAndNotify(e);
-    }
-    return false;
+    return logEvent(id, widgetType, exid, context, userid, UNKNOWN, device);
   }
 
-  public boolean logEvent(String id, String widgetType, String exid, String context, long userid, String hitID, String device) throws SQLException {
+  public boolean logEvent(String id, String widgetType, String exid, String context, long userid, String hitID,
+                          String device)  {
     return eventDAO.add(new Event(id, widgetType, exid, context, userid, -1, hitID, device));
   }
 
@@ -838,16 +717,6 @@ public class DatabaseImpl implements Database {
 
   public WordDAO getWordDAO() { return wordDAO;  }
   public PhoneDAO getPhoneDAO() { return phoneDAO;  }
-
-  private static class Pair {
-    final Map<Long, Integer> idToCount;
-    final Map<Long, Set<String>> idToUniqueCount;
-
-    public Pair(Map<Long, Integer> idToCount, Map<Long, Set<String>> idToUniqueCount) {
-      this.idToCount = idToCount;
-      this.idToUniqueCount = idToUniqueCount;
-    }
-  }
 
 /*  public List<Result> getResultsWithGrades() {
     List<Result> results = resultDAO.getResults();
@@ -1160,10 +1029,12 @@ public class DatabaseImpl implements Database {
         getAudioDAO(), installPath, configDir, false);
   }
 
-    @Override
-    public String getLanguage() {  return getServerProps().getLanguage();  }
+  @Override
+  public String getLanguage() {
+    return getServerProps().getLanguage();
+  }
 
-    public void writeContextZip(OutputStream out, Map<String, Collection<String>> typeToSection) throws Exception {
+  public void writeContextZip(OutputStream out, Map<String, Collection<String>> typeToSection) throws Exception {
     Collection<CommonExercise> exercisesForSelectionState = typeToSection.isEmpty() ?
         getExercises() :
         getSectionHelper().getExercisesForSelectionState(typeToSection);
