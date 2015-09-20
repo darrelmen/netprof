390b9f1fe0eaa95c0fd27db3f2f9efca3e20cc4c
Jennifer Melot <jennifer.melot@ll.mit.edu>
2015-09-20 11:29:04 -0400

    about to start testing context practice import
    Former-commit-id: 2e1edb2badf2df0248f20473745fa4a1a8fa3446 [formerly f5e8a421704bf76b16943a8991d4ada93b409b8d] [formerly 566ae208b029c5a2dfc416cdc8576d6976177eff]
    Former-commit-id: 7360809281e7b4f019c597b159bf8dca6ee27cb2 [formerly a2acbfdd5f38f053ed058200fdeb36e8b28073fb] [formerly 051d4ec1f57cc31084407dfef3b7667773190c48]
    Former-commit-id: 68ec6dbf671753c1604ef741286bbf741d3c63a3
    Former-commit-id: 8ea9cbb085461bfa7c9d9b44ff14f79511511e0c

--- src/mitll/langtest/server/database/contextPractice/ContextPracticeImport.java
+++ src/mitll/langtest/server/database/contextPractice/ContextPracticeImport.java
@@ -1,5 +1,4 @@
 package mitll.langtest.server.database.contextPractice;
-import mitll.langtest.client.sound.PlayAudioPanel;
 
 import java.io.*;
 import java.util.HashMap;
@@ -14,12 +13,13 @@ public class ContextPracticeImport {
 
     Pattern dialogEx = Pattern.compile("^\\s*Title\\s*:\\s*(.*)$");
     Pattern speakerEx = Pattern.compile("^Speaker\\s*(\\d+)\\s*:\\s*(.*)$");
-    Pattern partEx = Pattern.compile("^\\s*(\\d+)\\s*:\\s*(.*)$");
+    Pattern partEx = Pattern.compile("^\\s*(\\d+)\\s*:\\s*(.*)\\s*\\((.*),\\s*(.*)\\)\\s*$");
     private Map<String, String> sentToAudioPath = new HashMap<String, String>();
-    private Map<String, Iterable<String>> dialogToPartsMap = new HashMap<String, String[]>();
+    private Map<String, String[]> dialogToPartsMap = new HashMap<String, String[]>();
     private Map<String, String> sentToSlowAudioPath = new HashMap<String, String>();
-    private Map<String, Map<String, Integer>> dialogToSpeakerToLast = new HashMap<String, HashMap<String, Integer>>();
-    private
+    private Map<String, HashMap<String, Integer>> dialogToSpeakerToLast = new HashMap<String, HashMap<String, Integer>>();
+    private Map<String, HashMap<Integer, String>> dialogToSentIndexToSpeaker = new HashMap<String, HashMap<Integer, String>>();
+    private Map<String, HashMap<Integer, String>> dialogToSentIndexToSent = new HashMap<String, HashMap<Integer, String>>();
 
     public ContextPracticeImport(String file){
         try{
@@ -27,16 +27,41 @@ public class ContextPracticeImport {
             BufferedReader fin = new BufferedReader(reader);
             String s;
 
+            String currTitle = "";
+            HashMap<String, String> speakerToIdent = new HashMap<String,String>();
+            HashMap<String, String> identToSpeaker = new HashMap<String, String>();
+            HashMap<String, Integer> speakerToLast = new HashMap<String, Integer>();
+            HashMap<Integer, String> sentIndexToSpeaker = new HashMap<Integer, String>();
+            HashMap<Integer, String> sentIndexToSent = new HashMap<Integer, String>();
+            int sentIndex = 0;
             while((s = fin.readLine()) != null){
                 Matcher dm = dialogEx.matcher(s);
                 Matcher sm = speakerEx.matcher(s);
                 Matcher pm = partEx.matcher(s);
                 if(dm.matches()){
-
+                    if(currTitle != null) {
+                        dialogToSpeakerToLast.put(currTitle, speakerToLast);
+                        dialogToSentIndexToSent.put(currTitle, sentIndexToSent);
+                        dialogToSentIndexToSpeaker.put(currTitle, sentIndexToSpeaker);
+                    }
+                    currTitle = dm.group(1);
+                    speakerToIdent = new HashMap<String,String>();
+                    identToSpeaker = new HashMap<String, String>();
+                    speakerToLast = new HashMap<String, Integer>();
+                    sentIndexToSpeaker = new HashMap<Integer, String>();
+                    sentIndexToSent = new HashMap<Integer, String>();
+                    sentIndex = 0;
                 }else if(sm.matches()){
-
+                    speakerToIdent.put(sm.group(2), sm.group(1));
+                    identToSpeaker.put(sm.group(1), sm.group(2));
                 }else if(pm.matches()){
-
+                    sentToAudioPath.put(pm.group(2), pm.group(3));
+                    sentToSlowAudioPath.put(pm.group(2), pm.group(4));
+                    String speaker = identToSpeaker.get(pm.group(1));
+                    speakerToLast.put(speaker, sentIndex);
+                    sentIndexToSpeaker.put(sentIndex, speaker);
+                    sentIndexToSent.put(sentIndex, pm.group(2));
+                    sentIndex += 1;
                 }
             }
 
@@ -46,27 +71,27 @@ public class ContextPracticeImport {
     }
 
     public Map<String, String> getSentToAudioPath(){
-
+        return sentToAudioPath;
     }
 
-    public Map<String, Iterable<String>> getDialogToPartsMap() {
-
+    public Map<String, String[]> getDialogToPartsMap() {
+        return dialogToPartsMap;
     }
 
     public Map<String, String> getSentToSlowAudioPath() {
-
+        return sentToSlowAudioPath;
     }
 
-    public Map<String, Map<String, Integer>> getDialogToSpeakerToLast(){
-
+    public Map<String, HashMap<String, Integer>> getDialogToSpeakerToLast(){
+        return dialogToSpeakerToLast;
     }
 
-    public Map<String, Map<Integer, String>> getDialogToSentIndexToSpeaker(){
-
+    public Map<String, HashMap<Integer, String>> getDialogToSentIndexToSpeaker(){
+        return dialogToSentIndexToSpeaker;
     }
 
-    public Map<String, Map<Integer, String>> getDialogToSentIndexToSent(){
-
+    public Map<String, HashMap<Integer, String>> getDialogToSentIndexToSent(){
+        return dialogToSentIndexToSent;
     }
 
 
