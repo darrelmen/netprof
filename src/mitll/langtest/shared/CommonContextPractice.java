e19951c7dc6bbb4eae185364f7427e15a7e3622f
Jennifer Melot <jennifer.melot@ll.mit.edu>
2015-09-20 20:41:50 -0400

    whew, really almost works, just need to figure out why only the first dialog is getting imported
    Former-commit-id: 300f747a24eb50ce691e1ed3bb340c3e72ea6b3b [formerly 55557861aa65d72ccefb560730105f4c92bfd8b7] [formerly e65f1406e5b970f354807f39fbe2f3e698bd3f33]
    Former-commit-id: 7c521cc5f01dbb3f9acf2cf774ced3f896436081
    Former-commit-id: b791aecd1b27d1e5a29b6be1f69ffeffe58bd523

---
+++ src/mitll/langtest/shared/CommonContextPractice.java
@@ -0,0 +1,12 @@
+package mitll.langtest.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by je24276 on 9/20/15.
+ */
+public interface CommonContextPractice extends IsSerializable {
+
+    ContextPractice toContextPractice();
+
+}
