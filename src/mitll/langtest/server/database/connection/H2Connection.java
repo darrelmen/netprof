b8e228f409b5cba83c7ea5437fd16330070a6061
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-16 18:12:16 -0400

    cleaned up h2connection, added first step toward getting best scores
    Former-commit-id: 1c00a833ffe161ea551c2fee09ac91044aa9e6be [formerly 341280e4ca67300f9f1469d3d636764274e02e76] [formerly 1c00a833ffe161ea551c2fee09ac91044aa9e6be [formerly 341280e4ca67300f9f1469d3d636764274e02e76] [formerly 5b2d5dd5d028867a728b3dc0bdf54e08da55a4c9]]
    Former-commit-id: b45f32c1494613a4dadcafd15a578de3d623677c [formerly c33537df8d0db4f051cbfb3726cf3957b93b236b] [formerly ef345ad423706f7a10802ffde9a173b738d07e91] [formerly 98f51d4946f6770d479d4ac08408f0b6334a6474 [formerly 5a92c79e6a424388ba909eb88ca7e45199fedd09] [formerly 0de354e822bc78a79eb23e7ea614849c36cbd342]]
    Former-commit-id: e1540ecdc7df4113ab2290c6550713acb49fc70e [formerly 98f51d4946f6770d479d4ac08408f0b6334a6474 [formerly 5a92c79e6a424388ba909eb88ca7e45199fedd09]]
    Former-commit-id: 12ead0b1a07a49080797ec03741e8df26ccc4118
    Former-commit-id: 39fda6080a4340c8ae0ed5e91d727a52aa7be32b

--- src/mitll/langtest/server/database/connection/H2Connection.java
+++ src/mitll/langtest/server/database/connection/H2Connection.java
@@ -1,7 +1,9 @@
 package mitll.langtest.server.database.connection;
 
+import mitll.langtest.server.LogAndNotify;
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
 import org.apache.log4j.Logger;
-import org.h2.jdbcx.JdbcConnectionPool;
 
 import java.io.File;
 import java.sql.Connection;
@@ -11,19 +13,16 @@ import java.sql.Statement;
 
 /**
  * Makes H2 Connections.
- * <p/>
+ * <p>
  * User: GO22670
  * Date: 12/31/12
  * Time: 5:00 PM
  * To change this template use File | Settings | File Templates.
  */
 public class H2Connection implements DatabaseConnection {
-  private static final int CONNECTION_POOL_SIZE = 20;
   private static final Logger logger = Logger.getLogger(H2Connection.class);
   private static final int QUERY_CACHE_SIZE = 1024;
-  public static final int CACHE_SIZE_KB = 500000;
-  public static final int LOG_THRESHOLD = 10;
-  public static final int GROWTH_INCREMENT = 5;
+  private static final int CACHE_SIZE_KB = 500000;
 
   private Connection conn;
   private final int cacheSizeKB;
@@ -31,9 +30,13 @@ public class H2Connection implements DatabaseConnection {
   private static final int MAX_MEMORY_ROWS = 1000000;
   private static final int maxMemoryRows = MAX_MEMORY_ROWS;
   private static final boolean USE_MVCC = false;
-  private static final boolean USE_CP = false;
-  private JdbcConnectionPool cp;
 
+  /**
+   * @see mitll.langtest.server.database.DatabaseImpl#DatabaseImpl(String, String, String, ServerProperties, PathHelper, boolean, LogAndNotify)
+   * @param configDir
+   * @param dbName
+   * @param mustAlreadyExist
+   */
   public H2Connection(String configDir, String dbName, boolean mustAlreadyExist) {
     this(configDir, dbName, CACHE_SIZE_KB, QUERY_CACHE_SIZE, mustAlreadyExist);
   }
@@ -51,7 +54,7 @@ public class H2Connection implements DatabaseConnection {
 
   /**
    * h2 db must exist - don't try to make an empty one if it's not there.
-   * <p/>
+   * <p>
    * //jdbc:h2:file:/Users/go22670/DLITest/clean/netPron2/war/config/urdu/vlr-parle;IFEXISTS=TRUE;CACHE_SIZE=30000
    *
    * @param h2FilePath
@@ -74,52 +77,28 @@ public class H2Connection implements DatabaseConnection {
       logger.debug("connecting to " + url);
       org.h2.Driver.load();
       try {
-        //conn = DriverManager.getConnection(url, "", "");
-        long then = System.currentTimeMillis();
-        if (USE_CP) {
-          cp = JdbcConnectionPool.create(url, "", "");
-          cp.setMaxConnections(CONNECTION_POOL_SIZE);
-          //  cp.setLoginTimeout(2);
-          logger.debug("made cp " + cp);
-          logger.debug("max connections " + cp.getMaxConnections());
-          long now = System.currentTimeMillis();
-          if (now - then > 200) {
-            logger.debug("took " + (now - then) + " to create connection pool ");
-          }
-        } else {
-          conn = DriverManager.getConnection(url, "", "");
-        }
+        conn = DriverManager.getConnection(url, "", "");
       } catch (SQLException e) {
         logger.error("couldn't get connection ", e);
+
+        contextDestroyed();
       }
-      // cpconn.setAutoCommit(true);
-      //    } catch (SQLException e) {
-      //     conn = null;
-      //     logger.error("got error trying to create h2 connection with URL '" + url + "', exception = " + e, e);
-      //   }
     }
   }
 
   @Override
   public void contextDestroyed() {
-    if (conn == null) {
-      //logger.info("not never successfully created h2 connection ");
-    } //else {
     sendShutdown();
     cleanup();
-    //org.h2.Driver.unload();
-    //}
   }
 
   @Override
   public int connectionsOpen() {
-    if (cp != null) return cp.getActiveConnections();
-    else return 1;
+    return 1;
   }
 
   private void sendShutdown() {
     logger.info("send shutdown on connection " + conn);
-
     try {
       Connection connection = getConnection(this.getClass().toString());
       Statement stat = connection.createStatement();
@@ -127,7 +106,7 @@ public class H2Connection implements DatabaseConnection {
       stat.close();
       connection.close();
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.error("got " + e, e);
     }
   }
 
@@ -137,43 +116,13 @@ public class H2Connection implements DatabaseConnection {
       if (conn != null) {
         conn.close();
       }
-      if (cp != null) {
-        cp.dispose();
-      }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.error("got " + e, e);
     }
   }
 
   public Connection getConnection(String who) {
-    if (cp != null && cp.getActiveConnections() > LOG_THRESHOLD) {
-      logger.debug("get for " + who + " connection #" + cp.getActiveConnections());
-    }
-//    new Exception().printStackTrace();
-
-    long then = System.currentTimeMillis();
-    try {
-      if (cp != null && cp.getActiveConnections() == cp.getMaxConnections()) {
-        cp.setMaxConnections(cp.getMaxConnections() + GROWTH_INCREMENT);
-        logger.warn("\n\n" + "----> growing connection pool - now " + cp.getMaxConnections() + " active = " + cp.getActiveConnections());
-      }
-      //else if (cp.getActiveConnections() < CONNECTION_POOL_SIZE) {
-
-      //}
-      Connection connection = conn != null ? conn : cp.getConnection();
-      long now = System.currentTimeMillis();
-      if (cp != null && now - then > 200) {
-        logger.debug("took " + (now - then) + " getConnection, currently " + cp.getActiveConnections() + " open.");
-      }
-      return connection;
-    } catch (SQLException e) {
-      long now = System.currentTimeMillis();
-      if (cp != null && now - then > 200) {
-        logger.warn("took " + (now - then) + " to try to getConnection, currently " + cp.getActiveConnections() + " open.");
-      }
-      logger.error("huh? couldn't get connection ", e);
-      return null;
-    }
+    return conn;
   }
 
   public boolean isValid() {
@@ -182,6 +131,6 @@ public class H2Connection implements DatabaseConnection {
 
   @Override
   public boolean usingCP() {
-    return USE_CP;
+    return false;
   }
 }
