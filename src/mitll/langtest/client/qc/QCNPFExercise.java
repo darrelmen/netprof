bc1cf661b20019a3584d80b939d66a46804c114e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-16 15:50:45 -0400

    improvements to click on a token - and rendering of tokens in content
    Former-commit-id: 1171d9222163d27d2124ccf3022cfe2b15b5560c [formerly ac18647feda0b19d1d92b863d15d9db5ba3a6f63] [formerly 9ee87de2037959b9a436905c80f2c1bc70ca5b88 [formerly 4a77a430e9ee3bf584dd999d4f16f03944f00352]] [formerly 1171d9222163d27d2124ccf3022cfe2b15b5560c [formerly ac18647feda0b19d1d92b863d15d9db5ba3a6f63] [formerly 9ee87de2037959b9a436905c80f2c1bc70ca5b88 [formerly 4a77a430e9ee3bf584dd999d4f16f03944f00352]] [formerly 1171d9222163d27d2124ccf3022cfe2b15b5560c [formerly ac18647feda0b19d1d92b863d15d9db5ba3a6f63] [formerly 9ee87de2037959b9a436905c80f2c1bc70ca5b88 [formerly 4a77a430e9ee3bf584dd999d4f16f03944f00352]] [formerly 8be3f9879c0ac6ff74a4c2b0840c5f9dadf3b480]]]
    Former-commit-id: 0b57a24d75b5636288e5d700fac6d5b4933209da [formerly 96d3e04d7c1c1f1c056321b22082b788a56f5c35] [formerly fbd988f71d679ecd7cfb2c19500d8565d2e8fa17 [formerly a2d44d5ffb923d758fee0527422c3d0dd5c82203]] [formerly 0b57a24d75b5636288e5d700fac6d5b4933209da [formerly 96d3e04d7c1c1f1c056321b22082b788a56f5c35] [formerly fbd988f71d679ecd7cfb2c19500d8565d2e8fa17 [formerly a2d44d5ffb923d758fee0527422c3d0dd5c82203]] [formerly de236fce7ba589f4c20deebedec9de62486fff95]] [formerly ec2b192e4ec1c4e8023830c4d8b13542782a4858 [formerly 71c3c0fa4a15d7d1f3150a36d503c41da5b4187d] [formerly b6150080cc18ddd946cdc28ff0a297e733a10868 [formerly 9ac59a5b972a79b55a131921fc6637ff1e2b73d5]] [formerly ec2b192e4ec1c4e8023830c4d8b13542782a4858 [formerly 71c3c0fa4a15d7d1f3150a36d503c41da5b4187d] [formerly b6150080cc18ddd946cdc28ff0a297e733a10868 [formerly 9ac59a5b972a79b55a131921fc6637ff1e2b73d5]] [formerly 5ead93d9abaf6d0746e730ba1792024e79dbc195]]]
    Former-commit-id: ec2b192e4ec1c4e8023830c4d8b13542782a4858 [formerly 71c3c0fa4a15d7d1f3150a36d503c41da5b4187d] [formerly b6150080cc18ddd946cdc28ff0a297e733a10868 [formerly 9ac59a5b972a79b55a131921fc6637ff1e2b73d5]]
    Former-commit-id: 6deb6ad5bc7aa758a864e403a540b28c288352be [formerly ac85b4c16a7e5ca32d20301552a322064c6bb82e] [formerly 931211581a6a99bc875200a90a2bc81998b2fc91 [formerly e42925824de1cf5aee26f5f943389b6dca89719e]] [formerly 37425be4db4949ff3525cccfcc09e8c2c81ea188]
    Former-commit-id: e62f97c3f3bf58632fd488a099b28ace4e6dcfd7
    Former-commit-id: 5144fd46a39704e0ea3d85762807eaed243d76b0 [formerly a14b358f824e564cbc0c938a9f66241e02e357ac]
    Former-commit-id: 01f158fa1ea5533561c610eb34b83ca234b54933

--- src/mitll/langtest/client/qc/QCNPFExercise.java
+++ src/mitll/langtest/client/qc/QCNPFExercise.java
@@ -539,7 +539,7 @@ public class QCNPFExercise extends GoodwaveExercisePanel {
       audioRef = wavToMP3(audioRef);   // todo why do we have to do this?
     }
     final ASRScoringAudioPanel audioPanel = new ASRScoringAudioPanel(audioRef, e.getRefSentence(), service, controller,
-      controller.getProps().showSpectrogram(), scorePanel, 70, audio.isRegularSpeed()?" Regular speed":" Slow speed", e.getID());
+      controller.getProps().showSpectrogram(), scorePanel, 70, audio.isRegularSpeed()?" Regular speed":" Slow speed", e.getID(), e, instance);
     audioPanel.setShowColor(true);
     audioPanel.getElement().setId("ASRScoringAudioPanel");
     audioPanel.addPlayListener(new PlayListener() {
