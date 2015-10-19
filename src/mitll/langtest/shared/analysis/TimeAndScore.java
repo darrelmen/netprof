fe6cb538e952bb9818ecbeb371c65cbb10de6355
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-19 15:24:04 -0400

    final fixes to performance analysis graph
    Former-commit-id: df588ace1e8d0e1774140f8e9e4740ba6391b469
    Former-commit-id: 24b79af88b7f144dbf214d996e38e7d477758c87 [formerly 3ca560b3683637d0de1d64e00e9de4f1334c84e2]
    Former-commit-id: 6cbaa5ba50b72ef5730b18eca9eb7886f3311acb

--- src/mitll/langtest/shared/analysis/TimeAndScore.java
+++ src/mitll/langtest/shared/analysis/TimeAndScore.java
@@ -1,6 +1,6 @@
 package mitll.langtest.shared.analysis;
 
-import mitll.langtest.server.database.ResultDAO;
+import mitll.langtest.server.database.analysis.BestScore;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
@@ -16,27 +16,25 @@ public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
   private int count = 0;
   private float movingAverage;
 
-/*
-  public TimeAndScore(long timestamp, float score, int count) {
-    this.timestamp = timestamp;
-    this.score = score;
-    this.count = count;
-  }
-*/
 
-  public TimeAndScore(ResultDAO.BestScore bs, float movingAverage) {
+  public TimeAndScore(BestScore bs, float movingAverage) {
     timestamp = bs.getTimestamp();
     score = bs.getScore();
     count = bs.getCount();
     this.movingAverage = movingAverage;
   }
 
-  public TimeAndScore(List<ResultDAO.BestScore> bestScoreList) {
+  /**
+   * @see UserPerformance#addBestScores(List)
+   * @param bestScoreList
+   * @param binSize
+   */
+  public TimeAndScore(List<BestScore> bestScoreList, long binSize) {
     float total = 0;
-    for (ResultDAO.BestScore bs : bestScoreList) total += bs.getScore();
+    for (BestScore bs : bestScoreList) total += bs.getScore();
     score = total / (float) bestScoreList.size();
     count = bestScoreList.size();
-    this.timestamp = bestScoreList.get(0).getTimestamp();
+    this.timestamp = (bestScoreList.get(0).getTimestamp()/binSize)*binSize;
   }
 
   public TimeAndScore() {
@@ -67,5 +65,9 @@ public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
   @Override
   public int compareTo(TimeAndScore o) {
     return new Long(timestamp).compareTo(o.getTimestamp());
+  }
+
+  public void setMoving(float moving) {
+    this.movingAverage = moving;
   }
 }
