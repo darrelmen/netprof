cebbcca37426edd72b709e87a9cddd47494e503b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-19 19:08:35 -0400

    shows exercise on tooltip of analysis display
    Former-commit-id: 1f499e94e28fb2f5a8ccf5f6a2bede842c81a277 [formerly fd91080105d615cc6bf794b76e4325cda8c80ce2] [formerly 5a4237bef4e4fb629e79b01048ff2a71a7f82246 [formerly 6f172e40e65b917b58ab92a0d9d75438d3d00437]] [formerly 8f93a3f1dc76a730fdd30b347cd0a442f1e9b86c]
    Former-commit-id: 4233fb6be9fe35b78ac1879ca24009f9f5c4e587
    Former-commit-id: 4a8683926c5fc560f2ae192fadc8cf3d90ee4193 [formerly 2d17361636e35b92346adcb0de94b8d43251dcb8]
    Former-commit-id: 08856c52f92886602fea1ef03bdfd823ca8d85cd

--- src/mitll/langtest/client/custom/AnalysisPlot.java
+++ src/mitll/langtest/client/custom/AnalysisPlot.java
@@ -1,22 +1,49 @@
 package mitll.langtest.client.custom;
 
 import com.github.gwtbootstrap.client.ui.base.DivWidget;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.IsWidget;
 import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.list.ListInterface;
+import mitll.langtest.shared.CommonShell;
+import mitll.langtest.shared.ExerciseListWrapper;
 import mitll.langtest.shared.analysis.TimeAndScore;
 import mitll.langtest.shared.analysis.UserPerformance;
-import org.moxieapps.gwt.highcharts.client.Axis;
-import org.moxieapps.gwt.highcharts.client.Chart;
-import org.moxieapps.gwt.highcharts.client.Series;
+import org.moxieapps.gwt.highcharts.client.*;
 
-import java.util.List;
+import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * Created by go22670 on 10/19/15.
  */
 public class AnalysisPlot extends DivWidget implements IsWidget {
+  public static final String PRONUNCIATION_SCORE = "Pronunciation Score";
+  private final Logger logger = Logger.getLogger("AnalysisPlot");
+
+  private Map<Long, String> timeToId = new TreeMap<>();
+  private Map<String, CommonShell> idToEx = new TreeMap<>();
+
+  /**
+   * @param service
+   * @param id
+   * @see Navigation#addTabs()
+   */
   public AnalysisPlot(LangTestDatabaseAsync service, long id) {
+    service.getExerciseIds(1, new HashMap<String, Collection<String>>(), "", -1,
+        (int) id, "", false, false, false, false, new AsyncCallback<ExerciseListWrapper>() {
+          @Override
+          public void onFailure(Throwable throwable) {
+
+          }
+
+          @Override
+          public void onSuccess(ExerciseListWrapper exerciseListWrapper) {
+            for (CommonShell shell : exerciseListWrapper.getExercises()) idToEx.put(shell.getID(), shell);
+          }
+        });
+
     service.getPerformanceForUser(id, new AsyncCallback<UserPerformance>() {
       @Override
       public void onFailure(Throwable throwable) {
@@ -26,23 +53,48 @@ public class AnalysisPlot extends DivWidget implements IsWidget {
       @Override
       public void onSuccess(UserPerformance userPerformance) {
         clear();
-        add(getChart("Pronunciation over time", "Cumulative Average", "Items", userPerformance.getRawBestScores()));
-
+        List<TimeAndScore> rawBestScores = userPerformance.getRawBestScores();
+        add(getChart("Pronunciation over time", "Score and average", "Cumulative Average", rawBestScores));
+        setRawBestScores(rawBestScores);
       }
     });
   }
 
   private Chart getChart(
-                         String title, String subtitle, String seriesName,
-                         List<TimeAndScore> yValuesForUser) {
+      String title, String subtitle, String seriesName,
+      List<TimeAndScore> yValuesForUser) {
     Chart chart = new Chart()
+        .setZoomType(BaseChart.ZoomType.X)
         .setType(Series.Type.LINE)
         .setChartTitleText(title)
         .setChartSubtitleText(subtitle)
         .setMarginRight(10)
         .setOption("/credits/enabled", false)
         .setOption("/plotOptions/series/pointStart", 1)
-        .setOption("/legend/enabled", false);
+        .setOption("/legend/enabled", false)
+        .setToolTip(new ToolTip()
+            .setFormatter(new ToolTipFormatter() {
+              public String format(ToolTipData toolTipData) {
+                String s = timeToId.get(toolTipData.getXAsLong());
+                CommonShell commonShell = idToEx.get(s);
+                String foreignLanguage = commonShell == null ? "" : commonShell.getForeignLanguage();
+                String english = commonShell == null ? "" : commonShell.getEnglish();
+
+                String seriesName1 = toolTipData.getSeriesName();
+                boolean showEx = (seriesName1.equals(PRONUNCIATION_SCORE));
+                return "<b>" + seriesName1 + "</b><br/>" +
+                    DateTimeFormat.getFormat("E MMM d h:mm a").format(
+                        new Date(toolTipData.getXAsLong())
+                    ) +
+                    (showEx ?
+                        "<br/>Ex " + s +
+                            "<br/>" + foreignLanguage +
+                            "<br/>" + english
+                        : "")
+                    +
+                    "<br/>Score = " + toolTipData.getYAsLong() + "%";
+              }
+            }));
 
     addSeries(yValuesForUser, chart, seriesName);
 
@@ -73,7 +125,7 @@ public class AnalysisPlot extends DivWidget implements IsWidget {
     int i = 0;
     for (TimeAndScore ts : yValuesForUser) {
       data[i][0] = ts.getTimestamp();
-      data[i++][1] = ts.getCumulativeAverage()*100;
+      data[i++][1] = ts.getCumulativeAverage() * 100;
     }
 
     Series series = chart.createSeries()
@@ -82,6 +134,19 @@ public class AnalysisPlot extends DivWidget implements IsWidget {
 
     chart.addSeries(series);
 
+    data = new Number[yValuesForUser.size()][2];
+
+    i = 0;
+    for (TimeAndScore ts : yValuesForUser) {
+      data[i][0] = ts.getTimestamp();
+      data[i++][1] = ts.getScore() * 100;
+    }
+
+    series = chart.createSeries()
+        .setName(PRONUNCIATION_SCORE)
+        .setPoints(data);
+
+    chart.addSeries(series);
   }
 
   /**
@@ -95,11 +160,13 @@ public class AnalysisPlot extends DivWidget implements IsWidget {
     chart.getYAxis().setAxisTitleText(title)
         .setAllowDecimals(true)
         .setMin(0);
-//        .setMax(top);
-//
-//    chart.getXAxis().setAllowDecimals(false);
-
     chart.getXAxis()
         .setType(Axis.Type.DATE_TIME);
+  }
+
+  public void setRawBestScores(List<TimeAndScore> rawBestScores) {
+    for (TimeAndScore timeAndScore : rawBestScores) {
+      timeToId.put(timeAndScore.getTimestamp(), timeAndScore.getId());
+    }
   }
 }
