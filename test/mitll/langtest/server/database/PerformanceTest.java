256cccb978a800fab755eea4134a8f030310d98f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-19 14:22:37 -0400

    added performance analysis initial steps

---
+++ test/mitll/langtest/server/database/PerformanceTest.java
@@ -0,0 +1,93 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.shared.CommonExercise;
+import mitll.langtest.shared.analysis.UserPerformance;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by GO22670 on 1/30/14.
+ */
+public class PerformanceTest {
+  private static final String CHANGED = "changed";
+  private static final String ENGLISH = "english";
+  private static final Logger logger = Logger.getLogger(PerformanceTest.class);
+  private static DatabaseImpl database;
+  private static String dbName;
+
+  @BeforeClass
+  public static void setup() {
+    logger.debug("setup called");
+
+    String config = "spanish";//"mandarin";
+    File file = new File("war" + File.separator + "config" + File.separator + config + File.separator + "quizlet.properties");
+    String parent = file.getParent();
+    logger.debug("config dir " + parent);
+    logger.debug("config     " + file.getName());
+    //  dbName = "npfEnglish";//"mandarin";// "mandarin";
+    dbName = "npfSpanish";//"mandarin";// "mandarin";
+    database = new DatabaseImpl(parent, file.getName(), dbName, new ServerProperties(parent, file.getName()), new PathHelper("war"), false, null);
+    logger.debug("made " + database);
+    String media = parent + File.separator + "media";
+    logger.debug("media " + media);
+    database.setInstallPath(".", parent + File.separator + database.getServerProps().getLessonPlan(), "media");
+    List<CommonExercise> exercises = database.getExercises();
+  }
+
+  @Test
+  public void testBest() {
+    try {
+      BufferedWriter writer = getWriter("bestScore");
+      List<ResultDAO.BestScore> resultForUser = database.getResultDAO().getResultForUser(71);
+      for (ResultDAO.BestScore bestScore : resultForUser) {
+        logger.info("got " + bestScore);
+        writer.write(bestScore.toCSV() + "\n");
+      }
+      writer.close();
+
+      UserPerformance performance = database.getResultDAO().getResultForUserByBin(71, ResultDAO.FIVE_MINUTES);
+      writer = getWriter("UserPerformance");
+      writer.write(performance.toCSV());
+      writer.close();
+
+      writer = getWriter("RawUserPerformance");
+      writer.write(performance.toRawCSV());
+      writer.close();
+
+    } catch (IOException e) {
+
+
+    }
+  }
+
+  private BufferedWriter getWriter(String prefix) throws IOException {
+    SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("MM_dd_yy_HH_mm_ss");
+    String today = simpleDateFormat2.format(new Date());
+    File file = getReportFile(new PathHelper("war"), today, prefix);
+    logger.info("writing to " + file.getAbsolutePath());
+    return new BufferedWriter(new FileWriter(file));
+  }
+
+  private File getReportFile(PathHelper pathHelper, String today, String prefix) {
+    File reports = pathHelper.getAbsoluteFile("reports");
+    if (!reports.exists()) {
+      logger.debug("making dir " + reports.getAbsolutePath());
+      reports.mkdirs();
+    } else {
+      logger.debug("reports dir exists at " + reports.getAbsolutePath());
+    }
+    String fileName = prefix + "_report_" + today + ".csv";
+    return new File(reports, fileName);
+  }
+}
