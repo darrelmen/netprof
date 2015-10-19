b4d0b845471003528d7292c061b88afb9773fd39
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-19 15:24:04 -0400

    final fixes to performance analysis graph
    Former-commit-id: 46d9e089b586836a207ce6abbfea882406ab3884 [formerly 0a21b6773ef21bf187cfed4ad294e5b639d12059] [formerly df588ace1e8d0e1774140f8e9e4740ba6391b469]
    Former-commit-id: 9047d3ac4708df09fec78bd0c85ea53ea0113d72 [formerly 0a7867f67b6499a8861bc99955feb416b9d68aff] [formerly dfc7af350a3c4b85ec758cb802ae53b6b6a80e33]
    Former-commit-id: c2d3ebe564cb7c6a8ea11f5eb7d29c47af2b5ea4
    Former-commit-id: 30c1f7811d9146baa234d521b6f9a484c7935289

--- src/mitll/langtest/shared/analysis/UserPerformance.java
+++ src/mitll/langtest/shared/analysis/UserPerformance.java
@@ -1,12 +1,10 @@
 package mitll.langtest.shared.analysis;
 
 import mitll.langtest.server.database.ResultDAO;
+import mitll.langtest.server.database.analysis.BestScore;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by go22670 on 10/19/15.
@@ -18,28 +16,47 @@ public class UserPerformance implements Serializable {
 
   private long userID;
 
-/*  public UserPerformance(List<TimeAndScore> timeAndScores) {
-    this.timeAndScores = timeAndScores;
-  }*/
 
   public UserPerformance(long userID) {
     this.userID = userID;
   }
 
+  public UserPerformance(long userID,List<BestScore> resultsForQuery) {
+    this(userID);
+    setRawBestScores(resultsForQuery);
+  }
+
   public UserPerformance() {
   }
 
-  public void addBestScores(List<ResultDAO.BestScore> bestScores) {
-    addTimeAndScore(new TimeAndScore(bestScores));
+  public void setAtBinSize(List<BestScore> resultsForQuery, long binSize) {
+    Map<Long, List<BestScore>> bins = new TreeMap<>();
+    for (BestScore bs : resultsForQuery) {
+      long dayBin = bs.getTimestamp() / binSize;
+      List<BestScore> bestScores = bins.get(dayBin);
+      if (bestScores == null) bins.put(dayBin, bestScores = new ArrayList<BestScore>());
+      bestScores.add(bs);
+    }
 
+    for (Map.Entry<Long, List<BestScore>> pair : bins.entrySet()) {
+      addBestScores(pair.getValue(), binSize);
+    }
+    setRawBestScores(resultsForQuery);
   }
 
-  public void addTimeAndScore(TimeAndScore ts) {
+  /**
+   * @param bestScores
+   * @see ResultDAO#getResultForUserByBin(long, int)
+   */
+  private void addBestScores(List<BestScore> bestScores, long binSize) {
+    addTimeAndScore(new TimeAndScore(bestScores, binSize));
+  }
+
+  private void addTimeAndScore(TimeAndScore ts) {
     timeAndScores.add(ts);
-
   }
 
-  public int getTotal() {
+  private int getTotal() {
     return getTotal(timeAndScores);
   }
 
@@ -49,17 +66,17 @@ public class UserPerformance implements Serializable {
     return total;
   }
 
-  public int getRawTotal() {
+  private int getRawTotal() {
     return getTotal(rawTimeAndScores);
 
   }
 
-  public float getAverage() {
+  private float getAverage() {
     float total = getTotalScore(timeAndScores);
     return total / (float) getTotal();
   }
 
-  public float getRawAverage() {
+  private float getRawAverage() {
     float total = getTotalScore(rawTimeAndScores);
     return total / (float) getRawTotal();
   }
@@ -77,48 +94,68 @@ public class UserPerformance implements Serializable {
     return total;
   }
 
-  public List<TimeAndScore> getTimeAndScores() {
+  private List<TimeAndScore> getTimeAndScores() {
     return timeAndScores;
   }
 
   public String toString() {
     StringBuilder builder = new StringBuilder();
     builder.append("For user " + getUserID() + " " + getTotal() + " items, avg score " + getAverage());
-    for (TimeAndScore ts : getTimeAndScores()) builder.append("\n").append(ts);
+    for (TimeAndScore ts : getTimeAndScores()) {
+      builder.append("\n").append(ts);
+    }
     return builder.toString();
   }
 
-  public long getUserID() {
+  private long getUserID() {
     return userID;
   }
 
+  private void setMoving() {
+    float total = 0;
+    float count = 0;
+    for (TimeAndScore ts : getTimeAndScores()) {
+      total += ts.getScore() * ts.getCount();
+      count += ts.getCount();
+      float moving = total / count;
+      ts.setMoving(moving);
+    }
+  }
+
   public String toCSV() {
+    setMoving();
     StringBuilder builder = new StringBuilder();
-    builder.append(",,," + userID + "," + getTotal() + "," + getAverage());
+    builder.append(",,,," + userID + "," + getTotal() + "," + getAverage());
     for (TimeAndScore ts : getTimeAndScores()) builder.append("\n").append(ts.toCSV());
     return builder.toString();
   }
 
   public String toRawCSV() {
     StringBuilder builder = new StringBuilder();
-    builder.append(",,," + userID + "," + getRawTotal() + "," + getRawAverage());
+    builder.append(",,,," + userID + "," + getRawTotal() + "," + getRawAverage());
     for (TimeAndScore ts : getRawBestScores()) builder.append("\n").append(ts.toCSV());
     return builder.toString();
   }
 
-  public void setRawBestScores(List<ResultDAO.BestScore> rawBestScores) {
+  private void setRawBestScores(List<BestScore> rawBestScores) {
+    Collections.sort(rawBestScores, new Comparator<BestScore>() {
+      @Override
+      public int compare(BestScore o1, BestScore o2) {
+        return Long.valueOf(o1.getTimestamp()).compareTo(o2.getTimestamp());
+      }
+    });
+
     float total = 0;
     float count = 0;
-    for (ResultDAO.BestScore bs : rawBestScores) {
+    for (BestScore bs : rawBestScores) {
       total += bs.getScore();
       count++;
       float moving = total / count;
       rawTimeAndScores.add(new TimeAndScore(bs, moving));
     }
-    Collections.sort(rawTimeAndScores);
   }
 
-  public List<TimeAndScore> getRawBestScores() {
+  private List<TimeAndScore> getRawBestScores() {
     return rawTimeAndScores;
   }
 }
