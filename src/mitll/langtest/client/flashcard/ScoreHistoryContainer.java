009e67f3874106609127fb90f2a4d1ed073be8be
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-20 19:58:11 -0400

    made english and fl columns separate in score review after avp, sortable columns
    Former-commit-id: 1eaab4bfbf871104e596e98d4e16a9252559c105 [formerly 55eea3afd439a2ab7b5488fb50dbb1ad0294e220] [formerly 184b03c3080c4a09d06b53450bd2593a6f92972b [formerly eaa192db9b52178ab6c19a8fa3d69c3194173c2d]]
    Former-commit-id: c22ae772e59a598e0cd788e0802956faa5e9469f [formerly 1f0e6691779bc159f7896890332cf464e25e7afc]
    Former-commit-id: bca703a8b82e71696819e477038aa8d957e58392

--- src/mitll/langtest/client/flashcard/ScoreHistoryContainer.java
+++ src/mitll/langtest/client/flashcard/ScoreHistoryContainer.java
@@ -6,6 +6,8 @@ import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
+import com.google.gwt.user.cellview.client.TextHeader;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Panel;
 import mitll.langtest.client.custom.TooltipHelper;
@@ -14,26 +16,34 @@ import mitll.langtest.client.exercise.PagingContainer;
 import mitll.langtest.client.exercise.SimplePagingContainer;
 import mitll.langtest.shared.CommonShell;
 import mitll.langtest.shared.flashcard.ExerciseCorrectAndScore;
+import mitll.langtest.shared.sorter.ExerciseComparator;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
  * Created by go22670 on 10/20/15.
  */
 class ScoreHistoryContainer extends SimplePagingContainer<ExerciseCorrectAndScore> {
-  private static final int MAX_LENGTH_ID = 30;
-  private static final int TABLE_HISTORY_WIDTH = 326;
-//  private static final int ONE_TABLE_WIDTH = 275;//-(TABLE_HISTORY_WIDTH/2);//275;
-  //private static final int TABLE_WIDTH = 2 * ONE_TABLE_WIDTH;
-  private final Map<String, String> idToExercise = new HashMap<String, String>();
+  private final Logger logger = Logger.getLogger("ScoreHistoryContainer");
+
+  private static final int MAX_LENGTH_ID = 15;
+  public static final int TABLE_HISTORY_WIDTH = 420;
+  public static final int COL_WIDTH = 120;
+  public static final String CORRECT_INCORRECT_HISTORY_AND_AVERAGE_PRONUNCIATION_SCORE = "Correct/Incorrect history and average pronunciation score";
+
+  private final Map<String, CommonShell> idToExercise = new HashMap<String, CommonShell>();
+  private ExerciseComparator sorter;
 
   public ScoreHistoryContainer(ExerciseController controller, List<CommonShell> allExercises) {
     super(controller);
+    sorter = new ExerciseComparator(controller.getStartupInfo().getTypeOrder());
 
     for (CommonShell commonShell : allExercises) {
-      idToExercise.put(commonShell.getID(), commonShell.getTooltip());
+      idToExercise.put(commonShell.getID(), commonShell);
     }
   }
 
@@ -42,6 +52,7 @@ class ScoreHistoryContainer extends SimplePagingContainer<ExerciseCorrectAndScor
     tableWithPager.getElement().setId("TableScoreHistory");
     tableWithPager.setWidth(TABLE_HISTORY_WIDTH + "px");
     tableWithPager.addStyleName("floatLeft");
+
     for (ExerciseCorrectAndScore exerciseCorrectAndScore : sortedHistory) {
       addItem(exerciseCorrectAndScore);
     }
@@ -56,53 +67,195 @@ class ScoreHistoryContainer extends SimplePagingContainer<ExerciseCorrectAndScor
     return o;
   }
 
+  private void addEnglishAndFL() {
+    Column<ExerciseCorrectAndScore, SafeHtml> englishCol = getEnglishColumn();
+    englishCol.setSortable(true);
+    table.setColumnWidth(englishCol, COL_WIDTH +
+        "px");
+
+    String language = controller.getLanguage();
+    addColumn(englishCol, new TextHeader("English"));
+
+    Column<ExerciseCorrectAndScore, SafeHtml> flColumn = getFLColumn();
+    flColumn.setSortable(true);
+    table.setColumnWidth(flColumn,COL_WIDTH +
+        "px");
+
+    String headerForFL = language.equals("English") ? "Meaning" : language;
+    addColumn(flColumn, new TextHeader(headerForFL));
+
+    List<ExerciseCorrectAndScore> dataList = getList();
+
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler = getEnglishSorter(englishCol, dataList);
+    table.addColumnSortHandler(columnSortHandler);
+
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler2 = getFLSorter(flColumn, dataList);
+    table.addColumnSortHandler(columnSortHandler2);
+
+    // We know that the data is sorted alphabetically by default.
+//    table.getColumnSortList().push(englishCol);
+  }
+
+  private String truncate(String columnText) {
+    if (columnText.length() > MAX_LENGTH_ID) columnText = columnText.substring(0, MAX_LENGTH_ID - 3) + "...";
+    return columnText;
+  }
+
+  private ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> getEnglishSorter(Column<ExerciseCorrectAndScore, SafeHtml> englishCol,
+                                                                                List<ExerciseCorrectAndScore> dataList) {
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler = new ColumnSortEvent.ListHandler<ExerciseCorrectAndScore>(dataList);
+    columnSortHandler.setComparator(englishCol,
+        new Comparator<ExerciseCorrectAndScore>() {
+          public int compare(ExerciseCorrectAndScore o1, ExerciseCorrectAndScore o2) {
+            if (o1 == o2) {
+              return 0;
+            }
+
+            // Compare the name columns.
+            if (o1 != null) {
+              if (o2 == null) return 1;
+              else {
+                CommonShell shell1 = idToExercise.get(o1.getId());
+                CommonShell shell2 = idToExercise.get(o2.getId());
+
+                return sorter.compareStrings(shell1.getEnglish(), shell2.getEnglish());
+              }
+            }
+            return -1;
+          }
+        });
+    return columnSortHandler;
+  }
+
+  private ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> getFLSorter(Column<ExerciseCorrectAndScore, SafeHtml> flColumn,
+                                                                           List<ExerciseCorrectAndScore> dataList) {
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler2 = new ColumnSortEvent.ListHandler<ExerciseCorrectAndScore>(dataList);
+
+    columnSortHandler2.setComparator(flColumn,
+        new Comparator<ExerciseCorrectAndScore>() {
+          public int compare(ExerciseCorrectAndScore o1, ExerciseCorrectAndScore o2) {
+            if (o1 == o2) {
+              return 0;
+            }
+
+            // Compare the name columns.
+            if (o1 != null) {
+              if (o2 == null) return 1;
+              else {
+                CommonShell shell1 = idToExercise.get(o1.getId());
+                CommonShell shell2 = idToExercise.get(o2.getId());
+
+                String id1 = shell1.getForeignLanguage();
+                String id2 = shell2.getForeignLanguage();
+                return id1.toLowerCase().compareTo(id2.toLowerCase());
+              }
+            }
+            return -1;
+          }
+        });
+    return columnSortHandler2;
+  }
+
+  private ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> getScoreSorter(Column<ExerciseCorrectAndScore, SafeHtml> scoreCol,
+                                                                                List<ExerciseCorrectAndScore> dataList) {
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler = new ColumnSortEvent.ListHandler<ExerciseCorrectAndScore>(dataList);
+    columnSortHandler.setComparator(scoreCol,
+        new Comparator<ExerciseCorrectAndScore>() {
+          public int compare(ExerciseCorrectAndScore o1, ExerciseCorrectAndScore o2) {
+            if (o1 == o2) {
+              return 0;
+            }
+
+            if (o1 != null) {
+              if (o2 == null) {
+                return -1;
+              }
+              else {
+                int a1 = o1.getAvgScorePercent();
+                int a2 = o2.getAvgScorePercent();
+                int i = Integer.valueOf(a1).compareTo(a2);
+               // logger.info("a1 " + a1 + " vs " + a2 + " i " + i);
+                if (i == 0) {
+                  CommonShell shell1 = idToExercise.get(o1.getId());
+                  CommonShell shell2 = idToExercise.get(o2.getId());
+                  return shell1.getEnglish().compareTo(shell2.getEnglish());
+                }
+                else {
+                  return i;
+                }
+              }
+            }
+            else {
+              return -1;
+            }
+          }
+        });
+    return columnSortHandler;
+  }
+
+
   @Override
   protected void addColumnsToTable() {
-    Column<ExerciseCorrectAndScore, SafeHtml> column = getColumn();
-    column.setSortable(true);
+    addEnglishAndFL();
 
-    table.setWidth("100%", true);
-    table.addColumn(column, "Item");
-    table.setColumnWidth(column, "180px");
-
-    Column<ExerciseCorrectAndScore, SafeHtml> column2 = getColumn2();
-    column2.setSortable(true);
+    Column<ExerciseCorrectAndScore, SafeHtml> column2 = getHistoryColumn();
     table.addColumn(column2, "History");
     table.setColumnWidth(column2, "88px");
 
+    Column<ExerciseCorrectAndScore, SafeHtml> scoreColumn = getScoreColumn();
+    table.addColumn(scoreColumn, "Score");
 
-    Column<ExerciseCorrectAndScore, SafeHtml> column3 = getColumn3();
-    table.addColumn(column3, "Score");
-    column3.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-    column3.setSortable(true);
-    table.setColumnWidth(column3, "70" + "px");
+    scoreColumn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+    scoreColumn.setSortable(true);
+    table.setColumnWidth(scoreColumn, "70" + "px");
+    table.setWidth("100%", true);
 
-    new TooltipHelper().addTooltip(table, "Correct/Incorrect history and average pronunciation score");
+    ColumnSortEvent.ListHandler<ExerciseCorrectAndScore> columnSortHandler2 = getScoreSorter(scoreColumn,  getList());
+    table.addColumnSortHandler(columnSortHandler2);
+
+    new TooltipHelper().addTooltip(table, CORRECT_INCORRECT_HISTORY_AND_AVERAGE_PRONUNCIATION_SCORE);
   }
 
-  private Column<ExerciseCorrectAndScore, SafeHtml> getColumn() {
+  private Column<ExerciseCorrectAndScore, SafeHtml> getEnglishColumn() {
     return new Column<ExerciseCorrectAndScore, SafeHtml>(new SafeHtmlCell()) {
       @Override
       public SafeHtml getValue(ExerciseCorrectAndScore shell) {
-        String columnText = idToExercise.get(shell.getId());
-        String html = shell.getId();
-        if (columnText != null) {
-          if (columnText.length() > MAX_LENGTH_ID)
-            columnText = columnText.substring(0, MAX_LENGTH_ID - 3) + "...";
-
-          html = "<span style='float:left;" +
-              //"margin-left:-10px;" +
-              "'>" + columnText + "</span>";
-        }
-        return new SafeHtmlBuilder().appendHtmlConstant(html).toSafeHtml();
+        CommonShell shell1 = idToExercise.get(shell.getId());
+        String columnText = shell1.getEnglish();
+        return getSafeHtml(shell, truncate(columnText));
       }
     };
   }
 
-  private Column<ExerciseCorrectAndScore, SafeHtml> getColumn2() {
-
+  private Column<ExerciseCorrectAndScore, SafeHtml> getFLColumn() {
     return new Column<ExerciseCorrectAndScore, SafeHtml>(new SafeHtmlCell()) {
+      @Override
+      public SafeHtml getValue(ExerciseCorrectAndScore shell) {
+        CommonShell shell1 = idToExercise.get(shell.getId());
+        String toShow = shell1.getForeignLanguage();
 
+        if (controller.getLanguage().equalsIgnoreCase("english")) {
+          toShow = shell1.getMeaning();
+        }
+        String columnText = truncate(toShow);
+
+        return getSafeHtml(shell, columnText);
+      }
+    };
+  }
+
+  private SafeHtml getSafeHtml(ExerciseCorrectAndScore shell, String columnText) {
+    String html = shell.getId();
+    if (columnText != null) {
+      if (columnText.length() > MAX_LENGTH_ID)
+        columnText = columnText.substring(0, MAX_LENGTH_ID - 3) + "...";
+      html = "<span style='float:left;" + "'>" + columnText + "</span>";
+    }
+    return new SafeHtmlBuilder().appendHtmlConstant(html).toSafeHtml();
+  }
+
+  private Column<ExerciseCorrectAndScore, SafeHtml> getHistoryColumn() {
+    return new Column<ExerciseCorrectAndScore, SafeHtml>(new SafeHtmlCell()) {
       @Override
       public SafeHtml getValue(ExerciseCorrectAndScore shell) {
         String history = SetCompleteDisplay.getScoreHistory(shell.getCorrectAndScores());
@@ -115,7 +268,7 @@ class ScoreHistoryContainer extends SimplePagingContainer<ExerciseCorrectAndScor
     };
   }
 
-  private Column<ExerciseCorrectAndScore, SafeHtml> getColumn3() {
+  private Column<ExerciseCorrectAndScore, SafeHtml> getScoreColumn() {
     return new Column<ExerciseCorrectAndScore, SafeHtml>(new SafeHtmlCell()) {
       @Override
       public SafeHtml getValue(ExerciseCorrectAndScore shell) {
