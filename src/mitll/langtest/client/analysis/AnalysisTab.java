c6e23102569f44e4766f53700011f416e979c91f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-22 13:22:12 -0400

    click on an item in word list in analysis to review and study again
    Former-commit-id: bb3d189ecd5fb01447facb0369fea2ec3f258559 [formerly e0a06f75b611657d4507c83696374a4ca3151961] [formerly 4ef42b2ad19b6f2487a9784621a5211f7dca1dfd]
    Former-commit-id: f462f6b1b775ecf7a11e6dad3a703073807a6f18 [formerly 57d17d28c82db0f10c448e7fa06d24505bf1892b] [formerly 730682c727bd9c91e9d1db49e5fdca330e0f3174]
    Former-commit-id: c32dda6fad98994b9956bda70e8ea95e9bb801b0
    Former-commit-id: beb1181e8498cce346755674fbcf34a0e8663ad3

--- src/mitll/langtest/client/analysis/AnalysisTab.java
+++ src/mitll/langtest/client/analysis/AnalysisTab.java
@@ -5,8 +5,9 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.custom.AnalysisPlot;
+import mitll.langtest.client.custom.Navigation;
+import mitll.langtest.client.custom.tabs.TabAndContent;
 import mitll.langtest.client.exercise.ExerciseController;
-import mitll.langtest.client.list.ListInterface;
 import mitll.langtest.shared.analysis.WordScore;
 
 import java.util.List;
@@ -15,12 +16,18 @@ import java.util.List;
  * Created by go22670 on 10/21/15.
  */
 public class AnalysisTab extends DivWidget {
-  public AnalysisTab(LangTestDatabaseAsync service, ExerciseController controller, ListInterface list, int userid) {
+  /**
+   * @see Navigation#showAnalysis()
+   * @param service
+   * @param controller
+   * @param userid
+   */
+  public AnalysisTab(LangTestDatabaseAsync service, ExerciseController controller, int userid, ShowTab showTab) {
     AnalysisPlot analysisPlot = new AnalysisPlot(service, userid);
-    final WordContainer wordContainer = new WordContainer(controller,analysisPlot);
-    //   analysis.getContent().add(analysisPlot);
+    final WordContainer wordContainer = new WordContainer(controller,analysisPlot,showTab);
 
     add(analysisPlot);
+
     service.getWordScores(userid, new AsyncCallback<List<WordScore>>() {
       @Override
       public void onFailure(Throwable throwable) {
