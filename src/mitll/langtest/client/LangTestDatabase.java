a6bbdfbeb25b2e6099d7bf2ce0765bde56455214
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-22 16:08:56 -0400

    added call to get phone report for analysis
    Former-commit-id: d4bd188ee908735e701946f8cf11b0ea5febb45c [formerly c82a509598fcc5083637676bfbf0fa0ae4d0a8ad]
    Former-commit-id: 9b667fbef2469d734b02914b7125fe42ca6c8363

--- src/mitll/langtest/client/LangTestDatabase.java
+++ src/mitll/langtest/client/LangTestDatabase.java
@@ -3,6 +3,7 @@ package mitll.langtest.client;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import mitll.langtest.shared.*;
+import mitll.langtest.shared.analysis.PhoneReport;
 import mitll.langtest.shared.analysis.UserPerformance;
 import mitll.langtest.shared.analysis.WordScore;
 import mitll.langtest.shared.custom.UserExercise;
@@ -104,6 +105,8 @@ public interface LangTestDatabase extends RemoteService {
   UserPerformance getPerformanceForUser(long id);
 
   List<WordScore> getWordScores(long id);
+
+  PhoneReport getPhoneScores(long id);
 
   void logMessage(String message);
   void logEvent(String id, String widgetType, String exid, String context, long userid, String hitID, String device);
