8246cbae6aa1580452fd609fc015d785cfb63fd9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-26 15:35:38 -0400

    added play audio column
    Former-commit-id: c5a2b0fb92fd97501368f6b0ff82e76e9767900e [formerly 22a8f2eefb65b888985a593aec3564f619ae66b9] [formerly 2727d30b293ddd8003f3c15eba3cd379c26600ca [formerly c1a13265353e85ad22f8ec9aac6ed1ed7ee4f51c]]
    Former-commit-id: c0b92008227d2ffcbe57b26475249b869c0b42db [formerly 2e9c87d5410a62bf3ce8e6929edff2171702b5b8] [formerly 2a869855096205d475bdd161a88470a3d8954796 [formerly e663e4f3220a5d4c0e6aed34afdf36ab2a58f5a3]]
    Former-commit-id: 8e6fb0f7c6574735813d357cd01307910135325f [formerly d03515b1a3038c079bd2106f4ddb27aa72fe7571]
    Former-commit-id: 528d6f909a7957682a3bc3f384e5d521d77e371a

--- src/mitll/langtest/shared/analysis/WordScore.java
+++ src/mitll/langtest/shared/analysis/WordScore.java
@@ -9,8 +9,10 @@ import java.util.Map;
 
 /**
  * Created by go22670 on 10/21/15.
+ * @see mitll.langtest.client.analysis.WordContainer#getTableWithPager(List)
  */
 public class WordScore implements Serializable, Comparable<WordScore>{
+  private String fileRef;
   private String id;
   private long timestamp;
   private float pronScore;
@@ -26,14 +28,15 @@ public class WordScore implements Serializable, Comparable<WordScore>{
    * @param netPronImageTypeListMap
    */
   public WordScore(BestScore bs, Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeListMap) {
-    this(bs.getId(), bs.getScore(), bs.getTimestamp(), bs.getResultID(), netPronImageTypeListMap);
+    this(bs.getId(), bs.getScore(), bs.getTimestamp(), bs.getResultID(), bs.getFileRef(), netPronImageTypeListMap);
   }
 
-  public WordScore(String id, float pronScore, long timestamp, int resultID, Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeListMap) {
+  private WordScore(String id, float pronScore, long timestamp, int resultID, String fileRef, Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeListMap) {
     this.id = id;
     this.pronScore = (pronScore < 0) ? 0 : pronScore;
     this.timestamp = timestamp;
     this.resultID = resultID;
+    this.fileRef = fileRef;
     this.netPronImageTypeListMap = netPronImageTypeListMap;
   }
 
@@ -53,10 +56,10 @@ public class WordScore implements Serializable, Comparable<WordScore>{
   public float getPronScore() {
     return pronScore;
   }
-
+/*
   public int getResultID() {
     return resultID;
-  }
+  }*/
 
   public Map<NetPronImageType, List<TranscriptSegment>> getNetPronImageTypeListMap() {
     return netPronImageTypeListMap;
@@ -64,4 +67,8 @@ public class WordScore implements Serializable, Comparable<WordScore>{
 
   public String toString() { return id +"/" +resultID + " score " + pronScore+
       "  : " + netPronImageTypeListMap; }
+
+  public String getFileRef() {
+    return fileRef;
+  }
 }
