22098943f48649da0ba901002aae0cd325e0e7cd
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-10-27 18:32:09 -0400

    better phone count and better user map
    Former-commit-id: 97f30c58e042de91fea186438b9338d1a3db0078 [formerly fa1bfd01faaad66311f6d17645f348a5b318de0f] [formerly 7d87e415ec2e6da5261cd573dc5c2c592452c794 [formerly f25682610827a21c6ef0279b897f4521d82f62fb]] [formerly 97f30c58e042de91fea186438b9338d1a3db0078 [formerly fa1bfd01faaad66311f6d17645f348a5b318de0f] [formerly 7d87e415ec2e6da5261cd573dc5c2c592452c794 [formerly f25682610827a21c6ef0279b897f4521d82f62fb]] [formerly fbd8604eb7808411f875985b516f555da08c3f08 [formerly 75977dee9896da31df5ca87032269c94bff23df2] [formerly b541e02d83f811e0a4d1be6bdf0bc95e0d9596b9]]]
    Former-commit-id: f982c2c9a6d394082776723d8d6fa12e376622c6 [formerly 320548b1336be3125ab2ae12b6737e3c7baf6d4b] [formerly d18aa017a1ebbb69a0c245565da1bbee0ad2e281 [formerly 496f02e1eec5cfe9caa6b9f0fba56a4e842208a7]] [formerly 96acdff6af19796d384b8aefffc9c5c71678238d [formerly 7fbff415c68cbebef1000db5d9610546b62c6113]] [formerly 178ec72a0d8bd20fd63834e0f1c262890e9ebfbb [formerly 47abde43f5a760b5a00e9092a7332149d914a1fd] [formerly 2269e9586c8101e90c5a8df56145911d293eec0d [formerly b3b5209e4b4e70359daddf58dd70348ec79fed2d]] [formerly 6295b9c905718c967f0af0c41887351368153382 [formerly c1b450136830a0a494642aaed3bf13546f130e14]]]
    Former-commit-id: 178ec72a0d8bd20fd63834e0f1c262890e9ebfbb [formerly 47abde43f5a760b5a00e9092a7332149d914a1fd] [formerly 2269e9586c8101e90c5a8df56145911d293eec0d [formerly b3b5209e4b4e70359daddf58dd70348ec79fed2d]]
    Former-commit-id: 44f0889ad09d33f85d4dea33fd995b7b8b4cf21e [formerly 3cfe65e6bf5c941d3780cdcc336b63502d067349]
    Former-commit-id: 2bc11a03516a151de49186a6c42e3773d80df09f [formerly 6fb72f4fa3c2ffdc6211f6b081a28b8e38076f04]
    Former-commit-id: 02c7083e65511decba4ca8f3d2f0492aa026fad7

--- src/mitll/langtest/client/analysis/StudentAnalysis.java
+++ src/mitll/langtest/client/analysis/StudentAnalysis.java
@@ -7,8 +7,8 @@ import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.shared.User;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -19,50 +19,26 @@ public class StudentAnalysis extends DivWidget {
 
   public StudentAnalysis(final LangTestDatabaseAsync service, final ExerciseController controller,
                          final ShowTab showTab) {
-    service.getUserToResultCount(new AsyncCallback<Map<User, Integer>>() {
+    service.getUsersWithRecordings(new AsyncCallback<Collection<User>>() {
       @Override
       public void onFailure(Throwable throwable) {
-
+        logger.warning("Got " + throwable);
       }
 
       @Override
-      public void onSuccess(Map<User, Integer> userIntegerMap) {
+      public void onSuccess(Collection<User> users) {
+//        logger.info("got users num =  " + users.size());
         DivWidget rightSide = new DivWidget();
         UserContainer userContainer = new UserContainer(service, controller, rightSide, showTab);
-        List<User> users = new ArrayList<User>();
-        //HorizontalPanel hp = new HorizontalPanel();
-        //add(hp);
-
-        for (Map.Entry<User, Integer> pair : userIntegerMap.entrySet()) {
-          if (pair.getValue() > 0) {
-            User key = pair.getKey();
-          //  if (!key.getUserID().equals("gvidaver")) {
-            if (!key.isTeacher()) {
-              users.add(key);
-            }
-          //  }
+        List<User> filtered = new ArrayList<User>();
+        for (User user : users) {
+          if (!user.getUserID().equals("defectDetector")) {
+            filtered.add(user);
           }
         }
-        // hp.add(userContainer.getTableWithPager(users));
-        add(userContainer.getTableWithPager(users));
-
-        // hp.add(rightSide);
+        add(userContainer.getTableWithPager(filtered));
         add(rightSide);
       }
     });
-
-//    service.getWordScores(userid, new AsyncCallback<List<WordScore>>() {
-//      @Override
-//      public void onFailure(Throwable throwable) {
-//        logger.warning("Got " + throwable);
-//      }
-//
-//      @Override
-//      public void onSuccess(List<WordScore> wordScores) {
-//        final WordContainer wordContainer = new WordContainer(controller, analysisPlot, showTab);
-//        lowerHalf.add(wordContainer.getTableWithPager(wordScores));
-//        getPhoneReport(service, controller, userid, lowerHalf, analysisPlot, showTab);
-//      }
-//    });
   }
 }
