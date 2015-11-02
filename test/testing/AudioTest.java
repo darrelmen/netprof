def3d01992c64a370a83db8aa069ef4dfc7c112e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-02 18:13:24 -0500

    updated snr test
    Former-commit-id: 1b497cf0cbbeaf06f686b8efefc58402349a144c [formerly 7a4fe31b02805644e32a4c931c7bbbffd9485b42] [formerly 1af3b3f3f099d6b214de546683791945bf8cd979 [formerly 14e4094756a1ca14d9dff6b3f3c6145b688061fa]] [formerly f1a06d170944f3c153d588460d69dda7cdb05441]
    Former-commit-id: d1b1f7d0de0319aa2a30640264955f2ca37293c0
    Former-commit-id: a6776ea59ff0307d257c3f6d7f7c2085b01f5654 [formerly 50aba810a339e285af9ad83008eb92b5d74fda77]
    Former-commit-id: f5345695490fd3784c53c32423b5acfde44f4c6f

--- test/testing/AudioTest.java
+++ test/testing/AudioTest.java
@@ -1,11 +1,15 @@
 package testing;
 
+import mitll.langtest.server.audio.AudioConversion;
 import mitll.langtest.server.audio.DynamicRange;
 import org.junit.Test;
 
 import java.io.File;
 
 /**
+ *
+ *    String highPassFilterFile = conversion.getHighPassFilterFile(file.getAbsolutePath());
+ DynamicRange.RMSInfo dynamicRange = new DynamicRange().getDynamicRange(new File(highPassFilterFile));
  * Created by go22670 on 10/30/15.
  */
 public class AudioTest {
@@ -18,8 +22,26 @@ public class AudioTest {
         if (f.getName().endsWith(".wav")) {
           System.out.println("Got " + f.getName());
           new DynamicRange().getDynamicRange(f);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-         // break;
+  @Test
+  public void testRMS2() {
+    String file = "/Users/go22670/netPron2/snrs-no-hp";
+    try {
+      File file1 = new File(file);
+      File[] files = file1.listFiles();
+      for (File f : files) {
+        if (f.getName().endsWith(".wav")) {
+          System.out.println("Got " + f.getName());
+          String highPassFilterFile = new AudioConversion().getHighPassFilterFile(f.getAbsolutePath());
+          File file2 = new File(highPassFilterFile);
+        // File file2=f;
+          DynamicRange.RMSInfo dynamicRange = new DynamicRange().getDynamicRange(file2);
         }
       }
     } catch (Exception e) {
