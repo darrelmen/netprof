03f62c4a18781948e5097f9f99d84e9890be99e3
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-04 18:05:33 -0500

    layout improvements to analysis, shows proper dates for people, headings for sections
    Former-commit-id: 298070596c241e7842eb0bf018ec0b6ea8e2b59d
    Former-commit-id: b97f9a4f69b79422b3741b3aef7b99972ae8728b

--- src/mitll/langtest/client/analysis/StudentAnalysis.java
+++ src/mitll/langtest/client/analysis/StudentAnalysis.java
@@ -1,7 +1,12 @@
 package mitll.langtest.client.analysis;
 
+import com.github.gwtbootstrap.client.ui.Heading;
 import com.github.gwtbootstrap.client.ui.base.DivWidget;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HeaderPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.shared.User;
@@ -28,7 +33,6 @@ public class StudentAnalysis extends DivWidget {
 
       @Override
       public void onSuccess(Collection<UserInfo> users) {
-//        logger.info("got users num =  " + users.size());
         DivWidget rightSide = new DivWidget();
         UserContainer userContainer = new UserContainer(service, controller, rightSide, showTab);
         List<UserInfo> filtered = new ArrayList<UserInfo>();
@@ -41,7 +45,17 @@ public class StudentAnalysis extends DivWidget {
             logger.warning("skip " + user);
           }
         }
-        add(userContainer.getTableWithPager(filtered));
+        Heading students = new Heading(3, "Students", "5 or more recordings");
+        students.setWidth("300px");
+        //VerticalPanel leftSide = new VerticalPanel();
+        DivWidget leftSide = new DivWidget();
+        leftSide.add(students);
+        Panel tableWithPager = userContainer.getTableWithPager(filtered);
+        leftSide.add(tableWithPager);
+
+        add(leftSide);
+        rightSide.getElement().getStyle().setMarginTop(-40, Style.Unit.PX);
+        rightSide.getElement().getStyle().setMarginLeft(540, Style.Unit.PX);
         add(rightSide);
       }
     });
