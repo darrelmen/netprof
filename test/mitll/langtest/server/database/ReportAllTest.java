baa5f112d10cf1666d06703c149c7223eff47438
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-05 11:06:21 -0500

    fixed issue where we want to filter out users who don't record very much for student analysis but not for self analysis
    Former-commit-id: 8ba3e6201ff6e6ac98484dfc5859c42bc7e359a3 [formerly ddf73c354df5ecce41f01ec86c20e953d3aafd5b] [formerly 8ba3e6201ff6e6ac98484dfc5859c42bc7e359a3 [formerly ddf73c354df5ecce41f01ec86c20e953d3aafd5b] [formerly 2a2d5be1f13accf6ca735fd557afaf1e6a658137 [formerly 45e74b12783488d3cccf40b1fbc33259039960b2]]]
    Former-commit-id: ffc03bde6dd2987d8a0be5d7422b1250a09375a6 [formerly bd2a83883c1259c6db6a87d324c9a9dcd49d8246] [formerly 41fbe87ba69fe8fb51cb756239fd4adb23e6c87e [formerly 18c4f37d23ea508719ca026f396dc1cd8509db91]] [formerly d9a94584b84d2cb046ca5e8f53e4af695142f7ba [formerly 71453d76768f07a426c939f50a179515296e3bd4] [formerly 5710729be6bd1f354d285b98f8ccbfb3f1984379 [formerly 7bb56a2efbfdbeb2a21f8c6c8e61e2224215699b]]]
    Former-commit-id: d9a94584b84d2cb046ca5e8f53e4af695142f7ba [formerly 71453d76768f07a426c939f50a179515296e3bd4]
    Former-commit-id: 484c26389906cbf07417262d56460b879c97f6b9 [formerly ad5c5bca1afcc151f6ffb6e878780ed402380148]
    Former-commit-id: 45e2484bf88c8d388c749c6938b974f31f83fdcf

---
+++ test/mitll/langtest/server/database/ReportAllTest.java
@@ -0,0 +1,150 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.server.database.connection.DatabaseConnection;
+import mitll.langtest.server.database.connection.H2Connection;
+import mitll.langtest.shared.CommonExercise;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by GO22670 on 1/30/14.
+ */
+public class ReportAllTest extends  BaseTest {
+  private static final Logger logger = Logger.getLogger(ReportAllTest.class);
+
+  @Test
+  public void testReports() {
+    List<String> strings = getDBs();
+    logger.debug("Got " + strings);
+
+    List<String> configs = Arrays.asList(
+        "dari",
+        "egyptian",
+        "english",
+        "farsi",
+        "iraqi",
+        "korean",
+        "levantine",
+        "mandarin",
+        "msa",
+        "pashto1", "pashto2", "pashto3",
+
+        "russian"
+
+        , "spanish", "sudanese", "tagalog", "urdu"
+    );
+    int i = 0;
+    PathHelper war = new PathHelper("war");
+
+    //  configs = Collections.singletonList("spanish");
+
+    for (String db : strings) {
+      //String path = "/Users/go22670/Development/asr/performance-reports/dbs/" + db;
+      String config = configs.get(i++);
+
+      logger.info("doing " + config + " ------- ");
+
+      H2Connection connection = getH2(db);
+
+      DatabaseImpl database = getDatabase(connection, config, db);
+      database.doReport(war);
+
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      //  break;
+    }
+  }
+
+  @Test
+  public void testYTD() {
+    List<String> strings = getDBs();
+    logger.debug("Got " + strings);
+
+    List<String> configs = Arrays.asList(
+        "dari",
+        "egyptian",
+        "english",
+        "farsi",
+        "iraqi",
+        "korean",
+        "levantine",
+        "mandarin",
+        "msa",
+        "pashto1", "pashto2", "pashto3",
+
+        "russian"
+
+        , "spanish", "sudanese", "tagalog", "urdu"
+    );
+    int i = 0;
+    // PathHelper war = new PathHelper("war");
+
+    //  configs = Collections.singletonList("spanish");
+    Map<String, Integer> configToUsers = new TreeMap<>();
+    for (String db : strings) {
+      //String path = "/Users/go22670/Development/asr/performance-reports/dbs/" + db;
+      String config = configs.get(i++);
+
+      logger.info("doing " + config + " ------- ");
+
+      H2Connection connection = getH2(db);
+
+      DatabaseImpl database = getDatabase(connection, config, db);
+      int activeUsersYTD = database.getReport().getActiveUsersYTD();
+      logger.info(config + "," + activeUsersYTD);
+      configToUsers.put(config, activeUsersYTD);
+
+//      try {
+//      //  Thread.sleep(1000);
+//      } catch (InterruptedException e) {
+//        e.printStackTrace();
+//      }
+      //  break;
+    }
+    logger.info(configToUsers);
+  }
+
+  private H2Connection getH2(String db) {
+    String path = "../Development/asr/performance-reports/dbs/" + db.replaceAll(".h2.db", "");
+    logger.debug("got " + path);
+    return getH2Connection(path);
+  }
+
+  private List<String> getDBs() {
+    String s =
+        "npfDari.h2.db\n" +
+            "npfClassroomEgyptian.h2.db\n" +
+            "npfEnglish.h2.db\n" +
+            "npfFarsi.h2.db\n" +
+            "iraqi.h2.db\n" +
+            "npfLevantine.h2.db\n" +
+            "npfKorean.h2.db\n" +
+            "mandarin.h2.db\n" +
+            "msaClassroom.h2.db\n" +
+            "pashtoCE.h2.db\n" +
+            "pashto2.h2.db\n" +
+            "pashto3.h2.db\n" +
+            "npfRussian.h2.db\n"
+            +
+            "npfSpanish.h2.db\n" +
+
+            "sudaneseToday.h2.db\n" +
+            "npfTagalog.h2.db\n" +
+            "npfUrdu.h2.db\n";
+    String[] split = s.split("\n");
+    List<String> strings = Arrays.asList(split);
+    //  strings = Collections.singletonList("npfSpanish.h2.db");
+    return strings;
+  }
+}
