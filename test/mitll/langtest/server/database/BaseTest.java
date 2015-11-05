28c80cad694440587d8e0b89404d706a0023b91b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-05 11:06:21 -0500

    fixed issue where we want to filter out users who don't record very much for student analysis but not for self analysis
    Former-commit-id: f78403c2c5668b25095a236fc6420c24df8a1625 [formerly 1ec7e5f23cabe60f53d0f739a87332bcde6084e2] [formerly e4b6b6d8486eda61e78cf0997a7c343f736cd6dd [formerly 92290e5600cd15d5ff34ee407fb604924011845d]] [formerly f78403c2c5668b25095a236fc6420c24df8a1625 [formerly 1ec7e5f23cabe60f53d0f739a87332bcde6084e2] [formerly e4b6b6d8486eda61e78cf0997a7c343f736cd6dd [formerly 92290e5600cd15d5ff34ee407fb604924011845d]] [formerly f78403c2c5668b25095a236fc6420c24df8a1625 [formerly 1ec7e5f23cabe60f53d0f739a87332bcde6084e2] [formerly e4b6b6d8486eda61e78cf0997a7c343f736cd6dd [formerly 92290e5600cd15d5ff34ee407fb604924011845d]] [formerly 9baf94fc2e4816a958ab20db17c6b15718ad9246 [formerly c378902baba81d8a409fe3610dfb9166bcf29c24] [formerly c3a4be89cc1dcf833491c381d3dc444f43eebf10]]]]
    Former-commit-id: ebd73770c880ba20c800ef0504d149f926bc863f [formerly aeb749a70ac739777f6367151516abdaa72c9d9f] [formerly 34f3ba4680b5999a6e35d4d1842a041eb9b1c66f [formerly 8e4b630cd79c5b6b6d6da5754129e26d473c5ed4]] [formerly ebd73770c880ba20c800ef0504d149f926bc863f [formerly aeb749a70ac739777f6367151516abdaa72c9d9f] [formerly 34f3ba4680b5999a6e35d4d1842a041eb9b1c66f [formerly 8e4b630cd79c5b6b6d6da5754129e26d473c5ed4]] [formerly e8066a9343cf8aaab5bbd4fd10fd5f0f30cccd9c [formerly 03ae43e58bd6335ddd9da4c935f5bbbd1c74f5fd]]] [formerly a8fd26378febbadf4ffc6137eedd42b277ba0512 [formerly b6cb38baef3dfbaaa459e8032303cd736d370679] [formerly 1197261518a149b1d80e4070e536377ce6677bfc [formerly 20bfb3a3f91362647c8e2d68622a690b67fd218c]] [formerly a8fd26378febbadf4ffc6137eedd42b277ba0512 [formerly b6cb38baef3dfbaaa459e8032303cd736d370679] [formerly 1197261518a149b1d80e4070e536377ce6677bfc [formerly 20bfb3a3f91362647c8e2d68622a690b67fd218c]] [formerly 1663e9c73fbc6f4da9dc1007504817f44373d9e7 [formerly f2ca23e1832ac3d1ae4ab8191216c2250a1b80c4]]]]
    Former-commit-id: a8fd26378febbadf4ffc6137eedd42b277ba0512 [formerly b6cb38baef3dfbaaa459e8032303cd736d370679] [formerly 1197261518a149b1d80e4070e536377ce6677bfc [formerly 20bfb3a3f91362647c8e2d68622a690b67fd218c]]
    Former-commit-id: 434f0b81a85754ed81f4d06091e51a6fd438f411 [formerly c13ae5f91c4e25d489ba2c3fd26195b0a0c580a5] [formerly be37dd520afc9a6eb475ed07f036bfe46b458d01 [formerly 0a925de57719de2a89c5c76d84f54d62a226bd2f]] [formerly 1a320e7659bf99961330d750e13e312be0d0ad6b [formerly dd8ed07286b091c91921d2c077a2189a8454b46f]]
    Former-commit-id: 6db50df4747f231ead8aa41396584b6b9c1fa3c3 [formerly 8f2c394a174c14eab81cbe3bdc462e1239a556dc]
    Former-commit-id: 01dc6e6fdf9fa34860acdeb5bce5a08b12b55993 [formerly 883bfab7e071e139d1e1799f808df595699e4793]
    Former-commit-id: aa5c082f69452fb5aab80a55cdbc8c1d3bfac805

---
+++ test/mitll/langtest/server/database/BaseTest.java
@@ -0,0 +1,81 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.server.database.connection.DatabaseConnection;
+import mitll.langtest.server.database.connection.H2Connection;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by go22670 on 11/5/15.
+ */
+public class BaseTest {
+  private static final Logger logger = Logger.getLogger(BaseTest.class);
+
+  protected void finish(Connection connection, Statement statement, ResultSet rs) throws SQLException {
+    rs.close();
+    statement.close();
+  }
+
+  protected BufferedWriter getWriter(String prefix) throws IOException {
+    SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("MM_dd_yy_HH_mm_ss");
+    String today = simpleDateFormat2.format(new Date());
+    File file = getReportFile(new PathHelper("war"), today, prefix);
+    logger.info("writing to " + file.getAbsolutePath());
+    return new BufferedWriter(new FileWriter(file));
+  }
+
+  private File getReportFile(PathHelper pathHelper, String today, String prefix) {
+    File reports = pathHelper.getAbsoluteFile("reports");
+    if (!reports.exists()) {
+      logger.debug("making dir " + reports.getAbsolutePath());
+      reports.mkdirs();
+    } else {
+      logger.debug("reports dir exists at " + reports.getAbsolutePath());
+    }
+    String fileName = prefix + "_report_" + today + ".csv";
+    return new File(reports, fileName);
+  }
+
+  /**
+   * @see ReportAllTest#testReports()
+   * @see ReportAllTest#testYTD()
+   * @param connection
+   * @param config
+   * @param dbName
+   * @return
+   */
+  protected DatabaseImpl getDatabase(DatabaseConnection connection, String config, String dbName) {
+    String quizlet = "quizlet";
+    if (config.equals("msa")) quizlet = "classroom";
+    else if (config.equals("pashto1")) quizlet = "pashtoQuizlet1";
+    else if (config.equals("pashto2")) quizlet = "pashtoQuizlet2";
+    else if (config.equals("pashto3")) quizlet = "pashtoQuizlet3";
+    String config1 = config.startsWith("pashto") ? "pashto" : config;
+    File file = new File("war" + File.separator + "config" + File.separator + config1 + File.separator + quizlet +
+        ".properties");
+    String parent = file.getParent();
+    DatabaseImpl database =
+        new DatabaseImpl(connection, parent, file.getName(), dbName, new ServerProperties(parent, file.getName()), new PathHelper("war"), null);
+    // logger.debug("made " + database);
+
+    database.setInstallPath(".", parent + File.separator + database.getServerProps().getLessonPlan(), "media");
+    database.getExercises();
+    return database;
+  }
+
+  protected H2Connection getH2Connection(String path) {
+    return new H2Connection(".", path, true);
+  }
+}
