0a85d8fd446d68bb8001f0906c0ad9eca6fb1f41
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-09 15:37:11 -0500

    shows avp and learn tab info separately
    Former-commit-id: 2953df1d823f4eeac258094c4eeec0757f9546db [formerly 5ea20258ca54dbe7e07430556584cb1e3da72f22] [formerly 2953df1d823f4eeac258094c4eeec0757f9546db [formerly 5ea20258ca54dbe7e07430556584cb1e3da72f22] [formerly bfc30c8287d35cf4877ec5ee1311eedb92502dbf [formerly 69e60946e358c8c2c58c7d52fb1f9fdcebb67ab6]]]
    Former-commit-id: e89f03f17dca9c4c2bbbea3ae6356a0b63db43b6 [formerly f629baa643944442c0e60fb7427c54461dec4c39] [formerly 0242d9d7e8286108cadc819454407e0a8893eaba [formerly 7cbb883bb2449eaacd9a6e0bf152f7de25dfaeb4]] [formerly 368aa9b88c2047e9208ca0eaf9e493cddf13e6a1 [formerly 42b95760718bdbb0529309bd37c75735a25b76e5] [formerly 8b4c09e5ff0cdeb8aed75f9f5aac61b151808447 [formerly b59f8b2f4e4fae9b7af8777658328ab662187357]]]
    Former-commit-id: 368aa9b88c2047e9208ca0eaf9e493cddf13e6a1 [formerly 42b95760718bdbb0529309bd37c75735a25b76e5]
    Former-commit-id: 2900c832d0e99f62068cc5cb34dcd8d1400bf2f4 [formerly 59d06160f97ed403edde8435f897e168feb37efd]
    Former-commit-id: 570f1b3564181738ecbaf57d553fd7d7049385f9

--- src/mitll/langtest/shared/analysis/UserPerformance.java
+++ src/mitll/langtest/shared/analysis/UserPerformance.java
@@ -12,10 +12,11 @@ import java.util.*;
 
 public class UserPerformance implements Serializable {
 //  private transient final Logger logger = Logger.getLogger("UserPerformance");
-  //private List<TimeAndScore> timeAndScores = new ArrayList<>();
   private List<TimeAndScore> rawTimeAndScores = new ArrayList<>();
   private List<TimeAndScore> iPadTimeAndScores = new ArrayList<>();
-  private List<TimeAndScore> browserTimeAndScores = new ArrayList<>();
+ // private List<TimeAndScore> browserTimeAndScores = new ArrayList<>();
+  private List<TimeAndScore> learnTimeAndScores = new ArrayList<>();
+  private List<TimeAndScore> avpTimeAndScores   = new ArrayList<>();
 
   private long userID;
 
@@ -41,71 +42,13 @@ public class UserPerformance implements Serializable {
   }
 
   /**
-   * @see mitll.langtest.server.database.analysis.Analysis#getResultForUserByBin(long, int)
-   * @param resultsForQuery
-   * @param binSize
-   */
-/*  public void setAtBinSize(List<BestScore> resultsForQuery, long binSize) {
-    Map<Long, List<BestScore>> bins = new TreeMap<>();
-    for (BestScore bs : resultsForQuery) {
-      long dayBin = bs.getTimestamp() / binSize;
-      List<BestScore> bestScores = bins.get(dayBin);
-      if (bestScores == null) bins.put(dayBin, bestScores = new ArrayList<BestScore>());
-      bestScores.add(bs);
-    }
-
-    for (Map.Entry<Long, List<BestScore>> pair : bins.entrySet()) {
-      addBestScores(pair.getValue(), binSize);
-    }
-    setRawBestScores(resultsForQuery);
-  }*/
-
-  /**
-   * @paramx bestScores
-   * @seex #setAtBinSize(List, long)
-   */
- /* private void addBestScores(List<BestScore> bestScores, long binSize) {
-    addTimeAndScore(new TimeAndScore(bestScores, binSize));
-  }*/
-
-/*
-  private void addTimeAndScore(TimeAndScore ts) {
-    timeAndScores.add(ts);
-  }
-*/
-/*  private int getTotal() {
-    return getTotal(timeAndScores);
-  }*/
-
-/*  private int getTotal(Collection<TimeAndScore> timeAndScores) {
-    return timeAndScores.size();
-//    logger.info("getTotal " + timeAndScores.size());
-
-*//*    int total = 0;
-    for (TimeAndScore ts : timeAndScores) {
-      //logger.info("got " + ts);
-      total += ts.getCount();
-    }
-    return total;*//*
-  }*/
-
-  /**
    * @return
    * @see mitll.langtest.client.analysis.AnalysisPlot#AnalysisPlot(LangTestDatabaseAsync, long, String, int)
    * @see #getRawAverage()
    */
   public int getRawTotal() {
-//    logger.info("Raw total " + rawTimeAndScores.size());
-  //  return getTotal(rawTimeAndScores);
     return rawTimeAndScores.size();
   }
-
-/*  private float getAverage() {
-    float total = getTotalScore(timeAndScores);
-    int total1 = getTotal();
-    if (total1 == 0) total1 = 1;
-    return total / (float) total1;
-  }*/
 
   /**
    * @return
@@ -116,49 +59,15 @@ public class UserPerformance implements Serializable {
     return total / (float) getRawTotal();
   }
 
-  /*
-  private float getAverage(Collection<TimeAndScore> timeAndScores) {
-    float total = getTotalScore(timeAndScores);
-    return total / (float) getTotal();
-  }
-*/
-
   private float getTotalScore(Collection<TimeAndScore> timeAndScores) {
     float total = 0;
     for (TimeAndScore ts : timeAndScores) total += ts.getScore();// * ts.getCount();
     return total;
   }
 
-/*
-  private List<TimeAndScore> getTimeAndScores() {
-    return timeAndScores;
-  }
-*/
-
-
-
   private long getUserID() {
     return userID;
   }
-
-/*  private void setMoving() {
-    float total = 0;
-    float count = 0;
-    for (TimeAndScore ts : getTimeAndScores()) {
-      total += ts.getScore() * ts.getCount();
-      count += ts.getCount();
-      float moving = total / count;
-      ts.setMoving(moving);
-    }
-  }*/
-
-/*  public String toCSV() {
-    setMoving();
-    StringBuilder builder = new StringBuilder();
-    builder.append(",,,," + userID + "," + getTotal() + "," + getAverage());
-    for (TimeAndScore ts : getTimeAndScores()) builder.append("\n").append(ts.toCSV());
-    return builder.toString();
-  }*/
 
   public String toRawCSV() {
     StringBuilder builder = new StringBuilder();
@@ -191,7 +100,13 @@ public class UserPerformance implements Serializable {
       if (bs.isiPad()) {
         iPadTimeAndScores.add(timeAndScore);
       } else {
-        browserTimeAndScores.add(timeAndScore);
+        if (bs.isFlashcard()) {
+          avpTimeAndScores.add(timeAndScore);
+        }
+        else {
+          learnTimeAndScores.add(timeAndScore);
+        }
+     //   browserTimeAndScores.add(timeAndScore);
       }
     }
   }
@@ -200,12 +115,28 @@ public class UserPerformance implements Serializable {
     return rawTimeAndScores;
   }
 
+  /**
+   * @see mitll.langtest.client.analysis.AnalysisPlot#getChart(String, String, String, UserPerformance)
+   * @return
+   */
   public List<TimeAndScore> getiPadTimeAndScores() {
     return iPadTimeAndScores;
   }
 
-  public List<TimeAndScore> getBrowserTimeAndScores() {
+  /**
+   * @see mitll.langtest.client.analysis.AnalysisPlot#getChart(String, String, String, UserPerformance)
+   * @return
+   */
+/*  public List<TimeAndScore> getBrowserTimeAndScores() {
     return browserTimeAndScores;
+  }*/
+
+  public List<TimeAndScore> getLearnTimeAndScores() {
+    return learnTimeAndScores;
+  }
+
+  public List<TimeAndScore> getAvpTimeAndScores() {
+    return avpTimeAndScores;
   }
 
   public String toString() {
