214b4bf1f741a234d5389cc95cdcabc91f9fd8b8
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-09 15:37:11 -0500

    shows avp and learn tab info separately
    Former-commit-id: 9bad5289602fc5e9c936dd52e45cd88d66d91928 [formerly d56cf3c73d6cb0e3418316f3079486730468bb51]
    Former-commit-id: 629b7a1537fba585991a9a91a4d12afc3f32756b

--- src/mitll/langtest/shared/analysis/TimeAndScore.java
+++ src/mitll/langtest/shared/analysis/TimeAndScore.java
@@ -1,35 +1,30 @@
 package mitll.langtest.shared.analysis;
 
-import org.moxieapps.gwt.highcharts.client.Chart;
-
 import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by go22670 on 10/19/15.
- * @see mitll.langtest.client.custom.AnalysisPlot#addSeries(List, Chart, String)
+ *
+ * @see mitll.langtest.client.analysis.AnalysisPlot#addSeries
  */
 
-public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
+public class TimeAndScore implements Serializable, Comparable<TimeAndScore> {
   private String id;
 
   private long timestamp;
   private float score;
- // private int count = 0;
   private float cumulativeAverage;
-//  private boolean isIPad;
 
   /**
-   * @see UserPerformance#setRawBestScores(List)
    * @param bs
    * @param cumulativeAverage
+   * @see UserPerformance#setRawBestScores(List)
    */
   public TimeAndScore(BestScore bs, float cumulativeAverage) {
     this.id = bs.getId();
     timestamp = bs.getTimestamp();
     score = bs.getScore();
-   // count = bs.getCount();
-  //  isIPad = bs.isiPad();
     this.cumulativeAverage = cumulativeAverage;
   }
 
@@ -45,7 +40,6 @@ public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
     count = bestScoreList.size();
     this.timestamp = (bestScoreList.get(0).getTimestamp()/binSize)*binSize;
   }*/
-
   public TimeAndScore() {
   }
 
@@ -67,29 +61,23 @@ public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
     String format = getTimeString();
     return id + " at " + format + " avg score for " +
         //count + "\t" +
-        "=\t" + score +"\t"+ getCumulativeAverage();
+        "=\t" + score + "\t" + getCumulativeAverage();
   }
 
   private String getTimeString() {
-    return ""+getTimestamp();
+    return "" + getTimestamp();
   }
 
   public String toCSV() {
     return getTimeString() +
         //"," + count +
-        "," + score +","+ getCumulativeAverage();
+        "," + score + "," + getCumulativeAverage();
   }
 
   @Override
   public int compareTo(TimeAndScore o) {
     return new Long(timestamp).compareTo(o.getTimestamp());
   }
-
-/*
-  public void setMoving(float moving) {
-    this.cumulativeAverage = moving;
-  }
-*/
 
   public float getCumulativeAverage() {
     return cumulativeAverage;
@@ -98,10 +86,4 @@ public class TimeAndScore implements Serializable,Comparable<TimeAndScore>{
   public String getId() {
     return id;
   }
-
-/*
-  public boolean isIPad() {
-    return isIPad;
-  }
-*/
 }
