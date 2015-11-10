8ad69fd8bec57e8b265c0f87e7d0bfe1f43358cd
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-10 15:18:52 -0500

    shows initial, current, and diff scores for sounds
    Former-commit-id: b228e38d487cbeab81ec196606fa662f67134a00 [formerly f2ebdc469a6abe442728be7da570dcc3b7f6bc34] [formerly 5fdc522819f587393549b175ac76ee03f1dac0c1 [formerly f7d6cbf34289684aab85f193ceb3a72106c66fd4]] [formerly 329f75b9feeb5cc415f895410f60a5cd83224911 [formerly a36dae875388f97236d98e1f91ab7ca42531e05e] [formerly 1bc0cf356b4fc96dd2fa4462f108ec6e5972bee5 [formerly 1683b46ba799c4686a83a93297dc503c2e2c5fc4]]]
    Former-commit-id: 20fced68e012222a6f7ae8b94214192f93b65b61 [formerly b5303290ec90e1ed13d1a17bc59cc6a8621fa82a] [formerly 54383f1bdb4def9572841060d7abe7ec94ac4f19 [formerly f5c5e037331306aafa90af7587c5d4d3682ea0b6]]
    Former-commit-id: 714c3c49556c8c9ed08334677358b420a80dca56 [formerly aa82e98c7ebe28acce968123b6c5d9ef92b9f2b2]
    Former-commit-id: 68eadeb435836e6f4e8c0efd94602af49748a32e

--- src/mitll/langtest/client/analysis/PhoneAndScore.java
+++ src/mitll/langtest/client/analysis/PhoneAndScore.java
@@ -1,30 +1,57 @@
 package mitll.langtest.client.analysis;
 
+import com.google.gwt.user.cellview.client.Column;
+
+import java.util.List;
+
 /**
  * Created by go22670 on 10/22/15.
  */
 public class PhoneAndScore {
   private final String phone;
 
-  private final float score;
+  private final int score, current;
   private final int count;
 
   /**
-   * @see PhoneContainer#getTableWithPager
    * @param phone
    * @param score
    * @param count
+   * @see PhoneContainer#getTableWithPager
    */
-  public PhoneAndScore(String phone, float score, int count) { this.phone = phone; this.score = score; this.count = count;}
+  public PhoneAndScore(String phone, int score, int current, int count) {
+    this.phone = phone;
+    this.score = score;
+    this.current = current;
+    this.count = count;
+  }
 
+  /**
+   * @return
+   * @see PhoneContainer#getCountSorter(Column, List)
+   */
   public String getPhone() {
     return phone;
   }
 
-  public float getScore() {
+  /**
+   * @return
+   * @see PhoneContainer#getItemColumn()
+   */
+  public int getInitial() {
     return score;
   }
 
+  public int getCurrent() {
+    return current;
+  }
+
+  public int getDiff() { return current-getInitial();}
+
+  /**
+   * @return
+   * @see PhoneContainer#getCountColumn()
+   */
   public int getCount() {
     return count;
   }
