00cff83d6f1a4a9f88656605edd702ea92b2378e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-16 14:36:56 -0500

    fixed issue where couldn't click on an item to review it if it was on teacher defined list
    Former-commit-id: 2800325ef049fc8c2ba67e33aeb6067eeba23c8a [formerly ea08c306634c552e60009a549d6753f0fe25ff46] [formerly 2800325ef049fc8c2ba67e33aeb6067eeba23c8a [formerly ea08c306634c552e60009a549d6753f0fe25ff46] [formerly 2800325ef049fc8c2ba67e33aeb6067eeba23c8a [formerly ea08c306634c552e60009a549d6753f0fe25ff46] [formerly 8f067e4c5a44c0b6c651c584263780e13c44de89 [formerly 8394e23e5cb559b1d47b9911ae0f712dfc1d3f21]]]]
    Former-commit-id: 25cbfa19f9703786b79919beaaa49ca200871cd1 [formerly 5697ff78ee48bde65f36c00e7df1e52c016165b4] [formerly 25cbfa19f9703786b79919beaaa49ca200871cd1 [formerly 5697ff78ee48bde65f36c00e7df1e52c016165b4] [formerly fb782fe792240a53e1f6c7ded5b57bb9b7f42079 [formerly 28a1a2e92fc21870e82749c906dab1c41aad7456]]] [formerly 6cdf44bd19a16cf065f319206afb5f736c3d977c [formerly 4ff764cff368f9546939e4c9a4a6179b3919e5ba] [formerly 6cdf44bd19a16cf065f319206afb5f736c3d977c [formerly 4ff764cff368f9546939e4c9a4a6179b3919e5ba] [formerly ed2510951fa90d6acffbabbf3e10466f941a69f5 [formerly bcb96461dbc0252ee184820f09fe3f71d6ef93ea]]]]
    Former-commit-id: dd2862c1fc03a7f203ff9891239e72fb73095672 [formerly 6cdf44bd19a16cf065f319206afb5f736c3d977c [formerly 4ff764cff368f9546939e4c9a4a6179b3919e5ba]]
    Former-commit-id: d6b4dc2532ab54588787e9da74474b4207ddb5b1 [formerly 4d1a6046a62c385264b31af40b31f7e7bf7e3646] [formerly 5ea1d0e37e07dac8e982348c8eba7ff28317d451 [formerly f3853c4cb8a982fe8aa8a67b025659f4591b88b7]]
    Former-commit-id: e8640057db84c727d55e30d4bd687cd00f61022b [formerly 79013e724c1d79506913675b795e46185f41b42c]
    Former-commit-id: 325f100f36d00fd878b24baec6663f192e97dd8d

---
+++ src/mitll/langtest/client/bootstrap/DownloadHelper.java
@@ -0,0 +1,138 @@
+package mitll.langtest.client.bootstrap;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.custom.TooltipHelper;
+import mitll.langtest.client.instrumentation.EventRegistration;
+import mitll.langtest.client.list.SelectionState;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by go22670 on 11/16/15.
+ */
+public class DownloadHelper {
+  private static final String DOWNLOAD_SPREADSHEET = "Download spreadsheet and audio for selected sections.";
+  private static final String DOWNLOAD_AUDIO = "downloadAudio";
+
+  private final EventRegistration eventRegistration;
+  private final String instance;
+  private final FlexSectionExerciseList exerciseList;
+  private Anchor downloadLink;
+  private Anchor contextDownloadLink;
+  private final boolean isTeacher;
+
+  public DownloadHelper(EventRegistration eventRegistration, String instance,
+                        FlexSectionExerciseList exerciseList, boolean isTeacher) {
+    this.eventRegistration = eventRegistration;
+    this.instance = instance;
+    this.exerciseList = exerciseList;
+    this.isTeacher = isTeacher;
+  }
+
+  public FlexTable getDownloadLinks() {
+    FlexTable links = new FlexTable();
+    links.setWidget(0, 0,downloadLink = getDownloadLink());
+    if (isTeacher) {
+      links.setWidget(0, 1,contextDownloadLink = getContextDownloadLink());
+    }
+    return links;
+  }
+  public void updateDownloadLinks(SelectionState selectionState) {
+    if (downloadLink != null)        downloadLink.setHTML(getURLForDownload(selectionState));
+    if (contextDownloadLink != null) contextDownloadLink.setHTML(getURLForContextDownload(selectionState));
+  }
+
+
+  /**
+   * @return
+   * @see #addButtonRow
+   */
+  private Anchor getDownloadLink() {
+    final Anchor downloadLink = new Anchor(getDownloadURL());
+    addTooltip(downloadLink);
+    downloadLink.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        eventRegistration.logEvent(downloadLink, "DownloadLink", "N/A", "downloading audio");
+      }
+    });
+    downloadLink.getElement().setId("DownloadLink_" + instance);
+    return downloadLink;
+  }
+
+  /**
+   * @param widget
+   * @return
+   * @see #addButtonRow(java.util.List, com.github.gwtbootstrap.client.ui.FluidContainer, java.util.Collection)
+   * @see #getDownloadLink
+   */
+  private void addTooltip(Widget widget) {
+    new TooltipHelper().addTooltip(widget, DOWNLOAD_SPREADSHEET);
+  }
+
+  private Anchor getContextDownloadLink() {
+    final Anchor downloadLink = new Anchor(getDownloadContextURL());
+    new TooltipHelper().addTooltip(downloadLink, "Download spreadsheet and context audio for selected sections.");
+    downloadLink.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        eventRegistration.logEvent(downloadLink, "ContextDownloadLink", "N/A", "downloading context audio");
+      }
+    });
+    downloadLink.getElement().setId("ContextDownloadLink_" + instance);
+    return downloadLink;
+  }
+
+/*
+  public void updateDownloadLinks(SelectionState selectionState) {
+    if (downloadLink != null)        downloadLink.setHTML(downloadHelper.getURLForDownload(selectionState));
+    if (contextDownloadLink != null) contextDownloadLink.setHTML(downloadHelper.getURLForContextDownload(selectionState));
+  }
+*/
+
+
+  private SafeHtml getDownloadURL() {
+    SelectionState selectionState = exerciseList.getSelectionState();
+    return getURLForDownload(selectionState);
+  }
+
+  private SafeHtml getDownloadContextURL() {
+    SelectionState selectionState = exerciseList.getSelectionState();
+    return getURLForContextDownload(selectionState);
+  }
+
+  /**
+   * @param selectionState
+   * @return
+   * @see #showSelectionState(mitll.langtest.client.list.SelectionState)
+   */
+  private SafeHtml getURLForDownload(SelectionState selectionState) {
+    return getUrlDownloadLink(selectionState, DOWNLOAD_AUDIO, "download", "Download");
+  }
+
+  private SafeHtml getURLForContextDownload(SelectionState selectionState) {
+    return getUrlDownloadLink(selectionState, DOWNLOAD_AUDIO, "context", "Context");
+  }
+
+  private SafeHtml getUrlDownloadLink(SelectionState selectionState, String command, String request, String title) {
+    Map<String, Collection<String>> typeToSection = selectionState.getTypeToSection();
+
+    SafeHtmlBuilder sb = new SafeHtmlBuilder();
+    sb.appendHtmlConstant("<a class='" + "icon-download" +
+        "' href='" +
+        command +
+        "?request=" + request + "&" + typeToSection +
+        "'" +
+        ">");
+    sb.appendEscaped(" " + title);
+    sb.appendHtmlConstant("</a>");
+    return sb.toSafeHtml();
+  }
+}
