9e6eb2427e77bfc09ddecb9ddb66e5529145a537
Vidaver, Gordon - 0552 - MITLL <gordon.vidaver@ll.mit.edu>
2015-11-20 11:59:02 -0500

    formatting changes to analysis plot view so fits in on a laptop screen
    Former-commit-id: eca3bb60cd35a0361e8880eea62c2b68d472df9f [formerly abe97b3f6239cc14945e4a59d83b4025670eb971] [formerly 255f10af9f5ba96dbff68659561e44ebb6097ea5 [formerly 1f52cc7805b5955a9eedf807a51a782c014469bb]]
    Former-commit-id: fe970ff0011e082cb0194338f4d9d9daaea7d798 [formerly feb038e166d6f366c4093e5ddd61434fbecf547a] [formerly b3c0289d855c9074661c8dd75a3fbbdd825ecb15 [formerly ccb08e3b9fd7a76dbd53716757a5895a9b29ffec]]
    Former-commit-id: c72ecabccac147e6da54f49cf5b3fd90822eddc5 [formerly 4fc11bf204344acdf3609084f0885341953d6998]
    Former-commit-id: ac4b7bb81e28dad6f8e16dbdbe11bb5bc1f0c77a

--- src/mitll/langtest/client/analysis/PhonePlot.java
+++ src/mitll/langtest/client/analysis/PhonePlot.java
@@ -3,6 +3,7 @@ package mitll.langtest.client.analysis;
 import com.github.gwtbootstrap.client.ui.Label;
 import com.github.gwtbootstrap.client.ui.base.DivWidget;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import mitll.langtest.shared.CommonShell;
 import mitll.langtest.shared.analysis.PhoneSession;
@@ -21,172 +22,95 @@ import java.util.logging.Logger;
  * Created by go22670 on 10/19/15.
  */
 public class PhonePlot extends DivWidget implements IsWidget {
-  private final Logger logger = Logger.getLogger("PhonePlot");
+//  private final Logger logger = Logger.getLogger("PhonePlot");
 
-  public static final String AVERAGE = "Average";
+  private static final String AVERAGE = "Average";
   private static final int NARROW_WIDTH = 330;
   private static final int CHART_HEIGHT = 340;
-  private static final String PRONUNCIATION_SCORE = "Pronunciation Score";
+  //private static final String PRONUNCIATION_SCORE = "Pronunciation Score";
 
-  private final Map<Long, String> timeToId = new TreeMap<>();
+ // private final Map<Long, String> timeToId = new TreeMap<>();
   private final Map<Long, PhoneSession> timeToSession = new TreeMap<>();
-  private final Map<String, CommonShell> idToEx = new TreeMap<>();
+ // private final Map<String, CommonShell> idToEx = new TreeMap<>();
 
   public PhonePlot() {
     getElement().setId("PhonePlot");
   }
 
   /**
+   * @see PhoneContainer#showExamplesForSelectedSound()
    * @param rawBestScores
    * @param userChosenID
    * @param isNarrow
-   * @see PhoneContainer#gotClickOnItem(PhoneAndStats)
    */
-/*
-  public void showData(List<TimeAndScore> rawBestScores, String userChosenID, boolean isNarrow) {
-    clear();
-    int rawTotal = rawBestScores.size();
-    if (rawBestScores.isEmpty()) {
-      add(new Label("No Recordings yet to analyze. Please record yourself."));
-    } else {
-      Chart chart = getChart("<b>" + userChosenID + "</b>" +
-              " pronunciation score"
-          ,
-          "Score and average (" + rawTotal + " items " +
-              //": avg score " + (int) v +
-              " %)", "Cumulative Average", rawBestScores, isNarrow);
-      add(chart);
-    }
-    setRawBestScores(rawBestScores);
-  }
-*/
   public void showData2(List<PhoneSession> rawBestScores, String userChosenID, boolean isNarrow) {
     clear();
- //   int rawTotal = rawBestScores.size();//userPerformance.getRawTotal();
     if (rawBestScores.isEmpty()) {
       add(new Label("No Recordings yet to analyze. Please record yourself."));
     } else {
       Chart chart = getChart2("<b>" + userChosenID + "</b>" + " pronunciation score",
           "Average score and range" //+
-          //    " for " + rawTotal + " sessions"
           , "Range", rawBestScores, isNarrow);
       add(chart);
     }
     setRawBestScores2(rawBestScores);
   }
 
-
-  /**
-   * @param title
-   * @param subtitle
-   * @param seriesName
-   * @return
-   * @see #showData(List, String, boolean)
-   */
-/*  private Chart getChart(String title, String subtitle, String seriesName, List<TimeAndScore> rawBestScores, boolean narrow) {
-    Chart chart = new Chart()
-        .setZoomType(BaseChart.ZoomType.X)
-        .setType(Series.Type.SCATTER)
-        .setChartTitleText(title)
-        .setMarginRight(10)
-        .setOption("/credits/enabled", false)
-        .setOption("/plotOptions/series/pointStart", 1)
-        .setOption("/legend/enabled", false)
-        .setScatterPlotOptions(new ScatterPlotOptions()
-            .setMarker(new Marker()
-                .setRadius(5)
-                .setHoverState(new Marker()
-                    .setEnabled(true)
-                    .setLineColor(new Color(100, 100, 100))
-                )
-            )
-            .setHoverStateMarker(new Marker()
-                .setEnabled(false)
-            ))
-        .setToolTip(getToolTip());
-    if (narrow) chart.setWidth(NARROW_WIDTH);
-
-    addSeries(rawBestScores, chart, seriesName);
-
-    configureChart(chart, subtitle);
-    return chart;
-  }*/
-
-  private ToolTip getToolTip() {
-    return new ToolTip()
-        .setFormatter(new ToolTipFormatter() {
-          public String format(ToolTipData toolTipData) {
-            String s = timeToId.get(toolTipData.getXAsLong());
-            CommonShell commonShell = getIdToEx().get(s);
-            String foreignLanguage = commonShell == null ? "" : commonShell.getForeignLanguage();
-            String english = commonShell == null ? "" : commonShell.getEnglish();
-
-            String seriesName1 = toolTipData.getSeriesName();
-            boolean showEx = (seriesName1.contains(PRONUNCIATION_SCORE));
-            return "<b>" + seriesName1 + "</b><br/>" +
-                DateTimeFormat.getFormat("E MMM d yy h:mm a").format(
-                    new Date(toolTipData.getXAsLong())
-                ) +
-                (showEx ?
-                    "<br/>Ex " + s +
-                        "<br/>" + foreignLanguage +
-                        "<br/>" + english
-                    : "")
-                +
-                "<br/>Score = " + toolTipData.getYAsLong() + "%";
-          }
-        });
-  }
-
-  private DateTimeFormat format = DateTimeFormat.getFormat("E MMM d yy h:mm a");
-  private DateTimeFormat noYearFormat = DateTimeFormat.getFormat("E MMM d h:mm a");
-  DateTimeFormat shortFormat = DateTimeFormat.getFormat("MMM d, yy");
-  Date now = new Date();
+  private final DateTimeFormat format = DateTimeFormat.getFormat("E MMM d yy h:mm a");
+  private final DateTimeFormat noYearFormat = DateTimeFormat.getFormat("E MMM d h:mm a");
+  private final DateTimeFormat shortFormat = DateTimeFormat.getFormat("MMM d, yy");
+  private final String nowFormat = shortFormat.format(new Date());
 
   private ToolTip getToolTip2() {
     return new ToolTip()
         .setFormatter(new ToolTipFormatter() {
           public String format(ToolTipData toolTipData) {
-            try {
-              PhoneSession session = timeToSession.get(toolTipData.getXAsLong());
-              String seriesName1 = toolTipData.getSeriesName();
-
-              Date date = new Date(toolTipData.getXAsLong());
-
-              String nowFormat = shortFormat.format(now);
-              String shortForDate = shortFormat.format(date);
-
-              DateTimeFormat toUse = (nowFormat.substring(nowFormat.length() - 2).equals(shortForDate.substring(shortForDate.length() - 2))) ? noYearFormat : format;
-              String dateToShow = toUse.format(date);
-
-
-              if (seriesName1.equals(AVERAGE)) {
-                return "<b>" + seriesName1 + "</b>" +
-                    "<br/>" +
-                    dateToShow
-                    +
-                    "<br/>Mean = " + toolTipData.getYAsLong() + "%";
-              } else {
-                Point point = toolTipData.getPoint();
-                String s = /*session == null ? "" : */(" n = " + session.getCount());
-                String range = /*point == null ? "" :*/ ("range " + point.getLow() + "-" + point.getHigh());
-                String s1 = "<b>" + seriesName1 + "</b>" +
-                    "<br/>" +
-                    dateToShow
-                    +
-                    "<br/>" +
-                    //"Mean = " + toolTipData.getYAsLong() + "%" +
-                    range + s;
-                return s1;
-              }
-            } catch (Exception e) {
-              e.printStackTrace();
-              return "error " + e.getMessage();
-            }
+            return getTooltipText(toolTipData);
           }
         });
   }
 
+  private String getTooltipText(ToolTipData toolTipData) {
+    try {
+      PhoneSession session = timeToSession.get(toolTipData.getXAsLong());
+      String seriesName1 = toolTipData.getSeriesName();
+
+      Date date = new Date(toolTipData.getXAsLong());
+
+      String shortForDate = shortFormat.format(date);
+
+      DateTimeFormat toUse = sameYear(nowFormat, shortForDate) ? noYearFormat : format;
+      String dateToShow = toUse.format(date);
+
+      if (seriesName1.equals(AVERAGE)) {
+        return "<b>" + seriesName1 + "</b>" +
+            "<br/>" +
+            dateToShow
+            +
+            "<br/>Mean = " + toolTipData.getYAsLong() + "%";
+      } else {
+        Point point = toolTipData.getPoint();
+        String s = /*session == null ? "" : */(" n = " + session.getCount());
+        String range = /*point == null ? "" :*/ ("range " + point.getLow() + "-" + point.getHigh());
+        String s1 = "<b>" + seriesName1 + "</b>" +
+            "<br/>" +
+            dateToShow
+            +
+            "<br/>" +
+            //"Mean = " + toolTipData.getYAsLong() + "%" +
+            range + s;
+        return s1;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "error " + e.getMessage();
+    }
+  }
+
+  private boolean sameYear(String nowFormat, String shortForDate) {
+    return nowFormat.substring(nowFormat.length() - 2)
+        .equals(shortForDate.substring(shortForDate.length() - 2));
+  }
 
   /**
    * @param title
@@ -198,7 +122,7 @@ public class PhonePlot extends DivWidget implements IsWidget {
   private Chart getChart2(String title, String subtitle, String seriesName, List<PhoneSession> rawBestScores, boolean narrow) {
     Chart chart = new Chart()
         .setZoomType(BaseChart.ZoomType.X)
-        // .setType(Series.Type.ERRORBAR)
+            // .setType(Series.Type.ERRORBAR)
         .setChartTitleText(title)
         .setMarginRight(10)
         .setOption("/credits/enabled", false)
@@ -206,17 +130,19 @@ public class PhonePlot extends DivWidget implements IsWidget {
         .setOption("/legend/enabled", false)
         .setScatterPlotOptions(new ScatterPlotOptions()
             .setMarker(new Marker()
-                .setRadius(5)
-                .setHoverState(new Marker()
-                    .setEnabled(true)
-                    .setLineColor(new Color(100, 100, 100))
-                )
+                    .setRadius(5)
+                    .setHoverState(new Marker()
+                            .setEnabled(true)
+                            .setLineColor(new Color(100, 100, 100))
+                    )
             )
             .setHoverStateMarker(new Marker()
-                .setEnabled(false)
+                    .setEnabled(false)
             ))
         .setToolTip(getToolTip2());
 
+    int clientWidth = Window.getClientWidth();
+    if (clientWidth < 1450) narrow = true;
     if (narrow) chart.setWidth(NARROW_WIDTH);
 
     Highcharts.setOptions(
@@ -255,77 +181,14 @@ public class PhonePlot extends DivWidget implements IsWidget {
     chart.addSeries(series);
   }
 
-
-  /**
-   * @param yValuesForUser
-   * @param chart
-   * @param seriesTitle
-   * @paramx gameTimeSeconds
-   * @see #getChart
-   */
-  private void addSeries(List<TimeAndScore> yValuesForUser,
-                         Chart chart,
-                         String seriesTitle) {
-    addCumulativeAverage(yValuesForUser, chart, seriesTitle);
-    addDeviceData(yValuesForUser, chart);
-  }
-
-  private void addCumulativeAverage(List<TimeAndScore> yValuesForUser, Chart chart, String seriesTitle) {
-    Number[][] data = new Number[yValuesForUser.size()][2];
-
-    int i = 0;
-    for (TimeAndScore ts : yValuesForUser) {
-      data[i][0] = ts.getTimestamp();
-      data[i++][1] = ts.getCumulativeAverage() * 100;
-    }
-
-    Series series = chart.createSeries()
-        .setName(seriesTitle)
-        .setPoints(data)
-        .setType(Series.Type.LINE);
-
-    chart.addSeries(series);
-  }
-
-
   private void addMeans(List<PhoneSession> iPadData, Chart chart) {
-    Series series;
-    Number[][] data = getData(iPadData);
-
-    series = chart.createSeries()
+    Series series = chart.createSeries()
         .setName(AVERAGE)
-        .setPoints(data)
+        .setPoints(getData(iPadData))
         .setOption("color", "#00B800")
         .setType(Series.Type.SPLINE);
 
     chart.addSeries(series);
-  }
-
-  private void addDeviceData(List<TimeAndScore> iPadData, Chart chart) {
-    if (!iPadData.isEmpty()) {
-      Number[][] data;
-      Series series;
-      data = getDataForTimeAndScore(iPadData);
-
-      String iPadName = PRONUNCIATION_SCORE;
-      series = chart.createSeries()
-          .setName(iPadName)
-          .setPoints(data)
-          .setOption("color", "#00B800");
-
-      chart.addSeries(series);
-    }
-  }
-
-  private Number[][] getDataForTimeAndScore(List<TimeAndScore> yValuesForUser) {
-    Number[][] data = new Number[yValuesForUser.size()][2];
-
-    int i = 0;
-    for (TimeAndScore ts : yValuesForUser) {
-      data[i][0] = ts.getTimestamp();
-      data[i++][1] = ts.getScore() * 100;
-    }
-    return data;
   }
 
   private Number[][] getData(List<PhoneSession> yValuesForUser) {
@@ -355,11 +218,11 @@ public class PhonePlot extends DivWidget implements IsWidget {
     chart.setHeight(CHART_HEIGHT + "px");
   }
 
-  private void setRawBestScores(List<TimeAndScore> rawBestScores) {
+/*  private void setRawBestScores(List<TimeAndScore> rawBestScores) {
     for (TimeAndScore timeAndScore : rawBestScores) {
       timeToId.put(timeAndScore.getTimestamp(), timeAndScore.getId());
     }
-  }
+  }*/
 
   private void setRawBestScores2(List<PhoneSession> rawBestScores) {
     for (PhoneSession timeAndScore : rawBestScores) {
@@ -371,7 +234,9 @@ public class PhonePlot extends DivWidget implements IsWidget {
    * @return
    * @see WordContainer#getShell(String)
    */
+/*
   private Map<String, CommonShell> getIdToEx() {
     return idToEx;
   }
+*/
 }
