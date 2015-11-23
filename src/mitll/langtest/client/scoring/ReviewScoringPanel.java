e65efd7a1aa61bb331367fc475cfbf1f8100a544
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-23 14:15:34 -0500

    shows the error bar plot initially, remembers the last selected user
    Former-commit-id: 7b2b0899d5b5396fff4a43b1585d4ada370b06c8 [formerly 4186760ce0d5501cbd84a4c4e92b2665ce8c92f0] [formerly e6b032f5a53168cd067fb43ed6846fb3a33abb55 [formerly 1028d37b1cb439c25bc2ac0fd80873cccf7bb7fe]] [formerly 7b2b0899d5b5396fff4a43b1585d4ada370b06c8 [formerly 4186760ce0d5501cbd84a4c4e92b2665ce8c92f0] [formerly e6b032f5a53168cd067fb43ed6846fb3a33abb55 [formerly 1028d37b1cb439c25bc2ac0fd80873cccf7bb7fe]] [formerly 7b2b0899d5b5396fff4a43b1585d4ada370b06c8 [formerly 4186760ce0d5501cbd84a4c4e92b2665ce8c92f0] [formerly e6b032f5a53168cd067fb43ed6846fb3a33abb55 [formerly 1028d37b1cb439c25bc2ac0fd80873cccf7bb7fe]] [formerly bbad54740d7c56e3d0ebbebcc3160d3168ea7ced [formerly 08efa4b2e7cda33ab710d09d135b17f4dbc7d4dd] [formerly be721651865afad0180580b5e944da8260034b54 [formerly 5cc4acc8d31399a01ee4c35d2e35178688e00268]]]]]
    Former-commit-id: 7fb50a8c598efb4e045acdadc3f7e07d289393ef [formerly c5056e01b88da691efabd49f98952d28df512ac0] [formerly fd85241743eca8db961bb59f0f9802c6c524d384 [formerly f7019a22ae4999d3a7ab4c27e5ada9269dfe8453]] [formerly 7fb50a8c598efb4e045acdadc3f7e07d289393ef [formerly c5056e01b88da691efabd49f98952d28df512ac0] [formerly fd85241743eca8db961bb59f0f9802c6c524d384 [formerly f7019a22ae4999d3a7ab4c27e5ada9269dfe8453]] [formerly bd6b0455c5ad2fbf27a7ca27235b64e2f7d9f30f [formerly ce7d78d91efe00792a0d715b299198a6ada13fcc] [formerly 5ef4e9dbcdd7d4f604646ccc4ffaad0d7b47faa1 [formerly 7d465d333cf63f96c462e16a2d7f97a07530945f]]]] [formerly 864bc23278e7827007bec4ec9f8de2933b3e5a70 [formerly bf770fcc4062aaa7fcef2c02cb451fd4f63568d2] [formerly fe7f82d4da2e365c65384f429482eef5f2cdcb7d [formerly 67eecfacc3e7785ecbbb7e2a85f6332b0644ca20]] [formerly 864bc23278e7827007bec4ec9f8de2933b3e5a70 [formerly bf770fcc4062aaa7fcef2c02cb451fd4f63568d2] [formerly fe7f82d4da2e365c65384f429482eef5f2cdcb7d [formerly 67eecfacc3e7785ecbbb7e2a85f6332b0644ca20]] [formerly 1237d16386376a9b0349a9700add84a8e099bd78 [formerly 4769b52c04bfef63b16e841f7251e3fba751ebd8] [formerly d1ad88868cd2def687907e04a173b45f2ddcf4aa [formerly 50f90a1cf9409cc0cf7fc39da26f6262bf1ec541]]]]]
    Former-commit-id: 864bc23278e7827007bec4ec9f8de2933b3e5a70 [formerly bf770fcc4062aaa7fcef2c02cb451fd4f63568d2] [formerly fe7f82d4da2e365c65384f429482eef5f2cdcb7d [formerly 67eecfacc3e7785ecbbb7e2a85f6332b0644ca20]]
    Former-commit-id: 59855550da4f8114b2382156868d94ceed740e1c [formerly 6dc7902889b0ed752bda7b467daa93abf3ace39b] [formerly cfb8c9ad5f5cecb363ca63a9727ec2241056388b [formerly 83bac10be694dcc6fd70aaf34e0dff1e236d5367]] [formerly 29c3411b2a0e8ee660baa8bd81e3c9d6784afee4 [formerly 80feb94ba89da989f68ed3a964df7cad595930f8] [formerly c52a44d5b1a2ccd00ba06b40773cd68abe75b5eb [formerly eeb1bcf77a17c92112faca2bfdff2cf7daf66f03]]]
    Former-commit-id: b949cfdbe74e697ab9d3710f448395d2cf5766a9 [formerly 66cad7e2201d0a1076779083570dcca22151cd6a] [formerly f9461200b3b872229d373805356e0b91df5bfc42 [formerly a7a5ee9173e8e08a11436d928b49ad3f017a4c3b]]
    Former-commit-id: 0face12628ad527498335bcf2fbe88da8f101a67 [formerly 645f7a162921a0d415aeabb5b4e3adbb71228556]
    Former-commit-id: e02c9eff939412e0d63491d52acacf3c48b26790

--- src/mitll/langtest/client/scoring/ReviewScoringPanel.java
+++ src/mitll/langtest/client/scoring/ReviewScoringPanel.java
@@ -231,33 +231,6 @@ public class ReviewScoringPanel extends ScoringAudioPanel {
     });
   }
 
-/*  private Map<TranscriptSegment, List<TranscriptSegment>> getWordToPhones(PretestScore score) {
-    Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeToEndTime = score.getsTypeToEndTimes();
-    return getWordToPhones(netPronImageTypeToEndTime);
-  }*/
-
-/*  private Map<TranscriptSegment, List<TranscriptSegment>> getWordToPhones(Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeToEndTime) {
-    List<TranscriptSegment> words = netPronImageTypeToEndTime.get(NetPronImageType.WORD_TRANSCRIPT);
-    List<TranscriptSegment> phones = netPronImageTypeToEndTime.get(NetPronImageType.PHONE_TRANSCRIPT);
-
-    Map<TranscriptSegment, List<TranscriptSegment>> wordToPhones = new HashMap<>();
-    for (TranscriptSegment word : words) {
-      String event = word.getEvent();
-      if (event.equals("sil") || event.equals("<s>") || event.equals("</s>")) {
-
-      } else {
-        for (TranscriptSegment phone : phones) {
-          if (phone.getStart() >= word.getStart() && phone.getEnd() <= word.getEnd()) {
-            List<TranscriptSegment> orDefault = wordToPhones.get(word);
-            if (orDefault == null) wordToPhones.put(word, orDefault = new ArrayList<TranscriptSegment>());
-            orDefault.add(phone);
-          }
-        }
-      }
-    }
-    return wordToPhones;
-  }*/
-
   /**
    * @see #scoreAudio(String, long, String, ImageAndCheck, ImageAndCheck, int, int, int)
    * @param score
@@ -268,77 +241,6 @@ public class ReviewScoringPanel extends ScoringAudioPanel {
     return new WordTable().getWordTable(netPronImageTypeToEndTime);
   }
 
- /* private Widget getWordTable(Map<NetPronImageType, List<TranscriptSegment>> netPronImageTypeToEndTime) {
-    Map<TranscriptSegment, List<TranscriptSegment>> wordToPhones = getWordToPhones(netPronImageTypeToEndTime);
-
-    Table table = new Table();
-    table.getElement().setId("WordTable");
-    table.getElement().getStyle().clearWidth();
-    table.removeStyleName("table");
-
-    HTMLPanel srow = new HTMLPanel("tr", "");
-    table.add(srow);
-
-    HTMLPanel row = new HTMLPanel("tr", "");
-    table.add(row);
-
-
-    for (Map.Entry<TranscriptSegment, List<TranscriptSegment>> pair : wordToPhones.entrySet()) {
-      TranscriptSegment word = pair.getKey();
-
-      TableHeader w = new TableHeader(word.getEvent());
-      w.getElement().getStyle().setTextAlign(Style.TextAlign.CENTER);
-      String color = SimpleColumnChart.getColor(word.getScore());
-      w.getElement().getStyle().setBackgroundColor(color);
-
-      table.add(w);
-
-      HTMLPanel col;
-      srow.add(col = new HTMLPanel("td", ""));
-      HTML wscore = new HTML("" + getPercent(word.getScore()));
-      wscore.getElement().getStyle().setTextAlign(Style.TextAlign.CENTER);
-
-      col.add(wscore);
-
-      Table pTable = new Table();
-      pTable.removeStyleName("table");
-
-      col = new HTMLPanel("td", "");
-      col.getElement().getStyle().clearBorderStyle();
-      col.add(pTable);
-      row.add(col);
-
-      HTMLPanel row2 = new HTMLPanel("tr", "");
-      pTable.add(row2);
-
-      HTMLPanel row3 = new HTMLPanel("tr", "");
-      pTable.add(row3);
-
-      for (TranscriptSegment phone : pair.getValue()) {
-
-        String event = phone.getEvent();
-        if (!event.equals("sil")) {
-          TableHeader h = new TableHeader(event);
-          h.getElement().getStyle().setTextAlign(Style.TextAlign.CENTER);
-          pTable.add(h);
-          String color1 = SimpleColumnChart.getColor(phone.getScore());
-          h.getElement().getStyle().setBackgroundColor(color1);
-
-          //  HTML widget = new HTML(" <b>" + getPercent(phone.getScore()) +"</b>");
-          HTML widget = new HTML("" + getPercent(phone.getScore()));
-          widget.getElement().getStyle().setTextAlign(Style.TextAlign.CENTER);
-          widget.getElement().getStyle().setWidth(25, Style.Unit.PX);
-
-          col = new HTMLPanel("td", "");
-          col.add(widget);
-          row3.add(col);
-        }
-      }
-    }
-
-    return table;
-  }*/
-
   /**
    * @return
    * @see mitll.langtest.client.result.ResultManager#getAsyncTable(int)
@@ -346,5 +248,5 @@ public class ReviewScoringPanel extends ScoringAudioPanel {
   public Widget getTables() {
     return tablesContainer;
   }
-  public Widget getBelow() { return belowContainer; }
+  public Widget getBelow()  { return belowContainer; }
 }
