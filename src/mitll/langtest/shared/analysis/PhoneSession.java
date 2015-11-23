4576954a9a87eb50e2a392b95e00838e9029c6ce
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-23 18:15:30 -0500

    more or less got adaptive error bar display working
    Former-commit-id: 8c9e57fa4430a7621a3b6560096ac64a5ebeca29 [formerly 7a1dd064e23e9e28426dcc9ad80b2def4f9c1585] [formerly 574db91c497a663c7721980c0ba7b1735dc5e1ba]
    Former-commit-id: eb9cdcd95f8430225b72521d77488d62dd416233
    Former-commit-id: a1a2e2533a2b71b44dd82fc0eff548127b79c612

--- src/mitll/langtest/shared/analysis/PhoneSession.java
+++ src/mitll/langtest/shared/analysis/PhoneSession.java
@@ -12,25 +12,32 @@ public class PhoneSession implements Serializable, Comparable<PhoneSession> {
   private double meanTime;
   private long count;
   private long bin;
+  private long start;
+  private long end;
   private String phone;
 
-  public PhoneSession(String phone, long bin, long count, double mean, double stdev, double meanTime) {
+  public PhoneSession(String phone, long bin, long count, double mean, double stdev, double meanTime, long start, long end) {
     this.phone = phone;
     this.bin = bin;
     this.count = count;
     this.mean = mean;
     this.stdev = stdev;
     this.meanTime = meanTime;
+    this.start = start;
+    this.end = end;
   }
 
-  public PhoneSession() {}
+  public PhoneSession() {
+  }
 
   public double getMean() {
     return mean;
   }
+
   public double getStdev() {
     return stdev;
   }
+
   private double getMeanTime() {
     return meanTime;
   }
@@ -49,7 +56,23 @@ public class PhoneSession implements Serializable, Comparable<PhoneSession> {
   }
 
   public String toString() {
-    return phone + " : " + new Date(bin) + " n " + count +
+    return phone + " : " + new Date(bin) +
+
+        " start " + new Date(start)+
+        " end "  + new Date(end)+
+        " n " + count +
         " mean " + mean + "  stdev " + stdev + " time " + meanTime;
+  }
+
+  public long getStart() {
+    return start;
+  }
+
+  public long getEnd() {
+    return end;
+  }
+
+  public long getMiddle() {
+    return (start +end) / 2;
   }
 }
