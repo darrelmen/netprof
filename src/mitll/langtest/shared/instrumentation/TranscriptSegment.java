8072ee88a86a167e27d09e9839996888695f2aa0
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-23 19:03:41 -0500

    updated copyright
    Former-commit-id: 248e43359253adb592ef5d77cab782e45e731c7a [formerly 8ce68dcbfe1b53e9ad3e89c7a60bdbc156a0c003] [formerly c103fe79cd22bd61e24e56e6009b043ca2ad7fd3 [formerly 49be21f3cd99cd617fb889b613610690d2c780ec]] [formerly 264eaa22a939556243a3111ccac452880c5e06c1 [formerly dfcd4f2bc8bd281b2b2355b2c73565e7139b5161] [formerly c5f590e489b0e9cae5a860bfa2b017d163df4da3 [formerly c46d12177c8c943fe9065e68cf8095648537d77c]]]
    Former-commit-id: f7f3d887c0cd261fab50d231c9629c7d96cf4b4e [formerly 7f57e1afe69145f99faf922a792ff3921c971751] [formerly 3a54ab79e0faac09c6ad16e3a2b0880eac404656 [formerly 178b16d6070a0d97909411c0a4558b1ee8e24b0e]] [formerly d812dcf4a349bd550d9a6b0d8bff563129cbaa2d [formerly 76034a63f59deae7e666fbaf77cb98ea71d9a298] [formerly 46ae229e340a7c65a18caed885f39a3b530c6942 [formerly 4ee72f4e1b28bf2f8f97d8c69448e7f77685e2b2]]]
    Former-commit-id: 586097ba0bd2af5af7177032c44ed223eb36a6db [formerly 9e3478158b655aea038b71f19ae68fa4933c0b60] [formerly 1e1eb25131808ca147f3627a91fcff0581b00de1 [formerly 1dad1a21d3e7817b814c340faa87e4efceb29ede]]
    Former-commit-id: f2d65bf8b06733ce81c59618cf52707a02df22cf [formerly d46ff0690809d9d7a03fb970c23022eb5d80517b]
    Former-commit-id: 3d45e88ca3d930603cdcf9e7084cca1066daf8c1

--- src/mitll/langtest/shared/instrumentation/TranscriptSegment.java
+++ src/mitll/langtest/shared/instrumentation/TranscriptSegment.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
 package mitll.langtest.shared.instrumentation;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
