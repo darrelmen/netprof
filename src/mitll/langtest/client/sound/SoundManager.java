2472b060b874e0f350ba840b3b469872eda60c69
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-23 19:03:41 -0500

    updated copyright
    Former-commit-id: 01e5f893907c8c33223e562811fb3ab62b7e180b
    Former-commit-id: 5a5eba570bae0f9dcdcde770c68980c62dd46aaa [formerly fe93f84bc1595c4638ee445b4d5a30c9ebcd2ec3]
    Former-commit-id: 46726bfe1860191a8b74b6cb7c6ffc7bfc4d80da

--- src/mitll/langtest/client/sound/SoundManager.java
+++ src/mitll/langtest/client/sound/SoundManager.java
@@ -1,48 +1,8 @@
-package mitll.langtest.client.sound;
-
 /*
- 	This GWT class references files and uses code from the SoundManager2 project (version 297a.20110424) which is subject to this license:
-	
-	Software License Agreement (BSD License)
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
 
-	Copyright (c) 2007, Scott Schiller (schillmania.com)
-	All rights reserved.
-
-	Redistribution and use in source and binary forms, with or without modification,
-	are permitted provided that the following conditions are met:
-
-		-Redistributions of source code must retain the above copyright notice, this 
-		list of conditions and the following disclaimer.
-
-		-Redistributions in binary form must reproduce the above copyright notice, this
-		list of conditions and the following disclaimer in the documentation and/or
-		other materials provided with the distribution.
-
-		-Neither the name of schillmania.com nor the names of its contributors may be
-		used to endorse or promote products derived from this software without
-		specific prior written permission from schillmania.com.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	
-	-------------------------------------------------------------------------------------------------------------------------
-	
-	The project also uses files from the above project and subject to the above license. These files are:
-	swf/soundmanager2_debug.swf
-	swf/soundmanager2_flash_xdomain.zip
-	swf/soundmanager2_flash9_debug.swf
-	swf/soundmanager2_flash9.swf
-	swf/soundmanager2.swf
-	soundmanager2.js
-*/
+package mitll.langtest.client.sound;
 
 import java.util.Date;
 
