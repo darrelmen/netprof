e2a0b46d0e154032be30c0a86ab8aeff3c5a304f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-11-23 19:03:41 -0500

    updated copyright
    Former-commit-id: d16b55371b69858c198d6324c88d960f9014c06a [formerly 42183a1e5d7b13c0f5f74d9d4f4e5df846f64b97] [formerly d16b55371b69858c198d6324c88d960f9014c06a [formerly 42183a1e5d7b13c0f5f74d9d4f4e5df846f64b97] [formerly efb1579b0d29f972e57f850b00c751dcef9d1273]]
    Former-commit-id: 0eb9098fc68be9c5f9978a23037f7e67683cf1f5 [formerly 5abda011e9bfaa24b5364a3887db9ceab9902b8e] [formerly 96889737301a58bd5e100c7bd33241dd5e336b7e] [formerly 5b1f65e6315be9e0f7989d99fc590c4e6489654c [formerly 92a28618bf3f382d8bad73204f855e79e75a9b96] [formerly e19c56565b8a4b5aa03f290e237f2b2a826e26d4]]
    Former-commit-id: 5b1f65e6315be9e0f7989d99fc590c4e6489654c [formerly 92a28618bf3f382d8bad73204f855e79e75a9b96]
    Former-commit-id: ff644f2dbe11790fb589d05211f6e5110ebb3eb9
    Former-commit-id: 9b5131b84b53e9135f5897a99d7a8f4146f198af

--- src/mitll/langtest/server/database/UserDAO.java
+++ src/mitll/langtest/server/database/UserDAO.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
 package mitll.langtest.server.database;
 
 import mitll.langtest.server.ServerProperties;
