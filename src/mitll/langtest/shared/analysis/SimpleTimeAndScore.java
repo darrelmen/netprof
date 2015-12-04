32f74befdb5e6b16514e5607d7579a9a92178ef7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-03 19:51:38 -0500

    steps towards showing initial and current scores as first and last sessions of the right granularity
    Former-commit-id: 05495b7b1f53b8e5c0fbc2c38e2b6fbab001962d [formerly a21da0d15b5c6ea0eb6f45b364d0ff5d3309f8b9] [formerly 05495b7b1f53b8e5c0fbc2c38e2b6fbab001962d [formerly a21da0d15b5c6ea0eb6f45b364d0ff5d3309f8b9] [formerly 688756837df9c750933a853fd44b1f0b6ccf353b [formerly f78ba72bfec2379706ce9ac2965f5a702ed28622]]]
    Former-commit-id: 7ab71d0cf296ff9919465ca40cd020553ffb2457 [formerly 923323690e3de7e89f2476c165c5d09ad57636fa] [formerly 8314aad8e0a5a9d4351397a09ba46bf6c0566301 [formerly dfae4b32227ae32a2e21d9eab154db389efd45e2]] [formerly 471edc4816f4ec430db963eb8725574cdfa71303 [formerly d9aa4117a5617f78b173befd75b12b8982cd4d71] [formerly cb31285e3e82e8ad131ed260e48617ab165c4289 [formerly 9159bc0dec573d6e34432bfdece637b56794022f]]]
    Former-commit-id: 471edc4816f4ec430db963eb8725574cdfa71303 [formerly d9aa4117a5617f78b173befd75b12b8982cd4d71]
    Former-commit-id: 87663c545b8c8f484a65bcf4d99c6df817f4287e [formerly 664a0058da75257e756946810c8bc41a1fec1471]
    Former-commit-id: 79457c7c14509fc0826595e43469f23aa62442be

---
+++ src/mitll/langtest/shared/analysis/SimpleTimeAndScore.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
+package mitll.langtest.shared.analysis;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by go22670 on 10/19/15.
+ *
+ * @see mitll.langtest.client.analysis.AnalysisPlot#addSeries
+ */
+
+public class SimpleTimeAndScore implements Serializable {
+  private long timestamp;
+  private float score;
+
+  /**
+   * @param timestamp
+   * @param score
+   * @see mitll.langtest.server.database.PhoneDAO#getPhoneTimeSeries(List)
+   */
+  public SimpleTimeAndScore( long timestamp, float score) {
+    this.timestamp = timestamp;
+    this.score = score;
+  }
+
+  public SimpleTimeAndScore() {
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  /**
+   * TODO : make this an int
+   *
+   * @return
+   */
+  public float getScore() {
+    return score;
+  }
+
+  public String toString() {
+    String format = getTimeString();
+    return "at\t" + format + " avg score for " +
+        //count + "\t" +
+        "=\t" + score;
+  }
+
+  private String getTimeString() {
+    return "" + getTimestamp();
+  }
+
+}
