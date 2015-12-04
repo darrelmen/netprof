5f857f155a650a7b1a6ac6693ea2cb4e44488669
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-04 18:22:58 -0500

    fix issue with not playing audio on click if there's no ref audio
    Former-commit-id: c19c4709946e87a14b337b97293e53362e063758 [formerly a08b692e5c514edcba8f1d1ccc60bb8d455c8ee3 [formerly 65c09ac131bbef82eac8d3889b26aca6eb76da54] [formerly b6fc0268d3594482b984457b76653c3d9a40301f [formerly cf2cb596d2c35c989e08edb8ce0182d3466346c1]]] [formerly a08b692e5c514edcba8f1d1ccc60bb8d455c8ee3 [formerly 65c09ac131bbef82eac8d3889b26aca6eb76da54] [formerly b6fc0268d3594482b984457b76653c3d9a40301f [formerly cf2cb596d2c35c989e08edb8ce0182d3466346c1]] [formerly 6ce58ecac6a44eda442f0c117e2c788e320d9fa2]]
    Former-commit-id: a6758ec34559488248b5c1ffd8d2ee28a24addef [formerly 9569ba8de0518c186c85927164c7828dd042e8de] [formerly 9e9350a28cfd1d7ea8f20a49afacf60c6638e4ab [formerly 6a9dde0a489f3edd0044e50dad425381fc4b373d]] [formerly 971de834f8a95d302237063df37c4b2f31278e6a] [formerly e99350a6ef228792992c9e6dd2fd023c572cda26 [formerly e97b1a683c0948c0abd29ae778afa5631f9a033f] [formerly 5b33cf5684b13970e0b162eec3289d93bbc17c03 [formerly e4f44083206d5e4197a15c59bba93777d15fb1fe]] [formerly 66a96fa48b4e3939124effd50299c75bf1e0b4ba]]
    Former-commit-id: e99350a6ef228792992c9e6dd2fd023c572cda26 [formerly e97b1a683c0948c0abd29ae778afa5631f9a033f] [formerly 5b33cf5684b13970e0b162eec3289d93bbc17c03 [formerly e4f44083206d5e4197a15c59bba93777d15fb1fe]]
    Former-commit-id: 8327e31c9494342cb5cdc0b49cae33705016510b
    Former-commit-id: 519bb8c0d5684624d1f4ebbb995704864faf9644 [formerly c362cd5ec4a7102a7dae30d4782d3c627abc84d7]
    Former-commit-id: fec38e00a0c21a9c63dbf9e517d1e1e15c9657b2

--- src/mitll/langtest/client/sound/SoundPlayer.java
+++ src/mitll/langtest/client/sound/SoundPlayer.java
@@ -4,28 +4,32 @@
 
 package mitll.langtest.client.sound;
 
+import java.util.logging.Logger;
+
 /**
  * Created by go22670 on 11/19/15.
  */
 public class SoundPlayer extends SoundFeedback {
+ // private final Logger logger = Logger.getLogger("SoundPlayer");
+
   public SoundPlayer(SoundManagerAPI soundManager) {
     super(soundManager);
   }
 
   public synchronized void queueSong(String song, SoundFeedback.EndListener endListener) {
-    //logger.info("\t queueSong song " +song+ " -------  "+ System.currentTimeMillis());
+   // logger.info("\t queueSong song " +song+ " -------  "+ System.currentTimeMillis());
     destroySound(); // if there's something playing, stop it!
     createSound(song, endListener);
   }
 
   public synchronized void queueSong(String song) {
-    //logger.info("\t queueSong song " +song+ " -------  "+ System.currentTimeMillis());
+   // logger.info("\t queueSong song " +song+ " -------  "+ System.currentTimeMillis());
     destroySound(); // if there's something playing, stop it!
     createSound(song, null);
   }
 
   public synchronized void clear() {
-    //  logger.info("\t stop playing current sound -------  "+ System.currentTimeMillis());
+   // logger.info("\t stop playing current sound -------  "+ System.currentTimeMillis());
     destroySound(); // if there's something playing, stop it!
   }
 }
