36b9f00fcceec2e29d81fb602c0223b51a0101b6
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-04 18:22:58 -0500

    fix issue with not playing audio on click if there's no ref audio
    Former-commit-id: c19c4709946e87a14b337b97293e53362e063758 [formerly f31cd52a223b04a2a1e056afab846e96a95983e3 [formerly 8db1a776d027139123c2ae549c826b7345ee4860]] [formerly f31cd52a223b04a2a1e056afab846e96a95983e3 [formerly 8db1a776d027139123c2ae549c826b7345ee4860] [formerly 6ce58ecac6a44eda442f0c117e2c788e320d9fa2]]
    Former-commit-id: a704f1bdce34ae1ae9d95f99190484664456552e [formerly 71868c62a7a104e706c64c46291f747326c17027] [formerly 971de834f8a95d302237063df37c4b2f31278e6a] [formerly 1e8d0f65af5079c50d39bbdf1f5ebedcddd9b143 [formerly 166c68cddefe211227cf8efb03a1ded9160347e7] [formerly 66a96fa48b4e3939124effd50299c75bf1e0b4ba]]
    Former-commit-id: 1e8d0f65af5079c50d39bbdf1f5ebedcddd9b143 [formerly 166c68cddefe211227cf8efb03a1ded9160347e7]
    Former-commit-id: 8327e31c9494342cb5cdc0b49cae33705016510b
    Former-commit-id: ceef1299ce15894469141edb9da6fca9214ac4af

--- src/mitll/langtest/client/analysis/PlayAudio.java
+++ src/mitll/langtest/client/analysis/PlayAudio.java
@@ -4,32 +4,52 @@
 
 package mitll.langtest.client.analysis;
 
+import com.github.gwtbootstrap.client.ui.Icon;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
 import mitll.langtest.client.AudioTag;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.sound.SoundFeedback;
+import mitll.langtest.client.sound.SoundManagerAPI;
 import mitll.langtest.client.sound.SoundPlayer;
 import mitll.langtest.shared.CommonExercise;
 import mitll.langtest.shared.flashcard.CorrectAndScore;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by go22670 on 11/20/15.
  */
 public class PlayAudio {
+  private final Logger logger = Logger.getLogger("PlayAudio");
+
   private static final String WAV = ".wav";
   private static final String MP3 = "." + AudioTag.COMPRESSED_TYPE;
 
   private final LangTestDatabaseAsync service;
   private final SoundPlayer soundFeedback;
+  private Widget playFeedback;
 
-  com.google.gwt.user.client.Timer t;
+  private Timer t;
 
-  public PlayAudio(LangTestDatabaseAsync service, SoundPlayer soundFeedback) {
+  /**
+   * @param service
+   * @param soundFeedback
+   * @param playFeedback
+   * @see AnalysisPlot#AnalysisPlot(LangTestDatabaseAsync, long, String, int, SoundManagerAPI, Icon)
+   */
+  public PlayAudio(LangTestDatabaseAsync service, SoundPlayer soundFeedback, Widget playFeedback) {
     this.service = service;
     this.soundFeedback = soundFeedback;
+    this.playFeedback = playFeedback;
   }
+
+  /**
+   * @param id
+   * @param userid
+   */
   public void playLast(String id, long userid) {
     service.getExercise(id, userid, false, new AsyncCallback<CommonExercise>() {
       @Override
@@ -42,33 +62,77 @@ public class PlayAudio {
         CorrectAndScore correctAndScore = scores.get(scores.size() - 1);
         String refAudio = commonExercise.getRefAudio();
 
-        if (t != null) t.cancel();
+        if (t != null) {
+         // logger.info("cancel timer");
+          t.cancel();
+        }
         if (refAudio != null) {
           playLastThenRef(correctAndScore, refAudio);
+        } else {
+          playUserAudio(correctAndScore);
+       //   logger.info("no ref audio for " + commonExercise.getID());
         }
       }
     });
   }
 
-  public void playLastThenRef(CorrectAndScore correctAndScore, String refAudio) {
+  /**
+   * @param correctAndScore
+   * @param refAudio
+   */
+  private void playLastThenRef(CorrectAndScore correctAndScore, String refAudio) {
     final String path = getPath(refAudio);
-    soundFeedback.queueSong(getPath(correctAndScore.getPath()), new SoundFeedback.EndListener() {
+    final String path1 = getPath(correctAndScore.getPath());
+    soundFeedback.queueSong(path1, new SoundFeedback.EndListener() {
       @Override
       public void songStarted() {
-
+        playFeedback.setVisible(true);
+       // logger.info("\t songStarted song " + path1 + " -------  " + System.currentTimeMillis());
       }
 
       @Override
       public void songEnded() {
-        t = new com.google.gwt.user.client.Timer() {
+        playFeedback.setVisible(false);
+
+      //  logger.info("\t songEnded song " + path1 + " -------  " + System.currentTimeMillis());
+        t = new Timer() {
           @Override
           public void run() {
-            soundFeedback.queueSong(path);
+         //   logger.info("\t songEnded queue song " + path + " -------  " + System.currentTimeMillis());
+
+            soundFeedback.queueSong(path, new SoundFeedback.EndListener() {
+              @Override
+              public void songStarted() {
+                playFeedback.setVisible(true);
+              }
+
+              @Override
+              public void songEnded() {
+                playFeedback.setVisible(false);
+              }
+            });
           }
         };
         t.schedule(100);
       }
 
+    });
+  }
+
+  private void playUserAudio(CorrectAndScore correctAndScore) {
+    final String path1 = getPath(correctAndScore.getPath());
+    soundFeedback.queueSong(path1, new SoundFeedback.EndListener() {
+      @Override
+      public void songStarted() {
+        playFeedback.setVisible(true);
+       // logger.info("playUserAudio songStarted song " + path1 + " -------  " + System.currentTimeMillis());
+      }
+
+      @Override
+      public void songEnded() {
+      //  logger.info("playUserAudio songEnded song " + path1 + " -------  " + System.currentTimeMillis());
+        playFeedback.setVisible(false);
+      }
     });
   }
 
