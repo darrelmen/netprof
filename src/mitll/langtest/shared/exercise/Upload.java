f26ae2680062d8212a983f2011e37d98b9bc98a4
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-16 11:41:35 -0500

    moved exercise shared classes under exercise packaged, added upload dao and upload object
    Former-commit-id: 609c021bfe1f563d14010aaa8ad5fd00b47ccf61 [formerly 919cb5d8dd87ba4e019c496b816834f916dcfc8f] [formerly 5c87fca3578b8ee730dfa7cef64ea8f5b0dd6abd [formerly c2a9f371b13dcc9b743a3bb49c086ec3fd20601f]] [formerly 6f235f48a22040d313fb79d5ed09fc19bcfa311b]
    Former-commit-id: ab7c69f29f67439fb2dd175b46429200f223be25 [formerly 1af8aab29b7562224a6987578aa7a3f2f037bc65] [formerly de409d733eaa4bf69e9b5778b7eb151937f37110 [formerly 035d194a4235d556efb34a802d2a05163097ae97]] [formerly c276a31ee3344108b79b17a112c2387ece1fba40]
    Former-commit-id: c1f033bde2fc8ccb3044ec833202a2051d7babfa
    Former-commit-id: 11b108f7e4d0d331688ba1c7d9dbbfc88ee2d0f0 [formerly 26886f8ee717686a862a2dbb47f190ca1254bc35]
    Former-commit-id: 207ce45a929f10e9e99dd0c08000b96743733e0d

---
+++ src/mitll/langtest/shared/exercise/Upload.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
+package mitll.langtest.shared.exercise;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.Date;
+
+/**
+ * Created by go22670 on 12/16/15.
+ */
+public class Upload implements IsSerializable {
+  private long id;
+  private long user;
+  private String note;
+  private String fileRef;
+  private long timestamp;
+  private boolean enabled = true;
+  private String project;
+  private String sourceURL;
+
+  public Upload() {
+  }
+
+  public Upload(long id, long user, String note, String fileRef, long timestamp, boolean enabled,
+                String project, String sourceURL) {
+    this.id = id;
+    this.user = user;
+    this.note = note;
+    this.fileRef = fileRef;
+    this.timestamp = timestamp;
+    this.enabled = enabled;
+    this.project = project;
+    this.sourceURL = sourceURL;
+  }
+
+  @Override
+  public String toString() {
+    return "Upload by " + getUser() + " on " + new Date(getTimestamp()) + " note " + getNote() + " file " + getFileRef();
+  }
+
+  public long getUser() {
+    return user;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public String getFileRef() {
+    return fileRef;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public String getProject() {
+    return project;
+  }
+
+  public String getSourceURL() {
+    return sourceURL;
+  }
+}
