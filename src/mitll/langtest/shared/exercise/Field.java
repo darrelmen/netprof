2e80742ddac5f710d5c72a42f5e881fa6a13d7cb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-16 16:41:54 -0500

    initial steps towards an extensible db backed exercise system
    Former-commit-id: ca2fb9cfc4beacaa29a16afb680a7e91e72abd7b
    Former-commit-id: b76029a97a9bfb909fa080aef1c603437bab4d1e [formerly 94e1105c7f22e57ec12e8ef5c0763742101c0a92]
    Former-commit-id: 6b6a2345b160e827f441ae42fd37f7ae9fbc4559

---
+++ src/mitll/langtest/shared/exercise/Field.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
+package mitll.langtest.shared.exercise;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.Collection;
+
+/**
+ * Created by go22670 on 12/16/15.
+ */
+public class Field implements IsSerializable {
+  //private boolean isPrimitive;
+  private String name;
+  private FIELD_TYPE type;
+  //private boolean isCollection;
+  // private boolean isMap;
+  private String value;
+  private long objectRef;
+  private long id;
+  private FieldObject fieldObject;
+//  private String exid;
+
+  enum FIELD_TYPE {STRING, OBJECT, LIST, MAP}
+
+  public Field() {
+  }
+
+  public Field(long id, String name,
+               //       boolean isPrimitive,
+               //     boolean isCollection, boolean isMap,
+               FIELD_TYPE type, String value,
+               long objectRef) {
+    this.id = id;
+    this.name = name;
+    //  this.isPrimitive = isPrimitive;
+    // this.isCollection = isCollection;
+    // this.isMap = isMap;
+    this.type = type;
+    this.value = value;
+    this.objectRef = objectRef;
+//    this.exid = exid;
+  }
+
+  /**
+   * exObject.addField("english", new Field(-1, "english", true, false, "String", ex.getEnglish(), -1, ex.getID()));
+   */
+  public Field(String name, String value) {
+    this(-1, name,
+        //true,
+        // false, false,
+        FIELD_TYPE.STRING, value, -1);
+  }
+
+  public Field(String name, String value, long objectRef) {
+    this(-1, name,
+        //true,
+        // false, false,
+        FIELD_TYPE.STRING, value, objectRef);
+  }
+
+  public Field(String name, BaseObject baseObject) {
+    this(-1, name,
+        //true,
+        //isCollection, isMap,
+        FIELD_TYPE.OBJECT, "", baseObject.getObjectRef());
+    this.fieldObject = baseObject;
+  }
+
+  public Field(String name, ListContainer baseObject) {
+    this(-1, name,
+        //true,
+        //isCollection, isMap,
+        FIELD_TYPE.LIST, "", baseObject.getObjectRef());
+    this.fieldObject = baseObject;
+  }
+
+  public Field(String name, MapContainer baseObject) {
+    this(-1, name,
+        //true,
+        //isCollection, isMap,
+        FIELD_TYPE.MAP, "", baseObject.getObjectRef());
+    this.fieldObject = baseObject;
+  }
+
+  public boolean isPrimitive() {
+    return type == FIELD_TYPE.STRING;
+  }
+
+  public FIELD_TYPE getType() {
+    return type;
+  }
+
+/*  public boolean isCollection() {
+    return isCollection;
+  }*/
+
+  public String getValue() {
+    return value;
+  }
+
+  public void add(FieldObject object) {
+    ListContainer fieldObject = (ListContainer) this.fieldObject;
+    fieldObject.add(object);
+  }
+
+  public Collection<FieldObject> getList() {
+    ListContainer fieldObject = (ListContainer) this.fieldObject;
+    return fieldObject.getContents();
+  }
+
+  public void put(String name, FieldObject object) {
+    MapContainer fieldObject = (MapContainer) this.fieldObject;
+    fieldObject.put(name, object);
+  }
+
+  public long getObjectRef() {
+    return objectRef;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+/*
+  public String getExid() {
+    return exid;
+  }
+*/
+
+  @Override
+  public String toString() {
+    return //"ex " + getObjectRef() + "/" + getName() + " : " +
+        "" + (isPrimitive() ? value : fieldObject);
+  }
+
+  public String getName() {
+    return name;
+  }
+}
