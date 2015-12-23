bb47f2f4909a2de8577573fc9d775a06a04d4f79
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-23 15:22:34 -0500

    another attempt at merging amas code into webservice
    Former-commit-id: ff1fb96fbaada1d20de72e80242ae6b467b123d9 [formerly 440e8eb07c25f5e76af18ab41197e56ccbf4975a] [formerly ff1fb96fbaada1d20de72e80242ae6b467b123d9 [formerly 440e8eb07c25f5e76af18ab41197e56ccbf4975a] [formerly ff1fb96fbaada1d20de72e80242ae6b467b123d9 [formerly 440e8eb07c25f5e76af18ab41197e56ccbf4975a] [formerly fce03810eb000d973b61152da73f116751114aa7 [formerly 36eb6e7335c7f6c53858494a9e9e5d6e73ffa4ba]]]]
    Former-commit-id: 1eaa8cdab73b35c6305cc6b29eeb0ba9d7ef1729 [formerly 8793015fb75a00a1889a5a6d04356cb5bb221461] [formerly 1eaa8cdab73b35c6305cc6b29eeb0ba9d7ef1729 [formerly 8793015fb75a00a1889a5a6d04356cb5bb221461] [formerly 1ec68384267db41b5d289f4c60b87046baa94090 [formerly 265d512ea39bff15a73907ebc8f81492d0f75251]]] [formerly 7dd01ad3fc6fa1915a9831c17e25b93f4c0e7457 [formerly 42b6bfa7c06fa41798eefaca3062f08647ff0be7] [formerly 7dd01ad3fc6fa1915a9831c17e25b93f4c0e7457 [formerly 42b6bfa7c06fa41798eefaca3062f08647ff0be7] [formerly bbc0a210b5b860980415f0f5520fc7e2a904318e [formerly 016089d5c3752dfdf24f850386c31a6c0d6f44fa]]]]
    Former-commit-id: 7dd01ad3fc6fa1915a9831c17e25b93f4c0e7457 [formerly 42b6bfa7c06fa41798eefaca3062f08647ff0be7]
    Former-commit-id: 0a262fbbd6e02985134c56ebc52cef12c8359575 [formerly 4ce95e24197c6d076ef286a37a136171cd00e9a3] [formerly d3f2499a3753ea3667ea765d16d5ad3960d56f08 [formerly 6c17e0dcf7ab5c44ecf0bb7bafec1bb7114a1a20]]
    Former-commit-id: 1999c6365d08ffab86b1f761340ca97084ef6608 [formerly dc692d5b05e88030032fab00e6c3b6897ce55917]
    Former-commit-id: b4f1a172bae4418d0b9f5d113def55184204a845

---
+++ src/mitll/langtest/client/amas/InitialUI.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ */
+
+package mitll.langtest.client.amas;
+
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.Container;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
+import com.github.gwtbootstrap.client.ui.base.DivWidget;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import mitll.langtest.client.LangTest;
+import mitll.langtest.client.LangTestDatabase;
+import mitll.langtest.client.LangTestDatabaseAsync;
+import mitll.langtest.client.PropertyHandler;
+import mitll.langtest.client.flashcard.Flashcard;
+
+
+/**
+ * Created by go22670 on 12/21/15.
+ */
+public class InitialUI {
+  private final LangTest langTest;
+  private final PropertyHandler props;
+  private AutoCRTChapterNPFHelper learnHelper;
+
+
+  private final LangTestDatabaseAsync service = GWT.create(LangTestDatabase.class);
+
+  private final Flashcard flashcard;
+
+  private Panel headerRow;
+  private Panel firstRow;
+
+
+  public InitialUI(LangTest langTest, Flashcard flashcard) {
+    this.langTest = langTest;
+    this.flashcard = flashcard;
+    this.props = langTest.getProps();
+  }
+
+  /**
+   * If in ODA Mode, wait until we start a QUIZ?
+   * <p>
+   * Then only show the content part of it...
+   *
+   * @return
+   * @see #onModuleLoad2()
+   * @see #showLogin
+   * @see #populateRootAfterLogin()
+   */
+  void populateRootPanel() {
+    Container verticalContainer = getRootContainer();
+    // header/title line
+    // first row ---------------
+    Panel firstRow = makeFirstTwoRows(verticalContainer);
+
+    if (props.isOdaMode()) {
+      headerRow.setVisible(false);
+      populateBelowHeader(verticalContainer, firstRow);
+    } else if (!langTest.showLogin(verticalContainer, firstRow)) {
+      populateBelowHeader(verticalContainer, firstRow);
+    }
+  }
+
+
+  /**
+   * @param verticalContainer
+   * @return
+   * @see #populateRootPanel()
+   */
+  private Panel makeFirstTwoRows(Container verticalContainer) {
+    verticalContainer.add(headerRow = makeHeaderRow());
+    headerRow.getElement().setId("headerRow");
+
+    Panel firstRow = new DivWidget();
+    verticalContainer.add(firstRow);
+    this.firstRow = firstRow;
+    firstRow.getElement().setId("firstRow");
+    return firstRow;
+  }
+
+  /**
+   * @return
+   * @see #populateRootPanel()
+   */
+  private Panel makeHeaderRow() {
+    headerRow = new FluidRow();
+    headerRow.add(new Column(12, flashcard.getNPFHeaderRow()));
+    return headerRow;
+  }
+
+  /**
+   *
+   * @return
+   */
+  private Container getRootContainer() {
+    RootPanel.get().clear();   // necessary?
+
+    Container verticalContainer = new FluidContainer();
+    verticalContainer.getElement().setId("root_vertical_container");
+    return verticalContainer;
+  }
+
+  public int getHeightOfTopRows() {
+    return headerRow.getOffsetHeight();
+  }
+
+  /**
+   * @param verticalContainer
+   * @param firstRow          where we put the flash permission window if it gets shown
+   * @seex #handleCDToken(com.github.gwtbootstrap.client.ui.Container, com.google.gwt.user.client.ui.Panel, String, String)
+   * @see #populateRootPanel()
+   * @see #showLogin()
+   */
+  void populateBelowHeader(Container verticalContainer, Panel firstRow) {
+    RootPanel.get().clear();
+    RootPanel.get().add(verticalContainer);
+    /**
+     * {@link #makeFlashContainer}
+     */
+    firstRow.add(langTest.getFlashRecordPanel());
+    langTest.modeSelect();
+
+    learnHelper = new AutoCRTChapterNPFHelper(service, langTest, langTest.getUserManager(), langTest);
+    learnHelper.addNPFToContent(firstRow, "autoCRT");
+  }
+
+
+  public void onResize() {
+    if (learnHelper != null) learnHelper.onResize();
+  }
+
+  /**
+   * @see mitll.langtest.client.user.UserManager#getPermissionsAndSetUser(int)
+   * @see mitll.langtest.client.user.UserManager#login()
+   */
+
+  void showLogin() {
+    populateRootPanel();
+  }
+
+  /**
+   * @see #gotUser
+   * @see #configureUIGivenUser(long) (long)
+   */
+  void populateRootAfterLogin() {
+    if (!props.isOdaMode()) {
+      int childCount = firstRow.getElement().getChildCount();
+      // logger.info("populateRootAfterLogin root " + firstRow.getElement().getNodeName() + " childCount " + childCount);
+      if (childCount > 0) {
+        Node child = firstRow.getElement().getChild(0);
+        Element as = Element.as(child);
+        if (as.getId().contains("Login")) {
+          //   logger.info("populateRootAfterLogin found login...");
+          populateRootPanel();
+        }
+      }
+    }
+  }
+
+  void configureUIGivenUser() {
+    if (learnHelper != null && learnHelper.getExerciseList() != null) {
+      learnHelper.getExerciseList().restoreListFromHistory();
+    }
+  }
+}
