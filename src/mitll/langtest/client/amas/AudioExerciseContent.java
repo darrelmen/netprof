f3bb70bf731f7139c3e05e55c09f9915cb48b066
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2015-12-23 15:22:34 -0500

    another attempt at merging amas code into webservice
    Former-commit-id: 2b46fb5c39d153299d93cd698287a3ff3252f26d [formerly ff1fb96fbaada1d20de72e80242ae6b467b123d9] [formerly 78090c702d2f1591ec46dd31c27b1f07a18eb160 [formerly 440e8eb07c25f5e76af18ab41197e56ccbf4975a]] [formerly 2b46fb5c39d153299d93cd698287a3ff3252f26d [formerly ff1fb96fbaada1d20de72e80242ae6b467b123d9] [formerly 78090c702d2f1591ec46dd31c27b1f07a18eb160 [formerly 440e8eb07c25f5e76af18ab41197e56ccbf4975a]] [formerly ceea9b2e9d173e73d036ec8514e71ed7a2d4d8ce [formerly fce03810eb000d973b61152da73f116751114aa7] [formerly 869a40b59c1487cae78b04258300a8ce310a2b8c [formerly 36eb6e7335c7f6c53858494a9e9e5d6e73ffa4ba]]]]
    Former-commit-id: d850de7ac69a67136027484c5c89cd6ab5847a84 [formerly 1eaa8cdab73b35c6305cc6b29eeb0ba9d7ef1729] [formerly 63c2e5fc2aa4922e620260109200534ec8b13e00 [formerly 8793015fb75a00a1889a5a6d04356cb5bb221461]] [formerly 2e258c02175e51ce0bd5d696d54930e9cec9a2b6 [formerly 1ec68384267db41b5d289f4c60b87046baa94090] [formerly 6e706b926d235ad3a767d00f3daeb5ae5b2f2340 [formerly 265d512ea39bff15a73907ebc8f81492d0f75251]]] [formerly 10cb8b0a6d1175b9289eab50fbd4727373274c2d [formerly 7dd01ad3fc6fa1915a9831c17e25b93f4c0e7457] [formerly 961b078da3f7835269e5d8a2fe322bebea1bd613 [formerly 42b6bfa7c06fa41798eefaca3062f08647ff0be7]] [formerly e944a0df4b65c290c46c5db2999dc430e4af147d [formerly bbc0a210b5b860980415f0f5520fc7e2a904318e] [formerly 3bcaf6d6e12274e04c3ddebf87e17f00fb0f9974 [formerly 016089d5c3752dfdf24f850386c31a6c0d6f44fa]]]]
    Former-commit-id: 10cb8b0a6d1175b9289eab50fbd4727373274c2d [formerly 7dd01ad3fc6fa1915a9831c17e25b93f4c0e7457] [formerly 961b078da3f7835269e5d8a2fe322bebea1bd613 [formerly 42b6bfa7c06fa41798eefaca3062f08647ff0be7]]
    Former-commit-id: 76eb7be411f8ffce7f91f29e1959757533ff19a1 [formerly d3f2499a3753ea3667ea765d16d5ad3960d56f08] [formerly d3565c9d8c6f122e2459ca7b4a76817045425dfb [formerly 6c17e0dcf7ab5c44ecf0bb7bafec1bb7114a1a20]]
    Former-commit-id: 2c70d12a466a7c30bbe3f25bb80d55ad7c350382 [formerly 4ce95e24197c6d076ef286a37a136171cd00e9a3]
    Former-commit-id: 0a262fbbd6e02985134c56ebc52cef12c8359575

---
+++ src/mitll/langtest/client/amas/AudioExerciseContent.java
@@ -0,0 +1,192 @@
+package mitll.langtest.client.amas;
+
+import com.github.gwtbootstrap.client.ui.Heading;
+import com.github.gwtbootstrap.client.ui.base.DivWidget;
+import com.google.gwt.i18n.client.HasDirection;
+import com.google.gwt.i18n.shared.WordCountDirectionEstimator;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.PropertyHandler;
+import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.shared.amas.QAPair;
+import mitll.langtest.shared.exercise.CommonExercise;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GO22670
+ * Date: 11/1/13
+ * Time: 5:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AudioExerciseContent {
+ // private Logger logger = Logger.getLogger("AudioExerciseContent");
+
+  private static final String QUESTION = "question";
+  private static final String QUESTION_HEADING = "h4";
+  public static final String ITEM = "Passage";
+
+  private boolean rightAlignContent;
+  private String responseType;
+  private String language;
+
+  /**
+   * @param e
+   * @param controller
+   * @param includeExerciseID
+   * @param showQuestion
+   * @param content
+   * @param index
+   * @param totalInQuiz
+   * @return
+   * @see mitll.langtest.client.recorder.FeedbackRecordPanel#getQuestionContent
+   */
+  public Widget getQuestionContent(CommonExercise e, ExerciseController controller, boolean includeExerciseID,
+                                   boolean showQuestion, String content, int index, int totalInQuiz) {
+    rightAlignContent = controller.isRightAlignContent();
+    language = controller.getLanguage().toLowerCase();
+    responseType = controller.getProps().getResponseType();
+    return makeFlashcardForCRT(e, content, includeExerciseID, showQuestion, index, totalInQuiz);
+  }
+
+  /**
+   * Do hacky stuff to take the content and splice in an html 5 audio widget to play back audio.
+   *
+   * @param e
+   * @param content
+   * @param showQuestion
+   * @param index
+   * @param totalInQuiz
+   * @return
+   */
+  private Panel makeFlashcardForCRT(CommonExercise e, String content, boolean includeExerciseID, boolean showQuestion, int index, int totalInQuiz) {
+    Panel container = new FlowPanel();
+    container.getElement().setId("makeFlashcardForCRT_container");
+    addAudioRow(e, content, includeExerciseID, container, index, totalInQuiz);
+
+    QAPair qaPair = getQaPair(e);
+    if (showQuestion && qaPair != null) {
+      container.add(getMaybeRTLContent("<" +
+          QUESTION_HEADING +
+          " style='margin-right: 30px'>" + qaPair.getQuestion() + "</" +
+          QUESTION_HEADING +
+          ">", true));
+    }
+    return container;
+  }
+
+  private QAPair getQaPair(CommonExercise e) {
+    List<QAPair> foreignLanguageQuestions = e.getForeignLanguageQuestions();
+    QAPair qaPair = foreignLanguageQuestions.isEmpty() ? null : foreignLanguageQuestions.get(0);
+    if (foreignLanguageQuestions.isEmpty()) {
+      System.err.println("huh? no fl questions for " + e);
+    }
+    return qaPair;
+  }
+
+  /**
+   * @param e
+   * @param content
+   * @param includeExerciseID
+   * @param container
+   * @param index
+   * @param totalInQuiz
+   * @see #getContentFromPrefix(String)
+   * @seex #getAudioDiv
+   * @see #makeFlashcardForCRT
+   */
+  private void addAudioRow(CommonExercise e, String content, boolean includeExerciseID, Panel container, int index, int totalInQuiz) {
+    Panel horiz = new FlowPanel();
+    horiz.getElement().setId("item_and_content");
+    container.add(horiz);
+    if (includeExerciseID) {
+      DivWidget itemHeaderContainer = new DivWidget();
+     // itemHeaderContainer.setWidth("100%");
+      itemHeaderContainer.setHeight("20px");
+      itemHeaderContainer.getElement().setId("itemHeaderContainer");
+      Heading child = getItemHeader(index, totalInQuiz);
+      itemHeaderContainer.add(child);
+      horiz.add(itemHeaderContainer);
+    }
+
+    content = content.replaceAll("h2", "h4");
+    boolean isPlural = e.getQuestions().size() > 1;
+    content = changeAudioPrompt(content, isPlural);
+
+    HTML contentFromPrefix = getContentFromPrefix(content);
+
+    if (rightAlignContent) {
+      contentFromPrefix.addStyleName("floatRight");
+    }
+
+    horiz.add(contentFromPrefix);
+  }
+
+  private Heading getItemHeader(int index, int totalInQuiz) {
+    String text = ITEM + " #" + (index + 1) + " of " + totalInQuiz;
+    Heading child = new Heading(5, text);
+    child.getElement().setId("audio_exercise_item_header");
+    child.addStyleName("leftTenMargin");
+    child.addStyleName("floatLeft");
+    child.addStyleName("rightFiveMargin");
+    return child;
+  }
+
+  /**
+   * @param prefix
+   * @return
+   * @see #addAudioRow
+   */
+  private HTML getContentFromPrefix(String prefix) {
+    HTML contentPrefix = getMaybeRTLContent(prefix, true);
+    contentPrefix.addStyleName("marginRight");
+    contentPrefix.addStyleName("wrapword");
+    if (rightAlignContent) contentPrefix.addStyleName("rightAlign");
+    return contentPrefix;
+  }
+
+  private String changeAudioPrompt(String suffix, boolean isPlural) {
+    if (responseType.equals(PropertyHandler.TEXT)) {
+      String question = QUESTION;
+      suffix = suffix.replace("answer the " + question, "type your answer to the " + question);
+    }
+    if (isPlural) {
+      suffix = suffix.replace(QUESTION, "questions");
+    }
+    return suffix;
+  }
+
+  /**
+   * @param content          text we want to align
+   * @param requireAlignment so you can override it for certain widgets and not make it RTL
+   * @return HTML that has it's text-align set consistent with the language (RTL for arabic, etc.)
+   * @see #makeFlashcardForCRT
+   * @see #getContentFromPrefix
+   */
+  private HTML getMaybeRTLContent(String content, boolean requireAlignment) {
+    HasDirection.Direction direction =
+        requireAlignment && rightAlignContent ? HasDirection.Direction.RTL : WordCountDirectionEstimator.get().estimateDirection(content);
+    content = content
+        .replaceAll("<p> &nbsp; </p>", "")
+        .replaceAll("<h4>", "<div><h4 style='margin-left:0px' class='" + language + "'>")
+        .replaceAll("</h4>", "</h4></div>");
+
+    HTML html = new HTML(content, direction);
+    html.getElement().setId("maybeRTL_textContent");
+
+    if (requireAlignment && rightAlignContent) {
+      html.addStyleName("rightAlign");
+      html.addStyleName("rightTenMargin");
+    }
+    else {
+      html.addStyleName("leftTenMargin");
+    }
+    html.addStyleName(language);
+
+    html.addStyleName("wrapword");
+    return html;
+  }
+}
