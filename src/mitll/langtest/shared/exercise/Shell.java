894d641901f3f3a90cce6806e11275ba3385158f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-05 16:22:31 -0500

    partial steps towards getting it to compile
    Former-commit-id: 25afa9081990a77abdbdf9546c1c874d3c3db046 [formerly 952a8abd2393a2559107ea0eca125fd00572af3a] [formerly 7a4a571b3c63922d95a30ce3015c645b644f48d6 [formerly 94a02df4e8056ca06eec6c17d09d20734341e8d2]]
    Former-commit-id: ad6f89dd0d088a1123fc22e22e2a442c4b024b92 [formerly 43edb8ab2f8ff9f75c40246a028142ac106d5f29]
    Former-commit-id: 21b618019df05e9d2b38f2ce4bd9634d43979a55

--- src/mitll/langtest/shared/exercise/Shell.java
+++ src/mitll/langtest/shared/exercise/Shell.java
@@ -2,6 +2,8 @@ package mitll.langtest.shared.exercise;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+import java.util.Map;
+
 /**
  * Created by go22670 on 1/4/16.
  */
@@ -9,4 +11,17 @@ public interface Shell extends IsSerializable {
   String getID();
   STATE getState();
   void setState(STATE state);
+
+  STATE getSecondState();
+
+  void setSecondState(STATE state);
+
+  Map<String, String> getUnitToValue();
+
+  /**
+   * @see mitll.langtest.server.database.exercise.SectionHelper#addExerciseToLesson(CommonExercise, String, String)
+   * @param unit
+   * @param value
+   */
+  void addUnitToValue(String unit, String value);
 }
