e1799da60b9c48e6eb8be714bee2407dacda3523
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-05 16:22:31 -0500

    partial steps towards getting it to compile
    Former-commit-id: 1cda14f9268c8e5b47b3856968d462f6253dd737 [formerly 2d37741f451692800aaa087df786d2b84879cfb8]
    Former-commit-id: 3a8ccf24783584c3b8bfdfb9685eba2e82c32390 [formerly f9d8677cfed015631c4f8b7e0cfcf9c485a6a55a]
    Former-commit-id: 79d3e8f310bd3a6168d80ca3f181414f69e0204c

---
+++ src/mitll/langtest/shared/exercise/AnnoationExercise.java
@@ -0,0 +1,19 @@
+package mitll.langtest.shared.exercise;
+
+import mitll.langtest.shared.ExerciseAnnotation;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by go22670 on 1/5/16.
+ */
+public interface AnnoationExercise {
+  Map<String, ExerciseAnnotation> getFieldToAnnotation();
+
+  ExerciseAnnotation getAnnotation(String field);
+
+  void addAnnotation(String field, String status, String comment);
+
+  Collection<String> getFields();
+}
