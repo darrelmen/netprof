2a1b8e7ec48727204831eb531e7b72aaab1f1e9a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-05 16:22:31 -0500

    partial steps towards getting it to compile
    Former-commit-id: 3032bca9d870526f2c04a426a0ddf2ad83a475a6 [formerly 952a8abd2393a2559107ea0eca125fd00572af3a] [formerly f8bfcefac2279fce89f4e6a00ad346b33493db7d [formerly 94a02df4e8056ca06eec6c17d09d20734341e8d2]]
    Former-commit-id: 2e5e0d357cfcb97cccae982d6a20072809e77604 [formerly 43edb8ab2f8ff9f75c40246a028142ac106d5f29]
    Former-commit-id: 21b618019df05e9d2b38f2ce4bd9634d43979a55

--- src/mitll/langtest/shared/exercise/CommonExercise.java
+++ src/mitll/langtest/shared/exercise/CommonExercise.java
@@ -16,7 +16,7 @@ import java.util.Set;
 /**
  * Created by GO22670 on 3/20/2014.
  */
-public interface CommonExercise extends CommonShell {
+public interface CommonExercise extends CommonShell, AudioAttributeExercise, AnnoationExercise {
   String getContext();
   
   String getContextTranslation();
@@ -27,66 +27,9 @@ public interface CommonExercise extends CommonShell {
 
   String getTransliteration();
 
-  boolean hasRefAudio();
-
-  String getRefAudio();
-  String getRefAudioWithPrefs(Set<Long> prefs);
-
-  String getSlowAudioRef();
-
-  Collection<AudioAttribute> getAudioAttributes();
-  AudioAttribute getRegularSpeed();
-  AudioAttribute getSlowSpeed();
-
-  /**
-   * @see mitll.langtest.server.DatabaseServlet#getJsonForExercise(CommonExercise)
-   * @see mitll.langtest.server.ScoreServlet#getJsonArray(java.util.List)
-   * @return
-   */
-  AudioAttribute getLatestContext(boolean isMale);
-  AudioAttribute getRecordingsBy(long userID, boolean regularSpeed);
-  AudioAttribute getRecordingsBy(long userID, String speed);
-  Collection<AudioAttribute> getByGender(boolean isMale);
-  Collection<AudioAttribute> getDefaultUserAudio();
-
-  /**
-   * Sorted by user age. Gotta choose something...
-   * @param malesMap
-   * @return
-   */
-  List<MiniUser> getSortedUsers(Map<MiniUser, List<AudioAttribute>> malesMap);
-  Map<String, AudioAttribute> getAudioRefToAttr();
-  Map<MiniUser, List<AudioAttribute>> getUserMap(boolean isMale);
-
-  /**
-   * @see mitll.langtest.client.scoring.FastAndSlowASRScoringAudioPanel#getAfterPlayWidget()
-   * @param isMale
-   * @return
-   */
-  Map<MiniUser, List<AudioAttribute>> getMostRecentAudio(boolean isMale, Set<Long> preferredUsers);
-  void addAudio(AudioAttribute audioAttribute);
-
   CommonShell getShell();
 
-  Map<String, ExerciseAnnotation> getFieldToAnnotation();
-
-  ExerciseAnnotation getAnnotation(String field);
-
-  Map<String, String> getUnitToValue();
-
-  /**
-   * @see mitll.langtest.server.database.exercise.SectionHelper#addExerciseToLesson(CommonExercise, String, String)
-   * @param unit
-   * @param value
-   */
-  void addUnitToValue(String unit, String value);
-
-  void addAnnotation(String field, String status, String comment);
-
-//  long getModifiedDateTimestamp();
-
-  Collection<String> getFields();
-  boolean removeAudio(AudioAttribute audioAttribute);
+  //  long getModifiedDateTimestamp();
 
   /**
    * @see mitll.langtest.server.database.ResultDAO#attachScoreHistory(long, CommonExercise, boolean)
