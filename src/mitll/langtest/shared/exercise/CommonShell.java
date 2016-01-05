8baa991e6da622a08aee42eff8f27647b29de543
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-05 16:22:31 -0500

    partial steps towards getting it to compile
    Former-commit-id: e55d30900eebd245f64ab6afc0d8607bc55f1683 [formerly 9344a9b0265aad8b4ace2c81e699fe3b1a1336d8] [formerly 6ade3dc5b3802447e09eba2eb6ae44e080567bbb]
    Former-commit-id: d156d70751d256708ab12c22651f98f01ac74df5 [formerly ac2e2aff2e1219ad131599a93e3dc13b92db7854]
    Former-commit-id: efac47c86b3b90d0a93ae83fe9489bc729172474

--- src/mitll/langtest/shared/exercise/CommonShell.java
+++ src/mitll/langtest/shared/exercise/CommonShell.java
@@ -8,8 +8,6 @@ package mitll.langtest.shared.exercise;
  * Created by GO22670 on 3/21/2014.
  */
 public interface CommonShell extends Shell {
-  STATE getSecondState();
-  void setSecondState(STATE state);
 
   String getEnglish();
   String getMeaning();
