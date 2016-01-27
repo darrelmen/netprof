a8ac3abf65873ccd34bac43fc618b36b2d2f6e28
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-26 19:52:50 -0500

    closer to resolving merge problems
    Former-commit-id: 40c6469d460beb8683fd503dd11ee1594a822aef [formerly a49880982208ebd3713e8143bee600bd05f5de28] [formerly 40c6469d460beb8683fd503dd11ee1594a822aef [formerly a49880982208ebd3713e8143bee600bd05f5de28] [formerly 40c6469d460beb8683fd503dd11ee1594a822aef [formerly a49880982208ebd3713e8143bee600bd05f5de28] [formerly 440a50fc298d7ab78a7eedc89d86f9a4d9390d50 [formerly 759d1912f7baf5d2aa4bfc5c7f5a7fd858d48770]]]]
    Former-commit-id: 931c4eff524f45d8b7400c81840481a8a5fccc49 [formerly 4259ab245ee8ea9c9bb4376d265e26c3bfb3cc2f] [formerly 931c4eff524f45d8b7400c81840481a8a5fccc49 [formerly 4259ab245ee8ea9c9bb4376d265e26c3bfb3cc2f] [formerly ad848858b541e3dac97ec5a92ec9a7645bc517dc [formerly d16f518c572e2cdd865a78c153fc256423c45947]]] [formerly a5723d32d00b98d1fd801b8d25f0a83afd6d518d [formerly f744d8b9446f4805ef42c163452d7634ae8aa251] [formerly a5723d32d00b98d1fd801b8d25f0a83afd6d518d [formerly f744d8b9446f4805ef42c163452d7634ae8aa251] [formerly 33c4ddd0f14e9262e692c5a879159fd78db20d1c [formerly 2c5868f7b48c18392be9eb96cce32003df6532f2]]]]
    Former-commit-id: a5723d32d00b98d1fd801b8d25f0a83afd6d518d [formerly f744d8b9446f4805ef42c163452d7634ae8aa251]
    Former-commit-id: a918d300e0cead57ccb0e6545995eda6d6cea501 [formerly dccf3ff14273bcf6b51bd479ffd07ecfef00e80f] [formerly 2f825250d5ab0c9b15b591d298cce72834fc904e [formerly f719ee526721d0b3287e1e9593e7e64831121895]]
    Former-commit-id: 05601406607c40d6a7163d0e1b4b72366e9d5ae0 [formerly 74ce7bd50066424ccd7b475c89fb0799261b55e2]
    Former-commit-id: b49973d8aeecfd5e0ffbb1fe65a1833ca4bda881

---
+++ src/mitll/langtest/shared/amas/Answer.java
@@ -0,0 +1,56 @@
+package mitll.langtest.shared.amas;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by go22670 on 3/10/15.
+ */
+public class Answer implements IsSerializable {
+  double score = -1;
+  boolean correct = false;
+  private long resultID;
+
+  Answer() {} // support RPC
+
+  public Answer(double score, boolean correct, long resultID) {
+    this.score = score;
+    this.correct = correct;
+    this.resultID = resultID;
+  }
+  /**
+   * @see mitll.langtest.server.autocrt.AutoCRT#getFlashcardAnswer
+   * @param score
+   */
+  public void setScore(double score) { this.score = score; }
+
+  /**
+   * @see mitll.langtest.client.recorder.RecordButtonPanel#receivedAudioAnswer(mitll.langtest.shared.AudioAnswer, com.google.gwt.user.client.ui.Panel)
+   * @return score from hydec (see nnscore)
+   */
+  double getScore() { return score; }
+
+  /**
+   * @see mitll.langtest.client.recorder.RecordButtonPanel#receivedAudioAnswer(mitll.langtest.shared.AudioAnswer, com.google.gwt.user.client.ui.Panel)
+   * @see mitll.langtest.server.autocrt.AutoCRT#getAutoCRTDecodeOutput(String, int, java.io.File, mitll.langtest.shared.AudioAnswer)
+   * @return
+   */
+  boolean isCorrect() { return correct; }
+
+  /**
+   * @see mitll.langtest.server.autocrt.AutoCRT#getFlashcardAnswer
+   * @see mitll.langtest.server.autocrt.AutoCRT#markCorrectnessOnAnswer
+   * @param correct
+   */
+  public void setCorrect(boolean correct) {  this.correct = correct;  }
+
+  public long getResultID() {   return resultID;  }
+
+  /**
+   * @see mitll.langtest.server.audio.AudioFileHelper#writeAudioFile
+   * @param resultID
+   */
+  public void setResultID(long resultID) {  this.resultID = resultID;  }
+  public String toString() {
+    return "Answer id " + getResultID() + " correct " + isCorrect() + " score " + getScore();
+  }
+}
