97bca0b3e8ebd64608c79070f012b0e938a22576
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-26 19:52:50 -0500

    closer to resolving merge problems
    Former-commit-id: be2d4ea9885fbcfb09e7740b6f452ab3f107717e [formerly be2d4ea9885fbcfb09e7740b6f452ab3f107717e [formerly 402bb312549e80f832858a8a0239ee63bc3f9f35]]
    Former-commit-id: be451211315991e1745e1de33167c423aa15cc68 [formerly bcb9b685ce881b7d4abbbf4baef51508bba6dff4] [formerly 0c38632a24759bda742ae4a33c11918ff09152b8 [formerly 3454db215f2e11c61cbfb8b5fe97fcd2aa0c8b8a]]
    Former-commit-id: 0c38632a24759bda742ae4a33c11918ff09152b8
    Former-commit-id: 1a29c22080a80b1f213b8e033c41ccc706236cd8

--- src/mitll/langtest/server/database/exercise/SectionHelper.java
+++ src/mitll/langtest/server/database/exercise/SectionHelper.java
@@ -4,20 +4,11 @@
 
 package mitll.langtest.server.database.exercise;
 
-import mitll.langtest.shared.exercise.CommonExercise;
 import mitll.langtest.shared.SectionNode;
 import mitll.langtest.shared.exercise.Shell;
 import org.apache.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
