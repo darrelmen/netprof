a49880982208ebd3713e8143bee600bd05f5de28
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-01-26 19:52:50 -0500

    closer to resolving merge problems
    Former-commit-id: 759d1912f7baf5d2aa4bfc5c7f5a7fd858d48770

--- src/mitll/langtest/client/flashcard/AudioAnswerListener.java
+++ src/mitll/langtest/client/flashcard/AudioAnswerListener.java
@@ -9,6 +9,6 @@ import mitll.langtest.shared.AudioAnswer;
 /**
  * Created by go22670 on 2/11/14.
  */
-interface AudioAnswerListener {
+public interface AudioAnswerListener {
   void receivedAudioAnswer(AudioAnswer result);
 }
