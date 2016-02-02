f36c6b13aff87bdacf472ca241026984abfa9379
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-01 19:18:43 -0500

    compiles finally
    Former-commit-id: 00b2ef3e514246438749cc65b1e4903425f7c3d5 [formerly 6fd5533aaddde964c0caf8e758d6d3d297d3c40b] [formerly dc9e008c66c3ea233735baa15c533c5bf811dd7b]
    Former-commit-id: 2e0bc12c706f88561e5bdd2f8f4fee3157ffbf7a
    Former-commit-id: 2faa2a8edb34795994af8f15c611cff882d29c0f

---
+++ src/mitll/langtest/shared/exercise/EditableExercise.java
@@ -0,0 +1,7 @@
+package mitll.langtest.shared.exercise;
+
+/**
+ * Created by go22670 on 2/1/16.
+ */
+public interface EditableExercise extends CommonShell, AudioRefExercise, CombinedMutableUserExercise, AnnotationExercise, ScoredExercise {
+}
