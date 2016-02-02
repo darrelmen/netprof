f8c1abd4682635cac895fd273ea7be9217dc7ea7
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-01 19:18:43 -0500

    compiles finally
    Former-commit-id: 7d8e1e982f5711156d0414b7d1f4c0dd261c0b28 [formerly 32fc9965810c17546786d41687469f50be40efee] [formerly e721bb71ad3906466fb7978718394fcd44cd3e49 [formerly 363d2f20c93b45d148b63def1d3be95f4c9755cb]] [formerly 7f21e7cbf10dc667455fd68e309a46735594c471 [formerly 9bfa0f1331a6619d7fcc25841796f835c28fb126] [formerly cd61e726054d86cacc4a46b911f07c54edae9b05 [formerly 76bd13f5a332b1944ed91a3a5171647ebfeb6aa2]]]
    Former-commit-id: f55d65fe891df99bc35e5e37504b6ab5d25b2478 [formerly 6a610ced6c1451ee3d7be99cc21a728e3ab4e6e4] [formerly c2a693849222c980117b2f18adc6c707bc2a9a28 [formerly 6eebdd8caa3608b8fc6325b0f2d8683febe6aa52]]
    Former-commit-id: 97d9b9cb441b9bc51c676530f13769eab9d0824a [formerly 82b4bc249942b13efd335ec7b6fb14f08de21342]
    Former-commit-id: 5e2c56ecb18e16244fb43b474da936a03f812dba

---
+++ src/mitll/langtest/shared/exercise/MutableAnnotationExercise.java
@@ -0,0 +1,8 @@
+package mitll.langtest.shared.exercise;
+
+/**
+ * Created by go22670 on 2/1/16.
+ */
+public interface MutableAnnotationExercise {
+  void addAnnotation(String field, String status, String comment);
+}
