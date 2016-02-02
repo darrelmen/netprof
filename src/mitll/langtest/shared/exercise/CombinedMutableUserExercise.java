4e29d6161c63a1efdc7cb218f0c56aecad296241
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-02 11:48:44 -0500

    starts up properly
    Former-commit-id: 95f51800b5d3fbc159c50afba4d9f62679a55926 [formerly 972e6e74c056614a9025cbe7b97a58ecd19fb94f] [formerly 70ec5cc80e0d97feacc519252e327888577bac11 [formerly 0b519012e843f1fc9d15ea8db9c109e17268086c]] [formerly a405b0949ed569266d361fd0a7d43e3640a50c78 [formerly f0c51f8efb43ac47003cf765c7ae563bb568f743] [formerly fdb88107fbcd6224be25aa902ddb4b504d3c3a38]]
    Former-commit-id: 0b268d38d0e9a265b87d2f783f710b6198ad73c4 [formerly d5e91462a6dad3786813aa533b99e7d15041a5d8]
    Former-commit-id: 04a328be2659a2f34c258eedeaa4cc1659b5c134 [formerly 19f0c1252a546da716569cbaed00ed960ace01c8]
    Former-commit-id: 7ca4d458bdfef84660d709219bfa1430c358f6ce

--- src/mitll/langtest/shared/exercise/CombinedMutableUserExercise.java
+++ src/mitll/langtest/shared/exercise/CombinedMutableUserExercise.java
@@ -3,5 +3,6 @@ package mitll.langtest.shared.exercise;
 /**
  * Created by go22670 on 2/1/16.
  */
-public interface CombinedMutableUserExercise extends CommonExercise, MutableExercise, MutableAudioExercise, MutableUserExercise {
+public interface CombinedMutableUserExercise extends CommonExercise,
+    MutableExercise, MutableAudioExercise, MutableUserExercise, MutableAnnotationExercise {
 }
