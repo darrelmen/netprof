75340a9e28713bd637e372e48c767ab5c2b1a234
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-02 13:49:26 -0500

    added HasID interface, fixed some issues with comments
    Former-commit-id: 6c3a210e7178415a65d74ae7c042720c1f73b57a [formerly 0439229216e00e9bfbf579a22fba10ee74258326] [formerly 9c0f18bdd315da5d332f524e444fc7653061f251]
    Former-commit-id: 798a892a28b68eb3c79c5ad27e3e159e822d4f91 [formerly 1902e2250627bd4092ea8f2de426e45b12c4a594] [formerly 14bf478c3c7715a36dfa70f591d36b9768c6caaa]
    Former-commit-id: d29c69944c1f5658e4549b68fde29c65c44ac83b
    Former-commit-id: d30214527192704e4123b1f0adb2b87c887fdeb7

---
+++ src/mitll/langtest/shared/exercise/HasID.java
@@ -0,0 +1,10 @@
+package mitll.langtest.shared.exercise;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by go22670 on 2/2/16.
+ */
+public interface HasID extends IsSerializable {
+  String getID();
+}
