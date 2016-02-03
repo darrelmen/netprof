209b09504a04706050f45ca4171d6d823f9e587b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-03 15:07:29 -0500

    a number of bug fixes to review editable dialog
    Former-commit-id: b6aa3d0e0e47d7b662dfa8d9156906aa9a08b5ea [formerly 5d3a8ddb5c83fb34c3f52c86d966f892b2b85e37] [formerly 8a9256dc9ca30192f39abdc5700fdabd6d0e1ebd [formerly 76d6c3ce7cf1ff8541956f238c8af896c2207b16]]
    Former-commit-id: a0eb8b957fc6ed4a73a589f6aa0265133795babd [formerly 390a61417d45c4285093d82de134e36ec30204c1] [formerly 1fbdc3aa251f614d4b199ebe2892ec0efe642840 [formerly 306eab3c03b0b6471e0231f3e194b16bab6f0b35]]
    Former-commit-id: 8ea9c40db3c8cbb76d2c60e50ec938edf6fa7c84 [formerly 53fe1a0c60ee17d2a9ce7fa96b91b818e0227157]
    Former-commit-id: 65204c4a4dcc676ffa17a2af215a052cef37a813

---
+++ src/mitll/langtest/shared/exercise/MutableShell.java
@@ -0,0 +1,9 @@
+package mitll.langtest.shared.exercise;
+
+/**
+ * Created by go22670 on 2/3/16.
+ */
+public interface MutableShell {
+  void setEnglish(String english);
+  void setForeignLanguage(String foreignLanguage);
+}
