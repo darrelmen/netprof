9e3aa26f688247ffc8ea5feac14df4fa6dacd03c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-03 13:44:37 -0500

    fixed type parameterization issues
    Former-commit-id: d6b8537441c5a0eeb2624aa2685b1c025a198f82 [formerly 2168a4da22726b44850f6eca81143d36cff0174d] [formerly d6b8537441c5a0eeb2624aa2685b1c025a198f82 [formerly 2168a4da22726b44850f6eca81143d36cff0174d] [formerly d6b8537441c5a0eeb2624aa2685b1c025a198f82 [formerly 2168a4da22726b44850f6eca81143d36cff0174d] [formerly 199fa4e5e06df34d00de9e43842ae15272810406]]]
    Former-commit-id: b2f515a1f9171fa416096ae8df73c3d6dc813dc5 [formerly 347725333a12770530dea59bbf31b04bd9e6e7ad] [formerly b2f515a1f9171fa416096ae8df73c3d6dc813dc5 [formerly 347725333a12770530dea59bbf31b04bd9e6e7ad] [formerly e90d967a151baad967aa304b8076f7ccfd21b78b]] [formerly 152899c1b8a9df516567bb8078c684ba116bc803 [formerly d442042cd12ab29ebe8edec5587769a5809f9625] [formerly 152899c1b8a9df516567bb8078c684ba116bc803 [formerly d442042cd12ab29ebe8edec5587769a5809f9625] [formerly c772ecb5d96cea29cc856d70d84eb09253d92e7e]]]
    Former-commit-id: 152899c1b8a9df516567bb8078c684ba116bc803 [formerly d442042cd12ab29ebe8edec5587769a5809f9625]
    Former-commit-id: 1a9d143c85c4f2cf18eec7705dd1a2feda10a034 [formerly 089d9bca9b241b858c06932b5e52a64f4d48e805] [formerly 6294960b16bb083d258a762756a228a5f7f26fc2]
    Former-commit-id: 13d69647b752ef41fa593b308f25f1fe05afe95e
    Former-commit-id: e1aaf729085075c8e7d329a4c105184ffc23bb35

--- src/mitll/langtest/client/exercise/PostAnswerProvider.java
+++ src/mitll/langtest/client/exercise/PostAnswerProvider.java
@@ -4,7 +4,7 @@
 
 package mitll.langtest.client.exercise;
 
-import mitll.langtest.shared.exercise.Shell;
+import mitll.langtest.shared.exercise.HasID;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +13,6 @@ import mitll.langtest.shared.exercise.Shell;
  * Time: 11:39 AM
  * To change this template use File | Settings | File Templates.
  */
-public interface PostAnswerProvider<T extends Shell> {
-  void postAnswers(ExerciseController controller, T completedExercise);
+public interface PostAnswerProvider {
+  void postAnswers(ExerciseController controller, HasID completedExercise);
 }
