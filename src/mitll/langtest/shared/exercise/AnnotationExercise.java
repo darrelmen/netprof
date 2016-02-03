eba2cb35d16185c4be2330ce1c5ec85489134766
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-03 15:07:29 -0500

    a number of bug fixes to review editable dialog
    Former-commit-id: 3c9099f8138b4d53ac1bbc5bccc9d6ba10aac766 [formerly 9d5e5d15c06a54c2af59b1d5a4657b1b674f34b5] [formerly 3c9099f8138b4d53ac1bbc5bccc9d6ba10aac766 [formerly 9d5e5d15c06a54c2af59b1d5a4657b1b674f34b5] [formerly 3c9099f8138b4d53ac1bbc5bccc9d6ba10aac766 [formerly 9d5e5d15c06a54c2af59b1d5a4657b1b674f34b5] [formerly 37d246bd0dd681c97822658c00a5e1e3892fa281]]]
    Former-commit-id: 6d5582e66a162e5e4695fcc1ffbf79d7961b782c [formerly c3c38eadbc6b015311ef695eacc40c3470ea43e1] [formerly 6d5582e66a162e5e4695fcc1ffbf79d7961b782c [formerly c3c38eadbc6b015311ef695eacc40c3470ea43e1] [formerly 7178ca6adb96bb64516e92db5906ceb7ece1a499]] [formerly 1d19c2bfb9a0858c63e0be2d7f7dc5e4de3ae99f [formerly 6f6a606fc5b094bb7e846091782b36425f7acfc8] [formerly 1d19c2bfb9a0858c63e0be2d7f7dc5e4de3ae99f [formerly 6f6a606fc5b094bb7e846091782b36425f7acfc8] [formerly aa9f0e69f453afe0147182a9b58e6d3868d4e800]]]
    Former-commit-id: 1d19c2bfb9a0858c63e0be2d7f7dc5e4de3ae99f [formerly 6f6a606fc5b094bb7e846091782b36425f7acfc8]
    Former-commit-id: fa69b9a0d4c18b877125fb6cfa36460d6f6b7875 [formerly eb7b837684ed009bb389dcef8e1a0c85e7a4d574] [formerly 9b82ebf3a4eaf8d395e1e04cebddcacaf0fb392f]
    Former-commit-id: b74a5dcd4a35c1ee2369102455a5e5800a8518f3
    Former-commit-id: 0ae86c783066a1433a37b90aa0bbb73e2a2a707a

--- src/mitll/langtest/shared/exercise/AnnotationExercise.java
+++ src/mitll/langtest/shared/exercise/AnnotationExercise.java
@@ -9,12 +9,18 @@ import java.util.Map;
  * Created by go22670 on 1/5/16.
  */
 public interface AnnotationExercise {
+  /**
+   * @see mitll.langtest.server.database.custom.UserListManager#duplicate(CommonExercise)
+   * @return
+   */
   Map<String, ExerciseAnnotation> getFieldToAnnotation();
 
+  /**
+   * @see mitll.langtest.client.custom.dialog.EditableExerciseDialog#setFields(CommonShell)
+   * @param field
+   * @return
+   */
   ExerciseAnnotation getAnnotation(String field);
 
   Collection<String> getFields();
-
- // MutableAnnotationExercise getMutableAnnotation();
-
 }
