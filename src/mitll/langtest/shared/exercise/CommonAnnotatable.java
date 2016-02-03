31dbc3ea2f3022dc0566e93b5727f2cad65eb8cf
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-03 13:44:37 -0500

    fixed type parameterization issues
    Former-commit-id: 84aeae91cd411a87afcf39853973383665cd8296

---
+++ src/mitll/langtest/shared/exercise/CommonAnnotatable.java
@@ -0,0 +1,7 @@
+package mitll.langtest.shared.exercise;
+
+/**
+ * Created by go22670 on 2/3/16.
+ */
+public interface CommonAnnotatable extends CommonShell, AudioRefExercise, AnnotationExercise, ScoredExercise, MutableAnnotationExercise {
+}
