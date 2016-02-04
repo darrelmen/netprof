304eb7cd507845fcc77e1936542442c1277080df
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-04 18:47:26 -0500

    attempts to clean up Navigation and remove redundant helpers in an attempt to make learn helper reflect defect edit changes
    Former-commit-id: 8af974e518c3549fa919bce22111f00f1ebe156b [formerly 4a29e858f4b12a6b3687521111266b9ca06aecbb]
    Former-commit-id: 60ecf2e827399b39cc47ca7477e8d0c542a9c960

---
+++ src/mitll/langtest/client/custom/ReloadableContainer.java
@@ -0,0 +1,10 @@
+package mitll.langtest.client.custom;
+
+import mitll.langtest.client.list.Reloadable;
+
+/**
+ * Created by go22670 on 2/4/16.
+ */
+public interface ReloadableContainer {
+  Reloadable getReloadable();
+}
