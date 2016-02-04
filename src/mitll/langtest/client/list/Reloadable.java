919f9870c84748c0475736ca23aefb734b03b366
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-04 18:47:26 -0500

    attempts to clean up Navigation and remove redundant helpers in an attempt to make learn helper reflect defect edit changes
    Former-commit-id: d5befe169578405a6bd16550b8d0597f9bf22775 [formerly 60ecf2e827399b39cc47ca7477e8d0c542a9c960] [formerly 1c31983d36a47ba91b84babe56ece3e529274545 [formerly 4a29e858f4b12a6b3687521111266b9ca06aecbb]]
    Former-commit-id: d6c44fc30b08db8ee89f3161b9c37fcf4cc1d159 [formerly 630abff223e352bbb0672912318450b19cf54248] [formerly ee4339bcda0a7d3d856367560bd347713682cf21 [formerly 45ee5c15875cf24249b670dd0ab1f60d4f11a205]]
    Former-commit-id: ba76d856ea8099c964210fb3a72566e2cfe6c98d [formerly 3bd4210e26a70acad7aa41f5ef5d28558ee3551e]
    Former-commit-id: 42082fc9de27a1368422c98836f5d0722b7d0ba0

--- src/mitll/langtest/client/list/Reloadable.java
+++ src/mitll/langtest/client/list/Reloadable.java
@@ -4,7 +4,14 @@ package mitll.langtest.client.list;
  * Created by go22670 on 2/2/16.
  */
 public interface Reloadable {
+  void redraw();
   void reload();
 
   void reloadWithCurrent();
+
+  void clearCachedExercise();
+
+  String getCurrentExerciseID();
+
+  void loadExercise(String itemID);
 }
