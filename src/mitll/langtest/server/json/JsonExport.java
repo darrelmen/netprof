6cd50a4465ad7ed60992370fbf779cadaf1391c5
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-09 18:16:02 -0500

    added json export of exercises
    Former-commit-id: 177be68ca86900c999c5559ac7a0d508d90aa0a6 [formerly ddc8763cdb22e895874a7529b9c9ab04f981ba8f] [formerly 177be68ca86900c999c5559ac7a0d508d90aa0a6 [formerly ddc8763cdb22e895874a7529b9c9ab04f981ba8f] [formerly 192cf21095b73d4ad488394c0fdd73adeabbf562]]
    Former-commit-id: 8d1dc271064bd80a3dce9f1ab79de2aea51dc42f [formerly 646a1b973057a53418d2dfc03cae1c724cd08d0a] [formerly eed9bfd04fbfd13b4cd48697dd42b39d839bbfef] [formerly 168e2688266e3b212807703d2c0ed7efabaf9226 [formerly 15bbcd6bdb90387f8cb682327dfbb0a6125248c2] [formerly 05c3566de58be3c46dba450d458c30ee25e1e9ea]]
    Former-commit-id: 168e2688266e3b212807703d2c0ed7efabaf9226 [formerly 15bbcd6bdb90387f8cb682327dfbb0a6125248c2]
    Former-commit-id: 96556f1573c2e9f2ba2deecdaae9253c3e37bb54
    Former-commit-id: b6a9131be96006b397b486981e202ec2d3e89b6d

--- src/mitll/langtest/server/json/JsonExport.java
+++ src/mitll/langtest/server/json/JsonExport.java
@@ -1,13 +1,13 @@
 package mitll.langtest.server.json;
 
+import mitll.langtest.server.ScoreServlet;
 import mitll.langtest.server.database.exercise.SectionHelper;
 import mitll.langtest.server.sorter.ExerciseSorter;
 import mitll.langtest.shared.SectionNode;
-import mitll.langtest.shared.exercise.AudioAttribute;
-import mitll.langtest.shared.exercise.AudioRefExercise;
-import mitll.langtest.shared.exercise.CommonExercise;
+import mitll.langtest.shared.exercise.*;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
 
 import java.util.*;
 
@@ -15,27 +15,98 @@ import java.util.*;
  * Created by go22670 on 2/5/16.
  */
 public class JsonExport {
+  private static final Logger logger = Logger.getLogger(JsonExport.class);
+
   private static final String NO = "NO";
 
   private static final String TYPE = "type";
   private static final String NAME = "name";
   private static final String ITEMS = "items";
   private static final String CHILDREN = "children";
+  private static final String CTMREF = "ctmref";
+  private static final String CTFREF = "ctfref";
+  private static final String REF = "ref";
+  private static final String ID = "id";
+  private static final String FL = "fl";
+  private static final String TL = "tl";
+  private static final String EN = "en";
+  private static final String CT = "ct";
+  private static final String CTR = "ctr";
   private final Map<String, Integer> phoneToCount;
   private final SectionHelper<CommonExercise> sectionHelper;
   private final Set<Long> preferredVoices;
 
   /**
-   * @see mitll.langtest.server.ScoreServlet#getJsonNestedChapters(boolean)
    * @param phoneToCount
    * @param sectionHelper
    * @param preferredVoices
+   * @see mitll.langtest.server.ScoreServlet#getJsonNestedChapters(boolean)
    */
-  public JsonExport(Map<String, Integer> phoneToCount, SectionHelper<CommonExercise> sectionHelper,
+  public JsonExport(Map<String, Integer> phoneToCount,
+                    SectionHelper<CommonExercise> sectionHelper,
                     Set<Long> preferredVoices) {
     this.phoneToCount = phoneToCount;
     this.sectionHelper = sectionHelper;
     this.preferredVoices = preferredVoices;
+  }
+
+  public <T extends CommonShell & AudioAttributeExercise> void addJSONExerciseExport(JSONObject jsonObject,
+                                                                                     Collection<T> exercises) {
+
+    jsonObject.put("Count", exercises.size());
+
+    JSONArray value = new JSONArray();
+    for (String type : sectionHelper.getTypeOrder()) {
+      value.add(type);
+    }
+    jsonObject.put("UnitOrder", value);
+
+    JSONArray nesting = new JSONArray();
+
+    Collection<SectionNode> sectionNodes = sectionHelper.getSectionNodes();
+    addSections(nesting, sectionNodes);
+
+    jsonObject.put("UnitChapterNesting", nesting);
+    jsonObject.put(ScoreServlet.CONTENT, getExercisesAsJson(exercises));
+  }
+
+  private void addSections(JSONArray nesting, Collection<SectionNode> sectionNodes) {
+    for (SectionNode node : sectionNodes) {
+      JSONObject forNode = new JSONObject();
+      forNode.put("type", node.getType());
+      forNode.put("name", node.getName());
+      JSONArray children = new JSONArray();
+
+      Collection<SectionNode> children1 = node.getChildren();
+      if (!children1.isEmpty()) {
+        addSections(children, children1);
+      }
+      forNode.put("children", children);
+      nesting.add(forNode);
+    }
+  }
+
+  public <T extends CommonShell & AudioAttributeExercise> JSONArray getExercisesAsJson(Collection<T> exercises) {
+    JSONArray jsonArray = new JSONArray();
+    List<T> copy = new ArrayList<>(exercises);
+    Collections.sort(copy, new Comparator<T>() {
+      @Override
+      public int compare(T o1, T o2) {
+        return o1.getID().compareTo(o2.getID());
+      }
+    });
+
+    for (T exercise : exercises) {
+      JSONObject jsonForCommonExercise = getJsonForCommonExercise(exercise);
+      Map<String, String> unitToValue = exercise.getUnitToValue();
+      for (Map.Entry<String, String> pair : unitToValue.entrySet()) {
+        jsonForCommonExercise.put(pair.getKey(), pair.getValue());
+      }
+
+      jsonArray.add(jsonForCommonExercise);
+
+    }
+    return jsonArray;
   }
 
   /**
@@ -98,7 +169,6 @@ public class JsonExport {
 
     List<CommonExercise> copy = new ArrayList<>(exercisesForState);
 
-    //  boolean removeExercisesWithMissingAudio = REMOVE_EXERCISES_WITH_MISSING_AUDIO;
     if (removeExercisesWithMissingAudio) {
       Iterator<CommonExercise> iterator = copy.iterator();
       for (; iterator.hasNext(); ) {
@@ -106,12 +176,7 @@ public class JsonExport {
         if (!next.hasRefAudio()) iterator.remove();
       }
     }
-//    if (audioFileHelper != null) {
-    getExerciseSorter().sortedByPronLengthThenPhone(copy, phoneToCount);// audioFileHelper.getPhoneToCount());
-    ///  }
-    /* else {
-      logger.warn("audioFileHelper not set yet!");
-    }*/
+    getExerciseSorter().sortedByPronLengthThenPhone(copy, phoneToCount);
     return getJsonArray(copy);
   }
 
@@ -133,16 +198,7 @@ public class JsonExport {
    */
   private JSONArray getJsonArray(List<CommonExercise> copy) {
     JSONArray exercises = new JSONArray();
-
-/*    Map<String, List<AudioAttribute>> exToAudio = db.getAudioDAO().getExToAudio();
-    String installPath = pathHelper.getInstallPath();*/
-
     for (CommonExercise exercise : copy) {
-/*      List<AudioAttribute> audioAttributes = exToAudio.get(exercise.getID());
-      if (audioAttributes != null) {
-        db.getAudioDAO().attachAudio(exercise, installPath, relativeConfigDir, audioAttributes);
-      }*/
-      //if (!debug) ensureMP3s(exercise);
       exercises.add(getJsonForExercise(exercise));
     }
     return exercises;
@@ -156,30 +212,36 @@ public class JsonExport {
    * @param exercise
    * @return
    */
-  private JSONObject getJsonForExercise(CommonExercise exercise) {
-    JSONObject ex = new JSONObject();
-    ex.put("id", exercise.getID());
-    ex.put("fl", exercise.getForeignLanguage());
-    ex.put("tl", exercise.getTransliteration());
-    ex.put("en", exercise.getEnglish());
-    ex.put("ct", exercise.getContext());
-    ex.put("ctr", exercise.getContextTranslation());
+  private <T extends CommonShell & AudioAttributeExercise> JSONObject getJsonForExercise(T exercise) {
+    JSONObject ex = getJsonForCommonExercise(exercise);
+
     AudioAttribute latestContext = exercise.getLatestContext(true);
     //if (latestContext != null) {
     //  String author = latestContext.getUser().getUserID();
     //  if (CHECK_FOR_MP3) ensureMP3(latestContext.getAudioRef(), exercise.getContext(), author);
     // }
-    ex.put("ctmref", latestContext == null ? NO : latestContext.getAudioRef());
+    ex.put(CTMREF, latestContext == null ? NO : latestContext.getAudioRef());
     latestContext = exercise.getLatestContext(false);
     // if (latestContext != null) {
     // String author = latestContext.getUser().getUserID();
     // if (CHECK_FOR_MP3) ensureMP3(latestContext.getAudioRef(), exercise.getContext(), author);
     // }
-    ex.put("ctfref", latestContext == null ? NO : latestContext.getAudioRef());
-    ex.put("ref", exercise.hasRefAudio() ? exercise.getRefAudioWithPrefs(preferredVoices) : NO);
+    ex.put(CTFREF, latestContext == null ? NO : latestContext.getAudioRef());
+    ex.put(REF, exercise.hasRefAudio() ? exercise.getRefAudioWithPrefs(preferredVoices) : NO);
 
     addLatestRefs(preferredVoices, exercise, ex);
 
+    return ex;
+  }
+
+  private JSONObject getJsonForCommonExercise(CommonShell exercise) {
+    JSONObject ex = new JSONObject();
+    ex.put(ID, exercise.getID());
+    ex.put(FL, exercise.getForeignLanguage());
+    ex.put(TL, exercise.getTransliteration());
+    ex.put(EN, exercise.getEnglish());
+    ex.put(CT, exercise.getContext());
+    ex.put(CTR, exercise.getContextTranslation());
     return ex;
   }
 
@@ -192,15 +254,12 @@ public class JsonExport {
    * @see #getJsonForExercise
    */
   private void addLatestRefs(Set<Long> preferredVoices, AudioRefExercise exercise, JSONObject ex) {
-    //Set<Long> preferredVoices = serverProps.getPreferredVoices();
-
     String mr = null, ms = null, fr = null, fs = null;
     long mrt = 0, mst = 0, frt = 0, fst = 0;
     AudioAttribute mra = null, msa = null, fra = null, fsa = null;
 
     for (AudioAttribute audioAttribute : exercise.getAudioAttributes()) {
       long timestamp = audioAttribute.getTimestamp();
-      //boolean isPrefVoice = preferredVoices.contains(audioAttribute.getUserid());
       if (audioAttribute.isMale()) {
         if (audioAttribute.isRegularSpeed()) {
           if (timestamp >= mrt) {
@@ -264,57 +323,4 @@ public class JsonExport {
 //    }
     ex.put("fsr", fs == null ? NO : fs);
   }
-
-  /**
-   * @param sectionNodes
-   * @param removeExercisesWithMissingAudio
-   * @return
-   * @seexx mitll.langtest.server.ScoreServlet#getJsonLeastRecordedChapters
-   */
-/*  public JSONArray getContentAsJson2(Collection<SectionNode> sectionNodes, boolean removeExercisesWithMissingAudio) {
-    JSONArray jsonArray = new JSONArray();
-    Map<String, Collection<String>> typeToValues = new HashMap<>();
-
-    //if (audioFileHelper != null) {
-      for (SectionNode node : sectionNodes) {
-        String type = node.getType();
-        typeToValues.put(type, Collections.singletonList(node.getName()));
-        JSONObject jsonForNode = getJsonForNode2(node, typeToValues, removeExercisesWithMissingAudio);
-        typeToValues.remove(type);
-
-        jsonArray.add(jsonForNode);
-      }
-    //}
-    return jsonArray;
-  }*/
-
-  /**
-   * @param node
-   * @param typeToValues
-   * @param removeExercisesWithMissingAudio
-   * @return
-   * @see #getContentAsJson2
-   */
-/*  private JSONObject getJsonForNode2(SectionNode node, Map<String, Collection<String>> typeToValues, boolean removeExercisesWithMissingAudio) {
-    JSONObject jsonForNode = new JSONObject();
-    jsonForNode.put(TYPE, node.getType());
-    jsonForNode.put(NAME, node.getName());
-    JSONArray jsonArray = new JSONArray();
-
-    if (node.isLeaf()) {
-      JSONArray exercises = getJsonForSelection(typeToValues, removeExercisesWithMissingAudio);
-      jsonForNode.put(ITEMS, exercises);
-    } else {
-      List<SectionNode> children = node.getChildren();
-      Collections.sort(children); // by avg recorded number
-
-      for (SectionNode child : children) {
-        typeToValues.put(child.getType(), Collections.singletonList(child.getName()));
-        jsonArray.add(getJsonForNode2(child, typeToValues, removeExercisesWithMissingAudio));
-        typeToValues.remove(child.getType());
-      }
-    }
-    jsonForNode.put(CHILDREN, jsonArray);
-    return jsonForNode;
-  }*/
 }
