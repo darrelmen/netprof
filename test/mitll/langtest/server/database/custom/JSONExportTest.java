7d1b186dded6f101e2b97b48e985bc54c9816f33
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-09 18:16:02 -0500

    added json export of exercises
    Former-commit-id: 003c90a081ffef7e1984ac95da86d1f228aaae6c [formerly f2a5e9a0507366f1839470ce25af858d992d878d] [formerly 003c90a081ffef7e1984ac95da86d1f228aaae6c [formerly f2a5e9a0507366f1839470ce25af858d992d878d] [formerly 45919078113483e8c9493ccd8cc69a36f0b53926]]
    Former-commit-id: b6aebfbdc49c9e387f587a5a1a5da46462220b35 [formerly d04967ebf04e5944534ff99c75a445ff648d32c2] [formerly f7201891002ddd7a701dd6df72b7b3422ad1d3d0] [formerly cc9eac4612706ba1ed58cfc6f99e3426ca4a304e [formerly 80c26c2c02be06dd1d92b07c4f907c0a8671124d] [formerly 069c0a5304aca92e941422b9f4219640db60ab8c]]
    Former-commit-id: cc9eac4612706ba1ed58cfc6f99e3426ca4a304e [formerly 80c26c2c02be06dd1d92b07c4f907c0a8671124d]
    Former-commit-id: 9aef4938d63c40a69205422d59d8955f0d334a3f
    Former-commit-id: 1595fb659842dc6d7f5154a9eb4d53a8902f9f24

---
+++ test/mitll/langtest/server/database/custom/JSONExportTest.java
@@ -0,0 +1,73 @@
+package mitll.langtest.server.database.custom;
+
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.server.database.DatabaseImpl;
+import mitll.langtest.server.json.JsonExport;
+import mitll.langtest.shared.exercise.CommonExercise;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Created by go22670 on 2/9/16.
+ */
+public class JSONExportTest {
+  private static final String ENGLISH = "english";
+  private static final Logger logger = Logger.getLogger(JSONExportTest.class);
+  private static DatabaseImpl database;
+  //private static String dbName;
+
+  @BeforeClass
+  public static void setup() {
+    logger.debug("setup called");
+
+    String config = "spanish";//"mandarin";
+    // dbName = "npfSpanish";//"mandarin";// "mandarin";
+
+    getDatabase(config, "npfSpanish");
+  }
+
+  private static void getDatabase(String config, String dbName) {
+    File file = new File("war" + File.separator + "config" + File.separator + config + File.separator + "quizlet.properties");
+    String parent = file.getParent();
+    logger.debug("config dir " + parent);
+    logger.debug("config     " + file.getName());
+    //  dbName = "npfEnglish";//"mandarin";// "mandarin";
+    database = new DatabaseImpl(parent, file.getName(), dbName, new ServerProperties(parent, file.getName()), new PathHelper("war"), false, null);
+    logger.debug("made " + database);
+    String media = parent + File.separator + "media";
+    logger.debug("media " + media);
+    database.setInstallPath(".", parent + File.separator + database.getServerProps().getLessonPlan(), "media");
+    Collection<CommonExercise> exercises = database.getExercises();
+  }
+
+  @Test
+  public void testExport() {
+    JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
+    JSONArray exercisesAsJson = jsonExport.getExercisesAsJson(database.getExercises());
+    logger.info("got " +exercisesAsJson);
+
+
+    JSONObject jsonObject = new JSONObject();
+    jsonExport.addJSONExerciseExport(jsonObject,database.getExercises());
+
+    logger.info("got " +jsonObject);
+
+  }
+
+  @Test
+  public void testExport2() {
+    JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
+    JSONObject jsonObject = new JSONObject();
+    jsonExport.addJSONExerciseExport(jsonObject,database.getExercises());
+    logger.info("got " +jsonObject);
+
+  }
+
+}
