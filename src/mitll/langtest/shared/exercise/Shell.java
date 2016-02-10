f40371aa9dd9c278b687cadc52bb398326e0744d
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-10 14:20:48 -0500

    added JSONExerciseDAO
    Former-commit-id: 258ecb64fa8923e4b1a972a4b8be7ac7ba4f6bc9
    Former-commit-id: 3a64265bef27d2c239acf617931de2808971e3bf

--- src/mitll/langtest/shared/exercise/Shell.java
+++ src/mitll/langtest/shared/exercise/Shell.java
@@ -1,6 +1,8 @@
 package mitll.langtest.shared.exercise;
 
+import com.github.gwtbootstrap.client.ui.Button;
 import com.google.gwt.user.client.rpc.IsSerializable;
+import mitll.langtest.client.exercise.PagingContainer;
 
 import java.util.Map;
 
@@ -8,9 +10,18 @@ import java.util.Map;
  * Created by go22670 on 1/4/16.
  */
 public interface Shell extends HasID {
+  /**
+   * @see PagingContainer#getEnglishColumn()
+   * @return
+   */
   STATE getState();
   void setState(STATE state);
 
+  /**
+   * @see PagingContainer#getEnglishColumn()
+   * @see mitll.langtest.client.custom.dialog.ReviewEditableExercise#duplicateExercise(Button)
+   * @return
+   */
   STATE getSecondState();
 
   void setSecondState(STATE state);
@@ -18,7 +29,7 @@ public interface Shell extends HasID {
   Map<String, String> getUnitToValue();
 
   /**
-   * @see mitll.langtest.server.database.exercise.SectionHelper#addExerciseToLesson(CommonExercise, String, String)
+   * @see mitll.langtest.server.database.exercise.SectionHelper#addExerciseToLesson
    * @param unit
    * @param value
    */
