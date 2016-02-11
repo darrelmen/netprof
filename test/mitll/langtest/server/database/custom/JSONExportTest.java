257485eac11b56d9bedc1ed2ea6f9e75af14a440
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-10 19:28:58 -0500

    reads from exercise json correctly
    Former-commit-id: 4090b1fb13221f31ea7f8b0a4bc0c7100fb52082 [formerly fd90b8bf57cfa0d08cb553764a979e16a84e16e9] [formerly 82cdd1308642dd0729d028221a3de5cd1cceee96]
    Former-commit-id: 5269b2036f108f987f102613806dbdc12aa44207 [formerly cb1ef0e9cef828425c0e2ee4ea3b015555baa957]
    Former-commit-id: e88a36257365d5ac39ebdac56e6f82d88db148a2

--- test/mitll/langtest/server/database/custom/JSONExportTest.java
+++ test/mitll/langtest/server/database/custom/JSONExportTest.java
@@ -3,6 +3,7 @@ package mitll.langtest.server.database.custom;
 import mitll.langtest.server.PathHelper;
 import mitll.langtest.server.ServerProperties;
 import mitll.langtest.server.database.DatabaseImpl;
+import mitll.langtest.server.database.exercise.JSONExerciseDAO;
 import mitll.langtest.server.json.JsonExport;
 import mitll.langtest.shared.exercise.CommonExercise;
 import net.sf.json.JSONArray;
@@ -12,7 +13,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by go22670 on 2/9/16.
@@ -51,29 +56,144 @@ public class JSONExportTest {
   public void testExport() {
     JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
     JSONArray exercisesAsJson = jsonExport.getExercisesAsJson(database.getExercises());
-    logger.info("got " +exercisesAsJson);
+    logger.info("got " + exercisesAsJson);
 
     JSONObject jsonObject = new JSONObject();
-    jsonExport.addJSONExerciseExport(jsonObject,database.getExercises());
+    jsonExport.addJSONExerciseExport(jsonObject, database.getExercises());
 
-    logger.info("got " +jsonObject);
+    logger.info("got " + jsonObject);
   }
 
   @Test
   public void testExport2() {
     JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
     JSONObject jsonObject = new JSONObject();
-    jsonExport.addJSONExerciseExport(jsonObject,database.getExercises());
-    logger.info("got " +jsonObject);
+    jsonExport.addJSONExerciseExport(jsonObject, database.getExercises());
+    logger.info("got " + jsonObject);
 
   }
+
   @Test
   public void testExport3() {
     JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
     JSONObject jsonObject = new JSONObject();
-    jsonExport.addJSONExerciseExport(jsonObject,database.getExercises());
-    logger.info("got " +jsonObject);
+    jsonExport.addJSONExerciseExport(jsonObject, database.getExercises());
+    logger.info("got " + jsonObject);
     String s = jsonObject.toString();
+
+
     Collection<CommonExercise> exercises = jsonExport.getExercises(s);
+  }
+
+  @Test
+  public void testExport4() {
+    JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
+    Collection<CommonExercise> exercises = database.getExercises();
+    List<CommonExercise> copy = new ArrayList<>(exercises);
+    List<CommonExercise> exercises1 = copy.subList(0, 10);
+
+    JSONObject jsonObject = new JSONObject();
+    jsonExport.addJSONExerciseExport(jsonObject, exercises1);
+    logger.info("got " + jsonObject);
+    String text = jsonObject.toString();
+
+    try {
+      String pathname = "spanishSmall.json";
+      File file = new File(pathname);
+      logger.debug("writing to  " + file.getAbsolutePath());
+      try (PrintWriter out = new PrintWriter(file)) {
+        out.println(text);
+        out.flush();
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  @Test
+  public void testExportSmall() {
+    JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
+    Collection<CommonExercise> exercises = database.getExercises();
+    List<CommonExercise> copy = new ArrayList<>(exercises);
+    List<CommonExercise> exercises1 = copy;//.subList(0, 4000);
+
+    JSONObject jsonObject = new JSONObject();
+    jsonExport.addJSONExerciseExport(jsonObject, exercises1);
+    logger.info("got " + jsonObject);
+    String text = jsonObject.toString();
+
+    String pathname = "spanish.json";
+    File file = new File(pathname);
+    logger.debug("writing to  " + file.getAbsolutePath());
+
+    writeToFile(text, file);
+  }
+
+  void writeToFile(String text, File file) {
+    try {
+
+      String[] split = text.split("\\},\\{");
+      try (PrintWriter out = new PrintWriter(file)) {
+        for (int i = 0; i < split.length;i++) {
+          String s = split[i];
+          out.print(s);
+
+          if (i < split.length-1) {
+            out.println("},");
+            out.print("{");
+          }
+        }
+        out.flush();
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testExport6() {
+    JsonExport jsonExport = new JsonExport(null, database.getSectionHelper(), null);
+    Collection<CommonExercise> exercises = database.getExercises();
+    //List<CommonExercise> copy = new ArrayList<>(exercises);
+    //List<CommonExercise> exercises1 = copy.subList(0, 10);
+
+    JSONObject jsonObject = new JSONObject();
+    jsonExport.addJSONExerciseExport(jsonObject, exercises);
+    logger.info("got " + jsonObject);
+    String text = jsonObject.toString();
+    String[] split = text.split("\\},\\{");
+
+    try {
+      String pathname = "spanish2.json";
+      File file = new File(pathname);
+      logger.debug("writing to  " + file.getAbsolutePath());
+      try (PrintWriter out = new PrintWriter(file)) {
+        out.println(text);
+        out.flush();
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+  @Test
+  public void testExport5() {
+
+    Collection<CommonExercise> exercises = database.getExercises();
+    logger.info("Got " + exercises.size());
+  }
+
+  @Test
+  public void testImport() {
+    String file = "spanish.json";
+    JSONExerciseDAO exerciseDAO = new JSONExerciseDAO(file, database.getServerProps(), database.getUserListManager(), true);
+
+    database.setDependencies("bestAudio", ".", exerciseDAO);
+
+    List<CommonExercise> rawExercises = exerciseDAO.getRawExercises();
+
+    logger.info("Got " + rawExercises.size());
+
+    //for (CommonExercise exercise : rawExercises) logger.debug(exercise);
   }
 }
