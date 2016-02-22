f41ca46dfe54e1ba0658197d55a87d8e9d2bf40b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-22 15:24:11 -0500

    commented out console logging for now since IE 9 gets upset, minor fix in drop ref result table
    Former-commit-id: eef1bd666fdae4db7e0bd6d1e8daeb66f0f3d28b [formerly cd1697d89220272ba40455cb7aa70bc0ce0a96fa] [formerly 3d5c73a20406d3255429cf733d4555a1bfb8d912 [formerly f70b8892b2e3dadc7a8224722f32dcc7500e8009]] [formerly 0cd4af2c6863d531858fbaf8305a147c6b00b3cb]
    Former-commit-id: a2e5fb19f0037d53e965097f57f967b62aece669
    Former-commit-id: a8212ac73655d6ca8d356eb8e3914426b210bf2c [formerly 1117edba5a944c47188dba4eaa949a648618d801]
    Former-commit-id: a2382ed532cc5ed84b69c6d8c066acdb5ac2310d

---
+++ test/mitll/langtest/server/database/custom/AnalysisAgainTest.java
@@ -0,0 +1,56 @@
+package mitll.langtest.server.database.custom;
+
+import mitll.langtest.server.PathHelper;
+import mitll.langtest.server.ServerProperties;
+import mitll.langtest.server.database.DatabaseImpl;
+import mitll.langtest.shared.analysis.UserPerformance;
+import mitll.langtest.shared.analysis.WordScore;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by go22670 on 2/17/16.
+ */
+public class AnalysisAgainTest {
+  private static final Logger logger = Logger.getLogger(AnalysisAgainTest.class);
+  private static DatabaseImpl database;
+
+  @BeforeClass
+  public static void setup() {
+    getDatabase("sudanese");
+  }
+
+  private static void getDatabase(String config) {
+    File file = new File("war" + File.separator + "config" + File.separator + config + File.separator + "quizlet.properties");
+    String parent = file.getParent();
+    logger.debug("config dir " + parent);
+    logger.debug("config     " + file.getName());
+    //  dbName = "npfEnglish";//"mandarin";// "mandarin";
+    ServerProperties serverProps = new ServerProperties(parent, file.getName());
+    String dbName = serverProps.getH2Database();
+    database = new DatabaseImpl(parent, file.getName(), dbName, serverProps, new PathHelper("war"), false, null);
+    logger.debug("made " + database);
+    database.setInstallPath("war", parent + File.separator + database.getServerProps().getLessonPlan(), serverProps.getMediaDir());
+    database.getExercises();
+  }
+
+  @Test
+  public void testMe() {
+    database.getSectionHelper();
+
+    UserPerformance performance = database.getAnalysis().getPerformanceForUser(1, 1);
+    logger.info("got " + performance);
+
+    List<WordScore> wordScoresForUser = database.getAnalysis().getWordScoresForUser(1, 1);
+    for (WordScore ws : wordScoresForUser) {
+      //if (ws.getId().equals("50246"))
+      logger.info("ws " +ws.getId() + " " + ws.getNativeAudio() + " " + ws.getFileRef());
+    }
+
+  }
+
+}
