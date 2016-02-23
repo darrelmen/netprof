bb2b3abfea6309fbcf82720ec98dd9e9cfd45029
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-22 19:24:15 -0500

    added getamasexerciseids
    Former-commit-id: 5ab93600fb17cb40d43148720d549d1873463938 [formerly a75acde664b909bc761ecd1b171d1b001ecde916] [formerly 0fed6abffe3cf72c03607c31b95b3264271ba396]
    Former-commit-id: 284ce9b17e238db13288ee9a8a099366ec371b11 [formerly c2d72c4cff3f62cbe2ce6330b584606a3a132453] [formerly 5e7d9be434f130d7d74acdf4d5493689123f22e8]
    Former-commit-id: 58fa3ae1bf5245be3a807ecc6db542c5dd9bd7c8
    Former-commit-id: 40023a102e1314794fa83a9d7f826326ced08b37

--- src/mitll/langtest/client/custom/dialog/EditItem.java
+++ src/mitll/langtest/client/custom/dialog/EditItem.java
@@ -173,7 +173,7 @@ public class EditItem {
           }
 
           @Override
-          public List<CommonShell> rememberExercises(List<CommonShell> result) {
+          public Collection<CommonShell> rememberExercises(Collection<CommonShell> result) {
             clear();
             boolean addNewItem = includeAddItem;
 
