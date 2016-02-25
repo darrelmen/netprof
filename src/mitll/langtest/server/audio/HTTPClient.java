62020fb22cb8e918b5c5ce3516932f74382d20d9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-25 17:29:05 -0500

    initially got it working to get amasMSA from domino
    Former-commit-id: 1ebe415e3e3e8071cfae4070eb983bbbf2eaf5c6 [formerly 02b81c4d8298fef1e18ae3654051216f6fb35ce8] [formerly 9b32e8f02ef58ffc2e143aee794b5669b0e067ce]
    Former-commit-id: a985b7e63f9c03b1b5c19b8c9d72cc43553a793f [formerly 40b6d98edb52e75e8f9f2abe28a42386c3fa0dfc]
    Former-commit-id: b479f77388d9b7445c6410312b297b8710c735f0

--- src/mitll/langtest/server/audio/HTTPClient.java
+++ src/mitll/langtest/server/audio/HTTPClient.java
@@ -4,6 +4,7 @@
 
 package mitll.langtest.server.audio;
 
+import mitll.langtest.shared.amas.QAPair;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,181 +15,187 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CodingErrorAction;
+import java.util.Collection;
 
 // closes and reopens the connection after every call to sendAndReceive()
 public class HTTPClient {
-	private static final Logger logger = Logger.getLogger(HTTPClient.class);
+  private static final Logger logger = Logger.getLogger(HTTPClient.class);
 
-	private HttpURLConnection httpConn;
-	private BufferedReader receiver;
+  private HttpURLConnection httpConn;
+  //private BufferedReader receiver;
 
-	/* Constructor */
   public HTTPClient() {}
 
-	public HTTPClient(String url) {  connect(url);  }
+  /**
+   * @see mitll.langtest.server.autocrt.MiraClassifier#getMiraScore(int, String, String, String, Collection, String, QAPair)
+   * @param url
+   */
+  public HTTPClient(String url) {
+    try {
+      httpConn = setupPostHttpConn(url);
+    } catch (IOException e) {
+      logger.error("Error constructing HTTPClient:\n" + e, e);
+    }
+  }
 
-	public HTTPClient(String webserviceIP, boolean secure) {
-		String protocol = "http" + (secure ? "s" : "");
-		String url = protocol + "://" + webserviceIP;
-		connect(url);
-	}
+  /**
+   * @see mitll.langtest.server.autocrt.MiraClassifier#getMiraScore(int, String, String, String, Collection, String, QAPair)
+   * @param webserviceIP
+   */
+  public HTTPClient(String webserviceIP, boolean secure) {
+    this("http" + (secure ? "s" : "") + "://" + webserviceIP);
+  }
 
-  public HTTPClient(String webserviceIP, int webservicePort, boolean secure) {
-		String protocol = "http" + (secure ? "s" : "");
+/*  public HTTPClient(String webserviceIP, int webservicePort, boolean secure) {
+    String protocol = "http" + (secure ? "s" : "");
 		String url = protocol + "://" + webserviceIP + ":" + webservicePort;
 		connect(url);
-	}
+	}*/
 
-	/**
-	 * @param webserviceIP
-	 * @param webservicePort
-	 * @see mitll.langtest.server.scoring.ASRWebserviceScoring#runHydra
-	 */
-	public HTTPClient(String webserviceIP, int webservicePort, String service) {
-		String url = "http://" + webserviceIP + ":" + webservicePort + "/" + service;
-		connect(url);
-	}
+  /**
+   * @param webserviceIP
+   * @param webservicePort
+   * @see mitll.langtest.server.scoring.ASRWebserviceScoring#runHydra
+   */
+  public HTTPClient(String webserviceIP, int webservicePort, String service) {
+    this("http://" + webserviceIP + ":" + webservicePort + "/" + service);
+  }
 
-	private void connect(String url) {
-		try {
-			httpConn = setupHttpConn(url);
-		} catch (IOException e) {
-			logger.error("Error constructing HTTPClient:\n" + e, e);
-		}
-	}
+  /**
+   * @param webserviceIP
+   * @param webservicePort
+   * @see mitll.langtest.server.database.UserManagement#userExists
+   */
+/*
+  public HTTPClient(String webserviceIP, int webservicePort) {
+    this("http://" + webserviceIP + ":" + webservicePort);
+  }
+*/
 
-	/**
-	 * @see mitll.langtest.server.database.UserManagement#userExists(HttpServletRequest, String, String)
-	 * @param webserviceIP
-	 * @param webservicePort
-	 */
-	public HTTPClient(String webserviceIP, int webservicePort) {
-		String url = "http://" + webserviceIP + ":" + webservicePort;
+/*  private void connect(String url) {
+    try {
+      httpConn = setupPostHttpConn(url);
+    } catch (IOException e) {
+      logger.error("Error constructing HTTPClient:\n" + e, e);
+    }
+  }*/
 
-		logger.debug("connect to " + url);
-		try {
-			httpConn = setupHttpConn(url);
-		} catch (IOException e) {
-			logger.error("Error constructing HTTPClient:\n" + e, e);
-		}
-	}
+  /**
+   * @param url
+   * @return
+   * @see mitll.langtest.server.database.DatabaseImpl#userExists(HttpServletRequest, String, String)
+   */
+  public String readFromGET(String url) {
+    try {
+      return receive(setupGetHttpConn(url));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
 
-	/**
-	 * @param url
-	 * @return
-	 * @see mitll.langtest.server.database.DatabaseImpl#userExists(HttpServletRequest, String, String)
-	 */
-	public String readFromGET(String url) {
-		try {
-			return receive(setupGetHttpConn(url));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
+  private HttpURLConnection setupPostHttpConn(String url) throws IOException {
+    HttpURLConnection httpConn = (HttpURLConnection) (new URL(url)).openConnection();
+    httpConn.setRequestMethod("POST");
+    httpConn.setDoOutput(true);
+    httpConn.setConnectTimeout(5000);
+    httpConn.setReadTimeout(20000);
+    //httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+    setRequestProperties(httpConn);
+    return httpConn;
+  }
 
-	private HttpURLConnection setupHttpConn(String url) throws IOException {
-		HttpURLConnection httpConn = (HttpURLConnection) (new URL(url)).openConnection();
-		httpConn.setRequestMethod("POST");
-		httpConn.setDoOutput(true);
-		httpConn.setConnectTimeout(5000);
-		httpConn.setReadTimeout(20000);
-		//httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-		httpConn.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
-		httpConn.setRequestProperty("Accept-Charset", "UTF8");
-		httpConn.setRequestProperty("charset", "UTF8");
-		return httpConn;
-	}
+  private HttpURLConnection setupGetHttpConn(String url) throws IOException {
+    HttpURLConnection httpConn = (HttpURLConnection) (new URL(url)).openConnection();
+    httpConn.setRequestMethod("GET");
+    httpConn.setConnectTimeout(1000);
+    //httpConn.setReadTimeout(20000);
+    //httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+    setRequestProperties(httpConn);
+    return httpConn;
+  }
 
-	private HttpURLConnection setupGetHttpConn(String url) throws IOException {
-		HttpURLConnection httpConn = (HttpURLConnection) (new URL(url)).openConnection();
-		httpConn.setRequestMethod("GET");
-		httpConn.setConnectTimeout(1000);
-		//httpConn.setReadTimeout(20000);
-		//httpConn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-		httpConn.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
-		httpConn.setRequestProperty("Accept-Charset", "UTF8");
-		httpConn.setRequestProperty("charset", "UTF8");
-		return httpConn;
-	}
+  private void setRequestProperties(HttpURLConnection httpConn) {
+    httpConn.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
+    httpConn.setRequestProperty("Accept-Charset", "UTF8");
+    httpConn.setRequestProperty("charset", "UTF8");
+  }
 
 /*
-	private String read(HttpURLConnection conn) {
+  private String read(HttpURLConnection conn) {
 		return receive(conn);
 	}
 */
 
-	private void closeConn() {
-		httpConn.disconnect();
-		httpConn = null;
-	}
+  private void closeConn() {
+    httpConn.disconnect();
+    //httpConn = null;
+  }
 
-	private void send(String input) throws IOException {
-		logger.debug("SEND INPUT: " + input);
+  private void send(String input) throws IOException {
+    logger.debug("SEND INPUT: " + input);
 
-		OutputStream outputStream = httpConn.getOutputStream();
+    OutputStream outputStream = httpConn.getOutputStream();
 
-		CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
-		encoder.onMalformedInput(CodingErrorAction.REPORT);
-		encoder.onUnmappableCharacter(CodingErrorAction.REPORT);
-		BufferedWriter sender = new BufferedWriter(new OutputStreamWriter(outputStream, encoder));
-		sender.write(input);
-		sender.flush();
-		sender.close();
-	}
+    CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
+    encoder.onMalformedInput(CodingErrorAction.REPORT);
+    encoder.onUnmappableCharacter(CodingErrorAction.REPORT);
+    BufferedWriter sender = new BufferedWriter(new OutputStreamWriter(outputStream, encoder));
+    sender.write(input);
+    sender.flush();
+    sender.close();
+  }
 
-	private String receive() throws IOException {
-		HttpURLConnection httpConn = this.httpConn;
-		receiver = getReader(httpConn);
-		return receive(httpConn, receiver);
-	}
+  public String receive() throws IOException {
+    return receive(this.httpConn, getReader(this.httpConn));
+  }
 
-	private String receive(HttpURLConnection httpConn) {
-		try {
-			return receive(httpConn, getReader(httpConn));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
+  private String receive(HttpURLConnection httpConn) {
+    try {
+      return receive(httpConn, getReader(httpConn));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
 
-	private String receive(HttpURLConnection httpConn, BufferedReader reader) throws IOException {
-		int code = httpConn.getResponseCode();
-		if (code == 200) {
-			String output = "";
-			String current = "";
-			while ((current = reader.readLine()) != null) {
-				output += current + "\n";
-			}
-			reader.close();
-			receiver = null;
-			return output;
-		} else {
-			logger.error("Received HTTP Code of: " + code + ". Expecting code 200.");
-			return "";
-		}
-	}
+  private String receive(HttpURLConnection httpConn, BufferedReader reader) throws IOException {
+    int code = httpConn.getResponseCode();
+    if (code == 200) {
+      StringBuilder builder = new StringBuilder();
+      String current;
+      while ((current = reader.readLine()) != null) {
+        builder.append(current);
+        builder.append("\n");
+      }
+      reader.close();
+      return builder.toString();
+    } else {
+      logger.error("Reading " + httpConn.getURL() + " received HTTP Code of: " + code + ".");
+      return "";
+    }
+  }
 
-	private BufferedReader getReader(HttpURLConnection httpConn) throws IOException {
-		return new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF8"));
-	}
+  private BufferedReader getReader(HttpURLConnection httpConn) throws IOException {
+    return new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF8"));
+  }
 
-	private String sendAndReceive(String input) throws IOException {
-		try {
-			send(input);
-			return receive();
-		} catch (ConnectException ce) {
-			logger.error("sending " + input + " couldn't connect to server at  " + httpConn.getURL() + " got " + ce);
-			return "";
-		} catch (IOException e) {
-			logger.error("sending " + input + " got " + e, e);
-			throw e;
-		}
-	}
+  private String sendAndReceive(String input) throws IOException {
+    try {
+      send(input);
+      return receive();
+    } catch (ConnectException ce) {
+      logger.error("sending " + input + " couldn't connect to server at  " + httpConn.getURL() + " got " + ce);
+      return "";
+    } catch (IOException e) {
+      logger.error("sending " + input + " got " + e, e);
+      throw e;
+    }
+  }
 
-	public String sendAndReceiveAndClose(String input) throws IOException {
-		String s = sendAndReceive(input);
-		closeConn();
-		return s;
-	}
+  public String sendAndReceiveAndClose(String input) throws IOException {
+    String s = sendAndReceive(input);
+    closeConn();
+    return s;
+  }
 }
