f33db20cbcda017d192900dae7d34b6cea0b4bad
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-25 11:10:38 -0500

    steps towards cleaning up writeAudioFile and addAnswer pathway
    Former-commit-id: 5b17a02980bffda35ed942300366cb1fda62c40c [formerly 00cf40807b14c5d3ab18b90be0f38c0e2bba961b [formerly 493b4af415d5fa90bef7d72519682b3cad8f71f2] [formerly 8d32abc0ef3b16acc4fa96837c130f1044ceda51 [formerly 63f15f3f0f82847d7fd7d4364458e07ecb0f456d]]] [formerly 00cf40807b14c5d3ab18b90be0f38c0e2bba961b [formerly 493b4af415d5fa90bef7d72519682b3cad8f71f2] [formerly 8d32abc0ef3b16acc4fa96837c130f1044ceda51 [formerly 63f15f3f0f82847d7fd7d4364458e07ecb0f456d]] [formerly 00cf40807b14c5d3ab18b90be0f38c0e2bba961b [formerly 493b4af415d5fa90bef7d72519682b3cad8f71f2] [formerly 8d32abc0ef3b16acc4fa96837c130f1044ceda51 [formerly 63f15f3f0f82847d7fd7d4364458e07ecb0f456d]] [formerly 19ee4674c2a50403c479ef170d9015e5773674b1]]]
    Former-commit-id: 8464df224664f5c479440ca950a30c151442359f [formerly f83dcfa239e944f6f5df45b4b291e39588f3fb00] [formerly a3cc03998cba595ac0fd8f570799b9bac507bbf3 [formerly 06bb32cfa4bb66633b56d62caf1dd9bb343c01c4]] [formerly 8464df224664f5c479440ca950a30c151442359f [formerly f83dcfa239e944f6f5df45b4b291e39588f3fb00] [formerly a3cc03998cba595ac0fd8f570799b9bac507bbf3 [formerly 06bb32cfa4bb66633b56d62caf1dd9bb343c01c4]] [formerly 1e691b2c5e3bddaa7f67474caf7490eba0629851]] [formerly 1b9835cc6ee2b2eaebbfb82d3f76b19d3d5f23b3 [formerly 6985be4c612d1bfc81dfb32fafa097e822bc44ff] [formerly c5a9bcbaeaedc74cf53c90e778a4f6c0569d17a1 [formerly 1854c8b7a7b44907ff18fc5e65e0c9f453f0c1c0]] [formerly 1b9835cc6ee2b2eaebbfb82d3f76b19d3d5f23b3 [formerly 6985be4c612d1bfc81dfb32fafa097e822bc44ff] [formerly c5a9bcbaeaedc74cf53c90e778a4f6c0569d17a1 [formerly 1854c8b7a7b44907ff18fc5e65e0c9f453f0c1c0]] [formerly 25c8e1d249afdc4c34bd7ea4b64c558ec78044f9]]]
    Former-commit-id: 38a19a778e94d3cb18837337715a44d06995c41e [formerly 1b9835cc6ee2b2eaebbfb82d3f76b19d3d5f23b3 [formerly 6985be4c612d1bfc81dfb32fafa097e822bc44ff] [formerly c5a9bcbaeaedc74cf53c90e778a4f6c0569d17a1 [formerly 1854c8b7a7b44907ff18fc5e65e0c9f453f0c1c0]]]
    Former-commit-id: 477ccf1dbb93f5b84e012e203b468e7721251a4d [formerly 6c6afca2be4227cc4f989acd72c77da1bcb18a31] [formerly 9976e7dadbb15969ba292a398328330dbb159727 [formerly 4f1398f75914739aaddb77c641505adc5e47dc35]] [formerly ac602e89a360622e16a91bb0e62f68b3cff6c0e6]
    Former-commit-id: 8526c023e31d57518f1647fb7b15224a620ad6fa
    Former-commit-id: 120f536464db32d0d3519a6e481c5ea3e6b752b2 [formerly 84c4981de90bd16dbb511f554bb45792a5770a2e]
    Former-commit-id: bbde01b8b86ff437d34d7e9835a835f10f876fa5

--- src/mitll/langtest/server/audio/AudioCheck.java
+++ src/mitll/langtest/server/audio/AudioCheck.java
@@ -4,7 +4,6 @@
 
 package mitll.langtest.server.audio;
 
-import com.google.common.io.Files;
 import mitll.langtest.server.ServerProperties;
 import mitll.langtest.shared.AudioAnswer;
 import org.apache.commons.io.FileUtils;
@@ -20,7 +19,7 @@ import java.io.IOException;
  * Checks for two things -- is the audio long enough ({@link #MinRecordLength} and is
  * it just a recording of silence by looking at average power and variance :
  * ({@link #PowerThreshold} and ({@link #VarianceThreshold}.
- *
+ * <p>
  * User: GO22670
  * Date: 5/16/12
  * Time: 2:35 PM
@@ -36,18 +35,19 @@ public class AudioCheck {
   private static final double CLIPPED_RATIO = 0.005; // 1/2 %
   private static final double LOG_OF_TEN = Math.log(10.0);
 
-  private static final short clippedThreshold  = 32704; // 32768-64
-  private static final short clippedThresholdMinus  = -32704; // 32768-64
+  private static final short clippedThreshold = 32704; // 32768-64
+  private static final short clippedThresholdMinus = -32704; // 32768-64
   private static final short ct = 32112;
- // private static final short clippedThreshold2 = 32752; // 32768-16
- // private static final short clippedThreshold2Minus = -32752; // 32768-16
+  // private static final short clippedThreshold2 = 32752; // 32768-16
+  // private static final short clippedThreshold2Minus = -32752; // 32768-16
   private static final float MAX_VALUE = 32768.0f;
+  public static final ValidityAndDur INVALID_AUDIO = new ValidityAndDur();
   private final int MIN_DYNAMIC_RANGE;
-  ServerProperties props;
+  private final ServerProperties props;
 
   public AudioCheck(ServerProperties props) {
     this.props = props;
-    this.MIN_DYNAMIC_RANGE = props == null ? 26: props.getMinDynamicRange();
+    this.MIN_DYNAMIC_RANGE = props == null ? 26 : props.getMinDynamicRange();
   }
 
   private double dB(double power) {
@@ -55,16 +55,18 @@ public class AudioCheck {
   }
 
   /**
-   * @see mitll.langtest.server.LangTestDatabaseImpl#getImageForAudioFile
    * @param file
    * @return
+   * @see mitll.langtest.server.LangTestDatabaseImpl#getImageForAudioFile
    */
-  public double getDurationInSeconds(String file) { return getDurationInSeconds(new File(file)); }
+  public double getDurationInSeconds(String file) {
+    return getDurationInSeconds(new File(file));
+  }
 
   /**
-   * @see mitll.langtest.server.scoring.ASRScoring#scoreRepeatExercise
    * @param file
    * @return
+   * @see mitll.langtest.server.scoring.ASRScoring#scoreRepeatExercise
    */
   public double getDurationInSeconds(File file) {
     AudioInputStream audioInputStream = null;
@@ -77,9 +79,9 @@ public class AudioCheck {
       try {
         if (audioInputStream != null) audioInputStream.close();
       } catch (IOException e1) {
-        logger.error("got " +e,e);
+        logger.error("got " + e, e);
       }
-      logger.error("got " +e,e);
+      logger.error("got " + e, e);
     }
     return 0d;
   }
@@ -90,20 +92,21 @@ public class AudioCheck {
     return (frames + 0.0d) / format.getFrameRate();
   }
 
-  public ValidityAndDur checkWavFileRejectAnyTooLoud(File file) {
-    ValidityAndDur validityAndDur = checkWavFileWithClipThreshold(file, false);
-    addDynamicRange(file, /*isBrowser,*/ validityAndDur);
+  public ValidityAndDur checkWavFileRejectAnyTooLoud(File file, boolean quietAudioOK) {
+    ValidityAndDur validityAndDur = checkWavFileWithClipThreshold(file, false, quietAudioOK);
+    addDynamicRange(file, validityAndDur);
     return validityAndDur;
   }
 
   /**
    * TODO : consider passing in more isBrowser
-   * @see AudioConversion#isValid
+   *
    * @param file
    * @return
+   * @see AudioConversion#isValid
    */
-  public ValidityAndDur checkWavFile(File file) {
-    ValidityAndDur validityAndDur = checkWavFileWithClipThreshold(file, true);
+  public ValidityAndDur checkWavFile(File file, boolean quietAudioOK) {
+    ValidityAndDur validityAndDur = checkWavFileWithClipThreshold(file, true, quietAudioOK);
     addDynamicRange(file, validityAndDur);
     return validityAndDur;
   }
@@ -114,7 +117,7 @@ public class AudioCheck {
     DynamicRange.RMSInfo dynamicRange = new DynamicRange().getDynamicRange(highPass);
     deleteParentTempDir(highPass);
     if (dynamicRange.maxMin < MIN_DYNAMIC_RANGE) {
-      logger.warn("file " + file.getName() + " doesn't meet dynamic range threshold (" + MIN_DYNAMIC_RANGE+
+      logger.warn("file " + file.getName() + " doesn't meet dynamic range threshold (" + MIN_DYNAMIC_RANGE +
           "):\n" + dynamicRange);
       validityAndDur.validity = AudioAnswer.Validity.SNR_TOO_LOW;
     }
@@ -125,7 +128,7 @@ public class AudioCheck {
     try {
       FileUtils.deleteDirectory(new File(srcFile.getParent()));
     } catch (IOException e) {
-      logger.error("on " +srcFile+ " got " +e,e);
+      logger.error("on " + srcFile + " got " + e, e);
     }
   }
 
@@ -133,11 +136,11 @@ public class AudioCheck {
   /**
    * Verify audio messages
    *
-   * @see AudioConversion#isValid(File, boolean)
    * @param file audio byte array with header
    * @return true if well formed
+   * @see AudioConversion#isValid(File, boolean, boolean)
    */
-  private ValidityAndDur checkWavFileWithClipThreshold(File file, boolean usePercent){
+  private ValidityAndDur checkWavFileWithClipThreshold(File file, boolean usePercent, boolean quietAudioOK) {
     AudioInputStream ais = null;
     try {
       ais = AudioSystem.getAudioInputStream(file);
@@ -149,14 +152,14 @@ public class AudioCheck {
         logger.warn("huh? file " + file.getAbsoluteFile() + " is in big endian format?");
       }
       int fsize = format.getFrameSize();
-      assert(fsize == 2);
-      assert(format.getChannels() == 1);
+      assert (fsize == 2);
+      assert (format.getChannels() == 1);
       double dur = getDurationInSeconds(ais);
 
       long frameLength = ais.getFrameLength();
       if (frameLength < MinRecordLength) {
-        logger.debug("INFO: audio recording too short (Length: " + frameLength + ") < min (" +MinRecordLength+ ") ");
-        return new ValidityAndDur(AudioAnswer.Validity.TOO_SHORT, dur);
+        logger.debug("INFO: audio recording too short (Length: " + frameLength + ") < min (" + MinRecordLength + ") ");
+        return new ValidityAndDur(AudioAnswer.Validity.TOO_SHORT, dur, false);
       }
 
       // Verify audio power
@@ -189,77 +192,93 @@ public class AudioCheck {
           }
 
         fpower /= (float) WinSize;
-        fpower  = (float) dB((double) fpower);
+        fpower = (float) dB((double) fpower);
 
         pm += fpower;
         p2 += fpower * fpower;
-        n  += (float) fsize / 2.0f;
+        n += (float) fsize / 2.0f;
       }
 
-      float clippedRatio = ((float)countClipped) / (float)frameLength;
-      float clippedRatio2 = ((float)cc) / (float)frameLength;
+      float clippedRatio = ((float) countClipped) / (float) frameLength;
+      float clippedRatio2 = ((float) cc) / (float) frameLength;
       boolean wasClipped = usePercent ? clippedRatio > CLIPPED_RATIO : countClipped > 1;
       boolean wasClipped2 = usePercent ? clippedRatio2 > CLIPPED_RATIO : cc > 1;
 /*      logger.info("of " + total +" got " +countClipped + " out of " + n +"  or " + clippedRatio  + "/" +clippedRatio2+
         " not " + notClippedRatio +" wasClipped = " + wasClipped);*/
 
       float mean = pm / n;
-      float var  = p2 / n - mean * mean;
+      float var = p2 / n - mean * mean;
       double std = Math.sqrt(var);
       final boolean validAudio = mean > PowerThreshold || std > VarianceThreshold;
 
       if (wasClipped || !validAudio) {
         logger.info("checkWavFile: audio recording (Length: " + frameLength + ") " +
-          "mean power = " + mean + " (dB) vs " + PowerThreshold +
-          ", std = " + std + " vs " + VarianceThreshold+
-          " valid = " + validAudio +
-                " was clipped " + wasClipped + " (" + (clippedRatio * 100f) + "% samples clipped, # clipped = " + countClipped+ ") " +
-            " was clipped " + wasClipped2 + " (" + (clippedRatio2 * 100f) + "% samples clipped, # clipped = " + cc+ ")" +
-                " max = " + max + "/" +nmax
+            "mean power = " + mean + " (dB) vs " + PowerThreshold +
+            ", std = " + std + " vs " + VarianceThreshold +
+            " valid = " + validAudio +
+            " was clipped " + wasClipped + " (" + (clippedRatio * 100f) + "% samples clipped, # clipped = " + countClipped + ") " +
+            " was clipped " + wasClipped2 + " (" + (clippedRatio2 * 100f) + "% samples clipped, # clipped = " + cc + ")" +
+            " max = " + max + "/" + nmax
         );
       }
 
       boolean micDisconnected = mean < -79.999 && std < 0.001;
 
       AudioAnswer.Validity validity = validAudio ?
-        (wasClipped ?
-          AudioAnswer.Validity.TOO_LOUD :
-          AudioAnswer.Validity.OK) :
-        micDisconnected ?
-          AudioAnswer.Validity.MIC_DISCONNECTED :
-          AudioAnswer.Validity.TOO_QUIET;
+          (wasClipped ?
+              AudioAnswer.Validity.TOO_LOUD :
+              AudioAnswer.Validity.OK) :
+          micDisconnected ?
+              AudioAnswer.Validity.MIC_DISCONNECTED :
+              AudioAnswer.Validity.TOO_QUIET;
 
-      ValidityAndDur validityAndDur = new ValidityAndDur(validity, dur);
+      ValidityAndDur validityAndDur = new ValidityAndDur(validity, dur, quietAudioOK);
 
       //if (validityAndDur.validity != AudioAnswer.Validity.OK) {
-        //logger.info("validity " + validityAndDur);
+      //logger.info("validity " + validityAndDur);
       //}
       return validityAndDur;
     } catch (Exception e) {
-      logger.error("Got " +e,e);
+      logger.error("Got " + e, e);
     } finally {
       try {
         if (ais != null) ais.close();
       } catch (IOException e) {
-       logger.error("Got " +e,e);
+        logger.error("Got " + e, e);
       }
     }
 
-    return new ValidityAndDur(AudioAnswer.Validity.INVALID);
+    return INVALID_AUDIO;
   }
 
   public static class ValidityAndDur {
     private AudioAnswer.Validity validity;
+    private boolean isValid = false;
     public int durationInMillis;
     private double maxMinRange;
 
-    public ValidityAndDur(AudioAnswer.Validity validity) {
-      this(validity, 0d);
+    ValidityAndDur() {
+      this(AudioAnswer.Validity.INVALID, 0d, false);
     }
 
-    public ValidityAndDur(AudioAnswer.Validity validity, double dur) {
+    public ValidityAndDur(double dur) {
+      this(AudioAnswer.Validity.OK, dur, false);
+    }
+
+    /**
+     * @param validity
+     * @param dur
+     */
+    public ValidityAndDur(AudioAnswer.Validity validity, double dur, boolean quietAudioOK) {
       this.validity = validity;
       this.durationInMillis = (int) (1000d * dur);
+      isValid = validity ==
+          AudioAnswer.Validity.OK ||
+          (validity == AudioAnswer.Validity.TOO_QUIET && quietAudioOK);
+    }
+
+    public boolean isValid() {
+      return isValid; // == AudioAnswer.Validity.OK;
     }
 
     public AudioAnswer.Validity getValidity() {
@@ -270,14 +289,20 @@ public class AudioCheck {
       return maxMinRange;
     }
 
-    public void setMaxMinRange(double maxMinRange) {
+    void setMaxMinRange(double maxMinRange) {
       this.maxMinRange = maxMinRange;
     }
 
-    public String toString() { return "valid " + getValidity() + " dur " + durationInMillis + " max min " + maxMinRange; }
-
+    /**
+     * @param duration
+     * @see AudioConversion#convertBase64ToAudioFiles
+     */
     public void setDuration(double duration) {
       this.durationInMillis = (int) (1000d * duration);
+    }
+
+    public String toString() {
+      return "valid " + getValidity() + " dur " + durationInMillis + " max min " + maxMinRange;
     }
   }
 }
