ddaa0e924830180a0e2a15a6fd3db6d598497d4b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-25 11:10:38 -0500

    steps towards cleaning up writeAudioFile and addAnswer pathway
    Former-commit-id: 5b17a02980bffda35ed942300366cb1fda62c40c [formerly b792ad1a7981b40a302933d24f37bf32f0a163e5 [formerly 77ebbdcaf4604543039a13d06cf34745d34ec375]] [formerly b792ad1a7981b40a302933d24f37bf32f0a163e5 [formerly 77ebbdcaf4604543039a13d06cf34745d34ec375] [formerly b792ad1a7981b40a302933d24f37bf32f0a163e5 [formerly 77ebbdcaf4604543039a13d06cf34745d34ec375] [formerly 23183712c81841d794de064932a96e57019e61e4 [formerly 5d62dd214aaf771c048d622e53d71d7586c92c68]]]]
    Former-commit-id: 53b345226fc80694facfb1c3eb279e6a53bccd77 [formerly 56cd95488eceacc5c48b374fa9314b8dd3530da0] [formerly 53b345226fc80694facfb1c3eb279e6a53bccd77 [formerly 56cd95488eceacc5c48b374fa9314b8dd3530da0] [formerly 77c7f961727d475183e10a5592b0ac75973c6384 [formerly 1a1063b691ff01bc1131bd47cd2f41336422fad0]]] [formerly 503e13ee98c14c7e1516bcdc2883212032b9ae38 [formerly fc4169be31b472cfe82047abce4a30de2c283da5] [formerly 503e13ee98c14c7e1516bcdc2883212032b9ae38 [formerly fc4169be31b472cfe82047abce4a30de2c283da5] [formerly 187e75658cfe3ed509059a6d8a88de60b85572fc [formerly d277a477ff9eb1751b5fd691b020317a06a72b0b]]]]
    Former-commit-id: 38a19a778e94d3cb18837337715a44d06995c41e [formerly 503e13ee98c14c7e1516bcdc2883212032b9ae38 [formerly fc4169be31b472cfe82047abce4a30de2c283da5]]
    Former-commit-id: 3854d7cb510da93971219b79be20d1676c8a1191 [formerly 6b11702e41228a3503ff8d63efa96c0090151155] [formerly da930435d616cfc78bce3c035bac466322889986 [formerly 8c77fc4f6ac7909d3b51904fc2f79902264aba6b]]
    Former-commit-id: 7e207e7c0729c08e644af27882b1c55c64ac3c2a [formerly fb95b52c06986a46123eb87d6e858167c39013f2]
    Former-commit-id: dfd4cc60d65fa0e76636a77418ece2f78a1db6db

---
+++ src/mitll/langtest/server/database/AnswerInfo.java
@@ -0,0 +1,289 @@
+package mitll.langtest.server.database;
+
+import mitll.langtest.server.audio.AudioCheck;
+
+/**
+ * Created by go22670 on 2/24/16.
+ */
+public class AnswerInfo {
+  private int userid;
+  private String id;
+  private int questionID;
+  private String answer;
+  private String audioFile;
+  private boolean valid;
+  private String audioType;
+  private long durationInMillis;
+  private boolean correct;
+  private float pronScore;
+  private String deviceType;
+  private String device;
+  private String scoreJson;
+  private boolean withFlash;
+  private int processDur;
+  private int roundTripDur;
+  private String validity;
+  private double snr;
+
+
+  public static class AudioContext {
+    private int userid;
+    private String id;
+    private int questionID;
+    String audioType;
+
+    public AudioContext(int userid,
+                        String id,
+                        int questionID,
+                        String audioType) {
+      this.userid = userid;
+      this.id = id;
+      this.questionID = questionID;
+      this.audioType = audioType;
+    }
+
+    public int getUserid() {
+      return userid;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public int getQuestionID() {
+      return questionID;
+    }
+  }
+
+  public static class RecordingInfo {
+    String answer;
+    String audioFile;
+    String deviceType;
+    String device;
+    boolean withFlash;
+
+    public RecordingInfo(RecordingInfo other,String audioFile) {
+      this.answer = other.answer;
+      this.audioFile = audioFile;
+      this.deviceType = other.deviceType;
+      this.device = other.device;
+      this.withFlash = other.withFlash;
+    }
+
+    public RecordingInfo(String answer,
+                         String audioFile,
+                         String deviceType, String device,
+                         boolean withFlash) {
+      this.answer = answer;
+      this.audioFile = audioFile;
+      this.deviceType = deviceType;
+      this.device = device;
+      this.withFlash = withFlash;
+    }
+  }
+
+  public static class ScoreInfo {
+    boolean correct;
+    float pronScore;
+    String scoreJson;
+    int processDur;
+
+    public ScoreInfo(boolean correct,
+                     float pronScore,
+                     String scoreJson,
+                     int processDur) {
+      this.correct = correct;
+      this.pronScore = pronScore;
+      this.scoreJson = scoreJson;
+      this.processDur = processDur;
+    }
+  }
+
+  public AnswerInfo(AudioContext audioContext,
+                    RecordingInfo recordingInfo,
+                    //     boolean valid,
+                    AudioCheck.ValidityAndDur validity
+  ) {
+    this(audioContext.userid,
+        audioContext.id,
+        audioContext.questionID,
+        audioContext.audioType,
+
+        recordingInfo.answer,
+        recordingInfo.audioFile,
+        recordingInfo.deviceType,
+        recordingInfo.device,
+        recordingInfo.withFlash,
+
+        //    valid,
+        validity);
+  }
+
+  public AnswerInfo(int userid,
+                    String id,
+                    int questionID,
+                    String audioType,
+
+                    String answer,
+                    String audioFile,
+                    String deviceType, String device,
+                    boolean withFlash,
+
+                    //    boolean valid,
+                    AudioCheck.ValidityAndDur validity) {
+    this.userid = userid;
+    this.id = id;
+    this.questionID = questionID;
+    this.answer = answer;
+    this.audioFile = audioFile;
+    this.valid = validity.isValid();
+    this.audioType = audioType;
+    this.durationInMillis = validity.durationInMillis;
+    this.deviceType = deviceType;
+    this.device = device;
+    this.withFlash = withFlash;
+    this.validity = validity.getValidity().name();
+    this.roundTripDur = 0;//roundTripDur;
+    this.snr = validity.getMaxMinRange();
+  }
+
+  public AnswerInfo(AnswerInfo other,
+                    ScoreInfo scoreInfo) {
+    this(other, scoreInfo.correct, scoreInfo.pronScore, scoreInfo.scoreJson, scoreInfo.processDur);
+  }
+
+  public AnswerInfo(AnswerInfo other,
+                    boolean correct,
+                    float pronScore,
+                    String scoreJson,
+                    int processDur) {
+    this(other.getUserid(),
+        other.getId(),
+        other.getQuestionID(),
+        other.getAudioType(),
+        other.getAnswer(),
+        other.getAudioFile(),
+        other.getDeviceType(), other.getDevice(), other.isWithFlash(),
+
+        other.getDurationInMillis(),
+        other.isValid(),
+        other.getValidity(), other.getSnr(),
+
+        correct,
+        pronScore,
+        scoreJson,
+        processDur
+    );
+  }
+
+  public AnswerInfo(int userid, String id, int questionID,
+                    String audioType, String answer, String audioFile,
+                    String deviceType, String device, boolean withFlash,
+
+                    long durationInMillis,
+                    boolean valid,
+                    String validity,
+                    double snr,
+
+                    boolean correct, float pronScore, String scoreJson, int processDur
+                    //  int roundTripDur,
+  ) {
+    this.userid = userid;
+    this.id = id;
+    this.questionID = questionID;
+    this.answer = answer;
+    this.audioFile = audioFile;
+    this.valid = valid;
+    this.audioType = audioType;
+    this.durationInMillis = durationInMillis;
+    this.correct = correct;
+    this.pronScore = pronScore;
+    this.deviceType = deviceType;
+    this.device = device;
+    this.scoreJson = scoreJson;
+    this.withFlash = withFlash;
+    this.validity = validity;
+    this.processDur = processDur;
+    this.roundTripDur = 0;//roundTripDur;
+    this.snr = snr;
+  }
+
+  public int getUserid() {
+    return userid;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public int getQuestionID() {
+    return questionID;
+  }
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public String getAudioFile() {
+    return audioFile;
+  }
+
+  public boolean isValid() {
+    return valid;
+  }
+
+  public String getAudioType() {
+    return audioType;
+  }
+
+  public long getDurationInMillis() {
+    return durationInMillis;
+  }
+
+  public boolean isCorrect() {
+    return correct;
+  }
+
+  public float getPronScore() {
+    return pronScore;
+  }
+
+  public String getDeviceType() {
+    return deviceType;
+  }
+
+  public String getDevice() {
+    return device;
+  }
+
+  public String getScoreJson() {
+    return scoreJson;
+  }
+
+  public boolean isWithFlash() {
+    return withFlash;
+  }
+
+  public int getProcessDur() {
+    return processDur;
+  }
+
+  public int getRoundTripDur() {
+    return roundTripDur;
+  }
+
+  public String getValidity() {
+    return validity;
+  }
+
+  public double getSnr() {
+    return snr;
+  }
+
+  public String toString() {
+    return "answer for exid #" + id + " correct " + correct + " score " + pronScore +
+        " audio type " + audioType + " answer " + answer + " process " + processDur +
+        " validity " + validity + " snr " + snr +
+        " json " + scoreJson;
+  }
+}
