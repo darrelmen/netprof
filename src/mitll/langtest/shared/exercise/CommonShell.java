2364d1544755ecdce6e269ac148e759399037053
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-26 17:25:39 -0500

    simplify writeAudioFile pipeline call
    Former-commit-id: d5fcaec0222d45867f98df64009548241308afb9
    Former-commit-id: 5bd814982f19ce23eaa7977321c46904ffd8ed6d

--- src/mitll/langtest/shared/exercise/CommonShell.java
+++ src/mitll/langtest/shared/exercise/CommonShell.java
@@ -4,6 +4,8 @@
 
 package mitll.langtest.shared.exercise;
 
+import java.util.Collection;
+
 /**
  * Created by GO22670 on 3/21/2014.
  */
@@ -17,4 +19,10 @@ public interface CommonShell extends Shell {
   String getContextTranslation();
 
   MutableShell getMutableShell();
+
+  /**
+   * @see mitll.langtest.server.autocrt.DecodeCorrectnessChecker#getRefSentences(CommonExercise, String, boolean)
+   * @return
+   */
+  Collection<String> getRefSentences();
 }
