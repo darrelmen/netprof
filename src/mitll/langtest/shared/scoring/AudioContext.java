3bd6098a9e6bfa180bcf20d4930db37ded7bd207
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-26 17:25:39 -0500

    simplify writeAudioFile pipeline call
    Former-commit-id: d1504b25001688b4932bdb6333d5c5a153dea0c2 [formerly 45dbaf3b8ff1fe1a48150d38445f08c2e91af622] [formerly 06ea300acac1b3de2436579cb050747a0a289ff7 [formerly 165fb661d843aa61a0053bab7eb4bb275e9f8390]] [formerly d1504b25001688b4932bdb6333d5c5a153dea0c2 [formerly 45dbaf3b8ff1fe1a48150d38445f08c2e91af622] [formerly 06ea300acac1b3de2436579cb050747a0a289ff7 [formerly 165fb661d843aa61a0053bab7eb4bb275e9f8390]] [formerly d1504b25001688b4932bdb6333d5c5a153dea0c2 [formerly 45dbaf3b8ff1fe1a48150d38445f08c2e91af622] [formerly 06ea300acac1b3de2436579cb050747a0a289ff7 [formerly 165fb661d843aa61a0053bab7eb4bb275e9f8390]] [formerly 65c7db3eebb588ea30ab995000de1400bd9c1968 [formerly ed15915011b88eaf5ca2b8a847779ba5fd74ea45] [formerly 5c5fcfe358cbf5bf7700de0d1f51304183bc2a05 [formerly 32d31004660f11c32952917c4000d2ab9c164a6d]]]]]
    Former-commit-id: 63806ef3d6c583496d7dc038d81b3c829947ae6b [formerly 474e1ccf65a26d229eb9172a8202a0ea762c42e8] [formerly 731d410a52058649b2a2b044221166d59e51994b [formerly 5b85ee71d6d87c1eebd77ef463707f595ce283a8]] [formerly 63806ef3d6c583496d7dc038d81b3c829947ae6b [formerly 474e1ccf65a26d229eb9172a8202a0ea762c42e8] [formerly 731d410a52058649b2a2b044221166d59e51994b [formerly 5b85ee71d6d87c1eebd77ef463707f595ce283a8]] [formerly ac8193b07d5c5071566a1e367ba954d6b109c4c3 [formerly ac2c608b8cae4f74df003d43cb4b4cbb29238694] [formerly 5240024e1ae8069f8090e036141bda222552cf16 [formerly 41aeb198c878851897814def295285d53181731f]]]] [formerly ed14a6c73bff139e4fc494d5bf9651a34cce2778 [formerly fbb5f1bca5876187f91cb1bd681e94a528f13ab4] [formerly 576b3845b28bf1173d12a95714342da0b4a1f04f [formerly f17b44b9ffb305b0d87a9ffdd89a5f48a8ecba2b]] [formerly ed14a6c73bff139e4fc494d5bf9651a34cce2778 [formerly fbb5f1bca5876187f91cb1bd681e94a528f13ab4] [formerly 576b3845b28bf1173d12a95714342da0b4a1f04f [formerly f17b44b9ffb305b0d87a9ffdd89a5f48a8ecba2b]] [formerly 1874df1ae5976f43319a64dc354c4f12fdfe3e2f [formerly 29c148e7407e645af16500a1a77e36c674595ed1] [formerly ce3e9c7c4c3dbe82b394156f4d7f8c1b016c2a17 [formerly 1029204ad387089070c157670358ea47cf2cf011]]]]]
    Former-commit-id: ed14a6c73bff139e4fc494d5bf9651a34cce2778 [formerly fbb5f1bca5876187f91cb1bd681e94a528f13ab4] [formerly 576b3845b28bf1173d12a95714342da0b4a1f04f [formerly f17b44b9ffb305b0d87a9ffdd89a5f48a8ecba2b]]
    Former-commit-id: 1ad32fc060755bb6b6b90f27e55af454ee8de109 [formerly f80e73c3570d0a2bb0c072c2664cc0280a6e4d56] [formerly 24359e3c1d004c10db1f191a7f301cf39fd49967 [formerly f068f7a898b62c08ca1e1eb9e2cd56bbb34cce66]] [formerly c979356b5662f8c88a48cf680d8199d2889484f8 [formerly dd51516b7b09e587d080304c15b8a55cdb75851a] [formerly e320484054904adec5dbb095761c00db5c2dc61e [formerly 6d8eed2373e3dd9141c54075e8051c2c03dd3fc5]]]
    Former-commit-id: de4cb405a1a2ab216f4099a64e9e9ecbf11e6e9b [formerly 5612cab77468e04a548333bbe8d459160b3331fc] [formerly 7efad805efa37965e8bb0a793dd5a5da81fdfbd1 [formerly b7c26616ac07591c0444a08c5d3bb9e0cd2c7191]]
    Former-commit-id: f9d430fdac91a5904c3c2c75820a7b2465b850f8 [formerly 8876fe859797b7f07d78edfefed39dd7b6839280]
    Former-commit-id: 8883fe90d43d1fda9910ea6eadf8d10441b55111

---
+++ src/mitll/langtest/shared/scoring/AudioContext.java
@@ -0,0 +1,57 @@
+package mitll.langtest.shared.scoring;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by go22670 on 2/26/16.
+ */
+public class AudioContext  implements IsSerializable {
+  private int reqid;
+
+  private int userid;
+  private String id;
+  private int questionID;
+  private String audioType;
+
+  /**
+   * @param userid
+   * @param id
+   * @param questionID
+   * @param audioType
+   * @see
+   */
+  public AudioContext(int reqid,
+                      int userid,
+                      String id,
+                      int questionID,
+                      String audioType) {
+    this.reqid = reqid;
+    this.userid = userid;
+    this.id = id;
+    this.questionID = questionID;
+    this.audioType = audioType;
+  }
+
+  public AudioContext() {
+  }
+
+  public int getUserid() {
+    return userid;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public int getQuestionID() {
+    return questionID;
+  }
+
+  public int getReqid() {
+    return reqid;
+  }
+
+  public String getAudioType() {
+    return audioType;
+  }
+}
