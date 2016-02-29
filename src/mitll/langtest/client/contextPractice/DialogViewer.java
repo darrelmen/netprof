9ade0b92f467eaa31df8c5b109108053cda4469f
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-02-29 13:55:09 -0500

    made the dialog practice live in a separate javascript code split
    Former-commit-id: 8aace1237c149bfc0c013b01d869b3a4a6adaf54
    Former-commit-id: c7b9f0308684f682eb0fe1f4104809ef25c68ed0

---
+++ src/mitll/langtest/client/contextPractice/DialogViewer.java
@@ -0,0 +1,10 @@
+package mitll.langtest.client.contextPractice;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+/**
+ * Created by go22670 on 2/29/16.
+ */
+public interface DialogViewer {
+  void viewDialog(final HasWidgets contentPanel);
+}
