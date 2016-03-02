f30f5d99c65e169fa33e69e18205796eb9d7f65c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-02 15:05:26 -0500

    moved recording progress table out into separate table
    Former-commit-id: 9cb836d4fff85fa2ed90ef2af5ad59fa38574940 [formerly 686e4791d3dceedfdec04ffe7a5f9103d32bbf1b] [formerly fba108f2f90cf90fb5d05dddaabf208db064d842 [formerly 129ef878e880c294c0663bec9520e35ec7944da1]]
    Former-commit-id: a0c5cce530351a9dab547a9b9aa96718de6d466a [formerly 98a6f54c9d0ce68bffc8378abeb9539160b162bd]
    Former-commit-id: 7795f55d8a3047dc95a41f5425000ac3e5661d34

---
+++ src/mitll/langtest/client/custom/RecordingProgressTable.java
@@ -0,0 +1,78 @@
+package mitll.langtest.client.custom;
+
+import com.google.gwt.user.client.ui.FlexTable;
+import mitll.langtest.server.database.AudioDAO;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Shows recording progress in a table.
+ * Created by go22670 on 3/2/16.
+ */
+public class RecordingProgressTable extends FlexTable {
+  private static final String MALE_CONTEXT = AudioDAO.MALE_CONTEXT;
+  private static final String FEMALE_SLOW = AudioDAO.FEMALE_SLOW;
+  private static final String FEMALE_FAST = AudioDAO.FEMALE_FAST;
+  private static final String FEMALE_CONTEXT = AudioDAO.FEMALE_CONTEXT;
+  private static final String FEMALE = AudioDAO.FEMALE;
+  private static final String MALE_SLOW = AudioDAO.MALE_SLOW;
+  private static final String MALE = AudioDAO.MALE;
+  private static final String MALE_FAST = AudioDAO.MALE_FAST;
+
+  public void populate(Map<String, Float> result) {
+    float total = result.get("total");
+
+    getElement().setId("RecordingProgressTable");
+
+    int r = 0;
+    int col = 0;
+
+    List<String> labels = Arrays.asList("Male ", "regular", "slow");
+    List<String> keys = Arrays.asList(MALE, MALE_FAST, MALE_SLOW);
+    for (int i = 0; i < labels.size(); i++) {
+      col = setLabelAndVal(result, total, r, col, labels.get(i), keys.get(i));
+    }
+
+    labels = Arrays.<String>asList("Female ", "regular", "slow");
+    keys = Arrays.asList(FEMALE, FEMALE_FAST, FEMALE_SLOW);
+    col = 0;
+    r++;
+    for (int i = 0; i < labels.size(); i++) {
+      col = setLabelAndVal(result, total, r, col, labels.get(i), keys.get(i));
+    }
+
+    labels = Arrays.<String>asList("Context Male ", "Female");
+    keys = Arrays.asList(MALE_CONTEXT, FEMALE_CONTEXT);
+    col = 0;
+    r++;
+    for (int i = 0; i < labels.size(); i++) {
+      col = setLabelAndVal(result, total, r, col, labels.get(i), keys.get(i));
+    }
+  }
+
+  int setLabelAndVal(Map<String, Float> result, float total, int r, int col, String label, String key) {
+    setHTML(r, col++, label);
+    col = setCol(result, total, r, col, key);
+    return col;
+  }
+
+  int setCol(Map<String, Float> result, float total, int r, int col, String male) {
+    String sp = "&nbsp;";
+    String p = "<b>";
+    String s = "</b>" + sp;
+
+    setHTML(r, col++, sp + result.get(male).intValue() + "");
+    int percent = getPercent(result.get(male), total);
+
+    setHTML(r, col++, p + "<span" + ((percent == 100) ? " style='color:green'":"") +
+        ">"+percent + "%</span>" + s);
+    return col;
+  }
+
+  private int getPercent(Float male, float total) {
+    float ratio = total > 0 ? male / (total) : 0;
+    return (int) (ratio * 100f);
+  }
+}
