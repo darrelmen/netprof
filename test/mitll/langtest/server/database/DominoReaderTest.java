031b674c3bdca5094bf29773ec0bb321cc1dc0a3
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-07 16:32:09 -0500

    showed could read from domino url
    Former-commit-id: 4f564e26db363bcdeb58f1e0c0efc093f4334dd3

--- test/mitll/langtest/server/database/DominoReaderTest.java
+++ test/mitll/langtest/server/database/DominoReaderTest.java
@@ -2,25 +2,20 @@ package mitll.langtest.server.database;
 
 import mitll.langtest.server.PathHelper;
 import mitll.langtest.server.ServerProperties;
-import mitll.langtest.server.audio.AudioFileHelper;
-import mitll.langtest.shared.amas.AmasExerciseImpl;
-import mitll.langtest.shared.amas.QAPair;
+import mitll.langtest.shared.SectionNode;
+import mitll.langtest.shared.StartupInfo;
 import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.*;
 
 /**
  * Created by go22670 on 2/17/16.
  */
 public class DominoReaderTest {
   private static final Logger logger = Logger.getLogger(DominoReaderTest.class);
- // public static final String AM_LB_002 = "AM-LB-002";
   private static DatabaseImpl database;
 
   @BeforeClass
@@ -33,30 +28,24 @@ public class DominoReaderTest {
     String parent = file.getParent();
     String name = file.getName();
 
-    logger.debug("config dir " + parent +" config     " + name);
+    logger.debug("config dir " + parent + " config     " + name);
     ServerProperties serverProps = new ServerProperties(parent, name);
     database = new DatabaseImpl(parent, name, serverProps.getH2Database(), serverProps, new PathHelper("war"), false, null);
-   // logger.debug("made " + database);
+    // logger.debug("made " + database);
     database.setInstallPath("war", parent + File.separator + database.getServerProps().getLessonPlan(),
         serverProps.getMediaDir());
   }
 
+  public StartupInfo getStartupInfo() {
+    return new StartupInfo(database.getServerProps().getProperties(), database.getTypeOrder(), database.getSectionNodes());
+  }
+
   @Test
   public void testMe() {
-    //database.getAMASSectionHelper().report();
-
     Collection<?> exercises = database.getExercises();
-
-/*    Stream<AmasExerciseImpl> amasExerciseStream = exercises.stream().filter(ex -> ex.getID().equals("AM-LA-004"));
-    Optional<AmasExerciseImpl> first = amasExerciseStream.findFirst();
-    AmasExerciseImpl amasExercise = first.get();
-    logger.info("first " + first+  " audio '" + amasExercise.getAudioURL() + "'");
-    QAPair next1 = amasExercise.getForeignLanguageQuestions().iterator().next();
-    logger.info("q " + next1);*/
     Iterator<?> iterator = exercises.iterator();
     Object next2 = iterator.next();
 
-   // AmasExerciseImpl next = (AmasExerciseImpl) next2;
     logger.info("\n\ngot " + exercises.size());
     logger.info("e.g. " + next2);
     logger.info("e.g. " + iterator.next());
@@ -64,12 +53,13 @@ public class DominoReaderTest {
     logger.info("e.g. " + iterator.next());
     logger.info("e.g. " + iterator.next());
 
+    StartupInfo startupInfo = getStartupInfo();
+    logger.info("Got " + startupInfo);
+    SectionNode next = startupInfo.getSectionNodes().iterator().next();
+    Map<String, Collection<String>> typeToSection = new HashMap<>();
+    typeToSection.put(next.getType(), Collections.singleton(next.getName()));
+    Collection exercisesForSelectionState = database.getSectionHelper().getExercisesForSelectionState(typeToSection);
 
-
-//    AudioFileHelper audioFileHelper = new AudioFileHelper(new PathHelper("war"), database.getServerProps(), database, null);
-
-//    audioFileHelper.makeAutoCRT(".");
-
-  //  database.getAMASSectionHelper().report();
+    logger.info("got " + exercisesForSelectionState.size() + " e.g. " + exercisesForSelectionState.iterator().next());
   }
 }
