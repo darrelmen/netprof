e50393f0a4a06e010ce2e4abfbbc25a1b6d162fb
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-11 13:57:40 -0500

    shows domino doc ids, handles complete new items and legacy items
    Former-commit-id: d0d644e72dd4b6fa46752c5dccd0784f2d37e7cd [formerly cf65f6fe280a4bb71871c4135e6d1504e975a89b] [formerly d0d644e72dd4b6fa46752c5dccd0784f2d37e7cd [formerly cf65f6fe280a4bb71871c4135e6d1504e975a89b] [formerly 5fe04242a0ed16163d8a552caec97713d412b2ea]]
    Former-commit-id: 8f62927e1ffbab53455692d6b7901caaf1cedaae [formerly d1fbfcee45f807bfe677f241034c672f4afc8af7] [formerly 5bc26f592707f879bf4dbc4db91a99fc44293b80] [formerly b371630bad940348ddd2b3480d0552ac72be8afa [formerly e2bc0d283bd7fb4cb4463f09f22529d032d1ebbe] [formerly 306450061a5ee616da4b4e7b35e3405dab9e2103]]
    Former-commit-id: b371630bad940348ddd2b3480d0552ac72be8afa [formerly e2bc0d283bd7fb4cb4463f09f22529d032d1ebbe]
    Former-commit-id: 2a5797079c9aff53715132c943ebb84dff900e70
    Former-commit-id: 42b033d5dd7c0606b469b284bfe1242610e18ccf

--- src/mitll/langtest/server/database/exercise/JSONURLExerciseDAO.java
+++ src/mitll/langtest/server/database/exercise/JSONURLExerciseDAO.java
@@ -9,16 +9,20 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
+ * Read from a domino url - either documents or exam
+ *
+ * For exam, maybe we need a "latestExam" link?
+ *
  * Created by go22670 on 2/10/16.
  */
 public class JSONURLExerciseDAO extends JSONExerciseDAO {
   private static final Logger logger = Logger.getLogger(JSONURLExerciseDAO.class);
+  public static final String DOCUMENTS = "documents";
   private final Collection<String> typeOrder;
 
   /**
@@ -30,6 +34,31 @@ public class JSONURLExerciseDAO extends JSONExerciseDAO {
       boolean addDefects) {
     super("", serverProps, userListManager, addDefects);
     this.typeOrder = serverProps.getTypes();
+
+    readProjectInfo(serverProps);
+  }
+
+  /**
+   * TODO :  Consider reading font from project info
+   * @param serverProps
+   */
+  void readProjectInfo(ServerProperties serverProps) {
+    String baseURL = serverProps.getLessonPlan();
+
+    if (baseURL.endsWith(DOCUMENTS)) {
+      baseURL = baseURL.substring(0, baseURL.length() - DOCUMENTS.length());
+    }
+//    else if (baseURL.endsWith("exam"))
+    String projectInfo = new HTTPClient().readFromGET(baseURL);
+    JSONObject jsonObject = JSONObject.fromObject(projectInfo);
+    JSONObject language = jsonObject.getJSONObject("language");
+    String upperName = language.getString("name");
+
+    // TODO : consider setting language here.
+    String npLang = upperName.substring(0,1) + upperName.substring(1).toLowerCase();
+    //  logger.info("got language " + npLang);
+    boolean isLTR = language.get("direction").equals("LTR");
+    serverProps.setRTL(!isLTR);
   }
 
   @Override
@@ -44,7 +73,7 @@ public class JSONURLExerciseDAO extends JSONExerciseDAO {
     return Collections.emptyList();
   }
 
-  private String getJSON() throws IOException {
+  private String getJSON() {
     logger.info(serverProps.getLanguage() + " Reading from " + serverProps.getLessonPlan());
     this.now = System.currentTimeMillis();
     return new HTTPClient().readFromGET(serverProps.getLessonPlan());
@@ -62,13 +91,15 @@ public class JSONURLExerciseDAO extends JSONExerciseDAO {
 
   private static final SimpleDateFormat dateFmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-  long now = System.currentTimeMillis();
+  private long now = System.currentTimeMillis();
 
   private Exercise toExercise(JSONObject jsonObject) {
     JSONObject metadata = jsonObject.getJSONObject("metadata");
     JSONObject content = jsonObject.getJSONObject("content");
     String updateTime = jsonObject.getString("updateTime");
-    String npDID = metadata.getString("npDID");
+    String dominoID = ""+jsonObject.getInt("id");
+    boolean isLegacy = metadata.containsKey("npDID");
+    String npDID = isLegacy ? metadata.getString("npDID") : dominoID;
 
     long updateMillis = now;
     try {
@@ -78,27 +109,28 @@ public class JSONURLExerciseDAO extends JSONExerciseDAO {
       logger.warn(e.getMessage() + " : can't parse date '" + updateTime + "' for " + npDID);
     }
 
-    String fl = content.getString("pass");
-    String english = content.getString("trans");
-    String meaning = content.getString("meaning");
-    String transliteration = content.getString("translit");
+    String fl = noMarkup(content.getString("pass"));
+    String english = noMarkup(content.getString("trans"));
+    String meaning = noMarkup(content.getString("meaning"));
+    String transliteration = noMarkup(content.getString("translit"));
 
-    String context = content.getString("context");
-    String contextTranslation = content.getString("context_trans");
+    String context = noMarkup(content.getString("context"));
+    String contextTranslation = noMarkup(content.getString("context_trans"));
 
     Exercise exercise = new Exercise(
         npDID,
         english,
-        meaning,
         fl,
+        meaning,
         transliteration,
         context,
-        contextTranslation);
+        contextTranslation, dominoID);
     exercise.setUpdateTime(updateMillis);
+    if (!isLegacy) logger.info("NOT LEGACY " + exercise);
 
     for (String type : typeOrder) {
       try {
-        exercise.addUnitToValue(type, content.getString(type.toLowerCase()));
+        exercise.addUnitToValue(type, noMarkup(content.getString(type.toLowerCase())));
       } catch (Exception e) {
         logger.error("couldn't find unit/chapter '" + type + "' in content - see typeOrder property");
       }
@@ -106,4 +138,6 @@ public class JSONURLExerciseDAO extends JSONExerciseDAO {
 
     return exercise;
   }
+
+  private String noMarkup(String source) { return source.replaceAll("\\<.*?>","");}
 }
