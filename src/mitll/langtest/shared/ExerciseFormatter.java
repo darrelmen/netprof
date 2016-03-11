a23c8c621ce7eb57a2145e4b7982ccb91fe1fc92
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-11 13:57:40 -0500

    shows domino doc ids, handles complete new items and legacy items
    Former-commit-id: 5a3c6b05c0896b807dd9d2000255871db0c22c94 [formerly eeccdf319b40d7aa2df0c23ec8d7e19857b99f89] [formerly 6b375761f4350dda96db639ed003dc264d45fcde]
    Former-commit-id: 0c439e85ff79b38eff65399f2b98f05989032dd6
    Former-commit-id: a59fb027ef5d2f7ef9c40ff735e735eb65386ead

--- src/mitll/langtest/shared/ExerciseFormatter.java
+++ src/mitll/langtest/shared/ExerciseFormatter.java
@@ -4,8 +4,6 @@
 
 package mitll.langtest.shared;
 
-import mitll.langtest.shared.exercise.CommonExercise;
-
 /**
  * Created with IntelliJ IDEA.
  * User: GO22670
@@ -15,88 +13,14 @@ import mitll.langtest.shared.exercise.CommonExercise;
  */
 public class ExerciseFormatter {
   public static final String FOREIGN_LANGUAGE_PROMPT = "Say:";
-  public static final String ENGLISH_PROMPT = "Meaning:";
+  public static final String MEANING_PROMPT = "Meaning:";
+  public static final String ENGLISH_PROMPT = "Translation:";
   public static final String TRANSLITERATION = "Transliteration:";
- // private static final String TRANSLATION = "Translation:";
   public static final String CONTEXT = "Context:";
   public static final String CONTEXT_TRANSLATION = "Context Translation: ";
 
   /**
-   * @see mitll.langtest.server.database.exercise.ExcelImport#getExercise(String, String, String, String, String, String, boolean, String)
-   * @see mitll.langtest.shared.custom.UserExercise#toExercise()
-   * @param foreignPhrase
-   * @param translit
-   * @param english
-   * @param meaning
-   * @param language
-   * @return
-   */
-/*  public static String getContent(String foreignPhrase, String translit, String english, String meaning, String context,
-                                  String contextTranslation, String language) {
-    return getContent(foreignPhrase, translit, english, meaning, context, contextTranslation,
-      language.equalsIgnoreCase("english"),
-      language.equalsIgnoreCase("urdu"),
-      language.equalsIgnoreCase("pashto"));
-  }*/
-
-/*
-  public static String getContent(String foreignPhrase, String translit, String english, String meaning,
-                                  boolean isEnglish, boolean isUrdu, boolean isPashto) {
-    return getContent(foreignPhrase, translit, english, meaning, "", "", isEnglish, isUrdu, isPashto);
-  }
-*/
-
-  /**
-   * TODO : This is a bad idea -- the client side should do formatting, etc.
-   *
-   * @param foreignPhrase
-   * @param translit
-   * @param english
-   * @param meaning
-   * @param context
-   * @param isEnglish
-   * @param isUrdu
-   * @param isPashto
-   * @return
-   */
-/*
-  private static String getContent(String foreignPhrase, String translit, String english, String meaning, String context,
-                                  String contextTranslation, boolean isEnglish, boolean isUrdu, boolean isPashto) {
-    String arabicHTML = getArabic(foreignPhrase, isUrdu, isPashto, false);
-    String translitHTML = translit.length() > 0 ? getSpanWrapper(TRANSLITERATION, translit, false) : "";
-
-    String translationHTML = english != null && english.length() > 0 && !english.equals(foreignPhrase) ?
-      getSpanWrapper(TRANSLATION, english, false) : "";
-
-    String meaningHTML = (isEnglish && meaning.length() > 0) ? getSpanWrapper(ENGLISH_PROMPT, meaning, false) : "";
-
-    String contextHTML = (context.length() > 0) ? getSpanWrapper(CONTEXT, "\""+ context + "\"", false) : "";
-    
-    String contextTranslationHTML = (contextTranslation.length() > 0) ? getSpanWrapper(CONTEXT_TRANSLATION, "\""+ contextTranslation + "\"", false) : "";
-
-    return arabicHTML +
-      translitHTML +
-      translationHTML +
-      meaningHTML +
-      contextHTML +
-      contextTranslationHTML;
-  }
-*/
-
-/*  private static String getSpanWrapper(String rowTitle, String english, boolean includePrompt) {
-    String prompt = includePrompt ? "<span class=\"Instruction-title\">" +
-      rowTitle +
-      "</span>\n" : "";
-    String textClasses = "\"Instruction-data" + (rowTitle.equals(CONTEXT) ? " englishFont" : "")+ "\"";
-    return "<div class=\"Instruction\">\n" +
-      prompt +
-      "<span class=" + textClasses + "> " + english +
-      "</span>\n" +
-      "</div>";
-  }*/
-
-  /**
-   * @see mitll.langtest.client.exercise.WaveformExercisePanel#getExerciseContent(CommonExercise)
+   * @see mitll.langtest.client.exercise.WaveformExercisePanel#getExerciseContent
    * @param arabic
    * @return
    */
