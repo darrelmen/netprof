33a4fc9055f1e73a0c1df85d1e7798b4ad0c5230
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-24 19:36:52 -0400

    finally fixed the bug where the playback would not align with the word or phone alignment

--- src/mitll/langtest/client/sound/CompressedAudio.java
+++ src/mitll/langtest/client/sound/CompressedAudio.java
@@ -1,23 +1,30 @@
 package mitll.langtest.client.sound;
 
-import mitll.langtest.client.AudioTag;
+import mitll.langtest.client.BrowserCheck;
+
+import java.util.logging.Logger;
 
 /**
  * Created by go22670 on 3/23/16.
  */
 public class CompressedAudio {
+  private final Logger logger = Logger.getLogger("CompressedAudio");
+
+  private static final String OGG = ".ogg";
   private static final String WAV = ".wav";
-  private static final String MP3 = "." + AudioTag.COMPRESSED_TYPE;
+  private static final String MP3 = ".mp3";
 
-
+  private boolean isIE = BrowserCheck.getIEVersion() > 0;
   public static native boolean supportsOgg() /*-{
       if (typeof $wnd.Modernizr == "undefined") {
           console.log("no modernizr?");
           return false;
       }
       if ($wnd.Modernizr.audio.ogg) {
+          //console.log("Can play ogg - ");
           return true;
       } else {
+          console.log("Can NOT play ogg - ");
           return false;
           // not-supported
       }
@@ -29,7 +36,10 @@ public class CompressedAudio {
   }
 
   public String getPathNoSlashChange(String path) {
-    return (path.endsWith(WAV)) ? path.replace(WAV, supportsOgg() ? ".ogg" : MP3) : path;
+    boolean b1 = supportsOgg();
+    boolean useOGG = !isIE && b1;
+ //   logger.info("using ogg " + useOGG + " is IE  " + isIE   + " supports OGG " + b1 + " checking " + path);
+    return (path.endsWith(WAV)) ? path.replace(WAV, useOGG ? OGG : MP3) : (!useOGG && path.endsWith(OGG) ? path.replace(OGG,MP3) : path);
   }
 
   private String ensureForwardSlashes(String wavPath) {
