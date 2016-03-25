b242890594389952ca2c4f251e4942b65921abdc
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-25 19:27:20 -0400

    showed could read/write event object via postgres
    Former-commit-id: af967c28a1f26bb18fac228981b2820e0e32f295 [formerly 435ebf260a82dad89ffa1e3461bc2bda80c9506e]
    Former-commit-id: 2db65e7a80c56d993cb2bfedce9788e5a93c2da0

---
+++ src/mitll/langtest/server/database/excel/EventDAOToExcel.java
@@ -0,0 +1,110 @@
+package mitll.langtest.server.database.excel;
+
+import mitll.langtest.server.LogAndNotify;
+import mitll.langtest.server.database.Database;
+import mitll.langtest.shared.instrumentation.Event;
+import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by go22670 on 3/25/16.
+ */
+public class EventDAOToExcel {
+  private static final Logger logger = Logger.getLogger(EventDAOToExcel.class);
+
+  private static final String EVENTS = "Events";
+  private static final List<String> COLUMNS2 = Arrays.asList("id", "type", "exercise", "context", "userid", "timestamp", "time_millis", "hitID", "device");
+
+  protected final LogAndNotify logAndNotify;
+
+  public static final String ID = "ID";
+
+  public EventDAOToExcel(Database database) {
+    this.logAndNotify = database.getLogAndNotify();
+  }
+  /**
+   * @see mitll.langtest.server.DownloadServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+   * @param out
+   */
+  public void toXLSX(List<Event> all,OutputStream out) {
+    long then = System.currentTimeMillis();
+
+    //List<Event> all = getAll();
+    long now = System.currentTimeMillis();
+    if (now-then > 100) logger.info("toXLSX : took " + (now - then) + " millis to read " + all.size() +
+        " events from database");
+    then = now;
+
+    //Workbook wb = new XSSFWorkbook();
+    SXSSFWorkbook wb = new SXSSFWorkbook(1000); // keep 100 rows in memory, exceeding rows will be flushed to disk
+
+    Sheet sheet = wb.createSheet(EVENTS);
+    int rownum = 0;
+    Row headerRow = sheet.createRow(rownum++);
+    for (int i = 0; i < COLUMNS2.size(); i++) {
+      Cell headerCell = headerRow.createCell(i);
+      headerCell.setCellValue(COLUMNS2.get(i));
+    }
+
+    CellStyle cellStyle = wb.createCellStyle();
+    DataFormat dataFormat = wb.createDataFormat();
+
+    cellStyle.setDataFormat(dataFormat.getFormat("MMM dd HH:mm:ss"));
+
+    for (Event event : all) {
+      Row row = sheet.createRow(rownum++);
+      int j = 0;
+      Cell cell = row.createCell(j++);
+      cell.setCellValue(event.getWidgetID());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getWidgetType());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getExerciseID());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getContext());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getCreatorID());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(new Date(event.getTimestamp()));
+      cell.setCellStyle(cellStyle);
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getTimestamp());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getHitID());
+
+      cell = row.createCell(j++);
+      cell.setCellValue(event.getDevice());
+
+    }
+    now = System.currentTimeMillis();
+    if (now-then > 100) logger.warn("toXLSX : took " + (now-then) + " millis to write " + rownum+
+        " rows to sheet, or " + (now-then)/rownum + " millis/row");
+    then = now;
+    try {
+      wb.write(out);
+      now = System.currentTimeMillis();
+      if (now-then > 100) {
+        logger.warn("toXLSX : took " + (now-then) + " millis to write excel to output stream ");
+      }
+      out.close();
+      wb.dispose();
+    } catch (IOException e) {
+      logger.error("got " +e,e);
+      logAndNotify.logAndNotifyServerException(e);
+    }
+  }
+}
---
+++ src/mitll/langtest/server/database/hibernate/SessionManagement.java
@@ -0,0 +1,180 @@
+package mitll.langtest.server.database.hibernate;
+
+import mitll.langtest.server.database.connection.PostgreSQLConnection;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Created by go22670 on 3/25/16.
+ */
+public class SessionManagement {
+  private static final Logger logger = Logger.getLogger(SessionManagement.class);
+
+  private SessionFactory sessionFactory;
+
+  public SessionManagement(String dbName) {
+    makeFactory(dbName);
+  }
+
+  private void makeFactory(String dbName) {
+    StandardServiceRegistryBuilder configure = new StandardServiceRegistryBuilder()
+        .configure();
+
+    logger.info("using " + dbName);
+
+    doSpecialConfig(dbName, configure);
+    final StandardServiceRegistry registry = configure // configures settings from hibernate.cfg.xml
+        .build();
+
+    logger.debug("built the registry ");
+
+    try {
+      Metadata metadata = new MetadataSources(registry).buildMetadata();
+      sessionFactory = metadata.buildSessionFactory();
+      logger.info("made factory " + sessionFactory);
+    } catch (Exception e) {
+      logger.error("got " + e, e);
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy(registry);
+    }
+  }
+
+  protected void doSpecialConfig(String dbName, StandardServiceRegistryBuilder configure) {
+    configure.applySetting("hibernate.connection.url", "jdbc:postgresql://localhost/" + dbName);
+    create(dbName);
+  }
+
+  protected void create(String dbName) {
+    try {
+      Class.forName("org.postgresql.Driver").newInstance();
+    } catch (Exception e) {
+      logger.error("Got " + e, e);
+    }
+    try {
+      String host = "localhost";
+
+      Connection conn = DriverManager.getConnection(
+          "jdbc:postgresql://" +
+              host +
+              ":5432/", PostgreSQLConnection.DEFAULT_USER,
+          "");
+
+    //  String dbName1 = dbName.toLowerCase();
+      createDatabaseForgiving(dbName, conn);
+    } catch (SQLException e) {
+      logger.error("Got " + e, e);
+    }
+  }
+
+  void createDatabaseForgiving(String dbName, Connection conn) throws SQLException {
+    try {
+      createDatabase(dbName, conn);
+    } catch (SQLException e) {
+      if (!e.getMessage().contains("already exists")) {
+        throw e;
+      }
+      else logger.info("Got " +e.getMessage());
+    }
+  }
+
+  void createDatabase(String dbName, Connection conn) throws SQLException {
+    Statement s2 = conn.createStatement();
+    int myResult = s2.executeUpdate("CREATE DATABASE " + dbName);
+    s2.close();
+    conn.close();
+    logger.info("Got " + myResult);
+
+  }
+
+  public void close() throws Exception {
+    if (sessionFactory != null) {
+      sessionFactory.close();
+    }
+  }
+
+  public void doInTranscation(Consumer<Session> operations) {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    operations.accept(session);
+    session.getTransaction().commit();
+    session.close();
+  }
+
+  public <T> T getFromTransaction(SessionSupplier<T> supplier) {
+    // now lets pull events from the database and list them
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    supplier.setSession(session);
+    T t = supplier.get();
+//    List result = session.createQuery( "from Event" ).list();
+//    for ( Event event : (List<Event>) result ) {
+//      System.out.println( "Event (" + event.getDate() + ") : " + event.getTitle() );
+//    }
+    session.getTransaction().commit();
+    session.close();
+    return t;
+  }
+
+  public abstract static class SessionSupplier<T> implements Supplier<T> {
+    protected Session session;
+
+    //    public SessionSupplier() {}
+    //public SessionSupplier(Session session) {this.session = session;}
+    public abstract T get();
+
+    public void setSession(Session session) {
+      this.session = session;
+    }
+  }
+
+  public static void main(String[] arg) {
+    try {
+      SessionManagement sessionManagement = new SessionManagement("testagain");
+
+      sessionManagement.doInTranscation((session) -> {
+        session.save(new AnnotationEvent("Our very first event!", new Date()));
+        session.save(new AnnotationEvent("A follow up event", new Date()));
+      });
+
+      AnnotationEvent ret = sessionManagement.getFromTransaction(new SessionSupplier<AnnotationEvent>() {
+        @Override
+        public AnnotationEvent get() {
+          List<AnnotationEvent> result = session.createQuery("from AnnotationEvent").list();
+          return result.isEmpty() ? null : result.get(0);
+        }
+      });
+
+      logger.info("got " + ret);
+
+      List<AnnotationEvent> ret2 = sessionManagement.getFromTransaction(new SessionSupplier<List<AnnotationEvent>>() {
+        @Override
+        public List<AnnotationEvent> get() {
+          List<AnnotationEvent> result = session.createQuery("from AnnotationEvent").list();
+          return result;
+        }
+      });
+
+      logger.info("everything " + ret2);
+
+      sessionManagement.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+}
