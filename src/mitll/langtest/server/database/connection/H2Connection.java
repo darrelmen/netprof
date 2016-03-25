af967c28a1f26bb18fac228981b2820e0e32f295
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-25 19:27:20 -0400

    showed could read/write event object via postgres
    Former-commit-id: 435ebf260a82dad89ffa1e3461bc2bda80c9506e

--- src/mitll/langtest/server/database/connection/H2Connection.java
+++ src/mitll/langtest/server/database/connection/H2Connection.java
@@ -1,7 +1,6 @@
 /*
  * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
  */
-
 package mitll.langtest.server.database.connection;
 
 import mitll.langtest.server.LogAndNotify;
@@ -34,7 +33,7 @@ public class H2Connection implements DatabaseConnection {
   private static final int MAX_MEMORY_ROWS = 1000000;
   private static final int maxMemoryRows = MAX_MEMORY_ROWS;
   private static final boolean USE_MVCC = false;
-  LogAndNotify logAndNotify;
+  private LogAndNotify logAndNotify;
 
   /**
    *
