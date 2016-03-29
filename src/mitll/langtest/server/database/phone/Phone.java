3fae5b5f73dc0e8f30cfa8c82712f49fdcaf9dcd
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-29 18:41:07 -0400

    steps toward converting PhoneDAO to use hibernate

---
+++ src/mitll/langtest/server/database/phone/Phone.java
@@ -0,0 +1,86 @@
+package mitll.langtest.server.database.phone;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by go22670 on 3/29/16.
+ */
+@Entity
+public class Phone {
+  private Long id;
+
+  private long wid;
+  private long rid;
+  private String phone;
+  private int seq;
+  private float score;
+
+  public Phone() {}
+
+  public Phone(long rid, long wid, String phone, int seq, float score) {
+    this.rid = rid;
+    this.wid = wid;
+    this.phone = phone;
+    this.seq = seq;
+    this.score = score;
+  }
+
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  public Long getId() {
+    return id;
+  }
+  private void setId(Long id) {
+    this.id = id;
+  }
+
+  public String toString() {
+    return // "# " + id +
+        " rid " + rid + " wid " + wid + " : " + phone + " at " + seq + " score " + score;
+  }
+
+  public long getWid() {
+    return wid;
+  }
+
+//  @ManyToOne
+//  @JoinColumn(name="result_id",foreignKey = @ForeignKey(name = "RESULT_ID_FK"))
+  public long getRid() {
+    return rid;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public int getSeq() {
+    return seq;
+  }
+
+  public float getScore() {
+    return score;
+  }
+
+  private void setWid(long wid) {
+    this.wid = wid;
+  }
+
+  private void setRid(long rid) {
+    this.rid = rid;
+  }
+
+  private void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  private void setSeq(int seq) {
+    this.seq = seq;
+  }
+
+  private void setScore(float score) {
+    this.score = score;
+  }
+}
