54ccb50dd609660881642af59392ecb8fd555d9b
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-03-30 14:58:38 -0400

    added ability to filter on default audio
    Former-commit-id: 2ea61889e801564d3c3a8506c7e5cb2222bb536a [formerly e437e2f8aab2a9dbc528847c559ed839c40dd787] [formerly 7d1e57f3ada16371cc7ca3273f53c219b97a65b1 [formerly 4d4a7389bb017c16ad686a6e3f5c1ff75e945c23]]
    Former-commit-id: ba5228bcaa4f0dcc0c31703bc4fb90f8379ea8c2 [formerly b24f609e6260d41d08449ef37a663838e3cf545e] [formerly cda922b88af0ea6452feaa15c3196298d1f292e4 [formerly c120e297d82ddd8dceb59cae46b48a354128b40e]]
    Former-commit-id: edd574776361285bae8146afe1ef0a2b0caa7b84 [formerly 12de78b1c722e99ed0e837341823c69941a22be7]
    Former-commit-id: 4c665c5fbf10caf8efec602cdae55ed972810cae

--- src/mitll/langtest/client/scoring/GoodwaveExercisePanel.java
+++ src/mitll/langtest/client/scoring/GoodwaveExercisePanel.java
@@ -273,7 +273,7 @@ public abstract class GoodwaveExercisePanel<T extends CommonShell & AudioRefExer
     String path = e.getRefAudio() != null ? e.getRefAudio() : e.getSlowAudioRef();
 
     if (path != null) {
-      path = compressedAudio.getPathNoSlashChange(path);
+      path = CompressedAudio.getPathNoSlashChange(path);
     }
     contentAudio = getAudioPanel(path);
     contentAudio.setScreenPortion(screenPortion);
@@ -295,7 +295,7 @@ public abstract class GoodwaveExercisePanel<T extends CommonShell & AudioRefExer
     return new FastAndSlowASRScoringAudioPanel(getLocalExercise(), path, service, controller, scorePanel, instance);
   }
 
-  private CompressedAudio compressedAudio = new CompressedAudio();
+  //private CompressedAudio compressedAudio = new CompressedAudio();
 
   /**
    * @param commentToPost
