4f04bc7decf63b59f312bc3b81877317defd3923
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-04-01 20:05:35 -0400

    fixed issue with reloading content for amas

--- src/mitll/langtest/server/database/exercise/SimpleExerciseDAO.java
+++ src/mitll/langtest/server/database/exercise/SimpleExerciseDAO.java
@@ -2,7 +2,6 @@ package mitll.langtest.server.database.exercise;
 
 import mitll.langtest.server.database.DatabaseImpl;
 import mitll.langtest.server.database.UserManagement;
-import mitll.langtest.shared.amas.AmasExerciseImpl;
 import mitll.langtest.shared.exercise.CommonShell;
 
 import java.util.List;
