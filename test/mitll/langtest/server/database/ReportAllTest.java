7b4d4d97f6c3d8108d2b6e2764a78afd401b5c70
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-04-11 16:47:01 -0400

    added MergeTest to support merging data from egyptian into egyptian candidate
    Former-commit-id: 3e87741962556cded17fc6fff371589cd55e3fbf [formerly 9a1b93510c39a2c1951a9fb66622b0b944c63b8b] [formerly 3e87741962556cded17fc6fff371589cd55e3fbf [formerly 9a1b93510c39a2c1951a9fb66622b0b944c63b8b] [formerly 371b5c4630596080f8e1807aed16bc4989daf4e9 [formerly 6838c62ec2447e3ba7842032b11c80f9e1fae705]]]
    Former-commit-id: c083039d246b989d948be5972fe0d31c383ff490 [formerly a53af7daabff57baf4f006517698715236180d75] [formerly 0d8f3dc7583ea095c9b426e99b840db6ed169014 [formerly 5c059337e4e740232fd19047e019d4d3ece48130]] [formerly 8a13394ae47991b038c3503500ec4f4f03e13276 [formerly 741c82398b9f4a50531de745dd6abc5e52105212] [formerly 09ab67aa7ea27371158cf3d8df24588bf9caea2b [formerly a44acdbf289dfbdc398b16635135554ed8a6b7df]]]
    Former-commit-id: 8a13394ae47991b038c3503500ec4f4f03e13276 [formerly 741c82398b9f4a50531de745dd6abc5e52105212]
    Former-commit-id: 163885b4965e468e000bf81835d3fa6ca5437cd0 [formerly e48f40f552a7da98f95831d3e6fd7f5d73a77d43]
    Former-commit-id: b2f3ffcdcf6b79177fcde459e72f1a59285ccaa0

--- test/mitll/langtest/server/database/ReportAllTest.java
+++ test/mitll/langtest/server/database/ReportAllTest.java
@@ -19,7 +19,7 @@ public class ReportAllTest extends BaseTest {
   private static final Logger logger = Logger.getLogger(ReportAllTest.class);
   public static final boolean DO_ONE = false;
 
-  private static DatabaseImpl getDatabase(String config) {
+  protected static DatabaseImpl getDatabase(String config) {
     File file = new File("war" + File.separator + "config" + File.separator + config + File.separator + "quizlet.properties");
     String parent = file.getParent();
     String name = file.getName();
