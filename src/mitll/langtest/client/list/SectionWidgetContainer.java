948cbdb9b552d71e916f0d26c177d164042b7194
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-04-27 15:32:55 -0400

    fixes for history tokens and search - now we can use forward and back buttons and paste a URL into another tab and recover the selection
    Former-commit-id: 0be62ad6735dc5cad00afd7daa69b69937a617b3 [formerly 68bf87fc089beab11a34add21a1bb3e18e4e5795] [formerly d835f0877ab61b8619258b819a8960647212da58]
    Former-commit-id: 1bf3ea43d7da48946969291f807f651b9c2f5070
    Former-commit-id: c4e2a2308a10875cc44add013c8af18293146236

---
+++ src/mitll/langtest/client/list/SectionWidgetContainer.java
@@ -0,0 +1,106 @@
+package mitll.langtest.client.list;
+
+import com.google.gwt.user.client.History;
+import mitll.langtest.client.amas.SingleSelectExerciseList;
+import mitll.langtest.client.exercise.SectionWidget;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by go22670 on 4/27/16.
+ */
+public class SectionWidgetContainer<T extends SectionWidget> {
+  protected final Map<String, T> typeToBox = new HashMap<>();
+
+  public T getWidget(String type) {
+    return typeToBox.get(type);
+  }
+
+  public void setWidget(String type, T value) {
+    typeToBox.put(type, value);
+  }
+
+  /**
+   * @param search
+   * @param id
+   * @return
+   * @see ExerciseList#pushNewItem(String, String)
+   * @see HistoryExerciseList#pushNewSectionHistoryToken()
+   */
+  protected String getHistoryToken() {
+    if (typeToBox.isEmpty()) {
+      return History.getToken();
+    }
+    //logger.info("getHistoryToken for " + id + " examining " +typeToBox.size() + " boxes.");
+    StringBuilder unitAndChapterSelection = new StringBuilder();
+    for (String type : getTypes()) {
+      String section = getCurrentSelection(type);
+      if (!section.equals(HistoryExerciseList.ANY)) {
+        unitAndChapterSelection.append(type + "=" + section + ";");
+      }
+    }
+    return unitAndChapterSelection.toString();
+  }
+
+  /**
+   * @param type
+   * @return
+   * @see PagingExerciseList#getHistoryToken(String, String)
+   */
+  private String getCurrentSelection(String type) {
+    return typeToBox.get(type).getCurrentSelection();
+  }
+
+  public Collection<String> getTypes() {
+    return typeToBox.keySet();
+  }
+
+  public Collection<T> getValues() { return typeToBox.values(); }
+
+  public boolean hasType(String type) {
+    return typeToBox.containsKey(type);
+  }
+
+  /**
+   * Make sure all sections have a selection - quiz, test type, ilr level
+   *
+   * @return
+   * @see SingleSelectExerciseList#gotEmptyExerciseList()
+   * @see SingleSelectExerciseList#gotSelection()
+   * @see SingleSelectExerciseList#restoreListFromHistory
+   */
+  protected int getNumSelections() {
+    int count = 0;
+    // logger.info("type now " + typeToSelection);
+    for (T widget : typeToBox.values()) {
+      boolean hasSelection = !widget.getCurrentSelections().isEmpty();
+      //logger.info("getNumSelections : widget " + widget.getType() + " value now " + currentSelection);
+      if (hasSelection) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+
+  /**
+   * @param type
+   * @param sections
+   * @see #restoreListBoxState(SelectionState)
+   */
+  protected void selectItem(String type, Collection<String> sections) {
+    typeToBox.get(type).selectItem(sections, false);
+  }
+
+
+  public void clear() {
+    typeToBox.clear();
+  }
+
+  public void clearSelections() {
+    //logger.info("clearSelections");
+    for (String type : typeToBox.keySet()) typeToBox.get(type).clearAll();
+  }
+}
