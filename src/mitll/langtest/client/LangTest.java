dae9654022eeb3eea5ca72b205e00be5443cab84
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-04-28 16:15:15 -0400

    changes to support history across unit and chapter selections
    Former-commit-id: 23826a566644c7f070f84f05fc0507afe2f7b26c [formerly c1a25869ce764395e89700edf51d7d7e041977fa] [formerly 8d9acaf58f2276cd61aade299242e705cc25f3b6 [formerly 88c2c49a3757fc54d1fc613fb7bf27e71c7993f7]] [formerly 9dfc5beb96d95ca02590c7f6896bd74c8ecdd19c]
    Former-commit-id: a79107554e0dd1188123699ca0ce877e06652643
    Former-commit-id: e16754b1699450750aa3205250c48863e37e9d16 [formerly 2e4f9ba5b19074f831bb6c57ba1c19dc35aa7ca4]
    Former-commit-id: 33ceab8b6da491a3045d76ef41e7b14a6d433492

--- src/mitll/langtest/client/LangTest.java
+++ src/mitll/langtest/client/LangTest.java
@@ -97,11 +97,13 @@ import java.util.logging.Logger;
  * - Updated reporting to show all years, fill in month/week gaps with zeros
  * 1.2.10
  * - More small report changes
+ * 1.2.11
+ * - Fixed bugs with browser history forwards/backwards and clicking on characters
  */
 public class LangTest implements EntryPoint, UserFeedback, ExerciseController, UserNotification {
   private final Logger logger = Logger.getLogger("LangTest");
 
-  public static final String VERSION_INFO = "1.2.10";
+  public static final String VERSION_INFO = "1.3.0";
 
   private static final String VERSION = "v" + VERSION_INFO + "&nbsp;";
 
