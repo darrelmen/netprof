df03f590354fcff0844daa3fcdace6acdab4eb53
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-05-03 17:46:07 -0400

    partial attempts to fix broken download link bug
    Former-commit-id: c5c2cef77ae574f2e2341111f8ad86234e71927c [formerly 2b048427a17a5215fa03e3860cda1003d98f6d17]
    Former-commit-id: 00d6d64f42b560fe9df422b12a95a9e58053b2dd

--- src/mitll/langtest/client/custom/DownloadLink.java
+++ src/mitll/langtest/client/custom/DownloadLink.java
@@ -10,13 +10,19 @@ import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.Anchor;
 import mitll.langtest.client.exercise.ExerciseController;
+import mitll.langtest.shared.custom.UserList;
+import mitll.langtest.shared.exercise.HasID;
 
 /**
  * Created by go22670 on 10/20/15.
+ * @see ListManager#makeTabContent(UserList, String, HasID)
  */
 class DownloadLink {
+  public static final String DOWNLOAD_AUDIO = "downloadAudio";
+  public static final String DOWNLOAD_SPREADSHEET_AND_AUDIO = "Download spreadsheet and audio for list.";
+
   private final ExerciseController controller;
-  public DownloadLink(ExerciseController controller) {
+  DownloadLink(ExerciseController controller) {
     this.controller = controller;
   }
   /**
@@ -28,7 +34,7 @@ class DownloadLink {
    */
   public Anchor getDownloadLink(long listid, String linkid, final String name) {
     final Anchor downloadLink = new Anchor(getURLForDownload(listid));
-    new TooltipHelper().addTooltip(downloadLink, "Download spreadsheet and audio for list.");
+    new TooltipHelper().addTooltip(downloadLink, DOWNLOAD_SPREADSHEET_AND_AUDIO);
     downloadLink.addClickHandler(new ClickHandler() {
       @Override
       public void onClick(ClickEvent event) {
@@ -49,7 +55,7 @@ class DownloadLink {
     SafeHtmlBuilder sb = new SafeHtmlBuilder();
     sb.appendHtmlConstant("<a class='" + "icon-download" +
         "' href='" +
-        "downloadAudio" +
+        DOWNLOAD_AUDIO +
         "?list=" + listid +
         "'" +
         ">");
