4bae0810a68229761d519fd6a27f89e4ac99be67
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-05-03 19:32:57 -0400

    Fix for bug where couldn't jump from word in analysis if a chapter was selected, if word was not within the chapter
    Former-commit-id: 54853e6fffae8f7f798952b95a21916470787051 [formerly 0a7f5c8f5939880765aa51d1fdc8ca2097a00f05] [formerly d54ce02edf1d4298439485b49cc84d848fb10f8e]
    Former-commit-id: 3d7e2f31c5c739eff8cf3f44ba07df7f5f6fe2bc [formerly 54f72040235004379aec001666eb87ff94680126]
    Former-commit-id: 0f8ae49dece8f593d307828647a9a91efdf193b6

--- src/mitll/langtest/client/exercise/SectionWidget.java
+++ src/mitll/langtest/client/exercise/SectionWidget.java
@@ -6,6 +6,7 @@ package mitll.langtest.client.exercise;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.google.gwt.user.client.ui.Widget;
+import mitll.langtest.client.list.SectionWidgetContainer;
 
 import java.util.Collection;
 import java.util.List;
@@ -21,8 +22,10 @@ public interface SectionWidget {
   String getCurrentSelection();
   void clearSelectionState();
 
- // void selectItem(Collection<String> section, boolean doToggle);
-
+  /**
+   * @see mitll.langtest.client.bootstrap.ButtonGroupSectionWidget#selectItem(Collection)
+   * @see SectionWidgetContainer#clearSelections()
+   */
   void clearAll();
   void clearEnabled();
   void enableAll();
