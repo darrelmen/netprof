0db22c1e97d5bf9798d5f9884dae6a4ce8016c49
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-05-18 18:07:48 -0400

    updated distro and file header
    Former-commit-id: 703e073670dc9a1c1534c59205d97842e0750926 [formerly df3ca4ad8a128edc5f8e52bf1c15e4019f81fb22] [formerly f7841475045025cad85bb2826263cae9c77c299c [formerly 2b8d1f2fe44f0e6a0c684509b865d82df772de49]] [formerly 703e073670dc9a1c1534c59205d97842e0750926 [formerly df3ca4ad8a128edc5f8e52bf1c15e4019f81fb22] [formerly f7841475045025cad85bb2826263cae9c77c299c [formerly 2b8d1f2fe44f0e6a0c684509b865d82df772de49]] [formerly d0a2aa78e66d173d3c0c3becf0d391baee3c8f12]]
    Former-commit-id: 86812f3759ca346a93560a247ded88d9ce63bfcf [formerly d0a66ddf81938f20be38442a15b76cd43e67e8c8] [formerly 19a7551e99deb91949f7184925651e93fe353c59 [formerly 0514eaf07a57334c55a0d1df388aeb8930572544]] [formerly 454d9262be1828f5010fbbf6f305b5ce39adfeb5] [formerly 0549a8194d0ca51a4380360bc4ec70e31883544d [formerly c4ee327d4dbfc7d30f2cb84d48a2a73a704c9011] [formerly 2aa41ba59784532b03580497f43592658abf6052 [formerly 36066e55cb537138135a25e18342f959eba1a01a]] [formerly ec51379fb4caf23ca57c17fd803e0260d0803019]]
    Former-commit-id: 0549a8194d0ca51a4380360bc4ec70e31883544d [formerly c4ee327d4dbfc7d30f2cb84d48a2a73a704c9011] [formerly 2aa41ba59784532b03580497f43592658abf6052 [formerly 36066e55cb537138135a25e18342f959eba1a01a]]
    Former-commit-id: b20a8cd349b91172a2a10041870fce94faa56321
    Former-commit-id: ac5e713845e7d4c918d918d024531daf48b6ef29 [formerly 6d0304fc2227dd6f4566706714cf123d6cfad335]
    Former-commit-id: ee9f2cb635d997a73ebaf7455aec69f9da874b6e

--- src/mitll/langtest/client/sound/PlayAudioWidget.java
+++ src/mitll/langtest/client/sound/PlayAudioWidget.java
@@ -1,5 +1,33 @@
 /*
- * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ *
+ * DISTRIBUTION STATEMENT C. Distribution authorized to U.S. Government Agencies
+ * and their contractors; 2015. Other request for this document shall be referred
+ * to DLIFLC.
+ *
+ * WARNING: This document may contain technical data whose export is restricted
+ * by the Arms Export Control Act (AECA) or the Export Administration Act (EAA).
+ * Transfer of this data by any means to a non-US person who is not eligible to
+ * obtain export-controlled data is prohibited. By accepting this data, the consignee
+ * agrees to honor the requirements of the AECA and EAA. DESTRUCTION NOTICE: For
+ * unclassified, limited distribution documents, destroy by any method that will
+ * prevent disclosure of the contents or reconstruction of the document.
+ *
+ * This material is based upon work supported under Air Force Contract No.
+ * FA8721-05-C-0002 and/or FA8702-15-D-0001. Any opinions, findings, conclusions
+ * or recommendations expressed in this material are those of the author(s) and
+ * do not necessarily reflect the views of the U.S. Air Force.
+ *
+ * © 2015 Massachusetts Institute of Technology.
+ *
+ * The software/firmware is provided to you on an As-Is basis
+ *
+ * Delivered to the US Government with Unlimited Rights, as defined in DFARS
+ * Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice,
+ * U.S. Government rights in this work are defined by DFARS 252.227-7013 or
+ * DFARS 252.227-7014 as detailed above. Use of this work other than as specifically
+ * authorized by the U.S. Government may violate any copyrights that exist in this work.
+ *
+ *
  */
 
 package mitll.langtest.client.sound;
@@ -12,32 +40,15 @@ import mitll.langtest.client.instrumentation.EventRegistration;
 
 /**
  * Audio widgets like the little ones at <a href="http://www.schillmania.com/projects/soundmanager2/">Sound Manager 2</a>
- * Created by go22670 on 7/25/14.
+ * Copyright &copy; 2011-2016 Massachusetts Institute of Technology, Lincoln Laboratory
+ *
+ * @author <a href="mailto:gordon.vidaver@ll.mit.edu">Gordon Vidaver</a>
+ * @since 7/25/14.
  */
 public class PlayAudioWidget {
   public static native void addPlayer() /*-{
       $wnd.basicMP3Player.init();
   }-*/;
-
-  /**
-   * Super simple audio widget
-   * @param path
-   * @return
-   */
-/*  public Anchor getAudioWidget(String path) {
-    return getAudioWidget(path, "play");
-  }*/
-
-  /**
-   * Add event tracking - every play is logged
-   * @param path
-   * @param title
-   * @param eventRegistration
-   * @return
-   */
-/*  public Anchor getAudioWidgetWithEventRecording(String path, String title, EventRegistration eventRegistration) {
-    return getAudioWidgetWithEventRecording(path, title, "N/A", eventRegistration);
-  }*/
 
   /**
    * Log with the exercise it's associated with.
@@ -88,5 +99,4 @@ public class PlayAudioWidget {
   }
 
   private static String ensureForwardSlashes(String wavPath) {  return wavPath.replaceAll("\\\\", "/"); }
-
 }
