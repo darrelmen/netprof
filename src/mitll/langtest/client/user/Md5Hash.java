8e52a4801362f2801e0749aacd9fa87f913c6a82
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-05-18 18:07:48 -0400

    updated distro and file header
    Former-commit-id: 7486eedfe2dee9e182d33c479f4f3cb17cb07efd [formerly 828bfbacde6a91a60bbbfd2661456a3122bad517] [formerly 7486eedfe2dee9e182d33c479f4f3cb17cb07efd [formerly 828bfbacde6a91a60bbbfd2661456a3122bad517] [formerly d0a2aa78e66d173d3c0c3becf0d391baee3c8f12]]
    Former-commit-id: d0d89a03ea9aba535dcd9afb96e81a3eceeb145e [formerly 46a83989770170b3e0b3bc3c8af44348a3aeb617] [formerly 454d9262be1828f5010fbbf6f305b5ce39adfeb5] [formerly bea779078720610ab7c40853da02c25c8d3e7837 [formerly ff3e6bd917957668df988aeb4ded0aed7e2aab31] [formerly ec51379fb4caf23ca57c17fd803e0260d0803019]]
    Former-commit-id: bea779078720610ab7c40853da02c25c8d3e7837 [formerly ff3e6bd917957668df988aeb4ded0aed7e2aab31]
    Former-commit-id: b20a8cd349b91172a2a10041870fce94faa56321
    Former-commit-id: 7accd6ebdea2d7f0564f3821ee536f131daf824a

--- src/mitll/langtest/client/user/Md5Hash.java
+++ src/mitll/langtest/client/user/Md5Hash.java
@@ -1,5 +1,33 @@
 /*
- * Copyright © 2011-2015 Massachusetts Institute of Technology, Lincoln Laboratory
+ *
+ * DISTRIBUTION STATEMENT C. Distribution authorized to U.S. Government Agencies
+ * and their contractors; 2015. Other request for this document shall be referred
+ * to DLIFLC.
+ *
+ * WARNING: This document may contain technical data whose export is restricted
+ * by the Arms Export Control Act (AECA) or the Export Administration Act (EAA).
+ * Transfer of this data by any means to a non-US person who is not eligible to
+ * obtain export-controlled data is prohibited. By accepting this data, the consignee
+ * agrees to honor the requirements of the AECA and EAA. DESTRUCTION NOTICE: For
+ * unclassified, limited distribution documents, destroy by any method that will
+ * prevent disclosure of the contents or reconstruction of the document.
+ *
+ * This material is based upon work supported under Air Force Contract No.
+ * FA8721-05-C-0002 and/or FA8702-15-D-0001. Any opinions, findings, conclusions
+ * or recommendations expressed in this material are those of the author(s) and
+ * do not necessarily reflect the views of the U.S. Air Force.
+ *
+ * © 2015 Massachusetts Institute of Technology.
+ *
+ * The software/firmware is provided to you on an As-Is basis
+ *
+ * Delivered to the US Government with Unlimited Rights, as defined in DFARS
+ * Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice,
+ * U.S. Government rights in this work are defined by DFARS 252.227-7013 or
+ * DFARS 252.227-7014 as detailed above. Use of this work other than as specifically
+ * authorized by the U.S. Government may violate any copyrights that exist in this work.
+ *
+ *
  */
 
 package mitll.langtest.client.user;
@@ -7,6 +35,12 @@ package mitll.langtest.client.user;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Copyright &copy; 2011-2016 Massachusetts Institute of Technology, Lincoln Laboratory
+ *
+ * @author <a href="mailto:gordon.vidaver@ll.mit.edu">Gordon Vidaver</a>
+ * @since 10/2/13
+ */
 public class Md5Hash {
   /**
    * Generate MD5 digest.
@@ -28,7 +62,8 @@ public class Md5Hash {
   }
 
   private static final char[] HEX_CHARS =
-      new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+      new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
   private static String toHexString(byte[] bytes) {
     char[] hexString = new char[2 * bytes.length];
     int j = 0;
@@ -41,15 +76,5 @@ public class Md5Hash {
 
   public static String getHash(String toHash) {
     return toHexString(Md5Hash.getMd5Digest(toHash.getBytes()));
-
   }
-/*  public static void main(String []arg) {
-    String bueller = getHash("bueller");
-    System.out.println("hash " + bueller + " " + bueller.length());
-    String first = "074036D6CAEB34B3A23BEA17A449B05C";
-    System.out.println("hash " + first + " " + first.length());
-
-     first = "398E13BF1B80EC28F41E73AB791E276B";
-    System.out.println("hash " + first + " " + first.length());
-  }*/
 }
