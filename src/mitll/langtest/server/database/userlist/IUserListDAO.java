9e8090d9737e94f20ed83fb158403ede2824bb1a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-06-16 19:46:08 -0400

    added user list dao
    Former-commit-id: 3403f0044e453c5261609c414c148ad57c0c41f1 [formerly 544133dae8e6a52e7ed2b314bcc68c011f6e41d1]
    Former-commit-id: 0a62a2c70d1549ffa9de7bbcc2eeec54ae60259b

---
+++ src/mitll/langtest/server/database/userlist/IUserListDAO.java
@@ -0,0 +1,70 @@
+/*
+ *
+ * DISTRIBUTION STATEMENT C. Distribution authorized to U.S. Government Agencies
+ * and their contractors; 2015. Other request for this document shall be referred
+ * to DLIFLC.
+ *
+ * WARNING: This document may contain technical data whose export is restricted
+ * by the Arms Export Control Act (AECA) or the Export Administration Act (EAA).
+ * Transfer of this data by any means to a non-US person who is not eligible to
+ * obtain export-controlled data is prohibited. By accepting this data, the consignee
+ * agrees to honor the requirements of the AECA and EAA. DESTRUCTION NOTICE: For
+ * unclassified, limited distribution documents, destroy by any method that will
+ * prevent disclosure of the contents or reconstruction of the document.
+ *
+ * This material is based upon work supported under Air Force Contract No.
+ * FA8721-05-C-0002 and/or FA8702-15-D-0001. Any opinions, findings, conclusions
+ * or recommendations expressed in this material are those of the author(s) and
+ * do not necessarily reflect the views of the U.S. Air Force.
+ *
+ * © 2015 Massachusetts Institute of Technology.
+ *
+ * The software/firmware is provided to you on an As-Is basis
+ *
+ * Delivered to the US Government with Unlimited Rights, as defined in DFARS
+ * Part 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice,
+ * U.S. Government rights in this work are defined by DFARS 252.227-7013 or
+ * DFARS 252.227-7014 as detailed above. Use of this work other than as specifically
+ * authorized by the U.S. Government may violate any copyrights that exist in this work.
+ *
+ *
+ */
+
+package mitll.langtest.server.database.userlist;
+
+import mitll.langtest.server.database.userexercise.IUserExerciseDAO;
+import mitll.langtest.shared.custom.UserList;
+import mitll.langtest.shared.exercise.CommonShell;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface IUserListDAO {
+  void addVisitor(long listid, long userid);
+
+  void add(UserList userList);
+
+  void updateModified(long uniqueID);
+
+  int getCount();
+
+  List<UserList<CommonShell>> getAllByUser(long userid);
+
+  List<UserList<CommonShell>> getAllPublic(long userid);
+
+  boolean hasByName(long userid, String name);
+
+  List<UserList<CommonShell>> getByName(long userid, String name);
+
+  boolean remove(long unique);
+
+  UserList<CommonShell> getWithExercises(long unique);
+
+  UserList<CommonShell> getWhere(long unique, boolean warnIfMissing);
+
+  Collection<UserList<CommonShell>> getListsForUser(long userid);
+
+  void setUserExerciseDAO(IUserExerciseDAO userExerciseDAO);
+
+  void setPublicOnList(long userListID, boolean isPublic);
+}
