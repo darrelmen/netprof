df68528c814dca9b39053e499fbac3443b651c1c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-06-20 18:57:13 -0400

    Fix for bugs in writing and reading audio paths via answer and result dao
    Former-commit-id: c1a03088762f16177ae1547568329e7cdba962ce [formerly b9838643c53960e3e9dd6893ae724dbd119bd7ae]
    Former-commit-id: ca26ef62c1999c4e70be26b26d06ebfc78c07b95

--- src/mitll/langtest/server/database/connection/PostgreSQLConnection.java
+++ src/mitll/langtest/server/database/connection/PostgreSQLConnection.java
@@ -63,7 +63,7 @@ public class PostgreSQLConnection implements DatabaseConnection {
    * @param dbName
    * @paramx configDir
    * @paramx mustAlreadyExist
-   * @see mitll.langtest.server.database.DatabaseImpl#DatabaseImpl(String, String, String, ServerProperties, PathHelper, boolean, LogAndNotify)
+   * @see mitll.langtest.server.database.DatabaseImpl#DatabaseImpl(String, String, String, ServerProperties, PathHelper, boolean, LogAndNotify, boolean)
    */
   public PostgreSQLConnection(String dbName, LogAndNotify logAndNotify) {
     this.logAndNotify = logAndNotify;
