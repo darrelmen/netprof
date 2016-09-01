f3058d092e9d724f66759b359619a78d43f06e74
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-09-01 18:31:06 -0400

    refactored out a separate scoring service
    Former-commit-id: 6d28da499af5de60ee97bf6207644a919a7eb078

--- src/mitll/langtest/client/exercise/ExerciseController.java
+++ src/mitll/langtest/client/exercise/ExerciseController.java
@@ -40,6 +40,7 @@ import mitll.langtest.client.dialog.KeyPressHelper;
 import mitll.langtest.client.instrumentation.EventRegistration;
 import mitll.langtest.client.services.AudioServiceAsync;
 import mitll.langtest.client.services.QCServiceAsync;
+import mitll.langtest.client.services.ScoringServiceAsync;
 import mitll.langtest.client.sound.SoundManagerAPI;
 import mitll.langtest.client.user.UserFeedback;
 import mitll.langtest.shared.image.ImageResponse;
@@ -64,8 +65,17 @@ import java.util.Collection;
  */
 public interface ExerciseController extends EventRegistration {
   LangTestDatabaseAsync getService();
+
   AudioServiceAsync getAudioService();
+
   QCServiceAsync getQCService();
+
+  /**
+   * TODO : do we really need to do this?
+   * @return
+   */
+  ScoringServiceAsync getScoringService();
+
   UserFeedback getFeedback();
 
   String getBrowserInfo();
