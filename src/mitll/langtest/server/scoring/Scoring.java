06c96d6d4a6b193d91be21bb2cb1525404f04a5b
Jennifer Melot <jennifer.melot@ll.mit.edu>
2016-11-15 11:14:55 -0500

    Revert "Merge branch 'master' of https://g62code.llan.ll.mit.edu/gvidaver/netPron2"
    This reverts commit 6a182fec7839782de73f9d39c26512a455767eea [formerly a354cef338185910cf979d78c284e468cbc62296] [formerly 7ae30742a4fff91f0f207ca962ef88c36975882b [formerly eed2b7fa2d06472bc349be590fddadb0cc53cc56]] [formerly 69fc9812e51501e223d7cd07a02e048db8c52d2f [formerly c2a14c0a159b1f14c7224d177228b377e064f1ed] [formerly c0c889d9dafd7d1b53903a0fa6e7ad858ea77ec8 [formerly 165b6da96db8da8b642d65a59ca4765a344cc785]]], reversing
    changes made to 8ff7390258b31c8de02a6ef26f75293a55103fa4 [formerly c3eff83b600ab1675f82a0f5bc89f5cc1c41b59d] [formerly 52b5571374a7aade41cfef0deac69e0b7e3e96a0 [formerly 5d81b4075a9bf4150d0854e1ac5c2ed302082ee8]] [formerly a1eeb88a3e1da11cca179997fe95cc4c8a22484b [formerly 3dd66fa2c2277ae05560b5be2f2fd38eda1e38d1] [formerly 90624c7e056a392cc38bdc1a1c807a7b10b64548 [formerly 3fa8f4e96f40d2f3a508f518955bec344342f261]]].
    
    
    Former-commit-id: ba7422e2f40fc50a48b29588d10a306fd8646083 [formerly 7a3da33004a525a1c7c2868800e3af53d46574df] [formerly 90da64c5519ab867023a68de1d05c85cb9c48ab8 [formerly 84af62cddc0d783ddc299c27697d5e55b94967df]]
    Former-commit-id: 2dae68278379495c18ee1c619feedbb536752df3 [formerly cef28fb919c57339df9cd1f1418318fa06435dfc]
    Former-commit-id: ec65e2a2b34a72573a6c23dff1b9b32cb952feda

--- src/mitll/langtest/server/scoring/Scoring.java
+++ src/mitll/langtest/server/scoring/Scoring.java
@@ -419,12 +419,12 @@ public abstract class Scoring {
    * @return
    * @see mitll.langtest.server.scoring.Scoring#validLTS(String)
    */
-  Set<String> checkLTS(String foreignLanguagePhrase, String transliteration) {
-    return checkLTSHelper.checkLTS(foreignLanguagePhrase, transliteration);
+  Set<String> checkLTS(String foreignLanguagePhrase) {
+    return checkLTSHelper.checkLTS(foreignLanguagePhrase);
   }
 
-  public boolean validLTS(String fl, String transliteration) {
-    return checkLTS(fl, transliteration).isEmpty();
+  public boolean validLTS(String fl) {
+    return checkLTS(fl).isEmpty();
   }
 
   /**
