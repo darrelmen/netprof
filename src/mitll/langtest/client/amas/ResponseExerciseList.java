b1dc9ac5c78a93fff9a764d3d8ad1943298b1c3a
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-11-15 18:55:10 -0500

    made explicit whether a list should show check marks next to items or not
    Former-commit-id: 234528ebac17b5d2c1c39e3fa56acf843d735aca

--- src/mitll/langtest/client/amas/ResponseExerciseList.java
+++ src/mitll/langtest/client/amas/ResponseExerciseList.java
@@ -102,7 +102,7 @@ public class ResponseExerciseList extends SingleSelectExerciseList {
   public ResponseExerciseList(Panel secondRow, Panel currentExerciseVPanel, LangTestDatabaseAsync service,
                               UserFeedback feedback,
                               final ExerciseController controller, String instance) {
-    super(secondRow, currentExerciseVPanel, service, feedback, controller, instance, false);
+    super(secondRow, currentExerciseVPanel, service, feedback, controller, instance, false, true);
     innerContainer.getElement().getStyle().setPadding(5, Style.Unit.PX);
     String responseType = controller.getProps().getResponseType();
     responseTypeChanged(responseType);
