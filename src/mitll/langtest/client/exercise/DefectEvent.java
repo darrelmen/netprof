7b4af549179d8f144b38846714d5206989576961
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-11-22 18:01:45 -0500

    ensured consistency between fix defects and mark defects tabs
    Former-commit-id: a36667f1675b2a9d4fd10900b2036219a30d47b9

--- src/mitll/langtest/client/exercise/DefectEvent.java
+++ src/mitll/langtest/client/exercise/DefectEvent.java
@@ -7,7 +7,10 @@ import com.google.gwt.event.shared.GwtEvent;
  */
 public class DefectEvent extends GwtEvent<DefectEventHandler> {
   public static Type<DefectEventHandler> TYPE = new Type<DefectEventHandler>();
+  String source;
+  public DefectEvent(String source) { this.source = source;}
 
+  public String getSource() {return source;}
   @Override
   public Type<DefectEventHandler> getAssociatedType() {
     return TYPE;
