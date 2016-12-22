7bdac5df83826b0596cfcaa04bac38321ccfd48e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-12-22 15:31:11 -0500

    refreshed support for importing a config
    Former-commit-id: 627b0e5957fc456ec019a614b868014afd505b89 [formerly ce3041df47c2a5086db71c417e64012ce5e2d0d3]
    Former-commit-id: f7b28abbbe9cd495bb1fe26aea4a02ed57fc7dbe

---
+++ copy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -Djavax.net.ssl.keyStore=/etc/ssl/local/mongodb.jks -Djavax.net.ssl.keyStoreType=JKS -Djavax.net.ssl.keyStorePassword=changeit123 -Djavax.net.ssl.trustStore=/etc/ssl/local/cacerts -Djavax.net.ssl.trustStorePassword=changeit123 -cp WEB-INF/lib/*:WEB-INF/classes mitll.langtest.server.database.CopyToPostgres copy $1
