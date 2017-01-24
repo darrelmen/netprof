b3400749087a3dd8e20d4558730fbe95ebae4370
Jennifer Melot <jennifer.melot@ll.mit.edu>
2017-01-24 15:25:19 -0500

    ok at least something displays from angular, even if it isn't where I want it...
    Former-commit-id: 7d01d7488f9305be77d3675fe82fd46de3d3275f [formerly 647768bd45f2f47a70fda68a89f1bc6d647e969b]
    Former-commit-id: 9ca728cbfc1fecd1d95dc86ca6add55e34f4cb44

--- src/mitll/langtest/client/angularComponent/ACWindow.java
+++ src/mitll/langtest/client/angularComponent/ACWindow.java
@@ -1,5 +1,7 @@
 package mitll.langtest.client.angularComponent;
 
+import com.google.gwt.core.client.Callback;
+import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
@@ -8,6 +10,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import mitll.langtest.client.LangTestDatabaseAsync;
 import mitll.langtest.client.exercise.ExerciseController;
 import mitll.langtest.shared.ContextPractice;
+import com.google.gwt.user.client.Window;
 
 /**
  * Created by je24276 on 1/6/17.
@@ -21,11 +24,45 @@ public class ACWindow implements ACViewer{
     }
 
     public void viewAC(final HasWidgets contentPanel){
-        contentPanel.clear();
+        /*contentPanel.clear();
         final FlowPanel container = new FlowPanel();
-        HTML acContent = new HTML("Hi there! {{ test }}");
+        HTML acContent = new HTML("<div ng-app=\"myApp\">\n" +
+                "<div ng-controller=\"MyAppCtrl\">\n" +
+                "  {{variable}}\n" +
+                "  <button ng-click=\"increment()\">increment</button>\n" +
+                "</div>\n" +
+                "</div>\n");
+                //"<script type=\"text/javascript\" src=\"langtest/js/angular.js\"></script>\n"+
+                //"<script type=\"text/javascript\" src=\"langtest/js/simple-app.js\"></script>");
         container.add(acContent);
         contentPanel.add(container);
+        ScriptInjector.fromUrl("langtest/js/angular.js").setCallback(
+                new Callback<Void, Exception>() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Window.alert("Script load failed.");
+                    }
+
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        ScriptInjector.fromUrl("langtest/js/simple-app.js").setCallback(
+                                new Callback<Void, Exception>() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Window.alert("Script load failed.");
+                                    }
+
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Window.alert("Script load succeeded!!");
+
+                                    }
+                                }
+                        ).inject();
+                    }
+                }
+        ).inject();*/
+
     }
 
 }
