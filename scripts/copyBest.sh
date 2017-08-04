73fb1b4b4dc9c4b0307d34ca97e52448895e1b9e
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2017-08-04 19:38:32 -0400

    added copyBest script

---
+++ scripts/copyBest.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#sites=(npfClassroomCroatianEval)
+#$for file in ${sites[@]}
+for file in `ls ~/webapps | grep npfClass | grep -v war| grep -v Test | grep -v FrenchEval | grep -v SudaneseEval`;
+do
+lang=${file:12}
+langlc="${lang,,}"
+echo $langlc
+toPost=$langlc.tar.gz
+echo $toPost
+
+mkdir -p /data/tempBest/$langlc
+cd ~/webapps/$file
+
+tar cfz /data/tempBest/$langlc/$toPost bestAudio
+cd /data/tempBest/$langlc/
+split -b5G $toPost $langlc
+
+for file2 in `ls`
+do
+curl -ugvidaver -T $file2 "https://kws-bugs.ll.mit.edu/artifactory/dli-materials/NetProF-Audio/$langlc/bestAudio/$file2"
+done
+
+rm /data/tempBest/$langlc/*
+
+
+done
