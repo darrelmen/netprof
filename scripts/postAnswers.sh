3f0572ceeaae7d1304732e501f4c93cf2bcaa87c
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2017-09-13 14:57:57 -0400

    a little cleanup of scripts directory

--- scripts/postAnswers.sh
+++ scripts/postAnswers.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+#sites=(npfClassroomCroatianEval)
+#$for file in ${sites[@]}
 for file in `ls ~/webapps | grep npfClass | grep -v war| grep -v Test | grep -v FrenchEval | grep -v SudaneseEval`;
 do
 lang=${file:12}
 langlc="${lang,,}"
 echo $langlc
-toPost=$langlc.tar.gz
+suffix="Answers.tar.gz"
+toPost=$langlc$suffix
 echo $toPost
 
 mkdir -p /data/tempAnswers/$langlc
@@ -13,14 +16,16 @@ cd ~/webapps/$file
 
 tar cfz /data/tempAnswers/$langlc/$toPost answers
 cd /data/tempAnswers/$langlc/
-split -b5G $toPost $langlc
+#split -b5G $toPost $langlc
 
 for file2 in `ls`
 do
-curl -ugvidaver -T $file2 "https://kws-bugs.ll.mit.edu/artifactory/dli-materials/NetProF-Audio/$langlc/answersAudio/$file2"
+echo $file2
+echo curl -ugvidaver -T $file2 "https://kws-bugs.ll.mit.edu/artifactory/dli-materials/NetProF-Audio/$langlc/answersAudio/$file2"
 done
 
 rm /data/tempAnswers/$langlc/*
-
+#break;
 
 done
+
--- scripts/postBestFromNP.sh
+++ scripts/postBestFromNP.sh
@@ -2,6 +2,9 @@
 
 #sites=(npfClassroomCroatianEval)
 #$for file in ${sites[@]}
+
+date
+
 for file in `ls ~/webapps | grep npfClass | grep -v war| grep -v Test | grep -v FrenchEval | grep -v SudaneseEval`;
 do
 lang=${file:12}
@@ -15,14 +18,15 @@ cd ~/webapps/$file
 
 tar cfz /data/tempBest/$langlc/$toPost bestAudio
 cd /data/tempBest/$langlc/
-split -b5G $toPost $langlc
+#split -b5G $toPost $langlc
 
 for file2 in `ls`
 do
+date
 curl -ugvidaver -T $file2 "https://kws-bugs.ll.mit.edu/artifactory/dli-materials/NetProF-Audio/$langlc/bestAudio/$file2"
+date
 done
 
 rm /data/tempBest/$langlc/*
-
-
+date
 done
