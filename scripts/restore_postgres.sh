80e452ce15f4239a9dca83b567b92e2ac73030d9
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2018-04-27 19:44:36 -0400

    restore script update

--- scripts/restore_postgres.sh
+++ scripts/restore_postgres.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 date
-export PGPASSWORD=npadmin
-echo $PGPASSWORD
+#export PGPASSWORD=npadmin
+#echo $PGPASSWORD
 #PGPASSWORD=npadmin pg_dump -U netprof netprof --host 127.0.0.1 > ltea_data2.dump
-curl -ugvidaver:AP7UBZfNhCphhouwNrWyL2WqWX -O "https://kws-bugs.ll.mit.edu/artifactory/dli-archiving/ltea-data2-pg/daily/ltea_data2_dump.tar.gz"
+curl -ugvidaver:AP7UBZfNhCphhouwNrWyL2WqWX -o /tmp/netprof_pg.dump "https://kws-bugs.ll.mit.edu/artifactory/dli-archiving/ltea-data2-pg/daily/netprof_pg.dump"
 date
 #tar xvfz ltea_data2_dump.tar.gz
 #date
@@ -12,8 +12,26 @@ date
 #PGPASSWORD=npadmin psql -U netprof netprof_prod --host 127.0.0.1 < ltea_data2.dump
 #date
 
-PGPASSWORD=npadmin psql -U netprof netprof --host 127.0.0.1 < "drop database netprof_old;"
-PGPASSWORD=npadmin psql -U netprof netprof --host 127.0.0.1 < "alter database netprof rename to netprof_old;"
-PGPASSWORD=npadmin pg_restore -d netprof -U netprof -C netprof_pg.dump
+#PGPASSWORD=npadmin psql -U netprof netprof --host 127.0.0.1 -c "drop database netprof_old;"
+#PGPASSWORD=npadmin psql -U netprof netprof --host 127.0.0.1 < "alter database netprof rename to netprof_old;"
+#PGPASSWORD=npadmin pg_restore -d netprof -U netprof -C netprof_pg.dump
 
+
+# have to be postgres first
+sudo su - postgres
+
+# assumes postgres is owner of old netprof_old
+# assumes we've done this before
+date
+psql -U postgres --host 127.0.0.1 -c "drop database netprof_old;"
+
+# so if everything goes really badly we can go back, and lets us create a new netprof database
+date
+psql -U postgres --host 127.0.0.1 -c "alter database netprof rename to netprof_old;"
+
+# create database from dump
+date
+pg_restore -d netprof -U netprof -C /tmp/netprof_pg.dump
+
+date
 echo "Done!"
