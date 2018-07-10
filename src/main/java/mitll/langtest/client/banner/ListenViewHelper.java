088ffd5c4e8ce7cd362c4c3048e65fa3d6221246
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2018-07-10 14:26:53 -0400

    show header

--- src/main/java/mitll/langtest/client/banner/ListenViewHelper.java
+++ src/main/java/mitll/langtest/client/banner/ListenViewHelper.java
@@ -4,6 +4,7 @@ import com.github.gwtbootstrap.client.ui.Heading;
 import com.github.gwtbootstrap.client.ui.Image;
 import com.github.gwtbootstrap.client.ui.base.DivWidget;
 import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Panel;
@@ -17,18 +18,22 @@ import mitll.langtest.client.services.DialogServiceAsync;
 import mitll.langtest.shared.dialog.IDialog;
 import mitll.langtest.shared.dialog.IDialog.METADATA;
 import mitll.langtest.shared.exercise.ExerciseAttribute;
+import org.apache.xpath.operations.Div;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import static com.google.gwt.dom.client.Style.*;
+import static com.google.gwt.dom.client.Style.Unit.*;
 import static mitll.langtest.shared.dialog.IDialog.METADATA.*;
 
 /**
  * Created by go22670 on 4/5/17.
  */
 public class ListenViewHelper implements ContentView {
+  public static final int ROW_WIDTH = 97;
   private final Logger logger = Logger.getLogger("ListenViewHelper");
 
   ExerciseController controller;
@@ -66,53 +71,14 @@ public class ListenViewHelper implements ContentView {
       public void onSuccess(IDialog dialog) {
         List<ExerciseAttribute> attributes = dialog.getAttributes();
 
-        {
-          DivWidget rowOne = new DivWidget();
+        DivWidget outer = getHeader(dialog, attributes);
+        child.add(outer);
 
-          rowOne.setWidth("100%");
-          rowOne.getElement().getStyle().setBackgroundColor("#dff4fc");
-          rowOne.addStyleName("blueRow");
-          {
-            Heading w = new Heading(3, getAttrValue(attributes, FLPRESENTATION), getAttrValue(attributes, PRESENTATION));
-            w.setWidth("50%");
-            w.addStyleName("floatLeft");
-            w.getElement().getStyle().setBackgroundColor("#dff4fc");
-            rowOne.add(w);
-          }
+        DivWidget rowOne = new DivWidget();
+        rowOne.setHeight("40px");
+        rowOne.setWidth(98 +            "%");
 
-          {
-            Heading w1 = new Heading(3, dialog.getForeignLanguage(), dialog.getEnglish());
-            w1.addStyleName("rightAlign");
-            w1.addStyleName("floatRight");
-            w1.setWidth("50%");
-            w1.getElement().getStyle().setBackgroundColor("#dff4fc");
-            rowOne.add(w1);
-          }
-
-          child.add(rowOne);
-        }
-
-        {
-          DivWidget row = new DivWidget();
-
-          row.setWidth("100%");
-          {
-            com.google.gwt.user.client.ui.Image flag = getFlag(dialog.getImageRef());
-            flag.addStyleName("floatLeft");
-            row.add(flag);
-          }
-
-          {
-            Heading w1 = new Heading(3, dialog.getOrientation());
-            w1.addStyleName("rightAlign");
-            w1.addStyleName("floatRight");
-//            w1.setWidth("50%");
-  //          w1.getElement().getStyle().setBackgroundColor("#dff4fc");
-            row.add(w1);
-          }
-
-          child.add(row);
-        }
+        child.add(rowOne);
 
      /*   {
           DivWidget row2 = new DivWidget();
@@ -132,6 +98,74 @@ public class ListenViewHelper implements ContentView {
     });
   }
 
+  @NotNull
+  private DivWidget getHeader(IDialog dialog, List<ExerciseAttribute> attributes) {
+    DivWidget outer = new DivWidget();
+//        outer.getElement().getStyle().setBorderWidth(2,PX);
+//        outer.getElement().getStyle().setBorderColor("black");
+//        outer.getElement().getStyle().setBorderStyle(BorderStyle.SOLID);
+    {
+      DivWidget rowOne = new DivWidget();
+      rowOne.setHeight("40px");
+      rowOne.setWidth(98 +
+          "%");
+      rowOne.getElement().getStyle().setBackgroundColor("#dff4fc");
+      rowOne.addStyleName("blueRow");
+      {
+        Heading w = new Heading(3, getAttrValue(attributes, FLPRESENTATION), getAttrValue(attributes, PRESENTATION));
+        w.setWidth("49%");
+        w.addStyleName("floatLeft");
+        w.addStyleName("leftFiveMargin");
+        // w.addStyleName("bottomFiveMargin");
+        w.getElement().getStyle().setMarginTop(0, PX);
+
+        rowOne.add(w);
+      }
+
+      {
+        Heading w1 = new Heading(3, dialog.getForeignLanguage(), dialog.getEnglish());
+
+        w1.addStyleName("rightAlign");
+        w1.addStyleName("floatRight");
+        w1.addStyleName("rightFiveMargin");
+        w1.getElement().getStyle().setMarginTop(0, PX);
+
+        w1.setWidth("49%");
+        rowOne.add(w1);
+      }
+
+      rowOne.getElement().getStyle().setMarginBottom(10, PX);
+      outer.add(rowOne);
+    }
+
+    {
+      DivWidget row = new DivWidget();
+      row.addStyleName("cardBorderShadow");
+      row.setHeight("100px");
+      row.setWidth(ROW_WIDTH +
+          "%");
+      row.addStyleName("inlineFlex");
+      {
+        com.google.gwt.user.client.ui.Image flag = getFlag(dialog.getImageRef());
+        flag.addStyleName("floatLeft");
+        row.add(flag);
+      }
+
+      {
+        Heading w1 = new Heading(5, dialog.getOrientation());
+        w1.addStyleName("rightAlign");
+        w1.addStyleName("floatRight");
+        w1.addStyleName("rightTenMargin");
+        w1.addStyleName("wrapword");
+        w1.setWidth("85%");
+        //          w1.getElement().getStyle().setBackgroundColor("#dff4fc");
+        row.add(w1);
+      }
+      outer.add(row);
+    }
+    return outer;
+  }
+
 
   @NotNull
   private com.google.gwt.user.client.ui.Image getFlag(String cc) {
@@ -140,6 +174,7 @@ public class ListenViewHelper implements ContentView {
     image.setWidth("100px");
     return image;
   }
+
   private String getAttrValue(List<ExerciseAttribute> attributes, METADATA presentation) {
     ExerciseAttribute attr = getAttr((List<ExerciseAttribute>) attributes, (METADATA) presentation);
 
